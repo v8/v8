6bda17f12b4863ff1814b85a1b797210ff98f8e1
mads.s.ager@gmail.com <mads.s.ager@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2008-08-28 10:50:31 +0000

    Make test.py executable.
    Changed eol-style on debug-sourceinfo.js since the test expects CRLF
    endings.
    
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@27 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- tools/test.py
+++ tools/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#
 # Copyright 2008 Google Inc.  All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
