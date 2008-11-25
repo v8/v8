0974883a6e5fe95d58d6575947c95c80f76f4c03
christian.plesner.hansen@gmail.com <christian.plesner.hansen@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2008-11-25 14:26:45 +0000

    Made irregexp flags available in release mode
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@841 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -199,6 +199,13 @@ DEFINE_bool(usage_computation, true, "compute variable usage counts")
 DEFINE_bool(preemption, false,
             "activate a 100ms timer that switches between V8 threads")
 
+// irregexp
+DEFINE_bool(irregexp, false, "new regular expression code")
+DEFINE_bool(trace_regexps, false, "trace Irregexp execution")
+DEFINE_bool(trace_regexp_bytecodes, false, "trace Irregexp bytecode executon")
+DEFINE_bool(attempt_case_independent, false, "attempt to run Irregexp case independent")
+DEFINE_bool(irregexp_native, false, "use native code Irregexp implementation (IA32 only)")
+
 // Testing flags test/cctest/test-{flags,api,serialization}.cc
 DEFINE_bool(testing_bool_flag, true, "testing_bool_flag")
 DEFINE_int(testing_int_flag, 13, "testing_int_flag")
@@ -288,12 +295,6 @@ DEFINE_bool(debug_serialization, false,
 DEFINE_bool(collect_heap_spill_statistics, false,
             "report heap spill statistics along with heap_stats "
             "(requires heap_stats)")
-
-DEFINE_bool(irregexp, false, "new regular expression code")
-DEFINE_bool(trace_regexps, false, "trace Irregexp execution")
-DEFINE_bool(trace_regexp_bytecodes, false, "trace Irregexp bytecode executon")
-DEFINE_bool(attempt_case_independent, false, "attempt to run Irregexp case independent")
-DEFINE_bool(irregexp_native, false, "use native code Irregexp implementation (IA32 only)")
 
 //
 // Logging and profiling only flags
