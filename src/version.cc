bbabb689d10adfa3e0e87d8b1ef0e417c46e38d5
ager@chromium.org <ager@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2009-05-12 13:06:23 +0000

    Push bleeding_edge revision 1915 to trunk
    git-svn-id: http://v8.googlecode.com/svn/trunk@1916 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -35,7 +35,7 @@
 #define MAJOR_VERSION     1
 #define MINOR_VERSION     2
 #define BUILD_NUMBER      3
-#define PATCH_LEVEL       0
+#define PATCH_LEVEL       1
 #define CANDIDATE_VERSION false
 
 // Define SONAME to have the SCons build the put a specific SONAME into the
