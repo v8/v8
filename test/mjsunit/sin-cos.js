667176d910886017bc34b8b35c51ee0c1f757a75
ager@chromium.org <ager@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2009-06-15 15:57:06 +0000

    Another attempt at fixing sine & cosine test.
    TBR=kmillikin@chromium.org
    Review URL: http://codereview.chromium.org/125129
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@2173 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/mjsunit/sin-cos.js
+++ test/mjsunit/sin-cos.js
@@ -27,21 +27,14 @@
 
 // Test Math.sin and Math.cos.
 
-var input_sin = [0, Math.PI, Math.PI / 6, Math.PI / 2];
-var input_cos = [0, Math.PI, Math.PI / 6, Math.PI / 2];
+var input_sin = [0, Math.PI / 2];
+var input_cos = [0, Math.PI];
 
 var output_sin = input_sin.map(Math.sin);
-var output_cos = input_sin.map(Math.cos);
+var output_cos = input_cos.map(Math.cos);
 
-var expected_sin = [0,
-                    1.2246063538223773e-16,
-                    0.49999999999999994,
-                    1];
-
-var expected_cos = [1,
-                    -1,
-                    0.8660254037844387,
-                    6.123031769111886e-17];
+var expected_sin = [0, 1];
+var expected_cos = [1, -1];
 
 assertArrayEquals(expected_sin, output_sin, "sine");
 assertArrayEquals(expected_cos, output_cos, "cosine");
