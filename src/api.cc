fbd27be8d8221ad9582bfc224be9f339c9ddcdb4
ager@chromium.org <ager@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2009-07-17 16:31:25 +0000

    I will lint my code before submitting...
    git-svn-id: http://v8.googlecode.com/svn/branches/1.2@2507 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/api.cc
+++ src/api.cc
@@ -2494,7 +2494,7 @@ void v8::Object::SetPointerInInternalField(int index, void* value) {
     // Currently internal fields are used by DOM wrappers which only
     // get garbage collected by the mark-sweep collector, so we
     // pretenure the proxy.
-    HandleScope scope;    
+    HandleScope scope;
     i::Handle<i::Proxy> proxy =
         i::Factory::NewProxy(reinterpret_cast<i::Address>(value), i::TENURED);
     if (!proxy.is_null()) obj->SetInternalField(index, *proxy);
