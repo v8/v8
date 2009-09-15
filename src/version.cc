8a3bce2fa1b366077c9e63482f8e7fa87ab8f2bc
ager@chromium.org <ager@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2009-09-15 11:48:18 +0000

    Prepare push to trunk.  Now working on version 1.3.12. Review URL: http://codereview.chromium.org/204020
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@2889 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -34,7 +34,7 @@
 // cannot be changed without changing the SCons build script.
 #define MAJOR_VERSION     1
 #define MINOR_VERSION     3
-#define BUILD_NUMBER      11
+#define BUILD_NUMBER      12
 #define PATCH_LEVEL       0
 #define CANDIDATE_VERSION true
 
