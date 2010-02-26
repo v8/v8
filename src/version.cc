833cdd78f8ada3166f25f39ab88e9ce0096f81ac
sgjesse@chromium.org <sgjesse@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-02-26 10:06:16 +0000

    Merge r3962 to trunk
    BUG=http://crbug.com/36807
    TEST=none
    Review URL: http://codereview.chromium.org/661171
    
    git-svn-id: http://v8.googlecode.com/svn/trunk@3963 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -35,7 +35,7 @@
 #define MAJOR_VERSION     2
 #define MINOR_VERSION     1
 #define BUILD_NUMBER      2
-#define PATCH_LEVEL       1
+#define PATCH_LEVEL       2
 #define CANDIDATE_VERSION false
 
 // Define SONAME to have the SCons build the put a specific SONAME into the
