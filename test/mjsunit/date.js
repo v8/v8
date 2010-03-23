2b5c05ed798bcfd40031b985c94cf4d2e3874902
oleg@chromium.org <oleg@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-03-23 15:28:58 +0000

    Make the new Date test independent of the timezone in which it is run. Review URL: http://codereview.chromium.org/1084018
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@4236 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/mjsunit/date.js
+++ test/mjsunit/date.js
@@ -48,7 +48,7 @@ var dMax = new Date(8.64e15);
 assertEquals(8.64e15, dMax.getTime());
 assertEquals(275760, dMax.getFullYear());
 assertEquals(8, dMax.getMonth());
-assertEquals(13, dMax.getDate());
+assertEquals(13, dMax.getUTCDate());
 
 var dOverflow = new Date(8.64e15+1);
 assertTrue(isNaN(dOverflow.getTime()));
@@ -57,7 +57,7 @@ var dMin = new Date(-8.64e15);
 assertEquals(-8.64e15, dMin.getTime());
 assertEquals(-271821, dMin.getFullYear());
 assertEquals(3, dMin.getMonth());
-assertEquals(20, dMin.getDate());
+assertEquals(20, dMin.getUTCDate());
 
 var dUnderflow = new Date(-8.64e15-1);
 assertTrue(isNaN(dUnderflow.getTime()));
