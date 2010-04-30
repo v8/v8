74a10810f3e55ce820b434f0f8f3a5c9b484f288
antonm@chromium.org <antonm@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-04-30 09:21:54 +0000

    Bring r4503 to the 2.1 branch.
    Current custom call generators cannot cope with the case when receiver is not a
    JSArray.
    
    Add a support for bailout from custom call generators (just return undefined).
    
    BUG=684,42802
    
    Review URL: http://codereview.chromium.org/1774015
    
    git-svn-id: http://v8.googlecode.com/svn/branches/2.1@4553 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/ia32/stub-cache-ia32.cc
+++ src/ia32/stub-cache-ia32.cc
@@ -1223,6 +1223,11 @@ Object* CallStubCompiler::CompileArrayPushCall(Object* object,
   // -----------------------------------
   ASSERT(check == RECEIVER_MAP_CHECK);
 
+  // If object is not an array, bail out to regular call.
+  if (!object->IsJSArray()) {
+    return Heap::undefined_value();
+  }
+
   Label miss;
 
   // Get the receiver from the stack.
@@ -1371,6 +1376,11 @@ Object* CallStubCompiler::CompileArrayPopCall(Object* object,
   // -----------------------------------
   ASSERT(check == RECEIVER_MAP_CHECK);
 
+  // If object is not an array, bail out to regular call.
+  if (!object->IsJSArray()) {
+    return Heap::undefined_value();
+  }
+
   Label miss, empty_array, call_builtin;
 
   // Get the receiver from the stack.
@@ -1458,7 +1468,11 @@ Object* CallStubCompiler::CompileCallConstant(Object* object,
   if (function_info->HasCustomCallGenerator()) {
     CustomCallGenerator generator =
         ToCData<CustomCallGenerator>(function_info->function_data());
-    return generator(this, object, holder, function, name, check);
+    Object* result = generator(this, object, holder, function, name, check);
+    // undefined means bail out to regular compiler.
+    if (!result->IsUndefined()) {
+      return result;
+    }
   }
 
   Label miss_in_smi_check;
