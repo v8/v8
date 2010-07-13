20d4c6d52db06d8d689a2ad892cd329d12a671b0
whesse@chromium.org <whesse@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-07-13 08:55:06 +0000

    Fix compilation error on 64-bit MacOS V8 compilation.  ASSERT_EQ macro needs unambiguous similar integer types as arguments. Review URL: http://codereview.chromium.org/2982005
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@5048 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x64/codegen-x64.cc
+++ src/x64/codegen-x64.cc
@@ -10295,7 +10295,7 @@ void CompareStub::Generate(MacroAssembler* masm) {
     // A smi plus a heap object has the low bit set, a heap object plus
     // a heap object has the low bit clear.
     ASSERT_EQ(0, kSmiTag);
-    ASSERT_EQ(V8_UINT64_C(1), kSmiTagMask);
+    ASSERT_EQ(static_cast<int64_t>(1), kSmiTagMask);
     __ lea(rcx, Operand(rax, rdx, times_1, 0));
     __ testb(rcx, Immediate(kSmiTagMask));
     __ j(not_zero, &not_both_objects);
