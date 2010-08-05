4652465d9d69143e448ffcc1e695ffb0f67133c0
antonm@chromium.org <antonm@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-08-05 18:35:53 +0000

    Backport http://code.google.com/p/v8/source/detail?r=5181 to 2.2.
    TBR=vegorov@chromium.org
    
    Review URL: http://codereview.chromium.org/3089008
    
    git-svn-id: http://v8.googlecode.com/svn/branches/2.2@5190 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -35,7 +35,7 @@
 #define MAJOR_VERSION     2
 #define MINOR_VERSION     2
 #define BUILD_NUMBER      24
-#define PATCH_LEVEL       9
+#define PATCH_LEVEL       11
 #define CANDIDATE_VERSION false
 
 // Define SONAME to have the SCons build the put a specific SONAME into the
