66d13be5f9072441102ff11a0e041fe46458021f
erik.corry@gmail.com <erik.corry@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-08-26 08:53:00 +0000

    Fix incorrect encoding of single and double precision registers for some VFP instructions. Also fix incorrect disassembling of vldr/vstr.  This is a commit of http://codereview.chromium.org/3107027 for Rodolph Perfetta.
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@5352 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/arm/assembler-arm.h
+++ src/arm/assembler-arm.h
@@ -120,6 +120,11 @@ struct SwVfpRegister {
     ASSERT(is_valid());
     return 1 << code_;
   }
+  void split_code(int* vm, int* m) const  {
+    ASSERT(is_valid());
+    *m = code_ & 0x1;
+    *vm = code_ >> 1;
+  }
 
   int code_;
 };
@@ -151,6 +156,11 @@ struct DwVfpRegister {
   int bit() const  {
     ASSERT(is_valid());
     return 1 << code_;
+  }
+  void split_code(int* vm, int* m) const  {
+    ASSERT(is_valid());
+    *m = (code_ & 0x10) >> 4;
+    *vm = code_ & 0x0F;
   }
 
   int code_;
