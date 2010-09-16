40a8610d5c5afac28f5cc482a15f869f71c0c454
kaznacheev@chromium.org <kaznacheev@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-09-16 13:00:31 +0000

    Merge r5476 into 2.2 branch (issue3434004).
    Review URL: http://codereview.chromium.org/3430006
    
    git-svn-id: http://v8.googlecode.com/svn/branches/2.2@5479 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects.cc
+++ src/objects.cc
@@ -3706,7 +3706,7 @@ Object* DescriptorArray::RemoveTransitions() {
 }
 
 
-void DescriptorArray::Sort() {
+void DescriptorArray::SortUnchecked() {
   // In-place heap sort.
   int len = number_of_descriptors();
 
@@ -3756,7 +3756,11 @@ void DescriptorArray::Sort() {
       parent_index = child_index;
     }
   }
+}
 
+
+void DescriptorArray::Sort() {
+  SortUnchecked();
   SLOW_ASSERT(IsSortedNoDuplicates());
 }
 
@@ -5183,6 +5187,13 @@ bool SharedFunctionInfo::CanGenerateInlineConstructor(Object* prototype) {
   }
 
   return true;
+}
+
+
+void SharedFunctionInfo::ForbidInlineConstructor() {
+  set_compiler_hints(BooleanBit::set(compiler_hints(),
+                                     kHasOnlySimpleThisPropertyAssignments,
+                                     false));
 }
 
 
