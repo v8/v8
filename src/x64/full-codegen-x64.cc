8541d77d2d2a96c3e430974b3027f29b2a8e4a68
lrn@chromium.org <lrn@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2010-12-15 12:05:09 +0000

    Version 3.0.2.
    Revert version 3.0.1 and patch 3.0.1.1.
    
    Review URL: http://codereview.chromium.org/5862002
    
    git-svn-id: http://v8.googlecode.com/svn/trunk@6026 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x64/full-codegen-x64.cc
+++ src/x64/full-codegen-x64.cc
@@ -839,9 +839,7 @@ void FullCodeGenerator::VisitForInStatement(ForInStatement* stmt) {
   __ bind(&update_each);
   __ movq(result_register(), rbx);
   // Perform the assignment as if via '='.
-  { EffectContext context(this);
-    EmitAssignment(stmt->each(), stmt->AssignmentId());
-  }
+  EmitAssignment(stmt->each());
 
   // Generate code for the body of the loop.
   Visit(stmt->body());
@@ -1415,7 +1413,6 @@ void FullCodeGenerator::VisitAssignment(Assignment* expr) {
     case VARIABLE:
       EmitVariableAssignment(expr->target()->AsVariableProxy()->var(),
                              expr->op());
-      context()->Plug(rax);
       break;
     case NAMED_PROPERTY:
       EmitNamedPropertyAssignment(expr);
@@ -1524,7 +1521,7 @@ void FullCodeGenerator::EmitBinaryOp(Token::Value op,
 }
 
 
-void FullCodeGenerator::EmitAssignment(Expression* expr, int bailout_id) {
+void FullCodeGenerator::EmitAssignment(Expression* expr) {
   // Invalid left-hand sides are rewritten to have a 'throw
   // ReferenceError' on the left-hand side.
   if (!expr->IsValidLeftHandSide()) {
@@ -1572,7 +1569,6 @@ void FullCodeGenerator::EmitAssignment(Expression* expr, int bailout_id) {
       break;
     }
   }
-  context()->Plug(rax);
 }
 
 
@@ -1645,6 +1641,8 @@ void FullCodeGenerator::EmitVariableAssignment(Variable* var,
     }
     __ bind(&done);
   }
+
+  context()->Plug(rax);
 }
 
 
@@ -1681,9 +1679,10 @@ void FullCodeGenerator::EmitNamedPropertyAssignment(Assignment* expr) {
     __ push(Operand(rsp, kPointerSize));  // Receiver is under value.
     __ CallRuntime(Runtime::kToFastProperties, 1);
     __ pop(rax);
-    __ Drop(1);
+    context()->DropAndPlug(1, rax);
+  } else {
+    context()->Plug(rax);
   }
-  context()->Plug(rax);
 }
 
 
@@ -3128,7 +3127,6 @@ void FullCodeGenerator::VisitCountOperation(CountOperation* expr) {
         { EffectContext context(this);
           EmitVariableAssignment(expr->expression()->AsVariableProxy()->var(),
                                  Token::ASSIGN);
-          context.Plug(rax);
         }
         // For all contexts except kEffect: We have the result on
         // top of the stack.
@@ -3139,7 +3137,6 @@ void FullCodeGenerator::VisitCountOperation(CountOperation* expr) {
         // Perform the assignment as if via '='.
         EmitVariableAssignment(expr->expression()->AsVariableProxy()->var(),
                                Token::ASSIGN);
-        context()->Plug(rax);
       }
       break;
     case NAMED_PROPERTY: {
