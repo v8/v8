518761594397d041b8a42123ed74be465b4cff2d
whesse@chromium.org <whesse@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-01-14 16:10:00 +0000

    Fix accidental change to ia32 in r6329. Review URL: http://codereview.chromium.org/6362001
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@6335 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/ia32/lithium-codegen-ia32.cc
+++ src/ia32/lithium-codegen-ia32.cc
@@ -412,8 +412,8 @@ Immediate LCodeGen::ToImmediate(LOperand* op) {
 
 
 Operand LCodeGen::ToOperand(LOperand* op) const {
-  ASSERT(!op->IsRegister());
-  ASSERT(!op->IsDoubleRegister());
+  if (op->IsRegister()) return Operand(ToRegister(op));
+  if (op->IsDoubleRegister()) return Operand(ToDoubleRegister(op));
   ASSERT(op->IsStackSlot() || op->IsDoubleStackSlot());
   int index = op->index();
   if (index >= 0) {
