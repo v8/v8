c45a66432ffdcabec52317ea47c57a4fd5812deb
erik.corry@gmail.com <erik.corry@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-02-03 09:00:13 +0000

    Backport r6579 (code generation bug on ARM). Review URL: http://codereview.chromium.org/6286076
    git-svn-id: http://v8.googlecode.com/svn/branches/3.0@6600 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -35,7 +35,7 @@
 #define MAJOR_VERSION     3
 #define MINOR_VERSION     0
 #define BUILD_NUMBER      12
-#define PATCH_LEVEL       9
+#define PATCH_LEVEL       10
 #define CANDIDATE_VERSION false
 
 // Define SONAME to have the SCons build the put a specific SONAME into the
