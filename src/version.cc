36829b3c2bb839161783bd674638c615b7102eaa
sgjesse@chromium.org <sgjesse@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-03-23 11:41:45 +0000

    Change bleeding edge version to candidate
    R=ager@chromium.org
    Review URL: http://codereview.chromium.org/6723015
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@7325 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -38,7 +38,7 @@
 #define PATCH_LEVEL       0
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
-#define IS_CANDIDATE_VERSION 0
+#define IS_CANDIDATE_VERSION 1
 
 // Define SONAME to have the SCons build the put a specific SONAME into the
 // shared library instead the generic SONAME generated from the V8 version
