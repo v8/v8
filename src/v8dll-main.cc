be15ea860d4e32ca961fc71aee1e8756a27a591a
ager@chromium.org <ager@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-05-02 15:12:50 +0000

    Fix lint.
    TBR=sgjesse@chromium.org
    
    Review URL: http://codereview.chromium.org/6905168
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@7752 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/v8dll-main.cc
+++ src/v8dll-main.cc
@@ -1,4 +1,4 @@
-// Copyright 2010 the V8 project authors. All rights reserved.
+// Copyright 2011 the V8 project authors. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -28,7 +28,7 @@
 #include "../include/v8.h"
 
 #ifdef WIN32
-#include <windows.h>
+#include <windows.h>  // NOLINT
 
 extern "C" {
 BOOL WINAPI DllMain(HANDLE hinstDLL,
