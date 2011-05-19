eea59292e6fb51635f051aaf6097d61b2f483220
lrn@chromium.org <lrn@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-05-19 09:22:32 +0000

    Avoid conflict between method and type name.
    Make builder happy.
    
    Review URL: http://codereview.chromium.org/7046004
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@7954 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/preparser.cc
+++ src/preparser.cc
@@ -1,4 +1,4 @@
-// Copyright 2010 the V8 project authors. All rights reserved.
+// Copyright 2011 the V8 project authors. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -981,7 +981,7 @@ PreParser::Expression PreParser::ParsePrimaryExpression(bool* ok) {
       // FALLTHROUGH
     case i::Token::IDENTIFIER: {
       Identifier id = ParseIdentifier(CHECK_OK);
-      result = Expression::Identifier(id);
+      result = Expression::FromIdentifier(id);
       break;
     }
 
--- src/preparser.h
+++ src/preparser.h
@@ -1,4 +1,4 @@
-// Copyright 2010 the V8 project authors. All rights reserved.
+// Copyright 2011 the V8 project authors. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -124,7 +124,7 @@ class PreParser {
       return Expression(kUnknownExpression);
     }
 
-    static Expression Identifier(Identifier id) {
+    static Expression FromIdentifier(Identifier id) {
       return Expression(kIdentifierFlag | (id.type_ << kIdentifierShift));
     }
 
