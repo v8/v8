84cf91d128ca58547a2c4948fcb0901bb9bcff87
erik.corry@gmail.com <erik.corry@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-06-23 14:29:11 +0000

    Make scan-on-scavenge less verbose. Review URL: http://codereview.chromium.org/7248005
    git-svn-id: http://v8.googlecode.com/svn/branches/experimental@8400 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- gc/src/heap.h
+++ gc/src/heap.h
@@ -894,14 +894,14 @@ class Heap {
 
   inline void increment_scan_on_scavenge_pages() {
     scan_on_scavenge_pages_++;
-    if (FLAG_trace_gc) {
+    if (FLAG_trace_gc_verbose) {
       PrintF("Scan-on-scavenge pages: %d\n", scan_on_scavenge_pages_);
     }
   }
 
   inline void decrement_scan_on_scavenge_pages() {
     scan_on_scavenge_pages_--;
-    if (FLAG_trace_gc) {
+    if (FLAG_trace_gc_verbose) {
       PrintF("Scan-on-scavenge pages: %d\n", scan_on_scavenge_pages_);
     }
   }
