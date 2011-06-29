c5063d22aa5754ba06304ce590d8794367e37125
lrn@chromium.org <lrn@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-06-29 11:36:32 +0000

    Fix bug in semispace shrink-to.
    Review URL: http://codereview.chromium.org/7281006
    
    git-svn-id: http://v8.googlecode.com/svn/branches/experimental@8463 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- gc/src/spaces.cc
+++ gc/src/spaces.cc
@@ -1222,18 +1222,21 @@ bool SemiSpace::ShrinkTo(int new_capacity) {
   ASSERT((new_capacity & Page::kPageAlignmentMask) == 0);
   ASSERT(new_capacity >= initial_capacity_);
   ASSERT(new_capacity < capacity_);
-  Address end = start_ + maximum_capacity_;
-  Address start = end - new_capacity;
+  // Semispaces grow backwards from the end of their allocated capacity,
+  // so we find the before and after start addresses relative to the
+  // end of the space.
+  Address space_end = start_ + maximum_capacity_;
+  Address old_start = space_end - capacity_;
   size_t delta = capacity_ - new_capacity;
   ASSERT(IsAligned(delta, OS::AllocateAlignment()));
-  if (!heap()->isolate()->memory_allocator()->UncommitBlock(start, delta)) {
+  if (!heap()->isolate()->memory_allocator()->UncommitBlock(old_start, delta)) {
     return false;
   }
   capacity_ = new_capacity;
 
   int pages_after = capacity_ / Page::kPageSize;
   NewSpacePage* new_last_page =
-      NewSpacePage::FromAddress(end - pages_after * Page::kPageSize);
+      NewSpacePage::FromAddress(space_end - pages_after * Page::kPageSize);
   new_last_page->set_next_page(anchor());
   anchor()->set_prev_page(new_last_page);
   ASSERT(current_page_ == first_page());
