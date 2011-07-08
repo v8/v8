d9d40dd5d9627375729aad521866fd13ae800d6a
sgjesse@chromium.org <sgjesse@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-07-08 06:48:27 +0000

    MIPS: port Fix a typo in KeyedStoreIC, restore Kraken performance.
    Ported r8539 (46103ea)
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org//7327016
    Patch from Paul Lind <plind44@gmail.com>.
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@8569 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/stub-cache-mips.cc
+++ src/mips/stub-cache-mips.cc
@@ -4201,7 +4201,7 @@ void KeyedStoreStubCompiler::GenerateStoreFastElement(MacroAssembler* masm,
   // have been verified by the caller to not be a smi.
 
   // Check that the key is a smi.
-  __ JumpIfNotSmi(a0, &miss_force_generic);
+  __ JumpIfNotSmi(key_reg, &miss_force_generic);
 
   // Get the elements array and make sure it is a fast element array, not 'cow'.
   __ lw(elements_reg,
