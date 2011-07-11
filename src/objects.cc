5bc14449b1fe29fe786b5f63769fb23515f4f36a
vegorov@chromium.org <vegorov@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-07-11 13:36:09 +0000

    Merge r8596 into 3.2 branch.
    Fix bug in Map::TraverseTransitionTree.
    
    If map has an empty DescriptorArray we have to set map_or_index_field to NULL
    otherwise we will
    reset iteration state for a previously visited map in the transition tree which
    has a non-empty
    DescriptorArray. This might result in visiting the same map several times.
    
    R=whesse@chromium.org
    BUG=v8:1526
    Review URL: http://codereview.chromium.org/7294026
    
    git-svn-id: http://v8.googlecode.com/svn/branches/3.2@8598 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects.cc
+++ src/objects.cc
@@ -3745,6 +3745,8 @@ void Map::TraverseTransitionTree(TraverseCallback callback, void* data) {
         }
       }
       if (!map_done) continue;
+    } else {
+      map_or_index_field = NULL;
     }
     // That was the regular transitions, now for the prototype transitions.
     FixedArray* prototype_transitions =
