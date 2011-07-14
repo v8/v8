5d024ca5d2714391797421117c21e8708daaccd8
ricow@chromium.org <ricow@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-07-14 09:00:11 +0000

    Fix memory leak from sample/shell.cc.
    We did not dispose the semaphores used in SourceGroup
    Review URL: http://codereview.chromium.org/7366002
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@8649 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- samples/shell.cc
+++ samples/shell.cc
@@ -95,6 +95,11 @@ class SourceGroup {
                   begin_offset_(0),
                   end_offset_(0) { }
 
+  ~SourceGroup() {
+    delete next_semaphore_;
+    delete done_semaphore_;
+  }
+
   void Begin(char** argv, int offset) {
     argv_ = const_cast<const char**>(argv);
     begin_offset_ = offset;
