4487f8c0507f007bdbd81cdbdc7e91201596a4ab
kmillikin@chromium.org <kmillikin@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-08-03 09:10:35 +0000

    Revert "Revert "Fix a bug in scope analysis.""
    Reapply r8783 with an additional fix.
    
    Because the preparser and parser do not use the same scope analysis to
    determine if a function can be lazily compiled, the parser can have false
    positives.  Rather than treating this as a parse error, treat the preparser
    as authoritative and eagerly compile the function.
    
    R=lrn@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7565003
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@8797 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/cctest/test-api.cc
+++ test/cctest/test-api.cc
@@ -10663,17 +10663,16 @@ TEST(PreCompileInvalidPreparseDataError) {
            *exception_value);
 
   try_catch.Reset();
+
   // Overwrite function bar's start position with 200.  The function entry
-  // will not be found when searching for it by position.
+  // will not be found when searching for it by position and we should fall
+  // back on eager compilation.
   sd = v8::ScriptData::PreCompile(script, i::StrLength(script));
   sd_data = reinterpret_cast<unsigned*>(const_cast<char*>(sd->Data()));
   sd_data[kHeaderSize + 1 * kFunctionEntrySize + kFunctionEntryStartOffset] =
       200;
   compiled_script = Script::New(source, NULL, sd);
-  CHECK(try_catch.HasCaught());
-  String::AsciiValue second_exception_value(try_catch.Message()->Get());
-  CHECK_EQ("Uncaught SyntaxError: Invalid preparser data for function bar",
-           *second_exception_value);
+  CHECK(!try_catch.HasCaught());
 
   delete sd;
 }
