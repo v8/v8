735282fac27fd38f2558b31c1fee34b69afa275c
vitalyr@chromium.org <vitalyr@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-08-11 16:03:29 +0000

    Merge r8884 "Fix fun.apply(receiver, arguments) optimization" to 3.4.
    R=kmillikin@chromium.org
    BUG=v8:1592
    TEST=mjsunit/regress/regress-1592.js
    Review URL: http://codereview.chromium.org/7623016
    
    git-svn-id: http://v8.googlecode.com/svn/branches/3.4@8906 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects.h
+++ src/objects.h
@@ -4346,6 +4346,7 @@ class Script: public Struct {
 #define FUNCTIONS_WITH_ID_LIST(V)                   \
   V(Array.prototype, push, ArrayPush)               \
   V(Array.prototype, pop, ArrayPop)                 \
+  V(Function.prototype, apply, FunctionApply)       \
   V(String.prototype, charCodeAt, StringCharCodeAt) \
   V(String.prototype, charAt, StringCharAt)         \
   V(String, fromCharCode, StringFromCharCode)       \
