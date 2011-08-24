v8/v8

commit 1afbe92a0e04089f10daf0a44eed31b3d81089ec

Fixed minor bug in D8 related to mapping counters to a file.
Review URL: http://codereview.chromium.org/7717022

git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@9005 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/d8.cc
@@ -1237,8 +1237,6 @@ int Shell::RunMain(int argc, char* argv[]) {
       thread->Join();
       delete thread;
     }
-
-  OnExit();
 #endif  // V8_SHARED
   return 0;
 }
@@ -1289,6 +1287,7 @@ int Shell::Main(int argc, char* argv[]) {
   }
 
   V8::Dispose();
+  OnExit();
 
   return result;
 }

commit 02e70b799bdb27019ded94df02236145287b8d70

Fixed the shared library build of D8 that was broken by r9005.
Review URL: http://codereview.chromium.org/7701024

git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@9006 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/d8.cc
@@ -1287,7 +1287,10 @@ int Shell::Main(int argc, char* argv[]) {
   }
 
   V8::Dispose();
+
+#ifndef V8_SHARED
   OnExit();
+#endif  // V8_SHARED
 
   return result;
 }