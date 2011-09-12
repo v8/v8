df11bd7b756923585ecffda42709c17ddc2b6525
kmillikin@chromium.org <kmillikin@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-09-12 08:57:09 +0000

    Clean up Context::Lookup and its uses.
    Refactor Context::Lookup so it is more obvious.  Change the comment in
    contexts.h so it no longer indicates that it can return an arguments
    object (it can't) and clean up the call sites that had leftover dead code.
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7782030
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@9223 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/hydrogen.cc
+++ src/hydrogen.cc
@@ -4922,7 +4922,6 @@ void HGraphBuilder::VisitCall(Call* expr) {
 
   } else {
     VariableProxy* proxy = expr->expression()->AsVariableProxy();
-    // FIXME.
     bool global_call = proxy != NULL && proxy->var()->IsUnallocated();
 
     if (global_call) {
