b6808ee7909298faf4c0102819ccf62ee666495e
yangguo@chromium.org <yangguo@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-09-15 13:12:59 +0000

    Tickprocessor no longer prints error messages on log entries that are unrelated to profiling.
    BUG=v8:1683
    
    Review URL: http://codereview.chromium.org/7906003
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@9302 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- tools/logreader.js
+++ tools/logreader.js
@@ -134,9 +134,8 @@ LogReader.prototype.skipDispatch = function(dispatch) {
 LogReader.prototype.dispatchLogRow_ = function(fields) {
   // Obtain the dispatch.
   var command = fields[0];
-  if (!(command in this.dispatchTable_)) {
-    throw new Error('unknown command: ' + command);
-  }
+  if (!(command in this.dispatchTable_)) return;
+  
   var dispatch = this.dispatchTable_[command];
 
   if (dispatch === null || this.skipDispatch(dispatch)) {
