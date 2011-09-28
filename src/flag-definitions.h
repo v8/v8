355a2bf0a8e3b43efb865cde982bb46852b52157
erik.corry@gmail.com <erik.corry@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-09-28 11:15:23 +0000

    Temporarily switch on always-compact to flush out crash bugs. Review URL: http://codereview.chromium.org/8068001
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@9463 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -282,7 +282,7 @@ DEFINE_bool(native_code_counters, false,
             "generate extra code for manipulating stats counters")
 
 // mark-compact.cc
-DEFINE_bool(always_compact, false, "Perform compaction on every full GC")
+DEFINE_bool(always_compact, true, "Perform compaction on every full GC")
 DEFINE_bool(lazy_sweeping, true,
             "Use lazy sweeping for old pointer and data spaces")
 DEFINE_bool(cleanup_caches_in_maps_at_gc, true,
