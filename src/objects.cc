b99cabf438eadee7987ca2a720ed77dfa77f7dc2
erik.corry@gmail.com <erik.corry@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-10-07 09:45:13 +0000

    Port r9538 and r9544 to Trunk. Review URL: http://codereview.chromium.org/8195004
    git-svn-id: http://v8.googlecode.com/svn/trunk@9545 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects.cc
+++ src/objects.cc
@@ -3425,9 +3425,9 @@ void JSObject::DeleteHiddenProperty(String* key) {
 
 
 bool JSObject::HasHiddenProperties() {
-  LookupResult lookup;
-  LocalLookupRealNamedProperty(GetHeap()->hidden_symbol(), &lookup);
-  return lookup.IsFound();
+  return GetPropertyAttributePostInterceptor(this,
+                                             GetHeap()->hidden_symbol(),
+                                             false) != ABSENT;
 }
 
 
