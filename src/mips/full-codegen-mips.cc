55b7bfa2da880b3fce8dd5bfd53d8371c1bf5a1e
keuchel@chromium.org <keuchel@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-10-26 08:43:33 +0000

    MIPS: port Replace boolean indications of strict mode by an enum value.
    Port r9746 (804e4e4)
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8390030
    Patch from Paul Lind <plind44@gmail.com>.
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@9784 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/full-codegen-mips.cc
+++ src/mips/full-codegen-mips.cc
@@ -1115,7 +1115,7 @@ void FullCodeGenerator::EmitNewClosure(Handle<SharedFunctionInfo> info,
       !pretenure &&
       scope()->is_function_scope() &&
       info->num_literals() == 0) {
-    FastNewClosureStub stub(info->strict_mode() ? kStrictMode : kNonStrictMode);
+    FastNewClosureStub stub(info->strict_mode_flag());
     __ li(a0, Operand(info));
     __ push(a0);
     __ CallStub(&stub);
