bbab5326993fe9c9cf11dfa219f8d6d65f13c98f
yangguo@chromium.org <yangguo@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-11-18 11:16:58 +0000

    Fix debug builds.
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8595007
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@10029 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/scopes.cc
+++ src/scopes.cc
@@ -55,7 +55,7 @@ class ZoneAllocator: public Allocator {
 };
 
 
-static ZoneAllocator* LocalsMapAllocator = new ZoneAllocator();
+static ZoneAllocator* LocalsMapAllocator = ::new ZoneAllocator();
 
 
 // ----------------------------------------------------------------------------
