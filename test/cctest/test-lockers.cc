4cedd762e5ac2956769e727947dd1cca91701847
dslomov@chromium.org <dslomov@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2011-11-22 18:51:53 +0000

    Fix ExtensionsRegistration cctest on ARM. Reduced the number of created threads on ARM.
    Review URL: http://codereview.chromium.org/8639007
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@10045 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/cctest/test-lockers.cc
+++ test/cctest/test-lockers.cc
@@ -674,7 +674,11 @@ class IsolateGenesisThread : public JoinableThread {
 // Test installing extensions in separate isolates concurrently.
 // http://code.google.com/p/v8/issues/detail?id=1821
 TEST(ExtensionsRegistration) {
+#ifdef V8_TARGET_ARCH_ARM
+  const int kNThreads = 10;
+#else
   const int kNThreads = 40;
+#endif
   v8::RegisterExtension(new v8::Extension("test0",
                                           kSimpleExtensionSource));
   v8::RegisterExtension(new v8::Extension("test1",
