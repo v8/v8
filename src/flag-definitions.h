6f7d9d2312d171660ec413bee9f1ec9382e5d88e
danno@chromium.org <danno@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-02-16 15:47:36 +0000

    Enable smi_only_arrays by default.
    R=jkummerow@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9414007
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@10732 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -120,7 +120,7 @@ DEFINE_implication(harmony, harmony_proxies)
 DEFINE_implication(harmony, harmony_collections)
 
 // Flags for experimental implementation features.
-DEFINE_bool(smi_only_arrays, false, "tracks arrays with only smi values")
+DEFINE_bool(smi_only_arrays, true, "tracks arrays with only smi values")
 DEFINE_bool(clever_optimizations,
             true,
             "Optimize object size, Array shift, DOM strings and string +")
