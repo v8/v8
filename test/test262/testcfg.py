d5c7e87c500e1522bd1098ef939aced7ec9d4d29
mstarzinger@chromium.org <mstarzinger@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-03-28 09:33:19 +0000

    Fix test harness for Test262 to not use symlinks.
    This is necessary for the --download-data option to work on Windows
    where we do not have symlinks available. Note that we still have no
    automatic way of bumping the existing Test262 revision without deleting
    the data directory manually.
    
    R=jkummerow@chromium.org
    TEST=test262
    
    Review URL: https://chromiumcodereview.appspot.com/9866046
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@11172 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/test262/testcfg.py
+++ test/test262/testcfg.py
@@ -1,4 +1,4 @@
-# Copyright 2011 the V8 project authors. All rights reserved.
+# Copyright 2012 the V8 project authors. All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
@@ -103,23 +103,21 @@ class Test262TestConfiguration(test.TestConfiguration):
     revision = TEST_262_ARCHIVE_REVISION
     archive_url = TEST_262_URL % revision
     archive_name = join(self.root, 'test262-%s.tar.bz2' % revision)
-    directory_name = join(self.root, "test262-%s" % revision)
-    if not exists(directory_name) or not exists(archive_name):
-      if not exists(archive_name):
-        print "Downloading test data from %s ..." % archive_url
-        urllib.urlretrieve(archive_url, archive_name)
-      if not exists(directory_name):
-        print "Extracting test262-%s.tar.bz2 ..." % revision
-        md5 = hashlib.md5()
-        with open(archive_name,'rb') as f:
-          for chunk in iter(lambda: f.read(8192), ''):
-            md5.update(chunk)
-        if md5.hexdigest() != TEST_262_ARCHIVE_MD5:
-          raise Exception("Hash mismatch of test data file")
-        archive = tarfile.open(archive_name, 'r:bz2')
-        archive.extractall(join(self.root))
-      if not exists(join(self.root, 'data')):
-        os.symlink(directory_name, join(self.root, 'data'))
+    directory_name = join(self.root, 'data')
+    if not exists(archive_name):
+      print "Downloading test data from %s ..." % archive_url
+      urllib.urlretrieve(archive_url, archive_name)
+    if not exists(directory_name):
+      print "Extracting test262-%s.tar.bz2 ..." % revision
+      md5 = hashlib.md5()
+      with open(archive_name,'rb') as f:
+        for chunk in iter(lambda: f.read(8192), ''):
+          md5.update(chunk)
+      if md5.hexdigest() != TEST_262_ARCHIVE_MD5:
+        raise Exception("Hash mismatch of test data file")
+      archive = tarfile.open(archive_name, 'r:bz2')
+      archive.extractall(join(self.root))
+      os.rename(join(self.root, 'test262-%s' % revision), directory_name)
 
   def GetBuildRequirements(self):
     return ['d8']
