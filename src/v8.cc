6c392ffa79b6ee0cb7e6cc11724a7af0e43631ba
svenpanne@chromium.org <svenpanne@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-03-28 13:12:00 +0000

    Valgrind cleanliness, part 2: Delete lithium operand caches on exit.
    This fixes 5 leaks, returning 1.6kB of lost memory.
    
    Shocking news: I've actually introduced a 2nd-order macro for myself. I guess
    I've been assimilated... ;-)
    
    Review URL: https://chromiumcodereview.appspot.com/9860028
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@11179 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/v8.cc
+++ src/v8.cc
@@ -105,6 +105,7 @@ void V8::TearDown() {
   if (!has_been_set_up_ || has_been_disposed_) return;
 
   ElementsAccessor::TearDown();
+  LOperand::TearDownCaches();
 
   isolate->TearDown();
   delete isolate;
