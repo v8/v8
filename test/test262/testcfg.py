d2c166c7f796bb69d2ab88ab64d025b844a1694a
mstarzinger@chromium.org <mstarzinger@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-04-13 16:09:15 +0000

    Update to most recent test262 version.
    This also deletes downloaded test data files if their hashes are
    incorrect to automatically retry the download on the next run.
    
    R=jkummerow@chromium.org
    TEST=test262
    
    Review URL: https://chromiumcodereview.appspot.com/10080007
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@11322 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/test262/testcfg.py
+++ test/test262/testcfg.py
@@ -34,8 +34,8 @@ import hashlib
 import tarfile
 
 
-TEST_262_ARCHIVE_REVISION = '3a890174343c'  # This is the r309 revision.
-TEST_262_ARCHIVE_MD5 = 'be5d4cfbe69cef70430907b8f3a92b50'
+TEST_262_ARCHIVE_REVISION = 'fb327c439e20'  # This is the r334 revision.
+TEST_262_ARCHIVE_MD5 = '307acd166ec34629592f240dc12d57ed'
 TEST_262_URL = 'http://hg.ecmascript.org/tests/test262/archive/%s.tar.bz2'
 TEST_262_HARNESS = ['sta.js']
 
@@ -114,6 +114,7 @@ class Test262TestConfiguration(test.TestConfiguration):
         for chunk in iter(lambda: f.read(8192), ''):
           md5.update(chunk)
       if md5.hexdigest() != TEST_262_ARCHIVE_MD5:
+        os.remove(archive_name)
         raise Exception("Hash mismatch of test data file")
       archive = tarfile.open(archive_name, 'r:bz2')
       archive.extractall(join(self.root))
