65d2bd25bd0e3b7bb92e8e9a213837e4e4084185
mstarzinger@chromium.org <mstarzinger@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-04-18 16:56:20 +0000

    Revert "Issue 2089 Expose value wrapper's inner values"
    R=jkummerow@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10021056
    
    git-svn-id: http://v8.googlecode.com/svn/trunk@11377 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mirror-debugger.js
+++ src/mirror-debugger.js
@@ -596,23 +596,6 @@ ObjectMirror.prototype.protoObject = function() {
 };
 
 
-/**
- * Return the primitive value if this is object of Boolean, Number or String
- * type (but not Date). Otherwise return undefined. 
- */
-ObjectMirror.prototype.primitiveValue = function() {
-  if (!IS_STRING_WRAPPER(this.value_) && !IS_NUMBER_WRAPPER(this.value_) &&
-      !IS_BOOLEAN_WRAPPER(this.value_)) {
-    return void 0;
-  }
-  var primitiveValue = %_ValueOf(this.value_);
-  if (IS_UNDEFINED(primitiveValue)) {
-    return void 0;
-  }
-  return MakeMirror(primitiveValue);
-};
-
-
 ObjectMirror.prototype.hasNamedInterceptor = function() {
   // Get information on interceptors for this object.
   var x = %GetInterceptorInfo(this.value_);
@@ -2250,11 +2233,6 @@ JSONProtocolSerializer.prototype.serializeObject_ = function(mirror, content,
       this.serializeReference(mirror.constructorFunction());
   content.protoObject = this.serializeReference(mirror.protoObject());
   content.prototypeObject = this.serializeReference(mirror.prototypeObject());
-  
-  var primitiveValue = mirror.primitiveValue();
-  if (!IS_UNDEFINED(primitiveValue)) {
-    content.primitiveValue = this.serializeReference(primitiveValue);
-  }
 
   // Add flags to indicate whether there are interceptors.
   if (mirror.hasNamedInterceptor()) {
