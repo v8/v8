c5b9783edbb812e1044b1410d0872b904411410b
yangguo@chromium.org <yangguo@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-04-26 07:45:29 +0000

    Remove unused variable in src/platform-solaris.cc.
    Based on a patch contributed by Heinz Gies <heinz@licenser.net>
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10231004
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@11440 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/platform-solaris.cc
+++ src/platform-solaris.cc
@@ -487,12 +487,10 @@ void Thread::set_name(const char* name) {
 
 
 void Thread::Start() {
-  pthread_attr_t* attr_ptr = NULL;
   pthread_attr_t attr;
   if (stack_size_ > 0) {
     pthread_attr_init(&attr);
     pthread_attr_setstacksize(&attr, static_cast<size_t>(stack_size_));
-    attr_ptr = &attr;
   }
   pthread_create(&data_->thread_, NULL, ThreadEntry, this);
   ASSERT(data_->thread_ != kNoThread);
