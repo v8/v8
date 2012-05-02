22207e827020c0e8e13a499dba2411867d837eae
vegorov@chromium.org <vegorov@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-05-02 09:25:23 +0000

    Merge r11471 from the bleeding_edge to the 3.9 branch.
    Don't ignore return value of CommitCodePage in AllocateAlignedMemory.
    
    Release the mapping as whole if commit failed to avoid leaking virtual address space.
    
    R=mstarzinger@chromium.org
    BUG=chromium:118625
    
    Review URL: https://chromiumcodereview.appspot.com/10260012
    ------------------------------------------------------------------------
    Review URL: https://chromiumcodereview.appspot.com/10270015
    
    git-svn-id: http://v8.googlecode.com/svn/branches/3.9@11475 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/spaces.h
+++ src/spaces.h
@@ -1040,7 +1040,9 @@ class MemoryAllocator {
     return CodePageAreaEndOffset() - CodePageAreaStartOffset();
   }
 
-  static bool CommitCodePage(VirtualMemory* vm, Address start, size_t size);
+  MUST_USE_RESULT static bool CommitCodePage(VirtualMemory* vm,
+                                             Address start,
+                                             size_t size);
 
  private:
   Isolate* isolate_;
