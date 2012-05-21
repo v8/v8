460083b10101e49b0af275866aa936b12ed7c4fe
erik.corry@gmail.com <erik.corry@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-05-21 13:43:33 +0000

    Fix prepareElementsForSort crash bug=117879 . This is a back port to 3.8 of r11588 Review URL: https://chromiumcodereview.appspot.com/10413030
    git-svn-id: http://v8.googlecode.com/svn/branches/3.8@11612 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/runtime.h
+++ src/runtime.h
@@ -271,7 +271,6 @@ namespace internal {
   F(GetArrayKeys, 2, 1) \
   F(MoveArrayContents, 2, 1) \
   F(EstimateNumberOfElements, 1, 1) \
-  F(SwapElements, 3, 1) \
   \
   /* Getters and Setters */ \
   F(DefineAccessor, -1 /* 4 or 5 */, 1) \
@@ -530,8 +529,7 @@ namespace internal {
   F(RegExpExec, 4, 1)                                                        \
   F(RegExpConstructResult, 3, 1)                                             \
   F(GetFromCache, 2, 1)                                                      \
-  F(NumberToString, 1, 1)                                                    \
-  F(SwapElements, 3, 1)
+  F(NumberToString, 1, 1)
 
 
 //---------------------------------------------------------------------------
