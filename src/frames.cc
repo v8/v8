06723da2727769a07172da2f5ad22a4194db4a41
vegorov@chromium.org <vegorov@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-06-12 10:30:51 +0000

    Fix arm build.
    TBR=danno@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10543108
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@11773 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/frames.cc
+++ src/frames.cc
@@ -470,7 +470,7 @@ StackFrame::Type StackFrame::GetCallerState(State* state) const {
 
 
 Address StackFrame::UnpaddedFP() const {
-#if defined(V8_TARGET_ARCH_IA32) || defined(V8_HOST_ARCH_IA32)
+#if defined(V8_TARGET_ARCH_IA32)
   if (!is_optimized()) return fp();
   int32_t alignment_state = Memory::int32_at(
     fp() + JavaScriptFrameConstants::kDynamicAlignmentStateOffset);
