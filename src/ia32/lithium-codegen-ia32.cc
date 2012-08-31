2e652e6f915f3347c3da9dbb306a2d484ec3a8fa
yangguo@chromium.org <yangguo@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-08-31 14:45:31 +0000

    Fix VS2005 build.
    R=jkummerow@chromium.org
    BUG=v8:2313
    
    Review URL: https://chromiumcodereview.appspot.com/10907029
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12420 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/ia32/lithium-codegen-ia32.cc
+++ src/ia32/lithium-codegen-ia32.cc
@@ -4984,8 +4984,8 @@ void LCodeGen::EmitDeepCopy(Handle<JSObject> object,
           Handle<FixedDoubleArray>::cast(elements);
       for (int i = 0; i < elements_length; i++) {
         int64_t value = double_array->get_representation(i);
-        int32_t value_low = value & 0xFFFFFFFF;
-        int32_t value_high = value >> 32;
+        int32_t value_low = static_cast<int32_t>(value & 0xFFFFFFFF);
+        int32_t value_high = static_cast<int32_t>(value >> 32);
         int total_offset =
             elements_offset + FixedDoubleArray::OffsetOfElementAt(i);
         __ mov(FieldOperand(result, total_offset), Immediate(value_low));
