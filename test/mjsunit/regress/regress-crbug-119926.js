cc6fe90b2bf8c52b855efd2e23992b9425bcf80e
jkummerow@chromium.org <jkummerow@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-09-24 10:44:04 +0000

    Remove trailing whitespace
    R=mstarzinger@chromium.org
    
    Review URL: https://codereview.chromium.org/10969064
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12594 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/mjsunit/regress/regress-crbug-119926.js
+++ test/mjsunit/regress/regress-crbug-119926.js
@@ -25,7 +25,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// Flags: --gc-global 
+// Flags: --gc-global
 
 // Test that array elements don't break upon garbage collection.
 
