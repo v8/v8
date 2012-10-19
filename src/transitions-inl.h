4f40326fae7355ce899c892ac2b79b95d6de12a7
verwaest@chromium.org <verwaest@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-10-19 15:03:27 +0000

    Fast path for searching through simple transitions
    Review URL: https://chromiumcodereview.appspot.com/11189089
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12778 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/transitions-inl.h
+++ src/transitions-inl.h
@@ -191,6 +191,11 @@ PropertyDetails TransitionArray::GetTargetDetails(int transition_number) {
 
 
 int TransitionArray::Search(String* name) {
+  if (IsSimpleTransition()) {
+    String* key = GetKey(kSimpleTransitionIndex);
+    if (key->Equals(name)) return kSimpleTransitionIndex;
+    return kNotFound;
+  }
   return internal::Search<ALL_ENTRIES>(this, name);
 }
 
