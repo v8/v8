f910052543d56c073bdcff736980f84f492dbcc4
yangguo@chromium.org <yangguo@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2012-10-19 12:30:18 +0000

    Always invoke the default Array.sort functions from builtin functions, part 2.
    R=vegorov@chromium.org
    BUG=v8:2372
    
    Review URL: https://chromiumcodereview.appspot.com/11175007
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12774 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/mjsunit/regress/regress-builtin-array-op.js
+++ test/mjsunit/regress/regress-builtin-array-op.js
@@ -29,7 +29,8 @@
 // array operations.
 
 var foo =  "hest";
-Array.prototype.sort = function(fn) { foo = "fisk"; }
+Array.prototype.sort = function(fn) { foo = "fisk"; };
+Function.prototype.call = function() { foo = "caramel"; };
 var a = [2,3,1];
 a[100000] = 0;
 a.join();
