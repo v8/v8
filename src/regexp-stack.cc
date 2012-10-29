v8/v8

commit 889de3b780c7f3c3fdc11680c0e1be13e3885b5e

Fix memory leak in RegExpStack.
R=yangguo@chromium.org

Review URL: https://chromiumcodereview.appspot.com/11275037

git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12812 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/regexp-stack.cc
@@ -72,7 +72,7 @@ char* RegExpStack::RestoreStack(char* from) {
 
 
 void RegExpStack::Reset() {
-  if (thread_local_.memory_size_ > kMinimumStackSize) {
+  if (thread_local_.memory_size_ >= kMinimumStackSize) {
     DeleteArray(thread_local_.memory_);
     thread_local_ = ThreadLocal();
   }

commit 42c7166e751071e8ddf61e73e734b3cf4c152c35

Fix performance regression introduced by r12812.
R=yangguo@chromium.org

Review URL: https://chromiumcodereview.appspot.com/11301014

git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@12829 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/regexp-stack.cc
@@ -51,6 +51,7 @@ RegExpStack::RegExpStack()
 
 
 RegExpStack::~RegExpStack() {
+  thread_local_.Free();
 }
 
 
@@ -72,7 +73,7 @@ char* RegExpStack::RestoreStack(char* from) {
 
 
 void RegExpStack::Reset() {
-  if (thread_local_.memory_size_ >= kMinimumStackSize) {
+  if (thread_local_.memory_size_ > kMinimumStackSize) {
     DeleteArray(thread_local_.memory_);
     thread_local_ = ThreadLocal();
   }