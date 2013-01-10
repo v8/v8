c5cff2c75a3397df1c27246b2c7e2432a7413d22
mstarzinger@chromium.org <mstarzinger@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-01-10 14:21:27 +0000

    Make recent regression test resilient against GC stress.
    R=danno@chromium.org
    TEST=mjsunit/regress/regress-165637
    
    Review URL: https://codereview.chromium.org/11824062
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@13353 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/mjsunit/regress/regress-165637.js
+++ test/mjsunit/regress/regress-165637.js
@@ -45,6 +45,10 @@ function do_slices() {
   return Date.now() - start;
 }
 
+// Reset the GC interval to be off. Needed so that the runtime of this test
+// stays within bounds even if we run in GC stress mode.
+%SetFlags("--gc-interval=-1");
+
 // Should never take more than 3 seconds (if the bug is fixed, the test takes
 // considerably less time than 3 seconds).
 assertTrue(do_slices() < (3 * 1000));
