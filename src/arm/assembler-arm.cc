3a23c2f7df19dfe3a56679ecd3824d8ce5e5fe83
ulan@chromium.org <ulan@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-01-30 09:55:49 +0000

    Make DwVfpRegister::AllocationIndexToString use VFPRegister::Name
    This also fixes a bug caused by a missing comma after "d13" in
    the 'names' array in AllocationIndexToString.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/12082044
    Patch from Hans Wennborg <hans@chromium.org>.
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@13548 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/arm/assembler-arm.cc
+++ src/arm/assembler-arm.cc
@@ -101,42 +101,7 @@ const char* DwVfpRegister::AllocationIndexToString(int index) {
     if (index >= kDoubleRegZero.code())
       index += kNumReservedRegisters;
 
-    // TODO(hans): Maybe this could just use VFPRegisters::Name()?
-    const char* const names[] = {
-      "d0",
-      "d1",
-      "d2",
-      "d3",
-      "d4",
-      "d5",
-      "d6",
-      "d7",
-      "d8",
-      "d9",
-      "d10",
-      "d11",
-      "d12",
-      "d13"
-      "d14",
-      "d15",
-      "d16",
-      "d17",
-      "d18",
-      "d19",
-      "d20",
-      "d21",
-      "d22",
-      "d23",
-      "d24",
-      "d25",
-      "d26",
-      "d27",
-      "d28",
-      "d29",
-      "d30",
-      "d31"
-    };
-    return names[index];
+    return VFPRegisters::Name(index, true);
   } else {
     ASSERT(index == 0);
     return "sfpd0";
