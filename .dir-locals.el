a5780528b48d4e124e07927dfb4c8ef939905311
svenpanne@chromium.org <svenpanne@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-03-08 13:47:58 +0000

    Add .dir-locals.el.
    * .dir-locals.el: New file, to help Emacs users produce better
      changesets.  In particular, prevents automatic copyright year
      updates.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/12687003
    Patch from  Andy Wingo <wingo@igalia.com>.
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@13884 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;; Per-directory local variables for GNU Emacs 23 and later.
+
+((nil
+  (copyright-update . nil)
+  (fill-column . 80))
+ (c-mode
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 2))
+ (c++-mode
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 2)))
