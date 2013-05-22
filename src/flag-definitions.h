a63db0f4f65c8a05c820efb39c7a093f76f1098f
verwaest@chromium.org <verwaest@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-05-22 07:43:12 +0000

    Temporarily turn off tracking of fields for stability.
    R=ulan@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15686004
    
    git-svn-id: http://v8.googlecode.com/svn/trunk@14740 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -194,7 +194,7 @@ DEFINE_bool(clever_optimizations,
             "Optimize object size, Array shift, DOM strings and string +")
 DEFINE_bool(pretenure_literals, true, "allocate literals in old space")
 DEFINE_bool(track_fields, true, "track fields with only smi values")
-DEFINE_bool(track_double_fields, true, "track fields with double values")
+DEFINE_bool(track_double_fields, false, "track fields with double values")
 DEFINE_bool(track_heap_object_fields, true, "track fields with heap values")
 DEFINE_implication(track_double_fields, track_fields)
 DEFINE_implication(track_heap_object_fields, track_fields)
