6be103c041c27876ed2fee82edf64100e005b090
bmeurer@chromium.org <bmeurer@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-08-14 14:34:40 +0000

    Drop broken and unnecessary RequiredInputRepresentation() in HMathFloorOfDiv.
    R=titzer@chromium.org
    
    Review URL: https://codereview.chromium.org/23190002
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@16190 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/hydrogen-instructions.h
+++ src/hydrogen-instructions.h
@@ -3841,10 +3841,6 @@ class HMathFloorOfDiv: public HBinaryOperation {
 
   virtual HValue* EnsureAndPropagateNotMinusZero(BitVector* visited);
 
-  virtual Representation RequiredInputRepresentation(int index) {
-    return Representation::Integer32();
-  }
-
   DECLARE_CONCRETE_INSTRUCTION(MathFloorOfDiv)
 
  protected:
