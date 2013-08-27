dc6a16d6e084053611de1625e7f93ab5fd5e2649
hpayer@chromium.org <hpayer@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-08-27 11:51:03 +0000

    Filler sizes have to be Smis, fix x64 breakage.
    BUG=
    R=jkummerow@chromium.org
    
    Review URL: https://codereview.chromium.org/23577002
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@16355 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/hydrogen-instructions.h
+++ src/hydrogen-instructions.h
@@ -3246,8 +3246,10 @@ class HConstant V8_FINAL : public HTemplateInstruction<0> {
   static HConstant* CreateAndInsertAfter(Zone* zone,
                                          HValue* context,
                                          int32_t value,
+                                         Representation representation,
                                          HInstruction* instruction) {
-    HConstant* new_constant = HConstant::New(zone, context, value);
+    HConstant* new_constant =
+        HConstant::New(zone, context, value, representation);
     new_constant->InsertAfter(instruction);
     return new_constant;
   }
@@ -3255,8 +3257,10 @@ class HConstant V8_FINAL : public HTemplateInstruction<0> {
   static HConstant* CreateAndInsertBefore(Zone* zone,
                                           HValue* context,
                                           int32_t value,
+                                          Representation representation,
                                           HInstruction* instruction) {
-    HConstant* new_constant = HConstant::New(zone, context, value);
+    HConstant* new_constant =
+        HConstant::New(zone, context, value, representation);
     new_constant->InsertBefore(instruction);
     return new_constant;
   }
