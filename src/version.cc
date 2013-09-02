1e7a5edaa13fc08276bc48b86c09206f4b2aef8f
jkummerow@chromium.org <jkummerow@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-09-02 14:36:37 +0000

    Merged r16324 into 3.19 branch.
    Lower kInitialMaxFastElementArray constant to 95K
    
    BUG=v8:2790
    R=mstarzinger@chromium.org
    
    Review URL: https://codereview.chromium.org/23821003
    
    git-svn-id: http://v8.googlecode.com/svn/branches/3.19@16479 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -35,7 +35,7 @@
 #define MAJOR_VERSION     3
 #define MINOR_VERSION     19
 #define BUILD_NUMBER      18
-#define PATCH_LEVEL       21
+#define PATCH_LEVEL       22
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
 #define IS_CANDIDATE_VERSION 0
