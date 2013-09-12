4f09f428663cde5f5c4624a6016a66c98baf927c
palfia@homejinni.com <palfia@homejinni.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-09-12 21:17:43 +0000

    MIPS: Improve code generation for the HRandom instruction.
    Port r16684 (e0f70aa)
    
    Original commit message:
    Since the per-context random number generator is now
    properly seeded upon context creation, we do not need
    to check for lazy-initialization anymore, and so we
    can implement the HRandom instruction w/o having to
    call into the C function (which means we don't need
    to MarkAsCall anymore).
    
    TEST=cctest/test-random
    BUG=
    R=gergely@homejinni.com
    
    Review URL: https://codereview.chromium.org/23851028
    
    Patch from Balazs Kilvady <kilvadyb@homejinni.com>.
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@16695 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/lithium-mips.cc
+++ src/mips/lithium-mips.cc
@@ -1624,9 +1624,13 @@ LInstruction* LChunkBuilder::DoPower(HPower* instr) {
 LInstruction* LChunkBuilder::DoRandom(HRandom* instr) {
   ASSERT(instr->representation().IsDouble());
   ASSERT(instr->global_object()->representation().IsTagged());
-  LOperand* global_object = UseFixed(instr->global_object(), a0);
-  LRandom* result = new(zone()) LRandom(global_object);
-  return MarkAsCall(DefineFixedDouble(result, f0), instr);
+  LOperand* global_object = UseTempRegister(instr->global_object());
+  LOperand* scratch = TempRegister();
+  LOperand* scratch2 = TempRegister();
+  LOperand* scratch3 = TempRegister();
+  LRandom* result = new(zone()) LRandom(
+      global_object, scratch, scratch2, scratch3);
+  return DefineFixedDouble(result, f0);
 }
 
 
