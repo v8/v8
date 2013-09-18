7e0cc857655dad1f7b24011007252a5139eac2d4
machenbach@chromium.org <machenbach@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-09-18 08:58:01 +0000

    Make testing i18n the default.
    If i18n support is on when building with make, the intl test suite is automatically run with "check".
    
    On the bots, we'll turn it off explicitly, where required.
    
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/23702044
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@16785 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- Makefile
+++ Makefile
@@ -127,6 +127,7 @@ endif
 # i18nsupport=off
 ifeq ($(i18nsupport), off)
   GYPFLAGS += -Dv8_enable_i18n_support=0
+  TESTFLAGS += --noi18n
 endif
 # arm specific flags.
 # armv7=false/true
@@ -331,7 +332,7 @@ $(addsuffix .check, $(ANDROID_ARCHES)): \
 $(addsuffix .check, $(NACL_BUILDS)): $$(basename $$@)
 	@tools/run-tests.py $(TESTJOBS) --outdir=$(OUTDIR) \
 	     --arch-and-mode=$(basename $@) \
-	     --timeout=600 --nopresubmit \
+	     --timeout=600 --nopresubmit --noi18n \
 	     --command-prefix="tools/nacl-run.py"
 
 $(addsuffix .check, $(NACL_ARCHES)): \
--- tools/run-tests.py
+++ tools/run-tests.py
@@ -112,6 +112,9 @@ def BuildOptions():
   result.add_option("-m", "--mode",
                     help="The test modes in which to run (comma-separated)",
                     default="release,debug")
+  result.add_option("--no-i18n", "--noi18n",
+                    help="Skip internationalization tests",
+                    default=False, action="store_true")
   result.add_option("--no-network", "--nonetwork",
                     help="Don't distribute tests on the network",
                     default=(utils.GuessOS() != "linux"),
@@ -210,6 +213,8 @@ def ProcessOptions(options):
   if not options.flaky_tests in ["run", "skip", "dontcare"]:
     print "Unknown flaky test mode %s" % options.flaky_tests
     return False
+  if not options.no_i18n:
+    DEFAULT_TESTS.append("intl")
   return True
 
 
