0cb81e8e6233ef9f5a5bf91b41ca13ea7b066ce9
machenbach@chromium.org <machenbach@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-09-25 12:37:00 +0000

    Add windows to status variable list.
    This will allow to modify test expectation for system==windows.
    
    R=jkummerow@chromium.org
    
    Review URL: https://codereview.chromium.org/24528003
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@16941 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- tools/testrunner/local/statusfile.py
+++ tools/testrunner/local/statusfile.py
@@ -52,7 +52,7 @@ DEFS = {FAIL_OK: [FAIL, OKAY],
 # Support arches, modes to be written as keywords instead of strings.
 VARIABLES = {ALWAYS: True}
 for var in ["debug", "release", "android_arm", "android_ia32", "arm", "ia32",
-            "mipsel", "x64", "nacl_ia32", "nacl_x64", "macos"]:
+            "mipsel", "x64", "nacl_ia32", "nacl_x64", "macos", "windows"]:
   VARIABLES[var] = var
 
 
