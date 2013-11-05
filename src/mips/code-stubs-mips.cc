4e4169d599f487312bd168cc5ce4ea60c40e576a
plind44@gmail.com <plind44@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-11-05 19:56:40 +0000

    MIPS: Try to use Push instead of push sequences whenever possible.
    Port r17466 (e519285)
    
    BUG=
    R=plind44@gmail.com
    
    Review URL: https://codereview.chromium.org/58853003
    
    Patch from Balazs Kilvady <kilvadyb@homejinni.com>.
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@17507 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/code-stubs-mips.cc
+++ src/mips/code-stubs-mips.cc
@@ -5156,8 +5156,7 @@ void ICCompareStub::GenerateMiss(MacroAssembler* masm) {
         ExternalReference(IC_Utility(IC::kCompareIC_Miss), masm->isolate());
     FrameScope scope(masm, StackFrame::INTERNAL);
     __ Push(a1, a0);
-    __ push(ra);
-    __ Push(a1, a0);
+    __ Push(ra, a1, a0);
     __ li(t0, Operand(Smi::FromInt(op_)));
     __ addiu(sp, sp, -kPointerSize);
     __ CallExternalReference(miss, 3, USE_DELAY_SLOT);
