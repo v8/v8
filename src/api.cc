3c8cee2f8ddaf82ac1a9e45a61fdb3203abadd87
bmeurer@chromium.org <bmeurer@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-11-06 06:59:32 +0000

    Add isolate parameter to SetResourceConstraints, and deprecate version which depends on current isolate.
    BUG=None
    R=bmeurer@chromium.org
    
    Review URL: https://codereview.chromium.org/38993005
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@17509 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/api.cc
+++ src/api.cc
@@ -568,7 +568,14 @@ ResourceConstraints::ResourceConstraints()
 
 bool SetResourceConstraints(ResourceConstraints* constraints) {
   i::Isolate* isolate = EnterIsolateIfNeeded();
+  return SetResourceConstraints(reinterpret_cast<Isolate*>(isolate),
+                                constraints);
+}
 
+
+bool SetResourceConstraints(Isolate* v8_isolate,
+                            ResourceConstraints* constraints) {
+  i::Isolate* isolate = reinterpret_cast<i::Isolate*>(v8_isolate);
   int young_space_size = constraints->max_young_space_size();
   int old_gen_size = constraints->max_old_space_size();
   int max_executable_size = constraints->max_executable_size();
