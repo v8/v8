22507d2b676f48d4e89218595c664f2e325eb9f8
marja@chromium.org <marja@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-11-07 09:06:55 +0000

    Experimental lexer generator: variable naming fix.
    TBR=dcarney@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/64063002
    
    git-svn-id: http://v8.googlecode.com/svn/branches/experimental@17544 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- parser/tools/lexer_generator/generator.py
+++ parser/tools/lexer_generator/generator.py
@@ -80,8 +80,8 @@ def process_rules(parser_state):
   builder.set_character_classes(parser_state.character_classes)
   for k, v in parser_state.rules.items():
     graphs = []
-    for (graph, code, action, precedence) in v['regex']:
-      graphs.append(NfaBuilder.add_action(graph, (code, action, precedence)))
+    for (graph, precedence, code, action) in v['regex']:
+      graphs.append(NfaBuilder.add_action(graph, (precedence, code, action)))
     rule_map[k] = builder.nfa(NfaBuilder.or_graphs(graphs))
   html_data = []
   for rule_name, nfa in rule_map.items():
