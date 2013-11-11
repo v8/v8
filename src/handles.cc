d5cb83f4aa223d15e56e5b93a9f502395fdcb83c
mstarzinger@chromium.org <mstarzinger@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-11-11 16:27:36 +0000

    Fix invalid reuse of weak global handle in GetScriptWrapper.
    This fixes a direct usage of a weak global handle in GetScriptWrapper
    that just casted it to a strong local handle, while a subsequent GC
    might clear it. Handlepocalypse anyone?
    
    R=machenbach@chromium.org
    BUG=v8:2988
    TEST=mjsunit/regress/regress-2988
    
    Review URL: https://codereview.chromium.org/67273004
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@17622 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/handles.cc
+++ src/handles.cc
@@ -242,9 +242,9 @@ static void ClearWrapperCache(v8::Isolate* v8_isolate,
 
 Handle<JSValue> GetScriptWrapper(Handle<Script> script) {
   if (script->wrapper()->foreign_address() != NULL) {
-    // Return the script wrapper directly from the cache.
+    // Return a handle for the existing script wrapper from the cache.
     return Handle<JSValue>(
-        reinterpret_cast<JSValue**>(script->wrapper()->foreign_address()));
+        *reinterpret_cast<JSValue**>(script->wrapper()->foreign_address()));
   }
   Isolate* isolate = script->GetIsolate();
   // Construct a new script wrapper.
@@ -255,10 +255,10 @@ Handle<JSValue> GetScriptWrapper(Handle<Script> script) {
 
   // The allocation might have triggered a GC, which could have called this
   // function recursively, and a wrapper has already been created and cached.
-  // In that case, simply return the cached wrapper.
+  // In that case, simply return a handle for the cached wrapper.
   if (script->wrapper()->foreign_address() != NULL) {
     return Handle<JSValue>(
-        reinterpret_cast<JSValue**>(script->wrapper()->foreign_address()));
+        *reinterpret_cast<JSValue**>(script->wrapper()->foreign_address()));
   }
 
   result->set_value(*script);
