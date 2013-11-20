adb39abd6dc568fa0ec0126fef540b70e66c8713
danno@chromium.org <danno@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-11-20 12:58:10 +0000

    Activate Hydrogen-generated KeyedLoadDictionary ICs
    R=ulan@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/59813011
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@17921 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -202,7 +202,7 @@ DEFINE_implication(harmony_observation, harmony_collections)
 // Flags for experimental implementation features.
 DEFINE_bool(packed_arrays, true, "optimizes arrays that have no holes")
 DEFINE_bool(smi_only_arrays, true, "tracks arrays with only smi values")
-DEFINE_bool(compiled_keyed_dictionary_loads, false,
+DEFINE_bool(compiled_keyed_dictionary_loads, true,
             "use optimizing compiler to generate keyed dictionary load stubs")
 DEFINE_bool(clever_optimizations, true,
             "Optimize object size, Array shift, DOM strings and string +")
