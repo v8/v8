7a8a098a5e68a6e5e253ffd564a189248f63d259
machenbach@chromium.org <machenbach@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2013-12-11 15:05:53 +0000

    Make squash commits step more pythony in push-to-trunk script.
    Get rid of linux-only shell commands. Solve issue with quotation marks in commit messages.
    
    Further behavioral change: Strip white space on line endings. Strip trailing new lines.
    
    Test=python -m unittest test_scripts
    
    R=ulan@chromium.org
    
    Review URL: https://codereview.chromium.org/101763002
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@18305 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- tools/push-to-trunk/test_scripts.py
+++ tools/push-to-trunk/test_scripts.py
@@ -543,16 +543,10 @@ class ScriptTest(unittest.TestCase):
     cl = GetLastChangeLogEntries(TEST_CONFIG[CHANGELOG_FILE])
     self.assertEquals(cl_chunk, cl)
 
-  def testSquashCommits(self):
+  def _TestSquashCommits(self, change_log, expected_msg):
     TEST_CONFIG[CHANGELOG_ENTRY_FILE] = self.MakeEmptyTempFile()
     with open(TEST_CONFIG[CHANGELOG_ENTRY_FILE], "w") as f:
-      f.write("1999-11-11: Version 3.22.5\n")
-      f.write("\n")
-      f.write("        Log text 1.\n")
-      f.write("        Chromium issue 12345\n")
-      f.write("\n")
-      f.write("        Performance and stability improvements on all "
-              "platforms.\n")
+      f.write(change_log)
 
     self.ExpectGit([
       ["diff svn/trunk hash1", "patch content"],
@@ -562,15 +556,43 @@ class ScriptTest(unittest.TestCase):
     self.MakeStep().Persist("date", "1999-11-11")
 
     self.MakeStep(SquashCommits).Run()
-
-    msg = FileToText(TEST_CONFIG[COMMITMSG_FILE])
-    self.assertTrue(re.search(r"Version 3\.22\.5", msg))
-    self.assertTrue(re.search(r"Performance and stability", msg))
-    self.assertTrue(re.search(r"Log text 1\. Chromium issue 12345", msg))
-    self.assertFalse(re.search(r"\d+\-\d+\-\d+", msg))
+    self.assertEquals(FileToText(TEST_CONFIG[COMMITMSG_FILE]), expected_msg)
 
     patch = FileToText(TEST_CONFIG[ PATCH_FILE])
     self.assertTrue(re.search(r"patch content", patch))
+
+  def testSquashCommitsUnformatted(self):
+    change_log = """1999-11-11: Version 3.22.5
+
+        Log text 1.
+        Chromium issue 12345
+
+        Performance and stability improvements on all platforms.\n"""
+    commit_msg = """Version 3.22.5
+
+Log text 1. Chromium issue 12345
+
+Performance and stability improvements on all platforms."""
+    self._TestSquashCommits(change_log, commit_msg)
+
+  def testSquashCommitsFormatted(self):
+    change_log = """1999-11-11: Version 3.22.5
+
+        Long commit message that fills more than 80 characters (Chromium issue
+        12345).
+
+        Performance and stability improvements on all platforms.\n"""
+    commit_msg = """Version 3.22.5
+
+Long commit message that fills more than 80 characters (Chromium issue 12345).
+
+Performance and stability improvements on all platforms."""
+    self._TestSquashCommits(change_log, commit_msg)
+
+  def testSquashCommitsQuotationMarks(self):
+    change_log = """Line with "quotation marks".\n"""
+    commit_msg = """Line with "quotation marks"."""
+    self._TestSquashCommits(change_log, commit_msg)
 
   def _PushToTrunk(self, force=False, manual=False):
     TEST_CONFIG[DOT_GIT_LOCATION] = self.MakeEmptyTempFile()
