5be6daa942421a32c6c7cbd2ccae4dcdad347daa
machenbach@chromium.org <machenbach@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-01-31 09:10:56 +0000

    Fix deopt fuzzer variable set up.
    Adds the simulator variable configuration from r18959 to the deopt fuzzer.
    
    BUG=
    R=jkummerow@chromium.org
    
    Review URL: https://codereview.chromium.org/143403004
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@18975 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -388,13 +388,6 @@ def Execute(arch, mode, args, options, suites, workspace):
                         options.no_i18n)
 
   # Find available test suites and read test cases from them.
-  machine = platform.machine()
-  if (machine and
-      (arch == "mipsel" or arch == "arm") and
-      not arch.startswith(machine)):
-    use_simulator = True
-  else:
-    use_simulator = False
   variables = {
     "arch": arch,
     "asan": options.asan,
@@ -403,8 +396,8 @@ def Execute(arch, mode, args, options, suites, workspace):
     "isolates": options.isolates,
     "mode": mode,
     "no_i18n": options.no_i18n,
+    "simulator": utils.UseSimulator(arch),
     "system": utils.GuessOS(),
-    "simulator": use_simulator,
   }
   all_tests = []
   num_tests = 0
