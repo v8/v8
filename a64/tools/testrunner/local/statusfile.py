2af8c0eb41aaac9a4255508eb16569318b5f4bff
jacob.bramley@arm.com <jacob.bramley@arm.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-02-04 17:58:28 +0000

    A64: Synchronize with r16919.
    This brings experimental/a64 up to 2013-09-24, and introduces the new
    status file format.
    
    The new status files were taken generated versions from r16918. They are
    formatted slightly differently than in r16919, but we can fix this
    manually later.
    
    BUG=none
    R=m.m.capewell@googlemail.com
    
    Review URL: https://codereview.chromium.org/154113002
    
    git-svn-id: http://v8.googlecode.com/svn/branches/experimental@19081 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- a64/tools/testrunner/local/statusfile.py
+++ a64/tools/testrunner/local/statusfile.py
@@ -26,14 +26,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-# These imports are required for the on-demand conversion from
-# old to new status file format.
-from os.path import exists
-from os.path import getmtime
-
-from . import old_statusfile
-
-
 # These outcomes can occur in a TestCase's outcomes list:
 SKIP = "SKIP"
 FAIL = "FAIL"
@@ -116,18 +108,6 @@ def _ParseOutcomeList(rule, outcomes, target_dict, variables):
 
 
 def ReadStatusFile(path, variables):
-  # As long as the old-format .status files are authoritative, just
-  # create the converted version on demand and cache it to speed up
-  # subsequent runs.
-  if path.endswith(".status"):
-    newpath = path + "2"
-    if not exists(newpath) or getmtime(newpath) < getmtime(path):
-      print "Converting status file."
-      converted = old_statusfile.ConvertNotation(path).GetOutput()
-      with open(newpath, 'w') as f:
-        f.write(converted)
-    path = newpath
-
   with open(path) as f:
     global KEYWORDS
     contents = eval(f.read(), KEYWORDS)
