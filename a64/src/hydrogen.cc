ce7536f9ff2a2dda977024df0969ed929158f688
jochen@chromium.org <jochen@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-02-06 13:25:58 +0000

    A64: Disable inlining of functions that have a different context
    On A64, the register allocator doesn't have control of cp, so we can't
    arbitrarily switch the context
    
    BUG=none
    R=rodolph.perfetta@gmail.com, ulan@chromium.org, rodolph.perfetta@arm.com
    LOG=n
    
    Review URL: https://codereview.chromium.org/153623010
    
    git-svn-id: http://v8.googlecode.com/svn/branches/experimental@19157 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- a64/src/hydrogen.cc
+++ a64/src/hydrogen.cc
@@ -7023,6 +7023,19 @@ bool HOptimizedGraphBuilder::TryInline(CallKind call_kind,
     return false;
   }
 
+#if V8_TARGET_ARCH_A64
+  // Target must be able to use caller's context.
+  // TODO(jochen): Remove this block once A64's register allocator can allocate
+  // cp.
+  CompilationInfo* outer_info = current_info();
+  if (target->context() != outer_info->closure()->context() ||
+      outer_info->scope()->contains_with() ||
+      outer_info->scope()->num_heap_slots() > 0) {
+    TraceInline(target, caller, "target requires context change");
+    return false;
+  }
+#endif
+
   // Don't inline deeper than the maximum number of inlining levels.
   HEnvironment* env = environment();
   int current_level = 1;
@@ -7161,8 +7174,12 @@ bool HOptimizedGraphBuilder::TryInline(CallKind call_kind,
                                      function_state()->inlining_kind(),
                                      undefined_receiver);
 
+// TODO(jochen): Remove this #ifdef once A64's register allocator can allocate
+// cp.
+#if !V8_TARGET_ARCH_A64
   HConstant* context = Add<HConstant>(Handle<Context>(target->context()));
   inner_env->BindContext(context);
+#endif
 
   Add<HSimulate>(return_id);
   current_block()->UpdateEnvironment(inner_env);
