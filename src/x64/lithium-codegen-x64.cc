b765d3cdb90a121520e220f29d6a2b4902a2efcb
jarin@chromium.org <jarin@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-03-24 21:32:19 +0000

    Revert the (wrong) fix of the argument index check asserion.
    R=ishell@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/208423017
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@20219 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x64/lithium-codegen-x64.cc
+++ src/x64/lithium-codegen-x64.cc
@@ -2910,14 +2910,9 @@ void LCodeGen::DoAccessArgumentsAt(LAccessArgumentsAt* instr) {
       instr->index()->IsConstantOperand()) {
     int32_t const_index = ToInteger32(LConstantOperand::cast(instr->index()));
     int32_t const_length = ToInteger32(LConstantOperand::cast(instr->length()));
-    if (const_index < const_length) {
-      StackArgumentsAccessor args(arguments, const_length,
-                                  ARGUMENTS_DONT_CONTAIN_RECEIVER);
-      __ movp(result, args.GetArgumentOperand(const_index));
-    } else {
-      // This code should never be executed; just stop here.
-      __ int3();
-    }
+    StackArgumentsAccessor args(arguments, const_length,
+                                ARGUMENTS_DONT_CONTAIN_RECEIVER);
+    __ movp(result, args.GetArgumentOperand(const_index));
   } else {
     Register length = ToRegister(instr->length());
     // There are two words between the frame pointer and the last argument.
