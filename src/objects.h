a0355e3745efc4239bdeaf6259e7d57071a623cb
palfia@homejinni.com <palfia@homejinni.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-04-09 11:01:58 +0000

    Fix build with gcc 4.8 on Linux.
    This fixes a build failure on Linux with gcc 4.8, after r20581.
    
    BUG=
    R=ishell@chromium.org
    
    Review URL: https://codereview.chromium.org/229683003
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@20608 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects.h
+++ src/objects.h
@@ -4018,7 +4018,7 @@ class Dictionary: public HashTable<Shape, Key> {
   }
 
   int NextEnumerationIndex() {
-    return Smi::cast(FixedArray::get(kNextEnumerationIndexIndex))->value();
+    return Smi::cast(this->get(kNextEnumerationIndexIndex))->value();
   }
 
   // Returns a new array for dictionary usage. Might return Failure.
