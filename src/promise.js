b280ad6c44e2f8a38bf5aa5713c58a4d2d8155c9
mstarzinger@chromium.org <mstarzinger@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-04-14 11:24:40 +0000

    Try to switch Array builtins into strict mode.
    R=rossberg@chromium.org
    TEST=mjsunit,test262,webkit
    
    Review URL: https://codereview.chromium.org/233083003
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@20717 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/promise.js
+++ src/promise.js
@@ -25,7 +25,6 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 "use strict";
 
 // This file relies on the fact that the following declaration has been made
