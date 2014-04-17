c8c951204c3cf89e0360ea6fdfb363565922fc46
palfia@homejinni.com <palfia@homejinni.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-04-17 20:06:50 +0000

    MIPS: Serializer enable/disable flags need thread safety.
    Port r20855 (ac70e129)
    
    BUG=
    R=plind44@gmail.com
    
    Review URL: https://codereview.chromium.org/239173004
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@20859 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/assembler-mips.h
+++ src/mips/assembler-mips.h
@@ -427,6 +427,11 @@ class CpuFeatures : public AllStatic {
   // is enabled (snapshots must be portable).
   static void Probe();
 
+  // A special case for printing target and features, which we want to do
+  // before initializing the isolate
+  static void SetHintCreatingSnapshot();
+  static void ProbeWithoutIsolate();
+
   // Check whether a feature is supported by the target CPU.
   static bool IsSupported(CpuFeature f) {
     ASSERT(initialized_);
@@ -455,6 +460,9 @@ class CpuFeatures : public AllStatic {
   }
 
  private:
+  static void Probe(bool serializer_enabled);
+  static bool hint_creating_snapshot_;
+
   static bool Check(CpuFeature f, unsigned set) {
     return (set & flag2set(f)) != 0;
   }
