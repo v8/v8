81a101678fff600b0fd1e4176534cc754dc060b3
yangguo@chromium.org <yangguo@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-04-25 14:01:01 +0000

    Expose promise status through promise mirror.
    R=aandrey@chromium.org, rossberg@chromium.org, yurys@chromium.org
    BUG=v8:3093
    LOG=Y
    
    Review URL: https://codereview.chromium.org/257803005
    
    git-svn-id: http://v8.googlecode.com/svn/branches/bleeding_edge@20988 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/promise.js
+++ src/promise.js
@@ -338,3 +338,20 @@ function SetUpPromise() {
 }
 
 SetUpPromise();
+
+// Functions to expose promise details to the debugger.
+function GetPromiseStatus(promise) {
+  return GET_PRIVATE(promise, promiseStatus);
+}
+
+function GetPromiseOnResolve(promise) {
+  return GET_PRIVATE(promise, promiseOnResolve);
+}
+
+function GetPromiseOnReject(promise) {
+  return GET_PRIVATE(promise, promiseOnReject);
+}
+
+function GetPromiseValue(promise) {
+  return GET_PRIVATE(promise, promiseValue);
+}
