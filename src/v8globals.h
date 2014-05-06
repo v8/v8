53bf12681746c12d445c5a7492fbebab004abd72
rmcilroy@chromium.org <rmcilroy@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-05-06 15:56:17 +0000

    Arm64: Ensure that csp is always aligned to 16 byte values even if jssp is not.
    Even although the Arm64 specification specifies that csp
    only needs to be aligned to 16 bytes if it is dereferenced, some implementations show poor performance if csp is every set to a non-aligned value.  This CL ensures that csp is always aligned to 16 byte values on these platforms and adds checks to ensure this in debug mode.
    
    Also makes the following change:
     - Enable CPU support for arm64 to enable probing of cpu implementer and cpu part.
     - Add ALWAYS_ALIGN_CSP CpuFeature for Arm64 and set it based on runtime probing of the cpu implementer.
     - Rename PrepareForPush and PrepareForPop to PushPreamble and PopPostamble and move PopPostable after the pop.
     -
    
    R=jacob.bramley@arm.com, ulan@chromium.org
    
    Review URL: https://codereview.chromium.org/264773004
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21177 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/v8globals.h
+++ src/v8globals.h
@@ -413,7 +413,8 @@ enum CpuFeature { SSE4_1 = 32 + 19,  // x86
                   VFP32DREGS = 6,  // ARM
                   NEON = 7,    // ARM
                   SAHF = 0,    // x86
-                  FPU = 1};    // MIPS
+                  FPU = 1,     // MIPS
+                  ALWAYS_ALIGN_CSP = 1 };    // ARM64
 
 
 // Used to specify if a macro instruction must perform a smi check on tagged
