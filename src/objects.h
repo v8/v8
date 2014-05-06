ab96529a4a7936a3dd51c88de99221f24f0842b0
wingo@igalia.com <wingo@igalia.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-05-06 14:57:52 +0000

    Prevent liveedit on or under generators with open activations
    R=yangguo@chromium.org
    LOG=N
    TEST=mjsunit/harmony/generators-debug-liveedit.js
    BUG=
    
    Review URL: https://codereview.chromium.org/266983004
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21174 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects.h
+++ src/objects.h
@@ -7460,6 +7460,8 @@ class JSGeneratorObject: public JSObject {
   // cannot be resumed.
   inline int continuation();
   inline void set_continuation(int continuation);
+  inline bool is_closed();
+  inline bool is_executing();
   inline bool is_suspended();
 
   // [operand_stack]: Saved operand stack.
