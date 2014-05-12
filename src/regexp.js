362b203e7186ad2645d7d45e6758a99a02237c23
mstarzinger@chromium.org <mstarzinger@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-05-12 09:18:45 +0000

    Partially revert r21254 due to WebKit test failure.
    TBR=danno@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/280523002
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21257 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/regexp.js
+++ src/regexp.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-"use strict";
-
 // This file relies on the fact that the following declaration has been made
 // in runtime.js:
 // var $Object = global.Object;
