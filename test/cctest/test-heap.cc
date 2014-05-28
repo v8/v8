5599b3a7431a7ef82484a11e39370fd5cc8d08da
yangguo@chromium.org <yangguo@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-05-28 06:42:01 +0000

    Fix path tracer test.
    Path tracer is not compatible with --enable-slow-assert.
    
    R=mstarzinger@chromium.org
    BUG=v8:3350
    LOG=N
    
    Review URL: https://codereview.chromium.org/297193005
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21542 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- test/cctest/test-heap.cc
+++ test/cctest/test-heap.cc
@@ -4266,6 +4266,8 @@ TEST(ArrayShiftSweeping) {
 
 #ifdef DEBUG
 TEST(PathTracer) {
+  // Type cast checks fail because the path tracer abuses the map for marking.
+  if (i::FLAG_enable_slow_asserts) return;
   CcTest::InitializeVM();
   v8::HandleScope scope(CcTest::isolate());
 
