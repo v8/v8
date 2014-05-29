232ddd97562f4a45404c967dbe8fc123de4b31f4
weiliang.lin@intel.com <weiliang.lin@intel.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-05-29 13:11:55 +0000

    X87: Refactor HType to get rid of various hacks.
    port r21578 (dcf13aa)
    
    Original commit message:
    - Move HType to it's own file.
    - Add HType::HeapObject and some other useful types.
    - Get rid of the broken and useless HType::NonPrimitive.
    - Introduce HType::FromType() to convert from HeapType to HType.
    - Also add unit tests for HType.
    - Fix types in Crankshaft.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/307903002
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21579 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x87/lithium-x87.cc
+++ src/x87/lithium-x87.cc
@@ -1958,7 +1958,9 @@ LInstruction* LChunkBuilder::DoChange(HChange* instr) {
 LInstruction* LChunkBuilder::DoCheckHeapObject(HCheckHeapObject* instr) {
   LOperand* value = UseAtStart(instr->value());
   LInstruction* result = new(zone()) LCheckNonSmi(value);
-  if (!instr->value()->IsHeapObject()) result = AssignEnvironment(result);
+  if (!instr->value()->type().IsHeapObject()) {
+    result = AssignEnvironment(result);
+  }
   return result;
 }
 
