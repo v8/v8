293428ee2361e1568663a907412c008f261e55fc
plind44@gmail.com <plind44@gmail.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-06-05 16:47:02 +0000

    MIPS: Preliminary support for block contexts in hydrogen.
    Port r21684 (ea35030)
    
    Original commit message:
    Patch from Steven Keuchel <keuchel@chromium.org>
    
    BUG=v8:2198
    LOG=N
    TEST=mjsunit/harmony/block-let-crankshaft.js
    R=plind44@gmail.com
    
    Review URL: https://codereview.chromium.org/313183007
    
    Patch from Balazs Kilvady <kilvadyb@homejinni.com>.
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21701 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/lithium-codegen-mips.cc
+++ src/mips/lithium-codegen-mips.cc
@@ -5874,6 +5874,21 @@ void LCodeGen::DoLoadFieldByIndex(LLoadFieldByIndex* instr) {
 }
 
 
+void LCodeGen::DoStoreFrameContext(LStoreFrameContext* instr) {
+  Register context = ToRegister(instr->context());
+  __ sw(context, MemOperand(fp, StandardFrameConstants::kContextOffset));
+}
+
+
+void LCodeGen::DoAllocateBlockContext(LAllocateBlockContext* instr) {
+  Handle<ScopeInfo> scope_info = instr->scope_info();
+  __ li(at, scope_info);
+  __ Push(at, ToRegister(instr->function()));
+  CallRuntime(Runtime::kHiddenPushBlockContext, 2, instr);
+  RecordSafepoint(Safepoint::kNoLazyDeopt);
+}
+
+
 #undef __
 
 } }  // namespace v8::internal
