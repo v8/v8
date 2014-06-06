v8/v8

commit 5c3c644cd3ae32d54b6766b35348dc55eb390b9c

Make plot shell script nicer.
R=svenpanne@chromium.org

Review URL: https://codereview.chromium.org/284283005

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21338 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

tools/plot-timer-events
@@ -10,29 +10,43 @@ do
 done
 
 tools_path=`cd $(dirname "$0");pwd`
-if [ ! "$D8_PATH" ]; then
+if test ! "$D8_PATH"; then
   d8_public=`which d8`
-  if [ -x "$d8_public" ]; then D8_PATH=$(dirname "$d8_public"); fi
+  if test -x "$d8_public"; then D8_PATH=$(dirname "$d8_public"); fi
 fi
-[ -n "$D8_PATH" ] || D8_PATH=$tools_path/..
+
+if test -n "$D8_PATH"; then
+  D8_PATH=$tools_path/..
+fi
+
 d8_exec=$D8_PATH/d8
 
-if [ ! -x "$d8_exec" ]; then
+if test ! -x "$d8_exec"; then
   D8_PATH=`pwd`/out/native
   d8_exec=$D8_PATH/d8
 fi
 
-if [ ! -x "$d8_exec" ]; then
+if test ! -x "$d8_exec"; then
   d8_exec=`grep -m 1 -o '".*/d8"' $log_file | sed 's/"//g'`
 fi
 
-if [ ! -x "$d8_exec" ]; then
+if test ! -x "$d8_exec"; then
   echo "d8 shell not found in $D8_PATH"
   echo "To build, execute 'make native' from the V8 directory"
   exit 1
 fi
 
-if [[ "$@" != *--distortion* ]]; then
+
+contains=0;
+for arg in "$@"; do
+  `echo "$arg" | grep -q "^--distortion"`
+  if test $? -eq 0; then
+    contains=1
+    break
+  fi
+done
+
+if test "$contains" -eq 0; then
   # Try to find out how much the instrumentation overhead is.
   calibration_log=calibration.log
   calibration_script="for (var i = 0; i < 1000000; i++) print();"
@@ -70,7 +84,7 @@ cat $log_file |
     -- $@ $options 2>/dev/null > timer-events.plot
 
 success=$?
-if [[ $success != 0 ]] ; then
+if test $success -ne 0; then
     cat timer-events.plot
 else
     cat timer-events.plot | gnuplot > timer-events.png

commit 47e96513afd278f46d97e41e9dd031d0cd7bb665

Fix bug in tools/plot-timer-events
Bug was introduced in r21338.

R=jkummerow@chromium.org

Review URL: https://codereview.chromium.org/314143004

git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@21707 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

src/bootstrapper.cc
@@ -1071,7 +1071,7 @@ void Genesis::InitializeGlobal(Handle<GlobalObject> inner_global,
     native_context()->set_json_object(*json_object);
   }
 
-  { // -- A r r a y B u f f e r
+  {  // -- A r r a y B u f f e r
     Handle<JSFunction> array_buffer_fun =
         InstallFunction(
             global, "ArrayBuffer", JS_ARRAY_BUFFER_TYPE,
@@ -1081,7 +1081,7 @@ void Genesis::InitializeGlobal(Handle<GlobalObject> inner_global,
     native_context()->set_array_buffer_fun(*array_buffer_fun);
   }
 
-  { // -- T y p e d A r r a y s
+  {  // -- T y p e d A r r a y s
 #define INSTALL_TYPED_ARRAY(Type, type, TYPE, ctype, size)                    \
     {                                                                         \
       Handle<JSFunction> fun;                                                 \

tools/plot-timer-events
@@ -15,7 +15,7 @@ if test ! "$D8_PATH"; then
   if test -x "$d8_public"; then D8_PATH=$(dirname "$d8_public"); fi
 fi
 
-if test -n "$D8_PATH"; then
+if test ! -n "$D8_PATH"; then
   D8_PATH=$tools_path/..
 fi
 

tools/profviz/composer.js
@@ -502,7 +502,8 @@ function PlotScriptComposer(kResX, kResY, error_output) {
     execution_pauses.sort(
         function(a, b) { return b.duration() - a.duration(); });
 
-    var max_pause_time = execution_pauses[0].duration();
+    var max_pause_time = execution_pauses.length > 0
+        ? execution_pauses[0].duration() : 0;
     padding = kPauseLabelPadding * (range_end - range_start) / kResX;
     var y_scale = kY1Offset / max_pause_time / 2;
     for (var i = 0; i < execution_pauses.length && i < kNumPauseLabels; i++) {