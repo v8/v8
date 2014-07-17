6f23f4fe3b05c43aa16870b26d754ac911930ab1
balazs.kilvady@imgtec.com <balazs.kilvady@imgtec.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-07-17 18:24:59 +0000

    MIPS: StubCallInterfaceDescriptor and CallInterfaceDescriptor are unified under a base class InterfaceDescriptor.
    Port r22448 (a9e0b0e)
    
    Original commit message:
    Handling of the context register had to be massaged to effect the unification. This will make it easier to call hydrogen code stubs directly from crankshaft.
    
    BUG=
    R=mvstanton@chromium.org, paul.lind@imgtec.com
    
    Review URL: https://codereview.chromium.org/400743002
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@22456 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/lithium-mips.cc
+++ src/mips/lithium-mips.cc
@@ -1086,7 +1086,7 @@ LInstruction* LChunkBuilder::DoCallJSFunction(
 
 LInstruction* LChunkBuilder::DoCallWithDescriptor(
     HCallWithDescriptor* instr) {
-  const CallInterfaceDescriptor* descriptor = instr->descriptor();
+  const InterfaceDescriptor* descriptor = instr->descriptor();
 
   LOperand* target = UseRegisterOrConstantAtStart(instr->target());
   ZoneList<LOperand*> ops(instr->OperandCount(), zone());
@@ -2357,7 +2357,7 @@ LInstruction* LChunkBuilder::DoParameter(HParameter* instr) {
     CodeStubInterfaceDescriptor* descriptor =
         info()->code_stub()->GetInterfaceDescriptor();
     int index = static_cast<int>(instr->index());
-    Register reg = descriptor->GetParameterRegister(index);
+    Register reg = descriptor->GetEnvironmentParameterRegister(index);
     return DefineFixed(result, reg);
   }
 }
