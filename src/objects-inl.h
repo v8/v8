cc532e449b3af97bacbb357d2a66f055c33ad44c
rmcilroy@chromium.org <rmcilroy@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-07-28 10:04:36 +0000

    Tests that the GC doesn't mistake non-pointer constant pool entries as pointers.
    Adds a test for ConstantPoolArray to ensure that the GC
    doesn't mistake non-pointer entries as pointers and try
    to modify them during scavenge operations.
    
    Also adds asserts to ConstantPoolArray::set(int, *Object) to
    ensure we don't add new-space pointers in constant pool
    array.
    
    R=hpayer@chromium.org
    
    Committed: https://code.google.com/p/v8/source/detail?r=22608
    
    Review URL: https://codereview.chromium.org/396803009
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@22634 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects-inl.h
+++ src/objects-inl.h
@@ -2540,6 +2540,7 @@ void ConstantPoolArray::set(int index, Address value) {
 
 void ConstantPoolArray::set(int index, Object* value) {
   ASSERT(map() == GetHeap()->constant_pool_array_map());
+  ASSERT(!GetHeap()->InNewSpace(value));
   ASSERT(get_type(index) == HEAP_PTR);
   WRITE_FIELD(this, OffsetOfElementAt(index), value);
   WRITE_BARRIER(GetHeap(), this, OffsetOfElementAt(index), value);
@@ -2584,6 +2585,7 @@ void ConstantPoolArray::set_at_offset(int offset, Address value) {
 
 void ConstantPoolArray::set_at_offset(int offset, Object* value) {
   ASSERT(map() == GetHeap()->constant_pool_array_map());
+  ASSERT(!GetHeap()->InNewSpace(value));
   ASSERT(offset_is_type(offset, HEAP_PTR));
   WRITE_FIELD(this, offset, value);
   WRITE_BARRIER(GetHeap(), this, offset, value);
