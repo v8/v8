5372a2a769a31fb3d29d43301bace6f6a7eebf78
weiliang.lin@intel.com <weiliang.lin@intel.com@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-07-30 09:04:42 +0000

    X87: Clean up name distinction between Keyed ICs and Element Handlers
    port r22679.
    
    original commit message:
    
      Clean up name distinction between Keyed ICs and Element Handlers
    
    BUG=
    R=weiliang.lin@intel.com
    
    Review URL: https://codereview.chromium.org/426083006
    
    Patch from Chunyang Dai <chunyang.dai@intel.com>.
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@22696 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x87/macro-assembler-x87.cc
+++ src/x87/macro-assembler-x87.cc
@@ -1173,7 +1173,7 @@ void MacroAssembler::CheckAccessGlobalProxy(Register holder_reg,
 
 
 // Compute the hash code from the untagged key.  This must be kept in sync with
-// ComputeIntegerHash in utils.h and KeyedLoadGenericElementStub in
+// ComputeIntegerHash in utils.h and KeyedLoadGenericStub in
 // code-stub-hydrogen.cc
 //
 // Note: r0 will contain hash code
--- src/x87/stub-cache-x87.cc
+++ src/x87/stub-cache-x87.cc
@@ -1122,8 +1122,8 @@ Handle<Code> NamedStoreHandlerCompiler::CompileStoreInterceptor(
   __ push(scratch1());  // restore return address
 
   // Do tail-call to the runtime system.
-  ExternalReference store_ic_property =
-      ExternalReference(IC_Utility(IC::kStoreInterceptorProperty), isolate());
+  ExternalReference store_ic_property = ExternalReference(
+      IC_Utility(IC::kStorePropertyWithInterceptor), isolate());
   __ TailCallExternalReference(store_ic_property, 3, 1);
 
   // Return the generated code.
@@ -1145,7 +1145,7 @@ void NamedStoreHandlerCompiler::GenerateStoreArrayLength() {
 }
 
 
-Handle<Code> PropertyICCompiler::CompileIndexedStorePolymorphic(
+Handle<Code> PropertyICCompiler::CompileKeyedStorePolymorphic(
     MapHandleList* receiver_maps, CodeHandleList* handler_stubs,
     MapHandleList* transitioned_maps) {
   Label miss;
@@ -1343,7 +1343,7 @@ Handle<Code> PropertyICCompiler::CompilePolymorphic(TypeHandleList* types,
 #define __ ACCESS_MASM(masm)
 
 
-void IndexedHandlerCompiler::GenerateLoadDictionaryElement(
+void ElementHandlerCompiler::GenerateLoadDictionaryElement(
     MacroAssembler* masm) {
   // ----------- S t a t e -------------
   //  -- ecx    : key
