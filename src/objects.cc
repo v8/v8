356ac42defb183df3766867b83175754c5878437
verwaest@chromium.org <verwaest@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-08-05 09:24:27 +0000

    Reland "Also mark as prototype when passing in while creating a function." Skip gc-stress for test relying on stable feedback (and hence stable gc timing).
    BUG=
    R=hpayer@chromium.org
    
    Review URL: https://codereview.chromium.org/441873005
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@22852 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/objects.cc
+++ src/objects.cc
@@ -7288,6 +7288,7 @@ Handle<Map> Map::CopyForObserved(Handle<Map> map) {
 
 
 Handle<Map> Map::CopyAsPrototypeMap(Handle<Map> map) {
+  if (map->is_prototype_map()) return map;
   Handle<Map> result = Copy(map);
   result->mark_prototype_map();
   return result;
