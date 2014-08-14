964ec21e5350f8ed9e2f69e1fb78a166f1ee2d6b
bmeurer@chromium.org <bmeurer@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-08-14 09:24:40 +0000

    Fix MSVC build.
    And while we are at it, use the stupid MSVC compiler intrinsics.
    
    TEST=base-unittests/BitTest
    TBR=jarin@chromium.org
    
    Review URL: https://codereview.chromium.org/473773003
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@23123 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/base/bits.h
+++ src/base/bits.h
@@ -6,10 +6,29 @@
 #define V8_BASE_BITS_H_
 
 #include "include/v8stdint.h"
+#if V8_OS_WIN32
+#include "src/base/win32-headers.h"
+#endif
 
 namespace v8 {
 namespace base {
 namespace bits {
+
+#if V8_CC_MSVC
+
+#pragma intrinsic(_rotr)
+#pragma intrinsic(_rotr64)
+
+inline uint32_t RotateRight32(uint32_t value, uint32_t shift) {
+  return _rotr(value, shift);
+}
+
+
+inline uint64_t RotateRight64(uint64_t value, uint32_t shift) {
+  return _rotr64(value, shift);
+}
+
+#else  // V8_CC_MSVC
 
 inline uint32_t RotateRight32(uint32_t value, uint32_t shift) {
   if (shift == 0) return value;
@@ -17,10 +36,12 @@ inline uint32_t RotateRight32(uint32_t value, uint32_t shift) {
 }
 
 
-inline uint64_t RotateRight64(uint64_t value, uint64_t shift) {
+inline uint64_t RotateRight64(uint64_t value, uint32_t shift) {
   if (shift == 0) return value;
   return (value >> shift) | (value << (64 - shift));
 }
+
+#endif  // V8_CC_MSVC
 
 }  // namespace bits
 }  // namespace base
