bf56be001e73659628bf9a3bd6875958114fdbb2
bmeurer@chromium.org <bmeurer@chromium.org@ce2b1a6d-e550-0410-aec6-3dcde31c8c00>
2014-08-14 09:31:52 +0000

    Really fix windows now.
    TBR=jarin@chromium.org
    
    Review URL: https://codereview.chromium.org/472783002
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@23124 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/base/bits.h
+++ src/base/bits.h
@@ -14,34 +14,16 @@ namespace v8 {
 namespace base {
 namespace bits {
 
-#if V8_CC_MSVC
-
-#pragma intrinsic(_rotr)
-#pragma intrinsic(_rotr64)
-
-inline uint32_t RotateRight32(uint32_t value, uint32_t shift) {
-  return _rotr(value, shift);
-}
-
-
-inline uint64_t RotateRight64(uint64_t value, uint32_t shift) {
-  return _rotr64(value, shift);
-}
-
-#else  // V8_CC_MSVC
-
 inline uint32_t RotateRight32(uint32_t value, uint32_t shift) {
   if (shift == 0) return value;
   return (value >> shift) | (value << (32 - shift));
 }
 
 
-inline uint64_t RotateRight64(uint64_t value, uint32_t shift) {
+inline uint64_t RotateRight64(uint64_t value, uint64_t shift) {
   if (shift == 0) return value;
   return (value >> shift) | (value << (64 - shift));
 }
-
-#endif  // V8_CC_MSVC
 
 }  // namespace bits
 }  // namespace base
