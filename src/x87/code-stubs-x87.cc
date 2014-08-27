18235ae336f17b238b983c2f8d9ccf63a191b9cf
weiliang.lin@intel.com <weiliang.lin@intel.com>
2014-08-27 08:15:14 +0000

    X87: Minor-key-ify new LoadICTrampolineStub.
    port r23409.
    
    original commit message:
      Minor-key-ify new LoadICTrampolineStub.
    
    BUG=
    R=weiliang.lin@intel.com
    
    Review URL: https://codereview.chromium.org/513613003
    
    Patch from Chunyang Dai <chunyang.dai@intel.com>.
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@23434 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x87/code-stubs-x87.cc
+++ src/x87/code-stubs-x87.cc
@@ -4142,7 +4142,7 @@ void StubFailureTrampolineStub::Generate(MacroAssembler* masm) {
 
 void LoadICTrampolineStub::Generate(MacroAssembler* masm) {
   EmitLoadTypeFeedbackVector(masm, FullVectorLoadConvention::VectorRegister());
-  VectorLoadStub stub(isolate(), state_);
+  VectorLoadStub stub(isolate(), state());
   __ jmp(stub.GetCode(), RelocInfo::CODE_TARGET);
 }
 
