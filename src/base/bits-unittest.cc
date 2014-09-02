7e4de51089f1d68e2cdfadec46c54a3195872dc1
jarin@chromium.org <jarin@chromium.org>
2014-09-02 15:22:36 +0000

    Fix NaCl build.
    R=mstarzinger@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/527273003
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@23625 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/base/bits-unittest.cc
+++ src/base/bits-unittest.cc
@@ -84,12 +84,10 @@ TEST(Bits, RoundUpToPowerOfTwo32) {
 }
 
 
-#if 0
-// TODO(bmeurer): Fix this on NaCL builds.
 TEST(BitsDeathTest, DISABLE_IN_RELEASE(RoundUpToPowerOfTwo32)) {
-  ASSERT_DEATH({ RoundUpToPowerOfTwo32(0x80000001u); }, "0x80000000");
+  ASSERT_DEATH_IF_SUPPORTED({ RoundUpToPowerOfTwo32(0x80000001u); },
+                            "0x80000000");
 }
-#endif
 
 
 TEST(Bits, RoundDownToPowerOfTwo32) {
