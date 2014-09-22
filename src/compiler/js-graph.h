f5dcdee1150ed170d796017ac6e8ef965bf78fad
bmeurer@chromium.org <bmeurer@chromium.org>
2014-09-22 07:17:13 +0000

    [turbofan] Bounds check when lowering JSStoreProperty.
    R=titzer@chromium.org
    
    Review URL: https://codereview.chromium.org/559653005
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@24105 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/compiler/js-graph.h
+++ src/compiler/js-graph.h
@@ -65,6 +65,9 @@ class JSGraph : public ZoneObject {
 
   // Creates a Int32Constant node, usually canonicalized.
   Node* Int32Constant(int32_t value);
+  Node* Uint32Constant(uint32_t value) {
+    return Int32Constant(bit_cast<int32_t>(value));
+  }
 
   // Creates a Float64Constant node, usually canonicalized.
   Node* Float64Constant(double value);
@@ -109,6 +112,7 @@ class JSGraph : public ZoneObject {
 
   Factory* factory() { return isolate()->factory(); }
 };
+
 }  // namespace compiler
 }  // namespace internal
 }  // namespace v8
