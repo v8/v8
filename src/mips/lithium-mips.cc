7301ee52f78f905c5de15ac277b6206098331dec
balazs.kilvady@imgtec.com <balazs.kilvady@imgtec.com>
2014-09-24 10:17:24 +0000

    MIPS: Refactor bailout reasons and disable optimization in more cases.
    Port r24161 (008f335)
    
    BUG=
    R=dusan.milosavljevic@imgtec.com
    
    Review URL: https://codereview.chromium.org/593313004
    
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@24175 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/mips/lithium-mips.cc
+++ src/mips/lithium-mips.cc
@@ -430,12 +430,6 @@ LPlatformChunk* LChunkBuilder::Build() {
 }
 
 
-void LChunkBuilder::Abort(BailoutReason reason) {
-  info()->set_bailout_reason(reason);
-  status_ = ABORTED;
-}
-
-
 LUnallocated* LChunkBuilder::ToUnallocated(Register reg) {
   return new(zone()) LUnallocated(LUnallocated::FIXED_REGISTER,
                                   Register::ToAllocationIndex(reg));
@@ -2425,7 +2419,7 @@ LInstruction* LChunkBuilder::DoUnknownOSRValue(HUnknownOSRValue* instr) {
   } else {
     spill_index = env_index - instr->environment()->first_local_index();
     if (spill_index > LUnallocated::kMaxFixedSlotIndex) {
-      Abort(kTooManySpillSlotsNeededForOSR);
+      Retry(kTooManySpillSlotsNeededForOSR);
       spill_index = 0;
     }
   }
