73ef8581ff3bec650508d6bac94de1c084ca30f4
weiliang.lin@intel.com <weiliang.lin@intel.com>
2014-10-29 07:19:42 +0000

    X87: Use shared function info for eval cache key.
    port r24927.
    
    original commit message:
    
     Use shared function info for eval cache key.
    
    BUG=
    R=weiliang.lin@intel.com
    
    Review URL: https://codereview.chromium.org/689543002
    
    Patch from Chunyang Dai <chunyang.dai@intel.com>.
    
    Cr-Commit-Position: refs/heads/master@{#24961}
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@24961 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x87/full-codegen-x87.cc
+++ src/x87/full-codegen-x87.cc
@@ -2844,6 +2844,8 @@ void FullCodeGenerator::EmitResolvePossiblyDirectEval(int arg_count) {
     __ push(Immediate(isolate()->factory()->undefined_value()));
   }
 
+  // Push the enclosing function.
+  __ push(Operand(ebp, JavaScriptFrameConstants::kFunctionOffset));
   // Push the receiver of the enclosing function.
   __ push(Operand(ebp, (2 + info_->scope()->num_parameters()) * kPointerSize));
   // Push the language mode.
@@ -2853,7 +2855,7 @@ void FullCodeGenerator::EmitResolvePossiblyDirectEval(int arg_count) {
   __ push(Immediate(Smi::FromInt(scope()->start_position())));
 
   // Do the runtime call.
-  __ CallRuntime(Runtime::kResolvePossiblyDirectEval, 5);
+  __ CallRuntime(Runtime::kResolvePossiblyDirectEval, 6);
 }
 
 
