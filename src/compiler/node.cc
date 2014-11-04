cfb6e2a7b58480aa60daba607967fc201b3a3123
bmeurer@chromium.org <bmeurer@chromium.org>
2014-11-04 07:35:59 +0000

    [turbofan] Also print the non-value inputs in schedule.
    R=svenpanne@chromium.org
    
    Review URL: https://codereview.chromium.org/702643002
    
    Cr-Commit-Position: refs/heads/master@{#25092}
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@25092 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/compiler/node.cc
+++ src/compiler/node.cc
@@ -44,9 +44,9 @@ Node* Node::FindProjection(size_t projection_index) {
 
 std::ostream& operator<<(std::ostream& os, const Node& n) {
   os << n.id() << ": " << *n.op();
-  if (n.op()->InputCount() > 0) {
+  if (n.InputCount() > 0) {
     os << "(";
-    for (int i = 0; i < n.op()->InputCount(); ++i) {
+    for (int i = 0; i < n.InputCount(); ++i) {
       if (i != 0) os << ", ";
       os << n.InputAt(i)->id();
     }
