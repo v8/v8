79390123903f4a8b6e973f2d4e3743948b97aeb8
weiliang.lin@intel.com <weiliang.lin@intel.com>
2014-11-05 10:01:22 +0000

    Version 3.29.88.16 (merged r25125)
    x87: Set the FPU precision control to double-precision.
    
    R=jkummerow@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/700943004
    
    Cr-Commit-Position: refs/branch-heads/3.29@{#25137}
    git-svn-id: https://v8.googlecode.com/svn/branches/3.29@25137 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/x87/macro-assembler-x87.h
+++ src/x87/macro-assembler-x87.h
@@ -425,6 +425,7 @@ class MacroAssembler: public Assembler {
   void FXamSign();
   void X87CheckIA();
   void X87SetRC(int rc);
+  void X87SetFPUCW(int cw);
 
   void ClampUint8(Register reg);
   void ClampTOSToUint8(Register result_reg);
