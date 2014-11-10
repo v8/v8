30e6dce6861d38941d6cde162f4ba39e8fa47c30
machenbach@chromium.org <machenbach@chromium.org>
2014-11-10 10:40:59 +0000

    Upgrade version and change log after branch.
    R=jkummerow@chromium.org
    TBR=jkummerow@chromium.org
    
    Review URL: https://codereview.chromium.org/681113011
    
    Cr-Commit-Position: refs/heads/master@{#25237}
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@25237 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- src/version.cc
+++ src/version.cc
@@ -10,7 +10,7 @@
 // NOTE these macros are used by some of the tool scripts and the build
 // system so their names cannot be changed without changing the scripts.
 #define MAJOR_VERSION     3
-#define MINOR_VERSION     30
+#define MINOR_VERSION     31
 #define BUILD_NUMBER      0
 #define PATCH_LEVEL       0
 // Use 1 for candidates and 0 otherwise.
