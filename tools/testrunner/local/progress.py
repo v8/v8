0df953c9e12c1e3b0e37f2d4ef1ef8c319e095cb
machenbach@chromium.org <machenbach@chromium.org>
2014-11-11 06:06:20 +0000

    Fix test driver json output.
    TBR=tandrii@chromium.org
    
    Review URL: https://codereview.chromium.org/716713002
    
    Cr-Commit-Position: refs/heads/master@{#25254}
    git-svn-id: https://v8.googlecode.com/svn/branches/bleeding_edge@25254 ce2b1a6d-e550-0410-aec6-3dcde31c8c00

--- tools/testrunner/local/progress.py
+++ tools/testrunner/local/progress.py
@@ -333,7 +333,7 @@ class JsonTestProgressIndicator(ProgressIndicator):
       "stderr": test.output.stderr,
       "exit_code": test.output.exit_code,
       "result": test.suite.GetOutcome(test),
-      "expected": list(test.outcomes) or ["PASS"],
+      "expected": list(test.outcomes or ["PASS"]),
     })
 
 
