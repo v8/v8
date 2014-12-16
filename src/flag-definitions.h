0349f7b71fe2d64ac1b55989cdde322da7b5ef67
dcarney <dcarney@chromium.org>
2014-12-16 05:09:15 -0800

    [turbofan] enable stack slot reuse
    BUG=
    
    Review URL: https://codereview.chromium.org/793683002
    
    Cr-Commit-Position: refs/heads/master@{#25841}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -402,7 +402,7 @@ DEFINE_IMPLICATION(turbo_inlining_intrinsics, turbo_inlining)
 DEFINE_IMPLICATION(turbo_inlining, turbo_types)
 DEFINE_BOOL(turbo_profiling, false, "enable profiling in TurboFan")
 // TODO(dcarney): this is just for experimentation, remove when default.
-DEFINE_BOOL(turbo_reuse_spill_slots, false, "reuse spill slots in TurboFan")
+DEFINE_BOOL(turbo_reuse_spill_slots, true, "reuse spill slots in TurboFan")
 // TODO(dcarney): this is just for experimentation, remove when default.
 DEFINE_BOOL(turbo_delay_ssa_decon, false,
             "delay ssa deconstruction in TurboFan register allocator")
