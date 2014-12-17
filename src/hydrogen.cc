cc568d1b7ae7ff9f870f21aaad6698e2cd6cd9d2
arv <arv@chromium.org>
2014-12-17 10:38:38 -0800

    ES6 computed property names
    This adds support for computed property names, under the flag
    --harmony-computed-property-names, for both object literals and
    classes.
    
    This is a revert of the revert, a76419f0f497db156eb1010e9b4be398951280cb.
    
    This changes to do an early bailout in
    HOptimizedGraphBuilder::VisitObjectLiteral instead of doing that in the later
    loop.
    
    BUG=v8:3754
    LOG=Y
    TBR=dslomov@chromium.org
    
    Review URL: https://codereview.chromium.org/792233008
    
    Cr-Commit-Position: refs/heads/master@{#25868}

--- src/hydrogen.cc
+++ src/hydrogen.cc
@@ -5552,6 +5552,13 @@ void HOptimizedGraphBuilder::VisitObjectLiteral(ObjectLiteral* expr) {
   DCHECK(!HasStackOverflow());
   DCHECK(current_block() != NULL);
   DCHECK(current_block()->HasPredecessor());
+
+  for (int i = 0; i < expr->properties()->length(); i++) {
+    if (expr->properties()->at(i)->is_computed_name()) {
+      return Bailout(kComputedPropertyName);
+    }
+  }
+
   expr->BuildConstantProperties(isolate());
   Handle<JSFunction> closure = function_state()->compilation_info()->closure();
   HInstruction* literal;
@@ -5607,9 +5614,10 @@ void HOptimizedGraphBuilder::VisitObjectLiteral(ObjectLiteral* expr) {
 
   for (int i = 0; i < expr->properties()->length(); i++) {
     ObjectLiteral::Property* property = expr->properties()->at(i);
+    DCHECK(!property->is_computed_name());
     if (property->IsCompileTimeValue()) continue;
 
-    Literal* key = property->key();
+    Literal* key = property->key()->AsLiteral();
     Expression* value = property->value();
 
     switch (property->kind()) {
@@ -5635,7 +5643,7 @@ void HOptimizedGraphBuilder::VisitObjectLiteral(ObjectLiteral* expr) {
             }
 
             Handle<Map> map = property->GetReceiverType();
-            Handle<String> name = property->key()->AsPropertyName();
+            Handle<String> name = key->AsPropertyName();
             HInstruction* store;
             if (map.is_null()) {
               // If we don't know the monomorphic type, do a generic store.
