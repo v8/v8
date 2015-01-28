b957128b0156bcc42d8c98486cd978307043e087
balazs.kilvady <balazs.kilvady@imgtec.com>
2015-01-28 09:17:31 -0800

    MIPS: Continue learning for calls in crankshaft.
    Port 7d363783e14433032a4e8dd426fe0e27d1637fe5
    
    Original commit message:
    The type feedback vector makes this easy to do.
    This is a re-land of https://codereview.chromium.org/868453005/
    with a fix for the DCHECK failure.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/877253003
    
    Cr-Commit-Position: refs/heads/master@{#26311}

--- src/mips/lithium-mips.cc
+++ src/mips/lithium-mips.cc
@@ -262,6 +262,20 @@ void LInnerAllocatedObject::PrintDataTo(StringStream* stream) {
 }
 
 
+void LCallFunction::PrintDataTo(StringStream* stream) {
+  context()->PrintTo(stream);
+  stream->Add(" ");
+  function()->PrintTo(stream);
+  if (hydrogen()->HasVectorAndSlot()) {
+    stream->Add(" (type-feedback-vector ");
+    temp_vector()->PrintTo(stream);
+    stream->Add(" ");
+    temp_slot()->PrintTo(stream);
+    stream->Add(")");
+  }
+}
+
+
 void LCallJSFunction::PrintDataTo(StringStream* stream) {
   stream->Add("= ");
   function()->PrintTo(stream);
@@ -1251,7 +1265,15 @@ LInstruction* LChunkBuilder::DoCallNewArray(HCallNewArray* instr) {
 LInstruction* LChunkBuilder::DoCallFunction(HCallFunction* instr) {
   LOperand* context = UseFixed(instr->context(), cp);
   LOperand* function = UseFixed(instr->function(), a1);
-  LCallFunction* call = new(zone()) LCallFunction(context, function);
+  LOperand* slot = NULL;
+  LOperand* vector = NULL;
+  if (instr->HasVectorAndSlot()) {
+    slot = FixedTemp(a3);
+    vector = FixedTemp(a2);
+  }
+
+  LCallFunction* call =
+      new (zone()) LCallFunction(context, function, slot, vector);
   return MarkAsCall(DefineFixed(call, v0), instr);
 }
 
