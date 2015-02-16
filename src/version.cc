f79cd1a5451a14933832a0bb304e17ab1921e1ca
Michael Achenbach <machenbach@chromium.org>
2015-02-16 13:11:28 +0100

    Version 4.2.71
    Implement ES6 rest parameters (issue 2159).
    
    Performance and stability improvements on all platforms.
    
    Cr-Commit-Position: refs/heads/4.2.71@{#1}
    Cr-Branched-From: e66d39a6be459381e68d07d013170d45d57a706c-refs/heads/master@{#26653}

--- src/version.cc
+++ src/version.cc
@@ -11,11 +11,11 @@
 // system so their names cannot be changed without changing the scripts.
 #define MAJOR_VERSION     4
 #define MINOR_VERSION     2
-#define BUILD_NUMBER      0
+#define BUILD_NUMBER      71
 #define PATCH_LEVEL       0
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
-#define IS_CANDIDATE_VERSION 1
+#define IS_CANDIDATE_VERSION 0
 
 // Define SONAME to have the build system put a specific SONAME into the
 // shared library instead the generic SONAME generated from the V8 version
