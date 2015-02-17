24026a9944266c10b58917e82c974a7d46867507
balazs.kilvady <balazs.kilvady@imgtec.com>
2015-02-17 08:19:32 -0800

    MIPS64: [turbofan] Initial support for Switch.
    Port feb2890711e8072a9550abb89621929816fc9699
    
    Original commit message:
    Adds Switch and Case operators to TurboFan and handles them
    appropriately in instruction selection and code generation.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/932683002
    
    Cr-Commit-Position: refs/heads/master@{#26699}

--- src/compiler/mips64/code-generator-mips64.cc
+++ src/compiler/mips64/code-generator-mips64.cc
@@ -428,6 +428,9 @@ void CodeGenerator::AssembleArchInstruction(Instruction* instr) {
     case kArchJmp:
       AssembleArchJump(i.InputRpo(0));
       break;
+    case kArchSwitch:
+      AssembleArchSwitch(instr);
+      break;
     case kArchNop:
       // don't emit code for nops.
       break;
@@ -916,6 +919,32 @@ void CodeGenerator::AssembleArchJump(BasicBlock::RpoNumber target) {
 }
 
 
+void CodeGenerator::AssembleArchSwitch(Instruction* instr) {
+  MipsOperandConverter i(this, instr);
+  int const kNumLabels = static_cast<int>(instr->InputCount() - 1);
+  v8::internal::Assembler::BlockTrampolinePoolScope block_trampoline_pool(
+      masm());
+  Label here;
+
+  // Ensure that dd-ed labels goes to 8 byte aligned addresses.
+  if ((masm()->pc_offset() & 7) == 0) {
+    __ nop();
+  }
+  __ bal(&here);
+  __ nop();  // Branch delay slot nop.
+  __ bind(&here);
+  __ dsll(at, i.InputRegister(0), 3);
+  __ daddu(at, at, ra);
+  __ ld(at, MemOperand(at, 5 * v8::internal::Assembler::kInstrSize));
+  __ jr(at);
+  __ nop();  // Branch delay slot nop.
+
+  for (int index = 0; index < kNumLabels; ++index) {
+    __ dd(GetLabel(i.InputRpo(index + 1)));
+  }
+}
+
+
 // Assembles boolean materializations after an instruction.
 void CodeGenerator::AssembleArchBoolean(Instruction* instr,
                                         FlagsCondition condition) {
@@ -1280,6 +1309,12 @@ void CodeGenerator::AssembleSwap(InstructionOperand* source,
     // No other combinations are possible.
     UNREACHABLE();
   }
+}
+
+
+void CodeGenerator::AssembleJumpTable(Label** targets, size_t target_count) {
+  // On 64-bit MIPS we emit the jump tables inline.
+  UNREACHABLE();
 }
 
 
