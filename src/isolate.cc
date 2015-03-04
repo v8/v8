6e58be468a5f56a26522af77902c5abc394f57bf
yangguo <yangguo@chromium.org>
2015-03-04 06:27:22 -0800

    Revert of Remove default value for number of threads in Isolate::Init. (patchset #1 id:1 of https://codereview.chromium.org/978773002/)
    Reason for revert:
    build failures.
    
    Original issue's description:
    > Remove default value for number of threads in Isolate::Init.
    >
    > Just fixing a TODO.
    >
    > R=jochen@chromium.org
    >
    > Committed: https://crrev.com/fbcebbc5bdfd8eed82d121faf07b536eb3142fb7
    > Cr-Commit-Position: refs/heads/master@{#26987}
    
    TBR=jochen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/964743003
    
    Cr-Commit-Position: refs/heads/master@{#26988}

--- src/isolate.cc
+++ src/isolate.cc
@@ -2054,6 +2054,15 @@ bool Isolate::Init(Deserializer* des) {
     set_event_logger(Logger::DefaultEventLoggerSentinel);
   }
 
+  // Set default value if not yet set.
+  // TODO(yangguo): move this to ResourceConstraints::ConfigureDefaults
+  // once ResourceConstraints becomes an argument to the Isolate constructor.
+  if (max_available_threads_ < 1) {
+    // Choose the default between 1 and 4.
+    max_available_threads_ =
+        Max(Min(base::SysInfo::NumberOfProcessors(), 4), 1);
+  }
+
   if (FLAG_trace_hydrogen || FLAG_trace_hydrogen_stubs) {
     PrintF("Concurrent recompilation has been disabled for tracing.\n");
   } else if (OptimizingCompilerThread::Enabled(max_available_threads_)) {
