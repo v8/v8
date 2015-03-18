4764b133e38f96fe4efbf7c60eaae86dccc6235a
jochen <jochen@chromium.org>
2015-03-18 03:36:54 -0700

    Turn on overapproximation of the weak closure
    BUG=v8:3862
    R=hpayer@chromium.org
    LOG=y
    
    Review URL: https://codereview.chromium.org/1013133002
    
    Cr-Commit-Position: refs/heads/master@{#27265}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -612,7 +612,7 @@ DEFINE_BOOL(age_code, true,
             "old code (required for code flushing)")
 DEFINE_BOOL(incremental_marking, true, "use incremental marking")
 DEFINE_BOOL(incremental_marking_steps, true, "do incremental marking steps")
-DEFINE_BOOL(overapproximate_weak_closure, false,
+DEFINE_BOOL(overapproximate_weak_closure, true,
             "overapproximate weak closer to reduce atomic pause time")
 DEFINE_INT(min_progress_during_object_groups_marking, 128,
            "keep overapproximating the weak closure as long as we discover at "
