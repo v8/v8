d0e20d8221cec50c5466061f2ddf0fe82944b6e1
rossberg <rossberg@chromium.org>
2015-03-20 04:03:05 -0700

    [harmony] Move some regression tests to the right place
    TBR=dslomov@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1027693003
    
    Cr-Commit-Position: refs/heads/master@{#27339}

--- test/mjsunit/es6/regress/regress-347906.js
+++ test/mjsunit/es6/regress/regress-347906.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --allow-natives-syntax --harmony
+// Flags: --allow-natives-syntax
 
 function foo() {
   return Math.clz32(12.34);
