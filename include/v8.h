bb880058f6499510cff12d98dc7d524d35d769cb
erikcorry <erikcorry@chromium.org>
2015-03-24 09:17:42 -0700

    Track how many pages trigger fallback strategies in GC
    R=hpayer@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1029323003
    
    Cr-Commit-Position: refs/heads/master@{#27421}

--- include/v8.h
+++ include/v8.h
@@ -5073,7 +5073,7 @@ class V8_EXPORT Isolate {
   };
 
   /**
-   * Features reported via the SetUseCounterCallback callback. Do not chang
+   * Features reported via the SetUseCounterCallback callback. Do not change
    * assigned numbers of existing items; add new features to the end of this
    * list.
    */
@@ -5081,6 +5081,9 @@ class V8_EXPORT Isolate {
     kUseAsm = 0,
     kBreakIterator = 1,
     kLegacyConst = 2,
+    kMarkDequeOverflow = 3,
+    kStoreBufferOverflow = 4,
+    kSlotsBufferOverflow = 5,
     kUseCounterFeatureCount  // This enum value must be last.
   };
 
