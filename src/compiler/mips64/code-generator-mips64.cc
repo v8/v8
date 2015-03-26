ab027aeac386406ededc0db7e9f645a7dfd29de1
paul.lind <paul.lind@imgtec.com>
2015-03-26 15:50:54 -0700

    MIPS64: [turbofan] Fix loading of JSFunction from activation in case of adapter frame.
    Use 64-bit load.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1037863003
    
    Cr-Commit-Position: refs/heads/master@{#27489}

--- src/compiler/mips64/code-generator-mips64.cc
+++ src/compiler/mips64/code-generator-mips64.cc
@@ -1051,7 +1051,7 @@ void CodeGenerator::AssemblePrologue() {
     if (FLAG_code_comments) __ RecordComment("-- OSR entrypoint --");
     osr_pc_offset_ = __ pc_offset();
     // TODO(titzer): cannot address target function == local #-1
-    __ lw(a1, MemOperand(fp, JavaScriptFrameConstants::kFunctionOffset));
+    __ ld(a1, MemOperand(fp, JavaScriptFrameConstants::kFunctionOffset));
     DCHECK(stack_slots >= frame()->GetOsrStackSlotCount());
     stack_slots -= frame()->GetOsrStackSlotCount();
   }
