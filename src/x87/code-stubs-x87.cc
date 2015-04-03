d4a4f796afaaee9ed8c04b363bde392a5178be2d
chunyang.dai <chunyang.dai@intel.com>
2015-04-02 20:09:17 -0700

    X87: Generate common StoreFastElementStubs ahead of time
    port 16ee55097a01a229634095226f7a207bea3626de (r27536)
    
    original commit message:
    
      Generate common StoreFastElementStubs ahead of time
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1052413002
    
    Cr-Commit-Position: refs/heads/master@{#27597}

--- src/x87/code-stubs-x87.cc
+++ src/x87/code-stubs-x87.cc
@@ -2130,6 +2130,7 @@ void CodeStub::GenerateStubsAheadOfTime(Isolate* isolate) {
   CreateWeakCellStub::GenerateAheadOfTime(isolate);
   BinaryOpICStub::GenerateAheadOfTime(isolate);
   BinaryOpICWithAllocationSiteStub::GenerateAheadOfTime(isolate);
+  StoreFastElementStub::GenerateAheadOfTime(isolate);
 }
 
 
