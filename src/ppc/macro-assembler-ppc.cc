ae475b54c9d35a61cc4013147a66884d95cea240
michael_dawson <michael_dawson@ca.ibm.com>
2015-04-09 14:45:42 -0700

    PPC: Reland "Merge old data and pointer space."
    Port 59be4ba7f4f70dedcaac60914adaa2885a3da5d0
    
    Original commit message:
    This reverts commit cbfcee557503514bef20abdfc78ceb262c9c4d64.
    
    R=mbrandy@us.ibm.com
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1074953002
    
    Cr-Commit-Position: refs/heads/master@{#27723}

--- src/ppc/macro-assembler-ppc.cc
+++ src/ppc/macro-assembler-ppc.cc
@@ -1368,7 +1368,6 @@ void MacroAssembler::Allocate(int object_size, Register result,
   if ((flags & DOUBLE_ALIGNMENT) != 0) {
     // Align the next allocation. Storing the filler map without checking top is
     // safe in new-space because the limit of the heap is aligned there.
-    DCHECK((flags & PRETENURE_OLD_SPACE) == 0);
 #if V8_TARGET_ARCH_PPC64
     STATIC_ASSERT(kPointerAlignment == kDoubleAlignment);
 #else
@@ -1467,7 +1466,6 @@ void MacroAssembler::Allocate(Register object_size, Register result,
   if ((flags & DOUBLE_ALIGNMENT) != 0) {
     // Align the next allocation. Storing the filler map without checking top is
     // safe in new-space because the limit of the heap is aligned there.
-    DCHECK((flags & PRETENURE_OLD_SPACE) == 0);
 #if V8_TARGET_ARCH_PPC64
     STATIC_ASSERT(kPointerAlignment == kDoubleAlignment);
 #else
