c715098e85b93def76c0b3f54290c811dea04f01
yangguo <yangguo@chromium.org>
2015-04-22 05:11:18 -0700

    Always optimize for adding properties to native objects.
    R=jkummerow@chromium.org
    
    Review URL: https://codereview.chromium.org/1094383004
    
    Cr-Commit-Position: refs/heads/master@{#28003}

--- src/v8natives.js
+++ src/v8natives.js
@@ -17,9 +17,7 @@ var $isFinite = GlobalIsFinite;
 
 // Helper function used to install functions on objects.
 function InstallFunctions(object, attributes, functions) {
-  if (functions.length >= 8) {
-    %OptimizeObjectForAddingMultipleProperties(object, functions.length >> 1);
-  }
+  %OptimizeObjectForAddingMultipleProperties(object, functions.length >> 1);
   for (var i = 0; i < functions.length; i += 2) {
     var key = functions[i];
     var f = functions[i + 1];
@@ -69,9 +67,7 @@ function InstallGetterSetter(object, name, getter, setter) {
 
 // Helper function for installing constant properties on objects.
 function InstallConstants(object, constants) {
-  if (constants.length >= 4) {
-    %OptimizeObjectForAddingMultipleProperties(object, constants.length >> 1);
-  }
+  %OptimizeObjectForAddingMultipleProperties(object, constants.length >> 1);
   var attributes = DONT_ENUM | DONT_DELETE | READ_ONLY;
   for (var i = 0; i < constants.length; i += 2) {
     var name = constants[i];
