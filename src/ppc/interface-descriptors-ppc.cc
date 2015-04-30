2cc2ee0c1b0200e35ca39372433e71cc2d724a19
mbrandy <mbrandy@us.ibm.com>
2015-04-30 08:43:00 -0700

    PPC: Use a stub in crankshaft for grow store arrays.
    Port fb8e6136387339e8089bbf61d4ffee50e4cd957e
    
    Original commit message:
    We were deopting without learning anything.
    
    This is a rebase/reland of https://codereview.chromium.org/368263003
    
    BUG=
    R=mvstanton@chromium.org, dstence@us.ibm.com, michael_dawson@ca.ibm.com
    
    Review URL: https://codereview.chromium.org/1113063002
    
    Cr-Commit-Position: refs/heads/master@{#28177}

--- src/ppc/interface-descriptors-ppc.cc
+++ src/ppc/interface-descriptors-ppc.cc
@@ -54,6 +54,11 @@ const Register MathPowIntegerDescriptor::exponent() {
 }
 
 
+const Register GrowArrayElementsDescriptor::ObjectRegister() { return r3; }
+const Register GrowArrayElementsDescriptor::KeyRegister() { return r6; }
+const Register GrowArrayElementsDescriptor::CapacityRegister() { return r5; }
+
+
 void FastNewClosureDescriptor::Initialize(CallInterfaceDescriptorData* data) {
   Register registers[] = {cp, r5};
   data->Initialize(arraysize(registers), registers, NULL);
