b10fb64b0c320c47d449496c063ed38850e171f9
mbrandy <mbrandy@us.ibm.com>
2015-05-04 08:33:50 -0700

    PPC: Only swap undefined for the global object if necessary in the prologue
    Port fab3508062396c2768b5dcfa0fd7d40eba527d62
    
    R=verwaest@chromium.org, dstence@us.ibm.com, michael_dawson@ca.ibm.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1122773002
    
    Cr-Commit-Position: refs/heads/master@{#28203}

--- src/ppc/full-codegen-ppc.cc
+++ src/ppc/full-codegen-ppc.cc
@@ -123,7 +123,8 @@ void FullCodeGenerator::Generate() {
   // Sloppy mode functions and builtins need to replace the receiver with the
   // global proxy when called as functions (without an explicit receiver
   // object).
-  if (is_sloppy(info->language_mode()) && !info->is_native()) {
+  if (is_sloppy(info->language_mode()) && !info->is_native() &&
+      info->MayUseThis()) {
     Label ok;
     int receiver_offset = info->scope()->num_parameters() * kPointerSize;
     __ LoadP(r5, MemOperand(sp, receiver_offset), r0);
