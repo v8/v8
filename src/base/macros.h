64d73404393f25e2ba3a2b80106bdca2dcb95fe9
bmeurer <bmeurer@chromium.org>
2015-05-05 00:44:30 -0700

    [c++11] Remove remnants of pre-C++11 delete, final and override.
    These macros are not needed anymore, so there's no point in supporting
    them.
    
    Review URL: https://codereview.chromium.org/1123723003
    
    Cr-Commit-Position: refs/heads/master@{#28214}

--- src/base/macros.h
+++ src/base/macros.h
@@ -234,9 +234,9 @@ V8_INLINE Dest bit_cast(Source const& source) {
 
 // A macro to disallow the evil copy constructor and operator= functions
 // This should be used in the private: declarations for a class
-#define DISALLOW_COPY_AND_ASSIGN(TypeName)  \
-  TypeName(const TypeName&) V8_DELETE;      \
-  void operator=(const TypeName&) V8_DELETE
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&) = delete;      \
+  void operator=(const TypeName&) = delete
 
 
 // A macro to disallow all the implicit constructors, namely the
@@ -245,8 +245,8 @@ V8_INLINE Dest bit_cast(Source const& source) {
 // This should be used in the private: declarations for a class
 // that wants to prevent anyone from instantiating it. This is
 // especially useful for classes containing only static methods.
-#define DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName)  \
-  TypeName() V8_DELETE;                           \
+#define DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
+  TypeName() = delete;                           \
   DISALLOW_COPY_AND_ASSIGN(TypeName)
 
 
