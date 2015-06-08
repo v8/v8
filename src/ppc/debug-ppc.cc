05d816cbd23e8af43dad66aad867d904c62cb52e
mbrandy <mbrandy@us.ibm.com>
2015-06-08 11:20:41 -0700

    PPC: Vector ICs: debugger should save registers for vector store ics.
    Port bd32a9f711cb14c2b7c34512d43e78bd24960124
    
    R=mvstanton@chromium.org, dstence@us.ibm.com, michael_dawson@ca.ibm.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1169763003
    
    Cr-Commit-Position: refs/heads/master@{#28847}

--- src/ppc/debug-ppc.cc
+++ src/ppc/debug-ppc.cc
@@ -172,8 +172,11 @@ void DebugCodegen::GenerateStoreICDebugBreak(MacroAssembler* masm) {
   Register receiver = StoreDescriptor::ReceiverRegister();
   Register name = StoreDescriptor::NameRegister();
   Register value = StoreDescriptor::ValueRegister();
-  Generate_DebugBreakCallHelper(masm, receiver.bit() | name.bit() | value.bit(),
-                                0);
+  RegList regs = receiver.bit() | name.bit() | value.bit();
+  if (FLAG_vector_stores) {
+    regs |= VectorStoreICDescriptor::SlotRegister().bit();
+  }
+  Generate_DebugBreakCallHelper(masm, regs, 0);
 }
 
 
@@ -185,11 +188,7 @@ void DebugCodegen::GenerateKeyedLoadICDebugBreak(MacroAssembler* masm) {
 
 void DebugCodegen::GenerateKeyedStoreICDebugBreak(MacroAssembler* masm) {
   // Calling convention for IC keyed store call (from ic-ppc.cc).
-  Register receiver = StoreDescriptor::ReceiverRegister();
-  Register name = StoreDescriptor::NameRegister();
-  Register value = StoreDescriptor::ValueRegister();
-  Generate_DebugBreakCallHelper(masm, receiver.bit() | name.bit() | value.bit(),
-                                0);
+  GenerateStoreICDebugBreak(masm);
 }
 
 
