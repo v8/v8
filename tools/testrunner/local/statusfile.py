d869f4a4801d4ef6868c266c07f9e29d2e29cba5
arv <arv@chromium.org>
2015-06-09 10:27:12 -0700

    Update Test262 to 5/30
    This updates the test runner to run all tests in both sloppy
    and strict mode.
    
    It also marks the test in the status file as failing in
    sloppy mode even if all it does is marking the test as
    [PASS, FAIL]. A future CL will have to validate that the
    tests pass and fail in the correct mode.
    
    Review URL: https://codereview.chromium.org/1136553008
    
    Cr-Commit-Position: refs/heads/master@{#28879}

--- tools/testrunner/local/statusfile.py
+++ tools/testrunner/local/statusfile.py
@@ -57,7 +57,8 @@ for var in ["debug", "release", "big", "little",
             "android_arm", "android_arm64", "android_ia32", "android_x87",
             "android_x64", "arm", "arm64", "ia32", "mips", "mipsel",
             "mips64el", "x64", "x87", "nacl_ia32", "nacl_x64", "ppc", "ppc64",
-            "macos", "windows", "linux", "aix"]:
+            "macos", "windows", "linux", "aix",
+            "strict", "sloppy"]:
   VARIABLES[var] = var
 
 
