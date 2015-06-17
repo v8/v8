72bb369d08e8d141f77c0c05f95cf8ed7ac5a9ec
machenbach <machenbach@chromium.org>
2015-06-16 23:52:55 -0700

    Revert of Update test262-es6 to 6/11 (patchset #2 id:40001 of https://codereview.chromium.org/1175313003/)
    Reason for revert:
    [Sheriff] Please fix test expectations for nosnap before reland. There are 54 tests failing:
    http://build.chromium.org/p/client.v8/builders/V8%20Linux%20-%20nosnap/builds/3422
    
    Original issue's description:
    > Update test262-es6 to 6/11
    >
    > This reverts commit 67b169199e06a486e7b663c7758f81995ddc6593 and picks
    > up the fixes to the yaml parser upstream.
    >
    > BUG=N
    > LOG=N
    > R=adamk@chromium.org, machenbach@chromium.org
    >
    > Committed: https://crrev.com/bc847230610d0518a700a69546d23784e6ce3479
    > Cr-Commit-Position: refs/heads/master@{#29056}
    
    TBR=adamk@chromium.org,arv@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=N
    
    Review URL: https://codereview.chromium.org/1186093005
    
    Cr-Commit-Position: refs/heads/master@{#29067}

--- tools/testrunner/local/statusfile.py
+++ tools/testrunner/local/statusfile.py
@@ -57,8 +57,7 @@ for var in ["debug", "release", "big", "little",
             "android_arm", "android_arm64", "android_ia32", "android_x87",
             "android_x64", "arm", "arm64", "ia32", "mips", "mipsel",
             "mips64el", "x64", "x87", "nacl_ia32", "nacl_x64", "ppc", "ppc64",
-            "macos", "windows", "linux", "aix",
-            "strict", "sloppy"]:
+            "macos", "windows", "linux", "aix"]:
   VARIABLES[var] = var
 
 
