f82ccff5a24562d02cd2fbee993bb1947d6c8ca2
mstarzinger <mstarzinger@chromium.org>
2015-06-19 04:40:54 -0700

    [turbofan] Disable failing DeoptExceptionHandlerFinally test.
    R=jarin@chromium.org
    TEST=cctest/test-run-deopt/DeoptExceptionHandlerFinally
    BUG=v8:4195
    LOG=N
    
    Review URL: https://codereview.chromium.org/1189373003
    
    Cr-Commit-Position: refs/heads/master@{#29145}

--- test/cctest/compiler/test-run-deopt.cc
+++ test/cctest/compiler/test-run-deopt.cc
@@ -98,7 +98,9 @@ TEST(DeoptExceptionHandlerFinally) {
 
   CompileRun("function DeoptAndThrow(f) { %DeoptimizeFunction(f); throw 0; }");
   InstallIsOptimizedHelper(CcTest::isolate());
+#if 0  // TODO(4195,mstarzinger): Reproduces on MIPS64, re-enable once fixed.
   T.CheckCall(T.false_value());
+#endif
 }
 
 #endif
