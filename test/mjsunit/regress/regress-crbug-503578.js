3e2c6a2eb7f7a04d1df63aae0d5bcf2eaf4ff334
binji <binji@chromium.org>
2015-06-23 22:36:13 -0700

    Fix ReferenceError of Worker in regress-crbug-503578
    Worker is not defined on the V8 Shared bots.
    
    BUG=chromium:503578
    R=jarin@chromium.org
    LOG=n
    
    Review URL: https://codereview.chromium.org/1202763004
    
    Cr-Commit-Position: refs/heads/master@{#29246}

--- test/mjsunit/regress/regress-crbug-503578.js
+++ test/mjsunit/regress/regress-crbug-503578.js
@@ -2,14 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function __f_1() {
-  onmessage = function() {}
+if (this.Worker) {
+  function __f_1() {
+    onmessage = function() {}
+  }
+  function __f_0(byteLength) {
+    var __v_1 = new ArrayBuffer(byteLength);
+    var __v_5 = new Uint32Array(__v_1);
+    return __v_5;
+  }
+  var __v_6 = new Worker(__f_1);
+  var __v_3 = __f_0(16);
+  __v_6.postMessage(__v_3);
 }
-function __f_0(byteLength) {
-  var __v_1 = new ArrayBuffer(byteLength);
-  var __v_5 = new Uint32Array(__v_1);
-  return __v_5;
-}
-var __v_6 = new Worker(__f_1);
-var __v_3 = __f_0(16);
-__v_6.postMessage(__v_3);
