365ff33234dc61e6454aa46b8d5d95c857b53d00
machenbach <machenbach@chromium.org>
2015-06-30 11:42:38 -0700

    Revert of [strong] Add tests for loading from proxy, super, with access checks (patchset #4 id:80001 of https://codereview.chromium.org/1193633003/)
    Reason for revert:
    [Sheriff] Breaks gc stress:
    http://build.chromium.org/p/client.v8/builders/V8%20GC%20Stress%20-%202/builds/3968
    
    Original issue's description:
    > [strong] Add tests for loading from proxy, super, with access checks
    >
    > Followup for https://codereview.chromium.org/1168093002/
    >
    > These seem to already function correctly without the need for further runtime
    > changes.
    >
    > There will be a final follow-up for interceptors.
    >
    > BUG=v8:3956
    > LOG=N
    >
    > Committed: https://crrev.com/ea4e568363f48955e7de6b538e559132e9899c86
    > Cr-Commit-Position: refs/heads/master@{#29389}
    
    TBR=rossberg@chromium.org,conradw@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=v8:3956
    
    Review URL: https://codereview.chromium.org/1218123003
    
    Cr-Commit-Position: refs/heads/master@{#29390}

--- test/mjsunit/strong/load-builtins.js
+++ test/mjsunit/strong/load-builtins.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --strong-mode
+// Flags: --strong-mode --allow-natives-syntax
 
 function getGlobal() {
   return this;
