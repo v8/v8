3f5d39a1d6ea05e1b0b3ad8cc1e2d8e5dbfb29bc
ulan <ulan@chromium.org>
2015-07-02 07:37:23 -0700

    Fix memory-leak in default platform implementation of delayed tasks.
    BUG=
    
    Review URL: https://codereview.chromium.org/1221083004
    
    Cr-Commit-Position: refs/heads/master@{#29446}

--- src/libplatform/default-platform.cc
+++ src/libplatform/default-platform.cc
@@ -41,16 +41,21 @@ DefaultPlatform::~DefaultPlatform() {
   base::LockGuard<base::Mutex> guard(&lock_);
   queue_.Terminate();
   if (initialized_) {
-    for (std::vector<WorkerThread*>::iterator i = thread_pool_.begin();
-         i != thread_pool_.end(); ++i) {
+    for (auto i = thread_pool_.begin(); i != thread_pool_.end(); ++i) {
       delete *i;
     }
   }
-  for (std::map<v8::Isolate*, std::queue<Task*> >::iterator i =
-           main_thread_queue_.begin();
-       i != main_thread_queue_.end(); ++i) {
+  for (auto i = main_thread_queue_.begin(); i != main_thread_queue_.end();
+       ++i) {
     while (!i->second.empty()) {
       delete i->second.front();
+      i->second.pop();
+    }
+  }
+  for (auto i = main_thread_delayed_queue_.begin();
+       i != main_thread_delayed_queue_.end(); ++i) {
+    while (!i->second.empty()) {
+      delete i->second.top().second;
       i->second.pop();
     }
   }
