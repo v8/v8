98b56f0da6ad66ecc70de45e843636fd13083bf3
rossberg <rossberg@chromium.org>
2015-08-25 02:23:00 -0700

    Reship arrow functions
    ...in canary.
    
    This reverts commit c75af23299ec948cf2d809e8aa86b2c43184cde3.
    
    R=hablich@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1319443002
    
    Cr-Commit-Position: refs/heads/master@{#30346}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -204,7 +204,6 @@ DEFINE_BOOL(legacy_const, true, "legacy semantics for const in sloppy mode")
 
 // Features that are complete (but still behind --harmony/es-staging flag).
 #define HARMONY_STAGED(V)                                       \
-  V(harmony_arrow_functions, "harmony arrow functions")         \
   V(harmony_tostring, "harmony toString")                       \
   V(harmony_concat_spreadable, "harmony isConcatSpreadable")    \
   V(harmony_rest_parameters, "harmony rest parameters")         \
@@ -212,6 +211,7 @@ DEFINE_BOOL(legacy_const, true, "legacy semantics for const in sloppy mode")
 
 // Features that are shipping (turned on by default, but internal flag remains).
 #define HARMONY_SHIPPING(V)                                     \
+  V(harmony_arrow_functions, "harmony arrow functions")         \
   V(harmony_array_includes, "harmony Array.prototype.includes") \
   V(harmony_new_target, "harmony new.target")                   \
   V(harmony_object_observe, "harmony Object.observe")           \
