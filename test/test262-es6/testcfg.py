29ef63c6b512a4d938823e657b9634827f58f764
littledan <littledan@chromium.org>
2015-08-25 18:13:51 -0700

    Test262 roll to the 2015-8-25 version
    Review URL: https://codereview.chromium.org/1317723003
    
    Cr-Commit-Position: refs/heads/master@{#30365}

--- test/test262-es6/testcfg.py
+++ test/test262-es6/testcfg.py
@@ -39,8 +39,8 @@ from testrunner.local import utils
 from testrunner.objects import testcase
 
 # The revision hash needs to be 7 characters?
-TEST_262_ARCHIVE_REVISION = "258d212"  # This is the 2015-07-31 revision.
-TEST_262_ARCHIVE_MD5 = "a9b26e19ce582492642af973c8cee826"
+TEST_262_ARCHIVE_REVISION = "6137f75"  # This is the 2015-08-25 revision.
+TEST_262_ARCHIVE_MD5 = "c1eaf890d46e73d6c7e05ab21f76e668"
 TEST_262_URL = "https://github.com/tc39/test262/tarball/%s"
 TEST_262_HARNESS_FILES = ["sta.js", "assert.js"]
 
