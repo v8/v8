951f6b7a88fccbe14a6ddd8611e7bd98f9f7f7b1
kozyatinskiy <kozyatinskiy@chromium.org>
2015-08-27 12:01:03 -0700

    [V8] Report JSON parser script to DevTools
    If JSON contains SyntaxError then V8 will report exception and won't report compile error.
    
    LOG=Y
    BUG=chromium:515382
    R=yangguo@chromium.org,yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/1308123006
    
    Cr-Commit-Position: refs/heads/master@{#30426}

--- src/json-parser.h
+++ src/json-parser.h
@@ -7,6 +7,7 @@
 
 #include "src/char-predicates.h"
 #include "src/conversions.h"
+#include "src/debug/debug.h"
 #include "src/factory.h"
 #include "src/messages.h"
 #include "src/scanner.h"
@@ -244,9 +245,11 @@ MaybeHandle<Object> JsonParser<seq_one_byte>::ParseJson() {
         break;
     }
 
-    MessageLocation location(factory->NewScript(source_),
-                             position_,
-                             position_ + 1);
+    Handle<Script> script(factory->NewScript(source_));
+    // We should sent compile error event because we compile JSON object in
+    // separated source file.
+    isolate()->debug()->OnCompileError(script);
+    MessageLocation location(script, position_, position_ + 1);
     Handle<Object> error = factory->NewSyntaxError(message, argument);
     return isolate()->template Throw<Object>(error, &location);
   }
