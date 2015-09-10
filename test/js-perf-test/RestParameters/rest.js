0cc8eaa4c628543b520ab48667176967a225f7db
caitpotter88 <caitpotter88@gmail.com>
2015-09-10 06:44:58 -0700

    [es6] fixup for rest parameters perf test
    BUG=v8:2160
    LOG=N
    NOTRY=true
    R=adamk, machenbach@chromium.org, rossberg
    
    Review URL: https://codereview.chromium.org/1331803003
    
    Cr-Commit-Position: refs/heads/master@{#30682}

--- test/js-perf-test/RestParameters/rest.js
+++ test/js-perf-test/RestParameters/rest.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-new BenchmarkSuite('Basic', [1000], [
+new BenchmarkSuite('Basic1', [1000], [
   new Benchmark('BasicRest1', false, false, 0,
                 BasicRest1, BasicRest1Setup, BasicRest1TearDown)
 ]);
