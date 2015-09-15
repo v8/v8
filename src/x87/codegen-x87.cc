8c8c7523c2a6188d552ba35a7948285de0d40705
chunyang.dai <chunyang.dai@intel.com>
2015-09-15 04:51:38 -0700

    X87: Make FlushICache part of Assembler(Base) and take Isolate as parameter.
    port 9fc4fc141fa1d1aa1a3ccb901518cdffa0d6d268 (r30695).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1339293002
    
    Cr-Commit-Position: refs/heads/master@{#30743}

--- src/x87/codegen-x87.cc
+++ src/x87/codegen-x87.cc
@@ -182,7 +182,7 @@ MemMoveFunction CreateMemMoveFunction() {
   CodeDesc desc;
   masm.GetCode(&desc);
   DCHECK(!RelocInfo::RequiresRelocation(desc));
-  CpuFeatures::FlushICache(buffer, actual_size);
+  Assembler::FlushICacheWithoutIsolate(buffer, actual_size);
   base::OS::ProtectCode(buffer, actual_size);
   // TODO(jkummerow): It would be nice to register this code creation event
   // with the PROFILE / GDBJIT system.
@@ -617,7 +617,7 @@ void Code::PatchPlatformCodeAge(Isolate* isolate,
   uint32_t young_length = isolate->code_aging_helper()->young_sequence_length();
   if (age == kNoAgeCodeAge) {
     isolate->code_aging_helper()->CopyYoungSequenceTo(sequence);
-    CpuFeatures::FlushICache(sequence, young_length);
+    Assembler::FlushICache(isolate, sequence, young_length);
   } else {
     Code* stub = GetCodeAgeStub(isolate, age, parity);
     CodePatcher patcher(sequence, young_length);
