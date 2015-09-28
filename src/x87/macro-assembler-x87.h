bac284ee75c80be60cf0c41a144e7ff71e1e922b
chunyang.dai <chunyang.dai@intel.com>
2015-09-27 20:09:00 -0700

    X87: Full code shouldn't embed the type feedback vector.
    port c90c60ba2689fb524a1526503c9c87f534cf58fb (r30940)
    
    original commit message:
    
        Make sure to always reference it indirectly. This allows us to make the vector
        native-context dependent should we wish.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1369963002
    
    Cr-Commit-Position: refs/heads/master@{#30954}

--- src/x87/macro-assembler-x87.h
+++ src/x87/macro-assembler-x87.h
@@ -908,6 +908,9 @@ class MacroAssembler: public Assembler {
     return SafepointRegisterStackIndex(reg.code());
   }
 
+  // Load the type feedback vector from a JavaScript frame.
+  void EmitLoadTypeFeedbackVector(Register vector);
+
   // Activation support.
   void EnterFrame(StackFrame::Type type);
   void EnterFrame(StackFrame::Type type, bool load_constant_pool_pointer_reg);
