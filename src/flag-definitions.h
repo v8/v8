bab3b5508280e7a58720f701f9dbec96f81a16d8
adamk <adamk@chromium.org>
2015-09-30 12:17:23 -0700

    Revert "Stage sloppy block-scoped functions (Annex B 3.3)"
    The current implemention breaks sloppy mode code that uses function
    declarations inside blocks at top-level. Work is ongoing on a patch
    to fix this issue, but in the meantime it seems reasonable to move
    the feature out of staging.
    
    Manual revert of commit 6e07f5a75ba2c949ac96efabd5248c76b9957112.
    
    R=littledan@chromium.org
    BUG=chromium:535836
    LOG=y
    
    Review URL: https://codereview.chromium.org/1375213005
    
    Cr-Commit-Position: refs/heads/master@{#31029}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -188,16 +188,17 @@ DEFINE_IMPLICATION(es_staging, harmony)
 DEFINE_BOOL(legacy_const, true, "legacy semantics for const in sloppy mode")
 
 // Features that are still work in progress (behind individual flags).
-#define HARMONY_INPROGRESS(V)                                 \
-  V(harmony_modules, "harmony modules")                       \
-  V(harmony_regexps, "harmony regular expression extensions") \
-  V(harmony_proxies, "harmony proxies")                       \
-  V(harmony_unicode_regexps, "harmony unicode regexps")       \
-  V(harmony_tolength, "harmony ToLength")                     \
-  V(harmony_reflect, "harmony Reflect API")                   \
-  V(harmony_destructuring, "harmony destructuring")           \
-  V(harmony_default_parameters, "harmony default parameters") \
-  V(harmony_sharedarraybuffer, "harmony sharedarraybuffer")   \
+#define HARMONY_INPROGRESS(V)                                         \
+  V(harmony_modules, "harmony modules")                               \
+  V(harmony_regexps, "harmony regular expression extensions")         \
+  V(harmony_proxies, "harmony proxies")                               \
+  V(harmony_unicode_regexps, "harmony unicode regexps")               \
+  V(harmony_tolength, "harmony ToLength")                             \
+  V(harmony_reflect, "harmony Reflect API")                           \
+  V(harmony_sloppy_function, "harmony sloppy function block scoping") \
+  V(harmony_destructuring, "harmony destructuring")                   \
+  V(harmony_default_parameters, "harmony default parameters")         \
+  V(harmony_sharedarraybuffer, "harmony sharedarraybuffer")           \
   V(harmony_simd, "harmony simd")
 
 // Features that are complete (but still behind --harmony/es-staging flag).
@@ -205,7 +206,6 @@ DEFINE_BOOL(legacy_const, true, "legacy semantics for const in sloppy mode")
   V(harmony_tostring, "harmony toString")                             \
   V(harmony_concat_spreadable, "harmony isConcatSpreadable")          \
   V(harmony_sloppy, "harmony features in sloppy mode")                \
-  V(harmony_sloppy_function, "harmony sloppy function block scoping") \
   V(harmony_sloppy_let, "harmony let in sloppy mode")
 
 // Features that are shipping (turned on by default, but internal flag remains).
