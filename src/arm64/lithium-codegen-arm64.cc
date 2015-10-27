4b4bead9432223d8fe5563462fc907885636db3a
Jakob Kummerow <jkummerow@chromium.org>
2015-10-27 16:12:07 +0100

    Version 4.7.80.16 (cherry-pick)
    Merged 102e3e87e7bb8a647893149efa31bc2e771c7eb9
    
    [arm64] Fix jssp based spill slot accesses in Crankshaft
    
    R=hablich@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1426753003 .
    
    Cr-Commit-Position: refs/branch-heads/4.7@{#27}
    Cr-Branched-From: f3c89267db0fc6120d95046c3ff35a35ca34614f-refs/heads/master@{#31014}

--- src/arm64/lithium-codegen-arm64.cc
+++ src/arm64/lithium-codegen-arm64.cc
@@ -2811,6 +2811,8 @@ void LCodeGen::DoDoubleToIntOrSmi(LDoubleToIntOrSmi* instr) {
 
 void LCodeGen::DoDrop(LDrop* instr) {
   __ Drop(instr->count());
+
+  RecordPushedArgumentsDelta(instr->hydrogen_value()->argument_delta());
 }
 
 
