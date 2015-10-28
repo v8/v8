4e0045647192f300e02e98927180b799f644a3ed
zhengxing.li <zhengxing.li@intel.com>
2015-10-28 04:34:06 -0700

    X87: Fix the sqrt precision issue.
      In order to resolve the sqrt precision issue described in https://codereview.chromium.org/1425763002/.
      we change the implementation of CreateSqrtFunction() implementation of X87 so that the optimize compiler
      and full-compiler implementation are unified.
    
    R=weiliang.lin@intel.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1417553007
    
    Cr-Commit-Position: refs/heads/master@{#31625}

--- src/compiler/x87/code-generator-x87.cc
+++ src/compiler/x87/code-generator-x87.cc
@@ -972,24 +972,12 @@ void CodeGenerator::AssembleArchInstruction(Instruction* instr) {
       break;
     }
     case kX87Float64Sqrt: {
+      __ X87SetFPUCW(0x027F);
       __ fstp(0);
       __ fld_d(MemOperand(esp, 0));
-#if V8_GLIBC_PREREQ(2, 19)
-      __ push(edx);
-      __ sub(esp, Immediate(2 * kIntSize));
-      __ fnstcw(MemOperand(esp, 4));
-      __ mov(edx, Immediate(0xfeff));
-      __ and_(edx, MemOperand(esp, 4));
-      __ mov(MemOperand(esp, 0), edx);
-      __ fldcw(MemOperand(esp, 0));
-#endif
       __ fsqrt();
-#if V8_GLIBC_PREREQ(2, 19)
-      __ fldcw(MemOperand(esp, 4));
-      __ lea(esp, Operand(esp, 2 * kIntSize));
-      __ pop(edx);
-#endif
       __ lea(esp, Operand(esp, kDoubleSize));
+      __ X87SetFPUCW(0x037F);
       break;
     }
     case kX87Float64Round: {
