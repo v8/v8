e752f964b645fc7aafd99fd684914a10831619e8
adamk <adamk@chromium.org>
2015-11-12 07:59:02 -0800

    Ship --harmony-default-parameters
    BUG=v8:2160
    LOG=y
    
    Review URL: https://codereview.chromium.org/1438753002
    
    Cr-Commit-Position: refs/heads/master@{#31965}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -206,7 +206,6 @@ DEFINE_IMPLICATION(es_staging, harmony_destructuring)
 
 // Features that are complete (but still behind --harmony/es-staging flag).
 #define HARMONY_STAGED(V)                                     \
-  V(harmony_default_parameters, "harmony default parameters") \
   V(harmony_destructuring, "harmony destructuring")           \
   V(harmony_regexps, "harmony regular expression extensions") \
   V(harmony_sloppy, "harmony features in sloppy mode")        \
@@ -215,6 +214,7 @@ DEFINE_IMPLICATION(es_staging, harmony_destructuring)
 // Features that are shipping (turned on by default, but internal flag remains).
 #define HARMONY_SHIPPING(V)                                     \
   V(harmony_array_includes, "harmony Array.prototype.includes") \
+  V(harmony_default_parameters, "harmony default parameters")   \
   V(harmony_object_observe, "harmony Object.observe")           \
   V(harmony_rest_parameters, "harmony rest parameters")         \
   V(harmony_concat_spreadable, "harmony isConcatSpreadable")    \
