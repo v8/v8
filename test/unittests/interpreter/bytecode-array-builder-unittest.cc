daf185b243c581cf3acdc3bc1db787a6b662d5ec
oth <oth@chromium.org>
2015-11-24 10:10:46 -0800

    Revert of [Interpreter] Add CreateClosure to BytecodeGraphBuilder. (patchset #8 id:140001 of https://codereview.chromium.org/1458603012/ )
    Reason for revert:
    Build break.
    
    Original issue's description:
    > [Interpreter] Add CreateClosure to BytecodeGraphBuilder.
    >
    > Adds code and tests to support CreateClosure bytecode when building
    > graphs.
    >
    > BUG=v8:4280
    > LOG=N
    >
    > Committed: https://crrev.com/4cceb11b0929abcbc82bf0854554a9b66003335d
    > Cr-Commit-Position: refs/heads/master@{#32224}
    
    TBR=bmeurer@chromium.org,mythria@chromium.org,mstarzinger@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=v8:4280
    
    Review URL: https://codereview.chromium.org/1471913004
    
    Cr-Commit-Position: refs/heads/master@{#32225}

--- test/unittests/interpreter/bytecode-array-builder-unittest.cc
+++ test/unittests/interpreter/bytecode-array-builder-unittest.cc
@@ -94,10 +94,7 @@ TEST_F(BytecodeArrayBuilderTest, AllBytecodesGenerated) {
       .StoreKeyedProperty(reg, reg, 2056, LanguageMode::STRICT);
 
   // Emit closure operations.
-  Factory* factory = isolate()->factory();
-  Handle<SharedFunctionInfo> shared_info = factory->NewSharedFunctionInfo(
-      factory->NewStringFromStaticChars("function_a"), MaybeHandle<Code>());
-  builder.CreateClosure(shared_info, NOT_TENURED);
+  builder.CreateClosure(NOT_TENURED);
 
   // Emit argument creation operations.
   builder.CreateArguments(CreateArgumentsType::kMappedArguments)
@@ -217,11 +214,6 @@ TEST_F(BytecodeArrayBuilderTest, AllBytecodesGenerated) {
     builder.GetConstantPoolEntry(handle(Smi::FromInt(i), isolate()));
   }
   builder.LoadLiteral(Smi::FromInt(20000000));
-
-  // CreateClosureWide
-  Handle<SharedFunctionInfo> shared_info2 = factory->NewSharedFunctionInfo(
-      factory->NewStringFromStaticChars("function_b"), MaybeHandle<Code>());
-  builder.CreateClosure(shared_info2, NOT_TENURED);
 
   builder.Return();
 
