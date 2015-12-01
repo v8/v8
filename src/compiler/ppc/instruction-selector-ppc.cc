878e5059a92443169466610263a272520c7b37d9
mbrandy <mbrandy@us.ibm.com>
2015-11-30 19:31:57 -0800

    PPC: [turbofan] Implemented the TruncateFloat32ToUint64 TurboFan operator.
    R=ahaas@chromium.org, joransiu@ca.ibm.com, jyan@ca.ibm.com, michael_dawson@ca.ibm.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1480243003
    
    Cr-Commit-Position: refs/heads/master@{#32434}

--- src/compiler/ppc/instruction-selector-ppc.cc
+++ src/compiler/ppc/instruction-selector-ppc.cc
@@ -938,7 +938,7 @@ void InstructionSelector::VisitTruncateFloat64ToInt64(Node* node) {
 
 
 void InstructionSelector::VisitTruncateFloat32ToUint64(Node* node) {
-  UNIMPLEMENTED();
+  VisitRR(this, kPPC_DoubleToUint64, node);
 }
 
 
