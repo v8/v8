9151860c12f34c8234326efb89bfd7da5fcf40d8
mbrandy <mbrandy@us.ibm.com>
2015-12-02 13:58:52 -0800

    PPC: [stubs] A new approach to TF stubs
    Port 3e7e3ed726dd016dc6ae89803999a28fba2362c7
    
    Original commit message:
        * Add a sibling interface to InterpreterAssembler called
          CodeStubAssembler which provides a wrapper around the
          RawMachineAssembler and is intented to make it easy to build
          efficient cross-platform code stubs. Much of the implementation
          of CodeStubAssembler is shamelessly stolen from the
          InterpreterAssembler, and the idea is to eventually merge the
          two interfaces somehow, probably moving the
          InterpreterAssembler interface over to use the
          CodeStubAssembler. Short-term, however, the two interfaces
          shall remain decoupled to increase our velocity developing the
          two systems in parallel.
        * Implement the StringLength stub in TurboFan with the new
          CodeStubAssembler. Replace and remove the old Hydrogen-stub
          version.
        * Remove a whole slew of machinery to support JavaScript-style
          code stub generation, since it ultimately proved unwieldy,
          brittle and baroque. This cleanup includes removing the shared
          code stub context, several example stubs and a tangle of build
          file changes.
    
    R=danno@chromium.org, joransiu@ca.ibm.com, jyan@ca.ibm.com, michael_dawson@ca.ibm.com
    BUG=v8:4587
    LOG=n
    
    Review URL: https://codereview.chromium.org/1492633006
    
    Cr-Commit-Position: refs/heads/master@{#32532}

--- src/ppc/interface-descriptors-ppc.cc
+++ src/ppc/interface-descriptors-ppc.cc
@@ -390,27 +390,6 @@ void ApiAccessorDescriptor::InitializePlatformSpecific(
 }
 
 
-void MathRoundVariantCallFromUnoptimizedCodeDescriptor::
-    InitializePlatformSpecific(CallInterfaceDescriptorData* data) {
-  Register registers[] = {
-      r4,  // math rounding function
-      r6,  // vector slot id
-  };
-  data->InitializePlatformSpecific(arraysize(registers), registers);
-}
-
-
-void MathRoundVariantCallFromOptimizedCodeDescriptor::
-    InitializePlatformSpecific(CallInterfaceDescriptorData* data) {
-  Register registers[] = {
-      r4,  // math rounding function
-      r6,  // vector slot id
-      r7,  // type vector
-  };
-  data->InitializePlatformSpecific(arraysize(registers), registers);
-}
-
-
 void InterpreterPushArgsAndCallDescriptor::InitializePlatformSpecific(
     CallInterfaceDescriptorData* data) {
   Register registers[] = {
