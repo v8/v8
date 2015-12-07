931f99b1358c3af20f4644ef23298097112346a5
rmcilroy <rmcilroy@chromium.org>
2015-12-07 08:11:15 -0800

    Fix Arm64 code size multiplier.
    Fixes code size multiplier of Arm64 based on generated code size of Octane.
    
    Review URL: https://codereview.chromium.org/1504023002
    
    Cr-Commit-Position: refs/heads/master@{#32661}

--- src/full-codegen/full-codegen.h
+++ src/full-codegen/full-codegen.h
@@ -83,8 +83,7 @@ class FullCodeGenerator: public AstVisitor {
 #elif V8_TARGET_ARCH_ARM
   static const int kCodeSizeMultiplier = 149;
 #elif V8_TARGET_ARCH_ARM64
-// TODO(all): Copied ARM value. Check this is sensible for ARM64.
-  static const int kCodeSizeMultiplier = 149;
+  static const int kCodeSizeMultiplier = 220;
 #elif V8_TARGET_ARCH_PPC64
   static const int kCodeSizeMultiplier = 200;
 #elif V8_TARGET_ARCH_PPC
