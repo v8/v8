55f78b437126c98d90728dc2abc78d5c43ea639d
zhengxing.li <zhengxing.li@intel.com>
2015-12-06 22:47:56 -0800

    X87: [debugger] do not predict step in target for liveedit.
      port 1e671030b96d48105c00ada463e10d95d73dae24 (r32614)
    
      original commit message:
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1502053003
    
    Cr-Commit-Position: refs/heads/master@{#32641}

--- src/debug/x87/debug-x87.cc
+++ src/debug/x87/debug-x87.cc
@@ -105,22 +105,15 @@ void DebugCodegen::GenerateDebugBreakStub(MacroAssembler* masm,
 }
 
 
-void DebugCodegen::GeneratePlainReturnLiveEdit(MacroAssembler* masm) {
-  masm->ret(0);
-}
-
-
 void DebugCodegen::GenerateFrameDropperLiveEdit(MacroAssembler* masm) {
-  ExternalReference restarter_frame_function_slot =
-      ExternalReference::debug_restarter_frame_function_pointer_address(
-          masm->isolate());
-  __ mov(Operand::StaticVariable(restarter_frame_function_slot), Immediate(0));
-
   // We do not know our frame height, but set esp based on ebp.
   __ lea(esp, Operand(ebp, -1 * kPointerSize));
 
   __ pop(edi);  // Function.
   __ pop(ebp);
+
+  ParameterCount dummy(0);
+  __ FloodFunctionIfStepping(edi, no_reg, dummy, dummy);
 
   // Load context from the function.
   __ mov(esi, FieldOperand(edi, JSFunction::kContextOffset));
