46cb23c24b518114151c181bd75f628c2cee0f9e
littledan <littledan@chromium.org>
2015-12-10 14:51:54 -0800

    Disable new regression tests with noi18n
    BUG=chromium:487322
    R=adamk
    LOG=N
    
    Review URL: https://codereview.chromium.org/1514993002
    
    Cr-Commit-Position: refs/heads/master@{#32770}

--- test/mjsunit/regress/regress-487322.js
+++ test/mjsunit/regress/regress-487322.js
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Normalizes Kat{h,}mandu (chromium:487322)
-df = new Intl.DateTimeFormat('en-US', {'timeZone': 'Asia/Katmandu'})
-assertEquals('Asia/Katmandu', df.resolvedOptions().timeZone);
+if (this.Intl) {
+  // Normalizes Kat{h,}mandu (chromium:487322)
+  df = new Intl.DateTimeFormat('en-US', {'timeZone': 'Asia/Katmandu'})
+  assertEquals('Asia/Katmandu', df.resolvedOptions().timeZone);
 
-df = new Intl.DateTimeFormat('en-US', {'timeZone': 'Asia/Kathmandu'})
-assertEquals('Asia/Katmandu', df.resolvedOptions().timeZone);
+  df = new Intl.DateTimeFormat('en-US', {'timeZone': 'Asia/Kathmandu'})
+  assertEquals('Asia/Katmandu', df.resolvedOptions().timeZone);
 
-// Throws for unsupported time zones.
-assertThrows(() => Intl.DateTimeFormat(undefined, {timeZone: 'Aurope/Paris'}));
+  // Throws for unsupported time zones.
+  assertThrows(() => Intl.DateTimeFormat(undefined, {timeZone: 'Aurope/Paris'}));
+}
