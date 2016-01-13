57cea798debf6c1a3f74956299fd255d52a152df
yangguo <yangguo@chromium.org>
2016-01-13 03:01:10 -0800

    Disable concurrent osr.
    R=jarin@chromium.org
    BUG=v8:4650
    LOG=Y
    
    Review URL: https://codereview.chromium.org/1574113009
    
    Cr-Commit-Position: refs/heads/master@{#33261}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -407,7 +407,7 @@ DEFINE_INT(concurrent_recompilation_delay, 0,
            "artificial compilation delay in ms")
 DEFINE_BOOL(block_concurrent_recompilation, false,
             "block queued jobs until released")
-DEFINE_BOOL(concurrent_osr, true, "concurrent on-stack replacement")
+DEFINE_BOOL(concurrent_osr, false, "concurrent on-stack replacement")
 DEFINE_IMPLICATION(concurrent_osr, concurrent_recompilation)
 
 DEFINE_BOOL(omit_map_checks_for_leaf_maps, true,
