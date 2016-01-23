8aeb6080e11548c018e898a078a162c23a45f7b8
adamk <adamk@chromium.org>
2016-01-22 16:40:15 -0800

    Sloppy mode webcompat: allow conflicting function declarations in blocks
    The web appears to depend on being able to redeclare functions-in-blocks
    in sloppy mode (examples seen so far tend to redeclare identical functions,
    most likely accidentally).
    
    This patch opens a minimal hole: two same-named function declarations
    in the same scope are allowed, only in sloppy mode.
    
    BUG=v8:4693, chromium:579395
    LOG=y
    
    Review URL: https://codereview.chromium.org/1622723003
    
    Cr-Commit-Position: refs/heads/master@{#33478}

--- src/parsing/parser.cc
+++ src/parsing/parser.cc
@@ -1926,6 +1926,7 @@ Variable* Parser::Declare(Declaration* declaration,
   DCHECK(proxy->raw_name() != NULL);
   const AstRawString* name = proxy->raw_name();
   VariableMode mode = declaration->mode();
+  bool is_function_declaration = declaration->IsFunctionDeclaration();
   if (scope == nullptr) scope = scope_;
   Scope* declaration_scope =
       IsLexicalVariableMode(mode) ? scope : scope->DeclarationScope();
@@ -1952,7 +1953,7 @@ Variable* Parser::Declare(Declaration* declaration,
       // Declare the name.
       Variable::Kind kind = Variable::NORMAL;
       int declaration_group_start = -1;
-      if (declaration->IsFunctionDeclaration()) {
+      if (is_function_declaration) {
         kind = Variable::FUNCTION;
       } else if (declaration->IsVariableDeclaration() &&
                  declaration->AsVariableDeclaration()->is_class_declaration()) {
@@ -1963,8 +1964,11 @@ Variable* Parser::Declare(Declaration* declaration,
       var = declaration_scope->DeclareLocal(
           name, mode, declaration->initialization(), kind, kNotAssigned,
           declaration_group_start);
-    } else if (IsLexicalVariableMode(mode) ||
-               IsLexicalVariableMode(var->mode()) ||
+    } else if (((IsLexicalVariableMode(mode) ||
+                 IsLexicalVariableMode(var->mode())) &&
+                // Allow duplicate function decls for web compat, see bug 4693.
+                (is_strict(language_mode()) || !is_function_declaration ||
+                 !var->is_function())) ||
                ((mode == CONST_LEGACY || var->mode() == CONST_LEGACY) &&
                 !declaration_scope->is_script_scope())) {
       // The name was declared in this scope before; check for conflicting
