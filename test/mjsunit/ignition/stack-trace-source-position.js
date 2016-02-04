0f075613e74283bc3defce292a8cf94688f080ae
yangguo <yangguo@chromium.org>
2016-02-04 03:46:44 -0800

    [interpreter] temporarily remove --ignition from test.
    This is to avoid polluting fuzzer seeds with the --ignition flag
    until we figure out something better.
    
    TBR=mstarzinger@chromium.org
    
    Review URL: https://codereview.chromium.org/1661333002
    
    Cr-Commit-Position: refs/heads/master@{#33729}

--- test/mjsunit/ignition/stack-trace-source-position.js
+++ test/mjsunit/ignition/stack-trace-source-position.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --ignition --ignition-filter=f
+// Flags: --ignition-filter=f
 // Flags: --no-turbo
 
 // TODO(yangguo): fix for turbofan
