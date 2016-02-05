c844c036f2dd1e3847a2be90b510699ff7a21ebd
yangguo <yangguo@chromium.org>
2016-02-05 05:04:28 -0800

    Fix debug-script test expectation for no-snap build.
    NOTRY=true
    TBR=machenbach@chromium.org
    
    Review URL: https://codereview.chromium.org/1668393004
    
    Cr-Commit-Position: refs/heads/master@{#33772}

--- test/mjsunit/debug-script.js
+++ test/mjsunit/debug-script.js
@@ -73,8 +73,8 @@ for (i = 0; i < scripts.length; i++) {
 
 // This has to be updated if the number of native scripts change.
 assertEquals(%NativeScriptsCount(), named_native_count);
-// The 'gc' extension and one extras script is loaded.
-assertEquals(2, extension_count);
+// The 'gc' extension and one or two extras scripts are loaded.
+assertTrue(extension_count == 2 || extension_count == 3);
 // This script and mjsunit.js has been loaded.  If using d8, d8 loads
 // a normal script during startup too.
 assertTrue(normal_count == 2 || normal_count == 3);
