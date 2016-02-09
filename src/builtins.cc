6bc71431995d49d4ca4a2ea9c75e5add5f345225
cbruni <cbruni@chromium.org>
2016-02-09 05:13:57 -0800

    [counters] moving runtime counters to counter.h
    Additionally list C++ builtins as well under --runtime_call_stats.
    Let's try to keep all counters in one place, that makes it a bit
    easier to maintain and especially discard unused ones.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1678973002
    
    Cr-Commit-Position: refs/heads/master@{#33847}

--- src/builtins.cc
+++ src/builtins.cc
@@ -139,10 +139,26 @@ BUILTIN_LIST_C(DEF_ARG_TYPE)
                                                      Isolate* isolate);        \
   MUST_USE_RESULT static Object* Builtin_##name(                               \
       int args_length, Object** args_object, Isolate* isolate) {               \
+    RuntimeCallStats* stats = isolate->counters()->runtime_call_stats();       \
+    stats->Count_Builtin_##name++;                                             \
+    base::ElapsedTimer timer;                                                  \
+    bool timing = false;                                                       \
+    if (FLAG_runtime_call_stats && !stats->in_runtime_call) {                  \
+      stats->in_runtime_call = true;                                           \
+      timing = true;                                                           \
+      timer.Start();                                                           \
+    }                                                                          \
     name##ArgumentsType args(args_length, args_object);                        \
     isolate->counters()->runtime_calls()->Increment();                         \
-    return Builtin_Impl_##name(args, isolate);                                 \
+    Object* value = Builtin_Impl_##name(args, isolate);                        \
+    if (timing) {                                                              \
+      stats->in_runtime_call = false;                                          \
+      isolate->counters()->runtime_call_stats()->Time_Builtin_##name +=        \
+          timer.Elapsed();                                                     \
+    }                                                                          \
+    return value;                                                              \
   }                                                                            \
+                                                                               \
   MUST_USE_RESULT static Object* Builtin_Impl_##name(name##ArgumentsType args, \
                                                      Isolate* isolate)
 
