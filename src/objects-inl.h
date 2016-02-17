c383fc20ca66875fc5e5b9b7a6f848cbe2aaef17
verwaest <verwaest@chromium.org>
2016-02-17 08:18:17 -0800

    [runtime] Don't compute the hash for linear search in descriptor array
    I replaced the hidden string with hidden_properties_symbol, so we don't
    need the extra hash-check anymore. This is slightly faster anyway.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1707653003
    
    Cr-Commit-Position: refs/heads/master@{#34086}

--- src/objects-inl.h
+++ src/objects-inl.h
@@ -2591,8 +2591,8 @@ int BinarySearch(T* array, Name* name, int low, int high, int valid_entries,
 template <SearchMode search_mode, typename T>
 int LinearSearch(T* array, Name* name, int len, int valid_entries,
                  int* out_insertion_index) {
-  uint32_t hash = name->Hash();
   if (search_mode == ALL_ENTRIES) {
+    uint32_t hash = name->Hash();
     for (int number = 0; number < len; number++) {
       int sorted_index = array->GetSortedKeyIndex(number);
       Name* entry = array->GetKey(sorted_index);
@@ -2609,9 +2609,7 @@ int LinearSearch(T* array, Name* name, int len, int valid_entries,
     DCHECK(len >= valid_entries);
     DCHECK_NULL(out_insertion_index);  // Not supported here.
     for (int number = 0; number < valid_entries; number++) {
-      Name* entry = array->GetKey(number);
-      uint32_t current_hash = entry->Hash();
-      if (current_hash == hash && entry->Equals(name)) return number;
+      if (array->GetKey(number)->Equals(name)) return number;
     }
     return T::kNotFound;
   }
