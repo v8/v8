4fbca26cde10237b0333e4296302ea6bc436a52d
machenbach <machenbach@chromium.org>
2016-02-17 00:39:44 -0800

    [Swarming] Make gcmole wrapper cwd-independent.
    Some parts of the wrapped scripts seem to expect cwd to be
    the v8 directory.
    
    BUG=chromium:535160
    LOG=n
    TBR=tandrii@chromium.org, kjellander@chromium.org, mstarzinger@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1700963003
    
    Cr-Commit-Position: refs/heads/master@{#34059}

--- tools/gcmole/run-gcmole.py
+++ tools/gcmole/run-gcmole.py
@@ -12,10 +12,12 @@ CLANG_BIN = os.path.join(GCMOLE_PATH, 'gcmole-tools', 'bin')
 CLANG_PLUGINS = os.path.join(GCMOLE_PATH, 'gcmole-tools')
 LUA = os.path.join(GCMOLE_PATH, 'gcmole-tools', 'lua52')
 DRIVER = os.path.join(GCMOLE_PATH, 'gcmole.lua')
+BASE_PATH = os.path.dirname(os.path.dirname(GCMOLE_PATH))
 
 assert len(sys.argv) == 2
 
 sys.exit(subprocess.call(
     [LUA, DRIVER, sys.argv[1]],
     env={'CLANG_BIN': CLANG_BIN, 'CLANG_PLUGINS': CLANG_PLUGINS},
+    cwd=BASE_PATH,
 ))
