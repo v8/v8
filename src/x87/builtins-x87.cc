d9b1dbbee186574d7d485afa87e2421cbbf98427
zhengxing.li <zhengxing.li@intel.com>
2016-02-17 23:58:04 -0800

    X87: [es6] [interpreter] Add tail calls support to Ignition.
      port 32b4bc138252d9773cc90b2d4aae78d98844bba1 (r34083)
    
      original commit message:
      This CL introduces two new bytecodes TailCall and TailCallWide.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1703233003
    
    Cr-Commit-Position: refs/heads/master@{#34100}

--- src/x87/builtins-x87.cc
+++ src/x87/builtins-x87.cc
@@ -99,7 +99,6 @@ static void GenerateTailCallToSharedCode(MacroAssembler* masm) {
   __ jmp(ebx);
 }
 
-
 void Builtins::Generate_InOptimizationQueue(MacroAssembler* masm) {
   // Checking whether the queued function is ready for install is optional,
   // since we come across interrupts and stack checks elsewhere.  However,
@@ -678,7 +677,8 @@ static void Generate_InterpreterPushArgs(MacroAssembler* masm,
 
 
 // static
-void Builtins::Generate_InterpreterPushArgsAndCall(MacroAssembler* masm) {
+void Builtins::Generate_InterpreterPushArgsAndCallImpl(
+    MacroAssembler* masm, TailCallMode tail_call_mode) {
   // ----------- S t a t e -------------
   //  -- eax : the number of arguments (not including the receiver)
   //  -- ebx : the address of the first argument to be pushed. Subsequent
@@ -701,7 +701,9 @@ void Builtins::Generate_InterpreterPushArgsAndCall(MacroAssembler* masm) {
 
   // Call the target.
   __ Push(edx);  // Re-push return address.
-  __ Jump(masm->isolate()->builtins()->Call(), RelocInfo::CODE_TARGET);
+  __ Jump(masm->isolate()->builtins()->Call(ConvertReceiverMode::kAny,
+                                            tail_call_mode),
+          RelocInfo::CODE_TARGET);
 }
 
 
@@ -2015,6 +2017,16 @@ void PrepareForTailCall(MacroAssembler* masm, Register args_reg,
   __ movzx_b(scratch1, Operand::StaticVariable(debug_is_active));
   __ cmp(scratch1, Immediate(0));
   __ j(not_equal, &done, Label::kNear);
+
+  // Drop possible interpreter handler/stub frame.
+  {
+    Label no_interpreter_frame;
+    __ cmp(Operand(ebp, StandardFrameConstants::kMarkerOffset),
+           Immediate(Smi::FromInt(StackFrame::STUB)));
+    __ j(not_equal, &no_interpreter_frame, Label::kNear);
+    __ mov(ebp, Operand(ebp, StandardFrameConstants::kCallerFPOffset));
+    __ bind(&no_interpreter_frame);
+  }
 
   // Check if next frame is an arguments adaptor frame.
   Label no_arguments_adaptor, formal_parameter_count_loaded;
