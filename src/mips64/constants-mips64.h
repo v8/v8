4efc32ea3e931450e728596dea685fb44594d44d
alan.li <alan.li@imgtec.com>
2016-02-20 06:20:45 -0800

    Fix a WASM compiler test failure (invalid instructions).
    WASM compiler test will sometimes generate invalid instructions for
    DINS/INS.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1709633004
    
    Cr-Commit-Position: refs/heads/master@{#34175}

--- src/mips64/constants-mips64.h
+++ src/mips64/constants-mips64.h
@@ -1206,6 +1206,7 @@ Instruction::Type Instruction::InstructionType(TypeChecks checks) const {
     case SPECIAL3:
       switch (FunctionFieldRaw()) {
         case INS:
+        case DINS:
         case EXT:
         case DEXT:
         case DEXTM:
