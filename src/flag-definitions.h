008981cf12e9b9049cbd25b216e80fe45cd6e172
caitpotter88 <caitpotter88@gmail.com>
2016-03-01 08:14:30 -0800

    [esnext] stage Object.values/entries, Object.getOwnPropertyDescriptors
    BUG=v8:4663, v8:4725
    LOG=N
    R=littledan@chromium.org, adamk@chromium.org
    
    Review URL: https://codereview.chromium.org/1744193002
    
    Cr-Commit-Position: refs/heads/master@{#34399}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -212,16 +212,16 @@ DEFINE_IMPLICATION(es_staging, harmony_tailcalls)
   V(harmony_simd, "harmony simd")                                            \
   V(harmony_do_expressions, "harmony do-expressions")                        \
   V(harmony_tailcalls, "harmony tail calls")                                 \
-  V(harmony_object_values_entries, "harmony Object.values / Object.entries") \
-  V(harmony_object_own_property_descriptors,                                 \
-    "harmony Object.getOwnPropertyDescriptors()")                            \
   V(harmony_regexp_property, "harmony unicode regexp property classes")
 
 // Features that are complete (but still behind --harmony/es-staging flag).
-#define HARMONY_STAGED(V)                                     \
-  V(harmony_function_name, "harmony Function name inference") \
-  V(harmony_regexp_lookbehind, "harmony regexp lookbehind")   \
-  V(harmony_instanceof, "harmony instanceof support")
+#define HARMONY_STAGED(V)                                                    \
+  V(harmony_function_name, "harmony Function name inference")                \
+  V(harmony_regexp_lookbehind, "harmony regexp lookbehind")                  \
+  V(harmony_instanceof, "harmony instanceof support")                        \
+  V(harmony_object_values_entries, "harmony Object.values / Object.entries") \
+  V(harmony_object_own_property_descriptors,                                 \
+    "harmony Object.getOwnPropertyDescriptors()")
 
 // Features that are shipping (turned on by default, but internal flag remains).
 #define HARMONY_SHIPPING(V)                                               \
