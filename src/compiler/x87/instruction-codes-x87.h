7e66b57acb8f319611fb7b59fbe8d01f417894c4
zhengxing.li <zhengxing.li@intel.com>
2016-03-15 20:12:44 -0700

    X87: [wasm] Int64Lowering of Int64Add on ia32 and arm.
      port 1b230799364b7a6fbff3b0ffaee38bc06e19504b (r34747)
    
      original commit message:
      Int64Add is lowered to a new turbofan operator, Int32AddPair. The new
      operator takes 4 inputs an generates 2 outputs. The inputs are the low
      word of the left input, high word of the left input, the low word of the
      right input, and high word of the right input. The ouputs are the low
      and high word of the result of the addition.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1806833002
    
    Cr-Commit-Position: refs/heads/master@{#34803}

--- src/compiler/x87/instruction-codes-x87.h
+++ src/compiler/x87/instruction-codes-x87.h
@@ -31,6 +31,7 @@ namespace compiler {
   V(X87Shl)                        \
   V(X87Shr)                        \
   V(X87Sar)                        \
+  V(X87AddPair)                    \
   V(X87ShlPair)                    \
   V(X87ShrPair)                    \
   V(X87SarPair)                    \
