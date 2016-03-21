d50d7e18511c9cac1cdea54db6812107ec36a060
mstarzinger <mstarzinger@chromium.org>
2016-03-21 09:47:13 -0700

    [interpreter] Drop overprotective --harmony-instanceof check.
    The check in question will trigger because one of our unit tests still
    emits the bytecode (i.e. unittests/BytecodeArrayBuilderTest) even when
    the flag is turned on by default.
    
    R=rmcilroy@chromium.org
    TEST=unittests/BytecodeArrayBuilderTest.AllBytecodesGenerated
    
    Review URL: https://codereview.chromium.org/1820753003
    
    Cr-Commit-Position: refs/heads/master@{#34954}

--- src/interpreter/bytecode-array-builder.cc
+++ src/interpreter/bytecode-array-builder.cc
@@ -1433,7 +1433,6 @@ Bytecode BytecodeArrayBuilder::BytecodeForCompareOperation(Token::Value op) {
     case Token::Value::GTE:
       return Bytecode::kTestGreaterThanOrEqual;
     case Token::Value::INSTANCEOF:
-      DCHECK(!FLAG_harmony_instanceof);
       return Bytecode::kTestInstanceOf;
     case Token::Value::IN:
       return Bytecode::kTestIn;
