f62ac8aad2edb8d11c627269da24e5362d9cc6cb
jyan <jyan@ca.ibm.com>
2016-03-22 13:18:43 -0700

    S390: Turn StoreWithInterceptor into a turbofan stub
    Port 75ed4a6071a4a2561136173882c8f48432d5c2cd
    R=verwaest@chromium.org, joransiu@ca.ibm.com, mbrandy@us.ibm.com, michael_dawson@ca.ibm.com
    BUG=
    
    Review URL: https://codereview.chromium.org/1825623002
    
    Cr-Commit-Position: refs/heads/master@{#35005}

--- src/ic/s390/handler-compiler-s390.cc
+++ src/ic/s390/handler-compiler-s390.cc
@@ -706,17 +706,6 @@ Handle<Code> NamedStoreHandlerCompiler::CompileStoreCallback(
   return GetCode(kind(), Code::FAST, name);
 }
 
-Handle<Code> NamedStoreHandlerCompiler::CompileStoreInterceptor(
-    Handle<Name> name) {
-  __ Push(receiver(), this->name(), value());
-
-  // Do tail-call to the runtime system.
-  __ TailCallRuntime(Runtime::kStorePropertyWithInterceptor);
-
-  // Return the generated code.
-  return GetCode(kind(), Code::FAST, name);
-}
-
 Register NamedStoreHandlerCompiler::value() {
   return StoreDescriptor::ValueRegister();
 }
