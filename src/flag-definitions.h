5836807ea89b96ecfb4bbd8febf0d67bb8dae9ed
mstarzinger <mstarzinger@chromium.org>
2016-03-22 04:40:29 -0700

    [es6] Ship new ES6 instanceof operator semantics.
    R=hablich@chromium.org
    BUG=v8:4447
    LOG=n
    
    Review URL: https://codereview.chromium.org/1820903002
    
    Cr-Commit-Position: refs/heads/master@{#34984}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -210,7 +210,6 @@ DEFINE_IMPLICATION(es_staging, harmony_tailcalls)
 // Features that are complete (but still behind --harmony/es-staging flag).
 #define HARMONY_STAGED(V)                                                    \
   V(harmony_regexp_lookbehind, "harmony regexp lookbehind")                  \
-  V(harmony_instanceof, "harmony instanceof support")                        \
   V(harmony_object_values_entries, "harmony Object.values / Object.entries") \
   V(harmony_object_own_property_descriptors,                                 \
     "harmony Object.getOwnPropertyDescriptors()")                            \
@@ -220,6 +219,7 @@ DEFINE_IMPLICATION(es_staging, harmony_tailcalls)
 #define HARMONY_SHIPPING(V)                                           \
   V(harmony_array_prototype_values, "harmony Array.prototype.values") \
   V(harmony_function_name, "harmony Function name inference")         \
+  V(harmony_instanceof, "harmony instanceof support")                 \
   V(harmony_iterator_close, "harmony iterator finalization")          \
   V(harmony_regexps, "harmony regular expression extensions")         \
   V(harmony_unicode_regexps, "harmony unicode regexps")               \
