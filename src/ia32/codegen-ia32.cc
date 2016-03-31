e6b6e5545386985f1d0bec875307e5df52062d71
mythria <mythria@chromium.org>
2016-03-31 06:44:44 -0700

    [Interpreter] Changes GenerateDoubleToObject to push and pop rsi value.
    In the earlier implementation of GenerateDoubleToObject the context
    is loaded from the parent's frame. rsi is clobbered because it is used
    to store kHoleNan constnat. It is not always safe to peek at
    the parents frame. Bytecode handlers have TypedFrame and the type of
    frame is stored at FP + 1. GenerateDoubleToObject expects context
    to be store at that place. In the current implementation rsi is pushed
    onto the stack and is popped when exiting this function.
    
    BUG=v8:4280,chromium:597565
    LOG=N
    
    Review URL: https://codereview.chromium.org/1848473002
    
    Cr-Commit-Position: refs/heads/master@{#35163}

--- src/ia32/codegen-ia32.cc
+++ src/ia32/codegen-ia32.cc
@@ -704,6 +704,7 @@ void ElementsTransitionGenerator::GenerateDoubleToObject(
   __ cmp(edi, Immediate(masm->isolate()->factory()->empty_fixed_array()));
   __ j(equal, &only_change_map);
 
+  __ push(esi);
   __ push(eax);
   __ push(edx);
   __ push(ebx);
@@ -753,10 +754,10 @@ void ElementsTransitionGenerator::GenerateDoubleToObject(
 
   // Call into runtime if GC is required.
   __ bind(&gc_required);
-  __ mov(esi, Operand(ebp, StandardFrameConstants::kContextOffset));
   __ pop(ebx);
   __ pop(edx);
   __ pop(eax);
+  __ pop(esi);
   __ jmp(fail);
 
   // Box doubles into heap numbers.
@@ -818,7 +819,7 @@ void ElementsTransitionGenerator::GenerateDoubleToObject(
 
   // Restore registers.
   __ pop(eax);
-  __ mov(esi, Operand(ebp, StandardFrameConstants::kContextOffset));
+  __ pop(esi);
 
   __ bind(&success);
 }
