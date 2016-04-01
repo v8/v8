84492bb66b340f4e0df36758e98fddbb10b5d1dc
adamk <adamk@chromium.org>
2016-03-31 17:38:05 -0700

    Ship --harmony-regexp-exec
    There are still spec compliance fixes to be made, but this patch
    turns the flag to shipping to make sure we get more canary coverage
    and performance data from the bots.
    
    BUG=v8:4602
    LOG=y
    
    Review URL: https://codereview.chromium.org/1847103002
    
    Cr-Commit-Position: refs/heads/master@{#35181}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -204,7 +204,6 @@ DEFINE_IMPLICATION(es_staging, move_object_start)
 
 // Features that are complete (but still behind --harmony/es-staging flag).
 #define HARMONY_STAGED(V)                                                    \
-  V(harmony_regexp_exec, "harmony RegExp exec override behavior")            \
   V(harmony_regexp_lookbehind, "harmony regexp lookbehind")                  \
   V(harmony_tailcalls, "harmony tail calls")                                 \
   V(harmony_object_values_entries, "harmony Object.values / Object.entries") \
@@ -219,6 +218,7 @@ DEFINE_IMPLICATION(es_staging, move_object_start)
   V(harmony_instanceof, "harmony instanceof support")                 \
   V(harmony_iterator_close, "harmony iterator finalization")          \
   V(harmony_unicode_regexps, "harmony unicode regexps")               \
+  V(harmony_regexp_exec, "harmony RegExp exec override behavior")     \
   V(harmony_sloppy, "harmony features in sloppy mode")                \
   V(harmony_sloppy_let, "harmony let in sloppy mode")                 \
   V(harmony_sloppy_function, "harmony sloppy function block scoping") \
