cb3c4100ee844e9d6e805fe4cc97c142d63b8639
Michael Hablich <hablich@chromium.org>
2016-04-11 17:37:35 +0200

    Version 5.1.298.2 (cherry-pick)
    Merged 78049e9c4837f053575d6c71e53ae12fec99f1aa
    
    Revert of [compiler] Make feedback vector cope with flag changes. (patchset #1 id:1 of https://codereview.chromium.org/1869693003/ )
    
    BUG=chromium:600995
    LOG=N
    TBR=machenbach@chromium.org
    
    Review URL: https://codereview.chromium.org/1881473002 .
    
    Cr-Commit-Position: refs/heads/5.1.298@{#3}
    Cr-Branched-From: ad1784e5c68b14ff0400b7221ce55d1478066013-refs/heads/master@{#35345}

--- src/compiler.cc
+++ src/compiler.cc
@@ -744,11 +744,9 @@ void EnsureFeedbackVector(CompilationInfo* info) {
   if (!info->has_shared_info()) return;
 
   // If no type feedback vector exists, we create one now. At this point the
-  // AstNumbering pass has already run. Note the snapshot can contain outdated
-  // vectors for a different configuration, hence we also recreate a new vector
-  // when the function is not compiled (i.e. no code was serialized).
-  if (info->shared_info()->feedback_vector()->is_empty() ||
-      !info->shared_info()->is_compiled()) {
+  // AstNumbering pass has already run. Note that we should reuse any existing
+  // feedback vector rather than creating a new one.
+  if (info->shared_info()->feedback_vector()->is_empty()) {
     Handle<TypeFeedbackMetadata> feedback_metadata = TypeFeedbackMetadata::New(
         info->isolate(), info->literal()->feedback_vector_spec());
     Handle<TypeFeedbackVector> feedback_vector =
