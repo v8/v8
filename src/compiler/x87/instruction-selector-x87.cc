8e4fbb059a5ce1dec685754b43a8db43f70f82c2
zhengxing.li <zhengxing.li@intel.com>
2016-04-14 02:05:56 -0700

    X87: [wasm] Refactoring pair-shift code in the instruction selector of arm and ia32.
      port f954934d6237998f26b6784b22b1e6f02b8b430b (r35140)
    
      original commit message:
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1886243002
    
    Cr-Commit-Position: refs/heads/master@{#35476}

--- src/compiler/x87/instruction-selector-x87.cc
+++ src/compiler/x87/instruction-selector-x87.cc
@@ -599,8 +599,9 @@ void InstructionSelector::VisitInt32PairMul(Node* node) {
   Emit(kX87MulPair, 2, outputs, 4, inputs, 1, temps);
 }
 
-void InstructionSelector::VisitWord32PairShl(Node* node) {
-  X87OperandGenerator g(this);
+void VisitWord32PairShift(InstructionSelector* selector, InstructionCode opcode,
+                          Node* node) {
+  X87OperandGenerator g(selector);
 
   Node* shift = node->InputAt(2);
   InstructionOperand shift_operand;
@@ -617,49 +618,19 @@ void InstructionSelector::VisitWord32PairShl(Node* node) {
       g.DefineAsFixed(node, eax),
       g.DefineAsFixed(NodeProperties::FindProjection(node, 1), edx)};
 
-  Emit(kX87ShlPair, 2, outputs, 3, inputs);
+  selector->Emit(opcode, 2, outputs, 3, inputs);
+}
+
+void InstructionSelector::VisitWord32PairShl(Node* node) {
+  VisitWord32PairShift(this, kX87ShlPair, node);
 }
 
 void InstructionSelector::VisitWord32PairShr(Node* node) {
-  X87OperandGenerator g(this);
-
-  Node* shift = node->InputAt(2);
-  InstructionOperand shift_operand;
-  if (g.CanBeImmediate(shift)) {
-    shift_operand = g.UseImmediate(shift);
-  } else {
-    shift_operand = g.UseFixed(shift, ecx);
-  }
-  InstructionOperand inputs[] = {g.UseFixed(node->InputAt(0), eax),
-                                 g.UseFixed(node->InputAt(1), edx),
-                                 shift_operand};
-
-  InstructionOperand outputs[] = {
-      g.DefineAsFixed(node, eax),
-      g.DefineAsFixed(NodeProperties::FindProjection(node, 1), edx)};
-
-  Emit(kX87ShrPair, 2, outputs, 3, inputs);
+  VisitWord32PairShift(this, kX87ShrPair, node);
 }
 
 void InstructionSelector::VisitWord32PairSar(Node* node) {
-  X87OperandGenerator g(this);
-
-  Node* shift = node->InputAt(2);
-  InstructionOperand shift_operand;
-  if (g.CanBeImmediate(shift)) {
-    shift_operand = g.UseImmediate(shift);
-  } else {
-    shift_operand = g.UseFixed(shift, ecx);
-  }
-  InstructionOperand inputs[] = {g.UseFixed(node->InputAt(0), eax),
-                                 g.UseFixed(node->InputAt(1), edx),
-                                 shift_operand};
-
-  InstructionOperand outputs[] = {
-      g.DefineAsFixed(node, eax),
-      g.DefineAsFixed(NodeProperties::FindProjection(node, 1), edx)};
-
-  Emit(kX87SarPair, 2, outputs, 3, inputs);
+  VisitWord32PairShift(this, kX87SarPair, node);
 }
 
 void InstructionSelector::VisitWord32Ror(Node* node) {
