57e2eec996e7ac544c894f870808794af43b1648
akos.palfi <akos.palfi@imgtec.com>
2016-04-18 10:50:28 -0700

    MIPS64: Fix 'Migrate FastCloneShallowObjectStub to TurboFan'
    Port 5325379982015b5becb8c1f56b1797dd36a57942
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1900743002
    
    Cr-Commit-Position: refs/heads/master@{#35592}

--- src/full-codegen/mips64/full-codegen-mips64.cc
+++ src/full-codegen/mips64/full-codegen-mips64.cc
@@ -1425,7 +1425,7 @@ void FullCodeGenerator::VisitObjectLiteral(ObjectLiteral* expr) {
   } else {
     FastCloneShallowObjectStub stub(isolate(), expr->properties_count());
     __ CallStub(&stub);
-    __ lw(cp, MemOperand(fp, StandardFrameConstants::kContextOffset));
+    __ ld(cp, MemOperand(fp, StandardFrameConstants::kContextOffset));
   }
   PrepareForBailoutForId(expr->CreateLiteralId(), TOS_REG);
 
