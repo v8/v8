f8f551ae9a5cc406cf0b5919b23ad9a654720fe9
rossberg <rossberg@chromium.org>
2016-04-22 07:05:59 -0700

    Stage harmony-for-in
    TBR=hablich@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1915453002
    
    Cr-Commit-Position: refs/heads/master@{#35734}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -199,7 +199,6 @@ DEFINE_IMPLICATION(es_staging, move_object_start)
   V(harmony_sharedarraybuffer, "harmony sharedarraybuffer")             \
   V(harmony_simd, "harmony simd")                                       \
   V(harmony_do_expressions, "harmony do-expressions")                   \
-  V(harmony_for_in, "harmony for-in syntax")                            \
   V(harmony_regexp_property, "harmony unicode regexp property classes") \
   V(harmony_string_padding, "harmony String-padding methods")
 
@@ -207,6 +206,7 @@ DEFINE_IMPLICATION(es_staging, move_object_start)
 #define HARMONY_STAGED(V)                                                    \
   V(harmony_regexp_lookbehind, "harmony regexp lookbehind")                  \
   V(harmony_tailcalls, "harmony tail calls")                                 \
+  V(harmony_for_in, "harmony for-in syntax")                                 \
   V(harmony_object_values_entries, "harmony Object.values / Object.entries") \
   V(harmony_object_own_property_descriptors,                                 \
     "harmony Object.getOwnPropertyDescriptors()")
