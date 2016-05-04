e9b244c58a8dc45e9dca9a1496f301c3ed1ba385
jyan <jyan@ca.ibm.com>
2016-05-04 11:33:57 -0700

    S390: Add HasProperty code stub that tries simple lookups or jumps to runtime otherwise.
    Code common with ObjectHasOwnProperty builtin was moved to CodeStubAssembler.
    
    R=ishell@chromium.org, joransiu@ca.ibm.com, michael_dawson@ca.ibm.com, mbrandy@us.ibm.com
    BUG=v8:2743
    LOG=Y
    
    Review-Url: https://codereview.chromium.org/1946303002
    Cr-Commit-Position: refs/heads/master@{#36037}

--- src/full-codegen/s390/full-codegen-s390.cc
+++ src/full-codegen/s390/full-codegen-s390.cc
@@ -3593,7 +3593,7 @@ void FullCodeGenerator::VisitCompareOperation(CompareOperation* expr) {
     case Token::IN:
       VisitForStackValue(expr->right());
       SetExpressionPosition(expr);
-      EmitInExpression();
+      EmitHasProperty();
       PrepareForBailoutBeforeSplit(expr, false, NULL, NULL);
       __ CompareRoot(r2, Heap::kTrueValueRootIndex);
       Split(eq, if_true, if_false, fall_through);
