79c350b3c59846052fb962eb20ec11956d000271
jyan <jyan@ca.ibm.com>
2016-05-05 12:12:14 -0700

    S390: Add new relocation type WASM_MEMORY_SIZE_REFERENCE, use relocatable pointers to update wasm memory size references in generated code.
    port 117a56b7c2a88ab2b2f25fad88517d72f5edbac9
    
    Original commit message:
         - Add new RelocInfo mode WASM_MEMORY_SIZE_REFERENCE in the assembler and add relocation information to immediates in compare instructions.
         - Use relocatable constants for MemSize/BoundsCheck in the wasm compiler
    
    R=gdeepti@chromium.org, joransiu@ca.ibm.com, mbrandy@us.ibm.com, bjaideep@ca.ibm.com, michael_dawson@ca.ibm.com
    BUG=
    
    Review-Url: https://codereview.chromium.org/1955593003
    Cr-Commit-Position: refs/heads/master@{#36056}

--- src/compiler/s390/code-generator-s390.cc
+++ src/compiler/s390/code-generator-s390.cc
@@ -1937,21 +1937,23 @@ void CodeGenerator::AssembleMove(InstructionOperand* source,
           destination->IsRegister() ? g.ToRegister(destination) : kScratchReg;
       switch (src.type()) {
         case Constant::kInt32:
-#if !V8_TARGET_ARCH_S390X
-          if (src.rmode() == RelocInfo::WASM_MEMORY_REFERENCE) {
+#if V8_TARGET_ARCH_S390X
+          if (src.rmode() == RelocInfo::WASM_MEMORY_SIZE_REFERENCE) {
+#else
+          if (src.rmode() == RelocInfo::WASM_MEMORY_REFERENCE ||
+              src.rmode() == RelocInfo::WASM_MEMORY_SIZE_REFERENCE) {
+#endif
             __ mov(dst, Operand(src.ToInt32(), src.rmode()));
           } else {
             __ mov(dst, Operand(src.ToInt32()));
           }
-#else
-          __ mov(dst, Operand(src.ToInt32()));
-#endif  // V8_TARGET_ARCH_S390X
           break;
         case Constant::kInt64:
 #if V8_TARGET_ARCH_S390X
           if (src.rmode() == RelocInfo::WASM_MEMORY_REFERENCE) {
             __ mov(dst, Operand(src.ToInt64(), src.rmode()));
           } else {
+            DCHECK(src.rmode() != RelocInfo::WASM_MEMORY_SIZE_REFERENCE);
             __ mov(dst, Operand(src.ToInt64()));
           }
 #else
