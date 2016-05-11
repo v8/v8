fa160602a3b6703df1444d3bc463b60859b0f63e
littledan <littledan@chromium.org>
2016-05-10 18:00:23 -0700

    Unship V8 custom Promise methods
    The data from UseCounters are in--V8's custom Promise methods are not
    in wide use on the web (<.002%). Therefore, this patch removes them.
    That includes:
    - Promise.prototype.chain
    - Promise.defer -- the most widely used of the bunch
    - Promise.accept
    
    For now, those methods are still available by checking the "disable
    latest stable JavaScript features" flag, or --promise-extra at the
    command line, but I expect them to be fully removable.
    
    R=adamk
    CC=rossberg
    BUG=v8:3238,v8:4633
    
    Review-Url: https://codereview.chromium.org/1965183002
    Cr-Commit-Position: refs/heads/master@{#36152}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -184,9 +184,9 @@ DEFINE_BOOL(harmony, false, "enable all completed harmony features")
 DEFINE_BOOL(harmony_shipping, true, "enable all shipped harmony features")
 DEFINE_IMPLICATION(es_staging, harmony)
 
-DEFINE_BOOL(promise_extra, true, "additional V8 Promise functions")
-// Removing extra Promise functions is staged
-DEFINE_NEG_IMPLICATION(harmony, promise_extra)
+DEFINE_BOOL(promise_extra, false, "additional V8 Promise functions")
+// Removing extra Promise functions is shipped
+DEFINE_NEG_VALUE_IMPLICATION(harmony_shipping, promise_extra, true)
 
 // Activate on ClusterFuzz.
 DEFINE_IMPLICATION(es_staging, harmony_regexp_lookbehind)
