471893ccec1b280453bf49b8bba9f6331ee2b41b
rmcilroy <rmcilroy@chromium.org>
2016-06-01 07:47:01 -0700

    [Interpreter] Fix GenerateSmiToDouble to avoid assuming it is called from a JSFrame.
    GenerateSmiToDouble on ia32 assumes that it is called from a JSFrame and can restore
    the context from the StandardFrameConstants::kContextObject. In the case of the
    interpreter it is called from a interpreter handler stub frame which doesn't
    push the context onto it's frame. Instead, push and pop esi to explicitly restore it
    correctly.
    
    BUG=chromium:612386
    
    Review-Url: https://codereview.chromium.org/2011313003
    Cr-Commit-Position: refs/heads/master@{#36649}

--- src/ia32/codegen-ia32.cc
+++ src/ia32/codegen-ia32.cc
@@ -580,6 +580,7 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
 
   __ push(eax);
   __ push(ebx);
+  __ push(esi);
 
   __ mov(edi, FieldOperand(edi, FixedArray::kLengthOffset));
 
@@ -620,8 +621,9 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
 
   // Call into runtime if GC is required.
   __ bind(&gc_required);
+
   // Restore registers before jumping into runtime.
-  __ mov(esi, Operand(ebp, StandardFrameConstants::kContextOffset));
+  __ pop(esi);
   __ pop(ebx);
   __ pop(eax);
   __ jmp(fail);
@@ -656,11 +658,10 @@ void ElementsTransitionGenerator::GenerateSmiToDouble(
   __ sub(edi, Immediate(Smi::FromInt(1)));
   __ j(not_sign, &loop);
 
+  // Restore registers.
+  __ pop(esi);
   __ pop(ebx);
   __ pop(eax);
-
-  // Restore esi.
-  __ mov(esi, Operand(ebp, StandardFrameConstants::kContextOffset));
 
   __ bind(&only_change_map);
   // eax: value
