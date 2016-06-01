0d868a108b75e4e417e218c9088109cd9899de9f
bmeurer <bmeurer@chromium.org>
2016-05-31 21:19:00 -0700

    [turbofan] Simd128Values don't have a canonical representation.
    R=jarin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2024853002
    Cr-Commit-Position: refs/heads/master@{#36627}

--- src/compiler/js-typed-lowering.cc
+++ src/compiler/js-typed-lowering.cc
@@ -593,9 +593,10 @@ Reduction JSTypedLowering::ReduceJSStrictEqual(Node* node, bool invert) {
       return Replace(replacement);
     }
   }
-  if (r.OneInputCannotBe(Type::NumberOrString())) {
-    // For values with canonical representation (i.e. not string nor number) an
-    // empty type intersection means the values cannot be strictly equal.
+  if (r.OneInputCannotBe(Type::NumberOrSimdOrString())) {
+    // For values with canonical representation (i.e. neither String, nor
+    // Simd128Value nor Number) an empty type intersection means the values
+    // cannot be strictly equal.
     if (!r.left_type()->Maybe(r.right_type())) {
       Node* replacement = jsgraph()->BooleanConstant(invert);
       ReplaceWithValue(node, replacement);
