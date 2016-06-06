0d65554c057512d39d04e443a79ba5c54697be9d
machenbach <machenbach@chromium.org>
2016-06-06 01:55:13 -0700

    [gn] Port test262 archiving to gn
    This calls the action that archives test262 in gn. In gn
    we can't specify an action output outside the product
    directory. This works around it with an extra action
    stamp file in the product directory, while the archive
    remains in the test directory.
    
    We don't want to generate the archive in the product
    directory, as some legacy archiving scripts might include
    it and it's too large. It should only be included in the
    swarming tasks that are going to use it for testing.
    
    BUG=chromium:474921
    
    Review-Url: https://codereview.chromium.org/2034713005
    Cr-Commit-Position: refs/heads/master@{#36731}

--- test/test262/archive.py
+++ test/test262/archive.py
@@ -4,7 +4,13 @@
 # found in the LICENSE file.
 
 import os
+import sys
 import tarfile
+import time
+
+# In GN we expect the path to a stamp file as an argument.
+if len(sys.argv) == 2:
+  STAMP_FILE = os.path.abspath(sys.argv[1])
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,3 +27,10 @@ def filter_git(tar_info):
 
 with tarfile.open('data.tar', 'w') as tar:
   tar.add('data', filter=filter_git)
+
+# Workaround for GN. We can't specify the tarfile as output because it's
+# not in the product directory. Therefore we track running of this script
+# with an extra stamp file in the product directory.
+if len(sys.argv) == 2:
+  with open(STAMP_FILE, 'w') as f:
+    f.write(str(time.time()))
