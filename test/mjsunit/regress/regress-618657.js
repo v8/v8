6899f878ac486c2aa89d90f480f9516f07adc1ed
neis <neis@chromium.org>
2016-06-10 06:26:40 -0700

    [generators] Improve a test.
    R=mstarzinger@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2056283002
    Cr-Commit-Position: refs/heads/master@{#36895}

--- test/mjsunit/regress/regress-618657.js
+++ test/mjsunit/regress/regress-618657.js
@@ -5,6 +5,10 @@
 // Flags: --ignition --ignition-generators --ignition-filter=-foo
 
 function* foo() { yield 42 }
-var g = foo();
+function* goo() { yield 42 }
+var f = foo();
+var g = goo();
+assertEquals(42, f.next().value);
 assertEquals(42, g.next().value);
+assertEquals(true, f.next().done);
 assertEquals(true, g.next().done);
