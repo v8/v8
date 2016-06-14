7e23de3b250f82691380cc5b01fe413bdc9e74d6
Daniel Vogelheim <vogelheim@chromium.org>
2016-06-14 18:22:24 +0200

    Version 5.3.226.1 (cherry-pick)
    Merged d20e8183b4317b9511390e36b296da3ee355d9d0
    
    Revert of [turbofan] Introduce a dedicated CheckBounds operator. (patchset #5 id:80001 of https://codereview.chromium.org/2035893004/ )
    
    BUG=v8:4470,v8:5100
    LOG=N
    TBR=hablich@chromium.org
    
    Review URL: https://codereview.chromium.org/2065123002 .
    
    Cr-Commit-Position: refs/heads/5.3.226@{#2}
    Cr-Branched-From: 6c5152406f29676b50eb9db10b82b7dfb92ac5d6-refs/heads/master@{#36959}

--- src/compiler/opcodes.h
+++ src/compiler/opcodes.h
@@ -219,7 +219,6 @@
   V(ChangeFloat64ToTagged)         \
   V(ChangeTaggedToBit)             \
   V(ChangeBitToTagged)             \
-  V(CheckBounds)                   \
   V(CheckedUint32ToInt32)          \
   V(CheckedFloat64ToInt32)         \
   V(CheckedTaggedToInt32)          \
