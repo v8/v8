b6aa77d95f1da91407bc10d8d3344293cc7f3383
ishell <ishell@chromium.org>
2016-06-15 06:32:04 -0700

    [ic] Enable new LoadGlobalIC machinery.
    BUG=chromium:576312
    LOG=Y
    
    Review-Url: https://codereview.chromium.org/2066763004
    Cr-Commit-Position: refs/heads/master@{#37006}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -770,7 +770,7 @@ DEFINE_BOOL(use_idle_notification, true,
 DEFINE_BOOL(use_ic, true, "use inline caching")
 DEFINE_BOOL(trace_ic, false, "trace inline cache state transitions")
 DEFINE_BOOL(tf_load_ic_stub, true, "use TF LoadIC stub")
-DEFINE_BOOL_READONLY(new_load_global_ic, false,
+DEFINE_BOOL_READONLY(new_load_global_ic, true,
                      "use shortcut-style load global IC")
 
 // macro-assembler-ia32.cc
