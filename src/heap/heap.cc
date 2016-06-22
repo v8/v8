2618eb0a7bc040ddf17095325e1925ce27a48ab3
yangguo <yangguo@chromium.org>
2016-06-22 02:10:41 -0700

    [heap] compact more weak fixed arrays before serializing.
    R=mlippautz@chromium.org
    
    Review-Url: https://codereview.chromium.org/2087163002
    Cr-Commit-Position: refs/heads/master@{#37172}

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -5582,24 +5582,32 @@ DependentCode* Heap::LookupWeakObjectToCodeDependency(Handle<HeapObject> obj) {
   return DependentCode::cast(empty_fixed_array());
 }
 
+namespace {
+void CompactWeakFixedArray(Object* object) {
+  if (object->IsWeakFixedArray()) {
+    WeakFixedArray* array = WeakFixedArray::cast(object);
+    array->Compact<WeakFixedArray::NullCallback>();
+  }
+}
+}  // anonymous namespace
+
 void Heap::CompactWeakFixedArrays() {
   // Find known WeakFixedArrays and compact them.
-  i::HeapIterator iterator(this);
-  for (i::HeapObject* o = iterator.next(); o != NULL; o = iterator.next()) {
+  HeapIterator iterator(this);
+  for (HeapObject* o = iterator.next(); o != NULL; o = iterator.next()) {
     if (o->IsPrototypeInfo()) {
-      i::Object* prototype_users = i::PrototypeInfo::cast(o)->prototype_users();
+      Object* prototype_users = PrototypeInfo::cast(o)->prototype_users();
       if (prototype_users->IsWeakFixedArray()) {
-        i::WeakFixedArray* array = i::WeakFixedArray::cast(prototype_users);
-        array->Compact<i::JSObject::PrototypeRegistryCompactionCallback>();
+        WeakFixedArray* array = WeakFixedArray::cast(prototype_users);
+        array->Compact<JSObject::PrototypeRegistryCompactionCallback>();
       }
     } else if (o->IsScript()) {
-      i::Object* shared_list = i::Script::cast(o)->shared_function_infos();
-      if (shared_list->IsWeakFixedArray()) {
-        i::WeakFixedArray* array = i::WeakFixedArray::cast(shared_list);
-        array->Compact<i::WeakFixedArray::NullCallback>();
-      }
+      CompactWeakFixedArray(Script::cast(o)->shared_function_infos());
     }
   }
+  CompactWeakFixedArray(noscript_shared_function_infos());
+  CompactWeakFixedArray(script_list());
+  CompactWeakFixedArray(weak_stack_trace_list());
 }
 
 void Heap::AddRetainedMap(Handle<Map> map) {
