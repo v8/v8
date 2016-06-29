a7159577b7d092ef6283c51f8bb2c456b0e23a38
mlippautz <mlippautz@chromium.org>
2016-06-29 01:16:07 -0700

    [heap] Iterate handles with special left-trim visitor
    BUG=chromium:620553
    LOG=N
    R=hpayer@chromium.org
    
    Review-Url: https://codereview.chromium.org/2102243002
    Cr-Commit-Position: refs/heads/master@{#37366}

--- src/heap/scavenger.cc
+++ src/heap/scavenger.cc
@@ -445,8 +445,6 @@ void ScavengeVisitor::ScavengePointer(Object** p) {
   Object* object = *p;
   if (!heap_->InNewSpace(object)) return;
 
-  if (heap_->PurgeLeftTrimmedObject(p)) return;
-
   Scavenger::ScavengeObject(reinterpret_cast<HeapObject**>(p),
                             reinterpret_cast<HeapObject*>(object));
 }
