5d8cfbbd06d04e218a116f9176d278bcd21d9a9b
ahaas <ahaas@chromium.org>
2016-06-30 01:38:35 -0700

    [turbofan] Don't call String::Flatten in Constant::ToHeapObject()
    The call to String::Flatten can cause garbage collection and in general adds
    complexity to the code generation. It also blocks the way to run code generation on worker threads.
    
    The call to String::Flatten in Constant::ToHeapObject() seems not to be necessary
    for correctness. If removing this call affects performance negatively, we can revert
    this CL.
    
    Review-Url: https://codereview.chromium.org/2107243002
    Cr-Commit-Position: refs/heads/master@{#37422}

--- src/compiler/instruction.cc
+++ src/compiler/instruction.cc
@@ -527,9 +527,6 @@ Handle<HeapObject> Constant::ToHeapObject() const {
   DCHECK_EQ(kHeapObject, type());
   Handle<HeapObject> value(
       bit_cast<HeapObject**>(static_cast<intptr_t>(value_)));
-  if (value->IsConsString()) {
-    value = String::Flatten(Handle<String>::cast(value), TENURED);
-  }
   return value;
 }
 
