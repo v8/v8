1f53e42bd659aca9c2d81e858b410c1d25da9057
ben <ben@strongloop.com>
2016-07-07 14:29:32 -0700

    Handle symbols in FrameMirror#invocationText().
    Fix a TypeError when putting together the invocationText for a symbol
    method's stack frame.
    
    See https://github.com/nodejs/node/issues/7536.
    
    Review-Url: https://codereview.chromium.org/2122793003
    Cr-Commit-Position: refs/heads/master@{#37597}

--- src/debug/mirrors.js
+++ src/debug/mirrors.js
@@ -1499,6 +1499,12 @@ PropertyMirror.prototype.name = function() {
 };
 
 
+PropertyMirror.prototype.toText = function() {
+  if (IS_SYMBOL(this.name_)) return %SymbolDescriptiveString(this.name_);
+  return this.name_;
+};
+
+
 PropertyMirror.prototype.isIndexed = function() {
   for (var i = 0; i < this.name_.length; i++) {
     if (this.name_[i] < '0' || '9' < this.name_[i]) {
@@ -2034,10 +2040,10 @@ FrameMirror.prototype.invocationText = function() {
         if (display_receiver) {
           result += '.';
         }
-        result += property.name();
+        result += property.toText();
       } else {
         result += '[';
-        result += property.name();
+        result += property.toText();
         result += ']';
       }
       // Also known as - if the name in the function doesn't match the name
