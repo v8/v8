ec94ad400dc257af396efa3b1899bc3168347d82
cbruni <cbruni@chromium.org>
2016-07-19 08:25:39 -0700

    [runtime] temporarily transform IsContext check from DCHECK to CHECK
    We are enabling this trial on canary to see if we can flush out some missing
    context restores.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2164633002
    Cr-Commit-Position: refs/heads/master@{#37875}

--- src/arguments.h
+++ src/arguments.h
@@ -81,6 +81,7 @@ double ClobberDoubleRegisters(double x1, double x2, double x3, double x4);
 
 // TODO(cbruni): add global flag to check whether any tracing events have been
 // enabled.
+// TODO(cbruni): Convert the IsContext CHECK back to a DCHECK.
 #define RUNTIME_FUNCTION_RETURNS_TYPE(Type, Name)                             \
   static INLINE(Type __RT_impl_##Name(Arguments args, Isolate* isolate));     \
                                                                               \
@@ -94,7 +95,7 @@ double ClobberDoubleRegisters(double x1, double x2, double x3, double x4);
   }                                                                           \
                                                                               \
   Type Name(int args_length, Object** args_object, Isolate* isolate) {        \
-    DCHECK(isolate->context() == nullptr || isolate->context()->IsContext()); \
+    CHECK(isolate->context() == nullptr || isolate->context()->IsContext());  \
     CLOBBER_DOUBLE_REGISTERS();                                               \
     if (FLAG_runtime_call_stats) {                                            \
       return Stats_##Name(args_length, args_object, isolate);                 \
