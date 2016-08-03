aacbdacb890500302657950ed8d2af50935e221d
rmcilroy <rmcilroy@chromium.org>
2016-08-03 08:01:13 -0700

    [Crankshaft] Move don't crankshaft check before EnsureDeoptimizationSupport.
    Avoids compiling baseline code when the function isn't able to be
    optimized by crankshaft.
    
    BUG=chromium:632289
    
    Review-Url: https://codereview.chromium.org/2194453002
    Cr-Commit-Position: refs/heads/master@{#38304}

--- src/crankshaft/hydrogen.cc
+++ src/crankshaft/hydrogen.cc
@@ -115,7 +115,11 @@ class HOptimizedGraphBuilderWithPositions : public HOptimizedGraphBuilder {
 };
 
 HCompilationJob::Status HCompilationJob::CreateGraphImpl() {
-  bool dont_crankshaft = info()->shared_info()->dont_crankshaft();
+  if (!isolate()->use_crankshaft() ||
+      info()->shared_info()->dont_crankshaft()) {
+    // Crankshaft is entirely disabled.
+    return FAILED;
+  }
 
   // Optimization requires a version of fullcode with deoptimization support.
   // Recompile the unoptimized version of the code if the current version
@@ -137,11 +141,6 @@ HCompilationJob::Status HCompilationJob::CreateGraphImpl() {
   }
   DCHECK(info()->shared_info()->has_deoptimization_support());
   DCHECK(!info()->shared_info()->never_compiled());
-
-  if (!isolate()->use_crankshaft() || dont_crankshaft) {
-    // Crankshaft is entirely disabled.
-    return FAILED;
-  }
 
   // Check the whitelist for Crankshaft.
   if (!info()->shared_info()->PassesFilter(FLAG_hydrogen_filter)) {
