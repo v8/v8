57fc9a7906cf6129e1fab66d6f9565a147e6034c
zhengxing.li <zhengxing.li@intel.com>
2016-08-02 23:25:39 -0700

    X87: Make FastNewFunctionContextStub take slots parameter.
      port 5bc243978396ab902f115e49aff2cf4bcb5d8a4c (r38219)
    
      original commit message:
      This will enable the interpreter to add a bytecode and use the stub.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2205813002
    Cr-Commit-Position: refs/heads/master@{#38271}

--- src/crankshaft/x87/lithium-codegen-x87.cc
+++ src/crankshaft/x87/lithium-codegen-x87.cc
@@ -148,14 +148,13 @@ void LCodeGen::DoPrologue(LPrologue* instr) {
       __ Push(info()->scope()->GetScopeInfo(info()->isolate()));
       __ CallRuntime(Runtime::kNewScriptContext);
       deopt_mode = Safepoint::kLazyDeopt;
-    } else if (slots <= FastNewFunctionContextStub::kMaximumSlots) {
-      FastNewFunctionContextStub stub(isolate(), slots);
+    } else {
+      FastNewFunctionContextStub stub(isolate());
+      __ mov(FastNewFunctionContextDescriptor::SlotsRegister(),
+             Immediate(slots));
       __ CallStub(&stub);
       // Result of FastNewFunctionContextStub is always in new space.
       need_write_barrier = false;
-    } else {
-      __ push(edi);
-      __ CallRuntime(Runtime::kNewFunctionContext);
     }
     RecordSafepoint(deopt_mode);
 
