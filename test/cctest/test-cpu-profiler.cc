c9379e5e86faf7816c22f4bb8436bfadd0a651d6
jarin <jarin@chromium.org>
2016-08-09 05:08:31 -0700

    [turbofan] Fix TickEvents cctest typo.
    Review-Url: https://codereview.chromium.org/2225033004
    Cr-Commit-Position: refs/heads/master@{#38488}

--- test/cctest/test-cpu-profiler.cc
+++ test/cctest/test-cpu-profiler.cc
@@ -242,7 +242,7 @@ TEST(TickEvents) {
   EnqueueTickSampleEvent(
       processor,
       frame2_code->instruction_start() + frame2_code->ExecutableSize() / 2,
-      frame1_code->instruction_start() + frame2_code->ExecutableSize() / 2);
+      frame1_code->instruction_start() + frame1_code->ExecutableSize() / 2);
   EnqueueTickSampleEvent(processor, frame3_code->instruction_end() - 1,
                          frame2_code->instruction_end() - 1,
                          frame1_code->instruction_end() - 1);
