f03bebc77da30de2d15f52e482c015b94cd9000b
yangguo <yangguo@chromium.org>
2016-08-12 06:51:02 -0700

    [debugger] add mixed-stack tests.
    R=jgruber@chromium.org, mstarzinger@chromium.org
    BUG=v8:5265
    
    Review-Url: https://codereview.chromium.org/2246483002
    Cr-Commit-Position: refs/heads/master@{#38621}

--- src/runtime/runtime-test.cc
+++ src/runtime/runtime-test.cc
@@ -140,6 +140,46 @@ RUNTIME_FUNCTION(Runtime_OptimizeFunctionOnNextCall) {
   return isolate->heap()->undefined_value();
 }
 
+RUNTIME_FUNCTION(Runtime_InterpretFunctionOnNextCall) {
+  HandleScope scope(isolate);
+  DCHECK(args.length() == 1);
+  CONVERT_ARG_HANDLE_CHECKED(Object, function_object, 0);
+  if (!function_object->IsJSFunction()) {
+    return isolate->heap()->undefined_value();
+  }
+  Handle<JSFunction> function = Handle<JSFunction>::cast(function_object);
+
+  // Do not tier down if we are already on optimized code. Replacing optimized
+  // code without actual deoptimization can lead to funny bugs.
+  if (function->code()->kind() != Code::OPTIMIZED_FUNCTION &&
+      function->shared()->HasBytecodeArray()) {
+    function->ReplaceCode(*isolate->builtins()->InterpreterEntryTrampoline());
+  }
+  return isolate->heap()->undefined_value();
+}
+
+RUNTIME_FUNCTION(Runtime_BaselineFunctionOnNextCall) {
+  HandleScope scope(isolate);
+  DCHECK(args.length() == 1);
+  CONVERT_ARG_HANDLE_CHECKED(Object, function_object, 0);
+  if (!function_object->IsJSFunction()) {
+    return isolate->heap()->undefined_value();
+  }
+  Handle<JSFunction> function = Handle<JSFunction>::cast(function_object);
+
+  // Do not tier down if we are already on optimized code. Replacing optimized
+  // code without actual deoptimization can lead to funny bugs.
+  if (function->code()->kind() != Code::OPTIMIZED_FUNCTION &&
+      function->code()->kind() != Code::FUNCTION) {
+    if (function->shared()->HasBaselineCode()) {
+      function->ReplaceCode(function->shared()->code());
+    } else {
+      function->MarkForBaseline();
+    }
+  }
+
+  return isolate->heap()->undefined_value();
+}
 
 RUNTIME_FUNCTION(Runtime_OptimizeOsr) {
   HandleScope scope(isolate);
--- src/runtime/runtime.h
+++ src/runtime/runtime.h
@@ -836,6 +836,8 @@ namespace internal {
   F(RunningInSimulator, 0, 1)                 \
   F(IsConcurrentRecompilationSupported, 0, 1) \
   F(OptimizeFunctionOnNextCall, -1, 1)        \
+  F(InterpretFunctionOnNextCall, 1, 1)        \
+  F(BaselineFunctionOnNextCall, 1, 1)         \
   F(OptimizeOsr, -1, 1)                       \
   F(NeverOptimizeFunction, 1, 1)              \
   F(GetOptimizationStatus, -1, 1)             \
