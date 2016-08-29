eaa3573ad52025afde9af1f41f96e48d41159b9a
ahaas <ahaas@chromium.org>
2016-08-29 06:20:21 -0700

    [wasm] Do not check load/store offsets with DCHECK.
    These DCHECKs are executed when a wasm module is instantiated. However,
    invalid load/store offsets should trigger runtime traps, not
    instantiation-time errors.
    
    R=titzer@chromium.org
    
    Review-Url: https://codereview.chromium.org/2285223002
    Cr-Commit-Position: refs/heads/master@{#38981}

--- src/assembler.cc
+++ src/assembler.cc
@@ -350,11 +350,9 @@ void RelocInfo::update_wasm_memory_reference(
   DCHECK(IsWasmMemoryReference(rmode_) || IsWasmMemorySizeReference(rmode_));
   if (IsWasmMemoryReference(rmode_)) {
     Address updated_reference;
-    DCHECK(old_size == 0 || Memory::IsAddressInRange(
-                                old_base, wasm_memory_reference(), old_size));
     updated_reference = new_base + (wasm_memory_reference() - old_base);
-    DCHECK(new_size == 0 ||
-           Memory::IsAddressInRange(new_base, updated_reference, new_size));
+    // The reference is not checked here but at runtime. Validity of references
+    // may change over time.
     unchecked_update_wasm_memory_reference(updated_reference,
                                            icache_flush_mode);
   } else if (IsWasmMemorySizeReference(rmode_)) {
