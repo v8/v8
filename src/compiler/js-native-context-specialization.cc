3955bce3ea030ac4116c3e7357bdfea1a3a8bf34
Benedikt Meurer <bmeurer@google.com>
2016-09-02 07:53:08 +0200

    Merged: [turbofan] Remove representation axis for float64 fields.
    Revision: 5c6719fcd21c1af25afe5e30f5c1c95803b3f7ed
    
    BUG=chromium:636716
    LOG=N
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=ahaas@chromium.org
    
    Review URL: https://codereview.chromium.org/2308463002 .
    
    Cr-Commit-Position: refs/branch-heads/5.3@{#57}
    Cr-Branched-From: 820a23aade5e74a92d794e05a0c2b3597f0da4b5-refs/heads/5.3.332@{#2}
    Cr-Branched-From: 37538cb2c1b4d75c41af386cb4fedbe5566f5608-refs/heads/master@{#37308}

--- src/compiler/js-native-context-specialization.cc
+++ src/compiler/js-native-context-specialization.cc
@@ -260,6 +260,11 @@ Reduction JSNativeContextSpecialization::ReduceNamedAccess(
           field_type,  MachineType::AnyTagged(), kFullWriteBarrier};
       if (access_mode == AccessMode::kLoad) {
         if (field_type->Is(Type::UntaggedFloat64())) {
+          // TODO(turbofan): We remove the representation axis from the type to
+          // avoid uninhabited representation types. This is a workaround until
+          // the {PropertyAccessInfo} is using {MachineRepresentation} instead.
+          field_access.type = Type::Union(
+              field_type, Type::Representation(Type::Number(), zone()), zone());
           if (!field_index.is_inobject() || field_index.is_hidden_field() ||
               !FLAG_unbox_double_fields) {
             this_storage = this_effect =
@@ -276,6 +281,11 @@ Reduction JSNativeContextSpecialization::ReduceNamedAccess(
       } else {
         DCHECK_EQ(AccessMode::kStore, access_mode);
         if (field_type->Is(Type::UntaggedFloat64())) {
+          // TODO(turbofan): We remove the representation axis from the type to
+          // avoid uninhabited representation types. This is a workaround until
+          // the {PropertyAccessInfo} is using {MachineRepresentation} instead.
+          field_access.type = Type::Union(
+              field_type, Type::Representation(Type::Number(), zone()), zone());
           Node* check =
               graph()->NewNode(simplified()->ObjectIsNumber(), this_value);
           this_control = this_effect =
