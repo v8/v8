1ea41a466c2666e0b709efd320198f2f626c877b
mlippautz <mlippautz@chromium.org>
2016-09-06 07:56:56 -0700

    KeyedLookupCache and DescriptorLookupCache -> lookup-cache{-inl.h,.cc,.h}
    R=verwaest@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2316503004
    Cr-Commit-Position: refs/heads/master@{#39212}

--- src/lookup-cache-inl.h
+++ src/lookup-cache-inl.h
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/lookup.h"
+#include "src/lookup-cache.h"
+
+#include "src/objects-inl.h"
 
 namespace v8 {
 namespace internal {
