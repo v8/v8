9d6872cdf1f32580de2375f19acbc9bde2b83ac9
mstarzinger <mstarzinger@chromium.org>
2016-09-08 05:15:32 -0700

    [deoptimizer] Materialize JSFunction objects without context.
    This fixes the materialization of JSFunction objects to not rely on a
    context being available. The context has been cleared because it might
    be de-materiallized itself.
    
    R=bmeurer@chromium.org
    TEST=mjsunit/compiler/escape-analysis-materialize
    BUG=chromium:644245
    
    Review-Url: https://codereview.chromium.org/2320983002
    Cr-Commit-Position: refs/heads/master@{#39277}

--- src/deoptimizer.cc
+++ src/deoptimizer.cc
@@ -3774,14 +3774,15 @@ Handle<Object> TranslatedState::MaterializeAt(int frame_index,
           return object;
         }
         case JS_FUNCTION_TYPE: {
+          Handle<SharedFunctionInfo> temporary_shared =
+              isolate_->factory()->NewSharedFunctionInfo(
+                  isolate_->factory()->empty_string(), MaybeHandle<Code>(),
+                  false);
           Handle<JSFunction> object =
               isolate_->factory()->NewFunctionFromSharedFunctionInfo(
-                  handle(isolate_->object_function()->shared()),
-                  handle(isolate_->context()));
+                  map, temporary_shared, isolate_->factory()->undefined_value(),
+                  NOT_TENURED);
           slot->value_ = object;
-          // We temporarily allocated a JSFunction for the {Object} function
-          // within the current context, to break cycles in the object graph.
-          // The correct function and context will be set below once available.
           Handle<Object> properties = MaterializeAt(frame_index, value_index);
           Handle<Object> elements = MaterializeAt(frame_index, value_index);
           Handle<Object> prototype = MaterializeAt(frame_index, value_index);
