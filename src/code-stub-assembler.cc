0dbc719bb5dcf63f9b8c6acef01c7ea07be6631d
ishell <ishell@chromium.org>
2016-09-12 01:56:17 -0700

    [stubs] Fix performance regression on x64 caused by modified double hole check.
    Compare the whole word on 64-bit architectures with the kHoleNanInt64 value
    since comparing only upper part produces slightly worse code on x64.
    Currently TurboFan is not able to fold (array + ((const1 + index*scale) + const))
    into single addressing mode: (array + (const + index*scale)).
    
    BUG=chromium:645414
    
    Review-Url: https://codereview.chromium.org/2322283002
    Cr-Commit-Position: refs/heads/master@{#39335}

--- src/code-stub-assembler.cc
+++ src/code-stub-assembler.cc
@@ -1151,10 +1151,19 @@ Node* CodeStubAssembler::LoadDoubleWithHoleCheck(Node* base, Node* offset,
                                                  Label* if_hole,
                                                  MachineType machine_type) {
   if (if_hole) {
-    Node* element_upper =
-        Load(MachineType::Uint32(), base,
-             IntPtrAdd(offset, IntPtrConstant(kIeeeDoubleExponentWordOffset)));
-    GotoIf(Word32Equal(element_upper, Int32Constant(kHoleNanUpper32)), if_hole);
+    // TODO(ishell): Compare only the upper part for the hole once the
+    // compiler is able to fold addition of already complex |offset| with
+    // |kIeeeDoubleExponentWordOffset| into one addressing mode.
+    if (Is64()) {
+      Node* element = Load(MachineType::Uint64(), base, offset);
+      GotoIf(Word64Equal(element, Int64Constant(kHoleNanInt64)), if_hole);
+    } else {
+      Node* element_upper = Load(
+          MachineType::Uint32(), base,
+          IntPtrAdd(offset, IntPtrConstant(kIeeeDoubleExponentWordOffset)));
+      GotoIf(Word32Equal(element_upper, Int32Constant(kHoleNanUpper32)),
+             if_hole);
+    }
   }
   if (machine_type.IsNone()) {
     // This means the actual value is not needed.
