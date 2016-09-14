da1f911c4269048d24a3442791b18523455f3b24
neis <neis@chromium.org>
2016-09-14 10:56:17 -0700

    [modules] Make duplicate export error deterministic.
    In case of duplicate exports, always report the error for the very last
    one.
    
    R=adamk@chromium.org
    BUG=v8:5358,v8:1569
    
    Review-Url: https://codereview.chromium.org/2331003002
    Cr-Commit-Position: refs/heads/master@{#39424}

--- src/ast/modules.cc
+++ src/ast/modules.cc
@@ -136,17 +136,30 @@ void ModuleDescriptor::MakeIndirectExportsExplicit(Zone* zone) {
 
 const ModuleDescriptor::Entry* ModuleDescriptor::FindDuplicateExport(
     Zone* zone) const {
+  const ModuleDescriptor::Entry* candidate = nullptr;
   ZoneSet<const AstRawString*> export_names(zone);
   for (const auto& it : regular_exports_) {
     const Entry* entry = it.second;
     DCHECK_NOT_NULL(entry->export_name);
-    if (!export_names.insert(entry->export_name).second) return entry;
+    DCHECK(entry->location.IsValid());
+    bool is_duplicate = !export_names.insert(entry->export_name).second;
+    if (is_duplicate &&
+        (candidate == nullptr ||
+         entry->location.beg_pos > candidate->location.beg_pos)) {
+      candidate = entry;
+    }
   }
   for (auto entry : special_exports_) {
     if (entry->export_name == nullptr) continue;  // Star export.
-    if (!export_names.insert(entry->export_name).second) return entry;
+    DCHECK(entry->location.IsValid());
+    bool is_duplicate = !export_names.insert(entry->export_name).second;
+    if (is_duplicate &&
+        (candidate == nullptr ||
+         entry->location.beg_pos > candidate->location.beg_pos)) {
+      candidate = entry;
+    }
   }
-  return nullptr;
+  return candidate;
 }
 
 bool ModuleDescriptor::Validate(ModuleScope* module_scope,
