54b55e5f2ef826cb7e9c2dd9af0bc0edebabf41e
bmeurer <bmeurer@chromium.org>
2016-09-20 22:49:58 -0700

    [turbofan] Ship store-store elimination.
    R=hablich@chromium.org, jarin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2355113003
    Cr-Commit-Position: refs/heads/master@{#39573}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -491,9 +491,8 @@ DEFINE_BOOL(turbo_instruction_scheduling, false,
             "enable instruction scheduling in TurboFan")
 DEFINE_BOOL(turbo_stress_instruction_scheduling, false,
             "randomly schedule instructions to stress dependency tracking")
-DEFINE_BOOL(turbo_store_elimination, false,
+DEFINE_BOOL(turbo_store_elimination, true,
             "enable store-store elimination in TurboFan")
-DEFINE_IMPLICATION(turbo, turbo_store_elimination)
 
 // Flags to help platform porters
 DEFINE_BOOL(minimal, false,
