a0fb619621cf89b15e2a84b1089369daa780bde0
bmeurer <bmeurer@chromium.org>
2016-09-22 03:57:18 -0700

    [compiler] Bump --max-inlined-nodes default to 200.
    R=jarin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2361793002
    Cr-Commit-Position: refs/heads/master@{#39622}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -337,7 +337,7 @@ DEFINE_BOOL(use_write_barrier_elimination, true,
 DEFINE_INT(max_inlining_levels, 5, "maximum number of inlining levels")
 DEFINE_INT(max_inlined_source_size, 600,
            "maximum source size in bytes considered for a single inlining")
-DEFINE_INT(max_inlined_nodes, 196,
+DEFINE_INT(max_inlined_nodes, 200,
            "maximum number of AST nodes considered for a single inlining")
 DEFINE_INT(max_inlined_nodes_cumulative, 400,
            "maximum cumulative number of AST nodes considered for inlining")
