86bda7dd3bfdbeb48bac1e4a078d94195f35ee69
ishell <ishell@chromium.org>
2016-09-27 04:42:06 -0700

    [stubs] Enable TurboFan StoreIC dispatcher stub.
    BUG=chromium:576312
    
    Review-Url: https://codereview.chromium.org/2166843002
    Cr-Commit-Position: refs/heads/master@{#39764}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -802,7 +802,7 @@ DEFINE_BOOL(use_idle_notification, true,
 DEFINE_BOOL(use_ic, true, "use inline caching")
 DEFINE_BOOL(trace_ic, false, "trace inline cache state transitions")
 DEFINE_BOOL(tf_load_ic_stub, true, "use TF LoadIC stub")
-DEFINE_BOOL(tf_store_ic_stub, false, "use TF StoreIC stub")
+DEFINE_BOOL(tf_store_ic_stub, true, "use TF StoreIC stub")
 
 // macro-assembler-ia32.cc
 DEFINE_BOOL(native_code_counters, false,
