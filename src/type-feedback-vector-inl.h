65716011492fce55475f6e57fc3e8d588afede88
mythria <mythria@chromium.org>
2016-10-11 05:41:50 -0700

    [Interpreter] Collect feedback about Oddballs in Subtract Stub.
    Also include feedback about Oddballs when collecting the type feedback.
    For now, Number and NumberOrOddball are collected separately
    because crankshaft does not handle NumberOrOddballs consistently.
    This should change once we fix crankshaft.
    
    BUG=v8:4280, v8:5400
    LOG=N
    
    Committed: https://crrev.com/d5d283b27d406486e2f8d7b44c6d2b3db4f98458
    Review-Url: https://codereview.chromium.org/2406843002
    Cr-Original-Commit-Position: refs/heads/master@{#40124}
    Cr-Commit-Position: refs/heads/master@{#40170}

--- src/type-feedback-vector-inl.h
+++ src/type-feedback-vector-inl.h
@@ -128,6 +128,7 @@ BinaryOperationHint BinaryOperationHintFromFeedback(int type_feedback) {
     case BinaryOperationFeedback::kSignedSmall:
       return BinaryOperationHint::kSignedSmall;
     case BinaryOperationFeedback::kNumber:
+    case BinaryOperationFeedback::kNumberOrOddball:
       return BinaryOperationHint::kNumberOrOddball;
     case BinaryOperationFeedback::kString:
       return BinaryOperationHint::kString;
