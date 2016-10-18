ea51b8a764a80f047979e577c9d5945f0c83f362
clemensh <clemensh@chromium.org>
2016-10-18 01:19:00 -0700

    [debug] [reland] Consistently use script from FrameMirror
    ... instead of getting it from the FunctionMirror. For WASM frames
    (including asm.js -> WASM), the function is either unresolved or does
    not contain the script.
    
    The added test case failed before this CL.
    
    R=kozyatinskiy@chromium.org, yangguo@chromium.org, titzer@chromium.org
    BUG=v8:4203, chromium:656622
    
    Committed: https://crrev.com/ce32e2ffd835062d764f3c0ee6a32543417cb615
    Review-Url: https://codereview.chromium.org/2415073003
    Cr-Original-Commit-Position: refs/heads/master@{#40348}
    Cr-Commit-Position: refs/heads/master@{#40387}

--- src/inspector/debugger-script.js
+++ src/inspector/debugger-script.js
@@ -416,6 +416,7 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror)
     var frameDetails = frameMirror.details();
 
     var funcObject = frameDetails.func();
+    var scriptObject = frameDetails.script();
     var sourcePosition = frameDetails.sourcePosition();
     var thisObject = frameDetails.receiver();
 
@@ -448,6 +449,7 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror)
     // Calculated lazily.
     var scopeChain;
     var funcMirror;
+    var scriptMirror;
     var location;
     /** @type {!Array<?RawLocation>} */
     var scopeStartLocations;
@@ -516,7 +518,7 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror)
     {
         if (!details) {
             var scopeObjects = ensureScopeChain();
-            var script = ensureFuncMirror().script();
+            var script = ensureScriptMirror();
             /** @type {!Array<Scope>} */
             var scopes = [];
             for (var i = 0; i < scopeObjects.length; ++i) {
@@ -570,14 +572,24 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror)
     }
 
     /**
+     * @return {!ScriptMirror}
+     */
+    function ensureScriptMirror()
+    {
+        if (!scriptMirror) {
+            scriptMirror = MakeMirror(scriptObject);
+        }
+        return /** @type {!ScriptMirror} */(scriptMirror);
+    }
+
+    /**
      * @return {!{line: number, column: number}}
      */
     function ensureLocation()
     {
         if (!location) {
-            var script = ensureFuncMirror().script();
-            if (script)
-                location = script.locationFromPosition(sourcePosition, true);
+            var script = ensureScriptMirror();
+            location = script.locationFromPosition(sourcePosition, true);
             if (!location)
                 location = { line: 0, column: 0 };
         }
@@ -616,12 +628,12 @@ DebuggerScript._frameMirrorToJSCallFrame = function(frameMirror)
     }
 
     /**
-     * @return {number|undefined}
+     * @return {number}
      */
     function sourceID()
     {
-        var script = ensureFuncMirror().script();
-        return script && script.id();
+        var script = ensureScriptMirror();
+        return script.id();
     }
 
     /**
