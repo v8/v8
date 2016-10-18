1107aa8b7490f9a2f3f7cdabd9ddefdd62fcc88a
zhengxing.li <zhengxing.li@intel.com>
2016-10-18 01:10:22 -0700

    X87: [turbofan]: Micro optimizations to lea[l/q] on ia32/x64.
      port 87ae05c7c418bee367a2006f0658a3dd171ed619 (r40341)
    
      original commit message:
      Utilize all opportunities to turn leas into adds.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2429763002
    Cr-Commit-Position: refs/heads/master@{#40386}

--- src/compiler/x87/code-generator-x87.cc
+++ src/compiler/x87/code-generator-x87.cc
@@ -1870,7 +1870,7 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
           if (i.InputRegister(1).is(i.OutputRegister())) {
             __ shl(i.OutputRegister(), 1);
           } else {
-            __ lea(i.OutputRegister(), i.MemoryOperand());
+            __ add(i.OutputRegister(), i.InputRegister(1));
           }
         } else if (mode == kMode_M2) {
           __ shl(i.OutputRegister(), 1);
@@ -1881,6 +1881,9 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
         } else {
           __ lea(i.OutputRegister(), i.MemoryOperand());
         }
+      } else if (mode == kMode_MR1 &&
+                 i.InputRegister(1).is(i.OutputRegister())) {
+        __ add(i.OutputRegister(), i.InputRegister(0));
       } else {
         __ lea(i.OutputRegister(), i.MemoryOperand());
       }
