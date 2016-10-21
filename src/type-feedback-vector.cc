d2557f2e9da4beb904ad07f6b125d853b6d7476d
ishell <ishell@chromium.org>
2016-10-21 09:42:22 -0700

    [ic] Support data handlers that represent simple field stores.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2438553003
    Review-Url: https://chromiumcodereview.appspot.com/2438553003
    Cr-Original-Commit-Position: refs/heads/master@{#40503}
    Cr-Commit-Position: refs/heads/master@{#40511}

--- src/type-feedback-vector.cc
+++ src/type-feedback-vector.cc
@@ -733,18 +733,16 @@ void KeyedLoadICNexus::ConfigureMonomorphic(Handle<Name> name,
   }
 }
 
-
 void StoreICNexus::ConfigureMonomorphic(Handle<Map> receiver_map,
-                                        Handle<Code> handler) {
+                                        Handle<Object> handler) {
   Handle<WeakCell> cell = Map::WeakCellForMap(receiver_map);
   SetFeedback(*cell);
   SetFeedbackExtra(*handler);
 }
 
-
 void KeyedStoreICNexus::ConfigureMonomorphic(Handle<Name> name,
                                              Handle<Map> receiver_map,
-                                             Handle<Code> handler) {
+                                             Handle<Object> handler) {
   Handle<WeakCell> cell = Map::WeakCellForMap(receiver_map);
   if (name.is_null()) {
     SetFeedback(*cell);
