763dd402b62265aa419cab9ee7e65cd4762a293b
bmeurer <bmeurer@chromium.org>
2016-10-25 01:12:15 -0700

    [ignition] Tune code size multiplier.
    The interpreter is currently too aggressive in tiering up to TurboFan,
    especially for (expensive) OSR. Make it slightly less aggressive by
    choosing a more realistic code size multiplier.
    
    R=jarin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2448043002
    Cr-Commit-Position: refs/heads/master@{#40550}

--- src/interpreter/interpreter.h
+++ src/interpreter/interpreter.h
@@ -67,7 +67,7 @@ class Interpreter {
   }
 
   // TODO(ignition): Tune code size multiplier.
-  static const int kCodeSizeMultiplier = 32;
+  static const int kCodeSizeMultiplier = 72;
 
  private:
 // Bytecode handler generator functions.
