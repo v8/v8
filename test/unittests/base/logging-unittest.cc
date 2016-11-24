b9b96e5448492323c4d5ebe7ddb714ef0f1e1c32
Michael Hablich <hablich@chromium.org>
2016-11-25 00:10:59 +0100

    Version 5.7.56.2 (cherry-pick)
    Merged 29ee62443d87f8cdcf0157715f5205ab45d99c94
    
    Revert of [base] Pass scalar arguments by value in CHECK/DCHECK (patchset #3 id:40001 of https://codereview.chromium.org/2524093002/ )
    
    TBR=machenbach@chomium.org
    
    Review URL: https://codereview.chromium.org/2525343004 .
    
    Cr-Commit-Position: refs/heads/5.7.56@{#3}
    Cr-Branched-From: 49ea60ef9d4a0659a3ff3ade356aacc29d65d586-refs/heads/master@{#41274}

--- test/unittests/base/logging-unittest.cc
+++ test/unittests/base/logging-unittest.cc
@@ -8,18 +8,11 @@
 namespace v8 {
 namespace base {
 
-namespace {
-template <typename Lhs, typename Rhs>
-std::string *CallCheckEQ(Lhs lhs, Rhs rhs, const char *msg) {
-  return CheckEQImpl<Lhs, Rhs>(lhs, rhs, msg);
-}
-}  // namespace
-
 TEST(LoggingTest, CheckEQImpl) {
-  EXPECT_EQ(nullptr, CallCheckEQ(0.0, 0.0, ""));
-  EXPECT_EQ(nullptr, CallCheckEQ(0.0, -0.0, ""));
-  EXPECT_EQ(nullptr, CallCheckEQ(-0.0, 0.0, ""));
-  EXPECT_EQ(nullptr, CallCheckEQ(-0.0, -0.0, ""));
+  EXPECT_EQ(nullptr, CheckEQImpl(0.0, 0.0, ""));
+  EXPECT_EQ(nullptr, CheckEQImpl(0.0, -0.0, ""));
+  EXPECT_EQ(nullptr, CheckEQImpl(-0.0, 0.0, ""));
+  EXPECT_EQ(nullptr, CheckEQImpl(-0.0, -0.0, ""));
 }
 
 }  // namespace base
