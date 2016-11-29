fa2fdf275197185d9bca9d5e72c16a60722c3893
hpayer <hpayer@chromium.org>
2016-11-29 01:32:15 -0800

    [heap] Reland: Use store buffer for writes coming from mutator.
    This reverts commit 061c2ab23a1d4cd192b935e7912e7dfb1fed845d.
    
    BUG=chromium:648568, chromium:669270
    
    Review-Url: https://codereview.chromium.org/2529293004
    Cr-Commit-Position: refs/heads/master@{#41341}

--- src/heap/store-buffer.h
+++ src/heap/store-buffer.h
@@ -10,6 +10,7 @@
 #include "src/base/platform/platform.h"
 #include "src/cancelable-task.h"
 #include "src/globals.h"
+#include "src/heap/remembered-set.h"
 #include "src/heap/slot-set.h"
 
 namespace v8 {
@@ -28,7 +29,7 @@ class StoreBuffer {
   static const int kStoreBuffers = 2;
   static const intptr_t kDeletionTag = 1;
 
-  static void StoreBufferOverflow(Isolate* isolate);
+  V8_EXPORT_PRIVATE static void StoreBufferOverflow(Isolate* isolate);
 
   explicit StoreBuffer(Heap* heap);
   void SetUp();
@@ -63,6 +64,23 @@ class StoreBuffer {
   // will be written into the second field. When processing the store buffer
   // the more efficient Remove method will be called in this case.
   void DeleteEntry(Address start, Address end = nullptr);
+
+  void InsertEntry(Address slot) {
+    // Insertions coming from the GC are directly inserted into the remembered
+    // set. Insertions coming from the runtime are added to the store buffer to
+    // allow concurrent processing.
+    if (heap_->gc_state() == Heap::NOT_IN_GC) {
+      if (top_ + sizeof(Address) > limit_[current_]) {
+        StoreBufferOverflow(heap_->isolate());
+      }
+      *top_ = slot;
+      top_++;
+    } else {
+      // In GC the store buffer has to be empty at any time.
+      DCHECK(Empty());
+      RememberedSet<OLD_TO_NEW>::Insert(Page::FromAddress(slot), slot);
+    }
+  }
 
   // Used by the concurrent processing thread to transfer entries from the
   // store buffer to the remembered set.
