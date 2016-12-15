cb433bed0b940a7073b5215e6684b940eec54edd
mtrofin <mtrofin@chromium.org>
2016-12-15 11:18:42 -0800

    [wasm] simpler detection if we compiled asm-wasm
    BUG=643595
    
    Review-Url: https://codereview.chromium.org/2582583002
    Cr-Commit-Position: refs/heads/master@{#41738}

--- src/compiler.cc
+++ src/compiler.cc
@@ -1366,22 +1366,12 @@ bool CodeGenerationFromStringsAllowed(Isolate* isolate,
   }
 }
 
-bool ContainsAsmModule(const Scope* scope, Zone* zone) {
-  DCHECK_NOT_NULL(scope);
-  DCHECK_NOT_NULL(zone);
-  ZoneQueue<const Scope*> worklist(zone);
-  // We assume scopes form a tree, so no need to check for cycles
-  worklist.push(scope);
-  while (!worklist.empty()) {
-    const Scope* s = worklist.front();
-    worklist.pop();
-    if (s->IsAsmModule()) {
-      return true;
-    }
-    for (const Scope* child = s->inner_scope(); child != nullptr;
-         child = child->sibling()) {
-      worklist.push(child);
-    }
+bool ContainsAsmModule(Handle<Script> script) {
+  WeakFixedArray::Iterator iter(script->shared_function_infos());
+  DisallowHeapAllocation no_gc;
+
+  while (SharedFunctionInfo* info = iter.Next<SharedFunctionInfo>()) {
+    if (info->HasAsmWasmData()) return true;
   }
   return false;
 }
@@ -1528,7 +1518,7 @@ Handle<SharedFunctionInfo> Compiler::GetSharedFunctionInfoForScript(
       compilation_cache->PutScript(source, context, language_mode, result);
       if (FLAG_serialize_toplevel &&
           compile_options == ScriptCompiler::kProduceCodeCache &&
-          !ContainsAsmModule(info.scope(), &zone)) {
+          !ContainsAsmModule(script)) {
         HistogramTimerScope histogram_timer(
             isolate->counters()->compile_serialize());
         RuntimeCallTimerScope runtimeTimer(isolate,
