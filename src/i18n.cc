d20c23cd13c1affa8e4975dfbf42904282769b31
littledan <littledan@chromium.org>
2016-12-27 07:31:07 -0800

    Revert of [intl] Remove redundant type checking system (patchset #3 id:40001 of https://codereview.chromium.org/2591203002/ )
    Reason for revert:
    Issue https://bugs.chromium.org/p/chromium/issues/detail?id=677055 . I'll send out a follow-on reland, as it should still be possible to eliminate the redundant type system.
    
    Original issue's description:
    > [intl] Remove redundant type checking system
    >
    > Previously, the Intl implementation tracked types two ways:
    >  - In the intl_initialized_marker_symbol
    >  - In various named properties of the intl_impl_object_symbol value
    >
    > As far as I can tell, these will never disagree with each other,
    > modulo bugs in Intl itself. This patch removes the second type
    > checking system.
    >
    > BUG=v8:5751
    >
    > Review-Url: https://codereview.chromium.org/2591203002
    > Cr-Commit-Position: refs/heads/master@{#41941}
    > Committed: https://chromium.googlesource.com/v8/v8/+/0d5561b64d34129e6546947255e543c219c61655
    
    TBR=yangguo@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=v8:5751
    
    Review-Url: https://codereview.chromium.org/2601783002
    Cr-Commit-Position: refs/heads/master@{#41958}

--- src/i18n.cc
+++ src/i18n.cc
@@ -759,7 +759,16 @@ icu::SimpleDateFormat* DateFormat::InitializeDateTimeFormat(
 icu::SimpleDateFormat* DateFormat::UnpackDateFormat(
     Isolate* isolate,
     Handle<JSObject> obj) {
-  return reinterpret_cast<icu::SimpleDateFormat*>(obj->GetInternalField(0));
+  Handle<String> key =
+      isolate->factory()->NewStringFromStaticChars("dateFormat");
+  Maybe<bool> maybe = JSReceiver::HasOwnProperty(obj, key);
+  CHECK(maybe.IsJust());
+  if (maybe.FromJust()) {
+    return reinterpret_cast<icu::SimpleDateFormat*>(
+        obj->GetInternalField(0));
+  }
+
+  return NULL;
 }
 
 void DateFormat::DeleteDateFormat(const v8::WeakCallbackInfo<void>& data) {
@@ -814,7 +823,15 @@ icu::DecimalFormat* NumberFormat::InitializeNumberFormat(
 icu::DecimalFormat* NumberFormat::UnpackNumberFormat(
     Isolate* isolate,
     Handle<JSObject> obj) {
-  return reinterpret_cast<icu::DecimalFormat*>(obj->GetInternalField(0));
+  Handle<String> key =
+      isolate->factory()->NewStringFromStaticChars("numberFormat");
+  Maybe<bool> maybe = JSReceiver::HasOwnProperty(obj, key);
+  CHECK(maybe.IsJust());
+  if (maybe.FromJust()) {
+    return reinterpret_cast<icu::DecimalFormat*>(obj->GetInternalField(0));
+  }
+
+  return NULL;
 }
 
 void NumberFormat::DeleteNumberFormat(const v8::WeakCallbackInfo<void>& data) {
@@ -866,7 +883,14 @@ icu::Collator* Collator::InitializeCollator(
 
 icu::Collator* Collator::UnpackCollator(Isolate* isolate,
                                         Handle<JSObject> obj) {
-  return reinterpret_cast<icu::Collator*>(obj->GetInternalField(0));
+  Handle<String> key = isolate->factory()->NewStringFromStaticChars("collator");
+  Maybe<bool> maybe = JSReceiver::HasOwnProperty(obj, key);
+  CHECK(maybe.IsJust());
+  if (maybe.FromJust()) {
+    return reinterpret_cast<icu::Collator*>(obj->GetInternalField(0));
+  }
+
+  return NULL;
 }
 
 void Collator::DeleteCollator(const v8::WeakCallbackInfo<void>& data) {
@@ -921,7 +945,15 @@ icu::BreakIterator* BreakIterator::InitializeBreakIterator(
 
 icu::BreakIterator* BreakIterator::UnpackBreakIterator(Isolate* isolate,
                                                        Handle<JSObject> obj) {
-  return reinterpret_cast<icu::BreakIterator*>(obj->GetInternalField(0));
+  Handle<String> key =
+      isolate->factory()->NewStringFromStaticChars("breakIterator");
+  Maybe<bool> maybe = JSReceiver::HasOwnProperty(obj, key);
+  CHECK(maybe.IsJust());
+  if (maybe.FromJust()) {
+    return reinterpret_cast<icu::BreakIterator*>(obj->GetInternalField(0));
+  }
+
+  return NULL;
 }
 
 void BreakIterator::DeleteBreakIterator(
