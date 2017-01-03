6ef22472c112f52d7c0263dfc29a781f6719ad05
zhengxing.li <zhengxing.li@intel.com>
2017-01-02 22:03:55 -0800

    X87: [builtins] FastNewFunctionContextStub becomes a builtin.
      port f2e8c9786fdcc5f1cbb11676e9f4bfccada9b2d3 (r41988)
    
      original commit message:
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2603313002
    Cr-Commit-Position: refs/heads/master@{#42019}

--- src/crankshaft/x87/lithium-codegen-x87.cc
+++ src/crankshaft/x87/lithium-codegen-x87.cc
@@ -7,6 +7,7 @@
 #include "src/crankshaft/x87/lithium-codegen-x87.h"
 
 #include "src/base/bits.h"
+#include "src/builtins/builtins-constructor.h"
 #include "src/code-factory.h"
 #include "src/code-stubs.h"
 #include "src/codegen.h"
@@ -146,16 +147,17 @@ void LCodeGen::DoPrologue(LPrologue* instr) {
       __ CallRuntime(Runtime::kNewScriptContext);
       deopt_mode = Safepoint::kLazyDeopt;
     } else {
-      if (slots <= FastNewFunctionContextStub::MaximumSlots()) {
-        FastNewFunctionContextStub stub(isolate(),
-                                        info()->scope()->scope_type());
+      if (slots <=
+          ConstructorBuiltinsAssembler::MaximumFunctionContextSlots()) {
+        Callable callable = CodeFactory::FastNewFunctionContext(
+            isolate(), info()->scope()->scope_type());
         __ mov(FastNewFunctionContextDescriptor::SlotsRegister(),
                Immediate(slots));
-        __ CallStub(&stub);
-        // Result of FastNewFunctionContextStub is always in new space.
+        __ Call(callable.code(), RelocInfo::CODE_TARGET);
+        // Result of the FastNewFunctionContext builtin is always in new space.
         need_write_barrier = false;
       } else {
-        __ push(edi);
+        __ Push(edi);
         __ Push(Smi::FromInt(info()->scope()->scope_type()));
         __ CallRuntime(Runtime::kNewFunctionContext);
       }
