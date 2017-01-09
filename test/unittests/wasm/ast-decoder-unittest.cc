6df28f15bc6f18d8a4a85e648e7639dd4043400d
bjaideep <bjaideep@ca.ibm.com>
2017-01-09 08:42:42 -0800

    Merged: s390: Fix FunctionBodyDecoderTest to compile with gcc4.8.5
    Revision: e7fc42796c823fdee6b4f812f798679df42fba0c
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=bradnelson@chromium.org, machenbach@chromium.org, mstarzinger@chromium.org, titzer@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2625433002
    Cr-Commit-Position: refs/branch-heads/5.5@{#76}
    Cr-Branched-From: 3cbd5838bd8376103daa45d69dade929ee4e0092-refs/heads/5.5.372@{#1}
    Cr-Branched-From: b3c8b0ce2c9af0528837d8309625118d4096553b-refs/heads/master@{#40015}

--- test/unittests/wasm/ast-decoder-unittest.cc
+++ test/unittests/wasm/ast-decoder-unittest.cc
@@ -1823,7 +1823,7 @@ TEST_F(AstDecoderTest, BreakNesting_6_levels) {
 
       int depth = 6;
       int m = mask;
-      for (size_t pos = 0; pos < sizeof(code); pos++) {
+      for (size_t pos = 0; pos < sizeof(code) - 1; pos++) {
         if (code[pos] != kExprBlock) continue;
         if (m & 1) {
           code[pos] = kExprLoop;
