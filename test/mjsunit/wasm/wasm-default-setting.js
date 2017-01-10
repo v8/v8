34b63f050b1a247bb64ddc91c967501ce04e011f
titzer <titzer@chromium.org>
2017-01-10 11:44:56 -0800

    [wasm] Enable WASM by default (--expose-wasm=true).
    BUG=chromium:575167
    R=rossberg@chromium.org,ahaas@chromium.org,clemensh@chromium.org,bradnelson@chromium.org
    
    Review-Url: https://codereview.chromium.org/2623743003
    Cr-Commit-Position: refs/heads/master@{#42197}

--- test/mjsunit/wasm/wasm-default-setting.js
+++ test/mjsunit/wasm/wasm-default-setting.js
@@ -2,5 +2,4 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(titzer): remove this test when WASM ships.
-assertThrows(function() { var g = Wasm; });
+assertEquals("function", typeof WebAssembly.Module);
