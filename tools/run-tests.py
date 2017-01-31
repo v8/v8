e196d00df587b6513eb79059cd96e259dd0a763f
machenbach <machenbach@chromium.org>
2017-01-31 07:06:59 -0800

    [test] Add back lsan leak detection
    BUG=chromium:662388
    TBR=yangguo@chromium.org, glider@chromium.org, titzer@chromium.org
    
    Review-Url: https://codereview.chromium.org/2592663004
    Cr-Commit-Position: refs/heads/master@{#42815}

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -403,7 +403,14 @@ def SetupEnvironment(options):
   )
 
   if options.asan:
-    os.environ['ASAN_OPTIONS'] = symbolizer
+    os.environ['ASAN_OPTIONS'] = ":".join([
+      'detect_leaks=1',
+      symbolizer,
+    ])
+    os.environ['LSAN_OPTIONS'] = ":".join([
+      'suppressions=%s' % os.path.join(
+          BASE_DIR, 'tools', 'memory', 'lsan', 'suppressions.txt'),
+    ])
 
   if options.sancov_dir:
     assert os.path.exists(options.sancov_dir)
