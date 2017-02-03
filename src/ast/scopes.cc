8ee5102ab25748ff7100377f99b3dcdfc53f052a
marja <marja@chromium.org>
2017-02-03 00:49:47 -0800

    Merging 2 commits:
    ed080e69665c96f9e5e61cf1d0e1ce377fc5aca6 e87e82b8e7802696670d63b2a9a5b41c07f3f8ed
    
    1) Disable lazy parsing inside eval (see bug).
    
    2) Force ctxt allocation in eval scopes.
    
    BUG=chromium:686918,v8:5736
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    
    Review-Url: https://codereview.chromium.org/2676653005
    Cr-Commit-Position: refs/branch-heads/5.6@{#106}
    Cr-Branched-From: bdd3886218dfe76e8560eb8a18401942452ae859-refs/heads/5.6.326@{#1}
    Cr-Branched-From: 879f6599eee6e1dfcbe9a24bf688b261c03e9558-refs/heads/master@{#41014}

--- src/ast/scopes.cc
+++ src/ast/scopes.cc
@@ -1157,9 +1157,9 @@ bool Scope::AllowsLazyParsingWithoutUnresolvedVariables(
   // guaranteed to be correct.
   for (const Scope* s = this; s != outer; s = s->outer_scope_) {
     // Eval forces context allocation on all outer scopes, so we don't need to
-    // look at those scopes. Sloppy eval makes all top-level variables dynamic,
-    // whereas strict-mode requires context allocation.
-    if (s->is_eval_scope()) return !is_strict(s->language_mode());
+    // look at those scopes. Sloppy eval makes top-level non-lexical variables
+    // dynamic, whereas strict-mode requires context allocation.
+    if (s->is_eval_scope()) return is_sloppy(s->language_mode());
     // Catch scopes force context allocation of all variables.
     if (s->is_catch_scope()) continue;
     // With scopes do not introduce variables that need allocation.
@@ -1823,7 +1823,10 @@ bool Scope::MustAllocateInContext(Variable* var) {
   if (has_forced_context_allocation()) return true;
   if (var->mode() == TEMPORARY) return false;
   if (is_catch_scope()) return true;
-  if (is_script_scope() && IsLexicalVariableMode(var->mode())) return true;
+  if ((is_script_scope() || is_eval_scope()) &&
+      IsLexicalVariableMode(var->mode())) {
+    return true;
+  }
   return var->has_forced_context_allocation() || inner_scope_calls_eval_;
 }
 
