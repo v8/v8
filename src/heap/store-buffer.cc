d1e5676026a47d7c38ae60e57402cbe1793391e8
hpayer <hpayer@chromium.org>
2017-02-06 04:05:52 -0800

    [heap] Add debug code for investigating store buffer crasher.
    BUG=chromium:673308
    
    Review-Url: https://codereview.chromium.org/2677163002
    Cr-Commit-Position: refs/heads/master@{#42962}

--- src/heap/store-buffer.cc
+++ src/heap/store-buffer.cc
@@ -101,7 +101,10 @@ void StoreBuffer::MoveEntriesToRememberedSet(int index) {
   DCHECK_LT(index, kStoreBuffers);
   for (Address* current = start_[index]; current < lazy_top_[index];
        current++) {
-    DCHECK(!heap_->code_space()->Contains(*current));
+    CHECK(!heap_->code_space()->Contains(*current));
+    CHECK(heap_->old_space()->Contains(*current) ||
+          heap_->map_space()->Contains(*current) ||
+          heap_->lo_space()->ContainsSlow(*current));
     Address addr = *current;
     Page* page = Page::FromAnyPointerAddress(heap_, addr);
     if (IsDeletionAddress(addr)) {
