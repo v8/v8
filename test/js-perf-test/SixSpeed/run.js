b69b24a2a247c8316bb214f6f3ef947f3ea302ac
petermarshall <petermarshall@chromium.org>
2017-02-06 02:19:24 -0800

    [Test] Update all SixSpeed tests to run in separate processes.
    BUG=v8:5922
    
    Review-Url: https://codereview.chromium.org/2674873002
    Cr-Commit-Position: refs/heads/master@{#42954}

--- test/js-perf-test/SixSpeed/run.js
+++ test/js-perf-test/SixSpeed/run.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-load('../../base.js');
+load('../base.js');
 load(arguments[0] + '.js');
 
 var success = true;
