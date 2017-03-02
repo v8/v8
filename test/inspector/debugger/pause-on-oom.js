2e00025038253b20b1d4bd89ae061897b5fb9932
ulan <ulan@chromium.org>
2017-03-02 09:49:15 -0800

    [inspector] Enable debugger/pause-on-oom test with larger heap limit.
    BUG=v8:6018
    
    Review-Url: https://codereview.chromium.org/2729843002
    Cr-Commit-Position: refs/heads/master@{#43550}

--- test/inspector/debugger/pause-on-oom.js
+++ test/inspector/debugger/pause-on-oom.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --max-old-space-size=4
+// Flags: --max-old-space-size=8
 
 InspectorTest.log('Check pause on OOM');
 
