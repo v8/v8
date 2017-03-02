78867ad8707a0163b27b925d9282f18fd4c026ae
Michael Lippautz <mlippautz@chromium.org>
2017-03-02 18:03:31 +0100

    Remove object grouping
    Enbedders should switch to EmbedderHeapTracer API.
    
    BUG=v8:5828
    
    Change-Id: I82f2bc583d246617865a17f5904e02cd35f92fec
    Reviewed-on: https://chromium-review.googlesource.com/448539
    Reviewed-by: Hannes Payer <hpayer@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#43551}

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -1450,10 +1450,6 @@ void Heap::CallGCPrologueCallbacks(GCType gc_type, GCCallbackFlags flags) {
       }
     }
   }
-  if (FLAG_trace_object_groups && (gc_type == kGCTypeIncrementalMarking ||
-                                   gc_type == kGCTypeMarkSweepCompact)) {
-    isolate_->global_handles()->PrintObjectGroups();
-  }
 }
 
 
