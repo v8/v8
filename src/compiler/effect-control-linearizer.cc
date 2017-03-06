c56c2013adaa50028fb20f7579addfe014826ae2
neis <neis@chromium.org>
2017-03-06 03:07:58 -0800

    [compiler] Tune lowering of CheckedUint32ToInt32.
    Instead of creating a signed comparison with INT_MAX, create an unsigned
    comparison with 0.  This saves a few bytes in the generated code.
    
    R=jarin@chromium.org
    BUG=
    
    Review-Url: https://codereview.chromium.org/2715513007
    Cr-Commit-Position: refs/heads/master@{#43606}

--- src/compiler/effect-control-linearizer.cc
+++ src/compiler/effect-control-linearizer.cc
@@ -1488,9 +1488,8 @@ Node* EffectControlLinearizer::LowerCheckedInt32ToTaggedSigned(
 Node* EffectControlLinearizer::LowerCheckedUint32ToInt32(Node* node,
                                                          Node* frame_state) {
   Node* value = node->InputAt(0);
-  Node* max_int = __ Int32Constant(std::numeric_limits<int32_t>::max());
-  Node* is_safe = __ Uint32LessThanOrEqual(value, max_int);
-  __ DeoptimizeUnless(DeoptimizeReason::kLostPrecision, is_safe, frame_state);
+  Node* unsafe = __ Int32LessThan(value, __ Int32Constant(0));
+  __ DeoptimizeIf(DeoptimizeReason::kLostPrecision, unsafe, frame_state);
   return value;
 }
 
