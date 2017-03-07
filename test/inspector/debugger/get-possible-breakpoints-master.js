15bed19495e240db5f72a6ec6845ad5692950e80
mvstanton <mvstanton@chromium.org>
2017-03-07 05:27:55 -0800

    Adjust some tests to prepare enabling Ignition+TurboFan
    BUG=v8:5267, chromium:692409
    
    Review-Url: https://codereview.chromium.org/2720713003
    Cr-Commit-Position: refs/heads/master@{#43638}

--- test/inspector/debugger/get-possible-breakpoints-master.js
+++ test/inspector/debugger/get-possible-breakpoints-master.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Flags: --turbo
+
 InspectorTest.log('Checks Debugger.getPossibleBreakpoints');
 
 var source = utils.read('test/inspector/debugger/resources/break-locations.js');
