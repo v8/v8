55e3fb94ac9d1487100a958ca0ad467d61d47440
jyan <jyan@ca.ibm.com>
2017-03-11 20:40:22 -0800

    s390: cleanup old instruction decode routines
    R=joransiu@ca.ibm.com, bjaideep@ca.ibm.com
    BUG=
    
    Review-Url: https://codereview.chromium.org/2741053004
    Cr-Commit-Position: refs/heads/master@{#43732}

--- src/s390/simulator-s390.h
+++ src/s390/simulator-s390.h
@@ -309,18 +309,6 @@ class Simulator {
 
   // S390
   void Trace(Instruction* instr);
-  bool DecodeTwoByte(Instruction* instr);
-  bool DecodeFourByte(Instruction* instr);
-  bool DecodeFourByteArithmetic(Instruction* instr);
-  bool DecodeFourByteArithmetic64Bit(Instruction* instr);
-  bool DecodeFourByteFloatingPoint(Instruction* instr);
-  void DecodeFourByteFloatingPointIntConversion(Instruction* instr);
-  void DecodeFourByteFloatingPointRound(Instruction* instr);
-
-  bool DecodeSixByte(Instruction* instr);
-  bool DecodeSixByteArithmetic(Instruction* instr);
-  bool S390InstructionDecode(Instruction* instr);
-  void DecodeSixByteBitShift(Instruction* instr);
 
   // Used by the CL**BR instructions.
   template <typename T1, typename T2>
