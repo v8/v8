c1ad47893f4deca29a39b51ca77127041036cd7c
Ross McIlroy <rmcilroy@chromium.org>
2017-03-27 16:20:10 +0100

    Resize identity map by doubling instead of quadrupling.
    Perf-sheriffs please revert if this causes any performance regressions.
    
    BUG=
    
    Change-Id: I39075482f3c85d69407d6d8e5643d94c1a4425c2
    Reviewed-on: https://chromium-review.googlesource.com/461117
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#51470}

--- src/identity-map.cc
+++ src/identity-map.cc
@@ -11,7 +11,7 @@ namespace v8 {
 namespace internal {
 
 static const int kInitialIdentityMapSize = 4;
-static const int kResizeFactor = 4;
+static const int kResizeFactor = 2;
 
 IdentityMapBase::~IdentityMapBase() {
   // Clear must be called by the subclass to avoid calling the virtual
@@ -87,7 +87,8 @@ void* IdentityMapBase::DeleteIndex(int index) {
   size_--;
   DCHECK_GE(size_, 0);
 
-  if (size_ * kResizeFactor < capacity_ / kResizeFactor) {
+  if (capacity_ > kInitialIdentityMapSize &&
+      size_ * kResizeFactor < capacity_ / kResizeFactor) {
     Resize(capacity_ / kResizeFactor);
     return ret_value;  // No need to fix collisions as resize reinserts keys.
   }
