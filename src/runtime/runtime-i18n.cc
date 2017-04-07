a4f5c8ace54f275746ebebae30b31aa9e8b5e833
Yang Guo <yangguo@chromium.org>
2017-04-07 13:21:55 +0200

    Merged: Prepare for ICU's switch to char16_t
    Revision: fd5b3e755df541b44128caed625215017ef59989
    
    TBR=hablich@chromium.org
    BUG=v8:6062
    LOG=N
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    
    Change-Id: I27d0fbc8e6a8e3e394fc9154a60693746c170ab3
    Reviewed-on: https://chromium-review.googlesource.com/471349
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/branch-heads/5.7@{#160}
    Cr-Branched-From: 975e9a320b6eaf9f12280c35df98e013beb8f041-refs/heads/5.7.492@{#1}
    Cr-Branched-From: 8d76f0e3465a84bbf0bceab114900fbe75844e1f-refs/heads/master@{#42426}

--- src/runtime/runtime-i18n.cc
+++ src/runtime/runtime-i18n.cc
@@ -43,6 +43,7 @@
 #include "unicode/uloc.h"
 #include "unicode/unistr.h"
 #include "unicode/unum.h"
+#include "unicode/ustring.h"
 #include "unicode/uversion.h"
 
 
@@ -609,10 +610,11 @@ RUNTIME_FUNCTION(Runtime_InternalCompare) {
     String::FlatContent flat2 = string2->GetFlatContent();
     std::unique_ptr<uc16[]> sap1;
     std::unique_ptr<uc16[]> sap2;
-    const UChar* string_val1 = GetUCharBufferFromFlat(flat1, &sap1, length1);
-    const UChar* string_val2 = GetUCharBufferFromFlat(flat2, &sap2, length2);
-    result =
-        collator->compare(string_val1, length1, string_val2, length2, status);
+    icu::UnicodeString string_val1(
+        FALSE, GetUCharBufferFromFlat(flat1, &sap1, length1), length1);
+    icu::UnicodeString string_val2(
+        FALSE, GetUCharBufferFromFlat(flat2, &sap2, length2), length2);
+    result = collator->compare(string_val1, string_val2, status);
   }
   if (U_FAILURE(status)) return isolate->ThrowIllegalOperation();
 
