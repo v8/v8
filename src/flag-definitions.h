b354204253b134682f4b810780abbc1dee7aba37
Sathya Gunasekaran <gsathya@chromium.org>
2017-04-24 14:43:52 -0700

    [ESNext] ⛴ object rest/spread properties
    Bug: v8:5549
    Change-Id: I32b9853b6542c6ad04d12a9d54f31be9e3c76da8
    Reviewed-on: https://chromium-review.googlesource.com/486245
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Commit-Queue: Sathya Gunasekaran <gsathya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#44822}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -216,16 +216,16 @@ DEFINE_IMPLICATION(es_staging, harmony)
   V(harmony_regexp_lookbehind, "harmony regexp lookbehind")              \
   V(harmony_regexp_named_captures, "harmony regexp named captures")      \
   V(harmony_regexp_property, "harmony unicode regexp property classes")  \
-  V(harmony_object_rest_spread, "harmony object rest spread properties") \
   V(harmony_template_escapes,                                            \
     "harmony invalid escapes in tagged template literals")
 
 // Features that are shipping (turned on by default, but internal flag remains).
-#define HARMONY_SHIPPING_BASE(V)                      \
-  V(harmony_restrictive_generators,                   \
-    "harmony restrictions on generator declarations") \
-  V(harmony_trailing_commas,                          \
-    "harmony trailing commas in function parameter lists")
+#define HARMONY_SHIPPING_BASE(V)                           \
+  V(harmony_restrictive_generators,                        \
+    "harmony restrictions on generator declarations")      \
+  V(harmony_trailing_commas,                               \
+    "harmony trailing commas in function parameter lists") \
+  V(harmony_object_rest_spread, "harmony object rest spread properties")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_SHIPPING(V)                                        \
