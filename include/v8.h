a16c3c910548e37eb62bb467fea706a21f6a2d46
jbroman <jbroman@chromium.org>
2017-04-27 08:14:41 -0700

    Expose the ValueSerializer data format version as a compile-time constant.
    BUG=chromium:704293
    
    Review-Url: https://codereview.chromium.org/2804643006
    Cr-Commit-Position: refs/heads/master@{#44945}

--- include/v8.h
+++ include/v8.h
@@ -1793,8 +1793,6 @@ class V8_EXPORT ValueSerializer {
     virtual void FreeBufferMemory(void* buffer);
   };
 
-  static uint32_t GetCurrentDataFormatVersion();
-
   explicit ValueSerializer(Isolate* isolate);
   ValueSerializer(Isolate* isolate, Delegate* delegate);
   ~ValueSerializer();
