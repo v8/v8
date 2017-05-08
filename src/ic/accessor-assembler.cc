d2ba280d47bf6250aca27b1997c28dce3d0d2963
jkummerow <jkummerow@chromium.org>
2017-05-08 02:27:29 -0700

    [ic] Restore Function.prototype fast path for LoadIC_Uninitialized
    It was replaced by more generic handling  in 1320666798d, which
    is functionally fine, but for performance it makes sense to keep
    the fast path.
    
    Review-Url: https://codereview.chromium.org/2864463004
    Cr-Commit-Position: refs/heads/master@{#45155}

--- src/ic/accessor-assembler.cc
+++ src/ic/accessor-assembler.cc
@@ -1877,6 +1877,20 @@ void AccessorAssembler::LoadIC_Uninitialized(const LoadICParameters* p) {
                          LoadRoot(Heap::kpremonomorphic_symbolRootIndex),
                          SKIP_WRITE_BARRIER, 0, SMI_PARAMETERS);
 
+  {
+    // Special case for Function.prototype load, because it's very common
+    // for ICs that are only executed once (MyFunc.prototype.foo = ...).
+    Label not_function_prototype(this);
+    GotoIf(Word32NotEqual(instance_type, Int32Constant(JS_FUNCTION_TYPE)),
+           &not_function_prototype);
+    GotoIfNot(IsPrototypeString(p->name), &not_function_prototype);
+    Node* bit_field = LoadMapBitField(receiver_map);
+    GotoIf(IsSetWord32(bit_field, 1 << Map::kHasNonInstancePrototype),
+           &not_function_prototype);
+    Return(LoadJSFunctionPrototype(receiver, &miss));
+    BIND(&not_function_prototype);
+  }
+
   GenericPropertyLoad(receiver, receiver_map, instance_type, p->name, p, &miss,
                       kDontUseStubCache);
 
