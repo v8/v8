f70ceeae1d13a151a64cbdb39505ea46c1980db8
Tobias Tebbi <tebbi@chromium.org>
2017-05-30 11:38:07 +0200

    [turbofan] teach escape analysis about oddly occurring simplified number ops
    Bug: chromium:726638
    Change-Id: Ib30b147ec60f9f13c5164765f8c63be7a1339e9f
    Reviewed-on: https://chromium-review.googlesource.com/517497
    Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    Commit-Queue: Tobias Tebbi <tebbi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#45634}

--- src/compiler/escape-analysis.cc
+++ src/compiler/escape-analysis.cc
@@ -846,6 +846,10 @@ bool EscapeStatusAnalysis::CheckUsesForEscape(Node* uses, Node* rep,
       case IrOpcode::kObjectIsString:
       case IrOpcode::kObjectIsSymbol:
       case IrOpcode::kObjectIsUndetectable:
+#define CASE(opcode) case IrOpcode::k##opcode:
+        SIMPLIFIED_NUMBER_BINOP_LIST(CASE)
+        SIMPLIFIED_NUMBER_UNOP_LIST(CASE)
+#undef CASE
         if (SetEscaped(rep)) {
           TRACE("Setting #%d (%s) to escaped because of use by #%d (%s)\n",
                 rep->id(), rep->op()->mnemonic(), use->id(),
