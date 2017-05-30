697f1bdae3cdb4f9351d87a420e53d4c3f337df1
Jochen Eisinger <jochen@chromium.org>
2017-05-30 11:22:29 +0200

    Remove unused presubmit hooks
    We add the bot via the post-upload step, that's good enough
    
    R=machenbach@chromium.org,tandrii@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_rel_ng
    Change-Id: I8d582d101b4d2f308829da75df56e04a3dabadbf
    Reviewed-on: https://chromium-review.googlesource.com/518114
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#45594}

--- include/PRESUBMIT.py
+++ include/PRESUBMIT.py
@@ -11,23 +11,6 @@ for more details about the presubmit API built into depot_tools.
 import re
 
 
-def _GetTryMasters(project, change):
-  return {
-    'master.tryserver.chromium.linux': {
-      'linux_chromium_rel_ng': [],
-     },
-  }
-
-
-def GetPreferredTryMasters(project, change):
-  # TODO(jochen): Using the value of _GetTryMasters() instead of an empty
-  # value here would cause 'git cl try' to include the site isolation trybots,
-  # which would be nice. But it has the side effect of replacing, rather than
-  # augmenting, the default set of try servers. Re-enable this when we figure
-  # out a way to augment the default set.
-  return {}
-
-
 def PostUploadHook(cl, change, output_api):
   """git cl upload will call this hook after the issue is created/modified.
 
