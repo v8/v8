0732cbee894c0e518db9c2f337be3794aa46a681
bjaideep <bjaideep@ca.ibm.com>
2017-06-08 14:34:22 -0700

    PPC/s390: Inline Array.prototype.forEach in TurboFan
    Port 90c3a2d54b956d06ace2c97c6bfdb10ea9cb8e58
    
    Original Commit Message:
    
        This CL contains a few pieces:
    
        - A new mechanism to create "BuiltinContinuation" checkpoints in TurboFan
          graphs, which--when triggered--swizzle the values in the the FrameState to be
          parameters to a typically TF-generated builtin that resumes execution to finish
          the slow-case functionality.
        - Continuation builtins that have special handling in the deoptimizer and their own
          new frame type to ensure that the values they need to begin executing can be stashed
          away and restored immediately before the builtin is called via a trampoline that runs
          when the continuation builtin's frame execution resumes.
        - An implementation of Array.prototype.forEach in TurboFan that can be used to
          inline it. The inlined forEach implementation uses the checkpoints mechanism
          described above to deopt in the middle of the forEach in the cases that optimization
          invariants are violated. There is a slightly different continuation stub for each
          deopt point in the forEach implementation to ensure the correct side-effects, i.e.
          that the deopt of the builtin isn't programmatically observable.
    
    R=danno@chromium.org, joransiu@ca.ibm.com, jyan@ca.ibm.com, michael_dawson@ca.ibm.com
    BUG=
    LOG=N
    
    Review-Url: https://codereview.chromium.org/2926043005
    Cr-Commit-Position: refs/heads/master@{#45798}

--- src/builtins/s390/builtins-s390.cc
+++ src/builtins/s390/builtins-s390.cc
@@ -1703,6 +1703,72 @@ void Builtins::Generate_NotifyStubFailureSaveDoubles(MacroAssembler* masm) {
   Generate_NotifyStubFailureHelper(masm, kSaveFPRegs);
 }
 
+void Builtins::Generate_NotifyBuiltinContinuation(MacroAssembler* masm) {
+  {
+    FrameAndConstantPoolScope scope(masm, StackFrame::INTERNAL);
+    // Preserve possible return result from lazy deopt.
+    __ push(r2);
+    // Pass the function and deoptimization type to the runtime system.
+    __ CallRuntime(Runtime::kNotifyStubFailure, false);
+    __ pop(r2);
+  }
+
+  __ AddP(sp, sp, Operand(kPointerSize));  // Ignore state
+  __ Ret();                                // Jump to ContinueToBuiltin stub
+}
+
+namespace {
+void Generate_ContinueToBuiltinHelper(MacroAssembler* masm,
+                                      bool java_script_builtin,
+                                      bool with_result) {
+  const RegisterConfiguration* config(RegisterConfiguration::Turbofan());
+  int allocatable_register_count = config->num_allocatable_general_registers();
+  if (with_result) {
+    // Overwrite the hole inserted by the deoptimizer with the return value from
+    // the LAZY deopt point.
+    __ StoreP(
+        r2, MemOperand(
+                sp, config->num_allocatable_general_registers() * kPointerSize +
+                        BuiltinContinuationFrameConstants::kFixedFrameSize));
+  }
+  for (int i = allocatable_register_count - 1; i >= 0; --i) {
+    int code = config->GetAllocatableGeneralCode(i);
+    __ Pop(Register::from_code(code));
+    if (java_script_builtin && code == kJavaScriptCallArgCountRegister.code()) {
+      __ SmiUntag(Register::from_code(code));
+    }
+  }
+  __ LoadP(
+      fp,
+      MemOperand(sp, BuiltinContinuationFrameConstants::kFixedFrameSizeFromFp));
+  __ Pop(ip);
+  __ AddP(sp, sp,
+          Operand(BuiltinContinuationFrameConstants::kFixedFrameSizeFromFp));
+  __ Pop(r0);
+  __ LoadRR(r14, r0);
+  __ AddP(ip, ip, Operand(Code::kHeaderSize - kHeapObjectTag));
+  __ Jump(ip);
+}
+}  // namespace
+
+void Builtins::Generate_ContinueToCodeStubBuiltin(MacroAssembler* masm) {
+  Generate_ContinueToBuiltinHelper(masm, false, false);
+}
+
+void Builtins::Generate_ContinueToCodeStubBuiltinWithResult(
+    MacroAssembler* masm) {
+  Generate_ContinueToBuiltinHelper(masm, false, true);
+}
+
+void Builtins::Generate_ContinueToJavaScriptBuiltin(MacroAssembler* masm) {
+  Generate_ContinueToBuiltinHelper(masm, true, false);
+}
+
+void Builtins::Generate_ContinueToJavaScriptBuiltinWithResult(
+    MacroAssembler* masm) {
+  Generate_ContinueToBuiltinHelper(masm, true, true);
+}
+
 static void Generate_NotifyDeoptimizedHelper(MacroAssembler* masm,
                                              Deoptimizer::BailoutType type) {
   {
