94f7b49b8f5213dc3e8209dc905ceda0a690898e
Michael Achenbach <machenbach@chromium.org>
2017-06-27 15:14:09 +0200

    Merged: Squashed multiple commits.
    Merged: Reland "[build] Add filter script for official build"
    Revision: e3e0b6f46b2139333a0667eaeb63878285dc6e94
    
    Merged: [build] Fix filter script for official build
    Revision: c535258aab09f9809bf6e8a42d3913147e9a92fa
    
    BUG=v8:5918
    LOG=N
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    TBR=mathias@chromium.org
    
    Change-Id: I29139ffc422e65cb05359aab10adfb927ead52ae
    Reviewed-on: https://chromium-review.googlesource.com/549305
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6.0@{#49}
    Cr-Branched-From: 97dbf624a5eeffb3a8df36d24cdb2a883137385f-refs/heads/6.0.286@{#1}
    Cr-Branched-From: 12e6f1cb5cd9616da7b9d4a7655c088778a6d415-refs/heads/master@{#45439}

---
+++ tools/release/filter_build_files.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# Copyright 2017 the V8 project authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+Enumerates relevant build files for each platform.
+
+This can be used to filter the build directory before making an official
+archive. The archive should only contain files required for running or
+static linking, e.g. executables, startup files, libraries.
+
+The script is limited to release builds and assumes GN.
+"""
+
+import argparse
+import glob
+import itertools
+import json
+import os
+import re
+import sys
+
+EXECUTABLE_FILES = [
+  'd8',
+  'v8_hello_world',
+  'v8_parser_shell',
+  'v8_sample_process',
+]
+
+SUPPLEMENTARY_FILES = [
+  'icudtl.dat',
+  'natives_blob.bin',
+  'snapshot_blob.bin',
+  'v8_build_config.json',
+]
+
+LIBRARY_FILES = {
+  'linux': ['*.a', '*.so'],
+  'mac': ['*.a', '*.so'],
+  'win': ['*.lib', '*.dll'],
+}
+
+
+def main(argv):
+  parser = argparse.ArgumentParser(description=__doc__)
+
+  parser.add_argument('-d', '--dir', required=True,
+                      help='Path to the build directory.')
+  parser.add_argument('-p', '--platform', required=True,
+                      help='Target platform name: win|mac|linux.')
+  parser.add_argument('-o', '--json-output', required=True,
+                      help='Path to an output file. The files will '
+                           'be stored in json list with absolute paths.')
+  args = parser.parse_args()
+
+  if not os.path.isdir(args.dir):
+    parser.error('%s is not an existing directory.' % args.dir)
+
+  args.dir = os.path.abspath(args.dir)
+
+  list_of_files = []
+  def add_files_from_globs(globs):
+    list_of_files.extend(itertools.chain(*map(glob.iglob, globs)))
+
+  # Add toplevel executables, supplementary files and libraries.
+  extended_executable_files = [
+    f + '.exe' if args.platform == 'win' else f
+    for f in EXECUTABLE_FILES]
+  add_files_from_globs(
+      os.path.join(args.dir, f)
+      for f in extended_executable_files +
+               SUPPLEMENTARY_FILES +
+               LIBRARY_FILES[args.platform]
+  )
+
+  # Add libraries recursively from obj directory.
+  for root, _, __ in os.walk(os.path.join(args.dir, 'obj'), followlinks=True):
+    add_files_from_globs(
+        os.path.join(root, g) for g in LIBRARY_FILES[args.platform])
+
+  with open(args.json_output, 'w') as f:
+    json.dump(list_of_files, f)
+
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
