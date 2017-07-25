57031e82db6864b0c0caa67502b222a255ef9a96
Michael Starzinger <mstarzinger@chromium.org>
2017-07-25 10:21:59 +0200

    [asm.js] Enable --validate-asm by default.
    R=rmcilroy@chromium.org
    BUG=v8:4203
    
    Change-Id: I6741eaff237daa32fd4e88a125ebd51fe80d2adb
    Reviewed-on: https://chromium-review.googlesource.com/583655
    Commit-Queue: Michael Starzinger <mstarzinger@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#46879}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -509,7 +509,7 @@ DEFINE_UINT(skip_compiling_wasm_funcs, 0, "start compiling at function N")
 DEFINE_BOOL(wasm_break_on_decoder_error, false,
             "debug break when wasm decoder encounters an error")
 
-DEFINE_BOOL(validate_asm, false, "validate asm.js modules before compiling")
+DEFINE_BOOL(validate_asm, true, "validate asm.js modules before compiling")
 DEFINE_BOOL(suppress_asm_messages, false,
             "don't emit asm.js related messages (for golden file testing)")
 DEFINE_BOOL(trace_asm_time, false, "log asm.js timing info to the console")
