4f2e6e9ddd3a92d901caa481b9a4bcf460490413
Michael Hablich <hablich@chromium.org>
2017-07-25 11:07:32 +0000

    Revert "[inspector] support for cases when embedder doesn't call contextDestroyed"
    This reverts commit 87aae715dfe30b211bb62494debe724f4d999f53.
    
    Reason for revert: Speculative revert because of ASAN leaks in https://chromium-review.googlesource.com/584488
    
    Original change's description:
    > [inspector] support for cases when embedder doesn't call contextDestroyed
    > 
    > Node.js doesn't have good place to call contextDestroyed.
    > We need to cleanup everything on our side to allow clients to not call
    > contextDestroyed method.
    > 
    > R=​dgozman@chromium.org,eostroukhov@google.com
    > 
    > Bug: none
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_chromium_rel_ng
    > Change-Id: I6bfd4d6039f53eb994a2d20ecbca650744564e29
    > Reviewed-on: https://chromium-review.googlesource.com/575519
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#46849}
    
    TBR=dgozman@chromium.org,kozyatinskiy@chromium.org,eostroukhov@google.com,eostroukhov@chromium.org
    
    Change-Id: Ic3c2764159f809536670f73ab0cd5ea37317d400
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: none
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_chromium_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/584767
    Reviewed-by: Michael Hablich <hablich@chromium.org>
    Commit-Queue: Michael Hablich <hablich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#46865}

--- src/inspector/inspected-context.cc
+++ src/inspector/inspected-context.cc
@@ -15,38 +15,6 @@
 
 namespace v8_inspector {
 
-class InspectedContext::WeakCallbackData {
- public:
-  WeakCallbackData(InspectedContext* context, V8InspectorImpl* inspector,
-                   int groupId, int contextId)
-      : m_context(context),
-        m_inspector(inspector),
-        m_groupId(groupId),
-        m_contextId(contextId) {}
-
-  static void resetContext(const v8::WeakCallbackInfo<WeakCallbackData>& data) {
-    // InspectedContext is alive here because weak handler is still alive.
-    data.GetParameter()->m_context->m_context.Reset();
-    data.SetSecondPassCallback(&callContextCollected);
-  }
-
-  static void callContextCollected(
-      const v8::WeakCallbackInfo<WeakCallbackData>& data) {
-    // InspectedContext can be dead here since anything can happen between first
-    // and second pass callback.
-    WeakCallbackData* callbackData = data.GetParameter();
-    callbackData->m_inspector->contextCollected(callbackData->m_groupId,
-                                                callbackData->m_contextId);
-    delete callbackData;
-  }
-
- private:
-  InspectedContext* m_context;
-  V8InspectorImpl* m_inspector;
-  int m_groupId;
-  int m_contextId;
-};
-
 InspectedContext::InspectedContext(V8InspectorImpl* inspector,
                                    const V8ContextInfo& info, int contextId)
     : m_inspector(inspector),
@@ -67,10 +35,6 @@ InspectedContext::InspectedContext(V8InspectorImpl* inspector,
     m_inspector->console()->installMemoryGetter(
         info.context, v8::Local<v8::Object>::Cast(console));
   }
-  m_context.SetWeak(
-      new WeakCallbackData(this, m_inspector, m_contextGroupId, m_contextId),
-      &InspectedContext::WeakCallbackData::resetContext,
-      v8::WeakCallbackType::kParameter);
 }
 
 InspectedContext::~InspectedContext() {
