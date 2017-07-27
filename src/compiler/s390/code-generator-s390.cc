a2388599dbd9e93e32a6394f60df40105f6031d3
Jaideep Bajwa <bjaideep@ca.ibm.com>
2017-07-27 01:40:26 -0400

    PPC/s390: Reland "[arm] Restrict grouping pushes before a TailCall to registers only"
    Port 79bcb45447237158a61cbbdb122cdbc096f00e14
    
    Original Commit Message:
    
        This is a reland of a72b2f88a8cb44f12002f1168c639e07db9d5600
        Original change's description:
        > [arm] Restrict grouping pushes before a TailCall to registers only
        >
        > We optimize parallel moves performed before a TailCall by grouping adjacent
        > pushes. This way, we may use a single instruction to push multiple registers at
        > once. However, we also have support for pushing immediates and stack slots for
        > which the benefit is questionnable therefore this patch removes support for
        > them.
        >
        > Concerning immediate pushes, it looks like a mistake since we do not have
        > support for this case in `AssembleMove` so this patch removes it. Furthermore,
        > if we add a test for this case, we see that a `push ip` instruction is
        > generated, effectively pushing whatever was in `ip` at the time instead of
        > pushing a constant.
        >
        > Concerning stack slot pushes, we generate a more or less equivalent sequence of
        > instructions.
        >
        > Finally, grouping floating point pushes is not used anywhere so this patch
        > removes support for this also.
        >
        > Bug: v8:6553
        > Change-Id: I9b820d33361fc442dd813f66e1f96cda41009110
        > Reviewed-on: https://chromium-review.googlesource.com/567191
        > Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
        > Commit-Queue: Pierre Langlois <pierre.langlois@arm.com>
        > Cr-Commit-Position: refs/heads/master@{#46718}
    
    R=pierre.langlois@arm.com, joransiu@ca.ibm.com, jyan@ca.ibm.com, michael_dawson@ca.ibm.com
    BUG=
    LOG=N
    
    Change-Id: I8790c7a72f92803ea8fda3c6dc7e6b013e2e09e9
    Reviewed-on: https://chromium-review.googlesource.com/588471
    Reviewed-by: Junliang Yan <jyan@ca.ibm.com>
    Commit-Queue: Jaideep Bajwa <bjaideep@ca.ibm.com>
    Cr-Commit-Position: refs/heads/master@{#46949}

--- src/compiler/s390/code-generator-s390.cc
+++ src/compiler/s390/code-generator-s390.cc
@@ -1074,15 +1074,6 @@ void FlushPendingPushRegisters(TurboAssembler* tasm,
   pending_pushes->resize(0);
 }
 
-void AddPendingPushRegister(TurboAssembler* tasm,
-                            FrameAccessState* frame_access_state,
-                            ZoneVector<Register>* pending_pushes,
-                            Register reg) {
-  pending_pushes->push_back(reg);
-  if (pending_pushes->size() == 3 || reg.is(ip)) {
-    FlushPendingPushRegisters(tasm, frame_access_state, pending_pushes);
-  }
-}
 void AdjustStackPointerForTailCall(
     TurboAssembler* tasm, FrameAccessState* state, int new_slot_above_sp,
     ZoneVector<Register>* pending_pushes = nullptr,
@@ -1126,21 +1117,15 @@ void CodeGenerator::AssembleTailCallBeforeGap(Instruction* instr,
           tasm(), frame_access_state(),
           destination_location.index() - pending_pushes.size(),
           &pending_pushes);
-      if (source.IsStackSlot()) {
-        LocationOperand source_location(LocationOperand::cast(source));
-        __ LoadP(ip, g.SlotToMemOperand(source_location.index()));
-        AddPendingPushRegister(tasm(), frame_access_state(), &pending_pushes,
-                               ip);
-      } else if (source.IsRegister()) {
-        LocationOperand source_location(LocationOperand::cast(source));
-        AddPendingPushRegister(tasm(), frame_access_state(), &pending_pushes,
-                               source_location.GetRegister());
-      } else if (source.IsImmediate()) {
-        AddPendingPushRegister(tasm(), frame_access_state(), &pending_pushes,
-                               ip);
-      } else {
-        // Pushes of non-scalar data types is not supported.
-        UNIMPLEMENTED();
+      // Pushes of non-register data types are not supported.
+      DCHECK(source.IsRegister());
+      LocationOperand source_location(LocationOperand::cast(source));
+      pending_pushes.push_back(source_location.GetRegister());
+      // TODO(arm): We can push more than 3 registers at once. Add support in
+      // the macro-assembler for pushing a list of registers.
+      if (pending_pushes.size() == 3) {
+        FlushPendingPushRegisters(tasm(), frame_access_state(),
+                                  &pending_pushes);
       }
       move->Eliminate();
     }
