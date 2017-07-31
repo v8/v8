bbf41ece5a66b4f309fbde3128d06e4b8049496b
Jaideep Bajwa <bjaideep@ca.ibm.com>
2017-07-31 11:48:32 -0400

    Merged: Fix compilation failure due to overload of virtual function
    Revision: 709c906a9200b0a1401b58261f311de95707adc0
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=ivica.bogosavljevic@imgtec.com, machenbach@chromium.org, mlippautz@chromium.org
    
    Bug: 
    Change-Id: I0c2a7e7624993ef6257c5dfe4f58f7311f87e22d
    Reviewed-on: https://chromium-review.googlesource.com/594012
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Jaideep Bajwa <bjaideep@ca.ibm.com>
    Cr-Commit-Position: refs/branch-heads/6.0@{#108}
    Cr-Branched-From: 97dbf624a5eeffb3a8df36d24cdb2a883137385f-refs/heads/6.0.286@{#1}
    Cr-Branched-From: 12e6f1cb5cd9616da7b9d4a7655c088778a6d415-refs/heads/master@{#45439}

--- src/d8.cc
+++ src/d8.cc
@@ -135,6 +135,7 @@ class ShellArrayBufferAllocator : public ArrayBufferAllocatorBase {
     return malloc(length);
 #endif
   }
+  using ArrayBufferAllocatorBase::Free;
   void Free(void* data, size_t length) override {
 #if USE_VM
     if (RoundToPageSize(&length)) {
