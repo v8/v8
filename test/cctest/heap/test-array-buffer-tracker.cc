3fe14218583f58ac989a97b8aec02f82fd36e3f2
Ulan Degenbaev <ulan@chromium.org>
2017-08-01 21:46:16 +0200

    [heap] Add manual GC scope to test-array-buffer-tracker tests.
    These tests perform GC manually which does not work well with concurrent
    marking and stress incremental marking flags.
    
    BUG=chromium:694255
    
    Change-Id: I43e32957bf37053e0d3af07afa00b8bb40935ebd
    Reviewed-on: https://chromium-review.googlesource.com/596887
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#47070}

--- test/cctest/heap/test-array-buffer-tracker.cc
+++ test/cctest/heap/test-array-buffer-tracker.cc
@@ -27,6 +27,7 @@ namespace internal {
 // moving the objects through various spaces during GC phases.
 
 TEST(ArrayBuffer_OnlyMC) {
+  ManualGCScope manual_gc_scope;
   CcTest::InitializeVM();
   LocalContext env;
   v8::Isolate* isolate = env->GetIsolate();
@@ -54,6 +55,7 @@ TEST(ArrayBuffer_OnlyMC) {
 }
 
 TEST(ArrayBuffer_OnlyScavenge) {
+  ManualGCScope manual_gc_scope;
   CcTest::InitializeVM();
   LocalContext env;
   v8::Isolate* isolate = env->GetIsolate();
@@ -83,6 +85,7 @@ TEST(ArrayBuffer_OnlyScavenge) {
 }
 
 TEST(ArrayBuffer_ScavengeAndMC) {
+  ManualGCScope manual_gc_scope;
   CcTest::InitializeVM();
   LocalContext env;
   v8::Isolate* isolate = env->GetIsolate();
@@ -115,8 +118,7 @@ TEST(ArrayBuffer_ScavengeAndMC) {
 
 TEST(ArrayBuffer_Compaction) {
   if (FLAG_never_compact) return;
-  FLAG_concurrent_marking = false;
-  FLAG_stress_incremental_marking = false;
+  ManualGCScope manual_gc_scope;
   FLAG_manual_evacuation_candidates_selection = true;
   CcTest::InitializeVM();
   LocalContext env;
@@ -156,6 +158,7 @@ TEST(ArrayBuffer_UnregisterDuringSweep) {
 #ifdef VERIFY_HEAP
   i::FLAG_verify_heap = false;
 #endif  // VERIFY_HEAP
+  ManualGCScope manual_gc_scope;
 
   CcTest::InitializeVM();
   LocalContext env;
@@ -267,6 +270,7 @@ TEST(ArrayBuffer_LivePromotion) {
 
 TEST(ArrayBuffer_SemiSpaceCopyThenPagePromotion) {
   if (!i::FLAG_incremental_marking) return;
+  ManualGCScope manual_gc_scope;
   // The test verifies that the marking state is preserved across semispace
   // copy.
   CcTest::InitializeVM();
