d8e37c32f359032f16c6b5d8d1b03ba99939e5cc
Michaël Zasso <mic.besace@gmail.com>
2017-08-03 17:19:10 +0200

    FreeBSD/Solaris: Fix OS::Allocate
    The signature of the method was changed in
    implementation was not updated for those platforms.
    
    https: //chromium-review.googlesource.com/c/558876 but the
    Change-Id: I507d7877988287c352ee2690bac540f835c19ebf
    Reviewed-on: https://chromium-review.googlesource.com/600194
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#47258}

--- src/base/platform/platform-freebsd.cc
+++ src/base/platform/platform-freebsd.cc
@@ -41,10 +41,10 @@ TimezoneCache* OS::CreateTimezoneCache() {
 }
 
 void* OS::Allocate(const size_t requested, size_t* allocated,
-                   OS::MemoryPermission access) {
+                   OS::MemoryPermission access, void* hint) {
   const size_t msize = RoundUp(requested, getpagesize());
   int prot = GetProtectionFromMemoryPermission(access);
-  void* mbase = mmap(NULL, msize, prot, MAP_PRIVATE | MAP_ANON, -1, 0);
+  void* mbase = mmap(hint, msize, prot, MAP_PRIVATE | MAP_ANON, -1, 0);
 
   if (mbase == MAP_FAILED) return NULL;
   *allocated = msize;
