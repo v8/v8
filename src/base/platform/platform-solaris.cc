9f969bccb90bf716a51a8081efa942f2bd124a64
Michaël Zasso <targos@protonmail.com>
2017-08-10 13:57:34 +0200

    Merged: FreeBSD/Solaris: Fix OS::Allocate
    Revision: d8e37c32f359032f16c6b5d8d1b03ba99939e5cc
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=hablich@chromium.org
    
    Bug: v8:6695
    Change-Id: I15de807c75921bf84897d4f057bff8fc749c676b
    Reviewed-on: https://chromium-review.googlesource.com/609981
    Reviewed-by: Michael Hablich <hablich@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6.1@{#53}
    Cr-Branched-From: 1bf2e10ddb194d4c2871a87a4732613419de892d-refs/heads/6.1.534@{#1}
    Cr-Branched-From: e825c4318eb2065ffdf9044aa6a5278635c36427-refs/heads/master@{#46746}

--- src/base/platform/platform-solaris.cc
+++ src/base/platform/platform-solaris.cc
@@ -59,10 +59,10 @@ double SolarisTimezoneCache::LocalTimeOffset() {
 TimezoneCache* OS::CreateTimezoneCache() { return new SolarisTimezoneCache(); }
 
 void* OS::Allocate(const size_t requested, size_t* allocated,
-                   OS::MemoryPermission access) {
+                   OS::MemoryPermission access, void* hint) {
   const size_t msize = RoundUp(requested, getpagesize());
   int prot = GetProtectionFromMemoryPermission(access);
-  void* mbase = mmap(NULL, msize, prot, MAP_PRIVATE | MAP_ANON, -1, 0);
+  void* mbase = mmap(hint, msize, prot, MAP_PRIVATE | MAP_ANON, -1, 0);
 
   if (mbase == MAP_FAILED) return NULL;
   *allocated = msize;
