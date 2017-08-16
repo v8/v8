a50b67519ab3023d87ca03fad9375c78dc767cb2
Franziska Hinkelmann <franzih@chromium.org>
2017-08-16 13:48:15 +0200

    [runtime] Port TypedArraySetFormArrayLike to C++
    Bug: v8:6704
    Change-Id: I316f085801f4fb6a792124f9a6f80a16e6d43162
    Reviewed-on: https://chromium-review.googlesource.com/616721
    Commit-Queue: Franziska Hinkelmann <franzih@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#47369}

--- src/runtime/runtime.h
+++ src/runtime/runtime.h
@@ -619,6 +619,7 @@ namespace internal {
   F(ArrayBufferViewWasNeutered, 1, 1)    \
   F(TypedArrayGetLength, 1, 1)           \
   F(TypedArrayGetBuffer, 1, 1)           \
+  F(TypedArraySetFromArrayLike, 4, 1)    \
   F(TypedArraySetFastCases, 3, 1)        \
   F(TypedArraySortFast, 1, 1)            \
   F(TypedArrayMaxSizeInHeap, 0, 1)       \
