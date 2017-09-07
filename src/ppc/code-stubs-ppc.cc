06d6b55ff20197fb91deca525b7949d8acc35800
Jaideep Bajwa <bjaideep@ca.ibm.com>
2017-09-07 11:30:41 -0400

    Merged: Squashed multiple commits.
    Merged: PPC/s390: [Interpreter] Adapt Call bytecode handlers to drop their stack-frame.
    Revision: e5b93bd57f2bbed54a6e353b7945e07acc57dab3
    
    Merged: s390/PPC: Remove InterpreterExitTrampoline.
    Revision: 40c98daa177d8b06b2996c005a7886d36e19184f
    
    Merged: PPC: use the correct condition register
    Revision: b9747f357fc9c097bb9ec4b56ac19a988e2399a7
    
    BUG=chromium:753705
    LOG=N
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=hablich@chromium.org, joransiu@ca.ibm.com, jyan@ca.ibm.com, rmcilroy@chromium.org
    
    Change-Id: Ide8d15f6c3a3f11bce9359e481426007735708bb
    Reviewed-on: https://chromium-review.googlesource.com/655123
    Reviewed-by: Junliang Yan <jyan@ca.ibm.com>
    Commit-Queue: Junliang Yan <jyan@ca.ibm.com>
    Cr-Commit-Position: refs/branch-heads/6.2@{#7}
    Cr-Branched-From: efa2ac4129d30c7c72e84c16af3d20b44829f990-refs/heads/6.2.414@{#1}
    Cr-Branched-From: a861ebb762a60bf5cc2a274faee3620abfb06311-refs/heads/master@{#47693}

--- src/ppc/code-stubs-ppc.cc
+++ src/ppc/code-stubs-ppc.cc
@@ -1775,7 +1775,7 @@ void CallApiCallbackStub::Generate(MacroAssembler* masm) {
     __ LoadP(scratch, FieldMemOperand(accessor_holder, HeapObject::kMapOffset));
     __ LoadP(scratch2, FieldMemOperand(scratch, Map::kBitFieldOffset));
     __ andi(r0, scratch2, Operand(1 << Map::kIsConstructor));
-    __ bne(&skip_looking_for_constructor);
+    __ bne(&skip_looking_for_constructor, cr0);
     __ GetMapConstructor(context, scratch, scratch, scratch2);
     __ bind(&skip_looking_for_constructor);
     __ LoadP(context, FieldMemOperand(context, JSFunction::kContextOffset));
