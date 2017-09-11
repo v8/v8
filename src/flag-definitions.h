3ab49f664f6ff3d048f949aacdb178e249cbc3ec
Josh Wolfe <jwolfe@igalia.com>
2017-09-11 16:38:34 -0700

    [intl] Stage --harmony-number-format-to-parts
    R=littledan@chromium.org, adamk@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    CQ_INCLUDE_TRYBOTS=master.tryserver.v8:v8_linux_noi18n_rel_ng
    
    Bug: v8:5244
    Change-Id: Ib1ca48a843f017c6dcea430af08afbc170c9650d
    Reviewed-on: https://chromium-review.googlesource.com/661889
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Commit-Queue: Josh Wolfe <jwolfe@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#47977}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -189,22 +189,12 @@ DEFINE_BOOL(harmony_shipping, true, "enable all shipped harmony features")
 DEFINE_IMPLICATION(es_staging, harmony)
 
 // Features that are still work in progress (behind individual flags).
-#define HARMONY_INPROGRESS_BASE(V)                                    \
+#define HARMONY_INPROGRESS(V)                                         \
   V(harmony_array_prototype_values, "harmony Array.prototype.values") \
   V(harmony_function_sent, "harmony function.sent")                   \
   V(harmony_do_expressions, "harmony do-expressions")                 \
   V(harmony_class_fields, "harmony public fields in class literals")  \
   V(harmony_bigint, "harmony arbitrary precision integers")
-
-#ifdef V8_INTL_SUPPORT
-#define HARMONY_INPROGRESS(V)       \
-  HARMONY_INPROGRESS_BASE(V)        \
-  V(harmony_number_format_to_parts, \
-    "Intl.NumberFormat.prototype."  \
-    "formatToParts")
-#else
-#define HARMONY_INPROGRESS(V) HARMONY_INPROGRESS_BASE(V)
-#endif
 
 // Features that are complete (but still behind --harmony/es-staging flag).
 #define HARMONY_STAGED_BASE(V)                                          \
@@ -219,8 +209,10 @@ DEFINE_IMPLICATION(es_staging, harmony)
   V(harmony_promise_finally, "harmony Promise.prototype.finally")
 
 #ifdef V8_INTL_SUPPORT
-#define HARMONY_STAGED(V) \
-  HARMONY_STAGED_BASE(V)  \
+#define HARMONY_STAGED(V)                        \
+  HARMONY_STAGED_BASE(V)                         \
+  V(harmony_number_format_to_parts,              \
+    "Intl.NumberFormat.prototype.formatToParts") \
   V(harmony_plural_rules, "Intl.PluralRules")
 #else
 #define HARMONY_STAGED(V) HARMONY_STAGED_BASE(V)
