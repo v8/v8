43d6dc5615ff820af6564fcce4fb1bef1f11bd60
Ross McIlroy <rmcilroy@chromium.org>
2017-09-13 15:53:27 +0100

    [Interpreter] Move ast printing into interpreter.cc
    Moves ast printing out of codegen.cc and into interpreter.cc since this is
    the only place which calls it.
    
    BUG=v8:6409
    
    Change-Id: I7b730f6b4da76247f57e3cb4fa7895e638ea0517
    Reviewed-on: https://chromium-review.googlesource.com/664888
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#47996}

--- src/codegen.cc
+++ src/codegen.cc
@@ -10,14 +10,12 @@
 
 #include <memory>
 
-#include "src/ast/prettyprinter.h"
 #include "src/bootstrapper.h"
 #include "src/compilation-info.h"
 #include "src/counters.h"
 #include "src/debug/debug.h"
 #include "src/eh-frame.h"
 #include "src/objects-inl.h"
-#include "src/parsing/parse-info.h"
 #include "src/runtime/runtime.h"
 
 namespace v8 {
@@ -68,45 +66,6 @@ double modulo(double x, double y) {
 UNARY_MATH_FUNCTION(sqrt, CreateSqrtFunction)
 
 #undef UNARY_MATH_FUNCTION
-
-void CodeGenerator::MakeCodePrologue(ParseInfo* parse_info,
-                                     CompilationInfo* info, const char* kind) {
-  bool print_ast = false;
-  const char* ftype;
-
-  if (info->isolate()->bootstrapper()->IsActive()) {
-    print_ast = FLAG_print_builtin_ast;
-    ftype = "builtin";
-  } else {
-    print_ast = FLAG_print_ast;
-    ftype = "user-defined";
-  }
-
-  if (!FLAG_trace_codegen && !print_ast) return;
-
-  // Requires internalizing the AST, so make sure we are on the main thread and
-  // allow handle dereference and allocations.
-  // TODO(rmcilroy): Make ast-printer print ast raw strings instead of
-  // internalized strings to avoid internalizing here.
-  DCHECK(ThreadId::Current().Equals(info->isolate()->thread_id()));
-  AllowHandleDereference allow_deref;
-  AllowHandleAllocation allow_handles;
-  AllowHeapAllocation allow_gc;
-  parse_info->ast_value_factory()->Internalize(info->isolate());
-
-  if (FLAG_trace_codegen || print_ast) {
-    std::unique_ptr<char[]> name = info->GetDebugName();
-    PrintF("[generating %s code for %s function: %s]\n", kind, ftype,
-           name.get());
-  }
-
-#ifdef DEBUG
-  if (!info->IsStub() && print_ast) {
-    PrintF("--- AST ---\n%s\n",
-           AstPrinter(info->isolate()).PrintProgram(info->literal()));
-  }
-#endif  // DEBUG
-}
 
 Handle<Code> CodeGenerator::MakeCodeEpilogue(TurboAssembler* tasm,
                                              EhFrameWriter* eh_frame_writer,
