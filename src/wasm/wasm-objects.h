0752d20271ad17ae272fc5a3ea3414dccea3f9b2
Mircea Trofin <mtrofin@chromium.org>
2017-09-25 23:15:17 -0700

    Merged: [wasm] Weaken global handles used for indirect tables
    Revision: de296d14663aec87afd88141069b3e3cef9c3f86
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=hablich@chromium.org
    
    Bug: v8:6840
    Change-Id: I9a5a7b1b5cb73e8406018fd8ed9fb44636de7982
    Reviewed-on: https://chromium-review.googlesource.com/684054
    Reviewed-by: Michael Hablich <hablich@chromium.org>
    Commit-Queue: Mircea Trofin <mtrofin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6.2@{#43}
    Cr-Branched-From: efa2ac4129d30c7c72e84c16af3d20b44829f990-refs/heads/6.2.414@{#1}
    Cr-Branched-From: a861ebb762a60bf5cc2a274faee3620abfb06311-refs/heads/master@{#47693}

--- src/wasm/wasm-objects.h
+++ src/wasm/wasm-objects.h
@@ -25,32 +25,8 @@ namespace wasm {
 class InterpretedFrame;
 class WasmInterpreter;
 
-// When we compile or instantiate, we need to create global handles
-// for function tables. Normally, these handles get destroyed when the
-// respective objects get GCed. If we fail to construct those objects,
-// we can leak global hanles. The exit path in these cases isn't unique,
-// and may grow.
-//
-// This type addresses that.
-
 typedef Address GlobalHandleAddress;
 
-class GlobalHandleLifetimeManager final {
- public:
-  void Add(GlobalHandleAddress addr) { handles_.push_back(addr); }
-  // Call this when compilation or instantiation has succeeded, and we've
-  // passed the control to a JS object with a finalizer that'll destroy
-  // the handles.
-  void ReleaseWithoutDestroying() { handles_.clear(); }
-  ~GlobalHandleLifetimeManager() {
-    for (auto& addr : handles_) {
-      GlobalHandles::Destroy(reinterpret_cast<Object**>(addr));
-    }
-  }
-
- private:
-  std::vector<Address> handles_;
-};
 }  // namespace wasm
 
 class WasmCompiledModule;
@@ -92,9 +68,6 @@ class WasmModuleObject : public JSObject {
 
   static Handle<WasmModuleObject> New(
       Isolate* isolate, Handle<WasmCompiledModule> compiled_module);
-
- private:
-  static void Finalizer(const v8::WeakCallbackInfo<void>& data);
 };
 
 // Representation of a WebAssembly.Table JavaScript-level object.
@@ -180,6 +153,9 @@ class WasmInstanceObject : public JSObject {
   DECL_OPTIONAL_ACCESSORS(memory_buffer, JSArrayBuffer)
   DECL_OPTIONAL_ACCESSORS(globals_buffer, JSArrayBuffer)
   DECL_OPTIONAL_ACCESSORS(debug_info, WasmDebugInfo)
+  DECL_OPTIONAL_ACCESSORS(function_tables, FixedArray)
+  DECL_OPTIONAL_ACCESSORS(signature_tables, FixedArray)
+
   // FixedArray of all instances whose code was imported
   DECL_OPTIONAL_ACCESSORS(directly_called_instances, FixedArray)
 
@@ -190,6 +166,8 @@ class WasmInstanceObject : public JSObject {
     kMemoryBufferIndex,
     kGlobalsBufferIndex,
     kDebugInfoIndex,
+    kFunctionTablesIndex,
+    kSignatureTablesIndex,
     kDirectlyCalledInstancesIndex,
     kFieldCount
   };
@@ -201,6 +179,8 @@ class WasmInstanceObject : public JSObject {
   DEF_OFFSET(MemoryBuffer)
   DEF_OFFSET(GlobalsBuffer)
   DEF_OFFSET(DebugInfo)
+  DEF_OFFSET(FunctionTables)
+  DEF_OFFSET(SignatureTables)
   DEF_OFFSET(DirectlyCalledInstances)
 
   WasmModuleObject* module_object();
@@ -750,6 +730,10 @@ ACCESSORS(WasmInstanceObject, globals_buffer, JSArrayBuffer,
           kGlobalsBufferOffset)
 OPTIONAL_ACCESSORS(WasmInstanceObject, debug_info, WasmDebugInfo,
                    kDebugInfoOffset)
+OPTIONAL_ACCESSORS(WasmInstanceObject, function_tables, FixedArray,
+                   kFunctionTablesOffset)
+OPTIONAL_ACCESSORS(WasmInstanceObject, signature_tables, FixedArray,
+                   kSignatureTablesOffset)
 ACCESSORS(WasmInstanceObject, directly_called_instances, FixedArray,
           kDirectlyCalledInstancesOffset)
 
