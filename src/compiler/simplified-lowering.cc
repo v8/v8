6ddb5e7da75c96405911029f614b348da209cee7
Tobias Tebbi <tebbi@chromium.org>
2017-10-06 16:20:52 +0200

    Reland^2 "[turbofan] eagerly prune None types and deadness from the graph"
    Now, the EffectControlLinearizer connects all occurrences of Unreachable to the 
    graph end. This fixes issues with later phases running DeadCodeElimination and
    introducing new DeadValue nodes when processing uses of Unreachable.
    
    This is a reland of 3c4bc27f132b6221836e702684cdb4a3e0d009c0
    Original change's description:
    > Reland "[turbofan] eagerly prune None types and deadness from the graph"
    > 
    > This is a reland of e1cdda2512d9844e180883902b08ed9b302471c0
    > Original change's description:
    > > [turbofan] eagerly prune None types and deadness from the graph
    > > 
    > > In addition to using the {Dead} node to prune dead control nodes and nodes that 
    > > depend on them, we introduce a {DeadValue} node representing an impossible value 
    > > that can occur at any position in the graph. The extended {DeadCodeElimination}
    > > prunes {DeadValue} and its uses, inserting a crashing {Unreachable} node into
    > > the effect chain when possible. The remaining uses of {DeadValue} are handled
    > > in {EffectControlLinearizer}, where we always have access to the effect chain.
    > > In addition to explicitly introduced {DeadValue} nodes, we consider any value use
    > > of a node with type {None} as dead.
    > > 
    > > Bug: chromium:741225
    > > Change-Id: Icc4b636d1d018c452ba1a2fa7cd3e00e522f1655
    > > Reviewed-on: https://chromium-review.googlesource.com/641250
    > > Commit-Queue: Tobias Tebbi <tebbi@chromium.org>
    > > Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#48208}
    > 
    > Bug: chromium:741225
    > Change-Id: I21316913dae02864f7a6d7c9269405a79f054138
    > Reviewed-on: https://chromium-review.googlesource.com/692034
    > Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    > Commit-Queue: Tobias Tebbi <tebbi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#48232}
    
    Bug: chromium:741225
    Change-Id: I5702ec34856c075717162153adc765774453c45f
    Reviewed-on: https://chromium-review.googlesource.com/702264
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Tobias Tebbi <tebbi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#48366}

--- src/compiler/simplified-lowering.cc
+++ src/compiler/simplified-lowering.cc
@@ -2955,6 +2955,7 @@ class RepresentationSelector {
       case IrOpcode::kArgumentsElementsState:
       case IrOpcode::kArgumentsLengthState:
       case IrOpcode::kRuntimeAbort:
+      case IrOpcode::kUnreachable:
 // All JavaScript operators except JSToNumber have uniform handling.
 #define OPCODE_CASE(name) case IrOpcode::k##name:
         JS_SIMPLE_BINOP_LIST(OPCODE_CASE)
@@ -2971,7 +2972,8 @@ class RepresentationSelector {
         VisitInputs(node);
         // Assume the output is tagged.
         return SetOutput(node, MachineRepresentation::kTagged);
-
+      case IrOpcode::kDeadValue:
+        return SetOutput(node, MachineRepresentation::kNone);
       default:
         V8_Fatal(
             __FILE__, __LINE__,
