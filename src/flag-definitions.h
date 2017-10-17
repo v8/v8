3b1cfa38bcd616609f263ac3b6721c0214c45d19
Michael Hablich <hablich@chromium.org>
2017-10-17 16:11:21 +0000

    Revert "Merged: Revert "Enable icu-timezone-data by default""
    This reverts commit d7343703bc6a3c1dbd5ead201186c520ffc9e47b.
    
    Reason for revert: broke some branch builders like https://build.chromium.org/p/client.v8.branches/builders/V8%20arm%20-%20sim%20-%20beta%20branch%20-%20debug
    
    Original change's description:
    > Merged: Revert "Enable icu-timezone-data by default"
    > 
    > This reverts commit d9a25842d3aba82ead5e06c7fd7cb4896e6ad202.
    > This is for 6.3 branch (to go with Chromium 63 branch). 
    > 
    > Reason for revert:
    > 
    > I'm reverting this CL for a few reasons. #2 is the most significant and
    > I should have thought of that before making a switch. Sorry for that.
    > 
    > 1) perf-regression: http://crbug.com/769706
    > 2) http://crbug.com/612010 : ICU timezone update is not propagated to
    > zygote process so that new tabs will hold on to an old timezone even
    > after a timezone change on Linux and Chrome OS.
    > 
    > 3)  http://crbug.com/754053  : OS timezone detection issues on macOS
    > 10.13, Ubutu 16, RHEL 7, SuSe Linux 12 or newer. ; it's being fixed. So,
    > it actually ok.
    > 
    > 4)  http://crbug.com/771868  : timezone wrong in gmail: If it's due to
    > 
    > Revision: 28ef8dc7009d
    > 
    > BUG=chromium:612010,chromium:769706,chromium:771868,v8:2137,v8:6031,v8:6076
    > LOG=N
    > NOTRY=true
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > R=​adamk@chromium.org, hablich@chromium.org
    > 
    > Change-Id: Id0093c2bc69e5bc1d3b7147b7bbcc633ed625a45
    > Reviewed-on: https://chromium-review.googlesource.com/721919
    > Reviewed-by: Adam Klein <adamk@chromium.org>
    > Commit-Queue: Jungshik Shin <jshin@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/6.3@{#7}
    > Cr-Branched-From: 094a7c93dcdcd921de3883ba4674b7e1a0feffbe-refs/heads/6.3.292@{#1}
    > Cr-Branched-From: 18b8fbb528a8021e04a029e06eafee50b918bce0-refs/heads/master@{#48432}
    
    TBR=adamk@chromium.org,hablich@chromium.org,jshin@chromium.org
    
    Change-Id: I8b30f8f9c63b93cb000facbf910ed111ca3f9ab2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:612010, chromium:769706, chromium:771868, v8:2137, v8:6031, v8:6076
    Reviewed-on: https://chromium-review.googlesource.com/723324
    Reviewed-by: Michael Hablich <hablich@chromium.org>
    Commit-Queue: Michael Hablich <hablich@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6.3@{#18}
    Cr-Branched-From: 094a7c93dcdcd921de3883ba4674b7e1a0feffbe-refs/heads/6.3.292@{#1}
    Cr-Branched-From: 18b8fbb528a8021e04a029e06eafee50b918bce0-refs/heads/master@{#48432}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -266,8 +266,7 @@ HARMONY_SHIPPING(FLAG_SHIPPING_FEATURES)
 #undef FLAG_SHIPPING_FEATURES
 
 #ifdef V8_INTL_SUPPORT
-DEFINE_BOOL(icu_timezone_data, false,
-            "get information about timezones from ICU")
+DEFINE_BOOL(icu_timezone_data, true, "get information about timezones from ICU")
 #endif
 
 #ifdef V8_ENABLE_FUTURE
