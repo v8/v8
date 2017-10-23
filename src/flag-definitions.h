870fd90de945c123560f23d72009299d28c97483
Ulan Degenbaev <ulan@chromium.org>
2017-10-23 09:50:21 +0000

    Revert "[heap] Disable map retaining optimization."
    This reverts commit 00ba1dca22baee909e5a749c6c4c91c83b2ec0b1.
    
    Reason for revert: perf regressions crbug.com/774903 
    
    Original change's description:
    > [heap] Disable map retaining optimization.
    > 
    > The optimization keeps dying maps alive for several GCs to mitigate
    > code deoptimization with weak maps.
    > 
    > This patch disables the optimization to see if it still needed.
    > 
    > Bug: 
    > Change-Id: Ie5717967ad56858e6ae546c90fde73e8d5bcc4ec
    > Reviewed-on: https://chromium-review.googlesource.com/712598
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#48458}
    
    TBR=ulan@chromium.org,mlippautz@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Id57d7239a041b03e02a35ccbf5830ff9838a6246
    Reviewed-on: https://chromium-review.googlesource.com/733017
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#48822}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -579,7 +579,7 @@ DEFINE_INT(max_old_space_size, 0, "max size of the old space (in Mbytes)")
 DEFINE_INT(initial_old_space_size, 0, "initial old space size (in Mbytes)")
 DEFINE_BOOL(gc_global, false, "always perform global GCs")
 DEFINE_INT(gc_interval, -1, "garbage collect after <n> allocations")
-DEFINE_INT(retain_maps_for_n_gc, 0,
+DEFINE_INT(retain_maps_for_n_gc, 2,
            "keeps maps alive for <n> old space garbage collections")
 DEFINE_BOOL(trace_gc, false,
             "print one trace line following each garbage collection")
