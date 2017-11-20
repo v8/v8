82ca51467f01d2a07593cbfbb7f81a6fa7bb47f9
Camillo Bruni <cbruni@chromium.org>
2017-11-20 14:29:20 +0100

    Harden isolate initialization
    In the case of a corrupted snapshot we fall back to initializing the isolate
    from scratch. Howver, we don't ship the full SetupIsolateDelegate. This causes
    spurious failures during later initialization.
    
    This CL mostly turns the DCHECKs in SetupIsolateDelegate into hard CHECKs making
    it easier to spot these kind of failures.
    
    
    Bug: chromium:767846
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_rel_ng
    Change-Id: Ibe8a5beece27433439b1b09412f6110be703ff86
    Reviewed-on: https://chromium-review.googlesource.com/779189
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#49533}

--- src/api.cc
+++ src/api.cc
@@ -8609,6 +8609,9 @@ Isolate* IsolateNewImpl(internal::Isolate* isolate,
   // TODO(jochen): Once we got rid of Isolate::Current(), we can remove this.
   Isolate::Scope isolate_scope(v8_isolate);
   if (params.entry_hook || !i::Snapshot::Initialize(isolate)) {
+    // If snapshot data was provided and we failed to deserialize it must
+    // have been corrupted.
+    CHECK_NULL(isolate->snapshot_blob());
     base::ElapsedTimer timer;
     if (i::FLAG_profile_deserialization) timer.Start();
     isolate->Init(nullptr);
