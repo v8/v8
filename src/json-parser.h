d3104923b24180d32747287ba51b41e4af8b0da7
Jeremy Roman <jbroman@chromium.org>
2017-11-27 10:45:02 -0500

    Implement and use VectorSegment to avoid repeated allocation of ZoneVector properties.
    The parser holds a single vector whose backing storage is reused in calls
    to ParseJsonObject, so that once we reach the peak number of unstored
    properties no more allocations are required.
    
    This improves performance of parsing inputs like those in Speedometer VanillaJS
    by about 2% in my local measurement, and would presumably do better on more
    pathological inputs.
    
    This should also have the side effect of reducing peak memory usage at this time
    slightly, since we do fewer zone allocations which cannot be freed until the
    parse finishes.
    
    Bug: chromium:771227
    Change-Id: I8aa1514b37a74f82539f95f94292c8fa1582d66a
    Reviewed-on: https://chromium-review.googlesource.com/789511
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#49693}

--- src/json-parser.h
+++ src/json-parser.h
@@ -144,7 +144,7 @@ class JsonParser BASE_EMBEDDED {
   Zone* zone() { return &zone_; }
 
   void CommitStateToJsonObject(Handle<JSObject> json_object, Handle<Map> map,
-                               ZoneVector<Handle<Object>>* properties);
+                               Vector<const Handle<Object>> properties);
 
   Handle<String> source_;
   int source_length_;
@@ -157,6 +157,9 @@ class JsonParser BASE_EMBEDDED {
   Handle<JSFunction> object_constructor_;
   uc32 c0_;
   int position_;
+
+  // Property handles are stored here inside ParseJsonObject.
+  ZoneVector<Handle<Object>> properties_;
 };
 
 }  // namespace internal
