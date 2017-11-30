df659b17edecebf6f5b5b96edf28655e5ee612b5
Michael Achenbach <machenbach@chromium.org>
2017-11-30 11:10:52 +0100

    [test] Refactor variant flags
    This derives two global variables to ease adding new variants.
    
    Bug: chromium:788104
    Change-Id: Ibe933dc0999f6dfa0ae8bf2afa0a5de8e44aefca
    Reviewed-on: https://chromium-review.googlesource.com/799790
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Sergiy Byelozyorov <sergiyb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#49741}

--- tools/testrunner/local/variants.py
+++ tools/testrunner/local/variants.py
@@ -21,23 +21,9 @@ ALL_VARIANT_FLAGS = {
 }
 
 # FAST_VARIANTS implies no --always-opt.
-FAST_VARIANT_FLAGS = {
-  "default": [[]],
-  "future": [["--future"]],
-  "liftoff": [["--liftoff"]],
-  "minor_mc": [["--minor-mc"]],
-  "stress": [["--stress-opt"]],
-  # TODO(6792): Write protected code has been temporary added to the below
-  # variant until the feature has been enabled (or staged) by default.
-  "stress_incremental_marking":  [["--stress-incremental-marking", "--write-protect-code-memory"]],
-  # No optimization means disable all optimizations. OptimizeFunctionOnNextCall
-  # would not force optimization too. It turns into a Nop. Please see
-  # https://chromium-review.googlesource.com/c/452620/ for more discussion.
-  "nooptimization": [["--noopt"]],
-  "stress_background_compile": [["--background-compile", "--stress-background-compile"]],
-  "wasm_traps": [["--wasm_trap_handler", "--invoke-weak-callbacks", "--wasm-jit-to-native"]],
-}
+FAST_VARIANT_FLAGS = dict(
+    (k, [[f for f in v[0] if f != "--always-opt"]])
+    for k, v in ALL_VARIANT_FLAGS.iteritems()
+)
 
-ALL_VARIANTS = set(["default", "future", "liftoff", "minor_mc", "stress",
-                    "stress_incremental_marking", "nooptimization",
-                    "stress_background_compile", "wasm_traps"])
+ALL_VARIANTS = set(ALL_VARIANT_FLAGS.keys())
