3b065110528562a53533bef516237bf466e39096
Michal Majewski <majeski@google.com>
2017-11-30 13:57:45 +0100

    Reland "[test] Creating command before execution phase."
    This is a reland of 98cc9e862f9e8c0a8cde95e7b42beb808b88dab6
    Original change's description:
    > [test] Creating command before execution phase.
    > 
    > Immutable command class with shell, flags and
    > environment.
    > 
    > Command creation moved from worker to the main
    > process. Because of that there is no need to send
    > test cases beyond process boundaries and load test
    > suites in worker processes.
    > 
    > Bug: v8:6917
    > Change-Id: Ib6a44278095b4f7141eb9b96802fe3e8117678a6
    > Reviewed-on: https://chromium-review.googlesource.com/791710
    > Commit-Queue: Michał Majewski <majeski@google.com>
    > Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#49746}
    
    Bug: v8:6917
    Change-Id: I49c29a8db813c47909f2cc45070ac7721a447c7a
    Reviewed-on: https://chromium-review.googlesource.com/800370
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Michał Majewski <majeski@google.com>
    Cr-Commit-Position: refs/heads/master@{#49756}

--- tools/testrunner/local/progress.py
+++ tools/testrunner/local/progress.py
@@ -32,12 +32,8 @@ import os
 import sys
 import time
 
-from . import execution
 from . import junit_output
 from . import statusfile
-
-
-ABS_PATH_PREFIX = os.getcwd() + os.sep
 
 
 class ProgressIndicator(object):
@@ -69,18 +65,6 @@ class ProgressIndicator(object):
       'label': test.GetLabel(),
       'negative': negative_marker
     }
-
-  def _EscapeCommand(self, test):
-    command, _ = execution.GetCommand(test, self.runner.context)
-    parts = []
-    for part in command:
-      if ' ' in part:
-        # Escape spaces.  We may need to escape more characters for this
-        # to work properly.
-        parts.append('"%s"' % part)
-      else:
-        parts.append(part)
-    return " ".join(parts)
 
 
 class IndicatorNotifier(object):
@@ -123,7 +107,7 @@ class SimpleProgressIndicator(ProgressIndicator):
       if failed.output.stdout:
         print "--- stdout ---"
         print failed.output.stdout.strip()
-      print "Command: %s" % self._EscapeCommand(failed)
+      print "Command: %s" % failed.cmd.to_string()
       if failed.output.HasCrashed():
         print "exit code: %d" % failed.output.exit_code
         print "--- CRASHED ---"
@@ -205,7 +189,7 @@ class CompactProgressIndicator(ProgressIndicator):
       stderr = test.output.stderr.strip()
       if len(stderr):
         print self.templates['stderr'] % stderr
-      print "Command: %s" % self._EscapeCommand(test)
+      print "Command: %s" % test.cmd.to_string()
       if test.output.HasCrashed():
         print "exit code: %d" % test.output.exit_code
         print "--- CRASHED ---"
@@ -273,6 +257,7 @@ class MonochromeProgressIndicator(CompactProgressIndicator):
 class JUnitTestProgressIndicator(ProgressIndicator):
 
   def __init__(self, junitout, junittestsuite):
+    super(JUnitTestProgressIndicator, self).__init__()
     self.outputter = junit_output.JUnitTestOutput(junittestsuite)
     if junitout:
       self.outfile = open(junitout, "w")
@@ -293,7 +278,7 @@ class JUnitTestProgressIndicator(ProgressIndicator):
       stderr = test.output.stderr.strip()
       if len(stderr):
         fail_text += "stderr:\n%s\n" % stderr
-      fail_text += "Command: %s" % self._EscapeCommand(test)
+      fail_text += "Command: %s" % self.test.cmd.to_string()
       if test.output.HasCrashed():
         fail_text += "exit code: %d\n--- CRASHED ---" % test.output.exit_code
       if test.output.HasTimedOut():
@@ -307,6 +292,7 @@ class JUnitTestProgressIndicator(ProgressIndicator):
 class JsonTestProgressIndicator(ProgressIndicator):
 
   def __init__(self, json_test_results, arch, mode, random_seed):
+    super(JsonTestProgressIndicator, self).__init__()
     self.json_test_results = json_test_results
     self.arch = arch
     self.mode = mode
@@ -334,7 +320,7 @@ class JsonTestProgressIndicator(ProgressIndicator):
       {
         "name": test.GetLabel(),
         "flags": test.flags,
-        "command": self._EscapeCommand(test).replace(ABS_PATH_PREFIX, ""),
+        "command": test.cmd.to_string(relative=True),
         "duration": test.duration,
         "marked_slow": statusfile.IsSlow(
           test.suite.GetStatusFileOutcomes(test)),
@@ -364,7 +350,7 @@ class JsonTestProgressIndicator(ProgressIndicator):
     self.results.append({
       "name": test.GetLabel(),
       "flags": test.flags,
-      "command": self._EscapeCommand(test).replace(ABS_PATH_PREFIX, ""),
+      "command": test.cmd.to_string(relative=True),
       "run": test.run,
       "stdout": test.output.stdout,
       "stderr": test.output.stderr,
@@ -384,6 +370,7 @@ class JsonTestProgressIndicator(ProgressIndicator):
 class FlakinessTestProgressIndicator(ProgressIndicator):
 
   def __init__(self, json_test_results):
+    super(FlakinessTestProgressIndicator, self).__init__()
     self.json_test_results = json_test_results
     self.results = {}
     self.summary = {
