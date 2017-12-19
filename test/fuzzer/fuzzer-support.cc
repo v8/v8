004f348aba415381cf5b66801e0e0f26c41baae6
Max Moroz <mmoroz@chromium.org>
2017-12-19 09:43:05 -0800

    [fuzzer] Add attributes to LLVMFuzzerInitialize definition.
    That prevents the linker from dead-stripping the function, as it is not called
    directly, it is resolved in the runtime via dlsym().
    
    Bug: chromium:754124, chromium:787723
    Change-Id: I46a02ef01349f59b7ed944ce1483b7277e234a19
    Reviewed-on: https://chromium-review.googlesource.com/833995
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#50212}

--- test/fuzzer/fuzzer-support.cc
+++ test/fuzzer/fuzzer-support.cc
@@ -89,7 +89,11 @@ bool FuzzerSupport::PumpMessageLoop(
 
 }  // namespace v8_fuzzer
 
-extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
+// Explicitly specify some attributes to avoid issues with the linker dead-
+// stripping the following function on macOS, as it is not called directly
+// by fuzz target. LibFuzzer runtime uses dlsym() to resolve that function.
+extern "C" __attribute__((used)) __attribute__((visibility("default"))) int
+LLVMFuzzerInitialize(int* argc, char*** argv) {
   v8_fuzzer::FuzzerSupport::InitializeFuzzerSupport(argc, argv);
   return 0;
 }
