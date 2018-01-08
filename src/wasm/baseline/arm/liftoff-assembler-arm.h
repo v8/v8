0b92e95b21eed42e64fcdb452e4758b27c86ce6f
Clemens Hammacher <clemensh@chromium.org>
2018-01-08 12:54:14 +0100

    [Liftoff][cleanup] Add UNIMPLEMENTED to unimplemented methods
    This ensure that once features are added for other platform, we don't
    forget to implement all platform specific methods that are then being
    used.
    
    R=ahaas@chromium.org
    
    Bug: v8:6600
    Change-Id: I4cc948da280fdb63da1938edc6b391d180b88cad
    Reviewed-on: https://chromium-review.googlesource.com/853494
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#50401}

--- src/wasm/baseline/arm/liftoff-assembler-arm.h
+++ src/wasm/baseline/arm/liftoff-assembler-arm.h
@@ -11,41 +11,65 @@ namespace v8 {
 namespace internal {
 namespace wasm {
 
-void LiftoffAssembler::ReserveStackSpace(uint32_t bytes) {}
+void LiftoffAssembler::ReserveStackSpace(uint32_t bytes) { UNIMPLEMENTED(); }
 
-void LiftoffAssembler::LoadConstant(LiftoffRegister reg, WasmValue value) {}
+void LiftoffAssembler::LoadConstant(LiftoffRegister reg, WasmValue value) {
+  UNIMPLEMENTED();
+}
 
 void LiftoffAssembler::LoadFromContext(Register dst, uint32_t offset,
-                                       int size) {}
+                                       int size) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::SpillContext(Register context) {}
+void LiftoffAssembler::SpillContext(Register context) { UNIMPLEMENTED(); }
 
 void LiftoffAssembler::Load(LiftoffRegister dst, Register src_addr,
                             Register offset_reg, uint32_t offset_imm,
-                            LoadType type, LiftoffRegList pinned) {}
+                            LoadType type, LiftoffRegList pinned) {
+  UNIMPLEMENTED();
+}
 
 void LiftoffAssembler::Store(Register dst_addr, Register offset_reg,
                              uint32_t offset_imm, LiftoffRegister src,
-                             StoreType type, LiftoffRegList pinned) {}
+                             StoreType type, LiftoffRegList pinned) {
+  UNIMPLEMENTED();
+}
 
 void LiftoffAssembler::LoadCallerFrameSlot(LiftoffRegister dst,
-                                           uint32_t caller_slot_idx) {}
+                                           uint32_t caller_slot_idx) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::MoveStackValue(uint32_t dst_index, uint32_t src_index) {}
+void LiftoffAssembler::MoveStackValue(uint32_t dst_index, uint32_t src_index) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::MoveToReturnRegister(LiftoffRegister reg) {}
+void LiftoffAssembler::MoveToReturnRegister(LiftoffRegister reg) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::Move(LiftoffRegister dst, LiftoffRegister src) {}
+void LiftoffAssembler::Move(LiftoffRegister dst, LiftoffRegister src) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::Spill(uint32_t index, LiftoffRegister reg) {}
+void LiftoffAssembler::Spill(uint32_t index, LiftoffRegister reg) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::Spill(uint32_t index, WasmValue value) {}
+void LiftoffAssembler::Spill(uint32_t index, WasmValue value) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::Fill(LiftoffRegister reg, uint32_t index) {}
+void LiftoffAssembler::Fill(LiftoffRegister reg, uint32_t index) {
+  UNIMPLEMENTED();
+}
 
 #define DEFAULT_I32_BINOP(name, internal_name)                       \
   void LiftoffAssembler::emit_i32_##name(Register dst, Register lhs, \
-                                         Register rhs) {}
+                                         Register rhs) {             \
+    UNIMPLEMENTED();                                                 \
+  }
 
 // clang-format off
 DEFAULT_I32_BINOP(add, add)
@@ -59,31 +83,45 @@ DEFAULT_I32_BINOP(xor, xor)
 #undef DEFAULT_I32_BINOP
 
 void LiftoffAssembler::emit_f32_add(DoubleRegister dst, DoubleRegister lhs,
-                                    DoubleRegister rhs) {}
+                                    DoubleRegister rhs) {
+  UNIMPLEMENTED();
+}
 void LiftoffAssembler::emit_f32_sub(DoubleRegister dst, DoubleRegister lhs,
-                                    DoubleRegister rhs) {}
+                                    DoubleRegister rhs) {
+  UNIMPLEMENTED();
+}
 void LiftoffAssembler::emit_f32_mul(DoubleRegister dst, DoubleRegister lhs,
-                                    DoubleRegister rhs) {}
+                                    DoubleRegister rhs) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::emit_i32_test(Register reg) {}
+void LiftoffAssembler::emit_i32_test(Register reg) { UNIMPLEMENTED(); }
 
-void LiftoffAssembler::emit_i32_compare(Register lhs, Register rhs) {}
+void LiftoffAssembler::emit_i32_compare(Register lhs, Register rhs) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::emit_jump(Label* label) {}
+void LiftoffAssembler::emit_jump(Label* label) { UNIMPLEMENTED(); }
 
-void LiftoffAssembler::emit_cond_jump(Condition cond, Label* label) {}
+void LiftoffAssembler::emit_cond_jump(Condition cond, Label* label) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::StackCheck(Label* ool_code) {}
+void LiftoffAssembler::StackCheck(Label* ool_code) { UNIMPLEMENTED(); }
 
-void LiftoffAssembler::CallTrapCallbackForTesting() {}
+void LiftoffAssembler::CallTrapCallbackForTesting() { UNIMPLEMENTED(); }
 
-void LiftoffAssembler::AssertUnreachable(AbortReason reason) {}
+void LiftoffAssembler::AssertUnreachable(AbortReason reason) {
+  UNIMPLEMENTED();
+}
 
-void LiftoffAssembler::PushRegisters(LiftoffRegList regs) {}
+void LiftoffAssembler::PushRegisters(LiftoffRegList regs) { UNIMPLEMENTED(); }
 
-void LiftoffAssembler::PopRegisters(LiftoffRegList regs) {}
+void LiftoffAssembler::PopRegisters(LiftoffRegList regs) { UNIMPLEMENTED(); }
 
-void LiftoffAssembler::DropStackSlotsAndRet(uint32_t num_stack_slots) {}
+void LiftoffAssembler::DropStackSlotsAndRet(uint32_t num_stack_slots) {
+  UNIMPLEMENTED();
+}
 
 }  // namespace wasm
 }  // namespace internal
