997d5a6a5c72293776e686eefa8636a844c81a4a
Michael Achenbach <machenbach@chromium.org>
2018-01-15 10:45:38 +0100

    [test] Add code_serializer testing variant
    Also sort all variants in configuration mapping.
    
    Bug: v8:7285
    Change-Id: I2d3032b911e48c34557e47e50345fb29da7bbe85
    Reviewed-on: https://chromium-review.googlesource.com/866502
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#50584}

--- tools/testrunner/local/variants.py
+++ tools/testrunner/local/variants.py
@@ -4,25 +4,25 @@
 
 # Use this to run several variants of the tests.
 ALL_VARIANT_FLAGS = {
+  "code_serializer": [["--cache=code"]],
   "default": [[]],
   "future": [["--future"]],
+  # Alias of exhaustive variants, but triggering new test framework features.
+  "infra_staging": [[]],
   "liftoff": [["--liftoff"]],
   "minor_mc": [["--minor-mc"]],
-  "slow_path": [["--force-slow-path"]],
-  "stress": [["--stress-opt", "--always-opt"]],
-  "stress_incremental_marking":  [["--stress-incremental-marking"]],
   # No optimization means disable all optimizations. OptimizeFunctionOnNextCall
   # would not force optimization too. It turns into a Nop. Please see
   # https://chromium-review.googlesource.com/c/452620/ for more discussion.
   "nooptimization": [["--noopt"]],
+  "slow_path": [["--force-slow-path"]],
+  "stress": [["--stress-opt", "--always-opt"]],
   "stress_background_compile": [["--background-compile", "--stress-background-compile"]],
+  "stress_incremental_marking":  [["--stress-incremental-marking"]],
   # Trigger stress sampling allocation profiler with sample interval = 2^14
   "stress_sampling": [["--stress-sampling-allocation-profiler=16384"]],
   "trusted": [["--no-untrusted-code-mitigations"]],
   "wasm_traps": [["--wasm_trap_handler", "--invoke-weak-callbacks", "--wasm-jit-to-native"]],
-
-  # Alias of exhaustive variants, but triggering new test framework features.
-  "infra_staging": [[]],
 }
 
 ALL_VARIANTS = set(ALL_VARIANT_FLAGS.keys())
