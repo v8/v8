f3d22635d15d3fff40529b00bf6ba979776ad871
Benedikt Meurer <bmeurer@chromium.org>
2018-01-18 06:55:24 +0000

    Revert "[wasm] Fix page size constant + test limits on max pages."
    This reverts commit 183bc643b51f35bf57b1d5158d6df9f6a2693ba0.
    
    Reason for revert: https://logs.chromium.org/v/?s=chromium%2Fbb%2Fclient.v8%2FV8_Linux_-_debug%2F18780%2F%2B%2Frecipes%2Fsteps%2FCheck_-_extra%2F0%2Flogs%2Fmemory-size%2F0
    
    Original change's description:
    > [wasm] Fix page size constant + test limits on max pages.
    > 
    > BUG=chromium:801604
    > R=​bbudge@chromium.org,titzer@chromium.org
    > 
    > Change-Id: I5fa57d5f8ea288d7aef7d466cbf11df46c4dcd1d
    > Reviewed-on: https://chromium-review.googlesource.com/869468
    > Reviewed-by: Ben Titzer <titzer@chromium.org>
    > Reviewed-by: Bill Budge <bbudge@chromium.org>
    > Commit-Queue: Brad Nelson <bradnelson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#50665}
    
    TBR=bradnelson@chromium.org,bbudge@chromium.org,titzer@chromium.org
    
    Change-Id: I2b5a51f1a94e862f8ac4455d466e23d26c220202
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:801604
    Reviewed-on: https://chromium-review.googlesource.com/872950
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#50666}

--- test/mjsunit/wasm/memory-size.js
+++ test/mjsunit/wasm/memory-size.js
@@ -7,9 +7,6 @@
 load("test/mjsunit/wasm/wasm-constants.js");
 load("test/mjsunit/wasm/wasm-module-builder.js");
 
-var kV8MaxWasmMemoryPages = 32767;  // ~ 2 GiB
-var kSpecMaxWasmMemoryPages = 65536;  // 4 GiB
-
 (function testMemorySizeZero() {
   print("testMemorySizeZero()");
   var builder = new WasmModuleBuilder();
@@ -31,39 +28,4 @@ var kSpecMaxWasmMemoryPages = 65536;  // 4 GiB
          .exportFunc();
   var module = builder.instantiate();
   assertEquals(size, module.exports.memory_size());
-})();
-
-(function testMemorySizeSpecMaxOk() {
-  print("testMemorySizeV8Max()");
-  var builder = new WasmModuleBuilder();
-  builder.addMemory(1, kSpecMaxWasmMemoryPages, true);
-  builder.addFunction("memory_size", kSig_i_v)
-         .addBody([kExprMemorySize, kMemoryZero])
-         .exportFunc();
-  var module = builder.instantiate();
-  assertEquals(1, module.exports.memory_size());
-})();
-
-(function testMemorySizeV8MaxPlus1Throws() {
-  print("testMemorySizeV8MaxPlus1Throws()");
-  var builder = new WasmModuleBuilder();
-  builder.addMemory(kV8MaxWasmMemoryPages + 1,
-                    kV8MaxWasmMemoryPages + 1, false);
-  builder.addFunction("memory_size", kSig_i_v)
-         .addBody([kExprMemorySize, kMemoryZero])
-         .exportFunc();
-  assertThrows(() => builder.instantiate());
-})();
-
-(function testMemorySpecMaxOk() {
-  print("testMemoryInitialMaxOk()");
-  var mem = new WebAssembly.Memory(
-      {initial: 1, maximum: kSpecMaxWasmMemoryPages});
-  assertThrows(() => builder.instantiate());
-})();
-
-(function testMemoryInitialMaxPlus1Throws() {
-  print("testMemoryInitialMaxOk()");
-  assertThrows(() => new WebAssembly.Memory(
-      {initial: kV8WasmMaxMemoryPages + 1}));
 })();
