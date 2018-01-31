483c0313c3cda06c0422f5c33289e69fe5a151df
Adam Klein <adamk@chromium.org>
2018-01-31 14:03:34 -0800

    Revert "Reland: Enable --harmony-function-tostring by default"
    This reverts commit 6fe75e30aacff1f2e9b10a01e20bebd68fe56474.
    
    Enabling this flag caused a number of memory and performance
    regressions, so disabling on the 6.5 branch while I try to fix
    on tip-of-tree.
    
    Bug: v8:4958, chromium:801556, chromium:802400, chromium:807192
    Change-Id: Id5a2bb5362038f19140ba995af56bee6b1cf5b4f
    Reviewed-on: https://chromium-review.googlesource.com/896168
    Reviewed-by: Michael Hablich <hablich@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6.5@{#19}
    Cr-Branched-From: 73c55f57fe8506011ff854b15026ca765b669700-refs/heads/6.5.254@{#1}
    Cr-Branched-From: 594a1a0b6e551397cfdf50870f6230da34db2dc8-refs/heads/master@{#50664}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -214,6 +214,7 @@ DEFINE_IMPLICATION(harmony_class_fields, harmony_private_fields)
 
 // Features that are complete (but still behind --harmony/es-staging flag).
 #define HARMONY_STAGED(V)                                               \
+  V(harmony_function_tostring, "harmony Function.prototype.toString")   \
   V(harmony_restrict_constructor_return,                                \
     "harmony disallow non undefined primitive return value from class " \
     "constructor")                                                      \
@@ -226,7 +227,6 @@ DEFINE_IMPLICATION(harmony_class_fields, harmony_private_fields)
   V(harmony_sharedarraybuffer, "harmony sharedarraybuffer")             \
   V(harmony_regexp_named_captures, "harmony regexp named captures")     \
   V(harmony_regexp_property, "harmony Unicode regexp property classes") \
-  V(harmony_function_tostring, "harmony Function.prototype.toString")   \
   V(harmony_promise_finally, "harmony Promise.prototype.finally")
 
 #ifdef V8_INTL_SUPPORT
