1bc6d6bcc1234043e778497253f28dd1c5647025
Ali Ijaz Sheikh <ofrobots@google.com>
2018-02-01 16:22:24 -0800

    Merged: Squashed multiple commits.
    Merged: [Sampling Heap Profiler] Tolerate unmaterialized closure during deopt
    Revision: cedc81de4c3b5296ae81d23e7ec35af66ed99352
    
    Merged: [profiler] specially mark allocations during deopt
    Revision: a2a5a4952522f24862967c22a659c2af50dff3b7
    
    BUG=v8:7314
    LOG=N
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=ulan@chromium.org
    
    Change-Id: I71c841a7f7265df6b820640552dd21f41b386151
    Reviewed-on: https://chromium-review.googlesource.com/898522
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6.5@{#23}
    Cr-Branched-From: 73c55f57fe8506011ff854b15026ca765b669700-refs/heads/6.5.254@{#1}
    Cr-Branched-From: 594a1a0b6e551397cfdf50870f6230da34db2dc8-refs/heads/master@{#50664}

--- src/frames.h
+++ src/frames.h
@@ -704,6 +704,7 @@ class JavaScriptFrame : public StandardFrame {
 
   // Accessors.
   virtual JSFunction* function() const;
+  Object* unchecked_function() const;
   Object* receiver() const override;
   Object* context() const override;
   Script* script() const override;
