f7aa8ea00bbf200e9050a22ec84fab4f323849a7
Hannes Payer <hpayer@chromium.org>
2018-03-12 14:44:01 +0100

    [heap] Turn on --write-protect-code-memory.
    Bug: chromium:774108
    Change-Id: I04546edfbc20e92757bd6da0505868f357f8d40d
    Reviewed-on: https://chromium-review.googlesource.com/959005
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Hannes Payer <hpayer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#51884}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -671,7 +671,7 @@ DEFINE_BOOL(incremental_marking_wrappers, true,
 DEFINE_BOOL(trace_unmapper, false, "Trace the unmapping")
 DEFINE_BOOL(parallel_scavenge, true, "parallel scavenge")
 DEFINE_BOOL(trace_parallel_scavenge, false, "trace parallel scavenge")
-DEFINE_BOOL(write_protect_code_memory, false, "write protect code memory")
+DEFINE_BOOL(write_protect_code_memory, true, "write protect code memory")
 #ifdef V8_CONCURRENT_MARKING
 #define V8_CONCURRENT_MARKING_BOOL true
 #else
