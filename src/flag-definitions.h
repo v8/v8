581fd6679b8b6778509a531501b03bd7497f6653
Eric Holk <eholk@chromium.org>
2018-04-13 10:54:16 -0700

    [wasm] enable --wasm-trap-handler by default
    This will give us much better testing coverage for trap-based bounds
    checks.
    
    Note that this will not enable the trap handler by default in Chrome.
    Instead, Chrome will need to explicitly enable the feature using
    V8::EnableWebAssemblyTrapHandler.
    
    Bug: v8:5277
    Change-Id: I7d81f40c6f831c6fe7926375c677908952b78fa2
    Reviewed-on: https://chromium-review.googlesource.com/964711
    Reviewed-by: Ben Titzer <titzer@chromium.org>
    Commit-Queue: Eric Holk <eholk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#52627}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -603,9 +603,9 @@ DEFINE_BOOL(wasm_no_bounds_checks, false,
 DEFINE_BOOL(wasm_no_stack_checks, false,
             "disable stack checks (performance testing only)")
 
-DEFINE_BOOL(wasm_trap_handler, false,
+DEFINE_BOOL(wasm_trap_handler, true,
             "use signal handlers to catch out of bounds memory access in wasm"
-            " (experimental, currently Linux x86_64 only)")
+            " (currently Linux x86_64 only)")
 DEFINE_BOOL(wasm_fuzzer_gen_test, false,
             "Generate a test case when running a wasm fuzzer")
 DEFINE_IMPLICATION(wasm_fuzzer_gen_test, single_threaded)
