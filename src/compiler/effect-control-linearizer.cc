a6d07a809edf24ef42e4540ef079f64a4f7ff51f
Junliang Yan <jyan@ca.ibm.com>
2018-04-17 13:28:10 -0400

    Merged: [turbofan] fix LowerStringFromSingleCodePoint on big-endian
    Revision: 3539c22d63ca876df2303ccd90c1bea1c0a904fa
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=jarin@chromium.org, mstarzinger@chromium.org, sigurds@chromium.org
    
    Bug: v8:7651
    Change-Id: Ie5cf0cc64c014f4dfa7b0230e533f32a2a1eb79e
    Reviewed-on: https://chromium-review.googlesource.com/1015401
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Junliang Yan <jyan@ca.ibm.com>
    Cr-Commit-Position: refs/branch-heads/6.7@{#28}
    Cr-Branched-From: 8457e810efd34381448d51d93f50079cf1f6a812-refs/heads/6.7.288@{#2}
    Cr-Branched-From: e921be5c4f2c6407936bde750992dedbf47c1016-refs/heads/master@{#52547}

--- src/compiler/effect-control-linearizer.cc
+++ src/compiler/effect-control-linearizer.cc
@@ -3167,7 +3167,11 @@ Node* EffectControlLinearizer::LowerStringFromSingleCodePoint(Node* node) {
                                   __ Int32Constant(0xDC00));
 
         // codpoint = (trail << 16) | lead;
+#if V8_TARGET_BIG_ENDIAN
+        code = __ Word32Or(__ Word32Shl(lead, __ Int32Constant(16)), trail);
+#else
         code = __ Word32Or(__ Word32Shl(trail, __ Int32Constant(16)), lead);
+#endif
         break;
       }
     }
