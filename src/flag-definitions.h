386caa2e6bf6584415b1b963114a6e8550f8c876
Sathya Gunasekaran <gsathya@chromium.org>
2018-05-04 15:35:20 -0700

    [ES6] Ship array.prototype.values
    Bug: v8:4247
    Cq-Include-Trybots: luci.v8.try:v8_linux_noi18n_rel_ng
    Change-Id: Ibf5e7dd39cb81ac47af871edbe079d73839f9c1c
    Reviewed-on: https://chromium-review.googlesource.com/1045184
    Reviewed-by: Michael Hablich <hablich@chromium.org>
    Commit-Queue: Sathya Gunasekaran <gsathya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#53034}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -209,7 +209,6 @@ DEFINE_IMPLICATION(harmony_class_fields, harmony_private_fields)
 
 // Features that are still work in progress (behind individual flags).
 #define HARMONY_INPROGRESS(V)                                         \
-  V(harmony_array_prototype_values, "harmony Array.prototype.values") \
   V(harmony_do_expressions, "harmony do-expressions")                 \
   V(harmony_class_fields, "harmony fields in class literals")         \
   V(harmony_static_fields, "harmony static fields in class literals") \
@@ -237,7 +236,8 @@ DEFINE_IMPLICATION(harmony_class_fields, harmony_private_fields)
   V(harmony_optional_catch_binding, "allow omitting binding in catch blocks") \
   V(harmony_import_meta, "harmony import.meta property")                      \
   V(harmony_bigint, "harmony arbitrary precision integers")                   \
-  V(harmony_dynamic_import, "harmony dynamic import")
+  V(harmony_dynamic_import, "harmony dynamic import")                         \
+  V(harmony_array_prototype_values, "harmony Array.prototype.values")
 
 // Once a shipping feature has proved stable in the wild, it will be dropped
 // from HARMONY_SHIPPING, all occurrences of the FLAG_ variable are removed,
