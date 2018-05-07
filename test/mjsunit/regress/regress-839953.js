96186c4f1a9a05bc4f2b4fde7739f20b9ee6d10c
Marja Hölttä <marja@chromium.org>
2018-05-07 10:55:37 +0200

    [in-place weak refs] Fix: allow weak array types in large object space.
    BUG=v8:7308,chromium:839953
    
    Change-Id: I3738dc8169730763a587a2452421a54aff11e38e
    Reviewed-on: https://chromium-review.googlesource.com/1046645
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#53016}

---
+++ test/mjsunit/regress/regress-839953.js
@@ -0,0 +1,11 @@
+// Copyright 2018 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --verify-heap
+
+var code = '';
+for (let i = 0; i < 1 << 16; i++) {
+  code += 'x' + i + '= function() {};\n';
+}
+eval(code);
