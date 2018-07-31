38584fd47e8703e9a927b0515a29260de5f1db85
Michael Starzinger <mstarzinger@chromium.org>
2018-07-31 16:10:07 +0200

    Reland "[wasm] Publish new code from the background threads."
    This is a reland of f991465b4216887abbfc037657c6b779729366ef
    
    Original change's description:
    > [wasm] Publish new code from the background threads.
    > 
    > R=clemensh@chromium.org
    > BUG=v8:7921
    > 
    > Change-Id: Ib86cb5f742907b6e54365827facfc765867ca22e
    > Reviewed-on: https://chromium-review.googlesource.com/1156384
    > Reviewed-by: Clemens Hammacher <clemensh@chromium.org>
    > Commit-Queue: Michael Starzinger <mstarzinger@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#54985}
    
    Bug: v8:7921
    Change-Id: I08c5eb689fe4f8ef2f7b576f0145eb0ae617fd9d
    Reviewed-on: https://chromium-review.googlesource.com/1170603
    Reviewed-by: Clemens Hammacher <clemensh@chromium.org>
    Commit-Queue: Michael Starzinger <mstarzinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#55042}

--- src/compiler/wasm-compiler.cc
+++ src/compiler/wasm-compiler.cc
@@ -5172,6 +5172,7 @@ void TurbofanWasmCompilationUnit::ExecuteCompilation() {
     }
     if (ok_) wasm_code_ = info.wasm_code();
   }
+  if (ok_) wasm_unit_->native_module()->PublishCode(wasm_code_);
 }
 
 wasm::WasmCode* TurbofanWasmCompilationUnit::FinishCompilation(
@@ -5194,8 +5195,6 @@ wasm::WasmCode* TurbofanWasmCompilationUnit::FinishCompilation(
 
     return nullptr;
   }
-
-  wasm_unit_->native_module()->PublishCode(wasm_code_);
   return wasm_code_;
 }
 
