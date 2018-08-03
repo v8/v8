84894ce6d2af7feb9e1f5574409355120887326c
Peter Marshall <petermarshall@chromium.org>
2018-08-03 17:17:44 +0200

    Reland "[cpu-profiler] Turn on detailed line info for optimized code"
    This is a reland of 898f880aa7b45c44c9eaec8e9150c9f89983af10
    
    Original change's description:
    > [cpu-profiler] Turn on detailed line info for optimized code
    > 
    > Cautiously turn on this flag by default to check the impact on
    > performance bots. Could show minor regressions in old space and/or
    > code_and_metadata memory buckets.
    > 
    > Bug: v8:7983
    > Change-Id: Ic4369cdb0231f4f88eada699da948e8bb48a25fd
    > Reviewed-on: https://chromium-review.googlesource.com/1162234
    > Commit-Queue: Yang Guo <yangguo@chromium.org>
    > Reviewed-by: Yang Guo <yangguo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#54908}
    
    Bug: v8:7983
    Change-Id: Iff53bc19c92f26f386948bd4a58e8dcf0493cf0d
    Reviewed-on: https://chromium-review.googlesource.com/1177481
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#55156}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -1257,9 +1257,8 @@ DEFINE_BOOL(log_function_events, false,
 DEFINE_BOOL(prof, false,
             "Log statistical profiling information (implies --log-code).")
 
-DEFINE_BOOL(detailed_line_info, false,
+DEFINE_BOOL(detailed_line_info, true,
             "Always generate detailed line information for CPU profiling.")
-DEFINE_IMPLICATION(future, detailed_line_info)
 
 #if defined(ANDROID)
 // Phones and tablets have processors that are much slower than desktop
