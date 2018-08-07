690bda84eb023c7afa63c51131c474771fbf7bda
Creddy <chandanreddy@google.com>
2018-08-07 15:14:23 +0200

    [Interpreter] Do not use IC slots for property load/stores in an IIFE and top-level code
    An IIFE or top-level code is executed only once hence, there is no need to collect
    type feedback. We can save some memory by not using IC slots for property Loads/Stores
    within a IIFE/top-level code. This CL emits Runtime Get/Set property calls instead of LdaNamedProperty
    /StaNamedProperty for the property loads within a IIFE and top-level code.
    
    Change-Id: I3e0ce26d05d82bb3648cb9262c4e112a2c4556c9
    Reviewed-on: https://chromium-review.googlesource.com/1146579
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Chandan Reddy <chandanreddy@google.com>
    Cr-Commit-Position: refs/heads/master@{#54949}

--- src/parsing/preparser.h
+++ src/parsing/preparser.h
@@ -341,6 +341,7 @@ class PreParserExpression {
 
   // More dummy implementations of things PreParser doesn't need to track:
   void SetShouldEagerCompile() {}
+  void mark_as_iife() {}
 
   int position() const { return kNoSourcePosition; }
   void set_function_token_position(int position) {}
