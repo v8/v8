5d69010e269e3161a95de2030e14f0a291d00b42
Ben L. Titzer <titzer@chromium.org>
2018-08-16 15:15:07 +0200

    [asmjs] Properly validate asm.js heap sizes
    Enforce both engine limitations and spec (http://asmjs.org/spec/latest/)
    limitations on the size of asm.js heaps.
    
    R=clemensh@chromium.org
    CC=​mstarzinger@chromium.org
    
    Bug: chromium:873600
    Change-Id: I104c23bbd0a9a7c494f97f8f9e83ac5a37496dfd
    Reviewed-on: https://chromium-review.googlesource.com/1174411
    Commit-Queue: Ben Titzer <titzer@chromium.org>
    Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#55163}

--- test/mjsunit/wasm/asm-wasm-memory.js
+++ test/mjsunit/wasm/asm-wasm-memory.js
@@ -5,6 +5,7 @@
 // Flags: --validate-asm --allow-natives-syntax
 
 var stdlib = this;
+let kMinHeapSize = 4096;
 
 function assertValidAsm(func) {
   assertTrue(%IsAsmWasmCode(func), "must be valid asm code");
@@ -13,7 +14,7 @@ function assertValidAsm(func) {
 function assertWasm(expected, func, ffi) {
   print("Testing " + func.name + "...");
   assertEquals(
-      expected, func(stdlib, ffi, new ArrayBuffer(1024)).caller());
+      expected, func(stdlib, ffi, new ArrayBuffer(kMinHeapSize)).caller());
   assertValidAsm(func);
 }
 
@@ -38,7 +39,7 @@ assertWasm(7, TestInt32HeapAccess);
 
 
 function TestInt32HeapAccessExternal() {
-  var memory = new ArrayBuffer(1024);
+  var memory = new ArrayBuffer(kMinHeapSize);
   var memory_int32 = new Int32Array(memory);
   var module_decl = eval('(' + TestInt32HeapAccess.toString() + ')');
   var module = module_decl(stdlib, null, memory);
@@ -63,7 +64,7 @@ function TestHeapAccessIntTypes() {
     var code = TestInt32HeapAccess.toString();
     code = code.replace('Int32Array', types[i][1]);
     code = code.replace(/>> 2/g, types[i][2]);
-    var memory = new ArrayBuffer(1024);
+    var memory = new ArrayBuffer(kMinHeapSize);
     var memory_view = new types[i][0](memory);
     var module_decl = eval('(' + code + ')');
     var module = module_decl(stdlib, null, memory);
@@ -102,7 +103,7 @@ assertWasm(1, TestFloatHeapAccess);
 
 
 function TestFloatHeapAccessExternal() {
-  var memory = new ArrayBuffer(1024);
+  var memory = new ArrayBuffer(kMinHeapSize);
   var memory_float64 = new Float64Array(memory);
   var module_decl = eval('(' + TestFloatHeapAccess.toString() + ')');
   var module = module_decl(stdlib, null, memory);
@@ -146,7 +147,7 @@ TestFloatHeapAccessExternal();
     return {load: load, iload: iload, store: store, storeb: storeb};
   }
 
-  var memory = new ArrayBuffer(1024);
+  var memory = new ArrayBuffer(kMinHeapSize);
   var module_decl = eval('(' + TestByteHeapAccessCompat.toString() + ')');
   var m = module_decl(stdlib, null, memory);
   assertValidAsm(module_decl);
