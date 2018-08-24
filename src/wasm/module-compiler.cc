89bea4c050a3b73a21e7380cf805825cd52ab3c8
Stephan Herhut <herhut@chromium.org>
2018-08-24 10:43:38 +0200

    Reland "Use new arraybuffer deleter interface in d8"
    This is a reland of 524215be1a29a23fcb44eee31ae98f1cac0c7746
    
    Original change's description:
    > Use new arraybuffer deleter interface in d8
    > 
    > With this cl we start using the custom deleter to free externalized
    > array buffers. This also allows us to keep wasm memories registered
    > with the wasm memory tracker and thereby to propagate that a memory
    > is wasm allocated over postMessage calls.
    > 
    > Bug: v8:8073, chromium:836800
    > Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    > Change-Id: I57e3ea44d9c6633ada7996677dd1de4da810ab64
    > Reviewed-on: https://chromium-review.googlesource.com/1186681
    > Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    > Commit-Queue: Stephan Herhut <herhut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#55361}
    
    Bug: v8:8073, chromium:836800
    Change-Id: Ia3c057ced496363cfdd07eed16ed1d0c7a3f3084
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/1188222
    Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    Commit-Queue: Stephan Herhut <herhut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#55389}

--- src/wasm/module-compiler.cc
+++ src/wasm/module-compiler.cc
@@ -992,13 +992,9 @@ MaybeHandle<WasmInstanceObject> InstanceBuilder::Build() {
     Handle<JSArrayBuffer> memory = memory_.ToHandleChecked();
     memory->set_is_neuterable(false);
 
-    DCHECK_IMPLIES(use_trap_handler(),
-                   module_->origin == kAsmJsOrigin ||
-                       memory->is_wasm_memory() ||
-                       memory->backing_store() == nullptr ||
-                       // TODO(836800) Remove once is_wasm_memory transfers over
-                       // post-message.
-                       (enabled_.threads && memory->is_shared()));
+    DCHECK_IMPLIES(use_trap_handler(), module_->origin == kAsmJsOrigin ||
+                                           memory->is_wasm_memory() ||
+                                           memory->backing_store() == nullptr);
   } else if (initial_pages > 0 || use_trap_handler()) {
     // We need to unconditionally create a guard region if using trap handlers,
     // even when the size is zero to prevent null-dereference issues
