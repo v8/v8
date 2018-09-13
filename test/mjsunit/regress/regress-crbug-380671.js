e938b7a3e52e60b654cb543eaa827394e9bcbca4
Creddy <chandanreddy@google.com>
2018-09-13 18:14:29 +0200

    Enable OneShot optimizations by default
    BUG=v8:8072
    
    Change-Id: I2068d66644628fe1de7a6be30b8d43ecfae04e93
    Reviewed-on: https://chromium-review.googlesource.com/1220047
    Commit-Queue: Chandan Reddy <chandanreddy@google.com>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#55875}

--- test/mjsunit/regress/regress-crbug-380671.js
+++ test/mjsunit/regress/regress-crbug-380671.js
@@ -4,11 +4,15 @@
 
 // Flags: --mock-arraybuffer-allocator --expose-gc
 
-var buffer = new ArrayBuffer(0xc0000000);
-assertEquals(0xc0000000, buffer.byteLength);
-// We call the GC here to free up the large array buffer. Otherwise, the
-// mock allocator would allow us to allocate more than the physical memory
-// available on 32bit platforms, leaving the internal counters in an invalid
-// state.
-buffer = null;
-gc();
+function test() {
+  var buffer = new ArrayBuffer(0xc0000000);
+  assertEquals(0xc0000000, buffer.byteLength);
+  // We call the GC here to free up the large array buffer. Otherwise, the
+  // mock allocator would allow us to allocate more than the physical memory
+  // available on 32bit platforms, leaving the internal counters in an invalid
+  // state.
+  buffer = null;
+  gc();
+}
+
+test();
