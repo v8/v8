6989ec70a999e169122a59fc93f0957e77b74a86
Maya Lekova <mslekova@chromium.org>
2018-09-19 13:31:23 +0200

    [turbofan] Move JSHeapBroker and CompilationDependencies to another zone
    To make code consistent with fef047a4a5d98e763b7641e8c2f7248d63ba85da
    JSHeapBroker and CompilationDependencies are now being allocated and are using
    OptimizedCompilationInfo's zone, which is shared between compiler stages.
    
    Bug: v8:7790
    Change-Id: If622ce8b39587ff1c87700ab70e9bd4bc1acc68f
    Reviewed-on: https://chromium-review.googlesource.com/1233616
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Maya Lekova <mslekova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#56066}

--- src/compiler/pipeline.cc
+++ src/compiler/pipeline.cc
@@ -137,9 +137,9 @@ class PipelineData {
     javascript_ = new (graph_zone_) JSOperatorBuilder(graph_zone_);
     jsgraph_ = new (graph_zone_)
         JSGraph(isolate_, graph_, common_, javascript_, simplified_, machine_);
-    js_heap_broker_ = new (codegen_zone_) JSHeapBroker(isolate_, codegen_zone_);
+    js_heap_broker_ = new (info_->zone()) JSHeapBroker(isolate_, info_->zone());
     dependencies_ =
-        new (codegen_zone_) CompilationDependencies(isolate_, codegen_zone_);
+        new (info_->zone()) CompilationDependencies(isolate_, info_->zone());
   }
 
   // For WebAssembly compile entry point.
