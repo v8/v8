a3b97babfb19490a948a931b3966cf4ff8c2a2bd
Frank Tang <ftang@chromium.org>
2018-09-24 15:24:16 -0700

    [Intl] Ship Intl.RelativeTimeFormat
    Bug: v8:7869
    Change-Id: If87ab5126a41513d0c69acc849d483159f1a2fde
    Reviewed-on: https://chromium-review.googlesource.com/1241317
    Reviewed-by: Sathya Gunasekaran <gsathya@chromium.org>
    Commit-Queue: Frank Tang <ftang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#56223}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -226,23 +226,15 @@ DEFINE_IMPLICATION(harmony_class_fields, harmony_private_fields)
 #endif
 
 // Features that are complete (but still behind --harmony/es-staging flag).
-#define HARMONY_STAGED_BASE(V)                                             \
+#define HARMONY_STAGED(V)                                                  \
   V(harmony_public_fields, "harmony public fields in class literals")      \
   V(harmony_private_fields, "harmony private fields in class literals")    \
   V(harmony_numeric_separator, "harmony numeric separator between digits") \
   V(harmony_string_matchall, "harmony String.prototype.matchAll")          \
   V(harmony_static_fields, "harmony static fields in class literals")
 
-#ifdef V8_INTL_SUPPORT
-#define HARMONY_STAGED(V) \
-  HARMONY_STAGED_BASE(V)  \
-  V(harmony_intl_relative_time_format, "Intl.RelativeTimeFormat")
-#else
-#define HARMONY_STAGED(V) HARMONY_STAGED_BASE(V)
-#endif
-
 // Features that are shipping (turned on by default, but internal flag remains).
-#define HARMONY_SHIPPING(V)                                              \
+#define HARMONY_SHIPPING_BASE(V)                                         \
   V(harmony_string_trimming, "harmony String.prototype.trim{Start,End}") \
   V(harmony_sharedarraybuffer, "harmony sharedarraybuffer")              \
   V(harmony_function_tostring, "harmony Function.prototype.toString")    \
@@ -253,6 +245,14 @@ DEFINE_IMPLICATION(harmony_class_fields, harmony_private_fields)
   V(harmony_array_flat, "harmony Array.prototype.{flat,flatMap}")        \
   V(harmony_symbol_description, "harmony Symbol.prototype.description")  \
   V(harmony_global, "harmony global")
+
+#ifdef V8_INTL_SUPPORT
+#define HARMONY_SHIPPING(V) \
+  HARMONY_SHIPPING_BASE(V)  \
+  V(harmony_intl_relative_time_format, "Intl.RelativeTimeFormat")
+#else
+#define HARMONY_SHIPPING(V) HARMONY_SHIPPING_BASE(V)
+#endif
 
 // Once a shipping feature has proved stable in the wild, it will be dropped
 // from HARMONY_SHIPPING, all occurrences of the FLAG_ variable are removed,
