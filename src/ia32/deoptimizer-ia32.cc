3815ad6db2d8c222a4dc1b64c5b80a9f66ce8899
Sigurd Schneider <sigurds@chromium.org>
2018-09-25 10:19:48 +0200

    [ia32,root] Refactor register renaming
    This CL addresses comments left post-merge on
    https://crrev.com/c/1238653
    which improve readablity.
    
    R=jarin@chromium.org
    
    Bug: v8:6666, v8:8015
    Change-Id: Idafd848dafdd97af525646d6321e0ca40ce856c5
    Reviewed-on: https://chromium-review.googlesource.com/1242885
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#56192}

--- src/ia32/deoptimizer-ia32.cc
+++ src/ia32/deoptimizer-ia32.cc
@@ -47,12 +47,6 @@ void Deoptimizer::TableEntryGenerator::Generate() {
 
   __ pushad();
 
-  static constexpr Register scratch0 = esi;
-  static constexpr Register scratch1 = ecx;
-  static constexpr Register scratch2 = edx;
-  static constexpr Register scratch3 = eax;
-  static constexpr Register scratch4 = edi;
-
   ExternalReference c_entry_fp_address =
       ExternalReference::Create(IsolateAddressId::kCEntryFPAddress, isolate());
   __ mov(masm()->StaticVariable(c_entry_fp_address), ebp);
@@ -61,31 +55,30 @@ void Deoptimizer::TableEntryGenerator::Generate() {
       kNumberOfRegisters * kPointerSize + kDoubleRegsSize + kFloatRegsSize;
 
   // Get the bailout id from the stack.
-  __ mov(scratch0, Operand(esp, kSavedRegistersAreaSize));
+  __ mov(esi, Operand(esp, kSavedRegistersAreaSize));
 
   // Get the address of the location in the code object
-  // and compute the fp-to-sp delta in register scratch2.
-  __ mov(scratch1, Operand(esp, kSavedRegistersAreaSize + 1 * kPointerSize));
-  __ lea(scratch2, Operand(esp, kSavedRegistersAreaSize + 2 * kPointerSize));
+  // and compute the fp-to-sp delta in register edx.
+  __ mov(ecx, Operand(esp, kSavedRegistersAreaSize + 1 * kPointerSize));
+  __ lea(edx, Operand(esp, kSavedRegistersAreaSize + 2 * kPointerSize));
 
-  __ sub(scratch2, ebp);
-  __ neg(scratch2);
+  __ sub(edx, ebp);
+  __ neg(edx);
 
   // Allocate a new deoptimizer object.
-  __ PrepareCallCFunction(6, scratch3);
-  __ mov(scratch3, Immediate(0));
+  __ PrepareCallCFunction(6, eax);
+  __ mov(eax, Immediate(0));
   Label context_check;
-  __ mov(scratch4,
-         Operand(ebp, CommonFrameConstants::kContextOrFrameTypeOffset));
-  __ JumpIfSmi(scratch4, &context_check);
-  __ mov(scratch3, Operand(ebp, JavaScriptFrameConstants::kFunctionOffset));
+  __ mov(edi, Operand(ebp, CommonFrameConstants::kContextOrFrameTypeOffset));
+  __ JumpIfSmi(edi, &context_check);
+  __ mov(eax, Operand(ebp, JavaScriptFrameConstants::kFunctionOffset));
   __ bind(&context_check);
-  __ mov(Operand(esp, 0 * kPointerSize), scratch3);  // Function.
+  __ mov(Operand(esp, 0 * kPointerSize), eax);  // Function.
   __ mov(Operand(esp, 1 * kPointerSize),
          Immediate(static_cast<int>(deopt_kind())));
-  __ mov(Operand(esp, 2 * kPointerSize), scratch0);  // Bailout id.
-  __ mov(Operand(esp, 3 * kPointerSize), scratch1);  // Code address or 0.
-  __ mov(Operand(esp, 4 * kPointerSize), scratch2);  // Fp-to-sp delta.
+  __ mov(Operand(esp, 2 * kPointerSize), esi);  // Bailout id.
+  __ mov(Operand(esp, 3 * kPointerSize), ecx);  // Code address or 0.
+  __ mov(Operand(esp, 4 * kPointerSize), edx);  // Fp-to-sp delta.
   __ mov(Operand(esp, 5 * kPointerSize),
          Immediate(ExternalReference::isolate_address(isolate())));
   {
@@ -93,21 +86,21 @@ void Deoptimizer::TableEntryGenerator::Generate() {
     __ CallCFunction(ExternalReference::new_deoptimizer_function(), 6);
   }
 
-  // Preserve deoptimizer object in register scratch3 and get the input
+  // Preserve deoptimizer object in register eax and get the input
   // frame descriptor pointer.
-  __ mov(scratch0, Operand(scratch3, Deoptimizer::input_offset()));
+  __ mov(esi, Operand(eax, Deoptimizer::input_offset()));
 
   // Fill in the input registers.
   for (int i = kNumberOfRegisters - 1; i >= 0; i--) {
     int offset = (i * kPointerSize) + FrameDescription::registers_offset();
-    __ pop(Operand(scratch0, offset));
+    __ pop(Operand(esi, offset));
   }
 
   int float_regs_offset = FrameDescription::float_registers_offset();
   // Fill in the float input registers.
   for (int i = 0; i < XMMRegister::kNumRegisters; i++) {
     int dst_offset = i * kFloatSize + float_regs_offset;
-    __ pop(Operand(scratch0, dst_offset));
+    __ pop(Operand(esi, dst_offset));
   }
 
   int double_regs_offset = FrameDescription::double_registers_offset();
@@ -117,7 +110,7 @@ void Deoptimizer::TableEntryGenerator::Generate() {
     int dst_offset = code * kDoubleSize + double_regs_offset;
     int src_offset = code * kDoubleSize;
     __ movsd(xmm0, Operand(esp, src_offset));
-    __ movsd(Operand(scratch0, dst_offset), xmm0);
+    __ movsd(Operand(esi, dst_offset), xmm0);
   }
 
   // Clear FPU all exceptions.
@@ -128,62 +121,61 @@ void Deoptimizer::TableEntryGenerator::Generate() {
   // Remove the bailout id, return address and the double registers.
   __ add(esp, Immediate(kDoubleRegsSize + 2 * kPointerSize));
 
-  // Compute a pointer to the unwinding limit in register scratch1; that is
+  // Compute a pointer to the unwinding limit in register ecx; that is
   // the first stack slot not part of the input frame.
-  __ mov(scratch1, Operand(scratch0, FrameDescription::frame_size_offset()));
-  __ add(scratch1, esp);
+  __ mov(ecx, Operand(esi, FrameDescription::frame_size_offset()));
+  __ add(ecx, esp);
 
   // Unwind the stack down to - but not including - the unwinding
   // limit and copy the contents of the activation frame to the input
   // frame description.
-  __ lea(scratch2, Operand(scratch0, FrameDescription::frame_content_offset()));
+  __ lea(edx, Operand(esi, FrameDescription::frame_content_offset()));
   Label pop_loop_header;
   __ jmp(&pop_loop_header);
   Label pop_loop;
   __ bind(&pop_loop);
-  __ pop(Operand(scratch2, 0));
-  __ add(scratch2, Immediate(sizeof(uint32_t)));
+  __ pop(Operand(edx, 0));
+  __ add(edx, Immediate(sizeof(uint32_t)));
   __ bind(&pop_loop_header);
-  __ cmp(scratch1, esp);
+  __ cmp(ecx, esp);
   __ j(not_equal, &pop_loop);
 
   // Compute the output frame in the deoptimizer.
-  __ push(scratch3);
-  __ PrepareCallCFunction(1, scratch0);
-  __ mov(Operand(esp, 0 * kPointerSize), scratch3);
+  __ push(eax);
+  __ PrepareCallCFunction(1, esi);
+  __ mov(Operand(esp, 0 * kPointerSize), eax);
   {
     AllowExternalCallThatCantCauseGC scope(masm());
     __ CallCFunction(ExternalReference::compute_output_frames_function(), 1);
   }
-  __ pop(scratch3);
+  __ pop(eax);
 
-  __ mov(esp, Operand(scratch3, Deoptimizer::caller_frame_top_offset()));
+  __ mov(esp, Operand(eax, Deoptimizer::caller_frame_top_offset()));
 
   // Replace the current (input) frame with the output frames.
   Label outer_push_loop, inner_push_loop,
       outer_loop_header, inner_loop_header;
-  // Outer loop state: scratch3 = current FrameDescription**, scratch2 = one
+  // Outer loop state: eax = current FrameDescription**, edx = one
   // past the last FrameDescription**.
-  __ mov(scratch2, Operand(scratch3, Deoptimizer::output_count_offset()));
-  __ mov(scratch3, Operand(eax, Deoptimizer::output_offset()));
-  __ lea(scratch2, Operand(scratch3, edx, times_4, 0));
+  __ mov(edx, Operand(eax, Deoptimizer::output_count_offset()));
+  __ mov(eax, Operand(eax, Deoptimizer::output_offset()));
+  __ lea(edx, Operand(eax, edx, times_4, 0));
   __ jmp(&outer_loop_header);
   __ bind(&outer_push_loop);
-  // Inner loop state: scratch0 = current FrameDescription*, scratch1 = loop
+  // Inner loop state: esi = current FrameDescription*, ecx = loop
   // index.
-  __ mov(scratch0, Operand(scratch3, 0));
-  __ mov(scratch1, Operand(scratch0, FrameDescription::frame_size_offset()));
+  __ mov(esi, Operand(eax, 0));
+  __ mov(ecx, Operand(esi, FrameDescription::frame_size_offset()));
   __ jmp(&inner_loop_header);
   __ bind(&inner_push_loop);
-  __ sub(scratch1, Immediate(sizeof(uint32_t)));
-  __ push(Operand(scratch0, scratch1, times_1,
-                  FrameDescription::frame_content_offset()));
+  __ sub(ecx, Immediate(sizeof(uint32_t)));
+  __ push(Operand(esi, ecx, times_1, FrameDescription::frame_content_offset()));
   __ bind(&inner_loop_header);
-  __ test(scratch1, scratch1);
+  __ test(ecx, ecx);
   __ j(not_zero, &inner_push_loop);
-  __ add(scratch3, Immediate(kPointerSize));
+  __ add(eax, Immediate(kPointerSize));
   __ bind(&outer_loop_header);
-  __ cmp(scratch3, scratch2);
+  __ cmp(eax, edx);
   __ j(below, &outer_push_loop);
 
   // In case of a failed STUB, we have to restore the XMM registers.
@@ -191,17 +183,17 @@ void Deoptimizer::TableEntryGenerator::Generate() {
     int code = config->GetAllocatableDoubleCode(i);
     XMMRegister xmm_reg = XMMRegister::from_code(code);
     int src_offset = code * kDoubleSize + double_regs_offset;
-    __ movsd(xmm_reg, Operand(scratch0, src_offset));
+    __ movsd(xmm_reg, Operand(esi, src_offset));
   }
 
   // Push pc and continuation from the last output frame.
-  __ push(Operand(scratch0, FrameDescription::pc_offset()));
-  __ push(Operand(scratch0, FrameDescription::continuation_offset()));
+  __ push(Operand(esi, FrameDescription::pc_offset()));
+  __ push(Operand(esi, FrameDescription::continuation_offset()));
 
   // Push the registers from the last output frame.
   for (int i = 0; i < kNumberOfRegisters; i++) {
     int offset = (i * kPointerSize) + FrameDescription::registers_offset();
-    __ push(Operand(scratch0, offset));
+    __ push(Operand(esi, offset));
   }
 
   // Restore the registers from the stack.
