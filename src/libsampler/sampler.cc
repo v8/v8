e2a577c1056294db6d7406fec1c7758ffbbb8ad4
Torne (Richard Coles) <torne@google.com>
2018-09-28 14:45:52 -0400

    Remove support for obsolete bionic versions.
    The current Android NDK defines __BIONIC_HAVE_UCONTEXT_T for all
    architecures, so the old paths are no longer needed.
    
    Bug: chromium:437330
    Change-Id: I6314971e9ee1d78c4b73f8c1b37af7aa6f419b71
    Reviewed-on: https://chromium-review.googlesource.com/1252282
    Reviewed-by: Hannes Payer <hpayer@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#56322}

--- src/libsampler/sampler.cc
+++ src/libsampler/sampler.cc
@@ -21,19 +21,11 @@
 #include <mach/mach.h>
 // OpenBSD doesn't have <ucontext.h>. ucontext_t lives in <signal.h>
 // and is a typedef for struct sigcontext. There is no uc_mcontext.
-#elif(!V8_OS_ANDROID || defined(__BIONIC_HAVE_UCONTEXT_T)) && !V8_OS_OPENBSD
+#elif !V8_OS_OPENBSD
 #include <ucontext.h>
 #endif
 
 #include <unistd.h>
-
-// GLibc on ARM defines mcontext_t has a typedef for 'struct sigcontext'.
-// Old versions of the C library <signal.h> didn't define the type.
-#if V8_OS_ANDROID && !defined(__BIONIC_HAVE_UCONTEXT_T) && \
-    (defined(__arm__) || defined(__aarch64__)) && \
-    !defined(__BIONIC_HAVE_STRUCT_SIGCONTEXT)
-#include <asm/sigcontext.h>  // NOLINT
-#endif
 
 #elif V8_OS_WIN || V8_OS_CYGWIN
 
