994b23945cac45eff321c4fe19f1132cfc7a704b
Frank Tang <ftang@chromium.org>
2018-10-06 01:53:28 -0700

    [Intl] Add unit tests for Intl.Segmenter
    Bug: v8:6891
    Cq-Include-Trybots: luci.v8.try:v8_linux_noi18n_rel_ng
    Change-Id: I075c4f615a4366c34723104410e8445054a3cacd
    Reviewed-on: https://chromium-review.googlesource.com/c/1256867
    Reviewed-by: Sathya Gunasekaran <gsathya@chromium.org>
    Reviewed-by: Daniel Ehrenberg <littledan@chromium.org>
    Commit-Queue: Frank Tang <ftang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#56424}

--- test/intl/segmenter/constructor.js
+++ test/intl/segmenter/constructor.js
@@ -1,5 +1,5 @@
 // Copyright 2018 the V8 project authors. All rights reserved.
-// Use of this source code is governed by a BSD-lineBreakStyle license that can be
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // Flags: --harmony-intl-segmenter
