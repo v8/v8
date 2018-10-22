c949f19b457330fc9006d236d5d0a37cafa01696
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-10-22 07:18:49 -0700

    inspector: add couple tests
    Test to cover a lot of injected script source corner cases.
    
    R=dgozman@chromium.org
    
    Bug: chromium:595206
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_headless_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia631de58c5a92b39ec3933c56cf7e3f108b9bd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/1292688
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#56863}

--- test/inspector/protocol-test.js
+++ test/inspector/protocol-test.js
@@ -142,14 +142,22 @@ InspectorTest.ContextGroup = class {
 
   setupInjectedScriptEnvironment(session) {
     let scriptSource = '';
-    // First define all getters on Object.prototype.
-    let injectedScriptSource = utils.read('src/inspector/injected-script-source.js');
-    let getterRegex = /\.[a-zA-Z0-9]+/g;
-    let match;
-    let getters = new Set();
-    while (match = getterRegex.exec(injectedScriptSource)) {
-      getters.add(match[0].substr(1));
-    }
+    let getters = ["length","internalConstructorName","subtype","getProperty",
+        "objectHasOwnProperty","nullifyPrototype","primitiveTypes",
+        "closureTypes","prototype","all","RemoteObject","bind",
+        "PropertyDescriptor","object","get","set","value","configurable",
+        "enumerable","symbol","getPrototypeOf","nativeAccessorDescriptor",
+        "isBuiltin","hasGetter","hasSetter","getOwnPropertyDescriptor",
+        "description","formatAccessorsAsProperties","isOwn","name",
+        "typedArrayProperties","keys","getOwnPropertyNames",
+        "getOwnPropertySymbols","isPrimitiveValue","com","toLowerCase",
+        "ELEMENT","trim","replace","DOCUMENT","size","byteLength","toString",
+        "stack","substr","message","indexOf","key","type","unserializableValue",
+        "objectId","className","preview","proxyTargetValue","customPreview",
+        "CustomPreview","resolve","then","console","error","header","hasBody",
+        "stringify","ObjectPreview","ObjectPreviewType","properties",
+        "ObjectPreviewSubtype","getInternalProperties","wasThrown","indexes",
+        "overflow","valuePreview","entries"];
     scriptSource += `(function installSettersAndGetters() {
         let defineProperty = Object.defineProperty;
         let ObjectPrototype = Object.prototype;
@@ -158,7 +166,7 @@ InspectorTest.ContextGroup = class {
           set() { debugger; throw 42; }, get() { debugger; throw 42; },
           __proto__: null
         });`,
-        scriptSource += Array.from(getters).map(getter => `
+        scriptSource += getters.map(getter => `
         defineProperty(ObjectPrototype, '${getter}', {
           set() { debugger; throw 42; }, get() { debugger; throw 42; },
           __proto__: null
@@ -401,6 +409,9 @@ InspectorTest.runTestSuite = function(testSuite) {
 }
 
 InspectorTest.runAsyncTestSuite = async function(testSuite) {
+  const selected = testSuite.filter(test => test.name.startsWith('f_'));
+  if (selected.length)
+    testSuite = selected;
   for (var test of testSuite) {
     InspectorTest.log("\nRunning test: " + test.name);
     try {
