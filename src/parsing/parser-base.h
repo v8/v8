9df7ee392dd59a462a1ca7d201bc811e6fbb8be4
Toon Verwaest <verwaest@chromium.org>
2018-10-25 14:14:20 +0200

    [parser] Remove RETURN_IF* from ParseNewTarget
    Bug: v8:8363, v8:7926
    Change-Id: I45df1db051c69b05e0b84e432a072fd6bbdcf6d4
    Reviewed-on: https://chromium-review.googlesource.com/c/1299083
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#56988}

--- src/parsing/parser-base.h
+++ src/parsing/parser-base.h
@@ -3519,7 +3519,6 @@ typename ParserBase<Impl>::ExpressionT
 ParserBase<Impl>::ParseNewTargetExpression() {
   int pos = position();
   ExpectMetaProperty(Token::TARGET, "new.target", pos);
-  RETURN_IF_PARSE_ERROR;
 
   classifier()->RecordAssignmentPatternError(
       Scanner::Location(pos, end_position()),
