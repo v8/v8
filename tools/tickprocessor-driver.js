b32ee7b0f25a85611e7e709e67a098ae48a26a9d
Yang Guo <yangguo@chromium.org>
2018-10-31 07:37:33 +0100

    [d8] pass --no-arguments to omit top-level arguments
    TBR=petermarshall@chromium.org
    
    Bug: v8:8385
    Change-Id: Iba13004e0fd03a82cb65ed497d4bd2b4d006b424
    Reviewed-on: https://chromium-review.googlesource.com/c/1307417
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#57155}

--- tools/tickprocessor-driver.js
+++ tools/tickprocessor-driver.js
@@ -55,7 +55,7 @@ var entriesProviders = {
   'mac': MacCppEntriesProvider
 };
 
-var params = processArguments(arguments || []);
+var params = processArguments(arguments);
 var sourceMap = null;
 if (params.sourceMap) {
   initSourceMapSupport();
