fc5c8d7f7802d77d393a7ec9ad6ed7e3e5cb973c
Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-10-31 15:58:11 +0000

    Revert "inspector: move injected script source to native"
    This reverts commit 7e079c660b686d916ed5cd88bfa4ecf597300193.
    
    Reason for revert: native implementation should be ready for navigation.
    
    Original change's description:
    > inspector: move injected script source to native
    > 
    > - introduced ValueMirror interface, this interface contains methods to generate
    >   different protocol entities,
    > - introduced DebugPropertyIterator, this iterator iterates through object properties
    >   in the following order: exotic indices, enumerable strings, all other properties,
    > - removed all injected script infra, e.g. closure compiler,
    > 
    > R=​dgozman@chromium.org
    > TBR=yangguo@chromium.org
    > 
    > Bug: chromium:595206
    > Change-Id: Idcfc04489ee52e015ad1d1d191c3474cc65e63f2
    > Reviewed-on: https://chromium-review.googlesource.com/c/1308353
    > Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    > Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#57150}
    
    TBR=dgozman@chromium.org,yangguo@chromium.org,kozyatinskiy@chromium.org
    
    Change-Id: I8c5c61f4cfe5a66cd33eadd02ab4acec539cc3bb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:595206
    Reviewed-on: https://chromium-review.googlesource.com/c/1310055
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#57176}

--- src/inspector/v8-debugger.h
+++ src/inspector/v8-debugger.h
@@ -30,9 +30,6 @@ class V8InspectorImpl;
 class V8StackTraceImpl;
 struct V8StackTraceId;
 
-enum class WrapMode { kForceValue, kNoPreview, kWithPreview };
-enum class V8InternalValueType { kNone, kEntry, kScope, kScopeList };
-
 using protocol::Response;
 using ScheduleStepIntoAsyncCallback =
     protocol::Debugger::Backend::ScheduleStepIntoAsyncCallback;
@@ -137,12 +134,6 @@ class V8Debugger : public v8::debug::DebugDelegate,
   std::shared_ptr<AsyncStackTrace> stackTraceFor(int contextGroupId,
                                                  const V8StackTraceId& id);
 
-  bool addInternalObject(v8::Local<v8::Context> context,
-                         v8::Local<v8::Object> object,
-                         V8InternalValueType type);
-  V8InternalValueType getInternalType(v8::Local<v8::Context> context,
-                                      v8::Local<v8::Object> object);
-
  private:
   void clearContinueToLocation();
   bool shouldContinueToCurrentLocation();
@@ -169,8 +160,6 @@ class V8Debugger : public v8::debug::DebugDelegate,
                                            v8::Local<v8::Function>);
   v8::MaybeLocal<v8::Value> generatorScopes(v8::Local<v8::Context>,
                                             v8::Local<v8::Value>);
-  v8::MaybeLocal<v8::Array> collectionsEntries(v8::Local<v8::Context> context,
-                                               v8::Local<v8::Value> value);
 
   void asyncTaskScheduledForStack(const String16& taskName, void* task,
                                   bool recurring);
@@ -263,8 +252,6 @@ class V8Debugger : public v8::debug::DebugDelegate,
 
   uint32_t m_lastStableObjectId = 0;
   v8::Global<v8::debug::WeakMap> m_stableObjectId;
-
-  v8::Global<v8::debug::WeakMap> m_internalObjects;
 
   WasmTranslation m_wasmTranslation;
 
