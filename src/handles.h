434cd5c0eb7282a60063cd13c59fb8bb7e23d5a3
Clemens Hammacher <clemensh@chromium.org>
2018-11-12 10:35:06 +0100

    std::unary_function and binary removed from C++17
    Fixes building in MSVS with -std:c++17
    
    Refs: https://en.cppreference.com/w/cpp/utility/functional/unary_function
    Change-Id: Ibb05b6810cae029aae33957f3eb52cc3a9e4863e
    Reviewed-on: https://chromium-review.googlesource.com/c/1227322
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#57428}

--- src/handles.h
+++ src/handles.h
@@ -186,14 +186,14 @@ class Handle final : public HandleBase {
   bool equals(Handle<T> other) const { return address() == other.address(); }
 
   // Provide function object for location equality comparison.
-  struct equal_to : public std::binary_function<Handle<T>, Handle<T>, bool> {
+  struct equal_to {
     V8_INLINE bool operator()(Handle<T> lhs, Handle<T> rhs) const {
       return lhs.equals(rhs);
     }
   };
 
   // Provide function object for location hashing.
-  struct hash : public std::unary_function<Handle<T>, size_t> {
+  struct hash {
     V8_INLINE size_t operator()(Handle<T> const& handle) const {
       return base::hash<Address>()(handle.address());
     }
