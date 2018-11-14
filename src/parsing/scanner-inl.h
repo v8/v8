ed77644b6454f696f441383176ea80e134be454e
Leszek Swirski <leszeks@chromium.org>
2018-11-14 12:32:35 +0100

    [parser] Switch on token in ScanSingleToken
    Always generate a token for the first character in ScanSingleToken, and
    switch on that. If the token could change with subsequent characters, e.g.
    Token::ADD into Token::ASSIGN_ADD, then handle that in that token's case
    rather than going character-by-character.
    
    This allows us to have a tighter packing of the cases, and early detect
    numbers, keywords and identifiers.
    
    Change-Id: I8c3ac7a5453547abeb09fc90826a26390b15a415
    Reviewed-on: https://chromium-review.googlesource.com/c/1335547
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#57497}

--- src/parsing/scanner-inl.h
+++ src/parsing/scanner-inl.h
@@ -15,139 +15,60 @@ namespace internal {
 // Make sure tokens are stored as a single byte.
 STATIC_ASSERT(sizeof(Token::Value) == 1);
 
+// Get the shortest token that this character starts, the token may change
+// depending on subsequent characters.
+constexpr Token::Value GetOneCharToken(char c) {
+  // clang-format off
+  return
+      c == '(' ? Token::LPAREN :
+      c == ')' ? Token::RPAREN :
+      c == '{' ? Token::LBRACE :
+      c == '}' ? Token::RBRACE :
+      c == '[' ? Token::LBRACK :
+      c == ']' ? Token::RBRACK :
+      c == '?' ? Token::CONDITIONAL :
+      c == ':' ? Token::COLON :
+      c == ';' ? Token::SEMICOLON :
+      c == ',' ? Token::COMMA :
+      c == '.' ? Token::PERIOD :
+      c == '|' ? Token::BIT_OR :
+      c == '&' ? Token::BIT_AND :
+      c == '^' ? Token::BIT_XOR :
+      c == '~' ? Token::BIT_NOT :
+      c == '!' ? Token::NOT :
+      c == '<' ? Token::LT :
+      c == '>' ? Token::GT :
+      c == '%' ? Token::MOD :
+      c == '=' ? Token::ASSIGN :
+      c == '+' ? Token::ADD :
+      c == '-' ? Token::SUB :
+      c == '*' ? Token::MUL :
+      c == '/' ? Token::DIV :
+      c == '#' ? Token::PRIVATE_NAME :
+      c == '"' ? Token::STRING :
+      c == '\'' ? Token::STRING :
+      c == '`' ? Token::TEMPLATE_SPAN :
+      c == '\\' ? Token::IDENTIFIER :
+      // Whitespace or line terminator
+      c == ' ' ? Token::WHITESPACE :
+      c == '\t' ? Token::WHITESPACE :
+      c == '\v' ? Token::WHITESPACE :
+      c == '\f' ? Token::WHITESPACE :
+      c == '\r' ? Token::WHITESPACE :
+      c == '\n' ? Token::WHITESPACE :
+      // IsDecimalDigit must be tested before IsAsciiIdentifier
+      IsDecimalDigit(c) ? Token::NUMBER :
+      IsAsciiIdentifier(c) ? Token::IDENTIFIER :
+      Token::ILLEGAL;
+  // clang-format on
+}
+
 // Table of one-character tokens, by character (0x00..0x7F only).
-// clang-format off
-static const Token::Value one_char_tokens[] = {
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::LPAREN,       // 0x28
-  Token::RPAREN,       // 0x29
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::COMMA,        // 0x2C
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::COLON,        // 0x3A
-  Token::SEMICOLON,    // 0x3B
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::CONDITIONAL,  // 0x3F
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::LBRACK,     // 0x5B
-  Token::ILLEGAL,
-  Token::RBRACK,     // 0x5D
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::ILLEGAL,
-  Token::LBRACE,       // 0x7B
-  Token::ILLEGAL,
-  Token::RBRACE,       // 0x7D
-  Token::BIT_NOT,      // 0x7E
-  Token::ILLEGAL
+static const constexpr Token::Value one_char_tokens[128] = {
+#define CALL_GET_SCAN_FLAGS(N) GetOneCharToken(N),
+    INT_0_TO_127_LIST(CALL_GET_SCAN_FLAGS)
+#undef CALL_GET_SCAN_FLAGS
 };
-// clang-format on
 
 // ----------------------------------------------------------------------------
 // Keyword Matcher
@@ -396,162 +317,182 @@ V8_INLINE Token::Value Scanner::ScanSingleToken() {
   do {
     next().location.beg_pos = source_pos();
 
-    if (static_cast<unsigned>(c0_) <= 0x7F) {
-      Token::Value token = one_char_tokens[c0_];
-      if (token != Token::ILLEGAL) {
-        Advance();
-        return token;
+    if (V8_LIKELY(static_cast<unsigned>(c0_) <= kMaxAscii)) {
+      token = one_char_tokens[c0_];
+
+      switch (token) {
+        case Token::LPAREN:
+        case Token::RPAREN:
+        case Token::LBRACE:
+        case Token::RBRACE:
+        case Token::LBRACK:
+        case Token::RBRACK:
+        case Token::CONDITIONAL:
+        case Token::COLON:
+        case Token::SEMICOLON:
+        case Token::COMMA:
+        case Token::BIT_NOT:
+        case Token::ILLEGAL:
+          // One character tokens.
+          return Select(token);
+
+        case Token::STRING:
+          return ScanString();
+
+        case Token::LT:
+          // < <= << <<= <!--
+          Advance();
+          if (c0_ == '=') return Select(Token::LTE);
+          if (c0_ == '<') return Select('=', Token::ASSIGN_SHL, Token::SHL);
+          if (c0_ == '!') {
+            token = ScanHtmlComment();
+            continue;
+          }
+          return Token::LT;
+
+        case Token::GT:
+          // > >= >> >>= >>> >>>=
+          Advance();
+          if (c0_ == '=') return Select(Token::GTE);
+          if (c0_ == '>') {
+            // >> >>= >>> >>>=
+            Advance();
+            if (c0_ == '=') return Select(Token::ASSIGN_SAR);
+            if (c0_ == '>') return Select('=', Token::ASSIGN_SHR, Token::SHR);
+            return Token::SAR;
+          }
+          return Token::GT;
+
+        case Token::ASSIGN:
+          // = == === =>
+          Advance();
+          if (c0_ == '=') return Select('=', Token::EQ_STRICT, Token::EQ);
+          if (c0_ == '>') return Select(Token::ARROW);
+          return Token::ASSIGN;
+
+        case Token::NOT:
+          // ! != !==
+          Advance();
+          if (c0_ == '=') return Select('=', Token::NE_STRICT, Token::NE);
+          return Token::NOT;
+
+        case Token::ADD:
+          // + ++ +=
+          Advance();
+          if (c0_ == '+') return Select(Token::INC);
+          if (c0_ == '=') return Select(Token::ASSIGN_ADD);
+          return Token::ADD;
+
+        case Token::SUB:
+          // - -- --> -=
+          Advance();
+          if (c0_ == '-') {
+            Advance();
+            if (c0_ == '>' && next().after_line_terminator) {
+              // For compatibility with SpiderMonkey, we skip lines that
+              // start with an HTML comment end '-->'.
+              token = SkipSingleHTMLComment();
+              continue;
+            }
+            return Token::DEC;
+          }
+          if (c0_ == '=') return Select(Token::ASSIGN_SUB);
+          return Token::SUB;
+
+        case Token::MUL:
+          // * *=
+          Advance();
+          if (c0_ == '*') return Select('=', Token::ASSIGN_EXP, Token::EXP);
+          if (c0_ == '=') return Select(Token::ASSIGN_MUL);
+          return Token::MUL;
+
+        case Token::MOD:
+          // % %=
+          return Select('=', Token::ASSIGN_MOD, Token::MOD);
+
+        case Token::DIV:
+          // /  // /* /=
+          Advance();
+          if (c0_ == '/') {
+            uc32 c = Peek();
+            if (c == '#' || c == '@') {
+              Advance();
+              Advance();
+              token = SkipSourceURLComment();
+              continue;
+            }
+            token = SkipSingleLineComment();
+            continue;
+          }
+          if (c0_ == '*') {
+            token = SkipMultiLineComment();
+            continue;
+          }
+          if (c0_ == '=') return Select(Token::ASSIGN_DIV);
+          return Token::DIV;
+
+        case Token::BIT_AND:
+          // & && &=
+          Advance();
+          if (c0_ == '&') return Select(Token::AND);
+          if (c0_ == '=') return Select(Token::ASSIGN_BIT_AND);
+          return Token::BIT_AND;
+
+        case Token::BIT_OR:
+          // | || |=
+          Advance();
+          if (c0_ == '|') return Select(Token::OR);
+          if (c0_ == '=') return Select(Token::ASSIGN_BIT_OR);
+          return Token::BIT_OR;
+
+        case Token::BIT_XOR:
+          // ^ ^=
+          return Select('=', Token::ASSIGN_BIT_XOR, Token::BIT_XOR);
+
+        case Token::PERIOD:
+          // . Number
+          Advance();
+          if (IsDecimalDigit(c0_)) return ScanNumber(true);
+          if (c0_ == '.') {
+            if (Peek() == '.') {
+              Advance();
+              Advance();
+              return Token::ELLIPSIS;
+            }
+          }
+          return Token::PERIOD;
+
+        case Token::TEMPLATE_SPAN:
+          Advance();
+          return ScanTemplateSpan();
+
+        case Token::PRIVATE_NAME:
+          return ScanPrivateName();
+
+        case Token::WHITESPACE:
+          token = SkipWhiteSpace();
+          continue;
+
+        case Token::NUMBER:
+          return ScanNumber(false);
+
+        case Token::IDENTIFIER:
+          return ScanIdentifierOrKeyword();
+
+        default:
+          UNREACHABLE();
       }
     }
 
-    switch (c0_) {
-      case '"':
-      case '\'':
-        return ScanString();
-
-      case '<':
-        // < <= << <<= <!--
-        Advance();
-        if (c0_ == '=') return Select(Token::LTE);
-        if (c0_ == '<') return Select('=', Token::ASSIGN_SHL, Token::SHL);
-        if (c0_ == '!') {
-          token = ScanHtmlComment();
-          continue;
-        }
-        return Token::LT;
-
-      case '>':
-        // > >= >> >>= >>> >>>=
-        Advance();
-        if (c0_ == '=') return Select(Token::GTE);
-        if (c0_ == '>') {
-          // >> >>= >>> >>>=
-          Advance();
-          if (c0_ == '=') return Select(Token::ASSIGN_SAR);
-          if (c0_ == '>') return Select('=', Token::ASSIGN_SHR, Token::SHR);
-          return Token::SAR;
-        }
-        return Token::GT;
-
-      case '=':
-        // = == === =>
-        Advance();
-        if (c0_ == '=') return Select('=', Token::EQ_STRICT, Token::EQ);
-        if (c0_ == '>') return Select(Token::ARROW);
-        return Token::ASSIGN;
-
-      case '!':
-        // ! != !==
-        Advance();
-        if (c0_ == '=') return Select('=', Token::NE_STRICT, Token::NE);
-        return Token::NOT;
-
-      case '+':
-        // + ++ +=
-        Advance();
-        if (c0_ == '+') return Select(Token::INC);
-        if (c0_ == '=') return Select(Token::ASSIGN_ADD);
-        return Token::ADD;
-
-      case '-':
-        // - -- --> -=
-        Advance();
-        if (c0_ == '-') {
-          Advance();
-          if (c0_ == '>' && next().after_line_terminator) {
-            // For compatibility with SpiderMonkey, we skip lines that
-            // start with an HTML comment end '-->'.
-            token = SkipSingleHTMLComment();
-            continue;
-          }
-          return Token::DEC;
-        }
-        if (c0_ == '=') return Select(Token::ASSIGN_SUB);
-        return Token::SUB;
-
-      case '*':
-        // * *=
-        Advance();
-        if (c0_ == '*') return Select('=', Token::ASSIGN_EXP, Token::EXP);
-        if (c0_ == '=') return Select(Token::ASSIGN_MUL);
-        return Token::MUL;
-
-      case '%':
-        // % %=
-        return Select('=', Token::ASSIGN_MOD, Token::MOD);
-
-      case '/':
-        // /  // /* /=
-        Advance();
-        if (c0_ == '/') {
-          uc32 c = Peek();
-          if (c == '#' || c == '@') {
-            Advance();
-            Advance();
-            token = SkipSourceURLComment();
-            continue;
-          }
-          token = SkipSingleLineComment();
-          continue;
-        }
-        if (c0_ == '*') {
-          token = SkipMultiLineComment();
-          continue;
-        }
-        if (c0_ == '=') return Select(Token::ASSIGN_DIV);
-        return Token::DIV;
-
-      case '&':
-        // & && &=
-        Advance();
-        if (c0_ == '&') return Select(Token::AND);
-        if (c0_ == '=') return Select(Token::ASSIGN_BIT_AND);
-        return Token::BIT_AND;
-
-      case '|':
-        // | || |=
-        Advance();
-        if (c0_ == '|') return Select(Token::OR);
-        if (c0_ == '=') return Select(Token::ASSIGN_BIT_OR);
-        return Token::BIT_OR;
-
-      case '^':
-        // ^ ^=
-        return Select('=', Token::ASSIGN_BIT_XOR, Token::BIT_XOR);
-
-      case '.':
-        // . Number
-        Advance();
-        if (IsDecimalDigit(c0_)) return ScanNumber(true);
-        if (c0_ == '.') {
-          if (Peek() == '.') {
-            Advance();
-            Advance();
-            return Token::ELLIPSIS;
-          }
-        }
-        return Token::PERIOD;
-
-      case '`':
-        Advance();
-        return ScanTemplateSpan();
-
-      case '#':
-        return ScanPrivateName();
-
-      default:
-        if (unicode_cache_->IsIdentifierStart(c0_) ||
-            (CombineSurrogatePair() &&
-             unicode_cache_->IsIdentifierStart(c0_))) {
-          return ScanIdentifierOrKeyword();
-        }
-        if (IsDecimalDigit(c0_)) return ScanNumber(false);
-        if (c0_ == kEndOfInput) {
-          return source_->has_parser_error() ? Token::ILLEGAL : Token::EOS;
-        }
-        token = SkipWhiteSpace();
-        continue;
+    if (unicode_cache_->IsIdentifierStart(c0_) ||
+        (CombineSurrogatePair() && unicode_cache_->IsIdentifierStart(c0_))) {
+      return ScanIdentifierOrKeyword();
     }
+    if (c0_ == kEndOfInput) {
+      return source_->has_parser_error() ? Token::ILLEGAL : Token::EOS;
+    }
+    token = SkipWhiteSpace();
+
     // Continue scanning for tokens as long as we're just skipping whitespace.
   } while (token == Token::WHITESPACE);
 
