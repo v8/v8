6bafa880b4b463faa0303e5710a595ab0b1216e1
Jakob Gruber <jgruber@chromium.org>
2018-11-16 09:31:35 +0000

    Revert "[turbofan] Use feedback when reducing global loads/stores."
    This reverts commit 9c91b6877a702593a1a37f08f38a4e3c641d55f4.
    
    Reason for revert: https://ci.chromium.org/p/v8/builders/luci.v8.ci/V8%20Arm%20GC%20Stress/8864
    
    Original change's description:
    > [turbofan] Use feedback when reducing global loads/stores.
    > 
    > We already record the script context location or the property cell
    > as feedback of the global load/store IC, so Turbofan doesn't need
    > to do the lookups again.
    > 
    > Change-Id: I6cbd2937de344729cd8e146b4ff85ddf3de6a56e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1335691
    > Commit-Queue: Georg Neis <neis@chromium.org>
    > Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#57555}
    
    TBR=neis@chromium.org,ishell@chromium.org,bmeurer@chromium.org
    
    Change-Id: I99d72075e01348733fecdffc6b5572b96eb577b4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/1339860
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Jakob Gruber <jgruber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#57559}

--- src/ic/ic.cc
+++ src/ic/ic.cc
@@ -501,9 +501,8 @@ MaybeHandle<Object> LoadGlobalIC::Load(Handle<Name> name) {
       }
 
       if (FLAG_use_ic) {
-        if (nexus()->ConfigureLexicalVarMode(
-                lookup_result.context_index, lookup_result.slot_index,
-                lookup_result.mode == VariableMode::kConst)) {
+        if (nexus()->ConfigureLexicalVarMode(lookup_result.context_index,
+                                             lookup_result.slot_index)) {
           TRACE_HANDLER_STATS(isolate(), LoadGlobalIC_LoadScriptContextField);
         } else {
           // Given combination of indices can't be encoded, so use slow stub.
@@ -1359,9 +1358,8 @@ MaybeHandle<Object> StoreGlobalIC::Store(Handle<Name> name,
     }
 
     if (FLAG_use_ic) {
-      if (nexus()->ConfigureLexicalVarMode(
-              lookup_result.context_index, lookup_result.slot_index,
-              lookup_result.mode == VariableMode::kConst)) {
+      if (nexus()->ConfigureLexicalVarMode(lookup_result.context_index,
+                                           lookup_result.slot_index)) {
         TRACE_HANDLER_STATS(isolate(), StoreGlobalIC_StoreScriptContextField);
       } else {
         // Given combination of indices can't be encoded, so use slow stub.
