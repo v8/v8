e0766dbe5c4b52ce1b08a390c56e89f4dd2a0728
Dan Elphick <delphick@chromium.org>
2018-11-27 14:21:40 +0000

    [cleanup] Move FunctionTemplate::GetFunction to V8_DEPRECATED
    Bug: v8:7295, v8:8238
    Change-Id: Ide105b69ee77707a5a28d16abfb1ab44ebc3995d
    Reviewed-on: https://chromium-review.googlesource.com/c/1352273
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#57895}

--- include/v8.h
+++ include/v8.h
@@ -5739,7 +5739,7 @@ class V8_EXPORT FunctionTemplate : public Template {
       SideEffectType side_effect_type = SideEffectType::kHasSideEffect);
 
   /** Returns the unique function instance in the current execution context.*/
-  V8_DEPRECATE_SOON("Use maybe version", Local<Function> GetFunction());
+  V8_DEPRECATED("Use maybe version", Local<Function> GetFunction());
   V8_WARN_UNUSED_RESULT MaybeLocal<Function> GetFunction(
       Local<Context> context);
 
