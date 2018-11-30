9f6a854a7b967fffc2aabe54197ba068d411b0c4
Ujjwal Sharma <usharma1998@gmail.com>
2018-11-30 22:56:56 +0530

    [Intl] Remove dead code from runtime/runtime-intl.cc
    Cleanup runtime/runtime-intl.cc by removing dead code, namely
    Runtime_CanonicalizeLanguageTag and Runtime_GetDefaultICULocale.
    
    Bug: v8:8439,v8:5751
    Change-Id: Id8712b2f3b2ddffc0e289b904f17ae3b09faffae
    Reviewed-on: https://chromium-review.googlesource.com/c/1329705
    Commit-Queue: Sathya Gunasekaran <gsathya@chromium.org>
    Reviewed-by: Sathya Gunasekaran <gsathya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#57980}

--- src/runtime/runtime.h
+++ src/runtime/runtime.h
@@ -198,7 +198,6 @@ namespace internal {
 
 #ifdef V8_INTL_SUPPORT
 #define FOR_EACH_INTRINSIC_INTL(F, I) \
-  F(CanonicalizeLanguageTag, 1, 1)    \
   F(FormatList, 2, 1)                 \
   F(FormatListToParts, 2, 1)          \
   F(GetDefaultICULocale, 0, 1)        \
