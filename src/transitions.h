4204289210e7ab70e491ad9ac7a33daecc5e1725
Marja Hölttä <marja@chromium.org>
2018-12-04 17:15:38 +0100

    [Map::TransitionToDataProperty speed] Remove a branch in TransitionsAccessor::SearchTransition
    We checked against kNotFound twice; once in
    TransitionsAccessor::SearchTransitions and once in TransitionArray::Search. It's
    unnecessary to check twice.
    
    In local tests, this speeds up searching for a non-existent transition in a
    2-element TransitionArray by ~20%.
    
    BUG=v8:8547
    
    Change-Id: Ic220c4c0bfeeacd9b81953efad212ed15168437e
    Reviewed-on: https://chromium-review.googlesource.com/c/1361167
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#58038}

--- src/transitions.h
+++ src/transitions.h
@@ -302,6 +302,9 @@ class TransitionArray : public WeakFixedArray {
   int Search(PropertyKind kind, Name name, PropertyAttributes attributes,
              int* out_insertion_index = nullptr);
 
+  Map SearchAndGetTarget(PropertyKind kind, Name name,
+                         PropertyAttributes attributes);
+
   // Search a non-property transition (like elements kind, observe or frozen
   // transitions).
   inline int SearchSpecial(Symbol symbol, int* out_insertion_index = nullptr);
@@ -309,6 +312,8 @@ class TransitionArray : public WeakFixedArray {
   inline int SearchName(Name name, int* out_insertion_index = nullptr);
   int SearchDetails(int transition, PropertyKind kind,
                     PropertyAttributes attributes, int* out_insertion_index);
+  Map SearchDetailsAndGetTarget(int transition, PropertyKind kind,
+                                PropertyAttributes attributes);
 
   inline int number_of_transitions() const;
 
