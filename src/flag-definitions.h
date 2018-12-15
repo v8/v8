dde25872f58951bb0148cf43d6a504ab2f280485
Caitlin Potter <caitp@igalia.com>
2018-12-15 11:30:29 -0500

    [esnext] ship Object.fromEntries
    Enable --harmony-object-from-entries by default.
    
    Object.fromEntries is a new standard library method, whose proposal
    (https://tc39.github.io/proposal-object-from-entries/) is currently
    at stage 3.
    
    It simply creates a JSObject from an iterable collection of key/value
    pairs, such that `Object.fromEntries([ [ "a", "b" ] ]) -> { a: "b" }`
    
    BUG=v8:8021
    R=gsathya@chromium.org, adamk@chromium.org
    
    Change-Id: Ib55bb6ca43727d66b471c0fd14845735c1ca2894
    Reviewed-on: https://chromium-review.googlesource.com/c/1373918
    Commit-Queue: Caitlin Potter <caitp@igalia.com>
    Reviewed-by: Sathya Gunasekaran <gsathya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#58262}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -216,10 +216,9 @@ DEFINE_IMPLICATION(harmony_private_methods, harmony_private_fields)
 #endif
 
 // Features that are complete (but still behind --harmony/es-staging flag).
-#define HARMONY_STAGED_BASE(V)                                             \
-  V(harmony_private_fields, "harmony private fields in class literals")    \
-  V(harmony_numeric_separator, "harmony numeric separator between digits") \
-  V(harmony_object_from_entries, "harmony Object.fromEntries()")
+#define HARMONY_STAGED_BASE(V)                                          \
+  V(harmony_private_fields, "harmony private fields in class literals") \
+  V(harmony_numeric_separator, "harmony numeric separator between digits")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_STAGED(V)                        \
@@ -243,7 +242,8 @@ DEFINE_IMPLICATION(harmony_private_methods, harmony_private_fields)
   V(harmony_json_stringify, "well-formed JSON.stringify")                      \
   V(harmony_public_fields, "harmony public instance fields in class literals") \
   V(harmony_static_fields, "harmony static fields in class literals")          \
-  V(harmony_string_matchall, "harmony String.prototype.matchAll")
+  V(harmony_string_matchall, "harmony String.prototype.matchAll")              \
+  V(harmony_object_from_entries, "harmony Object.fromEntries()")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_SHIPPING(V)                      \
