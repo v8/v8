1482644474443156c73eb698b76e84b007f2613b
Ross McIlroy <rmcilroy@chromium.org>
2018-12-20 12:32:36 +0000

    [Flags] Enable bytecode flushing for --future.
    BUG=v8:8395
    
    Change-Id: Ibf97962f1bdfa5c83cd228569c88abcf9d0a7c51
    Reviewed-on: https://chromium-review.googlesource.com/c/1386487
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#58402}

--- src/flag-definitions.h
+++ src/flag-definitions.h
@@ -296,6 +296,7 @@ DEFINE_BOOL(future, FUTURE_BOOL,
             "not-too-far future")
 
 DEFINE_IMPLICATION(future, write_protect_code_memory)
+DEFINE_IMPLICATION(future, flush_bytecode)
 
 // Flags for experimental implementation features.
 DEFINE_BOOL(allocation_site_pretenuring, true,
