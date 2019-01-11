4a9f186bde6f13a38de26c0268563844fa157636
Jakob Gruber <jgruber@chromium.org>
2019-01-11 10:13:32 +0100

    [ia32] Remove custom MemMove function
    It isn't clear whether our custom generated MemMove function provides
    any benefits over std::memmove. This is an attempt to remove it. If
    bots seem unhappy we can revert.
    
    Bug: v8:7777,v8:8675
    Change-Id: I7f1a6e3050b6e635618593c04f7d51e448426ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/1405854
    Commit-Queue: Jakob Gruber <jgruber@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#58748}

--- src/memcopy.cc
+++ src/memcopy.cc
@@ -7,26 +7,7 @@
 namespace v8 {
 namespace internal {
 
-#if V8_TARGET_ARCH_IA32
-static void MemMoveWrapper(void* dest, const void* src, size_t size) {
-  memmove(dest, src, size);
-}
-
-// Initialize to library version so we can call this at any time during startup.
-static MemMoveFunction memmove_function = &MemMoveWrapper;
-
-// Defined in codegen-ia32.cc.
-MemMoveFunction CreateMemMoveFunction();
-
-// Copy memory area to disjoint memory area.
-V8_EXPORT_PRIVATE void MemMove(void* dest, const void* src, size_t size) {
-  if (size == 0) return;
-  // Note: here we rely on dependent reads being ordered. This is true
-  // on all architectures we currently support.
-  (*memmove_function)(dest, src, size);
-}
-
-#elif V8_OS_POSIX && V8_HOST_ARCH_ARM
+#if V8_OS_POSIX && V8_HOST_ARCH_ARM
 void MemCopyUint16Uint8Wrapper(uint16_t* dest, const uint8_t* src,
                                size_t chars) {
   uint16_t* limit = dest + chars;
@@ -56,12 +37,7 @@ static bool g_memcopy_functions_initialized = false;
 void init_memcopy_functions() {
   if (g_memcopy_functions_initialized) return;
   g_memcopy_functions_initialized = true;
-#if V8_TARGET_ARCH_IA32
-  MemMoveFunction generated_memmove = CreateMemMoveFunction();
-  if (generated_memmove != nullptr) {
-    memmove_function = generated_memmove;
-  }
-#elif V8_OS_POSIX && V8_HOST_ARCH_ARM
+#if V8_OS_POSIX && V8_HOST_ARCH_ARM
   memcopy_uint8_function = CreateMemCopyUint8Function(&MemCopyUint8Wrapper);
   memcopy_uint16_uint8_function =
       CreateMemCopyUint16Uint8Function(&MemCopyUint16Uint8Wrapper);
