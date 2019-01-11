55decb637e556941ef801945bde1d364f56eabc3
Benedikt Meurer <bmeurer@chromium.org>
2019-01-11 09:08:59 +0100

    [dx] Turn on zero cost async stack traces by default.
    Bug: v8:7522, v8:8673
    Change-Id: Iee2d6fda9291fbdd346d25d2c535874dba13fdc9
    Ref: nodejs/node#11865
    Design-Document: http://bit.ly/v8-zero-cost-async-stack-traces
    Reviewed-on: https://chromium-review.googlesource.com/c/1396425
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#58765}

--- test/mjsunit/es8/async-function-stacktrace.js
+++ test/mjsunit/es8/async-function-stacktrace.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Flags: --noasync-stack-traces
+
 async function test(func, funcs) {
   try {
     await func();
