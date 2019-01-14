f726d7f0c48d6e5f2024e1ec8289da735e62c041
Ben L. Titzer <titzer@chromium.org>
2019-01-14 15:47:59 +0100

    [mjsunit] Dont generate dumps from regression test
    The --perf-prof flag generates unwanted .dump files, while the
    --perf-prof-unwinding-info flag enables the functionality under test here.
    
    R=jgruber@chromium.org
    BUG=chromium:913844
    
    Change-Id: Ie5ee374977d6105854f42065600dac1bc5ba6df7
    Reviewed-on: https://chromium-review.googlesource.com/c/1409363
    Commit-Queue: Ben Titzer <titzer@chromium.org>
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#58791}

--- test/mjsunit/regress/regress-913844.js
+++ test/mjsunit/regress/regress-913844.js
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --disable-in-process-stack-traces --perf-prof --turbo-loop-rotation
+// Flags: --disable-in-process-stack-traces --perf-prof-unwinding-info --turbo-loop-rotation
 for (var x = 0; x < 1000000; x++)
 ;
