a9e93572d42663867fc62ef456b4fb1a92234c5a
Clemens Hammacher <clemensh@chromium.org>
2019-01-28 17:21:40 +0100

    [test] Check for illegal uses of mjsunit methods
    The assertThrows and assertDoesNotThrow methods expect either a
    function to execute, or a string to eval. In several tests however we
    accidentally passed the *result* of the statement to be tested instead
    of the code.
    This CL adds check to catch such error early, and removes wrong uses.
    In most places, we do not need to use assertDoesNotThrow anyway,
    because exceptions are handled as test failures.
    
    Drive-by: Unify catch syntax in mjsunit.js and make sure to propagate
    MjsUnitAssertionErrors correctly.
    
    R=mathias@chromium.org
    
    Bug: v8:8562
    Change-Id: I88894a667cbe0570774f748a9a23e8a527887a49
    Reviewed-on: https://chromium-review.googlesource.com/c/1439238
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#59277}

--- test/mjsunit/mjsunit.js
+++ test/mjsunit/mjsunit.js
@@ -213,7 +213,7 @@ var prettyPrinted;
   // TODO(neis): Remove try-catch once BigInts are enabled by default.
   try {
     BigIntPrototypeValueOf = BigInt.prototype.valueOf;
-  } catch(e) {}
+  } catch (e) {}
 
   function classOf(object) {
     // Argument must not be null or undefined.
@@ -480,14 +480,17 @@ var prettyPrinted;
     }
   };
 
+  function executeCode(code) {
+    if (typeof code === 'function')  return code();
+    if (typeof code === 'string') return eval(code);
+    failWithMessage(
+        'Given code is neither function nor string, but ' + (typeof code) +
+        ': <' + prettyPrinted(code) + '>');
+  }
 
   assertThrows = function assertThrows(code, type_opt, cause_opt) {
     try {
-      if (typeof code === 'function') {
-        code();
-      } else {
-        eval(code);
-      }
+      executeCode(code);
     } catch (e) {
       if (typeof type_opt === 'function') {
         assertInstanceof(e, type_opt);
@@ -508,11 +511,10 @@ var prettyPrinted;
     failWithMessage("Did not throw exception");
   };
 
-
   assertThrowsEquals = function assertThrowsEquals(fun, val) {
     try {
       fun();
-    } catch(e) {
+    } catch (e) {
       assertSame(val, e);
       return;
     }
@@ -533,15 +535,11 @@ var prettyPrinted;
     }
   };
 
-
-   assertDoesNotThrow = function assertDoesNotThrow(code, name_opt) {
+  assertDoesNotThrow = function assertDoesNotThrow(code, name_opt) {
     try {
-      if (typeof code === 'function') {
-        return code();
-      } else {
-        return eval(code);
-      }
+      executeCode(code);
     } catch (e) {
+      if (e instanceof MjsUnitAssertionError) throw e;
       failWithMessage("threw an exception: " + (e.message || e));
     }
   };
@@ -772,7 +770,7 @@ var prettyPrinted;
         return frame;
       });
       return "" + error.message + "\n" + ArrayPrototypeJoin.call(stack, "\n");
-    } catch(e) {};
+    } catch (e) {};
     return error.stack;
   }
 })();
