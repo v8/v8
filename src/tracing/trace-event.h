e0fd36b78c6e9bbbf175c24ff6c189894e3998d9
Nico Weber <thakis@chromium.org>
2019-02-21 09:22:18 -0500

    v8: Prep for removing TRACE_EVENT_SCOPED_CONTEXT.
    It's only used in tests, and for some reason v8 refers to a macro defined
    in src.git, so I need to remove this in v8 first before I can remove it
    in Chromium.
    
    Bug: chromium:934255
    Change-Id: I31ea32aa43cf7a5f518def7b91dce99dcb268709
    Reviewed-on: https://chromium-review.googlesource.com/c/1480911
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#59769}

--- src/tracing/trace-event.h
+++ src/tracing/trace-event.h
@@ -270,27 +270,6 @@ enum CategoryGroupEnabledFlags {
   INTERNAL_TRACE_EVENT_ADD_WITH_ID_AND_TIMESTAMP(                      \
       phase, category_group, name, id, timestamp, flags, ##__VA_ARGS__)
 
-// Enter and leave a context based on the current scope.
-#define INTERNAL_TRACE_EVENT_SCOPED_CONTEXT(category_group, name, context) \
-  struct INTERNAL_TRACE_EVENT_UID(ScopedContext) {                         \
-   public:                                                                 \
-    INTERNAL_TRACE_EVENT_UID(ScopedContext)(uint64_t cid) : cid_(cid) {    \
-      TRACE_EVENT_ENTER_CONTEXT(category_group, name, cid_);               \
-    }                                                                      \
-    ~INTERNAL_TRACE_EVENT_UID(ScopedContext)() {                           \
-      TRACE_EVENT_LEAVE_CONTEXT(category_group, name, cid_);               \
-    }                                                                      \
-                                                                           \
-   private:                                                                \
-    /* Local class friendly DISALLOW_COPY_AND_ASSIGN */                    \
-    INTERNAL_TRACE_EVENT_UID(ScopedContext)                                \
-    (const INTERNAL_TRACE_EVENT_UID(ScopedContext)&) {}                    \
-    void operator=(const INTERNAL_TRACE_EVENT_UID(ScopedContext)&) {}      \
-    uint64_t cid_;                                                         \
-  };                                                                       \
-  INTERNAL_TRACE_EVENT_UID(ScopedContext)                                  \
-  INTERNAL_TRACE_EVENT_UID(scoped_context)(context);
-
 #define TRACE_EVENT_CALL_STATS_SCOPED(isolate, category_group, name) \
   INTERNAL_TRACE_EVENT_CALL_STATS_SCOPED(isolate, category_group, name)
 
