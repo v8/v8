e7cc25124b5584ce60bc2ac5d96c0c8006500f0b
Clemens Hammacher <clemensh@chromium.org>
2019-03-11 13:18:46 +0100

    [wasm] Fix includes in wasm-code-manager.h
    This removes three unneeded includes from wasm-code-manager.h, and adds
    four that were missing.
    
    R=ahaas@chromium.org
    
    Bug: v8:7490, v8:8834
    Change-Id: I0f0f367c0ff748cc1561689b7e2badf3a7a01ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1513717
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#60168}

--- src/wasm/wasm-code-manager.h
+++ src/wasm/wasm-code-manager.h
@@ -5,11 +5,12 @@
 #ifndef V8_WASM_WASM_CODE_MANAGER_H_
 #define V8_WASM_WASM_CODE_MANAGER_H_
 
-#include <functional>
+#include <atomic>
 #include <list>
 #include <map>
-#include <unordered_map>
-#include <unordered_set>
+#include <memory>
+#include <utility>
+#include <vector>
 
 #include "src/base/macros.h"
 #include "src/builtins/builtins-definitions.h"
