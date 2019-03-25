dd07cbcfdfef0524856434387a64824aaee751dd
Milad Farazmand <miladfar@ca.ibm.com>
2019-03-25 09:52:18 -0400

    PPC/S390: [heap] Clean-up keys of oldspace weakmaps during scavenge
    Port 6604f1826d370e10962caf4a72b3801d370570bd
    
    Original Commit Message:
    
         This CL adds handling for cleaning up weakmap (EphemeronHashTable)
         keys during scavenge, even if the weakmap resides in oldspace.
    
    Change-Id: Idf8b6115e57b1229864afefe6ffee85acb5e7547
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1538320
    Reviewed-by: Milad Farazmand <miladfar@ca.ibm.com>
    Reviewed-by: Junliang Yan <jyan@ca.ibm.com>
    Commit-Queue: Milad Farazmand <miladfar@ca.ibm.com>
    Commit-Queue: Junliang Yan <jyan@ca.ibm.com>
    Cr-Commit-Position: refs/heads/master@{#60446}

--- src/ppc/macro-assembler-ppc.cc
+++ src/ppc/macro-assembler-ppc.cc
@@ -459,6 +459,32 @@ void TurboAssembler::RestoreRegisters(RegList registers) {
   MultiPop(regs);
 }
 
+void TurboAssembler::CallEphemeronKeyBarrier(Register object, Register address,
+                                             SaveFPRegsMode fp_mode) {
+  EphemeronKeyBarrierDescriptor descriptor;
+  RegList registers = descriptor.allocatable_registers();
+
+  SaveRegisters(registers);
+
+  Register object_parameter(
+      descriptor.GetRegisterParameter(EphemeronKeyBarrierDescriptor::kObject));
+  Register slot_parameter(descriptor.GetRegisterParameter(
+      EphemeronKeyBarrierDescriptor::kSlotAddress));
+  Register fp_mode_parameter(
+      descriptor.GetRegisterParameter(EphemeronKeyBarrierDescriptor::kFPMode));
+
+  push(object);
+  push(address);
+
+  pop(slot_parameter);
+  pop(object_parameter);
+
+  Move(fp_mode_parameter, Smi::FromEnum(fp_mode));
+  Call(isolate()->builtins()->builtin_handle(Builtins::kEphemeronKeyBarrier),
+       RelocInfo::CODE_TARGET);
+  RestoreRegisters(registers);
+}
+
 void TurboAssembler::CallRecordWriteStub(
     Register object, Register address,
     RememberedSetAction remembered_set_action, SaveFPRegsMode fp_mode) {
