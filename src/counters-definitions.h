0226bccaa2fbf7be5a3015a876c9c3f402986dbc
Michael Lippautz <mlippautz@chromium.org>
2019-04-02 19:59:44 +0200

    [heap] Add UMA for overall marking time and marking throughput
    Adds the following UMA counters:
    - V8.GCMarkingSum: Overall time spent in marking per GC cycle
    - V8.GCMainThreadMarkingThroughput: Overall marking throughput
      considering marking time spent on the main thread and
      allocated bytes after GC. Only reported if more than 1MB of
      live objects have been marked in the cycle.
    
    Bug: chromium:945806
    Change-Id: I24a37bf59f02da9aba984bed9de62fdb39be8882
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1547657
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Hannes Payer <hpayer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#60579}

--- src/counters-definitions.h
+++ src/counters-definitions.h
@@ -30,6 +30,10 @@ namespace internal {
   HR(gc_scavenger_scavenge_main, V8.GCScavenger.ScavengeMain, 0, 10000, 101)   \
   HR(gc_scavenger_scavenge_roots, V8.GCScavenger.ScavengeRoots, 0, 10000, 101) \
   HR(gc_mark_compactor, V8.GCMarkCompactor, 0, 10000, 101)                     \
+  HR(gc_marking_sum, V8.GCMarkingSum, 0, 10000, 101)                           \
+  /* Range and bucket matches BlinkGC.MainThreadMarkingThroughput. */          \
+  HR(gc_main_thread_marking_throughput, V8.GCMainThreadMarkingThroughput, 0,   \
+     100000, 50)                                                               \
   HR(scavenge_reason, V8.GCScavengeReason, 0, 21, 22)                          \
   HR(young_generation_handling, V8.GCYoungGenerationHandling, 0, 2, 3)         \
   /* Asm/Wasm. */                                                              \
