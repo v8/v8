2f4c928c65f2fa095be1991a58f78d10d55fca10
Clemens Hammacher <clemensh@chromium.org>
2019-04-24 12:43:58 +0200

    [api] Deprecate one {SetFlagsFromString} variant
    This deprecates the variant taking an int argument.
    Chromium was switched to the size_t variant in
    https://crrev.com/c/1583747 and
    https://crrev.com/c/1587597. Pdfium was switched here:
    https://pdfium-review.googlesource.com/c/pdfium/+/53996
    
    R=ulan@chromium.org
    
    Change-Id: I0f4e007b2edc7a176bd8865600bfd5e70b838c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1581640
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61243}

--- include/v8.h
+++ include/v8.h
@@ -8641,9 +8641,8 @@ class V8_EXPORT V8 {
    */
   static void SetFlagsFromString(const char* str);
   static void SetFlagsFromString(const char* str, size_t length);
-  V8_DEPRECATE_SOON("use size_t version",
-                    static void SetFlagsFromString(const char* str,
-                                                   int length));
+  V8_DEPRECATED("use size_t version",
+                static void SetFlagsFromString(const char* str, int length));
 
   /**
    * Sets V8 flags from the command line.
