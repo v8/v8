b76ccb94712a06bc3bffe2b993a639c16a25012c
Clemens Hammacher <clemensh@chromium.org>
2019-04-29 15:53:17 +0200

    [wasm][gc] Stress wasm code gc in --gc-stress mode
    This makes the gc-stress bots set the '--stress-wasm-code-gc' flag.
    Note that this also implicitly enables wasm code gc on these bots.
    
    R=mstarzinger@chromium.org, machenbach@chromium.org
    
    Cq-Include-Trybots: luci.v8.try:v8_linux_gc_stress_dbg
    Cq-Include-Trybots: luci.v8.try:v8_mac64_gc_stress_dbg
    Bug: v8:8217
    Change-Id: Idc4fb996f50632d5621200e138f3ddc572ed9b05
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1585721
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61089}

--- tools/testrunner/standard_runner.py
+++ tools/testrunner/standard_runner.py
@@ -53,7 +53,8 @@ GC_STRESS_FLAGS = ['--gc-interval=500', '--stress-compaction',
                    '--concurrent-recompilation-queue-length=64',
                    '--concurrent-recompilation-delay=500',
                    '--concurrent-recompilation',
-                   '--stress-flush-bytecode']
+                   '--stress-flush-bytecode',
+                   '--wasm-code-gc', '--stress-wasm-code-gc']
 
 RANDOM_GC_STRESS_FLAGS = ['--random-gc-interval=5000',
                           '--stress-compaction-random']
