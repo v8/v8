9d7c1947f52dcfa6c678f63022c19027bf5536ac
Johannes Henkel <johannes@chromium.org>
2019-05-02 13:50:38 -0700

    [DevTools] Add V8InspectorSession::state(), which returns binary (CBOR).
    Keep the existing method for compatibility, by converting
    to json from CBOR using the inspector_protocol_encoding library,
    via a v8 specific interface library that directs routines for
    converting between strings and doubles to v8's implementations.
    
    This change also brings in the encoding.h / encoding.cc files from the
    upstream inspector_protocol project. The only modification here
    are the header guards, and the namespace. I will fix roll.py to
    make it so that we pick up future changes.
    
    third_party/inspector_protocol/BUILD.gn is specific to v8, by necessity.
    third_party/inspector_protocol/.clang-format is a copy of the upstream
    file. If we don't put this, we'll find ourselves auto-formatting the roll,
    which is annoying.
    
    This is a reland of
    https://chromium-review.googlesource.com/c/v8/v8/+/1590627 with the
    only modification in the DEPS file; this time I'm including
    third_party/inspector_protocol/encoding/encoding{.h,cc} in addition to
    the relative include there. Not sure why this is needed but I'm hoping
    it gets me past the presubmit which may resolve the include path
    relative to the V8 base (the ../../third_party is needed for when V8 is
    embedded into Chromium).
    
    Change-Id: Ic76b2b5faa7e1cbdceb15aff3f369e9a303e3e85
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1593646
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61214}

--- src/inspector/v8-inspector-session-impl.cc
+++ src/inspector/v8-inspector-session-impl.cc
@@ -4,6 +4,8 @@
 
 #include "src/inspector/v8-inspector-session-impl.h"
 
+#include "src/base/logging.h"
+#include "src/base/macros.h"
 #include "src/inspector/injected-script.h"
 #include "src/inspector/inspected-context.h"
 #include "src/inspector/protocol/Protocol.h"
@@ -15,11 +17,16 @@
 #include "src/inspector/v8-debugger.h"
 #include "src/inspector/v8-heap-profiler-agent-impl.h"
 #include "src/inspector/v8-inspector-impl.h"
+#include "src/inspector/v8-inspector-protocol-encoding.h"
 #include "src/inspector/v8-profiler-agent-impl.h"
 #include "src/inspector/v8-runtime-agent-impl.h"
 #include "src/inspector/v8-schema-agent-impl.h"
 
 namespace v8_inspector {
+namespace {
+using ::v8_inspector_protocol_encoding::SpanFrom;
+using IPEStatus = ::v8_inspector_protocol_encoding::Status;
+}  // namespace
 
 // static
 bool V8InspectorSession::canDispatchMethod(const StringView& method) {
@@ -355,8 +362,17 @@ void V8InspectorSessionImpl::dispatchProtocolMessage(
 }
 
 std::unique_ptr<StringBuffer> V8InspectorSessionImpl::stateJSON() {
-  String16 json = m_state->toJSONString();
-  return StringBufferImpl::adopt(json);
+  std::vector<uint8_t> json;
+  IPEStatus status = ConvertCBORToJSON(SpanFrom(state()), &json);
+  DCHECK(status.ok());
+  USE(status);
+  return v8::base::make_unique<BinaryStringBuffer>(std::move(json));
+}
+
+std::vector<uint8_t> V8InspectorSessionImpl::state() {
+  std::vector<uint8_t> out;
+  m_state->writeBinary(&out);
+  return out;
 }
 
 std::vector<std::unique_ptr<protocol::Schema::API::Domain>>
