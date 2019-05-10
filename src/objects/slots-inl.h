d5efb9a764ab3118fcdf4eb4acb573a3fed632da
Igor Sheludko <ishell@chromium.org>
2019-05-10 13:01:21 +0200

    [ptr-compr] Add missing includes
    Bug: v8:7703
    Change-Id: I63813bb642073a86ee28957c40078ec78ce97ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1605733
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61409}

--- src/objects/slots-inl.h
+++ src/objects/slots-inl.h
@@ -10,6 +10,7 @@
 #include "src/base/atomic-utils.h"
 #include "src/memcopy.h"
 #include "src/objects.h"
+#include "src/objects/compressed-slots.h"
 #include "src/objects/heap-object.h"
 #include "src/objects/maybe-object.h"
 #include "src/ptr-compr-inl.h"
