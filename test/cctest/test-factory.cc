ff8f5fd714a9d25810f068ad35027a19875c6c45
Michael Achenbach <machenbach@chromium.org>
2019-05-10 07:46:57 +0000

    Revert "[heap] Add tests to CodeBuilder Build and TryBuild"
    This reverts commit 2980961182731f69c95d7a65e00917a31b1f2747.
    
    Reason for revert:
    https://ci.chromium.org/p/v8/builders/ci/V8%20Linux%20-%20nosnap%20-%20debug/23930
    
    Original change's description:
    > [heap] Add tests to CodeBuilder Build and TryBuild
    > 
    > Bug: chromium:957934
    > Change-Id: Ia1ce5f6150b7f862b38f4c135236b154a1b02d6c
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1594564
    > Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    > Commit-Queue: Maciej Goszczycki <goszczycki@google.com>
    > Cr-Commit-Position: refs/heads/master@{#61391}
    
    TBR=ulan@chromium.org,delphick@chromium.org,goszczycki@google.com
    
    Change-Id: I036f66115d9d00f6bbccc36a2c04cb087a94c447
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:957934
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1605721
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61399}

--- test/cctest/test-factory.cc
+++ test/cctest/test-factory.cc
@@ -13,45 +13,11 @@ namespace v8 {
 namespace internal {
 namespace test_factory {
 
-namespace {
-
-constexpr int kInstructionSize = 10 * MB;
-
-size_t NearHeapLimitCallback(void* raw_bool, size_t current_heap_limit,
-                             size_t initial_heap_limit) {
-  bool* oom_triggered = static_cast<bool*>(raw_bool);
-  *oom_triggered = true;
-  return kInstructionSize * 2;
-}
-
-class SetupIsolateWithSmallHeap {
- public:
-  SetupIsolateWithSmallHeap() {
-    FLAG_max_old_space_size = kInstructionSize / MB / 2;  // In MB.
-    v8::Isolate::CreateParams create_params;
-    create_params.array_buffer_allocator = CcTest::array_buffer_allocator();
-    isolate_ = reinterpret_cast<Isolate*>(v8::Isolate::New(create_params));
-    isolate_->heap()->AddNearHeapLimitCallback(NearHeapLimitCallback,
-                                               &oom_triggered_);
-  }
-
-  ~SetupIsolateWithSmallHeap() {
-    reinterpret_cast<v8::Isolate*>(isolate_)->Dispose();
-  }
-
-  Isolate* isolate() { return isolate_; }
-  bool oom_triggered() const { return oom_triggered_; }
-
- private:
-  Isolate* isolate_;
-  bool oom_triggered_ = false;
-};
-
-}  // namespace
-
 TEST(Factory_CodeBuilder) {
-  Isolate* isolate = CcTest::i_isolate();
-  HandleScope scope(isolate);
+  LocalContext env;
+  v8::Isolate* isolate = env->GetIsolate();
+  Isolate* i_isolate = reinterpret_cast<Isolate*>(isolate);
+  HandleScope scope(i_isolate);
 
   // Create a big function that ends up in CODE_LO_SPACE.
   const int instruction_size = kMaxRegularHeapObjectSize + 1;
@@ -67,44 +33,12 @@ TEST(Factory_CodeBuilder) {
   desc.unwinding_info_size = 0;
   desc.origin = nullptr;
   Handle<Code> code =
-      Factory::CodeBuilder(isolate, desc, Code::WASM_FUNCTION).Build();
+      Factory::CodeBuilder(i_isolate, desc, Code::WASM_FUNCTION).Build();
 
-  CHECK(isolate->heap()->InSpace(*code, CODE_LO_SPACE));
+  CHECK(i_isolate->heap()->InSpace(*code, CODE_LO_SPACE));
 #if VERIFY_HEAP
-  code->ObjectVerify(isolate);
+  code->ObjectVerify(i_isolate);
 #endif
-}
-
-UNINITIALIZED_TEST(Factory_CodeBuilder_BuildOOM) {
-  SetupIsolateWithSmallHeap isolate_scope;
-  HandleScope scope(isolate_scope.isolate());
-  std::unique_ptr<byte[]> instructions(new byte[kInstructionSize]);
-  CodeDesc desc;
-  desc.instr_size = kInstructionSize;
-  desc.buffer = instructions.get();
-
-  const Handle<Code> code =
-      Factory::CodeBuilder(isolate_scope.isolate(), desc, Code::WASM_FUNCTION)
-          .Build();
-
-  CHECK(!code.is_null());
-  CHECK(isolate_scope.oom_triggered());
-}
-
-UNINITIALIZED_TEST(Factory_CodeBuilder_TryBuildOOM) {
-  SetupIsolateWithSmallHeap isolate_scope;
-  HandleScope scope(isolate_scope.isolate());
-  std::unique_ptr<byte[]> instructions(new byte[kInstructionSize]);
-  CodeDesc desc;
-  desc.instr_size = kInstructionSize;
-  desc.buffer = instructions.get();
-
-  const MaybeHandle<Code> code =
-      Factory::CodeBuilder(isolate_scope.isolate(), desc, Code::WASM_FUNCTION)
-          .TryBuild();
-
-  CHECK(code.is_null());
-  CHECK(!isolate_scope.oom_triggered());
 }
 
 }  // namespace test_factory
