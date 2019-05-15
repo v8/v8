3e118baf00707e25fa7205b50683d9a7acf2dd5b
Igor Sheludko <ishell@chromium.org>
2019-05-15 19:30:00 +0200

    Update include path to date.h which was moved in the meanwhile
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:950851
    Change-Id: Ia5e547e7cd7947f91814cb24de3dcf3b822905da
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1611811
    Reviewed-by: Deepti Gandluri <gdeepti@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61552}

--- test/unittests/date-cache-unittest.cc
+++ test/unittests/date-cache-unittest.cc
@@ -1,8 +1,10 @@
 // Copyright 2019 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include "src/base/platform/platform.h"
-#include "src/date.h"
+
+#include "src/date/date.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "unicode/strenum.h"
 #include "unicode/timezone.h"
