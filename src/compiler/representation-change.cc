52636535fac20567f64913a20579a057ef46bd93
Tobias Tebbi <tebbi@chromium.org>
2019-05-21 17:34:13 +0200

    [turbofan] fix deopt-loop for specuative Boolean to Number conversion
    Bug: chromium:965513
    Change-Id: I18ff91c98ad6106c5d4df260e6ead6a9e2425dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1622119
    Commit-Queue: Tobias Tebbi <tebbi@chromium.org>
    Auto-Submit: Tobias Tebbi <tebbi@chromium.org>
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61730}

--- src/compiler/representation-change.cc
+++ src/compiler/representation-change.cc
@@ -843,7 +843,10 @@ Node* RepresentationChanger::GetFloat64RepresentationFor(
     }
   } else if (output_rep == MachineRepresentation::kBit) {
     CHECK(output_type.Is(Type::Boolean()));
-    if (use_info.truncation().IsUsedAsFloat64()) {
+    // TODO(tebbi): TypeCheckKind::kNumberOrOddball should imply Float64
+    // truncation, since this exactly means that we treat Oddballs as Numbers.
+    if (use_info.truncation().IsUsedAsFloat64() ||
+        use_info.type_check() == TypeCheckKind::kNumberOrOddball) {
       op = machine()->ChangeUint32ToFloat64();
     } else {
       CHECK_NE(use_info.type_check(), TypeCheckKind::kNone);
@@ -975,6 +978,7 @@ Node* RepresentationChanger::GetWord32RepresentationFor(
       CHECK(Truncation::Any(kIdentifyZeros)
                 .IsLessGeneralThan(use_info.truncation()));
       CHECK_NE(use_info.type_check(), TypeCheckKind::kNone);
+      CHECK_NE(use_info.type_check(), TypeCheckKind::kNumberOrOddball);
       Node* unreachable =
           InsertUnconditionalDeopt(use_node, DeoptimizeReason::kNotASmi);
       return jsgraph()->graph()->NewNode(
@@ -1261,6 +1265,7 @@ Node* RepresentationChanger::GetWord64RepresentationFor(
   } else if (output_rep == MachineRepresentation::kBit) {
     CHECK(output_type.Is(Type::Boolean()));
     CHECK_NE(use_info.type_check(), TypeCheckKind::kNone);
+    CHECK_NE(use_info.type_check(), TypeCheckKind::kNumberOrOddball);
     Node* unreachable =
         InsertUnconditionalDeopt(use_node, DeoptimizeReason::kNotASmi);
     return jsgraph()->graph()->NewNode(
