734c1456d942a03d79aab4b3b0e57afbc803ceea
Seth Brenith <seth.brenith@microsoft.com>
2019-05-28 08:03:18 -0700

    [intl] Avoid a temporary allocation while converting to ICU string
    If we need to build an icu::UnicodeString for a string that is currently
    in one-byte representation, we first have to expand the string's content
    into a two-byte representation. Doing so involves allocating an array,
    which is slow. With this change, we can convert short strings on the
    stack instead to save time. The cutoff length for what counts as "short"
    is pretty arbitrary, but we believe many strings fit into an 80-column
    line. This increases the score of cdjs in JetStream 2 by 35% on my
    machine, because cdjs is basically a test of localeCompare throughput.
    
    Bug: v8:9305
    
    Change-Id: Iba081ac5a8fa7659edf06ac97ba8acf3f8328d59
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1630848
    Commit-Queue: Seth Brenith <seth.brenith@microsoft.com>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#61901}

--- src/objects/intl-objects.cc
+++ src/objects/intl-objects.cc
@@ -196,10 +196,20 @@ icu::UnicodeString Intl::ToICUUnicodeString(Isolate* isolate,
   DCHECK(string->IsFlat());
   DisallowHeapAllocation no_gc;
   std::unique_ptr<uc16[]> sap;
-  return icu::UnicodeString(
-      GetUCharBufferFromFlat(string->GetFlatContent(no_gc), &sap,
-                             string->length()),
-      string->length());
+  // Short one-byte strings can be expanded on the stack to avoid allocating a
+  // temporary buffer.
+  constexpr int kShortStringSize = 80;
+  UChar short_string_buffer[kShortStringSize];
+  const UChar* uchar_buffer = nullptr;
+  const String::FlatContent& flat = string->GetFlatContent(no_gc);
+  int32_t length = string->length();
+  if (flat.IsOneByte() && length <= kShortStringSize) {
+    CopyChars(short_string_buffer, flat.ToOneByteVector().begin(), length);
+    uchar_buffer = short_string_buffer;
+  } else {
+    uchar_buffer = GetUCharBufferFromFlat(flat, &sap, length);
+  }
+  return icu::UnicodeString(uchar_buffer, length);
 }
 
 namespace {
