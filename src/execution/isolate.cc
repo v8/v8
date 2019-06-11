06930f7420588cd122a6317ea945deaa7454cd6c
Maya Lekova <mslekova@chromium.org>
2019-06-11 10:12:04 +0200

    Revert "Add Crash Keys support"
    This reverts commit 02103b276bee0bbee320121230e46e9b253a7f5e.
    
    Reason for revert: Speculative revert for breakage on roll:
    https://chromium-review.googlesource.com/c/chromium/src/+/1650264
    
    Failing build in breakpad:
    https://ci.chromium.org/p/chromium/builders/try/linux_chromium_asan_rel_ng/298512
    
    Original change's description:
    > Add Crash Keys support
    > 
    > This adds crash keys containing the isolate address and addresses of
    > the read_only, map, and code spaces to crash report minidumps.
    > When not compiling V8 with Chrome, a noop implementation is used.
    > 
    > Bug: v8:9323
    > Change-Id: I8523630e7a4ff792855163c06bf76dab35b1b9e5
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1641326
    > Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    > Commit-Queue: Irina Yatsenko <irinayat@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#62059}
    
    TBR=jkummerow@chromium.org,tmrts@chromium.org,irinayat@microsoft.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: v8:9323
    Change-Id: I29138292dd474cf60e2bf3cc9b0629a085abd31c
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1649787
    Reviewed-by: Maya Lekova <mslekova@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Maya Lekova <mslekova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#62077}

--- src/execution/isolate.cc
+++ src/execution/isolate.cc
@@ -32,7 +32,6 @@
 #include "src/debug/debug.h"
 #include "src/deoptimizer/deoptimizer.h"
 #include "src/diagnostics/compilation-statistics.h"
-#include "src/diagnostics/crash-key.h"
 #include "src/execution/frames-inl.h"
 #include "src/execution/isolate-inl.h"
 #include "src/execution/messages.h"
@@ -3284,19 +3283,6 @@ bool Isolate::InitWithSnapshot(ReadOnlyDeserializer* read_only_deserializer,
   return Init(read_only_deserializer, startup_deserializer);
 }
 
-static void AddCrashKeysForIsolateAndHeapPointers(Isolate* isolate) {
-  const int id = isolate->id();
-  crash::AddCrashKey(id, "isolate", reinterpret_cast<uintptr_t>(isolate));
-
-  auto heap = isolate->heap();
-  crash::AddCrashKey(id, "ro_space",
-    reinterpret_cast<uintptr_t>(heap->read_only_space()->first_page()));
-  crash::AddCrashKey(id, "map_space",
-    reinterpret_cast<uintptr_t>(heap->map_space()->first_page()));
-  crash::AddCrashKey(id, "code_space",
-    reinterpret_cast<uintptr_t>(heap->code_space()->first_page()));
-}
-
 bool Isolate::Init(ReadOnlyDeserializer* read_only_deserializer,
                    StartupDeserializer* startup_deserializer) {
   TRACE_ISOLATE(init);
@@ -3542,7 +3528,6 @@ bool Isolate::Init(ReadOnlyDeserializer* read_only_deserializer,
     PrintF("[Initializing isolate from scratch took %0.3f ms]\n", ms);
   }
 
-  AddCrashKeysForIsolateAndHeapPointers(this);
   return true;
 }
 
