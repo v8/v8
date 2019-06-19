82bc7d1a89c074ecd6e95261e5d112a2b65f34e5
Leszek Swirski <leszeks@chromium.org>
2019-06-19 13:26:51 +0200

    [map] Make NextFreePropertyIndex search backwards
    Now that fields are always in the same order as descriptors, we can
    change the property scan during NextFreePropertyIndex into a faster
    reverse search that simply tries to find the last field.
    
    Change-Id: I24d0781cd7d7c5b15998c35f281be69cd492d5ee
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1667402
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#62286}

--- src/objects/map.cc
+++ src/objects/map.cc
@@ -1346,17 +1346,16 @@ int Map::NumberOfEnumerableProperties() const {
 }
 
 int Map::NextFreePropertyIndex() const {
-  int free_index = 0;
   int number_of_own_descriptors = NumberOfOwnDescriptors();
   DescriptorArray descs = instance_descriptors();
-  for (int i = 0; i < number_of_own_descriptors; i++) {
+  // Search properties backwards to find the last field.
+  for (int i = number_of_own_descriptors - 1; i >= 0; --i) {
     PropertyDetails details = descs.GetDetails(i);
     if (details.location() == kField) {
-      int candidate = details.field_index() + details.field_width_in_words();
-      if (candidate > free_index) free_index = candidate;
+      return details.field_index() + details.field_width_in_words();
     }
   }
-  return free_index;
+  return 0;
 }
 
 bool Map::OnlyHasSimpleProperties() const {
