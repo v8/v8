139f83110ce67a033c22c04846b3b30287cf17f6
Z Nguyen-Huu <duongn@microsoft.com>
2019-06-25 08:43:50 -0700

    add micro-benchmark for proxy trap setPrototypeOf
    Bug: v8:6664
    Change-Id: If5a8a85a7537fa429fb58d1e0654ffe5f6a5897f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1669788
    Commit-Queue: Z Nguyen-Huu <duongn@microsoft.com>
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Reviewed-by: Maya Lekova <mslekova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#62363}

--- test/js-perf-test/Proxies/proxies.js
+++ test/js-perf-test/Proxies/proxies.js
@@ -625,3 +625,42 @@ newBenchmark("GetPrototypeOfWithTrap", {
   },
   teardown() {}
 });
+
+// ----------------------------------------------------------------------------
+
+newBenchmark("SetPrototypeOfWithoutTrap", {
+  setup() {
+    var obj = { x: 1 };
+    obj.__proto__ = {};
+    p = new Proxy(obj, {});
+  },
+  run() {
+    for(var i = 0; i < ITERATIONS; i++) {
+      value = Object.setPrototypeOf(p, [1]);
+    }
+    return value;
+  },
+  teardown() {}
+});
+
+// ----------------------------------------------------------------------------
+
+newBenchmark("SetPrototypeOfWithTrap", {
+  setup() {
+    var obj = { x: 1 };
+    obj.__proto__ = {};
+    p = new Proxy(obj, {
+      setPrototypeOf: function(target, proto) {
+        Object.setPrototypeOf(target, proto);
+        return true;
+      }
+    });
+  },
+  run() {
+    for(var i = 0; i < ITERATIONS; i++) {
+      value = Object.setPrototypeOf(p, [1]);
+    }
+    return value;
+  },
+  teardown() {}
+});
