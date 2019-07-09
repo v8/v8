925b17ba2f63af880f9fb18f4de19dd369766acc
Yutaka Hirano <yhirano@chromium.org>
2019-07-09 08:19:30 +0000

    Revert "Serialize native errors"
    This reverts commit 85bc4ef6c2114b48cd3ef3bffb33510061ab9926.
    
    Reason for revert: The tag 'e' conflicts with a blink serialization tag: kFileIndexTag.
    
    Original change's description:
    > Serialize native errors
    > 
    > Make native errors serializable.
    > 
    > The implementation is mostly straightforward, but there is one
    > exception: the stack property. Although the property is not specified,
    > the spec for error cloning asks us to preserve the property if
    > possible. This implementation serializes the property only when it is
    > a string, and otherwise ignores it.
    > 
    > Spec: https://github.com/whatwg/html/pull/4665
    > Intent-to-Ship: <TBD>
    > 
    > Bug: chromium:970079
    > Change-Id: I7f36b8b4fc5dff22d726d849ccfb9748d0888365
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1649257
    > Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Simon Zünd <szuend@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#62584}
    
    TBR=jbroman@chromium.org,yhirano@chromium.org,adamk@chromium.org,domenic@chromium.org,szuend@chromium.org
    
    Change-Id: Ia0cc902eaa1419cdb0cfec377d8a40fa914612c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:970079
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1692365
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#62589}

--- src/objects/value-serializer.h
+++ src/objects/value-serializer.h
@@ -128,7 +128,6 @@ class ValueSerializer {
   Maybe<bool> WriteJSArrayBuffer(Handle<JSArrayBuffer> array_buffer)
       V8_WARN_UNUSED_RESULT;
   Maybe<bool> WriteJSArrayBufferView(JSArrayBufferView array_buffer);
-  Maybe<bool> WriteJSError(Handle<JSObject> error) V8_WARN_UNUSED_RESULT;
   Maybe<bool> WriteWasmModule(Handle<WasmModuleObject> object)
       V8_WARN_UNUSED_RESULT;
   Maybe<bool> WriteWasmMemory(Handle<WasmMemoryObject> object)
@@ -277,7 +276,6 @@ class ValueDeserializer {
       V8_WARN_UNUSED_RESULT;
   MaybeHandle<JSArrayBufferView> ReadJSArrayBufferView(
       Handle<JSArrayBuffer> buffer) V8_WARN_UNUSED_RESULT;
-  MaybeHandle<Object> ReadJSError() V8_WARN_UNUSED_RESULT;
   MaybeHandle<JSObject> ReadWasmModule() V8_WARN_UNUSED_RESULT;
   MaybeHandle<JSObject> ReadWasmModuleTransfer() V8_WARN_UNUSED_RESULT;
   MaybeHandle<WasmMemoryObject> ReadWasmMemory() V8_WARN_UNUSED_RESULT;
