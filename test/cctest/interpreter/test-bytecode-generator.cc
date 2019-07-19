d9c4553a25621f6264beed40ab4a89af68dde357
Dan Elphick <delphick@chromium.org>
2019-07-19 10:37:12 +0100

    [test] Fix source positions in bytecode expectations
    Fix a cctest/test-bytecode-generator/PrivateMethods mismatch between the
    PrivateMethods source string and the snippet in the golden file due to
    missing newline at the end of the string. Change C++ raw string back to
    a normal string since in this case it makes it harder to see the
    problem.
    
    Change-Id: I3bea8873d37fbacac65548be8261f6b04104132f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1709413
    Auto-Submit: Dan Elphick <delphick@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#62823}

--- test/cctest/interpreter/test-bytecode-generator.cc
+++ test/cctest/interpreter/test-bytecode-generator.cc
@@ -2765,29 +2765,31 @@ TEST(PrivateMethods) {
   BytecodeExpectationsPrinter printer(CcTest::isolate());
 
   const char* snippets[] = {
-      R"({
-  class A {
-    #a() { return 1; }
-    callA() { return this.#a(); }
-  }
+      "{\n"
+      "  class A {\n"
+      "    #a() { return 1; }\n"
+      "    callA() { return this.#a(); }\n"
+      "  }\n"
+      "\n"
+      "  const a = new A;\n"
+      "  a.callA();\n"
+      "}\n",
 
-  const a = new A;
-  a.callA();
-})",
-      R"({
-  class D {
-    #d() { return 1; }
-    callD() { return this.#d(); }
-  }
+      "{\n"
+      "  class D {\n"
+      "    #d() { return 1; }\n"
+      "    callD() { return this.#d(); }\n"
+      "  }\n"
+      "\n"
+      "  class E extends D {\n"
+      "    #e() { return 2; }\n"
+      "    callE() { return this.callD() + this.#e(); }\n"
+      "  }\n"
+      "\n"
+      "  const e = new E;\n"
+      "  e.callE();\n"
+      "}\n"};
 
-  class E extends D {
-    #e() { return 2; }
-    callE() { return this.callD() + this.#e(); }
-  }
-
-  const e = new E;
-  e.callE();
-})"};
   CHECK(CompareTexts(BuildActual(printer, snippets),
                      LoadGolden("PrivateMethods.golden")));
   i::FLAG_harmony_private_methods = old_methods_flag;
