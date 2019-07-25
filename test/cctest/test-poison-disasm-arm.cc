2869d9de0d34c8242a633ee0d518ea949fbabd23
Artem Serov <artem.serov@arm.com>
2019-07-25 18:35:08 +0100

    [turbofan,arm64] Add float loads poisoning.
    Also extend load poisoning testing for arm and arm64.
    
    This is a port of I1ef202296744a39054366f2bc424d6952c3bbe9d,
    originally introduced for arm.
    
    Change-Id: I7d317bba6be633dd1e563daa7231d3c5e930f8e4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1691032
    Commit-Queue: Martyn Capewell <martyn.capewell@arm.com>
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#63519}

--- test/cctest/test-poison-disasm-arm.cc
+++ test/cctest/test-poison-disasm-arm.cc
@@ -101,11 +101,48 @@ TEST(DisasmPoisonPolymorphicLoad) {
       "csdb",                                             // spec. barrier
       "ldr <<BSt:r[0-9]+>>, \\[<<Obj>>, #\\+[0-9]+\\]",   // load backing store
       "and <<BSt>>, <<BSt>>, " + kPReg,                   // apply the poison
-      "ldr <<Prop:r[0-9]+>>, \\[<<Obj>>, #\\+[0-9]+\\]",  // load the property
+      "ldr <<Prop:r[0-9]+>>, \\[<<BSt>>, #\\+[0-9]+\\]",  // load the property
       "and <<Prop>>, <<Prop>>, " + kPReg,                 // apply the poison
                                                           // Ldone:
   };
   CHECK(CheckDisassemblyRegexPatterns("poly", patterns_array));
+#endif  // ENABLE_DISASSEMBLER
+}
+
+TEST(DisasmPoisonMonomorphicLoadFloat64) {
+#ifdef ENABLE_DISASSEMBLER
+  if (i::FLAG_always_opt || !i::FLAG_opt) return;
+
+  i::FLAG_allow_natives_syntax = true;
+  i::FLAG_untrusted_code_mitigations = true;
+
+  CcTest::InitializeVM();
+  v8::HandleScope scope(CcTest::isolate());
+
+  CompileRun(
+      "function mono(o) { return o.x; }"
+      "%PrepareFunctionForOptimization(mono);"
+      "mono({ x : 1.1 });"
+      "mono({ x : 1.1 });"
+      "%OptimizeFunctionOnNextCall(mono);"
+      "mono({ x : 1.1 });");
+
+  // Matches that the property access sequence is instrumented with
+  // poisoning.
+  std::vector<std::string> patterns_array = {
+      "ldr <<Map:r[0-9]+>>, \\[<<Obj:r[0-9]+>>, #-1\\]",   // load map
+      "ldr <<ExpMap:r[0-9]+>>, \\[pc, #",                  // load expected map
+      "cmp <<Map>>, <<ExpMap>>",                           // compare maps
+      "bne",                                               // deopt if different
+      "eorne " + kPReg + ", " + kPReg + ", " + kPReg,      // update the poison
+      "csdb",                                              // spec. barrier
+      "ldr <<Field:r[0-9]+>>, \\[<<Obj>>, #\\+[0-9]+\\]",  // load the field
+      "mov <<Mov:r[0-9]+>>, #[0-9]+",                      // addr. calculation
+      "add ip, <<Field>>, <<Mov>>",                        // addr. calculation
+      "and ip, ip, " + kPReg,                              // apply the poison
+      "vldr d[0-9]+, \\[ip",                               // load Float64
+  };
+  CHECK(CheckDisassemblyRegexPatterns("mono", patterns_array));
 #endif  // ENABLE_DISASSEMBLER
 }
 
