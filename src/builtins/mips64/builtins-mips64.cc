484c155825ab7caf1b8483e5bfa579bc50cffa8a
Yu Yin <xwafish@gmail.com>
2019-07-25 11:58:23 +0800

    [mips][wasm] Access "CEntryStub" from the root set.
    Port 5564356 https://crrev.com/c/1714656
    
    Original Commit Message:
    
        This removes a shortcut to a "CEntryStub" from the instance object and
        instead loads those values via the root set which is also referenced
        from the instance. It makes instance objects smaller.
    
    Change-Id: I62d16960cb18e3cd69c2fa56da85a6bfc6064db4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1716473
    Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    Commit-Queue: Yu Yin <xwafish@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#62907}

--- src/builtins/mips64/builtins-mips64.cc
+++ src/builtins/mips64/builtins-mips64.cc
@@ -2525,7 +2525,10 @@ void Builtins::Generate_WasmCompileLazy(MacroAssembler* masm) {
     __ Push(kWasmInstanceRegister, kWasmCompileLazyFuncIndexRegister);
     // Load the correct CEntry builtin from the instance object.
     __ Ld(a2, FieldMemOperand(kWasmInstanceRegister,
-                              WasmInstanceObject::kCEntryStubOffset));
+                              WasmInstanceObject::kIsolateRootOffset));
+    auto centry_id =
+        Builtins::kCEntry_Return1_DontSaveFPRegs_ArgvOnStack_NoBuiltinExit;
+    __ Ld(a2, MemOperand(a2, IsolateData::builtin_slot_offset(centry_id)));
     // Initialize the JavaScript context with 0. CEntry will use it to
     // set the current context on the isolate.
     __ Move(kContextRegister, Smi::zero());
