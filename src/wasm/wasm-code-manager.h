a3218aa7442bc606dcb689581be1d890d9253295
Clemens Hammacher <clemensh@chromium.org>
2019-08-02 11:21:21 +0200

    [wasm] Add support to allocate in a specific code region
    For multiple code spaces with multiple jump tables, we need to allocate
    the space for the jump tables within specific regions (the new code
    spaces) so they are within reachable distance.
    This CL adds support for that, and uses it for the first jump table.
    
    R=mstarzinger@chromium.org
    
    Bug: v8:9477
    Change-Id: Ibdf05c9500c2dfdb2c5f5f920b4422339aaab810
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1733069
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#63074}

--- src/wasm/wasm-code-manager.h
+++ src/wasm/wasm-code-manager.h
@@ -60,6 +60,10 @@ class V8_EXPORT_PRIVATE DisjointAllocationPool final {
   // failure.
   base::AddressRegion Allocate(size_t size);
 
+  // Allocate a contiguous region of size {size} within {region}. Return an
+  // empty pool on failure.
+  base::AddressRegion AllocateInRegion(size_t size, base::AddressRegion);
+
   bool IsEmpty() const { return regions_.empty(); }
   const std::list<base::AddressRegion>& regions() const { return regions_; }
 
@@ -294,6 +298,11 @@ class WasmCodeAllocator {
   // Allocate code space. Returns a valid buffer or fails with OOM (crash).
   Vector<byte> AllocateForCode(NativeModule*, size_t size);
 
+  // Allocate code space within a specific region. Returns a valid buffer or
+  // fails with OOM (crash).
+  Vector<byte> AllocateForCodeInRegion(NativeModule*, size_t size,
+                                       base::AddressRegion);
+
   // Sets permissions of all owned code space to executable, or read-write (if
   // {executable} is false). Returns true on success.
   V8_EXPORT_PRIVATE bool SetExecutable(bool executable);
@@ -517,7 +526,8 @@ class V8_EXPORT_PRIVATE NativeModule final {
   WasmCode* AddAndPublishAnonymousCode(Handle<Code>, WasmCode::Kind kind,
                                        const char* name = nullptr);
 
-  WasmCode* CreateEmptyJumpTable(uint32_t jump_table_size);
+  WasmCode* CreateEmptyJumpTableInRegion(uint32_t jump_table_size,
+                                         base::AddressRegion);
 
   // Called by the {WasmCodeAllocator} to register a new code space.
   void AddCodeSpace(base::AddressRegion);
