8b66189b6f3e8ce94a5d20b059538b13d9c44589
Ulan Degenbaev <ulan@chromium.org>
2019-08-02 13:32:29 +0200

    [heap] Bump up the number of scavenging tasks
    Increasing the number of scavenging tasks in the previous CL
    https://chromium-review.googlesource.com/c/v8/v8/+/1697245
    has helped with GC performance but increased memory usage.
    
    This CL ensures that there are at least two scavenging tasks if the
    new space size is larger than one MB.
    
    Bug: chromium:982663,chromium:985731
    Change-Id: I6edff8b90beb2740a5b75a51cf25f48aa5442482
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1733074
    Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#63060}

--- src/heap/scavenger.cc
+++ src/heap/scavenger.cc
@@ -371,7 +371,7 @@ void ScavengerCollector::MergeSurvivingNewLargeObjects(
 int ScavengerCollector::NumberOfScavengeTasks() {
   if (!FLAG_parallel_scavenge) return 1;
   const int num_scavenge_tasks =
-      static_cast<int>(heap_->new_space()->TotalCapacity()) / MB;
+      static_cast<int>(heap_->new_space()->TotalCapacity()) / MB + 1;
   static int num_cores = V8::GetCurrentPlatform()->NumberOfWorkerThreads() + 1;
   int tasks =
       Max(1, Min(Min(num_scavenge_tasks, kMaxScavengerTasks), num_cores));
