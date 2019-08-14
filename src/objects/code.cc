cfe6ceae3ffd2524123678eddec270523530bd7d
Dan Elphick <delphick@chromium.org>
2019-08-14 15:30:03 +0100

    Fix crash Code::DropStackFrameCacheCommon
    Fixes DCHECK failure in DropStackFrameCacheCommon by returning early if
    the source_position_table is Exception.
    
    Bug: chromium:990582, v8:8510
    Change-Id: I671f3e0cdc9f880dedf8ecd2fffb1083229dc6dc
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1752856
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Auto-Submit: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#63209}

--- src/objects/code.cc
+++ src/objects/code.cc
@@ -193,7 +193,10 @@ namespace {
 template <typename Code>
 void DropStackFrameCacheCommon(Code code) {
   i::Object maybe_table = code.source_position_table();
-  if (maybe_table.IsUndefined() || maybe_table.IsByteArray()) return;
+  if (maybe_table.IsUndefined() || maybe_table.IsByteArray() ||
+      maybe_table.IsException()) {
+    return;
+  }
   DCHECK(maybe_table.IsSourcePositionTableWithFrameCache());
   code.set_source_position_table(
       i::SourcePositionTableWithFrameCache::cast(maybe_table)
