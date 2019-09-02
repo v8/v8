539041c554adee2e4ed790a677b38e48696c3093
Igor Sheludko <ishell@chromium.org>
2019-09-02 13:12:59 +0200

    [ptr-compr][turbofan] Implement GraphAssembler::TaggedEqual() as WordEqual()
    ... which already does good the right job of generating 32-bit comparisons
    while Word32Equal() seems to break some optimizations in TurboFan and produce
    a little bit slower code.
    
    Bug: v8:8948
    Change-Id: I288656479fa32c34a7253c4879cb3566d7470a6a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1781047
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Tobias Tebbi <tebbi@chromium.org>
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#63501}

--- src/compiler/graph-assembler.cc
+++ src/compiler/graph-assembler.cc
@@ -99,10 +99,6 @@ Node* GraphAssembler::IntPtrEqual(Node* left, Node* right) {
 }
 
 Node* GraphAssembler::TaggedEqual(Node* left, Node* right) {
-  if (machine()->Is64() && COMPRESS_POINTERS_BOOL) {
-    // Allow implicit truncation.
-    return Word32Equal(left, right);
-  }
   return WordEqual(left, right);
 }
 
