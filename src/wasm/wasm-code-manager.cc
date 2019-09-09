d7d25d2abc5b0d784dc842e484181d94485f6db0
Clemens Hammacher <clemensh@chromium.org>
2019-09-09 14:52:24 +0200

    [wasm] Patch jump tables in all code spaces
    If there are multiple code spaces, make sure to patch the jump tables
    in all of them.
    
    R=mstarzinger@chromium.org
    
    Bug: v8:9477
    Change-Id: I2ec3d3de913b99623fd310004555337329588da0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1789289
    Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
    Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#63651}

--- src/wasm/wasm-code-manager.cc
+++ src/wasm/wasm-code-manager.cc
@@ -839,13 +839,13 @@ void NativeModule::UseLazyStub(uint32_t func_index) {
 
   // Add jump table entry for jump to the lazy compile stub.
   uint32_t slot_index = func_index - module_->num_imported_functions;
+  DCHECK_NULL(code_table_[slot_index]);
   DCHECK_NE(runtime_stub_entry(WasmCode::kWasmCompileLazy), kNullAddress);
   Address lazy_compile_target =
       lazy_compile_table_->instruction_start() +
       JumpTableAssembler::LazyCompileSlotIndexToOffset(slot_index);
-  JumpTableAssembler::PatchJumpTableSlot(main_jump_table_->instruction_start(),
-                                         slot_index, lazy_compile_target,
-                                         WasmCode::kFlushICache);
+  base::MutexGuard guard(&allocation_mutex_);
+  PatchJumpTablesLocked(func_index, lazy_compile_target);
 }
 
 // TODO(mstarzinger): Remove {Isolate} parameter once {V8_EMBEDDED_BUILTINS}
@@ -1035,9 +1035,7 @@ WasmCode* NativeModule::PublishCodeLocked(std::unique_ptr<WasmCode> code) {
     }
 
     if (update_jump_table) {
-      JumpTableAssembler::PatchJumpTableSlot(
-          main_jump_table_->instruction_start(), slot_idx,
-          code->instruction_start(), WasmCode::kFlushICache);
+      PatchJumpTablesLocked(code->index(), code->instruction_start());
     }
   }
   WasmCodeRefScope::AddRef(code.get());
@@ -1129,6 +1127,19 @@ WasmCode* NativeModule::CreateEmptyJumpTableInRegion(
       WasmCode::kJumpTable,                     // kind
       ExecutionTier::kNone}};                   // tier
   return PublishCode(std::move(code));
+}
+
+void NativeModule::PatchJumpTablesLocked(uint32_t func_index, Address target) {
+  // The caller must hold the {allocation_mutex_}, thus we fail to lock it here.
+  DCHECK(!allocation_mutex_.TryLock());
+
+  uint32_t slot_index = func_index - module_->num_imported_functions;
+  for (auto& code_space_data : code_space_data_) {
+    if (!code_space_data.jump_table) continue;
+    Address jump_table_base = code_space_data.jump_table->instruction_start();
+    JumpTableAssembler::PatchJumpTableSlot(jump_table_base, slot_index, target,
+                                           WasmCode::kFlushICache);
+  }
 }
 
 void NativeModule::AddCodeSpace(base::AddressRegion region) {
