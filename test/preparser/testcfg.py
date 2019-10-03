b64c05a17b8095a5a8d675b78f5af87ee1445ae3
Arpit Maheshwari <arpit08@protonmail.com>
2019-10-03 09:31:04 +0530

    Update testcfg.py
    Updated execfile() with exec(open().read()) for python3

--- test/preparser/testcfg.py
+++ test/preparser/testcfg.py
@@ -66,7 +66,7 @@ class TestSuite(testsuite.TestSuite):
           testsource = testsource.replace("$" + key, replacement[key]);
         Test(testname, testsource, expectation)
       return MkTest
-    execfile(pathname, {"Test": Test, "Template": Template})
+    exec(open(pathname).read(), {"Test": Test, "Template": Template})
 
   def ListTests(self):
     result = []
