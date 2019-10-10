bd7fc1588b12790f461944e307f189a2fc1cd0ca
Joyee Cheung <joyee@igalia.com>
2019-10-11 04:11:08 +0800

    Run class-methods-private and class-static-methods-private test262
    Bug: v8:8330, v8:9611
    Change-Id: Iec4771311a1be0a6d4fc299740c6a965aee6c291
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1854009
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Joyee Cheung <joyee@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#64227}

--- test/test262/testcfg.py
+++ test/test262/testcfg.py
@@ -59,10 +59,11 @@ FEATURE_FLAGS = {
   'regexp-match-indices': '--harmony-regexp-match-indices',
   # https://github.com/tc39/test262/pull/2395
   'regexp-named-groups': '--harmony-regexp-match-indices',
+  'class-methods-private': '--harmony-private-methods',
+  'class-static-methods-private': '--harmony-private-methods',
 }
 
-SKIPPED_FEATURES = set(['class-methods-private',
-                        'class-static-methods-private'])
+SKIPPED_FEATURES = set([])
 
 DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
