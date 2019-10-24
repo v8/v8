bdf42929a1e3f63b956d510249a5ede57d2a1be7
Shu-yu Guo <syg@chromium.org>
2019-10-24 17:34:24 +0000

    Revert "[strings] Fix hash for exactly 512MB long strings"
    This reverts commit 556f44c494b2b1e6ef878e4d2846baaee223b63b.
    
    Reason for revert: Test fatally OOMs on ARM. https://ci.chromium.org/p/v8/builders/ci/V8%20Arm/12336
    
    Original change's description:
    > [strings] Fix hash for exactly 512MB long strings
    > 
    > Bug: chromium:1016237
    > Change-Id: Idda1e44b5d578d1213aa54927ca68289bcdce8ac
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1878487
    > Commit-Queue: Jakob Kummerow <jkummerow@chromium.org>
    > Reviewed-by: Igor Sheludko <ishell@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#64552}
    
    TBR=jkummerow@chromium.org,ishell@chromium.org
    
    Change-Id: Ia942469346b0f11fcf853d21717fd127815f7fba
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1016237
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1879669
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#64553}

--- test/mjsunit/regress/regress-crbug-1016237.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 the V8 project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Flags: --allow-natives-syntax
-
-const kLength = Math.min(536870912, %StringMaxLength());
-const v2 = "foo".padEnd(kLength);
-delete v2[v2];
