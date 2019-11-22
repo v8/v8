94e21dea5287da7b823d81316c563e9a6feb5b7c
Joshua Litt <joshualitt@chromium.org>
2019-11-22 08:32:09 -0800

    [regexp] Add additional test for match indices.
    Bug: v8:9548
    Change-Id: I0842ca8ce49ea3a831ae4f168c6dfa7d65dfe063
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1930173
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Joshua Litt <joshualitt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#65156}

--- test/mjsunit/harmony/regexp-match-indices.js
+++ test/mjsunit/harmony/regexp-match-indices.js
@@ -135,3 +135,12 @@
   }
   assertTrue(rec());
 }
+
+// Match between matches.
+{
+  const re = /a+(?<A>zz)?(?<B>ii)?/;
+  const m = re.exec("xaaazzii");
+  assertTrue(/b+(?<C>cccc)?/.test("llllllbbbbbbcccc"));
+  assertEquals(m.indices, [[1, 8], [4, 6], [6, 8]]);
+  assertEquals(m.indices.groups, {'A': [4, 6], 'B': [6, 8]});
+}
