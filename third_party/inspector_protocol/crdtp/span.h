4a98a2608b0cc9d8dbb4dcd1ccceba916ab35f32
Johannes Henkel <johannes@chromium.org>
2019-12-09 12:33:50 -0800

    [DevTools] Roll inspector_protocol. (V8)
    Add Exported::AppendSerialized (consistency with Serialized interface).
    Deprecate Exported::writeBinary.
    
    Upstream PRs:
    https://chromium-review.googlesource.com/c/deps/inspector_protocol/+/1958506
    https://chromium-review.googlesource.com/c/deps/inspector_protocol/+/1941035
    
    Change-Id: I50d6db05ea7c1336022b0b63e5ec2c69488ab525
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1958575
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#65436}

--- third_party/inspector_protocol/crdtp/span.h
+++ third_party/inspector_protocol/crdtp/span.h
@@ -25,25 +25,25 @@ class span {
   constexpr span() : data_(nullptr), size_(0) {}
   constexpr span(const T* data, index_type size) : data_(data), size_(size) {}
 
-  const T* data() const { return data_; }
+  constexpr const T* data() const { return data_; }
 
-  const T* begin() const { return data_; }
-  const T* end() const { return data_ + size_; }
+  constexpr const T* begin() const { return data_; }
+  constexpr const T* end() const { return data_ + size_; }
 
-  const T& operator[](index_type idx) const { return data_[idx]; }
+  constexpr const T& operator[](index_type idx) const { return data_[idx]; }
 
-  span<T> subspan(index_type offset, index_type count) const {
+  constexpr span<T> subspan(index_type offset, index_type count) const {
     return span(data_ + offset, count);
   }
 
-  span<T> subspan(index_type offset) const {
+  constexpr span<T> subspan(index_type offset) const {
     return span(data_ + offset, size_ - offset);
   }
 
-  bool empty() const { return size_ == 0; }
+  constexpr bool empty() const { return size_ == 0; }
 
-  index_type size() const { return size_; }
-  index_type size_bytes() const { return size_ * sizeof(T); }
+  constexpr index_type size() const { return size_; }
+  constexpr index_type size_bytes() const { return size_ * sizeof(T); }
 
  private:
   const T* data_;
@@ -56,11 +56,11 @@ constexpr span<T> SpanFrom(const std::vector<T>& v) {
 }
 
 template <size_t N>
-span<uint8_t> SpanFrom(const char (&str)[N]) {
+constexpr span<uint8_t> SpanFrom(const char (&str)[N]) {
   return span<uint8_t>(reinterpret_cast<const uint8_t*>(str), N - 1);
 }
 
-inline span<uint8_t> SpanFrom(const char* str) {
+constexpr inline span<uint8_t> SpanFrom(const char* str) {
   return str ? span<uint8_t>(reinterpret_cast<const uint8_t*>(str), strlen(str))
              : span<uint8_t>();
 }
@@ -71,13 +71,13 @@ inline span<uint8_t> SpanFrom(const std::string& v) {
 
 // Less than / equality comparison functions for sorting / searching for byte
 // spans. These are similar to absl::string_view's < and == operators.
-inline bool SpanLessThan(span<uint8_t> x, span<uint8_t> y) noexcept {
+constexpr inline bool SpanLessThan(span<uint8_t> x, span<uint8_t> y) noexcept {
   auto min_size = std::min(x.size(), y.size());
   const int r = min_size == 0 ? 0 : memcmp(x.data(), y.data(), min_size);
   return (r < 0) || (r == 0 && x.size() < y.size());
 }
 
-inline bool SpanEquals(span<uint8_t> x, span<uint8_t> y) noexcept {
+constexpr inline bool SpanEquals(span<uint8_t> x, span<uint8_t> y) noexcept {
   auto len = x.size();
   if (len != y.size())
     return false;
