37f15b89eb429f0f73088e940fc485e6b237d914
Peter Marshall <petermarshall@chromium.org>
2020-01-02 15:29:26 +0100

    [cleanup] Remove unused RunsTasksOnCurrentThread on task runner
    This was needed for Perfetto at one point but now this is all handled
    internally by the Perfetto Client API. The implementation was
    potentially wrong as on some platforms we truncate the the thread ID
    https://chromium-review.googlesource.com/c/v8/v8/+/1720821/1/src/base/platform/platform-posix.cc#528
    
    Change-Id: I5124f4fb0df4d29ff78ab6c7a2c2b0c9a66a2af3
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1981508
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#65576}

--- test/unittests/libplatform/default-worker-threads-task-runner-unittest.cc
+++ test/unittests/libplatform/default-worker-threads-task-runner-unittest.cc
@@ -274,25 +274,5 @@ TEST(DefaultWorkerThreadsTaskRunnerUnittest, NoIdleTasks) {
   runner.Terminate();
 }
 
-TEST(DefaultWorkerThreadsTaskRunnerUnittest, RunsTasksOnCurrentThread) {
-  DefaultWorkerThreadsTaskRunner runner(1, RealTime);
-
-  base::Semaphore semaphore(0);
-
-  EXPECT_FALSE(runner.RunsTasksOnCurrentThread());
-
-  std::unique_ptr<TestTask> task1 = std::make_unique<TestTask>([&] {
-    EXPECT_TRUE(runner.RunsTasksOnCurrentThread());
-    semaphore.Signal();
-  });
-  runner.PostTask(std::move(task1));
-
-  semaphore.Wait();
-  EXPECT_FALSE(runner.RunsTasksOnCurrentThread());
-
-  runner.Terminate();
-  EXPECT_FALSE(runner.RunsTasksOnCurrentThread());
-}
-
 }  // namespace platform
 }  // namespace v8
