9ab99f2903099c3358fa83174fcc308a4e1ba8ab
Dominik Inführ <dinfuehr@chromium.org>
2020-01-13 18:33:39 +0100

    [heap] Round max_semi_space_size_ again to power-of-2
    Removing this caused performance regressions.
    
    Bug: chromium:1036514
    Change-Id: If24826874c45c21670a59f9dbbe57dacd44ae5fc
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1998080
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#65745}

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -4494,6 +4494,10 @@ void Heap::ConfigureHeap(const v8::ResourceConstraints& constraints) {
       // This will cause more frequent GCs when stressing.
       max_semi_space_size_ = MB;
     }
+    // TODO(dinfuehr): Rounding to a power of 2 is not longer needed. Remove it.
+    max_semi_space_size_ =
+        static_cast<size_t>(base::bits::RoundUpToPowerOfTwo64(
+            static_cast<uint64_t>(max_semi_space_size_)));
     max_semi_space_size_ = Max(max_semi_space_size_, kMinSemiSpaceSize);
     max_semi_space_size_ = RoundDown<Page::kPageSize>(max_semi_space_size_);
   }
