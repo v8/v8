a6a2e888d606b6e869c9a77a36535bf105393524
Dan Elphick <delphick@chromium.org>
2020-01-15 21:34:55 +0000

    [codegen] Don't return after CSA_CHECK failures
    Changes FastCheck failures to just call Unreachable (which in turn
    calls DebugBreak) instead of DebugBreak and then returning to just
    after the check. FastCheck is only called in release builds so this
    does not affect debug builds.
    
    This reduces the embedded instruction size from 1249720 to 1246812
    (2908 bytes) for ARM.
    
    Change-Id: If4b9b6810a53d64262a0fa9c2a1903e022748a22
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2002538
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#65803}

--- src/codegen/code-stub-assembler.cc
+++ src/codegen/code-stub-assembler.cc
@@ -280,10 +280,7 @@ void CodeStubAssembler::FastCheck(TNode<BoolT> condition) {
   Label ok(this), not_ok(this, Label::kDeferred);
   Branch(condition, &ok, &not_ok);
   BIND(&not_ok);
-  {
-    DebugBreak();
-    Goto(&ok);
-  }
+  Unreachable();
   BIND(&ok);
 }
 
