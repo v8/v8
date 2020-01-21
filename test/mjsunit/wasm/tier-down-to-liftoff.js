faccc95b77b3ac9aa9e3ca8843de18fbdd6f2407
Z Nguyen-Huu <duongn@microsoft.com>
2020-01-21 08:53:43 -0800

    Reland "[wasm] Perform NativeModule tier down in parallel."
    This is a reland of 3352fcc900d4c24891c5b4b0a9b3056ea7029909
    
    Disable stress-opt for test and check recompilation before clearing
    callbacks.
    
    Original change's description:
    > [wasm] Perform NativeModule tier down in parallel.
    >
    > Reuse logic in {CompileNativeModule} function in module-compiler.cc:
    > initialize parallel compile jobs, then wait for them to finish while
    > taking part in this compilation.
    >
    > Bug: v8:9654
    > Change-Id: I9974d9f8b516e9faec716a592c7c0ee9c7077d8e
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1977041
    > Commit-Queue: Z Nguyen-Huu <duongn@microsoft.com>
    > Reviewed-by: Clemens Backes <clemensb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#65763}
    
    Bug: v8:9654
    Change-Id: I8e8830f05e189596207365b7332a2cc25e493e47
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2002945
    Commit-Queue: Z Nguyen-Huu <duongn@microsoft.com>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#65901}

--- test/mjsunit/wasm/tier-down-to-liftoff.js
+++ test/mjsunit/wasm/tier-down-to-liftoff.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --allow-natives-syntax --wasm-tier-up
+// Flags: --allow-natives-syntax --liftoff --wasm-tier-up --no-stress-opt
 
 load('test/mjsunit/wasm/wasm-module-builder.js');
 
@@ -40,7 +40,6 @@ function check(instance) {
   const instance = create_builder().instantiate();
   check(instance);
 })();
-
 
 async function testTierDownToLiftoffAsync() {
   print(arguments.callee.name);
