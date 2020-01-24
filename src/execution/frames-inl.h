d422dc741cbb83e4fe4d3fd2781215c3dd6eee96
Peter Marshall <petermarshall@chromium.org>
2020-01-24 15:59:20 +0100

    [cpu-profiler] Allow wasm to js frames in SafeStackFrameIterator::frame
    SafeStackFrameIterator::Advance() will stop at wasm_to_js frames, so we
    should fully expect that they show up when calling frame() after
    Advance().
    
    This fixes some breakages for profiling wasm and asm.js.
    
    Bug: chromium:1010534, v8:10150
    Change-Id: Ifc78cd9eba33e777b33fc1cbea19ee376b25299c
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2019485
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#65980}

--- src/execution/frames-inl.h
+++ src/execution/frames-inl.h
@@ -307,7 +307,8 @@ JavaScriptFrame* StackTraceFrameIterator::javascript_frame() const {
 inline StackFrame* SafeStackFrameIterator::frame() const {
   DCHECK(!done());
   DCHECK(frame_->is_java_script() || frame_->is_exit() ||
-         frame_->is_builtin_exit() || frame_->is_wasm());
+         frame_->is_builtin_exit() || frame_->is_wasm() ||
+         frame_->is_wasm_to_js());
   return frame_;
 }
 
