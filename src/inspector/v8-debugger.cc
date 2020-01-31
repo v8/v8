b87586d29b2b75c7e2f6520c0ce4b8177527a392
Benedikt Meurer <bmeurer@chromium.org>
2020-01-31 15:24:34 +0100

    [inspector] Fix async stepping throw extension code.
    When async stepping through code where only extension scripts are on the
    stack, we the iterator is not gonna yield any frames, since it ignores
    all extension frames.
    
    Bug: chromium:1045863
    Change-Id: Ifdb966e47bf1d090fa695d066003c7aff343f658
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2033167
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#66069}

--- src/inspector/v8-debugger.cc
+++ src/inspector/v8-debugger.cc
@@ -295,7 +295,10 @@ bool V8Debugger::asyncStepOutOfFunction(int targetContextGroupId,
                                         bool onlyAtReturn) {
   v8::HandleScope handleScope(m_isolate);
   auto iterator = v8::debug::StackTraceIterator::Create(m_isolate);
-  CHECK(!iterator->Done());
+  // When stepping through extensions code, it is possible that the
+  // iterator doesn't have any frames, since we exclude all frames
+  // that correspond to extension scripts.
+  if (iterator->Done()) return false;
   bool atReturn = !iterator->GetReturnValue().IsEmpty();
   iterator->Advance();
   // Synchronous stack has more then one frame.
