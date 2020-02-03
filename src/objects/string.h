3d53c701ee9bfad3db65dbe4cf139775ca55531e
Jakob Kummerow <jkummerow@chromium.org>
2020-02-03 14:12:06 +0100

    Merged: [ptr-compr] Bump max string length to ~2**29
    Without pointer compression, the max string length on 64-bit platforms
    used to be 2**30 (minus header). With pointer-compression, this was
    accidentally lowered to 2**28 (which is the historical limit for 32-bit
    platforms). This CL bumps the limit on 64-bit platforms to 2**29, which
    is the maximum we can support given that any heap object's size in bytes
    must fit into a Smi (which are now 31-bit on all 64-bit platforms, with
    or without pointer compression).
    
    (cherry picked from commit ea56bf5513d0cbd2a35a9035c5c2996272b8b728)
    
    Bug: v8:10184
    Change-Id: I263544317d9e6137f6b6a044784a21f41a2761b0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2030916
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#66083}
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2046886
    Cr-Commit-Position: refs/branch-heads/8.1@{#8}
    Cr-Branched-From: a4dcd39d521d14c4b1cac020812e44ee04a7f244-refs/heads/8.1.307@{#1}
    Cr-Branched-From: f22c213304ec3542df87019aed0909b7dafeaa93-refs/heads/master@{#66031}

--- src/objects/string.h
+++ src/objects/string.h
@@ -363,16 +363,28 @@ class String : public TorqueGeneratedString<String, Name> {
   static const uc32 kMaxCodePoint = 0x10ffff;
 
   // Maximal string length.
-  // The max length is different on 32 and 64 bit platforms. Max length for a
-  // 32-bit platform is ~268.4M chars. On 64-bit platforms, max length is
-  // ~1.073B chars. The limit on 64-bit is so that SeqTwoByteString::kMaxSize
-  // can fit in a 32bit int: 2^31 - 1 is the max positive int, minus one bit as
-  // each char needs two bytes, subtract 24 bytes for the string header size.
-
+  // The max length is different on 32 and 64 bit platforms. Max length for
+  // 32-bit platforms is ~268.4M chars. On 64-bit platforms, max length is
+  // ~536.8M chars.
   // See include/v8.h for the definition.
   static const int kMaxLength = v8::String::kMaxLength;
-  static_assert(kMaxLength <= (Smi::kMaxValue / 2 - kHeaderSize),
-                "Unexpected max String length");
+  // There are several defining limits imposed by our current implementation:
+  // - any string's length must fit into a Smi.
+  static_assert(kMaxLength <= kSmiMaxValue,
+                "String length must fit into a Smi");
+  // - adding two string lengths must still fit into a 32-bit int without
+  //   overflow
+  static_assert(kMaxLength * 2 <= kMaxInt,
+                "String::kMaxLength * 2 must fit into an int32");
+  // - any heap object's size in bytes must be able to fit into a Smi, because
+  //   its space on the heap might be filled with a Filler; for strings this
+  //   means SeqTwoByteString::kMaxSize must be able to fit into a Smi.
+  static_assert(kMaxLength * 2 + kHeaderSize <= kSmiMaxValue,
+                "String object size in bytes must fit into a Smi");
+  // - any heap object's size in bytes must be able to fit into an int, because
+  //   that's what our object handling code uses almost everywhere.
+  static_assert(kMaxLength * 2 + kHeaderSize <= kMaxInt,
+                "String object size in bytes must fit into an int");
 
   // Max length for computing hash. For strings longer than this limit the
   // string length is used as the hash value.
