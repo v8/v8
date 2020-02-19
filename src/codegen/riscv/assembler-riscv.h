4130700a7239917f8baab22813f81362f7447213
Brice Dobry <brice.dobry@futurewei.com>
2020-02-19 07:23:19 -0800

    Add asm support for F and D extensions

--- src/codegen/riscv/assembler-riscv.h
+++ src/codegen/riscv/assembler-riscv.h
@@ -508,6 +508,84 @@ class V8_EXPORT_PRIVATE Assembler : public AssemblerBase {
   void RV_amominu_d(bool aq, bool rl, Register rd, Register rs1, Register rs2);
   void RV_amomaxu_d(bool aq, bool rl, Register rd, Register rs1, Register rs2);
 
+  // RV32F Standard Extension
+  void RV_flw(Register rd, Register rs1, int16_t imm12);
+  void RV_fsw(Register rs1, Register rs2, int16_t imm12);
+  void RV_fmadd_s(Register rd, Register rs1, Register rs2, Register rs3,
+                  uint8_t frm);
+  void RV_fmsub_s(Register rd, Register rs1, Register rs2, Register rs3,
+                  uint8_t frm);
+  void RV_fnmsub_s(Register rd, Register rs1, Register rs2, Register rs3,
+                   uint8_t frm);
+  void RV_fnmadd_s(Register rd, Register rs1, Register rs2, Register rs3,
+                   uint8_t frm);
+  void RV_fadd_s(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fsub_s(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fmul_s(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fdiv_s(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fsqrt_s(Register rd, Register rs1, uint8_t frm);
+  void RV_fsgnj_s(Register rd, Register rs1, Register rs2);
+  void RV_fsgnjn_s(Register rd, Register rs1, Register rs2);
+  void RV_fsgnjx_s(Register rd, Register rs1, Register rs2);
+  void RV_fmin_s(Register rd, Register rs1, Register rs2);
+  void RV_fmax_s(Register rd, Register rs1, Register rs2);
+  void RV_fcvt_w_s(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fcvt_wu_s(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fmv_x_w(Register rd, Register rs1);
+  void RV_feq_s(Register rd, Register rs1, Register rs2);
+  void RV_flt_s(Register rd, Register rs1, Register rs2);
+  void RV_fle_s(Register rd, Register rs1, Register rs2);
+  void RV_fclass_s(Register rd, Register rs1);
+  void RV_fcvt_s_w(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_s_wu(Register rd, Register rs1, uint8_t frm);
+  void RV_fmv_w_x(Register rd, Register rs1);
+
+  // RV64F Standard Extension (in addition to RV32F)
+  void RV_fcvt_l_s(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_lu_s(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_s_l(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_s_lu(Register rd, Register rs1, uint8_t frm);
+
+  // RV32D Standard Extension
+  void RV_fld(Register rd, Register rs1, int16_t imm12);
+  void RV_fsd(Register rs1, Register rs2, int16_t imm12);
+  void RV_fmadd_d(Register rd, Register rs1, Register rs2, Register rs3,
+                  uint8_t frm);
+  void RV_fmsub_d(Register rd, Register rs1, Register rs2, Register rs3,
+                  uint8_t frm);
+  void RV_fnmsub_d(Register rd, Register rs1, Register rs2, Register rs3,
+                   uint8_t frm);
+  void RV_fnmadd_d(Register rd, Register rs1, Register rs2, Register rs3,
+                   uint8_t frm);
+  void RV_fadd_d(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fsub_d(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fmul_d(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fdiv_d(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fsqrt_d(Register rd, Register rs1, uint8_t frm);
+  void RV_fsgnj_d(Register rd, Register rs1, Register rs2);
+  void RV_fsgnjn_d(Register rd, Register rs1, Register rs2);
+  void RV_fsgnjx_d(Register rd, Register rs1, Register rs2);
+  void RV_fmin_d(Register rd, Register rs1, Register rs2);
+  void RV_fmax_d(Register rd, Register rs1, Register rs2);
+  void RV_fcvt_s_d(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_fcvt_d_s(Register rd, Register rs1, Register rs2, uint8_t frm);
+  void RV_feq_d(Register rd, Register rs1, Register rs2);
+  void RV_flt_d(Register rd, Register rs1, Register rs2);
+  void RV_fle_d(Register rd, Register rs1, Register rs2);
+  void RV_fclass_d(Register rd, Register rs1);
+  void RV_fcvt_w_d(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_wu_d(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_d_w(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_d_wu(Register rd, Register rs1, uint8_t frm);
+
+  // RV64D Standard Extension (in addition to RV32D)
+  void RV_fcvt_l_d(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_lu_d(Register rd, Register rs1, uint8_t frm);
+  void RV_fmv_x_d(Register rd, Register rs1);
+  void RV_fcvt_d_l(Register rd, Register rs1, uint8_t frm);
+  void RV_fcvt_d_lu(Register rd, Register rs1, uint8_t frm);
+  void RV_fmv_d_x(Register rd, Register rs1);
+
   // Privileged
   void RV_uret();
   void RV_sret();
@@ -1977,6 +2055,12 @@ class V8_EXPORT_PRIVATE Assembler : public AssemblerBase {
   void GenInstrALUW_rr(uint8_t funct7, uint8_t funct3, Register rd,
                        Register rs1, Register rs2);
   void GenInstrPriv(uint8_t funct7, Register rs1, Register rs2);
+  void GenInstrLoadFP_ri(uint8_t funct3, Register rd, Register rs1,
+                         int16_t imm12);
+  void GenInstrStoreFP_rri(uint8_t funct3, Register rs1, Register rs2,
+                           int16_t imm12);
+  void GenInstrALUFP_rr(uint8_t funct7, uint8_t funct3, Register rd,
+                        Register rs1, Register rs2);
 
   // We have 3 different kind of encoding layout on MIPS.
   // However due to many different types of objects encoded in the same fields
