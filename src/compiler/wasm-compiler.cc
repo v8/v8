4c357f203be379f7bce02c3f3c915c215fc90dd6
Milad Farazmand <miladfar@ca.ibm.com>
2020-02-21 14:01:35 +0000

    [wasm-simd] Scalar lowering for load extends on BE machines
    LoadMem already does the proper sign extension based on the
    passed memtype, so all is left is splatting it to the desired
    number of lanes.
    
    Change-Id: I54317b6e2b4d3daf2e980315198577f61c3f556a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2068031
    Reviewed-by: Zhi An Ng <zhin@chromium.org>
    Reviewed-by: Deepti Gandluri <gdeepti@chromium.org>
    Commit-Queue: Deepti Gandluri <gdeepti@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#66436}

--- src/compiler/wasm-compiler.cc
+++ src/compiler/wasm-compiler.cc
@@ -3754,14 +3754,20 @@ Node* WasmGraphBuilder::LoadTransformBigEndian(
       result = graph()->NewNode(mcgraph()->machine()->I8x16Splat(), value);
       break;
     }
+    case LoadTransformation::kI16x8Load8x8S:
+    case LoadTransformation::kI16x8Load8x8U:
     case LoadTransformation::kS16x8LoadSplat: {
       result = graph()->NewNode(mcgraph()->machine()->I16x8Splat(), value);
       break;
     }
+    case LoadTransformation::kI32x4Load16x4S:
+    case LoadTransformation::kI32x4Load16x4U:
     case LoadTransformation::kS32x4LoadSplat: {
       result = graph()->NewNode(mcgraph()->machine()->I32x4Splat(), value);
       break;
     }
+    case LoadTransformation::kI64x2Load32x2S:
+    case LoadTransformation::kI64x2Load32x2U:
     case LoadTransformation::kS64x2LoadSplat: {
       result = graph()->NewNode(mcgraph()->machine()->I64x2Splat(), value);
       break;
