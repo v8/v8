36b80bea19f25dc1e18b4154bd43c3b51f52b69f
Igor Sheludko <ishell@chromium.org>
2020-02-27 12:52:06 +0100

    [ptr-compr] Fix 32-bit smis on non-ptr-compr builds (rev 2)
    Bug: v8:10047, v8:10257
    Change-Id: Idf7f5e0faf6b8d41795bb362b2473392cdb6e73d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2077682
    Reviewed-by: Santiago Aboy Solanes <solanes@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#66519}

--- src/builtins/x64/builtins-x64.cc
+++ src/builtins/x64/builtins-x64.cc
@@ -1213,9 +1213,9 @@ void Builtins::Generate_InterpreterEntryTrampoline(MacroAssembler* masm) {
   __ bind(&stack_check_interrupt);
   // Modify the bytecode offset in the stack to be kFunctionEntryBytecodeOffset
   // for the call to the StackGuard.
-  __ movq(Operand(rbp, InterpreterFrameConstants::kBytecodeOffsetFromFp),
-          Immediate(Smi::FromInt(BytecodeArray::kHeaderSize - kHeapObjectTag +
-                                 kFunctionEntryBytecodeOffset)));
+  __ Move(Operand(rbp, InterpreterFrameConstants::kBytecodeOffsetFromFp),
+          Smi::FromInt(BytecodeArray::kHeaderSize - kHeapObjectTag +
+                       kFunctionEntryBytecodeOffset));
   __ CallRuntime(Runtime::kStackGuard);
 
   // After the call, restore the bytecode array, bytecode offset and accumulator
