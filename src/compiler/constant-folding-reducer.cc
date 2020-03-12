2c834c53641550bfc0257ee6656953795cfc16de
Georg Neis <neis@chromium.org>
2020-03-12 14:13:24 +0100

    [turbofan] Clean up ConstantFoldingReducer
    Change-Id: Iaf7f83cc157a6f6680da8933560347f7f3503d56
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2098736
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#66706}

--- src/compiler/constant-folding-reducer.cc
+++ src/compiler/constant-folding-reducer.cc
@@ -11,6 +11,36 @@ namespace v8 {
 namespace internal {
 namespace compiler {
 
+namespace {
+Node* TryGetConstant(JSGraph* jsgraph, Node* node) {
+  Type type = NodeProperties::GetType(node);
+  Node* result;
+  if (type.IsNone()) {
+    result = nullptr;
+  } else if (type.Is(Type::Null())) {
+    result = jsgraph->NullConstant();
+  } else if (type.Is(Type::Undefined())) {
+    result = jsgraph->UndefinedConstant();
+  } else if (type.Is(Type::MinusZero())) {
+    result = jsgraph->MinusZeroConstant();
+  } else if (type.Is(Type::NaN())) {
+    result = jsgraph->NaNConstant();
+  } else if (type.Is(Type::Hole())) {
+    result = jsgraph->TheHoleConstant();
+  } else if (type.IsHeapConstant()) {
+    result = jsgraph->Constant(type.AsHeapConstant()->Ref());
+  } else if (type.Is(Type::PlainNumber()) && type.Min() == type.Max()) {
+    result = jsgraph->Constant(type.Min());
+  } else {
+    result = nullptr;
+  }
+  DCHECK_EQ(result != nullptr, type.IsSingleton());
+  DCHECK_IMPLIES(result != nullptr,
+                 NodeProperties::GetType(result).Equals(type));
+  return result;
+}
+}  // namespace
+
 ConstantFoldingReducer::ConstantFoldingReducer(Editor* editor, JSGraph* jsgraph,
                                                JSHeapBroker* broker)
     : AdvancedReducer(editor), jsgraph_(jsgraph), broker_(broker) {}
@@ -19,44 +49,15 @@ ConstantFoldingReducer::~ConstantFoldingReducer() = default;
 
 Reduction ConstantFoldingReducer::Reduce(Node* node) {
   DisallowHeapAccess no_heap_access;
-  // Check if the output type is a singleton.  In that case we already know the
-  // result value and can simply replace the node if it's eliminable.
   if (!NodeProperties::IsConstant(node) && NodeProperties::IsTyped(node) &&
-      node->op()->HasProperty(Operator::kEliminatable)) {
-    // TODO(v8:5303): We must not eliminate FinishRegion here. This special
-    // case can be removed once we have separate operators for value and
-    // effect regions.
-    if (node->opcode() == IrOpcode::kFinishRegion) return NoChange();
-    // We can only constant-fold nodes here, that are known to not cause any
-    // side-effect, may it be a JavaScript observable side-effect or a possible
-    // eager deoptimization exit (i.e. {node} has an operator that doesn't have
-    // the Operator::kNoDeopt property).
-    Type upper = NodeProperties::GetType(node);
-    if (!upper.IsNone()) {
-      Node* replacement = nullptr;
-      if (upper.IsHeapConstant()) {
-        replacement = jsgraph()->Constant(upper.AsHeapConstant()->Ref());
-      } else if (upper.Is(Type::MinusZero())) {
-        Factory* factory = jsgraph()->isolate()->factory();
-        ObjectRef minus_zero(broker(), factory->minus_zero_value());
-        replacement = jsgraph()->Constant(minus_zero);
-      } else if (upper.Is(Type::NaN())) {
-        replacement = jsgraph()->NaNConstant();
-      } else if (upper.Is(Type::Null())) {
-        replacement = jsgraph()->NullConstant();
-      } else if (upper.Is(Type::PlainNumber()) && upper.Min() == upper.Max()) {
-        replacement = jsgraph()->Constant(upper.Min());
-      } else if (upper.Is(Type::Undefined())) {
-        replacement = jsgraph()->UndefinedConstant();
+      node->opcode() != IrOpcode::kFinishRegion) {
+    Node* constant = TryGetConstant(jsgraph(), node);
+    if (constant != nullptr) {
+      if (node->op()->HasProperty(Operator::kEliminatable)) {
+        RelaxEffectsAndControls(node);
       }
-      if (replacement) {
-        // Make sure the node has a type.
-        if (!NodeProperties::IsTyped(replacement)) {
-          NodeProperties::SetType(replacement, upper);
-        }
-        ReplaceWithValue(node, replacement);
-        return Changed(replacement);
-      }
+      ReplaceWithValue(node, constant, node, node);
+      return Changed(node);
     }
   }
   return NoChange();
