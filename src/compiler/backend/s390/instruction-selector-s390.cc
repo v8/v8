252acd7ffc92f0574a1a383f6cd038d0b7f17163
Seth Brenith <seth.brenith@microsoft.com>
2020-03-24 16:58:39 +0000

    Revert "Move branch inversion on ==0 into platform-agnostic reducer"
    This reverts commit 0c72c7190068379983c5f0f57715001dd45650d3.
    
    Reason for revert: Wasm code size increase because not all pipelines use CommonOperatorReducer
    
    Original change's description:
    > Move branch inversion on ==0 into platform-agnostic reducer
    > 
    > This change is based on a discussion from
    > https://crrev.com/c/v8/v8/+/2053769/4/src/compiler/machine-operator-reducer.cc#1696
    > wherein Tobias suggested moving the folding away of ==0 operations out
    > of the platform-specific instruction selectors and into the
    > MachineOperatorReducer. I noticed that CommonOperatorReducer already
    > handles some very similar cases, so I have tried putting the ==0 folding
    > into CommonOperatorReducer instead. I'm happy to move it into
    > MachineOperatorReducer if that's better; I still don't have a very good
    > understanding of how roles are separated among reducers.
    > 
    > Change-Id: Ia0285bd9fafeef29d87cc88654bd6d355d467e8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2076498
    > Commit-Queue: Seth Brenith <seth.brenith@microsoft.com>
    > Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    > Reviewed-by: Clemens Backes <clemensb@chromium.org>
    > Reviewed-by: Georg Neis <neis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#66688}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:1061767
    Change-Id: Id1fdfb38357eb514d92ed3be0a683f077202faa4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2117789
    Commit-Queue: Seth Brenith <seth.brenith@microsoft.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#66862}

--- src/compiler/backend/s390/instruction-selector-s390.cc
+++ src/compiler/backend/s390/instruction-selector-s390.cc
@@ -1834,6 +1834,16 @@ void VisitLoadAndTest(InstructionSelector* selector, InstructionCode opcode,
 // Shared routine for word comparisons against zero.
 void InstructionSelector::VisitWordCompareZero(Node* user, Node* value,
                                                FlagsContinuation* cont) {
+  // Try to combine with comparisons against 0 by simply inverting the branch.
+  while (value->opcode() == IrOpcode::kWord32Equal && CanCover(user, value)) {
+    Int32BinopMatcher m(value);
+    if (!m.right().Is(0)) break;
+
+    user = value;
+    value = m.left().node();
+    cont->Negate();
+  }
+
   FlagsCondition fc = cont->condition();
   if (CanCover(user, value)) {
     switch (value->opcode()) {
