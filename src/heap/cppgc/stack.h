9173d7ad17d28a759527d9cd282e684f45cfe557
Michael Lippautz <mlippautz@chromium.org>
2020-03-31 23:56:19 +0200

    Reland "cppgc: Limit stack scan to clang-only"
    Bug: v8:10370
    Change-Id: Ia20388951ce420bdcd47d3c884ce762b87eefdf1
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2130272
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#66931}

--- src/heap/cppgc/stack.h
+++ src/heap/cppgc/stack.h
@@ -8,8 +8,12 @@
 #include "src/base/macros.h"
 
 // TODO(chromium:1056170): Implement all platforms.
+// TODO(chromium:1056170): Should use HOST arch instead of target arch. Fix
+// requires fixing e.g. simulator platforms.
+#ifdef __clang__
 #if defined(V8_TARGET_ARCH_X64)
 #define CPPGC_SUPPORTS_CONSERVATIVE_STACK_SCAN 1
+#endif
 #endif
 
 namespace cppgc {
