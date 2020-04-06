2b047a58f8a70dde526d392ebfee525d3a11edcb
Nico Hartmann <nicohartmann@chromium.org>
2020-04-06 13:16:45 +0000

    Revert "cppgc: Integrate conservative stack scan into GC"
    This reverts commit 44a1ed8f7f57cdea4744fb0cd4d9c5e9c6229da6.
    
    Reason for revert: https://ci.chromium.org/p/v8/builders/ci/V8%20Linux%20gcc/6124?
    
    Original change's description:
    > cppgc: Integrate conservative stack scan into GC
    > 
    > With this change we support allocation of objects and keeping them alive via
    > conservative stack scan.
    > 
    > Change-Id: Id94d7ced503ad0b1378643e0c13c2a8b65ad2327
    > Bug: chromium:1056170
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2135729
    > Reviewed-by: Omer Katz <omerkatz@chromium.org>
    > Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    > Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#67008}
    
    TBR=ulan@chromium.org,hpayer@chromium.org,mlippautz@chromium.org,bikineev@chromium.org,omerkatz@chromium.org
    
    Change-Id: I71bf7a9b7f54f0ec8d64b1b1781699adb0feb138
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1056170
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2137401
    Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    Commit-Queue: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#67009}

--- src/heap/cppgc/heap.cc
+++ src/heap/cppgc/heap.cc
@@ -6,9 +6,7 @@
 
 #include <memory>
 
-#include "src/base/platform/platform.h"
-#include "src/heap/cppgc/heap-object-header-inl.h"
-#include "src/heap/cppgc/stack.h"
+#include "src/heap/cppgc/heap-object-header.h"
 
 namespace cppgc {
 
@@ -18,54 +16,12 @@ std::unique_ptr<Heap> Heap::Create() {
 
 namespace internal {
 
-namespace {
-
-constexpr bool NeedsConservativeStackScan(Heap::GCConfig config) {
-  return config.stack_state == Heap::GCConfig::StackState::kNonEmpty;
-}
-
-}  // namespace
-
-// TODO(chromium:1056170): Replace with fast stack scanning once
-// object are allocated actual arenas/spaces.
-class StackMarker final : public StackVisitor {
- public:
-  explicit StackMarker(const std::vector<HeapObjectHeader*>& objects)
-      : objects_(objects) {}
-
-  void VisitPointer(const void* address) final {
-    for (auto* header : objects_) {
-      if (address >= header->Payload() &&
-          address < (header + header->GetSize())) {
-        header->TryMarkAtomic();
-      }
-    }
-  }
-
- private:
-  const std::vector<HeapObjectHeader*>& objects_;
-};
-
-Heap::Heap()
-    : stack_(std::make_unique<Stack>(v8::base::Stack::GetStackStart())) {}
-
-void Heap::CollectGarbage(GCConfig config) {
-  current_config_ = config;
-  // TODO(chromium:1056170): Replace with proper mark-sweep algorithm.
-  // "Marking".
-  if (NeedsConservativeStackScan(current_config_)) {
-    StackMarker marker(objects_);
-    stack_->IteratePointers(&marker);
-  }
-  // "Sweeping and finalization".
+void Heap::CollectGarbage() {
   for (HeapObjectHeader* header : objects_) {
-    if (header->IsMarked()) {
-      header->Unmark();
-    } else {
-      header->Finalize();
-      free(header);
-    }
+    header->Finalize();
+    free(header);
   }
+  objects_.clear();
 }
 
 }  // namespace internal
--- src/heap/cppgc/heap.h
+++ src/heap/cppgc/heap.h
@@ -5,7 +5,6 @@
 #ifndef V8_HEAP_CPPGC_HEAP_H_
 #define V8_HEAP_CPPGC_HEAP_H_
 
-#include <memory>
 #include <vector>
 
 #include "include/cppgc/gc-info.h"
@@ -15,34 +14,18 @@
 namespace cppgc {
 namespace internal {
 
-class Stack;
-
 class V8_EXPORT_PRIVATE Heap final : public cppgc::Heap {
  public:
-  struct GCConfig {
-    enum class StackState : uint8_t {
-      kEmpty,
-      kNonEmpty,
-    };
-
-    static GCConfig Default() { return {StackState::kNonEmpty}; }
-
-    StackState stack_state = StackState::kNonEmpty;
-  };
-
   static Heap* From(cppgc::Heap* heap) { return static_cast<Heap*>(heap); }
 
-  Heap();
+  Heap() = default;
   ~Heap() final = default;
 
   inline void* Allocate(size_t size, GCInfoIndex index);
 
-  void CollectGarbage(GCConfig config = GCConfig::Default());
+  void CollectGarbage();
 
  private:
-  GCConfig current_config_;
-
-  std::unique_ptr<Stack> stack_;
   std::vector<HeapObjectHeader*> objects_;
 };
 
