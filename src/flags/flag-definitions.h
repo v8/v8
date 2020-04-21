fc088cdaccadede84886eee881e67af9db53669a
Francis McCabe <fgm@chromium.org>
2020-04-21 21:57:57 +0000

    Revert "[weakrefs] Ship WeakRef and FinalizationRegistry."
    This reverts commit 30c6bd45be34b0517ae0401822857e3c9a934748.
    
    Reason for revert: Breaks 
    https://ci.chromium.org/p/v8/builders/ci/V8%20Linux64%20GC%20Stress%20-%20custom%20snapshot/30797?
    and
     https://ci.chromium.org/p/v8/builders/ci/V8%20Blink%20Linux/4190
    and
    https://test-results.appspot.com/data/layout_results/V8_Blink_Linux/4190/blink_web_tests%20%28retry%20shards%20with%20patch%29/layout-test-results/results.html
    
    
    Original change's description:
    > [weakrefs] Ship WeakRef and FinalizationRegistry.
    > 
    > I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/L04PqDk9eMU
    > Bug: v8:8179
    > Change-Id: I52aaa62cdab981b802fa4a986d60421ef6efcfbb
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2158371
    > Commit-Queue: Shu-yu Guo <syg@chromium.org>
    > Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    > Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#67295}
    
    TBR=ulan@chromium.org,rmcilroy@chromium.org,syg@chromium.org
    
    Change-Id: I22b7d91fcde9761c40d4ccaaccab24629cdb4249
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: v8:8179
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2158924
    Reviewed-by: Francis McCabe <fgm@chromium.org>
    Commit-Queue: Francis McCabe <fgm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#67297}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -217,6 +217,7 @@ DEFINE_IMPLICATION(harmony_weak_refs_with_cleanup_some, harmony_weak_refs)
   V(harmony_promise_any, "harmony Promise.any")                                \
   V(harmony_string_replaceall, "harmony String.prototype.replaceAll")          \
   V(harmony_regexp_sequence, "RegExp Unicode sequence properties")             \
+  V(harmony_weak_refs, "harmony weak references")                              \
   V(harmony_weak_refs_with_cleanup_some,                                       \
     "harmony weak references with FinalizationRegistry.prototype.cleanupSome") \
   V(harmony_regexp_match_indices, "harmony regexp match indices")              \
@@ -246,15 +247,14 @@ DEFINE_IMPLICATION(harmony_weak_refs_with_cleanup_some, harmony_weak_refs)
 #endif
 
 // Features that are shipping (turned on by default, but internal flag remains).
-#define HARMONY_SHIPPING_BASE(V)                                          \
-  V(harmony_namespace_exports,                                            \
-    "harmony namespace exports (export * as foo from 'bar')")             \
-  V(harmony_sharedarraybuffer, "harmony sharedarraybuffer")               \
-  V(harmony_import_meta, "harmony import.meta property")                  \
-  V(harmony_dynamic_import, "harmony dynamic import")                     \
-  V(harmony_promise_all_settled, "harmony Promise.allSettled")            \
-  V(harmony_private_methods, "harmony private methods in class literals") \
-  V(harmony_weak_refs, "harmony weak references")
+#define HARMONY_SHIPPING_BASE(V)                                   \
+  V(harmony_namespace_exports,                                     \
+    "harmony namespace exports (export * as foo from 'bar')")      \
+  V(harmony_sharedarraybuffer, "harmony sharedarraybuffer")        \
+  V(harmony_import_meta, "harmony import.meta property")           \
+  V(harmony_dynamic_import, "harmony dynamic import")              \
+  V(harmony_promise_all_settled, "harmony Promise.allSettled")     \
+  V(harmony_private_methods, "harmony private methods in class literals")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_SHIPPING(V)                               \
