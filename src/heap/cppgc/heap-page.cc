35655a8ef8099b9b258f58344b5349aefd67e267
Milad Farazmand <miladfar@ca.ibm.com>
2020-05-01 13:45:35 +0000

    cppgc: Fix GCC compilation error
    Without the added header, GCC may produce the following error:
    
    <_CharT>, std::istreambuf_iterator<_CharT>, const _CharT2&)
         find(istreambuf_iterator<_CharT> __first,
         ^~~~
    note: template argument deduction/substitution failed
    
    Change-Id: I92b226e469d780a778262847d90cf12a372dcc46
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2176052
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Milad Farazmand <miladfar@ca.ibm.com>
    Cr-Commit-Position: refs/heads/master@{#67514}

--- src/heap/cppgc/heap-page.cc
+++ src/heap/cppgc/heap-page.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <algorithm>
+
 #include "src/heap/cppgc/heap-page.h"
 
 #include "include/cppgc/internal/api-constants.h"
