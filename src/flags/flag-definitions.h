5fa30652ef69e32e595eea9cb3d4dfe4efbf99a9
Camillo Bruni <cbruni@chromium.org>
2020-05-06 19:37:23 +0200

    [harmony] Stage --harmony-top-level-await
    The V8-side of top-level await is complete.
    Staging this feature to get fuzzing coverage.
    
    Bug: chrome:1022182, v8:9344
    Change-Id: I1b88d0450aa148b84c62659628d492ffc3074d0f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2185132
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#67697}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -223,8 +223,7 @@ DEFINE_IMPLICATION(harmony_weak_refs_with_cleanup_some, harmony_weak_refs)
   V(harmony_regexp_sequence, "RegExp Unicode sequence properties")             \
   V(harmony_weak_refs_with_cleanup_some,                                       \
     "harmony weak references with FinalizationRegistry.prototype.cleanupSome") \
-  V(harmony_regexp_match_indices, "harmony regexp match indices")              \
-  V(harmony_top_level_await, "harmony top level await")
+  V(harmony_regexp_match_indices, "harmony regexp match indices")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_INPROGRESS(V)                       \
@@ -238,7 +237,8 @@ DEFINE_IMPLICATION(harmony_weak_refs_with_cleanup_some, harmony_weak_refs)
 #define HARMONY_STAGED_BASE(V)                                        \
   V(harmony_string_replaceall, "harmony String.prototype.replaceAll") \
   V(harmony_logical_assignment, "harmony logical assignment")         \
-  V(harmony_promise_any, "harmony Promise.any")
+  V(harmony_promise_any, "harmony Promise.any")                       \
+  V(harmony_top_level_await, "harmony top level await")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_STAGED(V)                                  \
