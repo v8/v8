b5939c758924f56824ca137e3bdc47bb373443e9
Milad Farazmand <miladfar@ca.ibm.com>
2020-05-12 13:38:05 +0000

    Revert "s390: [arm] Add missing RELATIVE_CODE_TARGET iteration"
    This reverts commit 9d3cca1cd3ad7c6653cab1cdf111d356f33f77cd.
    
    Reason for revert: Only the test needs to be skipped on s390. Refer to this: https://crrev.com/c/1981505
    
    Original change's description:
    > s390: [arm] Add missing RELATIVE_CODE_TARGET iteration
    > 
    > Port b766299d2c382cc9817e73225bbebe29ce62b9d1
    > Port 9592b043eed86db91a441d4bf78b7f0c8c2ce4dd
    > Port d915b8d668615a7d6d75cf7a61d3ca5a3d139799
    > 
    > Original Commit Message:
    > 
    >     Code object iteration was missing logic for RELATIVE_CODE_TARGET
    >     reloc entries. Garbage collection could thus miss objects that were
    >     referenced only as targets of pc-relative calls or jumps.
    > 
    >     RELATIVE_CODE_TARGETs are only used on arm, mips, and s390 and only
    >     at mksnapshot-time.
    > 
    >     This exposed another issue in that the interpreter entry trampoline
    >     copy we generate for profiling *did* contain relative calls in
    >     runtime-accessible code. This is a problem, since code space on arm is,
    >     by default, too large to be fully addressable through pc-relative
    >     calls. This CL thus also disables the related
    >     FLAG_interpreted_frames_native_stack feature on arm.
    > 
    >     objects.
    > 
    > R=​jgruber@chromium.org, joransiu@ca.ibm.com, jyan@ca.ibm.com, michael_dawson@ca.ibm.com
    > BUG=
    > LOG=N
    > 
    > Change-Id: Ifbcaed98d90a2730f0d6a8a7d32c621dab1ff5b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2087693
    > Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    > Reviewed-by: Junliang Yan <jyan@ca.ibm.com>
    > Commit-Queue: Milad Farazmand <miladfar@ca.ibm.com>
    > Cr-Commit-Position: refs/heads/master@{#66644}
    
    TBR=michael_dawson@ca.ibm.com,mlippautz@chromium.org,jyan@ca.ibm.com,jgruber@chromium.org,joransiu@ca.ibm.com,miladfar@ca.ibm.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Id645a9def23d278235ff77f25249d2187e8105ca
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2196521
    Reviewed-by: Milad Farazmand <miladfar@ca.ibm.com>
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Milad Farazmand <miladfar@ca.ibm.com>
    Cr-Commit-Position: refs/heads/master@{#67751}

--- test/cctest/interpreter/test-interpreter.cc
+++ test/cctest/interpreter/test-interpreter.cc
@@ -5042,7 +5042,7 @@ TEST(InterpreterGenerators) {
   }
 }
 
-#if !defined(V8_TARGET_ARCH_ARM) && !defined(V8_TARGET_ARCH_S390X)
+#ifndef V8_TARGET_ARCH_ARM
 TEST(InterpreterWithNativeStack) {
   i::FLAG_interpreted_frames_native_stack = true;
 
@@ -5064,7 +5064,7 @@ TEST(InterpreterWithNativeStack) {
   CHECK(code.is_interpreter_trampoline_builtin());
   CHECK_NE(code.address(), interpreter_entry_trampoline->address());
 }
-#endif  // !V8_TARGET_ARCH_ARM && !V8_TARGET_ARCH_S390X
+#endif  // V8_TARGET_ARCH_ARM
 
 TEST(InterpreterGetBytecodeHandler) {
   HandleAndZoneScope handles;
