50af61718d49c4f04d0839476c78e3b305d03de4
Seth Brenith <seth.brenith@microsoft.com>
2020-05-26 11:12:11 -0700

    [torque] Improve field types in Script
    This is a partial reland of https://crrev.com/c/v8/v8/+/2199640 .
    
    Change-Id: I9e7506ade0938079166e63926e11b67615f909e7
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2216239
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Commit-Queue: Seth Brenith <seth.brenith@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#68083}

--- src/profiler/heap-snapshot-generator.cc
+++ src/profiler/heap-snapshot-generator.cc
@@ -1129,7 +1129,7 @@ void V8HeapExplorer::ExtractScriptReferences(HeapEntry* entry, Script script) {
   SetInternalReference(entry, "source", script.source(), Script::kSourceOffset);
   SetInternalReference(entry, "name", script.name(), Script::kNameOffset);
   SetInternalReference(entry, "context_data", script.context_data(),
-                       Script::kContextOffset);
+                       Script::kContextDataOffset);
   TagObject(script.line_ends(), "(script line ends)");
   SetInternalReference(entry, "line_ends", script.line_ends(),
                        Script::kLineEndsOffset);
