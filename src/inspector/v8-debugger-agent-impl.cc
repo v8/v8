03ba73e4a2952ffefc908f73fe5e341f9aebf905
Benedikt Meurer <bmeurer@chromium.org>
2020-05-26 22:09:43 +0200

    [inspector] Report length and endColumn correctly for Wasm.
    Previously both the length and the endColumn for Wasm scripts were
    reported as 0, and that was sort of okayish, since the front-end
    was ignoring both of these fields in case of Wasm, and was applying
    special cases. But these special casing lead to some subtle bugs,
    and this is the first step towards a more uniform treatment.
    
    Source positions for Wasm are in terms of the bytecode, and the
    column field contains the bytecode offset here, while the line
    number field is always 0. Hence we send 0 for both startLine and
    endLine as before, but endColumn now corresponds to the bytecode
    size.
    
    Bug: chromium:1056632
    Change-Id: Ia8a9cfe454ed250b87a524f5cbcbbbe242205db6
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2215817
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#67997}

--- src/inspector/v8-debugger-agent-impl.cc
+++ src/inspector/v8-debugger-agent-impl.cc
@@ -1581,8 +1581,6 @@ void V8DebuggerAgentImpl::didParseSource(
     return;
   }
 
-  // TODO(herhut, dgozman): Report correct length for Wasm if needed for
-  // coverage. Or do not send the length at all and change coverage instead.
   if (scriptRef->isSourceLoadedLazily()) {
     m_frontend.scriptParsed(
         scriptId, scriptURL, 0, 0, 0, 0, contextId, scriptRef->hash(),
