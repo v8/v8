76debfda322741fc37ae8794e42d0eb5dd329dcb
Ng Zhi An <zhin@chromium.org>
2020-06-02 11:49:51 -0700

    [wasm-simd][liftoff] Fix I64x2Mul
    The I64x2Mul overwrote the lhs/rhs if they are the same as dst. So when
    deciding if we need temporaries, we should not only check the
    cache_state, but whether they alias dst or not.
    
    Bug: chromium:1088273
    Change-Id: I82efa9b45e0a3d321a06efde60971ce95b21490f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2225796
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#68114}

--- test/mjsunit/wasm/wasm-module-builder.js
+++ test/mjsunit/wasm/wasm-module-builder.js
@@ -474,6 +474,7 @@ let kExprI8x16Splat = 0x0f;
 let kExprI16x8Splat = 0x10;
 let kExprI32x4Splat = 0x11;
 let kExprF32x4Splat = 0x13;
+let kExprI16x8ExtractLaneU = 0x19;
 let kExprI8x16LtU = 0x26;
 let kExprI8x16LeU = 0x2a;
 let kExprI32x4Eq = 0x37;
@@ -482,6 +483,7 @@ let kExprV8x16AllTrue = 0x63;
 let kExprI8x16Add = 0x6e;
 let kExprI16x8ShrS = [0x8c, 01];
 let kExprV32x4AnyTrue = 0xa2;
+let kExprI64x2Mul = 0xd5;
 let kExprF32x4Min = 0xe8;
 
 // Compilation hint constants.
