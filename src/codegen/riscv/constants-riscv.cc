96440a015f76e31bb3f5eac3d1aa9d5bdd762eb2
Peng Wu <peng.wu@futurewei.com>
2020-06-04 14:40:35 -0700

    Add debugger command for 'c', 'si', 'print' in the simulator
    Note: 'break', 'stop', and 'gdb' commands are not yet supported

--- src/codegen/riscv/constants-riscv.cc
+++ src/codegen/riscv/constants-riscv.cc
@@ -14,14 +14,6 @@ namespace internal {
 
 // These register names are defined in a way to match the native disassembler
 // formatting. See for example the command "objdump -d <binary file>".
-
-/*
-const char* Registers::names_[kNumSimuRegisters] = {
-    "zero_reg", "at", "v0", "v1", "a0", "a1", "a2", "a3", "a4",
-    "a5",       "a6", "a7", "t0", "t1", "t2", "t3", "s0", "s1",
-    "s2",       "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0",
-    "k1",       "gp", "sp", "fp", "ra", "LO", "HI", "pc"};
-*/
 const char* Registers::names_[kNumSimuRegisters] = {
     "zero_reg", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "fp", "s1", "a0",
     "a1",       "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
