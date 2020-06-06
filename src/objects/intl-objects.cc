ab84c50219e5889030fc8bdc586474bccb7a76ef
Ng Zhi An <zhin@chromium.org>
2020-06-05 18:20:05 -0700

    [clang-tidy] Prefer static_cast to c style casts
    See
    https://clang.llvm.org/extra/clang-tidy/checks/google-readability-casting.html
    and https://google.github.io/styleguide/cppguide.html#Casting.
    
    Change-Id: Ic40a585e034e804da9ded1563e85f6dd44506da1
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2233988
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#68300}

--- src/objects/intl-objects.cc
+++ src/objects/intl-objects.cc
@@ -1893,8 +1893,8 @@ Maybe<Intl::ResolvedLocale> Intl::ResolveLocale(
 Handle<Managed<icu::UnicodeString>> Intl::SetTextToBreakIterator(
     Isolate* isolate, Handle<String> text, icu::BreakIterator* break_iterator) {
   text = String::Flatten(isolate, text);
-  icu::UnicodeString* u_text =
-      (icu::UnicodeString*)(Intl::ToICUUnicodeString(isolate, text).clone());
+  icu::UnicodeString* u_text = static_cast<icu::UnicodeString*>(
+      Intl::ToICUUnicodeString(isolate, text).clone());
 
   Handle<Managed<icu::UnicodeString>> new_u_text =
       Managed<icu::UnicodeString>::FromRawPtr(isolate, 0, u_text);
