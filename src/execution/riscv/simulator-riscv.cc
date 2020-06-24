a8e36787def4fe01e7480a917254a2bd49791e1e
Peng Wu <peng.wu@futurewei.com>
2020-06-24 13:49:08 -0700

    Unskip passed test and comment out unsupported debugger commands

--- src/execution/riscv/simulator-riscv.cc
+++ src/execution/riscv/simulator-riscv.cc
@@ -624,32 +624,32 @@ void RiscvDebugger::Debug() {
         PrintF("disasm [[<address/register>] <instructions>]\n");
         PrintF("  disassemble code, default is 10 instructions\n");
         PrintF("  from pc (alias 'di')\n");
-        PrintF("gdb\n");
-        PrintF("  enter gdb\n");
-        PrintF("break <address>\n");
-        PrintF("  set a break point on the address\n");
-        PrintF("del\n");
-        PrintF("  delete the breakpoint\n");
-        PrintF("stop feature:\n");
-        PrintF("  Description:\n");
-        PrintF("    Stops are debug instructions inserted by\n");
-        PrintF("    the Assembler::stop() function.\n");
-        PrintF("    When hitting a stop, the Simulator will\n");
-        PrintF("    stop and give control to the Debugger.\n");
-        PrintF("    All stop codes are watched:\n");
-        PrintF("    - They can be enabled / disabled: the Simulator\n");
-        PrintF("       will / won't stop when hitting them.\n");
-        PrintF("    - The Simulator keeps track of how many times they \n");
-        PrintF("      are met. (See the info command.) Going over a\n");
-        PrintF("      disabled stop still increases its counter. \n");
-        PrintF("  Commands:\n");
-        PrintF("    stop info all/<code> : print infos about number <code>\n");
-        PrintF("      or all stop(s).\n");
-        PrintF("    stop enable/disable all/<code> : enables / disables\n");
-        PrintF("      all or number <code> stop(s)\n");
-        PrintF("    stop unstop\n");
-        PrintF("      ignore the stop instruction at the current location\n");
-        PrintF("      from now on\n");
+        // FIXME (RISCV): the following commands are not yet supported
+        // PrintF("gdb \n");
+        // PrintF("  enter gdb\n");
+        // PrintF("break <address>\n");
+        // PrintF("  set a break point on the address\n");
+        // PrintF("del\n");
+        // PrintF("  delete the breakpoint\n");
+        // PrintF("stop feature:\n");
+        // PrintF("  Description:\n");
+        // PrintF("    Stops are debug instructions inserted by\n");
+        // PrintF("    the Assembler::stop() function.\n");
+        // PrintF("    When hitting a stop, the Simulator will\n");
+        // PrintF("    stop and give control to the Debugger.\n");
+        // PrintF("    All stop codes are watched:\n");
+        // PrintF("    - They can be enabled / disabled: the Simulator\n");
+        // PrintF("       will / won't stop when hitting them.\n");
+        // PrintF("    - The Simulator keeps track of how many times they \n");
+        // PrintF("      are met. (See the info command.) Going over a\n");
+        // PrintF("      disabled stop still increases its counter. \n");
+        // PrintF("  Commands:\n");
+        // PrintF("    stop info all/<code> : print infos about number
+        // <code>\n"); PrintF("      or all stop(s).\n"); PrintF("    stop
+        // enable/disable all/<code> : enables / disables\n"); PrintF("      all
+        // or number <code> stop(s)\n"); PrintF("    stop unstop\n"); PrintF("
+        // ignore the stop instruction at the current location\n"); PrintF("
+        // from now on\n");
       } else {
         PrintF("Unknown command: %s\n", cmd);
       }
