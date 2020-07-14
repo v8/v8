064b7c90a17cc6687efc33e13f207dc884456806
Zeynep Cankara <zcankara@google.com>
2020-07-14 18:42:13 +0100

    [tools][system-analyzer] Fix the color bug in Map Panel
    This CL fixes the Map Panel colors. The getColor
    function of Edge class was always falling to the
    default color unable to show correct colors in
    the timeline panel.
    
    Change-Id: Ide13b35703a656251222f512b2b9282f9f34cc04
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2297473
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Zeynep Cankara <zcankara@google.com>
    Cr-Commit-Position: refs/heads/master@{#68859}

--- tools/system-analyzer/map-processor.mjs
+++ tools/system-analyzer/map-processor.mjs
@@ -361,6 +361,8 @@ class V8Map {
 
 V8Map.cache = new Map();
 
+
+
 // ===========================================================================
 class Edge {
   constructor(type, name, reason, time, from, to) {
@@ -370,30 +372,31 @@ class Edge {
     this.time = time;
     this.from = from;
     this.to = to;
-    this.getColor = function(type){
-      switch (type) {
-        case 'new':
-          // green
-          return '#aedc6e';
-        case 'Normalize':
-          // violet
-          return '#d26edc';
-        case 'SlowToFast':
-          // orange
-          return '#dc9b6e';
-        case 'InitialMap':
-          // yellow
-          return '#EEFF41';
-        case 'Transition':
-          // pink/violet (primary)
-          return '#9B6EDC';
-        case 'ReplaceDescriptors':
-          // red
-          return '#dc6eae';
-      }
-      // pink/violet (primary)
-      return '#9B6EDC';
-    };
+  }
+
+  getColor() {
+    switch (this.type) {
+      case 'new':
+        // green
+        return '#aedc6e';
+      case 'Normalize':
+        // violet
+        return '#d26edc';
+      case 'SlowToFast':
+        // orange
+        return '#dc9b6e';
+      case 'InitialMap':
+        // yellow
+        return '#EEFF41';
+      case 'Transition':
+        // pink/violet (primary)
+        return '#9B6EDC';
+      case 'ReplaceDescriptors':
+        // red
+        return '#dc6eae';
+    }
+    // pink/violet (primary)
+    return '#9B6EDC';
   }
 
   finishSetup() {
@@ -676,6 +679,9 @@ class Timeline {
     return this.values.forEach(fn)
   }
 }
+
+
+
 
 // ===========================================================================
 class Chunk {
