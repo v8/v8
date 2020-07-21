d1fb6b5a7cc730feb304555db52cc1ae947d1068
Jakob Gruber <jgruber@chromium.org>
2020-07-21 12:11:31 +0200

    [infra] Add and enable nci_as_highest_tier variant
    With work on NCI proceeding, it makes sense to test multiple
    pipeline configurations.
    
    The nci variant (passes --turbo-nci) now spawns dedicated NCI
    compilation jobs and inserts generated code into the code cache.
    
    The nci_as_highest_tier variant (passes --turbo-nci-as-highest-tier)
    simply replaces TF with NCI code (no extra jobs, no extra caching).
    This mode stresses NCI generated code more than the nci variant, in
    which NCI code only runs on cache hits.
    
    Bug: v8:8888
    Change-Id: I4c2a43cce5271a6c288e7aba195dcc9daed6af9d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2299361
    Commit-Queue: Jakob Gruber <jgruber@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#68964}

--- tools/testrunner/local/variants.py
+++ tools/testrunner/local/variants.py
@@ -15,6 +15,7 @@ ALL_VARIANT_FLAGS = {
   "jitless": [["--jitless"]],
   "minor_mc": [["--minor-mc"]],
   "nci": [["--turbo-nci"]],
+  "nci_as_highest_tier": [["--turbo-nci-as-highest-tier"]],
   "no_lfa": [["--no-lazy-feedback-allocation"]],
   # No optimization means disable all optimizations. OptimizeFunctionOnNextCall
   # would not force optimization too. It turns into a Nop. Please see
