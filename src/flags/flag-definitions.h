3bd6d141602095e544842619f0fa9cd75c2adda9
Marja Hölttä <marja@chromium.org>
2020-08-07 12:08:02 +0200

    [Atomics.waitAsync] Stage Atomics.waitAsync
    Bug: v8:10239
    Change-Id: I5d8e9c85f97835bcabb0c42c7dc0db0fdb3f82fe
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2342851
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#69322}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -223,8 +223,7 @@ DEFINE_IMPLICATION(harmony_weak_refs_with_cleanup_some, harmony_weak_refs)
   V(harmony_regexp_sequence, "RegExp Unicode sequence properties")             \
   V(harmony_weak_refs_with_cleanup_some,                                       \
     "harmony weak references with FinalizationRegistry.prototype.cleanupSome") \
-  V(harmony_regexp_match_indices, "harmony regexp match indices")              \
-  V(harmony_atomics_waitasync, "harmony Atomics.waitAsync")
+  V(harmony_regexp_match_indices, "harmony regexp match indices")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_INPROGRESS(V)                       \
@@ -235,8 +234,9 @@ DEFINE_IMPLICATION(harmony_weak_refs_with_cleanup_some, harmony_weak_refs)
 #endif
 
 // Features that are complete (but still behind --harmony/es-staging flag).
-#define HARMONY_STAGED_BASE(V)                                        \
-  V(harmony_top_level_await, "harmony top level await")
+#define HARMONY_STAGED_BASE(V)                          \
+  V(harmony_top_level_await, "harmony top level await") \
+  V(harmony_atomics_waitasync, "harmony Atomics.waitAsync")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_STAGED(V)                                  \
