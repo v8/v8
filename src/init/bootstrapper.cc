985a9ddaa189a28da8c329ac12410ed3d74f08a8
Shu-yu Guo <syg@chromium.org>
2020-08-18 12:00:37 -0700

    Fix "name" property of %ThrowTypeError% to be spec-conformant
    This is a normative PR that reached consensus at the June 2019 TC39:
    https://github.com/tc39/test262/pull/2299
    
    Bug: v8:9646
    Change-Id: Idbeea703fe264da43825729e7b37a08a1bb10001
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2360907
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#69466}

--- src/init/bootstrapper.cc
+++ src/init/bootstrapper.cc
@@ -642,11 +642,14 @@ Handle<JSFunction> Genesis::GetThrowTypeErrorIntrinsic() {
   Handle<JSFunction> function = factory()->NewFunction(args);
   function->shared().DontAdaptArguments();
 
-  // %ThrowTypeError% must not have a name property.
-  if (JSReceiver::DeleteProperty(function, factory()->name_string())
-          .IsNothing()) {
-    DCHECK(false);
-  }
+  // %ThrowTypeError% must have a name property with an empty string value. Per
+  // spec, ThrowTypeError's name is non-configurable, unlike ordinary functions'
+  // name property. To redefine it to be non-configurable, use
+  // SetOwnPropertyIgnoreAttributes.
+  JSObject::SetOwnPropertyIgnoreAttributes(
+      function, factory()->name_string(), factory()->empty_string(),
+      static_cast<PropertyAttributes>(DONT_ENUM | DONT_DELETE | READ_ONLY))
+      .Assert();
 
   // length needs to be non configurable.
   Handle<Object> value(Smi::FromInt(function->length()), isolate());
