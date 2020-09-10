ed50458e6f81136c22a2394e273ce6a50254074b
Shu-yu Guo <syg@chromium.org>
2020-09-10 18:30:09 +0000

    Revert "Reland "[d8] Add d8 global variable""
    This reverts commit 120eb38883da6bfa50100580ae9c88cad685b730.
    
    Reason for revert: Maybe perturbed GC and caused optimization test to fail: https://ci.chromium.org/p/v8/builders/ci/V8%20Linux64%20GC%20Stress%20-%20custom%20snapshot/33042?
    
    Original change's description:
    > Reland "[d8] Add d8 global variable"
    > 
    > This is a reland of 6798619a69dfe3e244f85b83c4a327d94d426b32
    > 
    > Original change's description:
    > > [d8] Add d8 global variable
    > >
    > > - Add a a "d8" global variable where d8 can provide helpers.
    > >   This in in preparation of adding d8.log for testing our log parsers
    > >   written in JavaScript.
    > >
    > > - Separate d8 helper creation into individual functions.
    > >
    > > Bug: v8:1064
    > > Change-Id: I84e434452463afb93ae403f890d8841b20b00703
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2400990
    > > Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    > > Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#69801}
    > 
    > TBR=verwaest@chromium.org
    > 
    > Bug: v8:1064
    > Change-Id: I656d550b5ec87dc52dbe6cbbdddf7151ce25031f
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2403247
    > Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    > Reviewed-by: Mythri Alle <mythria@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#69819}
    
    TBR=cbruni@chromium.org,mythria@chromium.org,verwaest@chromium.org
    
    Change-Id: If6df69e30b097c78eb0ff2676f5e5c219edfae5b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: v8:1064
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2404323
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#69823}

--- src/d8/d8.cc
+++ src/d8/d8.cc
@@ -2071,11 +2071,6 @@ Local<String> Shell::Stringify(Isolate* isolate, Local<Value> value) {
 
 Local<ObjectTemplate> Shell::CreateGlobalTemplate(Isolate* isolate) {
   Local<ObjectTemplate> global_template = ObjectTemplate::New(isolate);
-  global_template->Set(Symbol::GetToStringTag(isolate),
-                       String::NewFromUtf8Literal(isolate, "global"));
-  global_template->Set(isolate, "version",
-                       FunctionTemplate::New(isolate, Version));
-
   global_template->Set(isolate, "print", FunctionTemplate::New(isolate, Print));
   global_template->Set(isolate, "printErr",
                        FunctionTemplate::New(isolate, PrintErr));
@@ -2094,79 +2089,8 @@ Local<ObjectTemplate> Shell::CreateGlobalTemplate(Isolate* isolate) {
   if (!options.omit_quit) {
     global_template->Set(isolate, "quit", FunctionTemplate::New(isolate, Quit));
   }
-  global_template->Set(isolate, "testRunner",
-                       Shell::CreateTestRunnerTemplate(isolate));
-  global_template->Set(isolate, "Realm", Shell::CreateRealmTemplate(isolate));
-  global_template->Set(isolate, "performance",
-                       Shell::CreatePerformanceTemplate(isolate));
-  global_template->Set(isolate, "Worker", Shell::CreateWorkerTemplate(isolate));
-  global_template->Set(isolate, "os", Shell::CreateOSTemplate(isolate));
-  global_template->Set(isolate, "d8", Shell::CreateD8Template(isolate));
-
-#ifdef V8_FUZZILLI
-  global_template->Set(
-      String::NewFromUtf8(isolate, "fuzzilli", NewStringType::kNormal)
-          .ToLocalChecked(),
-      FunctionTemplate::New(isolate, Fuzzilli), PropertyAttribute::DontEnum);
-#endif  // V8_FUZZILLI
-
-  if (i::FLAG_expose_async_hooks) {
-    global_template->Set(isolate, "async_hooks",
-                         Shell::CreateAsyncHookTemplate(isolate));
-  }
-
-  return global_template;
-}
-
-Local<ObjectTemplate> Shell::CreateOSTemplate(Isolate* isolate) {
-  Local<ObjectTemplate> os_template = ObjectTemplate::New(isolate);
-  AddOSMethods(isolate, os_template);
-  return os_template;
-}
-
-Local<FunctionTemplate> Shell::CreateWorkerTemplate(Isolate* isolate) {
-  Local<FunctionTemplate> worker_fun_template =
-      FunctionTemplate::New(isolate, WorkerNew);
-  Local<Signature> worker_signature =
-      Signature::New(isolate, worker_fun_template);
-  worker_fun_template->SetClassName(
-      String::NewFromUtf8Literal(isolate, "Worker"));
-  worker_fun_template->ReadOnlyPrototype();
-  worker_fun_template->PrototypeTemplate()->Set(
-      isolate, "terminate",
-      FunctionTemplate::New(isolate, WorkerTerminate, Local<Value>(),
-                            worker_signature));
-  worker_fun_template->PrototypeTemplate()->Set(
-      isolate, "terminateAndWait",
-      FunctionTemplate::New(isolate, WorkerTerminateAndWait, Local<Value>(),
-                            worker_signature));
-  worker_fun_template->PrototypeTemplate()->Set(
-      isolate, "postMessage",
-      FunctionTemplate::New(isolate, WorkerPostMessage, Local<Value>(),
-                            worker_signature));
-  worker_fun_template->PrototypeTemplate()->Set(
-      isolate, "getMessage",
-      FunctionTemplate::New(isolate, WorkerGetMessage, Local<Value>(),
-                            worker_signature));
-  worker_fun_template->InstanceTemplate()->SetInternalFieldCount(1);
-  return worker_fun_template;
-}
-
-Local<ObjectTemplate> Shell::CreateAsyncHookTemplate(Isolate* isolate) {
-  Local<ObjectTemplate> async_hooks_templ = ObjectTemplate::New(isolate);
-  async_hooks_templ->Set(isolate, "createHook",
-                         FunctionTemplate::New(isolate, AsyncHooksCreateHook));
-  async_hooks_templ->Set(
-      isolate, "executionAsyncId",
-      FunctionTemplate::New(isolate, AsyncHooksExecutionAsyncId));
-  async_hooks_templ->Set(
-      isolate, "triggerAsyncId",
-      FunctionTemplate::New(isolate, AsyncHooksTriggerAsyncId));
-  return async_hooks_templ;
-}
-
-Local<ObjectTemplate> Shell::CreateTestRunnerTemplate(Isolate* isolate) {
   Local<ObjectTemplate> test_template = ObjectTemplate::New(isolate);
+  global_template->Set(isolate, "testRunner", test_template);
   test_template->Set(isolate, "notifyDone",
                      FunctionTemplate::New(isolate, NotifyDone));
   test_template->Set(isolate, "waitUntilDone",
@@ -2175,20 +2099,13 @@ Local<ObjectTemplate> Shell::CreateTestRunnerTemplate(Isolate* isolate) {
   // installed on the global object can be hidden with the --omit-quit flag
   // (e.g. on asan bots).
   test_template->Set(isolate, "quit", FunctionTemplate::New(isolate, Quit));
-  return test_template;
-}
 
-Local<ObjectTemplate> Shell::CreatePerformanceTemplate(Isolate* isolate) {
-  Local<ObjectTemplate> performance_template = ObjectTemplate::New(isolate);
-  performance_template->Set(isolate, "now",
-                            FunctionTemplate::New(isolate, PerformanceNow));
-  performance_template->Set(
-      isolate, "measureMemory",
-      FunctionTemplate::New(isolate, PerformanceMeasureMemory));
-  return performance_template;
-}
+  global_template->Set(isolate, "version",
+                       FunctionTemplate::New(isolate, Version));
+  global_template->Set(Symbol::GetToStringTag(isolate),
+                       String::NewFromUtf8Literal(isolate, "global"));
 
-Local<ObjectTemplate> Shell::CreateRealmTemplate(Isolate* isolate) {
+  // Bind the Realm object.
   Local<ObjectTemplate> realm_template = ObjectTemplate::New(isolate);
   realm_template->Set(isolate, "current",
                       FunctionTemplate::New(isolate, RealmCurrent));
@@ -2213,12 +2130,68 @@ Local<ObjectTemplate> Shell::CreateRealmTemplate(Isolate* isolate) {
                       FunctionTemplate::New(isolate, RealmEval));
   realm_template->SetAccessor(String::NewFromUtf8Literal(isolate, "shared"),
                               RealmSharedGet, RealmSharedSet);
-  return realm_template;
-}
+  global_template->Set(isolate, "Realm", realm_template);
 
-Local<ObjectTemplate> Shell::CreateD8Template(Isolate* isolate) {
-  Local<ObjectTemplate> d8_template = ObjectTemplate::New(isolate);
-  return d8_template;
+  Local<ObjectTemplate> performance_template = ObjectTemplate::New(isolate);
+  performance_template->Set(isolate, "now",
+                            FunctionTemplate::New(isolate, PerformanceNow));
+  performance_template->Set(
+      isolate, "measureMemory",
+      FunctionTemplate::New(isolate, PerformanceMeasureMemory));
+  global_template->Set(isolate, "performance", performance_template);
+
+  Local<FunctionTemplate> worker_fun_template =
+      FunctionTemplate::New(isolate, WorkerNew);
+  Local<Signature> worker_signature =
+      Signature::New(isolate, worker_fun_template);
+  worker_fun_template->SetClassName(
+      String::NewFromUtf8Literal(isolate, "Worker"));
+  worker_fun_template->ReadOnlyPrototype();
+  worker_fun_template->PrototypeTemplate()->Set(
+      isolate, "terminate",
+      FunctionTemplate::New(isolate, WorkerTerminate, Local<Value>(),
+                            worker_signature));
+  worker_fun_template->PrototypeTemplate()->Set(
+      isolate, "terminateAndWait",
+      FunctionTemplate::New(isolate, WorkerTerminateAndWait, Local<Value>(),
+                            worker_signature));
+  worker_fun_template->PrototypeTemplate()->Set(
+      isolate, "postMessage",
+      FunctionTemplate::New(isolate, WorkerPostMessage, Local<Value>(),
+                            worker_signature));
+  worker_fun_template->PrototypeTemplate()->Set(
+      isolate, "getMessage",
+      FunctionTemplate::New(isolate, WorkerGetMessage, Local<Value>(),
+                            worker_signature));
+  worker_fun_template->InstanceTemplate()->SetInternalFieldCount(1);
+  global_template->Set(isolate, "Worker", worker_fun_template);
+
+  Local<ObjectTemplate> os_templ = ObjectTemplate::New(isolate);
+  AddOSMethods(isolate, os_templ);
+  global_template->Set(isolate, "os", os_templ);
+
+#ifdef V8_FUZZILLI
+  global_template->Set(
+      String::NewFromUtf8(isolate, "fuzzilli", NewStringType::kNormal)
+          .ToLocalChecked(),
+      FunctionTemplate::New(isolate, Fuzzilli), PropertyAttribute::DontEnum);
+#endif  // V8_FUZZILLI
+
+  if (i::FLAG_expose_async_hooks) {
+    Local<ObjectTemplate> async_hooks_templ = ObjectTemplate::New(isolate);
+    async_hooks_templ->Set(
+        isolate, "createHook",
+        FunctionTemplate::New(isolate, AsyncHooksCreateHook));
+    async_hooks_templ->Set(
+        isolate, "executionAsyncId",
+        FunctionTemplate::New(isolate, AsyncHooksExecutionAsyncId));
+    async_hooks_templ->Set(
+        isolate, "triggerAsyncId",
+        FunctionTemplate::New(isolate, AsyncHooksTriggerAsyncId));
+    global_template->Set(isolate, "async_hooks", async_hooks_templ);
+  }
+
+  return global_template;
 }
 
 static void PrintMessageCallback(Local<Message> message, Local<Value> error) {
