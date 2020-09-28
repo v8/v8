a3eeeb2a99ee6a7adc57dcf8d64f9f1afdae12ff
Jakob Kummerow <jkummerow@chromium.org>
2020-09-28 19:16:17 +0200

    [cleanup] Drop empty header file
    Bug: v8:10933
    Change-Id: I33aad48c65292fa4e886c4485518735bc5926fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2435647
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Jakob Gruber <jgruber@chromium.org>
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#70188}

--- src/builtins/builtins-object-gen.cc
+++ src/builtins/builtins-object-gen.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/builtins/builtins-object-gen.h"
-
 #include "src/builtins/builtins-utils-gen.h"
 #include "src/builtins/builtins.h"
 #include "src/codegen/code-stub-assembler.h"
