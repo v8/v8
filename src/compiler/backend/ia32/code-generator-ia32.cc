974b749d0f27c4ad80b1d795914041b5abf2512e
Bill Budge <bbudge@chromium.org>
2020-10-26 13:40:21 -0700

    Merged: [wasm][simd][ia32] Fix unaligned loads for F32x4Abs, F32x4Neg, etc.
    - Fix unaligned accesses by inserting a move.
    - These ops seem to have the issue:
    
    https://source.chromium.org/chromium/chromium/src/+/master:v8/src/compiler/backend/ia32/instruction-selector-ia32.cc;drc=fbc697b50b13df0c4078ad9c1fc6991f1f670e2e;l=2150
    
    Bug: chromium:1142528
    (cherry picked from commit 421ea59ca89365ba97f1e05f7a29606e26e0a257)
    
    Tbr: bbudge@chromium.org
    Change-Id: Ifa050d9a6be4fe0b42f425a4488f25d228f8e831
    No-Try: true
    No-Presubmit: true
    No-Tree-Checks: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2513864
    Reviewed-by: Zhi An Ng <zhin@chromium.org>
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/8.7@{#25}
    Cr-Branched-From: 0d81cd72688512abcbe1601015baee390c484a6a-refs/heads/8.7.220@{#1}
    Cr-Branched-From: 942c2ef85caef00fcf02517d049f05e9a3d4b440-refs/heads/master@{#70196}

--- src/compiler/backend/ia32/code-generator-ia32.cc
+++ src/compiler/backend/ia32/code-generator-ia32.cc
@@ -2214,9 +2214,11 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
         __ psrld(kScratchDoubleReg, 1);
         __ andps(dst, kScratchDoubleReg);
       } else {
+        // TODO(zhin) Improve codegen for this case.
         __ pcmpeqd(dst, dst);
+        __ movups(kScratchDoubleReg, src);
         __ psrld(dst, 1);
-        __ andps(dst, src);
+        __ andps(dst, kScratchDoubleReg);
       }
       break;
     }
@@ -2236,9 +2238,11 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
         __ pslld(kScratchDoubleReg, 31);
         __ xorps(dst, kScratchDoubleReg);
       } else {
+        // TODO(zhin) Improve codegen for this case.
         __ pcmpeqd(dst, dst);
+        __ movups(kScratchDoubleReg, src);
         __ pslld(dst, 31);
-        __ xorps(dst, src);
+        __ xorps(dst, kScratchDoubleReg);
       }
       break;
     }
@@ -2251,7 +2255,9 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
       break;
     }
     case kSSEF32x4Sqrt: {
-      __ sqrtps(i.OutputSimd128Register(), i.InputOperand(0));
+      // TODO(zhin) Improve codegen for this case.
+      __ movups(kScratchDoubleReg, i.InputOperand(0));
+      __ sqrtps(i.OutputSimd128Register(), kScratchDoubleReg);
       break;
     }
     case kAVXF32x4Sqrt: {
@@ -3644,8 +3650,10 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
         __ pcmpeqd(kScratchDoubleReg, kScratchDoubleReg);
         __ pxor(dst, kScratchDoubleReg);
       } else {
+        // TODO(zhin) Improve codegen for this case.
         __ pcmpeqd(dst, dst);
-        __ pxor(dst, src);
+        __ movups(kScratchDoubleReg, src);
+        __ pxor(dst, kScratchDoubleReg);
       }
       break;
     }
