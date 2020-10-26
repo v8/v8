813a95d94de7d31912a249e091a8ee426941c772
Ross McIlroy <rmcilroy@chromium.org>
2020-10-26 14:17:39 +0000

    [Test] Prevent --deopt-every-n-times for test.
    BUG=v8:11056
    
    Change-Id: I044a9f5dc0a37df6076eb1f3f36e3d71ec7b7915
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2498692
    Auto-Submit: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#70759}

--- test/mjsunit/compiler/test-dynamic-map-check-deprecated-maps2.js
+++ test/mjsunit/compiler/test-dynamic-map-check-deprecated-maps2.js
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --allow-natives-syntax --turboprop --turboprop-dynamic-map-checks --opt
-// Flags: --no-always-opt
+// Flags: --allow-natives-syntax --turboprop --turboprop-dynamic-map-checks
+// Flags: --opt --no-always-opt --deopt-every-n-times=0
 
 function b(a) { return a; }
 
