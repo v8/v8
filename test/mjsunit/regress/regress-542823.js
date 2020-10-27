2c555da9e5d9e7340e05a579847d07958674abc4
Leszek Swirski <leszeks@chromium.org>
2020-10-27 13:27:02 +0100

    [test] Fix mjsunit/regress/regress-542823 on PPC
    PPC has a larger page size than other platforms, so increase the page
    size in the test to account for this.
    
    Change-Id: I392064e9ef3f87c5bddb7763b35661aee5b4669d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2502330
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Milad Fa <mfarazma@redhat.com>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#70857}

--- test/mjsunit/regress/regress-542823.js
+++ test/mjsunit/regress/regress-542823.js
@@ -5,12 +5,13 @@
 // Flags: --allow-natives-syntax
 
 (function() {
-  const page_size_bits = 18;
-  const max_heap_object_size = (1 << (page_size_bits - 1));
+  // kPageSizeBits is 19 on PPC.
+  const kPageSizeBits = 19;
+  const kMaxHeapObjectSize = (1 << (kPageSizeBits - 1));
 
   const filler = "Large amount of text per element, so that the joined array is"
       + "large enough to be allocated in the large object space"
-  const size = Math.ceil(max_heap_object_size / filler.length + 1);
+  const size = Math.ceil(kMaxHeapObjectSize / filler.length + 1);
   const arr = Array(size).fill(filler);
 
   for (let i = 0; i < 10; i++) {
