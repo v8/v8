d8c8efecc4af77382bc4ddc3b789f3f437e209f8
Zhi An Ng <zhin@chromium.org>
2020-10-30 01:20:22 +0000

    [wasm-simd] Enable lowering for select test
    With more of scalar lowering implemented, this test can now run with
    lowering. The only other tests that skip lowering are prototype
    instructions.
    
    Bug: v8:10507
    Change-Id: Ia149956d2d406b6f76eb9155765474c23b89b894
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2507317
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Ben Smith <binji@chromium.org>
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#71215}

--- test/cctest/wasm/test-run-wasm-simd.cc
+++ test/cctest/wasm/test-run-wasm-simd.cc
@@ -2814,7 +2814,7 @@ WASM_SIMD_SELECT_TEST(8x16)
 // Test Select by making a mask where the 0th and 3rd lanes are non-zero and the
 // rest 0. The mask is not the result of a comparison op.
 #define WASM_SIMD_NON_CANONICAL_SELECT_TEST(format)                           \
-  WASM_SIMD_TEST_NO_LOWERING(S##format##NonCanonicalSelect) {                 \
+  WASM_SIMD_TEST(S##format##NonCanonicalSelect) {                             \
     WasmRunner<int32_t, int32_t, int32_t, int32_t> r(execution_tier,          \
                                                      lower_simd);             \
     byte val1 = 0;                                                            \
