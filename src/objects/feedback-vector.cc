8574179a90d6e3a11dced3c1789c60b80994fbcd
Mike Stanton <mvstanton@chromium.org>
2020-11-03 13:08:55 +0100

    [feedback] Only take read lock for data pairs on the background thread
    Slight performance regression by adding locking around feedback
    vector get/set. The lock isn't necessary for reads on the main thread,
    since the main thread is the only source of change.
    
    Bug: chromium:1144777
    Change-Id: I7cc9898ad0d8e8c468ba150c0bc6bef3176fd256
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2516475
    Reviewed-by: Santiago Aboy Solanes <solanes@chromium.org>
    Commit-Queue: Michael Stanton <mvstanton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#70947}

--- src/objects/feedback-vector.cc
+++ src/objects/feedback-vector.cc
@@ -500,11 +500,16 @@ void NexusConfig::SetFeedbackPair(FeedbackVector vector,
 
 std::pair<MaybeObject, MaybeObject> NexusConfig::GetFeedbackPair(
     FeedbackVector vector, FeedbackSlot slot) const {
-  base::SharedMutexGuard<base::kShared> shared_mutex_guard(
-      isolate()->feedback_vector_access());
+  if (mode() == BackgroundThread) {
+    isolate()->feedback_vector_access()->LockShared();
+  }
   MaybeObject feedback = vector.Get(slot);
   MaybeObject feedback_extra = vector.Get(slot.WithOffset(1));
-  return std::make_pair(feedback, feedback_extra);
+  auto return_value = std::make_pair(feedback, feedback_extra);
+  if (mode() == BackgroundThread) {
+    isolate()->feedback_vector_access()->UnlockShared();
+  }
+  return return_value;
 }
 
 FeedbackNexus::FeedbackNexus(Handle<FeedbackVector> vector, FeedbackSlot slot)
