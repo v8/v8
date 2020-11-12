04cdcd5a88df5e57e1a49a656761244a71fa8f92
Santiago Aboy Solanes <solanes@chromium.org>
2020-11-12 10:30:53 +0000

    [compiler] Move InternalizedString to kNeverSerialized
    Bug: v8:7790
    Change-Id: I8311b1f4b8239d0688a4b9b9057d6082942bbc00
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2534471
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Santiago Aboy Solanes <solanes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#71159}

--- src/compiler/js-heap-broker.cc
+++ src/compiler/js-heap-broker.cc
@@ -870,12 +870,10 @@ StringData::StringData(JSHeapBroker* broker, ObjectData** storage,
 class InternalizedStringData : public StringData {
  public:
   InternalizedStringData(JSHeapBroker* broker, ObjectData** storage,
-                         Handle<InternalizedString> object);
-
-  uint32_t array_index() const { return array_index_; }
-
- private:
-  uint32_t array_index_;
+                         Handle<InternalizedString> object)
+      : StringData(broker, storage, object) {
+    DCHECK(!FLAG_turbo_direct_heap_access);
+  }
 };
 
 ObjectData* StringData::GetCharAsString(JSHeapBroker* broker, uint32_t index,
@@ -897,11 +895,6 @@ ObjectData* StringData::GetCharAsString(JSHeapBroker* broker, uint32_t index,
   chars_as_strings_.push_back({index, result});
   return result;
 }
-
-InternalizedStringData::InternalizedStringData(
-    JSHeapBroker* broker, ObjectData** storage,
-    Handle<InternalizedString> object)
-    : StringData(broker, storage, object) {}
 
 namespace {
 
