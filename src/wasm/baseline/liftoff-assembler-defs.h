2795d8e4d8c6ff5d61c2e16dc594660c786efde2
Junliang Yan <jyan@ca.ibm.ca>
2020-11-26 11:01:44 -0500

    s390x: define liftoff cache registers
    Change-Id: I79fc56578878f326725272d67a9936b04b22a02f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2562119
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Junliang Yan <junyan@redhat.com>
    Cr-Commit-Position: refs/heads/master@{#71438}

--- src/wasm/baseline/liftoff-assembler-defs.h
+++ src/wasm/baseline/liftoff-assembler-defs.h
@@ -69,6 +69,14 @@ constexpr RegList kLiftoffAssemblerFpCacheRegs = CPURegister::ListOf(
     d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d16, d17,
     d18, d19, d20, d21, d22, d23, d24, d25, d26, d27, d28, d29);
 
+#elif V8_TARGET_ARCH_S390X
+
+constexpr RegList kLiftoffAssemblerGpCacheRegs =
+    Register::ListOf(r2, r3, r4, r5, r6, r7, r8);
+
+constexpr RegList kLiftoffAssemblerFpCacheRegs = DoubleRegister::ListOf(
+    d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12);
+
 #else
 
 constexpr RegList kLiftoffAssemblerGpCacheRegs = 0xff;
