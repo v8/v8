c2537ebd0ef88d3e75bc8ec95fadcfd7e4b1ae0a
Georg Neis <neis@chromium.org>
2021-01-08 09:46:03 +0100

    [compiler] Update test expectation
    Now that the underlying bug is fixed, we can expect the test to always
    pass.
    
    Also simplify the test a tiny bit and skip it on debug builds because
    it's slow.
    
    Bug: chromium:1161357
    Change-Id: I2ce5e064b4f707f4bd680f04df95d5a342bec1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2616220
    Reviewed-by: Santiago Aboy Solanes <solanes@chromium.org>
    Commit-Queue: Santiago Aboy Solanes <solanes@chromium.org>
    Auto-Submit: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#71972}

--- test/mjsunit/compiler/regress-1161357.js
+++ test/mjsunit/compiler/regress-1161357.js
@@ -3,13 +3,8 @@
 // found in the LICENSE file.
 
 for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 32767; j++) {
-    Number;
-  }
-  for (let j = 0; j < 2335; j++) {
-    Number;
-  }
-  var arr = [, ...(new Int16Array(0xffff)), 4294967296];
+  for (let j = 0; j < 32767; j++) Number;
+  for (let j = 0; j < 2335; j++) Number;
+  var arr = [, ...(new Int16Array(0xffff)), 0.5];
   arr.concat(Number, arr)
 }
-eval(``);
