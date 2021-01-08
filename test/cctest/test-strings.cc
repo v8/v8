b3d09001eb012ebf75816f5be51a2b1db47e55f2
Santiago Aboy Solanes <solanes@chromium.org>
2021-01-08 17:13:24 +0000

    [objects] Remove uncached internal external string type
    We shouldn't be creating those anymore since they are not thread-safe.
    
    Bug: v8:7790
    Change-Id: I4546d995fa32eb076c8dfe9d95301fad719c9e07
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2615347
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Santiago Aboy Solanes <solanes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#72006}

--- test/cctest/test-strings.cc
+++ test/cctest/test-strings.cc
@@ -2142,7 +2142,6 @@ TEST(InternalizeExternalStringUncachedWithCopyTwoByte) {
   CHECK(external->is_uncached());
 
   // Internalize succesfully, with a copy.
-  CHECK(!external->IsInternalizedString());
   Handle<String> internal = factory->InternalizeString(external);
   CHECK(!external->IsInternalizedString());
   CHECK(internal->IsInternalizedString());
