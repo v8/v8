388a99e8c024b0d06286cd14659e7fb8640db017
Liviu Rau <liviurau@chromium.org>
2021-01-19 17:16:01 +0100

    [tools] Use CAS in 'mb.py run -s'
    Bug: chromium:1166989
    Change-Id: I6eca7745ee4457d86ecda65a3972011899bac9c2
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2637219
    Auto-Submit: Liviu Rau <liviurau@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#72168}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -359,6 +359,9 @@ class MetaBuildWrapper(object):
     # swarming parameters, if possible.
     #
     # TODO(dpranke): Also, add support for sharding and merging results.
+    # TODO(liviurau): While this seems to not be used in V8 yet, we need to add
+    # a switch for internal try-bots, since they need to use 'chrome-swarming'
+    cas_instance = 'chromium-swarm'
     dimensions = []
     for k, v in self._DefaultDimensions() + self.args.dimensions:
       dimensions += ['-d', k, v]
@@ -371,9 +374,7 @@ class MetaBuildWrapper(object):
         'archive',
         '-i',
         self.ToSrcRelPath('%s/%s.isolate' % (build_dir, target)),
-        '-s',
-        self.ToSrcRelPath('%s/%s.isolated' % (build_dir, target)),
-        '-I', 'isolateserver.appspot.com',
+        '-cas-instance', cas_instance,
         '-dump-json',
         archive_json_path,
       ]
@@ -388,7 +389,7 @@ class MetaBuildWrapper(object):
           'Failed to read JSON file "%s"' % archive_json_path, file=sys.stderr)
       return 1
     try:
-      isolated_hash = archive_hashes[target]
+      cas_digest = archive_hashes[target]
     except Exception:
       self.Print(
           'Cannot find hash for "%s" in "%s", file content: %s' %
@@ -400,8 +401,7 @@ class MetaBuildWrapper(object):
         self.executable,
         self.PathJoin('tools', 'swarming_client', 'swarming.py'),
           'run',
-          '-s', isolated_hash,
-          '-I', 'isolateserver.appspot.com',
+          '-digests', cas_digest,
           '-S', 'chromium-swarm.appspot.com',
       ] + dimensions
     if self.args.extra_args:
