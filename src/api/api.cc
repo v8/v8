987f0b75bb8f227e6909c0e85e696d207f45dcb3
Jan Wilken Dörrie <jdoerrie@chromium.org>
2021-01-22 14:06:57 +0100

    [C++17] Apply consistent use of noexcept
    In C++17 noexcept becomes part of the type system and thus needs to be
    consistently applied between function declarations and definitions.
    
    Change-Id: Ia34faa9d9d1f18916655fd5a1a8ec9f6b414f1e9
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2643391
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#72255}

--- src/api/api.cc
+++ src/api/api.cc
@@ -11315,12 +11315,9 @@ RegisterState::RegisterState()
     : pc(nullptr), sp(nullptr), fp(nullptr), lr(nullptr) {}
 RegisterState::~RegisterState() = default;
 
-RegisterState::RegisterState(const RegisterState& other) V8_NOEXCEPT {
-  *this = other;
-}
+RegisterState::RegisterState(const RegisterState& other) { *this = other; }
 
-RegisterState& RegisterState::operator=(const RegisterState& other)
-    V8_NOEXCEPT {
+RegisterState& RegisterState::operator=(const RegisterState& other) {
   if (&other != this) {
     pc = other.pc;
     sp = other.sp;
