f630c44d89668d48fce132b5c1bf8c5188581b11
Clemens Backes <clemensb@chromium.org>
2021-01-28 13:11:26 +0100

    [presubmit] Skip api.cc when checking for V8_NOEXCEPT
    The "noexcept" annotation is part of the type since C++17, so we cannot
    just change v8.h to add the annotation everywhere. So instead ignore
    missing V8_NOEXCEPT annotations in api.cc
    
    R=marja@chromium.org
    CC=jdoerrie@chromium.org
    
    No-Try: true
    Bug: v8:8616
    Change-Id: I8f68a6d20653ee8c0e3d995cf585b36cbc15c5f0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2655446
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#72397}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -480,8 +480,10 @@ def _CheckNoexceptAnnotations(input_api, output_api):
   def FilterFile(affected_file):
     return input_api.FilterSourceFile(
         affected_file,
-        files_to_check=(r'src/.*', r'test/.*'))
-
+        files_to_check=(r'src[\\\/].*', r'test[\\\/].*'),
+        # Skip api.cc since we cannot easily add the 'noexcept' annotation to
+        # public methods.
+        files_to_skip=(r'src[\\\/]api[\\\/]api\.cc',))
 
   # matches any class name.
   class_name = r'\b([A-Z][A-Za-z0-9_:]*)(?:::\1)?'
