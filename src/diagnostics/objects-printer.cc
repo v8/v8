6a1f77e02dd2a4c8f247334d7ad9c6dda31028a2
Shu-yu Guo <syg@chromium.org>
2021-02-03 09:17:54 -0800

    Merged: [top-level-await] Implement spec fix for cycle root detection
    There is a bug in the top-level await spec draft such that async
    strongly connected components are not always evaluated before their
    depending modules.
    
    See https://github.com/tc39/proposal-top-level-await/pull/161 for full
    discussion and spec fix.
    
    (cherry picked from commit 9689b17687b21c800c3f7400df4255c55b9c6ec0)
    
    Bug: v8:11376
    No-Try: true
    No-Presubmit: true
    No-Tree-Checks: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2667772
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#72508}
    Change-Id: I99b91f3e7cfa7bd76d39388bcffab4dd73bcce25
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2688060
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/branch-heads/8.9@{#37}
    Cr-Branched-From: 16b9bbbd581c25391981aa03180b76aa60463a3e-refs/heads/8.9.255@{#1}
    Cr-Branched-From: d16a2a688498bd1c3e6a49edb25d8c4ca56232dc-refs/heads/master@{#72039}

--- src/diagnostics/objects-printer.cc
+++ src/diagnostics/objects-printer.cc
@@ -1666,6 +1666,7 @@ void SourceTextModule::SourceTextModulePrint(std::ostream& os) {  // NOLINT
   os << "\n - origin: " << Brief(script.GetNameOrSourceURL());
   os << "\n - requested_modules: " << Brief(requested_modules());
   os << "\n - import_meta: " << Brief(import_meta());
+  os << "\n - cycle_root: " << Brief(cycle_root());
   os << "\n";
 }
 
