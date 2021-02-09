91c135c6d6bc364981c087fcd3318d3f49270deb
Milad Fa <mfarazma@redhat.com>
2021-02-09 19:26:43 +0000

    Revert "PPC/s390: [wasm-simd][liftoff] Implement i8x16.popcnt"
    This reverts commit 21b3181adfc8b083009e9b1158d1da3b3363f729.
    
    Reason for revert: Original CL is reverted.
    
    Original change's description:
    > PPC/s390: [wasm-simd][liftoff] Implement i8x16.popcnt
    >
    > Port 00babf0718492f0ed18744fa67f132af0d0ec584
    >
    > Original Commit Message:
    >
    >     Extract i8x16.popcnt implementation into a macro-assembler function, and
    >     reuse it in Liftoff.
    >
    > R=​zhin@chromium.org, joransiu@ca.ibm.com, junyan@redhat.com, midawson@redhat.com
    > BUG=
    > LOG=N
    >
    > Change-Id: Id0f14597a97f90424aa450b2527ea71da1b2e8ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2679273
    > Reviewed-by: Junliang Yan <junyan@redhat.com>
    > Commit-Queue: Junliang Yan <junyan@redhat.com>
    > Cr-Commit-Position: refs/heads/master@{#72601}
    
    TBR=joransiu@ca.ibm.com,zhin@chromium.org,mfarazma@redhat.com,midawson@redhat.com,junyan@redhat.com
    
    Change-Id: I54dd854e305d0fbbd475c0f756bc41acac74fa56
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2685060
    Reviewed-by: Milad Fa <mfarazma@redhat.com>
    Commit-Queue: Milad Fa <mfarazma@redhat.com>
    Cr-Commit-Position: refs/heads/master@{#72604}

--- src/wasm/baseline/s390/liftoff-assembler-s390.h
+++ src/wasm/baseline/s390/liftoff-assembler-s390.h
@@ -1791,11 +1791,6 @@ void LiftoffAssembler::emit_i8x16_shuffle(LiftoffRegister dst,
   bailout(kSimd, "i8x16_shuffle");
 }
 
-void LiftoffAssembler::emit_i8x16_popcnt(LiftoffRegister dst,
-                                         LiftoffRegister src) {
-  bailout(kSimd, "i8x16.popcnt");
-}
-
 void LiftoffAssembler::emit_i8x16_splat(LiftoffRegister dst,
                                         LiftoffRegister src) {
   bailout(kUnsupportedArchitecture, "emit_i8x16splat");
