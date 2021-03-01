5defb206ff77d1f4ef8e4056948828cdf72593a7
Omer Katz <omerkatz@chromium.org>
2021-03-01 11:24:21 +0100

    cppgc: Object sizes for cpp objects in heap snapshot
    This CL sets it so that the actual object size is reported only when
    cppgc_enable_object_names is set to true. Otherwise the heap snapshot
    merges nodes and the sizes don't make sense.
    
    Also, this will resolve a web test failure for the library.
    
    Bug: chromium:1056170
    Change-Id: I32f6552560bdfad4b64c67b21292543978042a81
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2726499
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#73092}

--- src/heap/cppgc-js/cpp-snapshot.cc
+++ src/heap/cppgc-js/cpp-snapshot.cc
@@ -35,11 +35,19 @@ using cppgc::internal::HeapObjectHeader;
 class EmbedderNode : public v8::EmbedderGraph::Node {
  public:
   explicit EmbedderNode(const char* name, size_t size)
-      : name_(name), size_(size) {}
+      : name_(name), size_(size) {
+    USE(size_);
+  }
   ~EmbedderNode() override = default;
 
   const char* Name() final { return name_; }
-  size_t SizeInBytes() final { return size_; }
+  size_t SizeInBytes() final {
+#if CPPGC_SUPPORTS_OBJECT_NAMES
+    return size_;
+#else   // !CPPGC_SUPPORTS_OBJECT_NAMES
+    return 0;
+#endif  // !CPPGC_SUPPORTS_OBJECT_NAMES
+  }
 
   void SetWrapperNode(v8::EmbedderGraph::Node* wrapper_node) {
     wrapper_node_ = wrapper_node;
