e292db5dfc134d7c1ebcb25750c67afb04645e32
Junliang Yan <junyan@redhat.com>
2021-03-18 16:34:33 -0400

    Reland "Reland "s390x: [liftoff] implement CallRuntimeStub""
    This is a reland of 0655aa0573da1e1e6911dc4b49d48becc542ed5e
    
    Original change's description:
    > Reland "s390x: [liftoff] implement CallRuntimeStub"
    >
    > This is a reland of 88c1ae95b6f1e22b32f23333c0f68fc82e4a2901
    >
    > Original change's description:
    > > s390x: [liftoff] implement CallRuntimeStub
    > >
    > > Change-Id: I505ea67af31006f3f233eef390f234cfc0188d6c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2774479
    > > Reviewed-by: Milad Fa <mfarazma@redhat.com>
    > > Commit-Queue: Junliang Yan <junyan@redhat.com>
    > > Cr-Commit-Position: refs/heads/master@{#73525}
    >
    > Change-Id: Id5d0ece78533439870fdc6b000026fe04e576448
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2774561
    > Reviewed-by: Milad Fa <mfarazma@redhat.com>
    > Commit-Queue: Junliang Yan <junyan@redhat.com>
    > Cr-Commit-Position: refs/heads/master@{#73549}
    
    Change-Id: I287a833a3c12869377b83bd66a1bf096f514e034
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2787194
    Reviewed-by: Milad Fa <mfarazma@redhat.com>
    Commit-Queue: Junliang Yan <junyan@redhat.com>
    Cr-Commit-Position: refs/heads/master@{#73689}

--- src/wasm/baseline/s390/liftoff-assembler-s390.h
+++ src/wasm/baseline/s390/liftoff-assembler-s390.h
@@ -2850,7 +2850,7 @@ void LiftoffAssembler::TailCallIndirect(Register target) {
 }
 
 void LiftoffAssembler::CallRuntimeStub(WasmCode::RuntimeStubId sid) {
-  bailout(kUnsupportedArchitecture, "CallRuntimeStub");
+  Call(static_cast<Address>(sid), RelocInfo::WASM_STUB_CALL);
 }
 
 void LiftoffAssembler::AllocateStackSlot(Register addr, uint32_t size) {
