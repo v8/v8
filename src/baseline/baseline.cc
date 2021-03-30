92cbe0e33d85e09d37c5f6f28f00e66e97b9ce1c
Milad Fa <mfarazma@redhat.com>
2021-03-30 08:33:02 -0400

    [sparkplug] fix compilation error on unsupported architectures
    Handler was changed on supported architectures in this CL:
    https://crrev.com/c/2786846
    
    Change-Id: Ib7a63f12eccb956c4d7b84297f2f1514f053431a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2787196
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Milad Fa <mfarazma@redhat.com>
    Cr-Commit-Position: refs/heads/master@{#73724}

--- src/baseline/baseline.cc
+++ src/baseline/baseline.cc
@@ -4,6 +4,8 @@
 
 #include "src/baseline/baseline.h"
 
+#include "src/handles/maybe-handles.h"
+
 // TODO(v8:11421): Remove #if once baseline compiler is ported to other
 // architectures.
 #if V8_TARGET_ARCH_IA32 || V8_TARGET_ARCH_X64 || V8_TARGET_ARCH_ARM64 || \
@@ -46,8 +48,8 @@ void EmitReturnBaseline(MacroAssembler* masm) {
 namespace v8 {
 namespace internal {
 
-Handle<Code> GenerateBaselineCode(Isolate* isolate,
-                                  Handle<SharedFunctionInfo> shared) {
+MaybeHandle<Code> GenerateBaselineCode(Isolate* isolate,
+                                       Handle<SharedFunctionInfo> shared) {
   UNREACHABLE();
 }
 
