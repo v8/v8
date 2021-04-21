b30fa8a77e4716c8278e8d4e38359da1548e8b00
Lu Yahan <yahan@iscas.ac.cn>
2021-04-21 16:38:32 +0800

    [riscv64][codegen] Add static interface descriptors
       Port 2871e05cc323c5fcaa0bf5a817d9c39a53d40239
    
    Bug: v8:11420
    Change-Id: Iaf7a4bc64cecdfc11decefd19c7e741a90003c6d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2834632
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Brice Dobry <brice.dobry@futurewei.com>
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Yahan Lu <yahan@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/master@{#74113}

--- src/codegen/interface-descriptors-inl.h
+++ src/codegen/interface-descriptors-inl.h
@@ -26,6 +26,8 @@
 #include "src/codegen/mips64/interface-descriptors-mips64-inl.h"
 #elif V8_TARGET_ARCH_MIPS
 #include "src/codegen/mips/interface-descriptors-mips-inl.h"
+#elif V8_TARGET_ARCH_RISCV64
+#include "src/codegen/riscv64/interface-descriptors-riscv64-inl.h"
 #else
 #error Unsupported target architecture.
 #endif
@@ -270,7 +272,7 @@ constexpr auto BaselineOutOfLinePrologueDescriptor::registers() {
   // TODO(v8:11421): Implement on other platforms.
 #if V8_TARGET_ARCH_X64 || V8_TARGET_ARCH_ARM64 || V8_TARGET_ARCH_IA32 || \
     V8_TARGET_ARCH_ARM || V8_TARGET_ARCH_PPC || V8_TARGET_ARCH_PPC64 ||  \
-    V8_TARGET_ARCH_S390
+    V8_TARGET_ARCH_S390 || V8_TARGET_ARCH_RISCV64
   return RegisterArray(
       kContextRegister, kJSFunctionRegister, kJavaScriptCallArgCountRegister,
       kJavaScriptCallExtraArg1Register, kJavaScriptCallNewTargetRegister,
@@ -285,7 +287,7 @@ constexpr auto BaselineLeaveFrameDescriptor::registers() {
   // TODO(v8:11421): Implement on other platforms.
 #if V8_TARGET_ARCH_IA32 || V8_TARGET_ARCH_X64 || V8_TARGET_ARCH_ARM64 || \
     V8_TARGET_ARCH_ARM || V8_TARGET_ARCH_PPC || V8_TARGET_ARCH_PPC64 ||  \
-    V8_TARGET_ARCH_S390
+    V8_TARGET_ARCH_S390 || V8_TARGET_ARCH_RISCV64
   return RegisterArray(ParamsSizeRegister(), WeightRegister());
 #else
   return DefaultRegisterArray();
