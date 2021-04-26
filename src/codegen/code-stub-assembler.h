4c074516397b89c5cfe9de9857018484f73445ef
Camillo Bruni <cbruni@chromium.org>
2021-04-26 20:04:14 +0200

    [promises] Fix slow path when context promise hooks are present
    Bug: chromium:1201936
    Change-Id: I1ee545e33587ddf4a5c7e1cbd64b53d36c75a146
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2850936
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#74267}

--- src/codegen/code-stub-assembler.h
+++ src/codegen/code-stub-assembler.h
@@ -3565,12 +3565,10 @@ class V8_EXPORT_PRIVATE CodeStubAssembler
     return IsAnyPromiseHookEnabledOrDebugIsActiveOrHasAsyncEventDelegate(
         PromiseHookFlags());
   }
-  TNode<BoolT> IsAnyPromiseHookEnabledOrHasAsyncEventDelegate(
-      TNode<Uint32T> flags);
-  TNode<BoolT>
-  IsAnyPromiseHookEnabledOrHasAsyncEventDelegate() {
-    return IsAnyPromiseHookEnabledOrHasAsyncEventDelegate(
-        PromiseHookFlags());
+
+  TNode<BoolT> NeedsAnyPromiseHooks(TNode<Uint32T> flags);
+  TNode<BoolT> NeedsAnyPromiseHooks() {
+    return NeedsAnyPromiseHooks(PromiseHookFlags());
   }
 
   // for..in helpers
