00845abb322414ad76f6cdd943cc39cc8c96ae86
Toon Verwaest <verwaest@chromium.org>
2021-04-29 09:45:17 +0200

    [debug] Include Token::CLASS in class scopes and ContainsPosition
    While evaluating a class literal the containing function points to
    Token::CLASS. It may have pushed a context for that class that uses
    the range of the class scope. So far the class scope had a range that
    started after the class name or class token in case of anonymous
    classes. That means the source position of the function frame doesn't
    point to a position that is included in the active context range. This
    breaks the debugger because it relies on being able to find the
    matching parser scope for the active context by looking at the source
    position.
    
    The fix is two-fold:
    - extend the class scope source range to include Token::CLASS
    - update ScopeChainRetriever::ContainsPosition to include the start
      position of class scopes as a valid source position. We can't always
      include start due to arrow functions that don't have braces.
    
    Bug: chromium:1156498
    Change-Id: I9ec640c6326289dadcb154bb0a329ca6f8188f8b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2857957
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Toon Verwaest <verwaest@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#74268}

--- src/parsing/parser-base.h
+++ src/parsing/parser-base.h
@@ -4629,7 +4629,7 @@ typename ParserBase<Impl>::ExpressionT ParserBase<Impl>::ParseClassLiteral(
   ClassInfo class_info(this);
   class_info.is_anonymous = is_anonymous;
 
-  scope()->set_start_position(end_position());
+  scope()->set_start_position(class_token_pos);
   if (Check(Token::EXTENDS)) {
     ClassScope::HeritageParsingScope heritage(class_scope);
     FuncNameInferrerState fni_state(&fni_);
