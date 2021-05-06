f753bc821487122552a6d46697090301c98102f7
Milad Fa <mfarazma@redhat.com>
2021-05-06 11:07:30 -0400

    Merged: PPC: Add Power10 to the supported list and enable related features
    Revision: 530080c44af254646a3cc3f364aac26b1b5ac10c
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=junyan@redhat.com
    
    Bug: v8:11742
    Change-Id: Icaf857ea2481a6f2520f522f57ed76f51a4c6c70
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2876988
    Reviewed-by: Lutz Vahl <vahl@chromium.org>
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Commit-Queue: Lutz Vahl <vahl@chromium.org>
    Cr-Commit-Position: refs/branch-heads/9.1@{#49}
    Cr-Branched-From: 0e4ac64a8cf298b14034a22f9fe7b085d2cb238d-refs/heads/9.1.269@{#1}
    Cr-Branched-From: f565e72d5ba88daae35a59d0f978643e2343e912-refs/heads/master@{#73847}

--- src/base/cpu.cc
+++ src/base/cpu.cc
@@ -31,6 +31,9 @@
 #ifndef POWER_9
 #define POWER_9 0x20000
 #endif
+#ifndef POWER_10
+#define POWER_10 0x40000
+#endif
 #endif
 #if V8_OS_POSIX
 #include <unistd.h>  // sysconf()
@@ -804,7 +807,9 @@ CPU::CPU()
 
   part_ = -1;
   if (auxv_cpu_type) {
-    if (strcmp(auxv_cpu_type, "power9") == 0) {
+    if (strcmp(auxv_cpu_type, "power10") == 0) {
+      part_ = kPPCPower10;
+    } else if (strcmp(auxv_cpu_type, "power9") == 0) {
       part_ = kPPCPower9;
     } else if (strcmp(auxv_cpu_type, "power8") == 0) {
       part_ = kPPCPower8;
@@ -825,6 +830,9 @@ CPU::CPU()
 
 #elif V8_OS_AIX
   switch (_system_configuration.implementation) {
+    case POWER_10:
+      part_ = kPPCPower10;
+      break;
     case POWER_9:
       part_ = kPPCPower9;
       break;
