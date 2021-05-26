7c30ae29c0f61179bf09727535ac47650ab14149
Lu Yahan <yahan@iscas.ac.cn>
2021-05-26 16:05:23 +0800

    [riscv64] Fix build failed
    Change-Id: Ib86c4d6237251308fb16cef73e22f2efaa8ecbdc
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2919308
    Reviewed-by: Brice Dobry <brice.dobry@futurewei.com>
    Commit-Queue: Yahan Lu <yahan@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/master@{#74800}

--- src/codegen/riscv64/macro-assembler-riscv64.cc
+++ src/codegen/riscv64/macro-assembler-riscv64.cc
@@ -260,7 +260,7 @@ void TurboAssembler::CallRecordWriteStub(
     Register object, Register address,
     RememberedSetAction remembered_set_action, SaveFPRegsMode fp_mode,
     StubCallMode mode) {
-  DCHECK(!AreAliased(object, address, value));
+  DCHECK(!AreAliased(object, address));
   WriteBarrierDescriptor descriptor;
   RegList registers = descriptor.allocatable_registers();
   SaveRegisters(registers);
