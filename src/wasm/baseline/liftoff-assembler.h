dd4d29a200e874d39c70e0753e263fcc130304ad
Clemens Backes <clemensb@chromium.org>
2021-06-14 13:55:27 +0000

    Revert "[wasm][liftoff] Cache the memory start register"
    This reverts commit fd93f3382461a50c6d828d5b57a5880190dc7c2b.
    
    Reason for revert: Out of bounds errors in Unity benchmark (link is internal).
    
    Original change's description:
    > [wasm][liftoff] Cache the memory start register
    >
    > WebAssembly functions often have subsequent memory accesses, and each of
    > these memory accesses need the start address of the memory in a register.
    > With this CL the register with the memory start address is cached, so
    > only the first memory access has to load the memory start address into a
    > register, subsequent memory accesses can just reuse the register.
    >
    > In first measurements with the epic benchmark this reduces the size of
    > the generated Liftoff code by a bit more than 5%.
    >
    > R=​clemensb@chromium.org
    >
    > Bug: v8:11862
    > Change-Id: Ic33e7e3c00a4209570821269c728187affbeadcf
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2947403
    > Commit-Queue: Andreas Haas <ahaas@chromium.org>
    > Reviewed-by: Clemens Backes <clemensb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#75113}
    
    Bug: v8:11862
    Change-Id: I20c7e7d729cf9846499db90c02f8581d7f994ace
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2960216
    Auto-Submit: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#75132}

--- src/wasm/baseline/liftoff-assembler.h
+++ src/wasm/baseline/liftoff-assembler.h
@@ -200,7 +200,6 @@ class LiftoffAssembler : public TurboAssembler {
     uint32_t register_use_count[kAfterMaxLiftoffRegCode] = {0};
     LiftoffRegList last_spilled_regs;
     Register cached_instance = no_reg;
-    Register cached_mem_start = no_reg;
 
     bool has_unused_register(RegClass rc, LiftoffRegList pinned = {}) const {
       if (kNeedI64RegPair && rc == kGpRegPair) {
@@ -251,45 +250,29 @@ class LiftoffAssembler : public TurboAssembler {
     // Volatile registers are registers which are used for caching values that
     // can easily be reloaded. Those are returned first if we run out of free
     // registers.
+    // Note: This interface is a bit more generic than currently needed, in
+    // anticipation of more "volatile registers" being added later.
     bool has_volatile_register(LiftoffRegList candidates) {
-      return (cached_instance != no_reg && candidates.has(cached_instance)) ||
-             (cached_mem_start != no_reg && candidates.has(cached_mem_start));
+      return cached_instance != no_reg && candidates.has(cached_instance);
     }
 
     LiftoffRegister take_volatile_register(LiftoffRegList candidates) {
-      DCHECK(has_volatile_register(candidates));
-      Register reg = no_reg;
-      if (cached_instance != no_reg && candidates.has(cached_instance)) {
-        reg = cached_instance;
-        cached_instance = no_reg;
-      } else {
-        DCHECK(candidates.has(cached_mem_start));
-        reg = cached_mem_start;
-        cached_mem_start = no_reg;
-      }
-
-      LiftoffRegister ret{reg};
+      DCHECK(candidates.has(cached_instance));
+      LiftoffRegister ret{cached_instance};
       DCHECK_EQ(1, register_use_count[ret.liftoff_code()]);
       register_use_count[ret.liftoff_code()] = 0;
       used_registers.clear(ret);
+      cached_instance = no_reg;
       return ret;
     }
 
-    void SetCacheRegister(Register* cache, Register reg) {
-      DCHECK_EQ(no_reg, *cache);
-      *cache = reg;
+    void SetInstanceCacheRegister(Register reg) {
+      DCHECK_EQ(no_reg, cached_instance);
+      cached_instance = reg;
       int liftoff_code = LiftoffRegister{reg}.liftoff_code();
       DCHECK_EQ(0, register_use_count[liftoff_code]);
       register_use_count[liftoff_code] = 1;
       used_registers.set(reg);
-    }
-
-    void SetInstanceCacheRegister(Register reg) {
-      SetCacheRegister(&cached_instance, reg);
-    }
-
-    void SetMemStartCacheRegister(Register reg) {
-      SetCacheRegister(&cached_mem_start, reg);
     }
 
     Register TrySetCachedInstanceRegister(LiftoffRegList pinned) {
@@ -307,24 +290,13 @@ class LiftoffAssembler : public TurboAssembler {
       return new_cache_reg;
     }
 
-    void ClearCacheRegister(Register* cache) {
-      if (*cache == no_reg) return;
-      int liftoff_code = LiftoffRegister{*cache}.liftoff_code();
+    void ClearCachedInstanceRegister() {
+      if (cached_instance == no_reg) return;
+      int liftoff_code = LiftoffRegister{cached_instance}.liftoff_code();
       DCHECK_EQ(1, register_use_count[liftoff_code]);
       register_use_count[liftoff_code] = 0;
-      used_registers.clear(*cache);
-      *cache = no_reg;
-    }
-
-    void ClearCachedInstanceRegister() { ClearCacheRegister(&cached_instance); }
-
-    void ClearCachedMemStartRegister() {
-      ClearCacheRegister(&cached_mem_start);
-    }
-
-    void ClearAllCacheRegisters() {
-      ClearCacheRegister(&cached_instance);
-      ClearCacheRegister(&cached_mem_start);
+      used_registers.clear(cached_instance);
+      cached_instance = no_reg;
     }
 
     void inc_used(LiftoffRegister reg) {
@@ -579,8 +551,6 @@ class LiftoffAssembler : public TurboAssembler {
       if (cache_state_.is_free(r)) continue;
       if (r.is_gp() && cache_state_.cached_instance == r.gp()) {
         cache_state_.ClearCachedInstanceRegister();
-      } else if (r.is_gp() && cache_state_.cached_mem_start == r.gp()) {
-        cache_state_.ClearCachedMemStartRegister();
       } else {
         SpillRegister(r);
       }
