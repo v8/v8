4fc4efb74e2ac8396f85343d4df7a5dc4275285f
Clemens Backes <clemensb@chromium.org>
2021-06-14 09:55:43 +0200

    [flags] Stage wasm code protection behind --future
    This configuration (PKU with fallback to mprotect) is already finched,
    and can hopefully be enabled by default soon. Hence stage it behind
    --future to get more test and fuzzer coverage.
    
    R=jkummerow@chromium.org
    CC=dlehmann@google.com
    
    Bug: v8:11714
    Change-Id: I5c38c71582e79e3348e6296cbf175655d00a635c
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2959611
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#75128}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -910,10 +910,12 @@ DEFINE_DEBUG_BOOL(trace_wasm_native_heap, false,
                   "trace wasm native heap events")
 DEFINE_BOOL(wasm_write_protect_code_memory, false,
             "write protect code memory on the wasm native heap with mprotect")
+DEFINE_WEAK_IMPLICATION(future, wasm_write_protect_code_memory)
 DEFINE_BOOL(wasm_memory_protection_keys, false,
             "protect wasm code memory with PKU if available, no protection "
             "without support; fallback to mprotect by adding "
             "--wasm-write-protect-code-memory")
+DEFINE_WEAK_IMPLICATION(future, wasm_memory_protection_keys)
 DEFINE_DEBUG_BOOL(trace_wasm_serialization, false,
                   "trace serialization/deserialization")
 DEFINE_BOOL(wasm_async_compilation, true,
