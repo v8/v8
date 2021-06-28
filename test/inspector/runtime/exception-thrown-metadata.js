a76f72605bd81d861cf99298664b40ef5303365e
Sigurd Schneider <sigurds@chromium.org>
2021-06-28 15:55:51 +0200

    Add test for https://crrev.com/c/2992710
    Change-Id: Icd0acbc07698ef59804a6aae01b347e1e173cba9
    Bug: chromium:1213393
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2992711
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Auto-Submit: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#75418}

--- test/inspector/runtime/exception-thrown-metadata.js
+++ test/inspector/runtime/exception-thrown-metadata.js
@@ -9,4 +9,5 @@ Protocol.Runtime.onExceptionThrown(message => InspectorTest.logMessage(message))
 contextGroup.addScript("throw inspector.newExceptionWithMetaData('myerror', 'foo', 'bar');");
 Protocol.Runtime.evaluate({ expression: "setTimeout(() => { \n  throw inspector.newExceptionWithMetaData('myerror2', 'foo2', 'bar2'); }, 0)" });
 Protocol.Runtime.evaluate({ expression: "setTimeout(() => { \n  throw 2; }, 0)" });
+Protocol.Runtime.evaluate({ expression: "setTimeout(() => { \n  throw {}; }, 0)" });
 InspectorTest.waitForPendingTasks().then(InspectorTest.completeTest);
