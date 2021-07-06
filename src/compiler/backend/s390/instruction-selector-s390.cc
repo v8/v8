798caa85d69fa1dc00b87044eb9ac5c4f595b223
Milad Fa <mfarazma@redhat.com>
2021-07-06 11:36:51 -0400

    S390: optimize loading of reversed bytes
    This adds a check similar to Word32ReverseBytes.
    
    Change-Id: I3a75f29ae1b9cb75dd0fda471d6d42ca21915bf9
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3009795
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Commit-Queue: Milad Fa <mfarazma@redhat.com>
    Cr-Commit-Position: refs/heads/master@{#75591}

--- src/compiler/backend/s390/instruction-selector-s390.cc
+++ src/compiler/backend/s390/instruction-selector-s390.cc
@@ -1084,6 +1084,19 @@ void InstructionSelector::VisitInt64AbsWithOverflow(Node* node) {
 
 void InstructionSelector::VisitWord64ReverseBytes(Node* node) {
   S390OperandGenerator g(this);
+  NodeMatcher input(node->InputAt(0));
+  if (CanCover(node, input.node()) && input.IsLoad()) {
+    LoadRepresentation load_rep = LoadRepresentationOf(input.node()->op());
+    if (load_rep.representation() == MachineRepresentation::kWord64) {
+      Node* base = input.node()->InputAt(0);
+      Node* offset = input.node()->InputAt(1);
+      Emit(kS390_LoadReverse64 | AddressingModeField::encode(kMode_MRR),
+           // TODO(miladfarca): one of the base and offset can be imm.
+           g.DefineAsRegister(node), g.UseRegister(base),
+           g.UseRegister(offset));
+      return;
+    }
+  }
   Emit(kS390_LoadReverse64RR, g.DefineAsRegister(node),
        g.UseRegister(node->InputAt(0)));
 }
