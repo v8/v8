40b87a2997685dd1162838e4304ce23a10bd5d92
Georg Neis <neis@chromium.org>
2021-07-13 17:26:47 +0200

    [M90-LTS][compiler] Fix a bug in CodeGenerator::AddTranslationForOperand
    No-Try=true
    No-Presubmit=true
    No-Tree-Checks=true
    (cherry picked from commit 374354bfe4a30740b96936b33e522d6fcd1cda67)
    
    Bug: chromium:1228407
    Change-Id: I20941e8aaa4a1d82b035a5387cdd1b08b8994c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3024153
    Auto-Submit: Georg Neis <neis@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#75712}
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3040843
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Cr-Commit-Position: refs/branch-heads/9.0@{#66}
    Cr-Branched-From: bd0108b4c88e0d6f2350cb79b5f363fbd02f3eb7-refs/heads/9.0.257@{#1}
    Cr-Branched-From: 349bcc6a075411f1a7ce2d866c3dfeefc2efa39d-refs/heads/master@{#73001}

--- src/compiler/backend/code-generator.cc
+++ src/compiler/backend/code-generator.cc
@@ -1376,7 +1376,8 @@ void CodeGenerator::AddTranslationForOperand(Instruction* instr,
       default:
         UNREACHABLE();
     }
-    if (literal.object().equals(info()->closure())) {
+    if (literal.object().equals(info()->closure()) &&
+        info()->function_context_specializing()) {
       translations_.StoreJSFrameFunction();
     } else {
       int literal_id = DefineDeoptimizationLiteral(literal);
