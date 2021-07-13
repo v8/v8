0e7f91956cecd8918ba4e8f721a59bae9f339b59
Georg Neis <neis@chromium.org>
2021-07-13 17:26:47 +0200

    Merged: [compiler] Fix a bug in CodeGenerator::AddTranslationForOperand
    (cherry picked from commit 374354bfe4a30740b96936b33e522d6fcd1cda67)
    
    Bug: chromium:1228407
    No-Try: true
    No-Presubmit: true
    No-Tree-Checks: true
    Change-Id: I358d8736b7b5f87300496cbb39a7689d8207d85f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3027260
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Commit-Queue: Adam Klein <adamk@chromium.org>
    Cr-Commit-Position: refs/branch-heads/9.1@{#77}
    Cr-Branched-From: 0e4ac64a8cf298b14034a22f9fe7b085d2cb238d-refs/heads/9.1.269@{#1}
    Cr-Branched-From: f565e72d5ba88daae35a59d0f978643e2343e912-refs/heads/master@{#73847}

--- src/compiler/backend/code-generator.cc
+++ src/compiler/backend/code-generator.cc
@@ -1400,7 +1400,8 @@ void CodeGenerator::AddTranslationForOperand(Instruction* instr,
       default:
         UNREACHABLE();
     }
-    if (literal.object().equals(info()->closure())) {
+    if (literal.object().equals(info()->closure()) &&
+        info()->function_context_specializing()) {
       translations_.StoreJSFrameFunction();
     } else {
       int literal_id = DefineDeoptimizationLiteral(literal);
