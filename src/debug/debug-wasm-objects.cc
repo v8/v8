877ad411357edad1570bfc6b11c5715f2c8e8267
Andreas Haas <ahaas@chromium.org>
2021-07-19 14:49:37 +0200

    [wasm][debug] Fix GCMole issue in AddWasmTableObjectInternalProperties/2
    An object got allocated as part of a parameter expression, which may
    have caused GC issues.
    
    R=bmeurer@chromium.org
    
    Bug: chromium:1230041
    Change-Id: I94537db7d0be5200049fbc6bd3eda0d49b6d7c17
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3038525
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#75801}

--- src/debug/debug-wasm-objects.cc
+++ src/debug/debug-wasm-objects.cc
@@ -1135,7 +1135,8 @@ Handle<ArrayList> AddWasmTableObjectInternalProperties(
   int length = table->current_length();
   Handle<FixedArray> entries = isolate->factory()->NewFixedArray(length);
   for (int i = 0; i < length; ++i) {
-    entries->set(i, *WasmTableObject::Get(isolate, table, i));
+    auto entry = WasmTableObject::Get(isolate, table, i);
+    entries->set(i, *entry);
   }
   Handle<JSArray> final_entries = isolate->factory()->NewJSArrayWithElements(
       entries, i::PACKED_ELEMENTS, length);
