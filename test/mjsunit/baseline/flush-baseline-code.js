27d01ae032ba1bda974d60fec96b8c07d2b803e7
Mythri A <mythria@chromium.org>
2021-07-20 20:40:25 +0100

    [mjsunit] Fix flush-baseline-code test
    With lazy feedback allocation we don't tier up to baseline early. This
    test requires us to tier up to baseline. So add
    --lazy_feedback_allocation as a required flag.
    
    Bug: v8:12009
    Change-Id: Ibbc1d1cc74ae368ef414f513a0cd46e9bf068186
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3042718
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Auto-Submit: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Zhi An Ng <zhin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#75829}

--- test/mjsunit/baseline/flush-baseline-code.js
+++ test/mjsunit/baseline/flush-baseline-code.js
@@ -4,7 +4,7 @@
 
 // Flags: --expose-gc --stress-flush-bytecode --allow-natives-syntax
 // Flags: --baseline-batch-compilation-threshold=0 --sparkplug
-// Flags: --no-always-sparkplug
+// Flags: --no-always-sparkplug --lazy-feedback-allocation
 
 function HasBaselineCode(f) {
   let opt_status = %GetOptimizationStatus(f);
