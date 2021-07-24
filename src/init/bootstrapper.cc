d63ca69c2244d079a68d96a908fa1f3a34d6291f
legendecas <legendecas@gmail.com>
2021-07-24 16:22:43 +0800

    [builtins] Implement Array#findFromLast and friends
    This proposal reached Stage 3 at the July 2021 TC39.
    
    https://github.com/tc39/proposal-array-find-from-last
    
    Bug: v8:11990
    Change-Id: I1364b46b7ed4bc56e4b3024d14bde799f9878b5a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3037160
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#76007}

--- src/init/bootstrapper.cc
+++ src/init/bootstrapper.cc
@@ -4405,6 +4405,39 @@ EMPTY_INITIALIZE_GLOBAL_FOR_FEATURE(harmony_intl_more_timezone)
 
 #undef EMPTY_INITIALIZE_GLOBAL_FOR_FEATURE
 
+void Genesis::InitializeGlobal_harmony_array_find_last() {
+  if (!FLAG_harmony_array_find_last) return;
+
+  {
+    Handle<JSFunction> array_function(native_context()->array_function(),
+                                      isolate());
+    Handle<JSObject> array_prototype(
+        JSObject::cast(array_function->instance_prototype()), isolate());
+
+    SimpleInstallFunction(isolate_, array_prototype, "findLast",
+                          Builtin::kArrayPrototypeFindLast, 1, false);
+    SimpleInstallFunction(isolate_, array_prototype, "findLastIndex",
+                          Builtin::kArrayPrototypeFindLastIndex, 1, false);
+
+    Handle<JSObject> unscopables = Handle<JSObject>::cast(
+        JSObject::GetProperty(isolate(), array_prototype,
+                              isolate()->factory()->unscopables_symbol())
+            .ToHandleChecked());
+
+    InstallTrueValuedProperty(isolate_, unscopables, "findLast");
+    InstallTrueValuedProperty(isolate_, unscopables, "findLastIndex");
+  }
+
+  {
+    Handle<JSObject> prototype(native_context()->typed_array_prototype(),
+                               isolate());
+    SimpleInstallFunction(isolate_, prototype, "findLast",
+                          Builtin::kTypedArrayPrototypeFindLast, 1, false);
+    SimpleInstallFunction(isolate_, prototype, "findLastIndex",
+                          Builtin::kTypedArrayPrototypeFindLastIndex, 1, false);
+  }
+}
+
 void Genesis::InitializeGlobal_harmony_object_has_own() {
   if (!FLAG_harmony_object_has_own) return;
 
