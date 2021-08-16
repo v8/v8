1cbb68a5506cc5804f75048b23fee5416d4aed91
Ng Zhi An <zhin@chromium.org>
2021-08-16 13:11:03 -0700

    [wasm-simd] Optimize I64x2UConvertI32x4High
    Fixed: v8:12095
    Bug: v8:12095
    Change-Id: If2021397000958ccdd058b99ce8f4d6e8d4d2836
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3097106
    Reviewed-by: Deepti Gandluri <gdeepti@chromium.org>
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#76398}

--- src/codegen/shared-ia32-x64/macro-assembler-shared-ia32-x64.cc
+++ src/codegen/shared-ia32-x64/macro-assembler-shared-ia32-x64.cc
@@ -877,11 +877,16 @@ void SharedTurboAssembler::I64x2UConvertI32x4High(XMMRegister dst,
     vpxor(scratch, scratch, scratch);
     vpunpckhdq(dst, src, scratch);
   } else {
-    if (dst != src) {
-      movaps(dst, src);
+    if (dst == src) {
+      // xorps can be executed on more ports than pshufd.
+      xorps(scratch, scratch);
+      punpckhdq(dst, scratch);
+    } else {
+      CpuFeatureScope sse_scope(this, SSE4_1);
+      // No dependency on dst.
+      pshufd(dst, src, 0xEE);
+      pmovzxdq(dst, dst);
     }
-    xorps(scratch, scratch);
-    punpckhdq(dst, scratch);
   }
 }
 
