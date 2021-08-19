ea8ed0a9d1e18449c7c1eb0285293a602039bf3d
Omer Katz <omerkatz@chromium.org>
2021-08-19 08:56:02 +0000

    Revert "cppgc: Enable checks for assignments in prefinalizers"
    This reverts commit edcc8ff5b5f4e68905bf140718e224161b1adaa1.
    
    Reason for revert: https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Blink%20Linux%20Debug/10806/overview
    A prefinalizer is creating a WeakMember from a raw pointer to a dead object for checking whether it is in a set.
    
    Original change's description:
    > cppgc: Enable checks for assignments in prefinalizers
    >
    > Bug: v8:11749
    > Change-Id: Ic027f732030fb6a2befeffeca9db2eacfd0830a5
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3099953
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#76370}
    
    Bug: v8:11749
    Change-Id: I0c90f232df9ae363f05f8b9ba26c2a7eede8a269
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3106646
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#76377}

--- test/unittests/heap/cppgc/prefinalizer-unittest.cc
+++ test/unittests/heap/cppgc/prefinalizer-unittest.cc
@@ -285,7 +285,7 @@ class GCedHolder : public GarbageCollected<GCedHolder> {
 }  // namespace
 
 #if V8_ENABLE_CHECKS
-#ifdef CPPGC_VERIFY_HEAP
+#ifdef CPPGC_CHECK_ASSIGNMENTS_IN_PREFINALIZERS
 
 TEST_F(PrefinalizerDeathTest, PrefinalizerCantRewireGraphWithDeadObjects) {
   Persistent<LinkedNode> root{MakeGarbageCollected<LinkedNode>(
@@ -318,7 +318,7 @@ TEST_F(PrefinalizerDeathTest, PrefinalizerCantRessurectObjectOnHeap) {
   EXPECT_DEATH_IF_SUPPORTED(PreciseGC(), "");
 }
 
-#endif  // CPPGC_VERIFY_HEAP
+#endif  // CPPGC_CHECK_ASSIGNMENTS_IN_PREFINALIZERS
 #endif  // V8_ENABLE_CHECKS
 
 }  // namespace internal
