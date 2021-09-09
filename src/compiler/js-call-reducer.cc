0d42c9d0b1b7e95e20c5e80c448fb2e1ef22bcbc
Marja Hölttä <marja@chromium.org>
2021-09-09 10:09:10 +0200

    [builtins] Unify ArrayPrototypeSlice & ReduceArrayPrototypeSlice
    They need to agree about when to delegate to CloneFastJSArray, since it
    produces arrays which are potentially COW. If they don't agree, TF
    generates code which produces a COW array and then expects it to be
    non-COW -> immediate deopt.
    
    This CL gets rid of the discrepancy in the case when there's exactly
    one argument and it's the number 0.
    
    Some corner cases remain, e.g., 1st argument not a number but ToInteger
    returns 0. These should be extremely rare in the real world.
    
    Bug: v8:12194
    Change-Id: I10230245c97f8997da4d79702f29ebff11297229
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3147910
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#76745}

--- src/compiler/js-call-reducer.cc
+++ src/compiler/js-call-reducer.cc
@@ -5950,9 +5950,13 @@ Reduction JSCallReducer::ReduceArrayPrototypeSlice(Node* node) {
   Effect effect = n.effect();
   Control control = n.control();
 
-  // Optimize for the case where we simply clone the {receiver},
-  // i.e. when the {start} is zero and the {end} is undefined
-  // (meaning it will be set to {receiver}s "length" property).
+  // Optimize for the case where we simply clone the {receiver}, i.e. when the
+  // {start} is zero and the {end} is undefined (meaning it will be set to
+  // {receiver}s "length" property). This logic should be in sync with
+  // ReduceArrayPrototypeSlice (to a reasonable degree). This is because
+  // CloneFastJSArray produces arrays which are potentially COW. If there's a
+  // discrepancy, TF generates code which produces a COW array and then expects
+  // it to be non-COW (or the other way around) -> immediate deopt.
   if (!NumberMatcher(start).Is(0) ||
       !HeapObjectMatcher(end).Is(factory()->undefined_value())) {
     return NoChange();
