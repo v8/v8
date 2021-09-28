5f79f478b5bdd9efabfeafac12ee7181b4b1d0ea
Dominik Inführ <dinfuehr@chromium.org>
2021-09-28 18:57:31 +0200

    [heap] Improve ephemeron processing
    Refactor code such that the linear algorithm is actually executed
    outside the method for the fixpoint iteration. Also added a CHECK
    which verifies that iterating the ephemerons one more time results in
    no further marked objects.
    
    Also force another iteration when ProcessMarkingWorklist() processed
    some object. In such cases we need to re-process all ephemerons
    otherwise ephemeron semantics might be broken.
    
    No-Try: true
    No-Presubmit: true
    No-Tree-Checks: true
    Bug: chromium:1252918
    Change-Id: I9123dda50e34227a04825fd8b3172368286cc76f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3190100
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#77131}
    (cherry picked from commit 1054ee7f349d6be22e9518cf9b794b206d0e5818)
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3191394
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Shu-yu Guo <syg@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Auto-Submit: Krishna Govind <govind@chromium.org>

--- src/heap/concurrent-marking.cc
+++ src/heap/concurrent-marking.cc
@@ -445,7 +445,7 @@ void ConcurrentMarking::Run(JobDelegate* delegate,
     isolate->PrintWithTimestamp("Starting concurrent marking task %d\n",
                                 task_id);
   }
-  bool ephemeron_marked = false;
+  bool another_ephemeron_iteration = false;
 
   {
     TimedScope scope(&time_ms);
@@ -455,7 +455,7 @@ void ConcurrentMarking::Run(JobDelegate* delegate,
 
       while (weak_objects_->current_ephemerons.Pop(task_id, &ephemeron)) {
         if (visitor.ProcessEphemeron(ephemeron.key, ephemeron.value)) {
-          ephemeron_marked = true;
+          another_ephemeron_iteration = true;
         }
       }
     }
@@ -508,6 +508,7 @@ void ConcurrentMarking::Run(JobDelegate* delegate,
           current_marked_bytes += visited_size;
         }
       }
+      if (objects_processed > 0) another_ephemeron_iteration = true;
       marked_bytes += current_marked_bytes;
       base::AsAtomicWord::Relaxed_Store<size_t>(&task_state->marked_bytes,
                                                 marked_bytes);
@@ -523,7 +524,7 @@ void ConcurrentMarking::Run(JobDelegate* delegate,
 
       while (weak_objects_->discovered_ephemerons.Pop(task_id, &ephemeron)) {
         if (visitor.ProcessEphemeron(ephemeron.key, ephemeron.value)) {
-          ephemeron_marked = true;
+          another_ephemeron_iteration = true;
         }
       }
     }
@@ -544,8 +545,8 @@ void ConcurrentMarking::Run(JobDelegate* delegate,
     base::AsAtomicWord::Relaxed_Store<size_t>(&task_state->marked_bytes, 0);
     total_marked_bytes_ += marked_bytes;
 
-    if (ephemeron_marked) {
-      set_ephemeron_marked(true);
+    if (another_ephemeron_iteration) {
+      set_another_ephemeron_iteration(true);
     }
   }
   if (FLAG_trace_concurrent_marking) {
