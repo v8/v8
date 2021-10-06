a03ed9d61b8c893ac204a1e4b0cbdc02c8469cc9
Vasili Skurydzin <vasili.skurydzin@ibm.com>
2021-10-06 10:30:39 -0400

    aix: Fix stack overflow error in debug mode on regress-1067270.js
    Change-Id: I49a4e2f05028279cd69d3909a9ca80f4c0acb1c5
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3208649
    Reviewed-by: Milad Fa <mfarazma@redhat.com>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Vasili Skurydzin <vasili.skurydzin@ibm.com>
    Cr-Commit-Position: refs/heads/main@{#77266}

--- test/mjsunit/regress/regress-1067270.js
+++ test/mjsunit/regress/regress-1067270.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
-// Flags: --allow-natives-syntax --stack-size=1200
+// Flags: --allow-natives-syntax --stack-size=1400
 
 const needle = Array(1802).join(" +") + Array(16884).join("A");
 const string = "A";
