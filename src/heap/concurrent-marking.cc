b36b2074ac2dba7b917d92b8d2be7aba2a9457ac
Shu-yu Guo <syg@chromium.org>
2021-10-13 12:47:31 -0700

    [heap] Add missing SynchronizePageAccess
    The concurrent marker consults the page flags to see if it should skip
    objects in the shared heap, and it was missing a SynchronizePageAccess,
    causing TSAN false positives.
    
    Bug: v8:12314, v8:12007
    Change-Id: I888a68a3eddaa3dfa1644364226010def8d2a9b6
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3219946
    Auto-Submit: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#77390}

--- src/heap/concurrent-marking.cc
+++ src/heap/concurrent-marking.cc
@@ -265,6 +265,7 @@ class ConcurrentMarkingVisitor final
       DCHECK(!HasWeakHeapObjectTag(object));
       if (!object.IsHeapObject()) continue;
       HeapObject heap_object = HeapObject::cast(object);
+      concrete_visitor()->SynchronizePageAccess(heap_object);
       BasicMemoryChunk* target_page =
           BasicMemoryChunk::FromHeapObject(heap_object);
       if (!is_shared_heap_ && target_page->InSharedHeap()) continue;
