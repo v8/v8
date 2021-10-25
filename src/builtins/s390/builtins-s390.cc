cf98260e462bd2c3907c8478189cd197de7d8404
Milad Fa <mfarazma@redhat.com>
2021-10-25 08:27:06 -0400

    S390: Fix compilation error on builtins
    variable 'pushed_stack_space' set but not used.
    
    Change-Id: I427f76448b9933f9c8f1169709e28eef4512241f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3241543
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Commit-Queue: Milad Fa <mfarazma@redhat.com>
    Cr-Commit-Position: refs/heads/main@{#77526}

--- src/builtins/s390/builtins-s390.cc
+++ src/builtins/s390/builtins-s390.cc
@@ -642,6 +642,7 @@ void Generate_JSEntryVariant(MacroAssembler* masm, StackFrame::Type type,
   // pop the faked function when we return.
   Handle<Code> trampoline_code =
       masm->isolate()->builtins()->code_handle(entry_trampoline);
+  USE(pushed_stack_space);
   DCHECK_EQ(kPushedStackSpace, pushed_stack_space);
   __ Call(trampoline_code, RelocInfo::CODE_TARGET);
 
