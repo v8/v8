0b678368b0e576578ce27254245ff0d1eeef7cb3
Jakob Gruber <jgruber@chromium.org>
2021-11-02 11:57:48 +0100

    Merged: [regexp] Release regexp zone memory during JS parsing
    Since early regexp errors were implemented in
    crrev.com/a56874d3eb5cff9f8f04f899ac45d0d2ef88ab4d, the JS parser
    calls into the regexp parser to validate the regexp literal syntax.
    
    For these calls, the JS parser passes its Zone to the regexp parser.
    This means that scripts with multiple regexp literals are all parsed
    using the same Zone memory. Very large scripts with many (think
    hundreds of thousands) regexp literals may thus run out of memory
    whereas previously they would parse and run successfully.
    
    This CL fixes the OOMs by resetting the state of the JS parser Zone
    around regexp parser calls. We introduce a new ZoneScope class,
    similar to HandleScope, which controls the lifetime of zone objects
    allocated within its scope. In other words:
    
     {
       ZoneScope zone_scope(zone);  // Store zone state S.
       // ... Allocate objects O in zone.
       // zone is now in state S'.
     }
     // zone_scope goes out of scope, reset zone to state S. Objects O
     // are freed and no longer usable.
    
    (cherry picked from commit 30cab7b1323690db20c1c02963de55538685e9b3)
    
    No-Try: true
    No-Presubmit: true
    No-Treechecks: true
    Bug: chromium:1264014,v8:896
    Change-Id: I3e7ac36f25a9d6c4eda2460bd1bea9814685e89b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3256783
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Jakob Gruber <jgruber@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#77646}
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3268729
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/9.6@{#20}
    Cr-Branched-From: 0b7bda016178bf438f09b3c93da572ae3663a1f7-refs/heads/9.6.180@{#1}
    Cr-Branched-From: 41a5a247d9430b953e38631e88d17790306f7a4c-refs/heads/main@{#77244}

--- src/parsing/parser-base.h
+++ src/parsing/parser-base.h
@@ -1799,6 +1799,7 @@ bool ParserBase<Impl>::ValidateRegExpLiteral(const AstRawString* pattern,
   // TODO(jgruber): If already validated in the preparser, skip validation in
   // the parser.
   DisallowGarbageCollection no_gc;
+  ZoneScope zone_scope(zone());  // Free regexp parser memory after use.
   const unsigned char* d = pattern->raw_data();
   if (pattern->is_one_byte()) {
     return RegExp::VerifySyntax(zone(), stack_limit(),
@@ -5322,7 +5323,7 @@ typename ParserBase<Impl>::BlockT ParserBase<Impl>::ParseBlock(
     body->set_scope(scope()->FinalizeBlockScope());
   }
 
-  body->InitializeStatements(statements, zone_);
+  body->InitializeStatements(statements, zone());
   return body;
 }
 
