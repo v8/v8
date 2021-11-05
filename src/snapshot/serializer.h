cd4419ed119f043606fc93c60e78eec6fa3528f0
Ng Zhi An <zhin@chromium.org>
2021-11-05 11:23:21 -0700

    [serializer] Fix -Wshadow warnings
    Bug: v8:12244,v8:12245
    Change-Id: I801821448459bf2b74c99b36f6882d59bb24a273
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3264645
    Reviewed-by: Jakob Gruber <jgruber@chromium.org>
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#77776}

--- src/snapshot/serializer.h
+++ src/snapshot/serializer.h
@@ -350,7 +350,7 @@ class Serializer : public SerializerDeserializer {
 
   // Disallow GC during serialization.
   // TODO(leszeks, v8:10815): Remove this constraint.
-  DISALLOW_GARBAGE_COLLECTION(no_gc)
+  DISALLOW_GARBAGE_COLLECTION(no_gc_)
 
   Isolate* isolate_;
   HotObjectsList hot_objects_;
