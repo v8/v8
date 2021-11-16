ae569cf877c19331e9d396ab26a3adf9e53a3273
Milad Fa <mfarazma@redhat.com>
2021-11-16 14:59:52 -0500

    PPC/s390: Update comments about argument count in interface descriptors
    Port 2e955523d1d960f6046e979aa5f0c7c25017221e
    
    Original Commit Message:
    
        Comments in interface-descriptors stated that the argument count is
        without the receiver, which is no longer true (see [1]).
    
        [1] https://crrev.com/c/3140608
    
    R=pthier@chromium.org, joransiu@ca.ibm.com, junyan@redhat.com, midawson@redhat.com
    BUG=
    LOG=N
    
    Change-Id: I1bbce6672f19a3e508676e4b23cf9910542cbb21
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3285407
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Reviewed-by: Joran Siu <joransiu@ca.ibm.com>
    Commit-Queue: Milad Fa <mfarazma@redhat.com>
    Cr-Commit-Position: refs/heads/main@{#77949}

--- src/codegen/s390/interface-descriptors-s390-inl.h
+++ src/codegen/s390/interface-descriptors-s390-inl.h
@@ -121,7 +121,7 @@ constexpr auto CallTrampolineDescriptor::registers() {
 
 // static
 constexpr auto CallVarargsDescriptor::registers() {
-  // r2 : number of arguments (on the stack, not including receiver)
+  // r2 : number of arguments (on the stack)
   // r3 : the target to call
   // r6 : arguments list length (untagged)
   // r4 : arguments list (FixedArray)
@@ -139,13 +139,13 @@ constexpr auto CallForwardVarargsDescriptor::registers() {
 // static
 constexpr auto CallFunctionTemplateDescriptor::registers() {
   // r3 : function template info
-  // r4 : number of arguments (on the stack, not including receiver)
+  // r4 : number of arguments (on the stack)
   return RegisterArray(r3, r4);
 }
 
 // static
 constexpr auto CallWithSpreadDescriptor::registers() {
-  // r2: number of arguments (on the stack, not including receiver)
+  // r2: number of arguments (on the stack)
   // r3 : the target to call
   // r4 : the object to spread
   return RegisterArray(r3, r2, r4);
@@ -160,7 +160,7 @@ constexpr auto CallWithArrayLikeDescriptor::registers() {
 
 // static
 constexpr auto ConstructVarargsDescriptor::registers() {
-  // r2 : number of arguments (on the stack, not including receiver)
+  // r2 : number of arguments (on the stack)
   // r3 : the target to call
   // r5 : the new target
   // r6 : arguments list length (untagged)
@@ -179,7 +179,7 @@ constexpr auto ConstructForwardVarargsDescriptor::registers() {
 
 // static
 constexpr auto ConstructWithSpreadDescriptor::registers() {
-  // r2 : number of arguments (on the stack, not including receiver)
+  // r2 : number of arguments (on the stack)
   // r3 : the target to call
   // r5 : the new target
   // r4 : the object to spread
@@ -241,7 +241,7 @@ constexpr auto InterpreterDispatchDescriptor::registers() {
 
 // static
 constexpr auto InterpreterPushArgsThenCallDescriptor::registers() {
-  return RegisterArray(r2,   // argument count (not including receiver)
+  return RegisterArray(r2,   // argument count
                        r4,   // address of first argument
                        r3);  // the target callable to be call
 }
@@ -249,7 +249,7 @@ constexpr auto InterpreterPushArgsThenCallDescriptor::registers() {
 // static
 constexpr auto InterpreterPushArgsThenConstructDescriptor::registers() {
   return RegisterArray(
-      r2,   // argument count (not including receiver)
+      r2,   // argument count
       r6,   // address of the first argument
       r3,   // constructor to call
       r5,   // new target
