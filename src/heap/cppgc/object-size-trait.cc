53d9e8b19c8086f2cf30ac5adb4e311bbe39adb0
Michael Lippautz <mlippautz@chromium.org>
2021-11-24 20:30:54 +0100

    cppgc: Fix data race in DCHECK in ObjectSizeTrait
    The DCHECK must use atomic accessors as well.
    
    Change-Id: I94983c1e38bc9d436f1577509788fc21e3d4e374
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3300143
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78077}

--- src/heap/cppgc/object-size-trait.cc
+++ src/heap/cppgc/object-size-trait.cc
@@ -26,7 +26,7 @@ size_t BaseObjectSizeTrait::GetObjectSizeForGarbageCollectedMixin(
   const auto& header =
       BasePage::FromPayload(address)
           ->ObjectHeaderFromInnerAddress<AccessMode::kAtomic>(address);
-  DCHECK(!header.IsLargeObject());
+  DCHECK(!header.IsLargeObject<AccessMode::kAtomic>());
   return header.ObjectSize<AccessMode::kAtomic>();
 }
 
