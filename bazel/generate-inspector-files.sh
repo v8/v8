b5a4311f2682e9c3633d99768fe8c367c9e9e7e2
Piotr Sikora <piotrsikora@google.com>
2021-12-13 16:08:05 -0800

    [bazel] Generate inspector files without building inspector.
    While there, make sure to exit on failures (e.g. missing tools).
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>
    Change-Id: Ie84425bbedefc8c37cf12afbf0ad541caa125ac0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3333634
    Reviewed-by: Victor Gomes <victorgomes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78388}

--- bazel/generate-inspector-files.sh
+++ bazel/generate-inspector-files.sh
@@ -2,13 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+set -e
+
 BAZEL_OUT=$1
 
 # Create a default GN output folder
 gn gen out/inspector
 
 # Generate inspector files
-autoninja -C out/inspector src/inspector:inspector
+autoninja -C out/inspector src/inspector:protocol_generated_sources
 
 # Create directories in bazel output folder
 mkdir -p $BAZEL_OUT/include/inspector
