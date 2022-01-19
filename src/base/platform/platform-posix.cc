f82b30cef9bf193301cd20a2b6d1650d8d297be0
Samuel Groß <saelo@chromium.org>
2022-01-19 17:32:32 +0100

    [base] Introduce fast path for AddressSpaceReservation::Allocate
    In case the requested permissions are kNoAccess, nothing needs to be
    done as the mapping backing an AddressSpaceReservation is always
    mapped kNoAccess. This fixes a performance regression on macOS.
    
    Bug: chromium:1287599
    Change-Id: I77d80489caf477e29434f9d0a06899746cb9403f
    Cq-Include-Trybots: luci.v8.try:v8_linux64_heap_sandbox_dbg_ng,v8_linux_arm64_sim_heap_sandbox_dbg_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3398144
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78697}

--- src/base/platform/platform-posix.cc
+++ src/base/platform/platform-posix.cc
@@ -889,6 +889,12 @@ bool AddressSpaceReservation::Allocate(void* address, size_t size,
                                        OS::MemoryPermission access) {
   // The region is already mmap'ed, so it just has to be made accessible now.
   DCHECK(Contains(address, size));
+  if (access == OS::MemoryPermission::kNoAccess) {
+    // Nothing to do. We don't want to call SetPermissions with kNoAccess here
+    // as that will for example mark the pages as discardable, which is
+    // probably not desired here.
+    return true;
+  }
   return OS::SetPermissions(address, size, access);
 }
 
