517ed4ad001fd702331093e8ffa549b4dce8fb61
Nico Hartmann <nicohartmann@chromium.org>
2022-01-27 13:48:40 +0100

    Reland "[Torque] Generalize Torque literals to larger size"
    Previously, literals in Torque were stored as double values, which
    made it impossible to precisely represent 64 bit integer values.
    This CL replaces the old literal expression with an integer and
    floating point literal expression that are unbounded in size. We
    allow implicit conversion of these literals to arbitary integer
    and floating point types respectively and insert a corresponding
    bounds check into generated CSA.
    
    Changes in the reland: Simplified IntegerLiteral to single digit.
    
    Bug: v8:7793, chromium:1289282
    Change-Id: I31c762c2f31165c7a1d0b07842b764e5851ce189
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3406750
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Commit-Queue: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78811}

--- tools/debug_helper/debug-macro-shims.h
+++ tools/debug_helper/debug-macro-shims.h
@@ -8,6 +8,7 @@
 #ifndef V8_TORQUE_DEBUG_MACRO_SHIMS_H_
 #define V8_TORQUE_DEBUG_MACRO_SHIMS_H_
 
+#include "src/numbers/integer-literal.h"
 #include "src/objects/smi.h"
 #include "tools/debug_helper/debug-helper-internal.h"
 
@@ -77,6 +78,9 @@ inline Value<int32_t> SmiUntag(d::MemoryAccessor accessor, uintptr_t s_t) {
   Smi s(s_t);
   return {d::MemoryAccessResult::kOk, s.value()};
 }
+inline Value<uintptr_t> SmiFromInt32(d::MemoryAccessor accessor, int32_t i) {
+  return {d::MemoryAccessResult::kOk, Smi::FromInt(i).ptr()};
+}
 inline Value<bool> UintPtrLessThan(d::MemoryAccessor accessor, uintptr_t a,
                                    uintptr_t b) {
   return {d::MemoryAccessResult::kOk, a < b};
@@ -96,6 +100,19 @@ inline Value<bool> Word32Equal(d::MemoryAccessor accessor, uint32_t a,
 inline Value<bool> Word32NotEqual(d::MemoryAccessor accessor, uint32_t a,
                                   uint32_t b) {
   return {d::MemoryAccessResult::kOk, a != b};
+}
+// This is used in a nested call where we cannot pass Value<int32_t>.
+inline int31_t ConstexprIntegerLiteralToInt31(d::MemoryAccessor accessor,
+                                              const IntegerLiteral& i) {
+  return i.To<int32_t>();
+}
+inline int32_t ConstexprIntegerLiteralToInt32(d::MemoryAccessor accessor,
+                                              const IntegerLiteral& i) {
+  return i.To<int32_t>();
+}
+inline intptr_t ConstexprIntegerLiteralToIntptr(d::MemoryAccessor accessor,
+                                                const IntegerLiteral& i) {
+  return i.To<intptr_t>();
 }
 
 }  // namespace CodeStubAssembler
