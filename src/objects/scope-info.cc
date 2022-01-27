6aa029c1b5f4debc9a4743e0fd049ae68e419030
Milad Fa <mfarazma@redhat.com>
2022-01-27 14:08:20 -0500

    [runtime] fix gcc compilation error
    This fixes the `unused variable` error in release builds
    with gcc.
    
    Change-Id: I637dac3968e768960035960b846b2783f92571d2
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3421511
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78822}

--- src/objects/scope-info.cc
+++ src/objects/scope-info.cc
@@ -278,6 +278,7 @@ Handle<ScopeInfo> ScopeInfo::Create(IsolateT* isolate, Zone* zone, Scope* scope,
           } else {
             Handle<NameToIndexHashTable> new_table = NameToIndexHashTable::Add(
                 isolate, local_names_hashtable, var->name(), local_index);
+            USE(new_table);
             DCHECK_EQ(*new_table, *local_names_hashtable);
           }
           scope_info.set(context_local_info_base + local_index,
