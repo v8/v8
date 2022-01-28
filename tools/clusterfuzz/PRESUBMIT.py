5e294b1741da99fbc8d91542796d8d998b4871ce
Al Muthanna Athamina <almuthanna@chromium.org>
2022-01-28 14:46:13 +0100

    Add clusterfuzz trials config file with presubmit checks
    We can now specify the trials for clusterfuzz using the file
    clusterfuzz_trials_config.json. There is also a presubmit check
    to make sure that it is formatted correctly.
    
    Change-Id: Iafb7063b63b1daeb7653830542d13b419cf187d6
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3416191
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78848}

--- tools/clusterfuzz/PRESUBMIT.py
+++ tools/clusterfuzz/PRESUBMIT.py
@@ -2,7 +2,62 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
+
+try:
+  basestring       # Python 2
+except NameError:  # Python 3
+  basestring = str
+
+
+def _CheckTrialsConfig(input_api, output_api):
+  def FilterFile(affected_file):
+    return input_api.FilterSourceFile(
+        affected_file,
+        files_to_check=(r'.+clusterfuzz_trials_config\.json',))
+
+  results = []
+  for f in input_api.AffectedFiles(
+      file_filter=FilterFile, include_deletes=False):
+    with open(f.AbsoluteLocalPath()) as j:
+      try:
+        trials = json.load(j)
+        for trial in trials:
+          if not all (k in trial for k in ('app_args', 'app_name', 'probability')):
+            results.append('trial %s is not configured correctly' % trial)
+          if trial['app_name'] != 'd8':
+            results.append('trial %s has an incorrect app_name' % trial)
+          if not isinstance(trial['probability'], float):
+            results.append('trial %s probability is not a float' % trial)
+          if not (0 <= trial['probability'] <= 1):
+            results.append('trial %s has invalid probability value' % trial)
+          if not isinstance(trial['app_args'], basestring) or not trial['app_args']:
+            results.append('trial %s should have a non-empty string for app_args' % trial)
+      except Exception as e:
+        results.append(
+            'JSON validation failed for %s. Error:\n%s' % (f.LocalPath(), e))
+
+  return [output_api.PresubmitError(r) for r in results]
+
+def _RunTests(input_api, output_api):
+  return input_api.RunTests(input_api.canned_checks.GetUnitTestsInDirectory(
+      input_api, output_api, '.', files_to_check=['v8_foozzie_test.py$']))
+
+def _CommonChecks(input_api, output_api):
+  """Checks common to both upload and commit."""
+  checks = [
+    _CheckTrialsConfig,
+    _RunTests,
+  ]
+
+  return sum([check(input_api, output_api) for check in checks], [])
+
 def CheckChangeOnCommit(input_api, output_api):
-  tests = input_api.canned_checks.GetUnitTestsInDirectory(
-      input_api, output_api, '.', files_to_check=['v8_foozzie_test.py$'])
-  return input_api.RunTests(tests)
+  results = []
+  results.extend(_CommonChecks(input_api, output_api))
+  return results
+
+def CheckChangeOnUpload(input_api, output_api):
+  results = []
+  results.extend(_CommonChecks(input_api, output_api))
+  return results
