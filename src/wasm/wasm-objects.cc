63454f1afb901f72fe4b53d74248e4b67c15748c
Clemens Backes <clemensb@chromium.org>
2022-02-01 12:30:54 +0100

    [wasm] Allocate tuples for function tables in old space
    We often allocate many of them (six digit numbers), and allocating them
    in new space causes a lot of GCs.
    This CL makes table initialization more than 30% faster for Photoshop,
    which again makes each Wasm instantiation ~20% faster.
    
    R=manoskouk@chromium.org
    
    Bug: v8:12593
    Change-Id: Ia913079b6412fa718b55c62681ba23751f3934e3
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3429204
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78888}

--- src/wasm/wasm-objects.cc
+++ src/wasm/wasm-objects.cc
@@ -624,9 +624,12 @@ void WasmTableObject::SetFunctionTablePlaceholder(
     Handle<WasmInstanceObject> instance, int func_index) {
   // Put (instance, func_index) as a Tuple2 into the entry_index.
   // The {WasmExportedFunction} will be created lazily.
+  // Allocate directly in old space as the tuples are typically long-lived, and
+  // we create many of them, which would result in lots of GC when initializing
+  // large tables.
   Handle<Tuple2> tuple = isolate->factory()->NewTuple2(
       instance, Handle<Smi>(Smi::FromInt(func_index), isolate),
-      AllocationType::kYoung);
+      AllocationType::kOld);
   table->entries().set(entry_index, *tuple);
 }
 
