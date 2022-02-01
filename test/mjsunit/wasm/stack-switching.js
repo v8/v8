cb12a3e45c3717a779e76c3180ed13ee27fae926
Thibaud Michaud <thibaudm@chromium.org>
2022-02-01 16:56:51 +0000

    Revert "Reland "[wasm] Resume suspender on resolved promise""
    This reverts commit f942f656dcfdbdeb8b7146ebce95146a220a3226.
    
    Reason for revert: Breaks gc-stress
    
    Original change's description:
    > Reland "[wasm] Resume suspender on resolved promise"
    >
    > This is a reland of a865d16bc27624ed63122b4239042d9f49d140b3
    >
    > Changes:
    > - Make the next ID atomic
    > - Leave more space for runtime calls in debug mode
    >
    > Original change's description:
    > > [wasm] Resume suspender on resolved promise
    > >
    > > Implement the WasmResume builtin, which resumes a wasm suspender
    > > when the corresponding JS promise resolves.
    > >
    > > Drive-by 1: Fix detection of empty stacks in the stack frame iterator.
    > > Drive-by 2: Add a stack ID for better tracing.
    > >
    > > R=ahaas@chromium.org
    > > CC=​fgm@chromium.org
    > >
    > > Bug: v8:12191
    > > Change-Id: Ifa3f00c4259f802292b04d426c739e9b551f87b9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3420827
    > > Reviewed-by: Andreas Haas <ahaas@chromium.org>
    > > Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    > > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#78842}
    >
    > Bug: v8:12191
    > Change-Id: I3c231690b27be79a0c00e13043342bb4a3628886
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3427203
    > Reviewed-by: Andreas Haas <ahaas@chromium.org>
    > Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#78890}
    
    Bug: v8:12191
    Change-Id: I5037419b6cee7a3bb49c1649e5a5d11a935a9b28
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Auto-submit: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3429500
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Thibaud Michaud <thibaudm@chromium.org>
    Owners-Override: Maya Lekova <mslekova@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#78893}

--- test/mjsunit/wasm/stack-switching.js
+++ test/mjsunit/wasm/stack-switching.js
@@ -48,49 +48,6 @@ load("test/mjsunit/wasm/wasm-module-builder.js");
   let wrapped_export = suspender.returnPromiseOnSuspend(instance.exports.test);
   let combined_promise = wrapped_export();
   assertEquals(0, instance.exports.g.value);
-  combined_promise.then(_ => assertEquals(42, instance.exports.g.value));
-})();
-
-// Check that we can suspend back out of a resumed computation.
-(function TestStackSwitchSuspendLoop() {
-  print(arguments.callee.name);
-  let builder = new WasmModuleBuilder();
-  builder.addGlobal(kWasmI32, true).exportAs('g');
-  import_index = builder.addImport('m', 'import', kSig_i_v);
-  // Pseudo-code for the wasm function:
-  // for (i = 0; i < 5; ++i) {
-  //   g = g + import();
-  // }
-  builder.addFunction("test", kSig_v_v)
-      .addLocals(kWasmI32, 1)
-      .addBody([
-          kExprI32Const, 5,
-          kExprLocalSet, 0,
-          kExprLoop, kWasmVoid,
-            kExprCallFunction, import_index, // suspend
-            kExprGlobalGet, 0, // resume
-            kExprI32Add,
-            kExprGlobalSet, 0,
-            kExprLocalGet, 0,
-            kExprI32Const, 1,
-            kExprI32Sub,
-            kExprLocalTee, 0,
-            kExprBrIf, 0,
-          kExprEnd,
-      ]).exportFunc();
-  let suspender = new WebAssembly.Suspender();
-  let i = 0;
-  // The n-th call to the import returns a promise that resolves to n.
-  function js_import() {
-    return new Promise((resolve) => { resolve(++i); });
-  };
-  let wasm_js_import = new WebAssembly.Function({parameters: [], results: ['i32']}, js_import);
-  let suspending_wasm_js_import = suspender.suspendOnReturnedPromise(wasm_js_import);
-  let instance = builder.instantiate({m: {import: suspending_wasm_js_import}});
-  let wrapped_export = suspender.returnPromiseOnSuspend(instance.exports.test);
-  let chained_promise = wrapped_export();
-  assertEquals(0, instance.exports.g.value);
-  chained_promise.then(_ => assertEquals(15, instance.exports.g.value));
 })();
 
 (function TestStackSwitchGC() {
