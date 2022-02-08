a94d12317a301886ecb384f84ae51c1b0041bc0e
Michael Achenbach <machenbach@chromium.org>
2022-02-08 11:13:48 +0100

    Merged: [infra] Testrunner fixes for Python3 compatibility
    Revision: 9e9d9142d64f81d88f4c95f20151d764be685f6a
    
    BUG=chromium:1292016
    R=liviurau@chromium.org
    
    Change-Id: Ia5913ab0532031b68b12b6cd7f92c1381401cba0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3447361
    Reviewed-by: Liviu Rau <liviurau@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/branch-heads/9.8@{#20}
    Cr-Branched-From: e218afa8473132b56a9e1532be7920dd130aeb7e-refs/heads/9.8.177@{#1}
    Cr-Branched-From: 86ebfc969cde382122a4d429f2380f06175ea2a8-refs/heads/main@{#78312}

--- tools/testrunner/outproc/base.py
+++ tools/testrunner/outproc/base.py
@@ -2,7 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import itertools
+try: # Python3
+  from itertools import zip_longest
+except ImportError: # Python2
+  from itertools import izip_longest as zip_longest
 
 from ..testproc.base import (
     DROP_RESULT, DROP_OUTPUT, DROP_PASS_OUTPUT, DROP_PASS_STDOUT)
@@ -146,7 +149,7 @@ class ExpectedOutProc(OutProc):
       expected_lines = f.readlines()
 
     for act_iterator in self._act_block_iterator(output):
-      for expected, actual in itertools.izip_longest(
+      for expected, actual in zip_longest(
           self._expected_iterator(expected_lines),
           act_iterator,
           fillvalue=''
