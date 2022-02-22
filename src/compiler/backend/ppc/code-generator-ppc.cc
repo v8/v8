ddc409cbd4800dcec9e9d67f3505d524c0391109
Milad Fa <mfarazma@redhat.com>
2022-02-22 16:44:10 -0500

    PPC/s390: [compiler] Always use full version of RecordWrite builtin
    Port 1b437aa87d59b6677dddff7727f1d62b75c53f78
    
    Original Commit Message:
    
        When we know that the value in a write barrier is a map, we know that
        we are not going to have an old-to-new reference (maps are always in
        old generation). Therefore we also don't really need the generational
        barrier in RecordWrite. While this is technically correct, we don't
        gain much from this optimization. The inline and out-of-line generated
        code for the barrier is still the same as in all other cases. Which
        means that outside marking we don't even reach the RecordWrite builtin.
        Most write barrier executions happen outside incremental marking, hence
        performance of the incremental marking barrier isn't critical. This CL
        always uses the full RecordWrite builtin using a flag in order to
        allow for an easy revert.
    
        This CL is motivated by the shared heap work, which needs an additional
        always-on barrier in the future (similar to OLD_TO_NEW) to keep a
        OLD_TO_SHARED remembered set up-to-date. While maps are always in the
        old generation, they maybe by located in the shared heap.
    
    R=dinfuehr@chromium.org, joransiu@ca.ibm.com, junyan@redhat.com, midawson@redhat.com
    BUG=
    LOG=N
    
    Change-Id: I4e763419b3b007a668073e1577cbff9127d15940
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3481263
    Reviewed-by: Joran Siu <joransiu@ca.ibm.com>
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Commit-Queue: Milad Farazmand <mfarazma@redhat.com>
    Cr-Commit-Position: refs/heads/main@{#79214}

--- src/compiler/backend/ppc/code-generator-ppc.cc
+++ src/compiler/backend/ppc/code-generator-ppc.cc
@@ -187,8 +187,10 @@ class OutOfLineRecordWrite final : public OutOfLineCode {
       __ add(scratch1_, object_, offset_);
     }
     RememberedSetAction const remembered_set_action =
-        mode_ > RecordWriteMode::kValueIsMap ? RememberedSetAction::kEmit
-                                             : RememberedSetAction::kOmit;
+        mode_ > RecordWriteMode::kValueIsMap ||
+                FLAG_use_full_record_write_builtin
+            ? RememberedSetAction::kEmit
+            : RememberedSetAction::kOmit;
     SaveFPRegsMode const save_fp_mode = frame()->DidAllocateDoubleRegisters()
                                             ? SaveFPRegsMode::kSave
                                             : SaveFPRegsMode::kIgnore;
