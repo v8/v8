7633fbc96c1a88000abc4be15e79567d2a4440ea
Vasili Skurydzin <vasili.skurydzin@ibm.com>
2022-03-15 10:19:42 -0700

    aix: Don't use ppc linux page size
    Change-Id: I2ff012f0da2c536d78e12c7b8f02fe2d28b7b7ea
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3526338
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Milad Farazmand <mfarazma@redhat.com>
    Commit-Queue: Vasili Skurydzin <vasili.skurydzin@ibm.com>
    Cr-Commit-Position: refs/heads/main@{#79501}

--- src/base/build_config.h
+++ src/base/build_config.h
@@ -237,9 +237,9 @@ constexpr int kReturnAddressStackSlotCount =
     V8_TARGET_ARCH_STORES_RETURN_ADDRESS_ON_STACK ? 1 : 0;
 
 // Number of bits to represent the page size for paged spaces.
-#if defined(V8_HOST_ARCH_PPC) || defined(V8_HOST_ARCH_PPC64)
-// Native PPC has large (64KB) physical pages.
-// Simulator needs to use the same value as x64.
+#if (defined(V8_HOST_ARCH_PPC) || defined(V8_HOST_ARCH_PPC64)) && !defined(_AIX)
+// Native PPC linux has large (64KB) physical pages.
+// Simulator (and Aix) need to use the same value as x64.
 const int kPageSizeBits = 19;
 #elif defined(ENABLE_HUGEPAGE)
 // When enabling huge pages, adjust V8 page size to take up exactly one huge
