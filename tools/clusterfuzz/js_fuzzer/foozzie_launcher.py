dfe465d1ba9480f9737779b98dd89bda30ea0ea2
Michael Achenbach <machenbach@chromium.org>
2022-04-04 10:22:26 +0200

    [foozzie] Switch wrapper to Py3
    No-Try: true
    Bug: chromium:1288926
    Change-Id: I05402b802f6d17dfea1211a682d3ed38f83e5aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3565722
    Auto-Submit: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Liviu Rau <liviurau@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#79726}

--- tools/clusterfuzz/js_fuzzer/foozzie_launcher.py
+++ tools/clusterfuzz/js_fuzzer/foozzie_launcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2020 the V8 project authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -7,6 +7,9 @@
 """
 Launcher for the foozzie differential-fuzzing harness. Wraps foozzie
 with Python2 for backwards-compatibility when bisecting.
+
+Obsolete now after switching to Python3 entirely. We keep the launcher
+for a transition period.
 """
 
 import os
@@ -15,13 +18,6 @@ import subprocess
 import sys
 
 if __name__ == '__main__':
-  # In some cases or older versions, the python executable is passed as
-  # first argument. Let's be robust either way, with or without full
-  # path or version.
-  if re.match(r'.*python.*', sys.argv[1]):
-    args = sys.argv[2:]
-  else:
-    args = sys.argv[1:]
-  process = subprocess.Popen(['python2'] + args)
+  process = subprocess.Popen(args)
   process.communicate()
   sys.exit(process.returncode)
