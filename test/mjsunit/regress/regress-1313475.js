0a1bf43d19b98484d6096ddc51165e8632dc0c08
Clemens Backes <clemensb@chromium.org>
2022-04-07 11:45:37 +0200

    [test] Remove flag that causes contradictions
    Some test variants and fuzzers set their own GC interval, so the flag
    specified in the regression test causes flag contradictions.
    The test failure was flaky anyway, so this change is only a slight
    reduction in reproducability, and the test will still be used as seed
    for the fuzzers.
    
    R=machenbach@chromium.org
    
    Bug: chromium:1313475
    Change-Id: I7c7084ab34fe46d691b841921d42a487cc8a1cad
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3576114
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#79845}

--- test/mjsunit/regress/regress-1313475.js
+++ test/mjsunit/regress/regress-1313475.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --expose-gc --invoke-weak-callbacks --random-gc-interval=2000
+// Flags: --expose-gc --invoke-weak-callbacks
 
 // We spawn a new worker which creates a Realm, then terminate the main thread
 // which will also terminate the worker.
