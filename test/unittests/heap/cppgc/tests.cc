3509f2da245c1ffc9fe56f3e08a89d543d954970
Leszek Swirski <leszeks@chromium.org>
2022-04-07 11:59:54 +0200

    [sandbox] Fix sandbox initialisation in cppgc unittests
    Bug: v8:12781
    Change-Id: I0df1c8a17482f13d210f7d2424a15e15c71108c0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3576115
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Samuel Groß <saelo@chromium.org>
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#79849}

--- test/unittests/heap/cppgc/tests.cc
+++ test/unittests/heap/cppgc/tests.cc
@@ -29,6 +29,9 @@ void TestWithPlatform::SetUpTestSuite() {
   // For non-standalone builds, we need to initialize V8's platform so that it
   // can be looked-up by trace-event.h.
   v8::V8::InitializePlatform(platform_->GetV8Platform());
+#ifdef V8_SANDBOX
+  CHECK(v8::V8::InitializeSandbox());
+#endif  // V8_SANDBOX
   v8::V8::Initialize();
 #endif  // !CPPGC_IS_STANDALONE
 }
