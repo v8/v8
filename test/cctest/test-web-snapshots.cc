ce488c0156feb88b52adb1f8d9066dd8ee70deaa
Marja Hölttä <marja@chromium.org>
2022-05-09 17:51:45 +0200

    Reland: [web snapshot] Recognize builtins
    Builtins are not snapshotted, but instead we insert "builtin wrappers"
    into the snapshot, and create references to the corresponding builtin
    when deserializing.
    
    Subclassing builtins will be implemented in a follow-up CL.
    
    First version: https://chromium-review.googlesource.com/c/v8/v8/+/3630080
    Fix: initialize builtin_objects_handle_
    
    Bug: v8:11525,v8:12820
    Change-Id: Ia2b5d41af5d7f577f1b02356b22a8760963009e4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3635718
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#80430}

--- test/cctest/test-web-snapshots.cc
+++ test/cctest/test-web-snapshots.cc
@@ -16,9 +16,9 @@ namespace {
 void TestWebSnapshotExtensive(
     const char* snapshot_source, const char* test_source,
     std::function<void(v8::Isolate*, v8::Local<v8::Context>)> tester,
-    uint32_t string_count, uint32_t symbol_count, uint32_t map_count,
-    uint32_t context_count, uint32_t function_count, uint32_t object_count,
-    uint32_t array_count) {
+    uint32_t string_count, uint32_t symbol_count, uint32_t builtin_object_count,
+    uint32_t map_count, uint32_t context_count, uint32_t function_count,
+    uint32_t object_count, uint32_t array_count) {
   CcTest::InitializeVM();
   v8::Isolate* isolate = CcTest::isolate();
 
@@ -40,6 +40,7 @@ void TestWebSnapshotExtensive(
     CHECK_EQ(string_count, serializer.string_count());
     CHECK_EQ(symbol_count, serializer.symbol_count());
     CHECK_EQ(map_count, serializer.map_count());
+    CHECK_EQ(builtin_object_count, serializer.builtin_object_count());
     CHECK_EQ(context_count, serializer.context_count());
     CHECK_EQ(function_count, serializer.function_count());
     CHECK_EQ(object_count, serializer.object_count());
@@ -58,6 +59,7 @@ void TestWebSnapshotExtensive(
     CHECK_EQ(string_count, deserializer.string_count());
     CHECK_EQ(symbol_count, deserializer.symbol_count());
     CHECK_EQ(map_count, deserializer.map_count());
+    CHECK_EQ(builtin_object_count, deserializer.builtin_object_count());
     CHECK_EQ(context_count, deserializer.context_count());
     CHECK_EQ(function_count, deserializer.function_count());
     CHECK_EQ(object_count, deserializer.object_count());
@@ -68,8 +70,9 @@ void TestWebSnapshotExtensive(
 void TestWebSnapshot(const char* snapshot_source, const char* test_source,
                      const char* expected_result, uint32_t string_count,
                      uint32_t symbol_count, uint32_t map_count,
-                     uint32_t context_count, uint32_t function_count,
-                     uint32_t object_count, uint32_t array_count) {
+                     uint32_t builtin_object_count, uint32_t context_count,
+                     uint32_t function_count, uint32_t object_count,
+                     uint32_t array_count) {
   TestWebSnapshotExtensive(
       snapshot_source, test_source,
       [test_source, expected_result](v8::Isolate* isolate,
@@ -77,8 +80,8 @@ void TestWebSnapshot(const char* snapshot_source, const char* test_source,
         v8::Local<v8::String> result = CompileRun(test_source).As<v8::String>();
         CHECK(result->Equals(new_context, v8_str(expected_result)).FromJust());
       },
-      string_count, symbol_count, map_count, context_count, function_count,
-      object_count, array_count);
+      string_count, symbol_count, map_count, builtin_object_count,
+      context_count, function_count, object_count, array_count);
 }
 
 }  // namespace
@@ -89,14 +92,15 @@ TEST(Minimal) {
   const char* expected_result = "lol";
   uint32_t kStringCount = 1;  // 'foo'; 'key' is in-place.
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
   uint32_t kObjectCount = 1;
   uint32_t kArrayCount = 0;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 TEST(EmptyObject) {
@@ -104,6 +108,7 @@ TEST(EmptyObject) {
   const char* test_source = "foo";
   uint32_t kStringCount = 1;  // 'foo'
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
@@ -118,8 +123,9 @@ TEST(EmptyObject) {
                  i_isolate->native_context()->object_function().initial_map());
       };
   TestWebSnapshotExtensive(snapshot_source, test_source, tester, kStringCount,
-                           kSymbolCount, kMapCount, kContextCount,
-                           kFunctionCount, kObjectCount, kArrayCount);
+                           kSymbolCount, kBuiltinObjectCount, kMapCount,
+                           kContextCount, kFunctionCount, kObjectCount,
+                           kArrayCount);
 }
 
 TEST(Numbers) {
@@ -134,6 +140,7 @@ TEST(Numbers) {
   const char* test_source = "foo";
   uint32_t kStringCount = 1;  // 'foo'; 'a'...'f' are in-place.
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
@@ -175,8 +182,9 @@ TEST(Numbers) {
         CHECK_EQ(f, -std::numeric_limits<double>::infinity());
       };
   TestWebSnapshotExtensive(snapshot_source, test_source, tester, kStringCount,
-                           kSymbolCount, kMapCount, kContextCount,
-                           kFunctionCount, kObjectCount, kArrayCount);
+                           kSymbolCount, kBuiltinObjectCount, kMapCount,
+                           kContextCount, kFunctionCount, kObjectCount,
+                           kArrayCount);
 }
 
 TEST(Oddballs) {
@@ -189,6 +197,7 @@ TEST(Oddballs) {
   const char* test_source = "foo";
   uint32_t kStringCount = 1;  // 'foo'; 'a'...'d' are in-place.
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
@@ -207,8 +216,9 @@ TEST(Oddballs) {
         CHECK(d->IsUndefined());
       };
   TestWebSnapshotExtensive(snapshot_source, test_source, tester, kStringCount,
-                           kSymbolCount, kMapCount, kContextCount,
-                           kFunctionCount, kObjectCount, kArrayCount);
+                           kSymbolCount, kBuiltinObjectCount, kMapCount,
+                           kContextCount, kFunctionCount, kObjectCount,
+                           kArrayCount);
 }
 
 TEST(Function) {
@@ -218,14 +228,15 @@ TEST(Function) {
   const char* expected_result = "11525";
   uint32_t kStringCount = 2;  // 'foo', function source code. 'key' is in-place.
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 1;
   uint32_t kObjectCount = 1;
   uint32_t kArrayCount = 0;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 TEST(InnerFunctionWithContext) {
@@ -240,14 +251,15 @@ TEST(InnerFunctionWithContext) {
   // Strings: 'foo', 'result', function source code (inner). 'key' is in-place.
   uint32_t kStringCount = 3;
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 1;
   uint32_t kFunctionCount = 1;
   uint32_t kObjectCount = 1;
   uint32_t kArrayCount = 0;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 TEST(InnerFunctionWithContextAndParentContext) {
@@ -268,14 +280,15 @@ TEST(InnerFunctionWithContextAndParentContext) {
   // Strings: 'foo', function source code (innerinner), 'part1', 'part2'.
   uint32_t kStringCount = 4;
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 2;
   uint32_t kFunctionCount = 1;
   uint32_t kObjectCount = 1;
   uint32_t kArrayCount = 0;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 TEST(RegExp) {
@@ -283,6 +296,7 @@ TEST(RegExp) {
   const char* test_source = "foo";
   uint32_t kStringCount = 3;  // 'foo', RegExp pattern, RegExp flags
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
@@ -305,8 +319,9 @@ TEST(RegExp) {
         CHECK(no_match->IsNull());
       };
   TestWebSnapshotExtensive(snapshot_source, test_source, tester, kStringCount,
-                           kSymbolCount, kMapCount, kContextCount,
-                           kFunctionCount, kObjectCount, kArrayCount);
+                           kSymbolCount, kBuiltinObjectCount, kMapCount,
+                           kContextCount, kFunctionCount, kObjectCount,
+                           kArrayCount);
 }
 
 TEST(RegExpNoFlags) {
@@ -314,6 +329,7 @@ TEST(RegExpNoFlags) {
   const char* test_source = "foo";
   uint32_t kStringCount = 3;  // 'foo', RegExp pattern, RegExp flags
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
@@ -336,8 +352,9 @@ TEST(RegExpNoFlags) {
         CHECK(no_match->IsNull());
       };
   TestWebSnapshotExtensive(snapshot_source, test_source, tester, kStringCount,
-                           kSymbolCount, kMapCount, kContextCount,
-                           kFunctionCount, kObjectCount, kArrayCount);
+                           kSymbolCount, kBuiltinObjectCount, kMapCount,
+                           kContextCount, kFunctionCount, kObjectCount,
+                           kArrayCount);
 }
 
 TEST(SFIDeduplication) {
@@ -704,6 +721,7 @@ TEST(FunctionKinds) {
   const char* test_source = "foo";
   uint32_t kStringCount = 2;  // 'foo', source code. 'a'...'f' in-place.
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 6;
@@ -727,8 +745,9 @@ TEST(FunctionKinds) {
                            FunctionKind::kAsyncGeneratorFunction);
       };
   TestWebSnapshotExtensive(snapshot_source, test_source, tester, kStringCount,
-                           kSymbolCount, kMapCount, kContextCount,
-                           kFunctionCount, kObjectCount, kArrayCount);
+                           kSymbolCount, kBuiltinObjectCount, kMapCount,
+                           kContextCount, kFunctionCount, kObjectCount,
+                           kArrayCount);
 }
 
 // Test that concatenating JS code to the snapshot works.
@@ -880,14 +899,15 @@ TEST(InPlaceStringsInArrays) {
   const char* expected_result = "onetwothree";
   uint32_t kStringCount = 1;  // 'foo'; Other strings are in-place.
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 0;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
   uint32_t kObjectCount = 0;
   uint32_t kArrayCount = 1;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 TEST(RepeatedInPlaceStringsInArrays) {
@@ -896,14 +916,15 @@ TEST(RepeatedInPlaceStringsInArrays) {
   const char* expected_result = "onetwoone";
   uint32_t kStringCount = 2;  // 'foo', 'one'; Other strings are in-place.
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 0;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
   uint32_t kObjectCount = 0;
   uint32_t kArrayCount = 1;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 TEST(InPlaceStringsInObjects) {
@@ -913,14 +934,15 @@ TEST(InPlaceStringsInObjects) {
   // 'foo'. Other strings are in-place.
   uint32_t kStringCount = 1;
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
   uint32_t kObjectCount = 1;
   uint32_t kArrayCount = 0;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 TEST(RepeatedInPlaceStringsInObjects) {
@@ -930,14 +952,52 @@ TEST(RepeatedInPlaceStringsInObjects) {
   // 'foo', 'one'. Other strings are in-place.
   uint32_t kStringCount = 2;
   uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 0;
   uint32_t kMapCount = 1;
   uint32_t kContextCount = 0;
   uint32_t kFunctionCount = 0;
   uint32_t kObjectCount = 1;
   uint32_t kArrayCount = 0;
   TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
-                  kSymbolCount, kMapCount, kContextCount, kFunctionCount,
-                  kObjectCount, kArrayCount);
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
+}
+
+TEST(builtin_objects) {
+  const char* snapshot_source = "var foo = {a: Error.prototype};";
+  const char* test_source = "foo.a == Error.prototype ? \"pass\" : \"fail\"";
+  const char* expected_result = "pass";
+  // 'foo', 'Error.prototype'. Other strings are in-place.
+  uint32_t kStringCount = 2;
+  uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 1;
+  uint32_t kMapCount = 1;
+  uint32_t kContextCount = 0;
+  uint32_t kFunctionCount = 0;
+  uint32_t kObjectCount = 1;
+  uint32_t kArrayCount = 0;
+  TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
+}
+
+TEST(builtin_objectsDeduplicated) {
+  const char* snapshot_source =
+      "var foo = {a: Error.prototype, b: Error.prototype}";
+  const char* test_source = "foo.a === Error.prototype ? \"pass\" : \"fail\"";
+  const char* expected_result = "pass";
+  // 'foo', 'Error.prototype'. Other strings are in-place.
+  uint32_t kStringCount = 2;
+  uint32_t kSymbolCount = 0;
+  uint32_t kBuiltinObjectCount = 1;
+  uint32_t kMapCount = 1;
+  uint32_t kContextCount = 0;
+  uint32_t kFunctionCount = 0;
+  uint32_t kObjectCount = 1;
+  uint32_t kArrayCount = 0;
+  TestWebSnapshot(snapshot_source, test_source, expected_result, kStringCount,
+                  kSymbolCount, kBuiltinObjectCount, kMapCount, kContextCount,
+                  kFunctionCount, kObjectCount, kArrayCount);
 }
 
 }  // namespace internal
