83f03eef26c3edb52056151f4cd62bf619b41cf8
Igor Sheludko <ishell@chromium.org>
2022-05-12 15:47:42 +0000

    Revert "Reland "[rwx][mac] Enable fast W^X on Apple Silicon (M1)""
    This reverts commit 411a015758911530eeb2919fbd68be0ecceb81de.
    
    Reason for revert: crbug.com/1324829 (Canary crashes)
    
    Original change's description:
    > Reland "[rwx][mac] Enable fast W^X on Apple Silicon (M1)"
    >
    > This is a reland of commit e8cac3776e3521eb8b5ac238a811083f78513ee8
    > Broken jitless mode is fixed in a separate CL.
    >
    > Original change's description:
    > > [rwx][mac] Enable fast W^X on Apple Silicon (M1)
    > >
    > > Bug: v8:12797
    > > Change-Id: I53bb803dd77db5bdd42b1a1b4b568e63857adf31
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3598861
    > > Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    > > Commit-Queue: Igor Sheludko <ishell@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#80396}
    >
    > Bug: v8:12797
    > Change-Id: I5436ceced5d4673f3b80d9cacdb3d6e84012c6e7
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3635720
    > Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    > Commit-Queue: Igor Sheludko <ishell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#80450}
    
    Bug: v8:12797
    Change-Id: If776ccb6ce9f41a84b94409ec5cae91c0915f129
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3644620
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#80496}

--- src/common/globals.h
+++ src/common/globals.h
@@ -188,7 +188,8 @@ using CodeT = Code;
 //
 #if V8_HAS_PTHREAD_JIT_WRITE_PROTECT && \
     !(defined(V8_COMPRESS_POINTERS) && !defined(V8_EXTERNAL_CODE_SPACE))
-#define V8_HEAP_USE_PTHREAD_JIT_WRITE_PROTECT true
+// TODO(v8:12797): enable fast W^X permissions switching on Apple Silicon.
+#define V8_HEAP_USE_PTHREAD_JIT_WRITE_PROTECT false
 #else
 #define V8_HEAP_USE_PTHREAD_JIT_WRITE_PROTECT false
 #endif
