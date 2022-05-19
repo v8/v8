ac3c8f8ff25cefae0deae5a2f93b7589225993dc
Jakob Kummerow <jkummerow@chromium.org>
2022-05-19 21:40:27 +0200

    [wasm-gc] Bump array.init limit to 10,000
    By popular demand.
    
    Bug: v8:7748
    Change-Id: I6892d5cb92066ecc56574b5f27a09088c692e071
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3650927
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#80655}

--- src/wasm/wasm-limits.h
+++ src/wasm/wasm-limits.h
@@ -58,7 +58,7 @@ constexpr size_t kV8MaxWasmMemories = 1;
 // GC proposal. These limits are not standardized yet.
 constexpr size_t kV8MaxWasmStructFields = 999;
 constexpr uint32_t kV8MaxRttSubtypingDepth = 31;
-constexpr size_t kV8MaxWasmArrayInitLength = 999;
+constexpr size_t kV8MaxWasmArrayInitLength = 10000;
 
 static_assert(kV8MaxWasmTableSize <= 4294967295,  // 2^32 - 1
               "v8 should not exceed WebAssembly's non-web embedding limits");
