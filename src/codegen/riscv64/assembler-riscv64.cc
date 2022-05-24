caa55eca659aaade58f72b25458b9e22621a7c54
Lu Yahan <yahan@iscas.ac.cn>
2022-05-24 09:48:56 +0800

    [riscv64] Implement emit_i8x16_relaxed_swizzle
    Change-Id: Ie6c88a0e371b2ce0bfad6ac887168e08fce26c05
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3662541
    Commit-Queue: ji qiu <qiuji@iscas.ac.cn>
    Auto-Submit: Yahan Lu <yahan@iscas.ac.cn>
    Reviewed-by: ji qiu <qiuji@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#80704}

--- src/codegen/riscv64/assembler-riscv64.cc
+++ src/codegen/riscv64/assembler-riscv64.cc
@@ -2913,7 +2913,6 @@ uint8_t vsew_switch(VSew vsew) {
 
 void Assembler::vl(VRegister vd, Register rs1, uint8_t lumop, VSew vsew,
                    MaskType mask) {
-  std::cout << "vl" << std::endl;
   uint8_t width = vsew_switch(vsew);
   GenInstrV(LOAD_FP, width, vd, rs1, lumop, mask, 0b00, 0, 0b000);
 }
