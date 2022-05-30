11a304ffeba4a9635ba71b90c273ff2f429e687d
Maya Lekova <mslekova@chromium.org>
2022-05-30 16:32:32 +0200

    [megadom] Fix crash in d8 when realm is entered
    Bug: chromium:1329234
    Change-Id: I59f171d3e2ab0c07f79f631971b1695b9f706600
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3677294
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Maya Lekova <mslekova@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#80850}

--- src/d8/d8.cc
+++ src/d8/d8.cc
@@ -3002,7 +3002,9 @@ void Shell::NodeTypeCallback(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
   // TODO(mslekova): Enable this once we have signature check in TF.
   PerIsolateData* data = PerIsolateData::Get(isolate);
-  CHECK(data->GetDomNodeCtor()->HasInstance(args.This()));
+  if (!data->GetDomNodeCtor()->HasInstance(args.This())) {
+    isolate->ThrowError("Calling .nodeType on wrong instance type.");
+  }
 
   args.GetReturnValue().Set(v8::Number::New(isolate, 1));
 }
