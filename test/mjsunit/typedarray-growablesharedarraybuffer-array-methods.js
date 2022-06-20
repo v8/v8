668a3e0eb5f5e33b111ca0519188ae82c2eaa33f
Marja Hölttä <marja@chromium.org>
2022-06-20 17:35:36 +0200

    [rab/gsab] Undo test splitting and make tests faster
    Part 1:
    Revert "PPC: skip slow tests on the ppc simulator"
    
    This reverts commit 9dfac00a1d1590ca66ef7084a1124223d0a59d62.
    
    Part 2:
    Make the slow test faster.
    
    Bug: v8:11111
    Change-Id: I8f0291098d29917fa65c4b5b28bf03cbdbe7ebc6
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3714229
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#81301}

---
+++ test/mjsunit/typedarray-growablesharedarraybuffer-array-methods.js
@@ -0,0 +1,157 @@
+// Copyright 2022 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Flags: --harmony-rab-gsab --allow-natives-syntax
+
+'use strict';
+
+d8.file.execute('test/mjsunit/typedarray-helpers.js');
+
+(function ArrayConcatDefault() {
+  for (let ctor of ctors) {
+    const gsab = CreateGrowableSharedArrayBuffer(4 * ctor.BYTES_PER_ELEMENT,
+                                                 8 * ctor.BYTES_PER_ELEMENT);
+    const lengthTracking = new ctor(gsab);
+
+    const taWrite = new ctor(gsab);
+    for (let i = 0; i < 4; ++i) {
+      WriteToTypedArray(taWrite, i, i + 1);
+    }
+
+    // Orig. array: [1, 2, 3, 4]
+    //              [1, 2, 3, 4, ...] << lengthTracking
+
+    function helper(receiver, ...params) {
+      return ToNumbers(Array.prototype.concat.call(receiver, ...params));
+    }
+
+    // TypedArrays aren't concat spreadable.
+    assertEquals([lengthTracking, 5, 6, 7],
+                 helper(lengthTracking, [5, 6], [7]));
+
+    // Resizing doesn't matter since the TA is added as a single item.
+    gsab.grow(6 * ctor.BYTES_PER_ELEMENT);
+    assertEquals([lengthTracking, 5, 6, 7],
+                 helper(lengthTracking, [5, 6], [7]));
+  }
+})();
+
+(function ArrayConcatConcatSpreadable() {
+  for (let ctor of ctors) {
+    const gsab = CreateGrowableSharedArrayBuffer(4 * ctor.BYTES_PER_ELEMENT,
+                                                 8 * ctor.BYTES_PER_ELEMENT);
+    const fixedLength = new ctor(gsab, 0, 4);
+    const fixedLengthWithOffset = new ctor(gsab, 2 * ctor.BYTES_PER_ELEMENT, 2);
+    const lengthTracking = new ctor(gsab, 0);
+    const lengthTrackingWithOffset = new ctor(gsab, 2 * ctor.BYTES_PER_ELEMENT);
+    fixedLength[Symbol.isConcatSpreadable] = true;
+    fixedLengthWithOffset[Symbol.isConcatSpreadable] = true;
+    lengthTracking[Symbol.isConcatSpreadable] = true;
+    lengthTrackingWithOffset[Symbol.isConcatSpreadable] = true;
+
+    const taWrite = new ctor(gsab);
+    for (let i = 0; i < 4; ++i) {
+      WriteToTypedArray(taWrite, i, i + 1);
+    }
+
+    // Orig. array: [1, 2, 3, 4]
+    //              [1, 2, 3, 4] << fixedLength
+    //                    [3, 4] << fixedLengthWithOffset
+    //              [1, 2, 3, 4, ...] << lengthTracking
+    //                    [3, 4, ...] << lengthTrackingWithOffset
+
+    function helper(receiver, ...params) {
+      return ToNumbers(Array.prototype.concat.call(receiver, ...params));
+    }
+
+    assertEquals([0, 1, 2, 3, 4, 5, 6], helper([0], fixedLength, [5, 6]));
+    assertEquals([0, 3, 4, 5, 6], helper([0], fixedLengthWithOffset, [5, 6]));
+    assertEquals([0, 1, 2, 3, 4, 5, 6], helper([0], lengthTracking, [5, 6]));
+    assertEquals([0, 3, 4, 5, 6],
+                 helper([0], lengthTrackingWithOffset, [5, 6]));
+
+    // Grow.
+    gsab.grow(6 * ctor.BYTES_PER_ELEMENT);
+    for (let i = 0; i < 6; ++i) {
+      WriteToTypedArray(taWrite, i, i + 1);
+    }
+    // Orig. array: [1, 2, 3, 4, 5, 6]
+    //              [1, 2, 3, 4] << fixedLength
+    //                    [3, 4] << fixedLengthWithOffset
+    //              [1, 2, 3, 4, 5, 6, ...] << lengthTracking
+    //                    [3, 4, 5, 6, ...] << lengthTrackingWithOffset
+
+    assertEquals([0, 1, 2, 3, 4, 7, 8], helper([0], fixedLength, [7, 8]));
+    assertEquals([0, 3, 4, 7, 8], helper([0], fixedLengthWithOffset, [7, 8]));
+    assertEquals([0, 1, 2, 3, 4, 5, 6, 7, 8],
+                 helper([0], lengthTracking, [7, 8]));
+    assertEquals([0, 3, 4, 5, 6, 7, 8],
+                 helper([0], lengthTrackingWithOffset, [7, 8]));
+  }
+})();
+
+// Hand-crafted test to hit a somewhat esoteric code path in Array.p.concat.
+(function ArrayConcatConcatDictionaryElementsProto() {
+  for (let ctor of ctors) {
+    const gsab = CreateGrowableSharedArrayBuffer(4 * ctor.BYTES_PER_ELEMENT,
+                                                 8 * ctor.BYTES_PER_ELEMENT);
+    const fixedLength = new ctor(gsab, 0, 4);
+    const fixedLengthWithOffset = new ctor(gsab, 2 * ctor.BYTES_PER_ELEMENT, 2);
+    const lengthTracking = new ctor(gsab, 0);
+    const lengthTrackingWithOffset = new ctor(gsab, 2 * ctor.BYTES_PER_ELEMENT);
+
+    const taWrite = new ctor(gsab);
+    for (let i = 0; i < 4; ++i) {
+      WriteToTypedArray(taWrite, i, i + 1);
+    }
+
+    // Orig. array: [1, 2, 3, 4]
+    //              [1, 2, 3, 4] << fixedLength
+    //                    [3, 4] << fixedLengthWithOffset
+    //              [1, 2, 3, 4, ...] << lengthTracking
+    //                    [3, 4, ...] << lengthTrackingWithOffset
+
+    const largeIndex = 5000;
+    function helper(ta) {
+      const newArray = [];
+      newArray[largeIndex] = 11111;  // Force dictionary mode.
+      assertTrue(%HasDictionaryElements(newArray));
+      newArray.__proto__ = ta;
+      return Array.prototype.concat.call([], newArray);
+    }
+
+    function assertArrayContents(expectedStart, array) {
+      for (let i = 0; i < expectedStart.length; ++i) {
+        assertEquals(expectedStart[i], Number(array[i]));
+      }
+      assertEquals(largeIndex + 1, array.length);
+      // Don't check every index to keep the test run time reasonable.
+      for (let i = expectedStart.length; i < largeIndex - 1; i += 153) {
+        assertEquals(undefined, array[i]);
+      }
+      assertEquals(11111, Number(array[largeIndex]));
+    }
+
+    assertArrayContents([1, 2, 3, 4], helper(fixedLength));
+    assertArrayContents([3, 4], helper(fixedLengthWithOffset));
+    assertArrayContents([1, 2, 3, 4], helper(lengthTracking));
+    assertArrayContents([3, 4], helper(lengthTrackingWithOffset));
+
+    // Grow.
+    gsab.grow(6 * ctor.BYTES_PER_ELEMENT);
+    for (let i = 0; i < 6; ++i) {
+      WriteToTypedArray(taWrite, i, i + 1);
+    }
+    // Orig. array: [1, 2, 3, 4, 5, 6]
+    //              [1, 2, 3, 4] << fixedLength
+    //                    [3, 4] << fixedLengthWithOffset
+    //              [1, 2, 3, 4, 5, 6, ...] << lengthTracking
+    //                    [3, 4, 5, 6, ...] << lengthTrackingWithOffset
+
+    assertArrayContents([1, 2, 3, 4], helper(fixedLength));
+    assertArrayContents([3, 4], helper(fixedLengthWithOffset));
+    assertArrayContents([1, 2, 3, 4, 5, 6], helper(lengthTracking));
+    assertArrayContents([3, 4, 5, 6], helper(lengthTrackingWithOffset));
+  }
+})();
