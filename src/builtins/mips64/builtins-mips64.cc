eea893d59417c76a26ebd8b776a7fcb450442138
Liu Yu <liuyu@loongson.cn>
2022-06-24 14:58:50 +0800

    [loong64][mips64][wasm][stack-switching] Support rejected promises
    Port commit e35039e7736c5aab4840f9e2c08da32ff26cc877
    
    Bug: v8:12191
    Change-Id: I1e6c49c22b3b94306d5b46e2672594cb842232d1
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3723159
    Reviewed-by: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Commit-Queue: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Auto-Submit: Liu Yu <liuyu@loongson.cn>
    Cr-Commit-Position: refs/heads/main@{#81347}

--- src/builtins/mips64/builtins-mips64.cc
+++ src/builtins/mips64/builtins-mips64.cc
@@ -2835,6 +2835,11 @@ void Builtins::Generate_WasmResume(MacroAssembler* masm) {
   __ Trap();
 }
 
+void Builtins::Generate_WasmReject(MacroAssembler* masm) {
+  // TODO(v8:12191): Implement for this platform.
+  __ Trap();
+}
+
 void Builtins::Generate_WasmOnStackReplace(MacroAssembler* masm) {
   // Only needed on x64.
   __ Trap();
