b243d5442161dc1e1eedbdcfd3dd6e4e0ba936fd
Maya Lekova <mslekova@chromium.org>
2022-06-30 11:59:07 +0000

    Revert "[foozzie] Silence a frequently occuring correctness bug"
    This reverts commit a618a4a341acabbd21a248a4cbea4f5dc484fc33.
    
    Reason for revert: Original CL got reverted, we don't need the suppression anymore.
    
    Original change's description:
    > [foozzie] Silence a frequently occuring correctness bug
    >
    > This CL adds back a patch of Math.pow for correctness fuzzing, which
    > drops some precision and hides a difference on the fast path.
    >
    > The same suppression was previously used on https://crbug.com/693426.
    >
    > No-Try: true
    > Bug: chromium:1339320
    > Change-Id: Id52f25f8a2b6b5aeca956587b16a10c61aa68e36
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3726295
    > Reviewed-by: Maya Lekova <mslekova@chromium.org>
    > Commit-Queue: Maya Lekova <mslekova@chromium.org>
    > Auto-Submit: Michael Achenbach <machenbach@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#81408}
    
    Bug: chromium:1339320
    Change-Id: Id4cf04f9480b3052978ee7ca3dd83d7ee16845c8
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3736446
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#81470}

--- tools/clusterfuzz/foozzie/v8_suppressions.js
+++ tools/clusterfuzz/foozzie/v8_suppressions.js
@@ -6,16 +6,3 @@
 // You can temporarily change JS behavior here to silence known problems.
 // Please refer to a bug in a comment and remove the suppression once the
 // problem is fixed.
-
-// Suppress https://crbug.com/1339320
-(function () {
-  var oldMathPow = Math.pow
-  Math.pow = function(a, b){
-    var s = "" + oldMathPow(a, b)
-    // Low tech precision mock. Limit digits in string representation.
-    // The phrases Infinity and NaN don't match the split("e").
-    s = s.split("e");
-    s[0] = s[0].substr(0, 15);
-    return parseFloat(s.join("e"));
-  }
-})();
