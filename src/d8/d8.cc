ec9fa4365d002e2e9ecdefed6563475eb730f5fd
Maya Lekova <mslekova@chromium.org>
2022-07-04 16:57:28 +0200

    [megadom] Turn an expensive check only in DEBUG
    This CL fixes a performance regression by conditionally compiling an
    expensive instance check only in debug mode.
    
    Bug: chromium:1329463
    Change-Id: I2cceb7c06fa6db967923047a4dee63a93a0b72a2
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3743385
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Auto-Submit: Maya Lekova <mslekova@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#81519}

--- src/d8/d8.cc
+++ src/d8/d8.cc
@@ -3028,11 +3028,15 @@ Local<String> Shell::Stringify(Isolate* isolate, Local<Value> value) {
 
 void Shell::NodeTypeCallback(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
-  // TODO(mslekova): Enable this once we have signature check in TF.
+
+  // HasInstance does a slow prototype chain lookup, and this function is used
+  // for micro benchmarks too.
+#ifdef DEBUG
   PerIsolateData* data = PerIsolateData::Get(isolate);
   if (!data->GetDomNodeCtor()->HasInstance(args.This())) {
     isolate->ThrowError("Calling .nodeType on wrong instance type.");
   }
+#endif
 
   args.GetReturnValue().Set(v8::Number::New(isolate, 1));
 }
