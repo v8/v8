17d248cfdb667e49e464d31f1ce0a6af1165f46d
Nikolaos Papaspyrou <nikolaos@chromium.org>
2022-07-25 14:00:14 +0200

    [heap] Improve tests for inner pointer resolution
    This CL refactors the mechanism for testing the implementation of inner
    pointer resolution using the marking bitmap. It allows for more than one
    page, where objects can be allocated. It also keeps a list of allocated
    objects that are automatically tested.
    
    Bug: v8:12851
    Change-Id: I470dc1154aca1ebc3d8526872717747829f83396
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3784605
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#81952}

--- test/unittests/heap/marking-inner-pointer-resolution-unittest.cc
+++ test/unittests/heap/marking-inner-pointer-resolution-unittest.cc
@@ -25,19 +25,17 @@ class InnerPointerResolutionTest : public TestWithIsolate {
     // intended.
     int index_in_cell = -1;
     enum { CONSECUTIVE, PAD_WHITE, PAD_BLACK } padding = CONSECUTIVE;
-    Address address = kNullAddress;  // The object's address is stored here.
+    // The id of the page on which the object was allocated and its address are
+    // stored here.
+    int page_id = -1;
+    Address address = kNullAddress;
   };
 
-  InnerPointerResolutionTest() {
-    OldSpace* old_space = heap()->old_space();
-    EXPECT_NE(nullptr, old_space);
-    page_ = allocator()->AllocatePage(MemoryAllocator::AllocationMode::kRegular,
-                                      old_space, NOT_EXECUTABLE);
-    EXPECT_NE(nullptr, page_);
-  }
+  InnerPointerResolutionTest() = default;
 
   ~InnerPointerResolutionTest() override {
-    allocator()->Free(MemoryAllocator::FreeMode::kImmediately, page_);
+    for (auto [id, page] : pages_)
+      allocator()->Free(MemoryAllocator::FreeMode::kImmediately, page);
   }
 
   InnerPointerResolutionTest(const InnerPointerResolutionTest&) = delete;
@@ -45,81 +43,116 @@ class InnerPointerResolutionTest : public TestWithIsolate {
       delete;
 
   Heap* heap() { return isolate()->heap(); }
-  Page* page() { return page_; }
   MemoryAllocator* allocator() { return heap()->memory_allocator(); }
   MarkCompactCollector* collector() { return heap()->mark_compact_collector(); }
 
-  // Creates a list of objects in the page and ensures that the page is
-  // iterable.
-  void CreateObjects(std::vector<ObjectRequest>& objects) {
-    Address ptr = page()->area_start();
-    for (size_t i = 0; i < objects.size(); ++i) {
-      CHECK_EQ(0, objects[i].size % kTaggedSize);
+  // Create, free and lookup pages.
+
+  int CreatePage() {
+    OldSpace* old_space = heap()->old_space();
+    DCHECK_NE(nullptr, old_space);
+    auto* page = allocator()->AllocatePage(
+        MemoryAllocator::AllocationMode::kRegular, old_space, NOT_EXECUTABLE);
+    EXPECT_NE(nullptr, page);
+    int page_id = next_page_id_++;
+    DCHECK_EQ(pages_.end(), pages_.find(page_id));
+    pages_[page_id] = page;
+    return page_id;
+  }
+
+  void FreePage(int page_id) {
+    DCHECK_LE(0, page_id);
+    auto it = pages_.find(page_id);
+    DCHECK_NE(pages_.end(), it);
+    allocator()->Free(MemoryAllocator::FreeMode::kImmediately, it->second);
+    pages_.erase(it);
+  }
+
+  Page* LookupPage(int page_id) {
+    DCHECK_LE(0, page_id);
+    auto it = pages_.find(page_id);
+    DCHECK_NE(pages_.end(), it);
+    return it->second;
+  }
+
+  // Creates a list of objects in a page and ensures that the page is iterable.
+  void CreateObjectsInPage(const std::vector<ObjectRequest>& objects) {
+    int page_id = CreatePage();
+    Page* page = LookupPage(page_id);
+    Address ptr = page->area_start();
+    for (auto object : objects) {
+      DCHECK_EQ(0, object.size % kTaggedSize);
 
       // Check if padding is needed.
-      const uint32_t index = page()->AddressToMarkbitIndex(ptr);
-      const int index_in_cell = Bitmap::IndexInCell(index);
-      if (objects[i].index_in_cell < 0) {
-        objects[i].index_in_cell = index_in_cell;
-      } else if (objects[i].padding != ObjectRequest::CONSECUTIVE) {
-        DCHECK_LE(0, objects[i].index_in_cell);
-        DCHECK_GT(Bitmap::kBitsPerCell, objects[i].index_in_cell);
-        const bool black = objects[i].padding == ObjectRequest::PAD_BLACK;
-        objects[i].padding = ObjectRequest::CONSECUTIVE;
+      int index_in_cell = Bitmap::IndexInCell(page->AddressToMarkbitIndex(ptr));
+      if (object.index_in_cell < 0) {
+        object.index_in_cell = index_in_cell;
+      } else if (object.padding != ObjectRequest::CONSECUTIVE) {
+        DCHECK_LE(0, object.index_in_cell);
+        DCHECK_GT(Bitmap::kBitsPerCell, object.index_in_cell);
         const int needed_padding_size =
-            ((Bitmap::kBitsPerCell + objects[i].index_in_cell - index_in_cell) %
+            ((Bitmap::kBitsPerCell + object.index_in_cell - index_in_cell) %
              Bitmap::kBitsPerCell) *
             Bitmap::kBytesPerCell;
         if (needed_padding_size > 0) {
-          ObjectRequest pad{
-              needed_padding_size,
-              ObjectRequest::FREE,
-              black ? ObjectRequest::BLACK_AREA : ObjectRequest::WHITE,
-              index_in_cell,
-              ObjectRequest::CONSECUTIVE,
-              ptr};
-          objects.insert(objects.begin() + i, pad);
-          CreateObject(pad);
+          ObjectRequest pad{needed_padding_size,
+                            ObjectRequest::FREE,
+                            object.padding == ObjectRequest::PAD_BLACK
+                                ? ObjectRequest::BLACK_AREA
+                                : ObjectRequest::WHITE,
+                            index_in_cell,
+                            ObjectRequest::CONSECUTIVE,
+                            page_id,
+                            ptr};
           ptr += needed_padding_size;
-          continue;
+          DCHECK_LE(ptr, page->area_end());
+          CreateObject(pad);
+          index_in_cell = Bitmap::IndexInCell(page->AddressToMarkbitIndex(ptr));
         }
       }
 
       // This will fail if the marking bitmap's implementation parameters change
       // (e.g., Bitmap::kBitsPerCell) or the size of the page header changes.
       // In this case, the tests will need to be revised accordingly.
-      EXPECT_EQ(index_in_cell, objects[i].index_in_cell);
+      EXPECT_EQ(index_in_cell, object.index_in_cell);
 
-      objects[i].address = ptr;
-      CreateObject(objects[i]);
-      ptr += objects[i].size;
+      object.page_id = page_id;
+      object.address = ptr;
+      ptr += object.size;
+      DCHECK_LE(ptr, page->area_end());
+      CreateObject(object);
     }
 
     // Create one last object that uses the remaining space on the page; this
     // simulates freeing the page's LAB.
-    const int remaining_size = static_cast<int>(page_->area_end() - ptr);
-    const uint32_t index = page()->AddressToMarkbitIndex(ptr);
+    const int remaining_size = static_cast<int>(page->area_end() - ptr);
+    const uint32_t index = page->AddressToMarkbitIndex(ptr);
     const int index_in_cell = Bitmap::IndexInCell(index);
-    ObjectRequest last{
-        remaining_size, ObjectRequest::FREE,        ObjectRequest::WHITE,
-        index_in_cell,  ObjectRequest::CONSECUTIVE, ptr};
-    objects.push_back(last);
+    ObjectRequest last{remaining_size,
+                       ObjectRequest::FREE,
+                       ObjectRequest::WHITE,
+                       index_in_cell,
+                       ObjectRequest::CONSECUTIVE,
+                       page_id,
+                       ptr};
     CreateObject(last);
   }
 
   void CreateObject(const ObjectRequest& object) {
+    objects_.push_back(object);
+
     // "Allocate" (i.e., manually place) the object in the page, set the map
     // and the size.
     switch (object.type) {
       case ObjectRequest::REGULAR: {
-        CHECK_LE(2 * kTaggedSize, object.size);
+        DCHECK_LE(2 * kTaggedSize, object.size);
         ReadOnlyRoots roots(heap());
         HeapObject heap_object(HeapObject::FromAddress(object.address));
         heap_object.set_map_after_allocation(roots.unchecked_fixed_array_map(),
                                              SKIP_WRITE_BARRIER);
         FixedArray arr(FixedArray::cast(heap_object));
         arr.set_length((object.size - FixedArray::SizeFor(0)) / kTaggedSize);
-        CHECK_EQ(object.size, arr.AllocatedSize());
+        DCHECK_EQ(object.size, arr.AllocatedSize());
         break;
       }
       case ObjectRequest::FREE:
@@ -136,22 +169,24 @@ class InnerPointerResolutionTest : public TestWithIsolate {
             HeapObject::FromAddress(object.address));
         break;
       case ObjectRequest::BLACK:
-        CHECK_LE(2 * kTaggedSize, object.size);
+        DCHECK_LE(2 * kTaggedSize, object.size);
         collector()->marking_state()->WhiteToBlack(
             HeapObject::FromAddress(object.address));
         break;
-      case ObjectRequest::BLACK_AREA:
-        collector()->marking_state()->bitmap(page_)->SetRange(
-            page_->AddressToMarkbitIndex(object.address),
-            page_->AddressToMarkbitIndex(object.address + object.size));
+      case ObjectRequest::BLACK_AREA: {
+        Page* page = LookupPage(object.page_id);
+        collector()->marking_state()->bitmap(page)->SetRange(
+            page->AddressToMarkbitIndex(object.address),
+            page->AddressToMarkbitIndex(object.address + object.size));
         break;
+      }
     }
   }
 
   // This must be called with a created object and an offset inside it.
   void RunTestInside(const ObjectRequest& object, int offset) {
-    CHECK_LE(0, offset);
-    CHECK_GT(object.size, offset);
+    DCHECK_LE(0, offset);
+    DCHECK_GT(object.size, offset);
     Address base_ptr =
         collector()->FindBasePtrForMarking(object.address + offset);
     if (object.type == ObjectRequest::FREE ||
@@ -165,40 +200,43 @@ class InnerPointerResolutionTest : public TestWithIsolate {
 
   // This must be called with an address not contained in any created object.
   void RunTestOutside(Address ptr) {
-    CHECK(!page()->Contains(ptr));
     Address base_ptr = collector()->FindBasePtrForMarking(ptr);
     EXPECT_EQ(kNullAddress, base_ptr);
   }
 
-  void TestWith(std::vector<ObjectRequest> objects) {
-    CreateObjects(objects);
-    for (auto object : objects) {
+  void TestAll() {
+    for (auto object : objects_) {
       RunTestInside(object, 0);
       RunTestInside(object, 1);
       RunTestInside(object, object.size / 2);
       RunTestInside(object, object.size - 1);
     }
-    const Address outside_ptr = page()->area_start() - 42;
-    CHECK_LE(page()->address(), outside_ptr);
-    RunTestOutside(outside_ptr);
+    for (auto [id, page] : pages_) {
+      const Address outside_ptr = page->area_start() - 42;
+      DCHECK_LE(page->address(), outside_ptr);
+      RunTestOutside(outside_ptr);
+    }
   }
 
  private:
-  Page* page_;
+  std::map<int, Page*> pages_;
+  int next_page_id_ = 0;
+  std::vector<ObjectRequest> objects_;
 };
 
 }  // namespace
 
 TEST_F(InnerPointerResolutionTest, EmptyPage) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({});
+  CreateObjectsInPage({});
+  TestAll();
 }
 
-// Tests with some objects layed out randomly.
+// Tests with some objects laid out randomly.
 
 TEST_F(InnerPointerResolutionTest, NothingMarked) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {64},
       {48},
       {52},
@@ -209,11 +247,12 @@ TEST_F(InnerPointerResolutionTest, NothingMarked) {
       {8},
       {42176},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, AllMarked) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {64, ObjectRequest::REGULAR, ObjectRequest::BLACK},
       {48, ObjectRequest::REGULAR, ObjectRequest::GREY},
       {52, ObjectRequest::REGULAR, ObjectRequest::BLACK},
@@ -224,11 +263,12 @@ TEST_F(InnerPointerResolutionTest, AllMarked) {
       {8, ObjectRequest::REGULAR, ObjectRequest::BLACK},
       {42176, ObjectRequest::REGULAR, ObjectRequest::BLACK},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, SomeMarked) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE},
       {48, ObjectRequest::REGULAR, ObjectRequest::WHITE},
       {52, ObjectRequest::REGULAR, ObjectRequest::BLACK},
@@ -239,11 +279,12 @@ TEST_F(InnerPointerResolutionTest, SomeMarked) {
       {8, ObjectRequest::REGULAR, ObjectRequest::WHITE},
       {42176, ObjectRequest::REGULAR, ObjectRequest::GREY},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, BlackAreas) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE},
       {48, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA},
       {52, ObjectRequest::REGULAR, ObjectRequest::BLACK},
@@ -254,13 +295,14 @@ TEST_F(InnerPointerResolutionTest, BlackAreas) {
       {8, ObjectRequest::REGULAR, ObjectRequest::WHITE},
       {42176, ObjectRequest::REGULAR, ObjectRequest::GREY},
   });
+  TestAll();
 }
 
 // Tests with specific object layout, to cover interesting and corner cases.
 
 TEST_F(InnerPointerResolutionTest, ThreeMarkedObjectsInSameCell) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       // Some initial large unmarked object, followed by a small marked object
       // towards the end of the cell.
       {512},
@@ -275,11 +317,12 @@ TEST_F(InnerPointerResolutionTest, ThreeMarkedObjectsInSameCell) {
       {64, ObjectRequest::REGULAR, ObjectRequest::BLACK, 17,
        ObjectRequest::PAD_WHITE},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, ThreeBlackAreasInSameCell) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       // Some initial large unmarked object, followed by a small black area
       // towards the end of the cell.
       {512},
@@ -294,78 +337,86 @@ TEST_F(InnerPointerResolutionTest, ThreeBlackAreasInSameCell) {
       {64, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA, 17,
        ObjectRequest::PAD_WHITE},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, SmallBlackAreaAtPageStart) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE, 30,
        ObjectRequest::PAD_BLACK},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, SmallBlackAreaAtPageStartUntilCellBoundary) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {8, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA},
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE, 0,
        ObjectRequest::PAD_BLACK},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, LargeBlackAreaAtPageStart) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {42 * Bitmap::kBitsPerCell * Bitmap::kBytesPerCell,
        ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA},
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE, 30,
        ObjectRequest::PAD_BLACK},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, LargeBlackAreaAtPageStartUntilCellBoundary) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {42 * Bitmap::kBitsPerCell * Bitmap::kBytesPerCell,
        ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA},
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE, 0,
        ObjectRequest::PAD_BLACK},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, SmallBlackAreaStartingAtCellBoundary) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {512},
       {20, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA, 0,
        ObjectRequest::PAD_WHITE},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, LargeBlackAreaStartingAtCellBoundary) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {512},
       {42 * Bitmap::kBitsPerCell * Bitmap::kBytesPerCell + 64,
        ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA, 0,
        ObjectRequest::PAD_WHITE},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, SmallBlackAreaEndingAtCellBoundary) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {512},
       {8, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA, 13,
        ObjectRequest::PAD_WHITE},
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE, 0,
        ObjectRequest::PAD_BLACK},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, LargeBlackAreaEndingAtCellBoundary) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {512},
       {42 * Bitmap::kBitsPerCell * Bitmap::kBytesPerCell + 64,
        ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA, 0,
@@ -373,10 +424,12 @@ TEST_F(InnerPointerResolutionTest, LargeBlackAreaEndingAtCellBoundary) {
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE, 0,
        ObjectRequest::PAD_BLACK},
   });
+  TestAll();
 }
+
 TEST_F(InnerPointerResolutionTest, TwoSmallBlackAreasAtCellBoundaries) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {512},
       {24, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA, 0,
        ObjectRequest::PAD_WHITE},
@@ -385,25 +438,52 @@ TEST_F(InnerPointerResolutionTest, TwoSmallBlackAreasAtCellBoundaries) {
       {64, ObjectRequest::REGULAR, ObjectRequest::WHITE, 0,
        ObjectRequest::PAD_BLACK},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, BlackAreaOfOneCell) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {512},
       {Bitmap::kBitsPerCell * Bitmap::kBytesPerCell, ObjectRequest::REGULAR,
        ObjectRequest::BLACK_AREA, 0, ObjectRequest::PAD_WHITE},
   });
+  TestAll();
 }
 
 TEST_F(InnerPointerResolutionTest, BlackAreaOfManyCells) {
   if (FLAG_enable_third_party_heap) return;
-  TestWith({
+  CreateObjectsInPage({
       {512},
       {17 * Bitmap::kBitsPerCell * Bitmap::kBytesPerCell,
        ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA, 0,
        ObjectRequest::PAD_WHITE},
   });
+  TestAll();
+}
+
+// Test with more pages.
+
+TEST_F(InnerPointerResolutionTest, TwoPages) {
+  if (FLAG_enable_third_party_heap) return;
+  CreateObjectsInPage({
+      {64, ObjectRequest::REGULAR, ObjectRequest::WHITE},
+      {52, ObjectRequest::REGULAR, ObjectRequest::BLACK},
+      {512, ObjectRequest::REGULAR, ObjectRequest::WHITE},
+      {60, ObjectRequest::REGULAR, ObjectRequest::BLACK},
+      {42176, ObjectRequest::REGULAR, ObjectRequest::GREY},
+  });
+  CreateObjectsInPage({
+      {512, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA},
+      {64, ObjectRequest::REGULAR, ObjectRequest::WHITE},
+      {48, ObjectRequest::REGULAR, ObjectRequest::BLACK_AREA},
+      {52, ObjectRequest::REGULAR, ObjectRequest::BLACK},
+      {4, ObjectRequest::FREE, ObjectRequest::GREY},
+      {8, ObjectRequest::FREE, ObjectRequest::GREY},
+      {8, ObjectRequest::REGULAR, ObjectRequest::WHITE},
+      {60, ObjectRequest::REGULAR, ObjectRequest::BLACK},
+  });
+  TestAll();
 }
 
 #endif  // V8_ENABLE_INNER_POINTER_RESOLUTION_MB
