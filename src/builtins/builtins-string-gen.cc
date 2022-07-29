c0690fa8f04b6f78001140602c80d22fb9ad79f2
Joyee Cheung <joyee@igalia.com>
2022-07-29 18:35:23 +0800

    Reland "[heap] pre-populate the single_character_string_cache"
    This is a reland of commit 07e11a64e4394de038722be84bea3ec51812ce24.
    
    The original change removed the fill_thehole_and_call_runtime bailout
    in StringBuiltinsAssembler::StringToArray() so when the string
    is external and cannot be unpacked, the FixedArray won't be filled
    with holes before we call into the runtime, thus failing a
    heap verification if a GC happens before the array is filled. This
    reland adds back the bailout for this case.
    
    Bug: v8:12718, chromium:1330410
    
    Original change's description:
    > [heap] pre-populate the single_character_string_cache
    >
    > This simplifies the code and removes the runtime overhead of
    > spontaneously adding strings to the cache.
    >
    > Bug: v8:12718
    > Change-Id: I2ed49bd82e3baf2563eeb8f463be72c0308c52c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3616553
    > Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    > Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    > Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    > Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    > Commit-Queue: Joyee Cheung <joyee@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#80803}
    
    Change-Id: I25e8724d511a8d0d971fa2a9b6ba8a0eafce4413
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3793525
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    Commit-Queue: Joyee Cheung <joyee@igalia.com>
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#82082}

--- src/builtins/builtins-string-gen.cc
+++ src/builtins/builtins-string-gen.cc
@@ -1269,7 +1269,7 @@ TNode<JSArray> StringBuiltinsAssembler::StringToArray(
     TNode<RawPtrT> string_data =
         to_direct.PointerToData(&fill_thehole_and_call_runtime);
     TNode<IntPtrT> string_data_offset = to_direct.offset();
-    TNode<FixedArray> cache = SingleCharacterStringCacheConstant();
+    TNode<FixedArray> cache = SingleCharacterStringTableConstant();
 
     BuildFastLoop<IntPtrT>(
         IntPtrConstant(0), length,
@@ -1285,9 +1285,7 @@ TNode<JSArray> StringBuiltinsAssembler::StringToArray(
           TNode<UintPtrT> code_index = ChangeUint32ToWord(char_code);
           TNode<Object> entry = LoadFixedArrayElement(cache, code_index);
 
-          // If we cannot find a char in the cache, fill the hole for the fixed
-          // array, and call runtime.
-          GotoIf(IsUndefined(entry), &fill_thehole_and_call_runtime);
+          CSA_DCHECK(this, Word32BinaryNot(IsUndefined(entry)));
 
           StoreFixedArrayElement(elements, index, entry);
         },
