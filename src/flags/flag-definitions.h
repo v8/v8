f1a4104ff9ad4e613c8f42695bbbcdaac59c071f
Paolo Severini <paolosev@microsoft.com>
2022-08-05 13:07:02 -0700

    Re-enable inlining of JS->Wasm calls by default
    Bug: v8:11092
    Change-Id: Ic0f30c35dfc4acda286c157722eb794a0aa4a360
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3814072
    Commit-Queue: Paolo Severini <paolosev@microsoft.com>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#82239}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -963,8 +963,7 @@ DEFINE_BOOL(
 DEFINE_BOOL(turbo_fast_api_calls, true, "enable fast API calls from TurboFan")
 DEFINE_BOOL(turbo_compress_translation_arrays, false,
             "compress translation arrays (experimental)")
-DEFINE_WEAK_IMPLICATION(future, turbo_inline_js_wasm_calls)
-DEFINE_BOOL(turbo_inline_js_wasm_calls, false, "inline JS->Wasm calls")
+DEFINE_BOOL(turbo_inline_js_wasm_calls, true, "inline JS->Wasm calls")
 DEFINE_BOOL(turbo_use_mid_tier_regalloc_for_huge_functions, true,
             "fall back to the mid-tier register allocator for huge functions")
 DEFINE_BOOL(turbo_force_mid_tier_regalloc, false,
