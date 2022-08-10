12099544c82261765981e272de80a8beadd4a6f1
Jakob Kummerow <jkummerow@chromium.org>
2022-08-10 12:20:51 +0200

    [stringrefs] Slightly improve string.concat performance
    The WasmStringConcat builtin delegates all the work to yet another
    builtin (StringAdd_CheckNone); so from optimized code we might as
    well call the latter directly.
    
    Bug: v8:12868
    Change-Id: I3876ce1d6341befac5d49ba02c4af47637ad4ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3823124
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#82436}

--- src/compiler/wasm-compiler.cc
+++ src/compiler/wasm-compiler.cc
@@ -5844,8 +5844,9 @@ Node* WasmGraphBuilder::StringConcat(Node* head, CheckForNull head_null_check,
                                      wasm::WasmCodePosition position) {
   if (head_null_check == kWithNullCheck) head = AssertNotNull(head, position);
   if (tail_null_check == kWithNullCheck) tail = AssertNotNull(tail, position);
-  return gasm_->CallBuiltin(Builtin::kWasmStringConcat, Operator::kNoDeopt,
-                            head, tail);
+  return gasm_->CallBuiltin(
+      Builtin::kStringAdd_CheckNone, Operator::kEliminatable, head, tail,
+      LOAD_INSTANCE_FIELD(NativeContext, MachineType::TaggedPointer()));
 }
 
 Node* WasmGraphBuilder::StringEqual(Node* a, CheckForNull a_null_check, Node* b,
