8d76f6d7cb2f4582cd42291f634f65610192c174
Clemens Backes <clemensb@chromium.org>
2022-08-12 15:41:37 +0200

    [flags] Reduce alignment to 4kB on Android
    The previous CL (https://crrev.com/c/3810187) caused a 86.9kB binary
    size regression on arm64 Android.
    The 64kB alignment is actually only needed on Linux, as Android
    always uses 4kB. This reduces the binary size by 98.3kB in a local test,
    resulting in a 11.4kB reduction overall (over both patches).
    
    R=cbruni@chromium.org
    
    Bug: v8:12887
    Change-Id: If0c44d1b447e6982876a6ced8680b904613e39d5
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3829473
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#82503}

--- src/base/build_config.h
+++ src/base/build_config.h
@@ -73,8 +73,9 @@ constexpr int kPageSizeBits = 18;
 #if defined(V8_OS_MACOS) && defined(V8_HOST_ARCH_ARM64)
 // MacOS on arm64 uses 16kB pages.
 constexpr int kMinimumOSPageSize = 16 * 1024;
-#elif defined(V8_OS_LINUX) && defined(V8_HOST_ARCH_ARM64)
-// Linux on arm64 can be configured for up to 64kB pages.
+#elif defined(V8_OS_LINUX) && !defined(V8_OS_ANDROID) && \
+    defined(V8_HOST_ARCH_ARM64)
+// Linux on arm64 (excluding android) can be configured for up to 64kB pages.
 constexpr int kMinimumOSPageSize = 64 * 1024;
 #else
 // Everything else uses 4kB pages.
