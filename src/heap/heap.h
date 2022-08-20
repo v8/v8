4898cd54da29ec392b81f51f9ab7033fa5a3e149
Dominik Inführ <dinfuehr@chromium.org>
2022-08-20 19:14:10 +0200

    Reland "[heap] Ensure all old-to-shared slots are recorded"
    This is a reland of commit c3a5c5b1e00707890a2b3deb59efcfedd3dd377e
    
    The previous CL was writing into the wrong sets when invoking
    CollectSlots<OLD_TO_SHARED>(). Also move the NULL checks out of
    that condition to also check this for chunks in the young generation.
    
    Original change's description:
    > [heap] Ensure all old-to-shared slots are recorded
    >
    > This CL adds verification of the old-to-shared remembered set to
    > --verify-heap. During shared GCs client heaps will be scanned for
    > references into the shared heap, this CL will CHECK that every found
    > slot is contained in the old-to-shared remembered set. After this
    > gets a bit more stable, the full heap iteration can be dropped and we
    > can fully rely on the remembered set instead.
    >
    > Bug: v8:11708
    > Change-Id: I0b5c4edfe3271306e4e7af7394472534113e1953
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3792605
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#82578}
    
    Bug: v8:11708
    Change-Id: I24b7787977f06708efb7a017dd1ec72f78d0ea13
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3841570
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#82653}

--- src/heap/heap.h
+++ src/heap/heap.h
@@ -1261,6 +1261,9 @@ class Heap {
   // spaces.
   V8_EXPORT_PRIVATE bool ContainsCode(HeapObject value) const;
 
+  // Checks whether object resides in the non-read-only shared heap.
+  static inline bool InSharedWritableHeap(MaybeObject object);
+
   // Checks whether an address/object is in the non-read-only heap (including
   // auxiliary area and unused area). Use IsValidHeapObject if checking both
   // heaps is required.
