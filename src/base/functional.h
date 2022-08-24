d75a0eed1cc9256466f078c06edfd25cef753be1
Tobias Tebbi <tebbi@chromium.org>
2022-08-24 19:21:58 +0200

    [base] allow inlining of hash_combine to improve performance
    This improved Turboshaft value numbering performance
    significantly in local testing.
    
    Change-Id: I8b8c8e3a67da9b83b57efc34c08ca693e84d9076
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3852486
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Tobias Tebbi <tebbi@chromium.org>
    Auto-Submit: Tobias Tebbi <tebbi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#82708}

--- src/base/functional.h
+++ src/base/functional.h
@@ -15,6 +15,7 @@
 #include <utility>
 
 #include "src/base/base-export.h"
+#include "src/base/bits.h"
 #include "src/base/macros.h"
 
 namespace v8 {
@@ -66,10 +67,80 @@ namespace base {
 template <typename>
 struct hash;
 
+// Thomas Wang, Integer Hash Functions.
+// https://gist.github.com/badboy/6267743
+template <typename T>
+V8_INLINE size_t hash_value_unsigned_impl(T v) {
+  switch (sizeof(T)) {
+    case 4: {
+      // "32 bit Mix Functions"
+      v = ~v + (v << 15);  // v = (v << 15) - v - 1;
+      v = v ^ (v >> 12);
+      v = v + (v << 2);
+      v = v ^ (v >> 4);
+      v = v * 2057;  // v = (v + (v << 3)) + (v << 11);
+      v = v ^ (v >> 16);
+      return static_cast<size_t>(v);
+    }
+    case 8: {
+      switch (sizeof(size_t)) {
+        case 4: {
+          // "64 bit to 32 bit Hash Functions"
+          v = ~v + (v << 18);  // v = (v << 18) - v - 1;
+          v = v ^ (v >> 31);
+          v = v * 21;  // v = (v + (v << 2)) + (v << 4);
+          v = v ^ (v >> 11);
+          v = v + (v << 6);
+          v = v ^ (v >> 22);
+          return static_cast<size_t>(v);
+        }
+        case 8: {
+          // "64 bit Mix Functions"
+          v = ~v + (v << 21);  // v = (v << 21) - v - 1;
+          v = v ^ (v >> 24);
+          v = (v + (v << 3)) + (v << 8);  // v * 265
+          v = v ^ (v >> 14);
+          v = (v + (v << 2)) + (v << 4);  // v * 21
+          v = v ^ (v >> 28);
+          v = v + (v << 31);
+          return static_cast<size_t>(v);
+        }
+      }
+    }
+  }
+  UNREACHABLE();
+}
 
 V8_INLINE size_t hash_combine() { return 0u; }
 V8_INLINE size_t hash_combine(size_t seed) { return seed; }
-V8_BASE_EXPORT size_t hash_combine(size_t seed, size_t value);
+
+// This code was taken from MurmurHash.
+V8_INLINE size_t hash_combine(size_t seed, size_t value) {
+#if V8_HOST_ARCH_32_BIT
+  const uint32_t c1 = 0xCC9E2D51;
+  const uint32_t c2 = 0x1B873593;
+
+  value *= c1;
+  value = bits::RotateRight32(value, 15);
+  value *= c2;
+
+  seed ^= value;
+  seed = bits::RotateRight32(seed, 13);
+  seed = seed * 5 + 0xE6546B64;
+#else
+  const uint64_t m = uint64_t{0xC6A4A7935BD1E995};
+  const uint32_t r = 47;
+
+  value *= m;
+  value ^= value >> r;
+  value *= m;
+
+  seed ^= value;
+  seed *= m;
+#endif  // V8_HOST_ARCH_32_BIT
+  return seed;
+}
+
 template <typename T, typename... Ts>
 V8_INLINE size_t hash_combine(T const& v, Ts const&... vs) {
   return hash_combine(hash_combine(vs...), hash<T>()(v));
@@ -93,9 +164,17 @@ V8_BASE_HASH_VALUE_TRIVIAL(unsigned char)
 V8_BASE_HASH_VALUE_TRIVIAL(unsigned short)  // NOLINT(runtime/int)
 #undef V8_BASE_HASH_VALUE_TRIVIAL
 
-V8_BASE_EXPORT size_t hash_value(unsigned int);
-V8_BASE_EXPORT size_t hash_value(unsigned long);       // NOLINT(runtime/int)
-V8_BASE_EXPORT size_t hash_value(unsigned long long);  // NOLINT(runtime/int)
+V8_INLINE size_t hash_value(unsigned int v) {
+  return hash_value_unsigned_impl(v);
+}
+
+V8_INLINE size_t hash_value(unsigned long v) {  // NOLINT(runtime/int)
+  return hash_value_unsigned_impl(v);
+}
+
+V8_INLINE size_t hash_value(unsigned long long v) {  // NOLINT(runtime/int)
+  return hash_value_unsigned_impl(v);
+}
 
 #define V8_BASE_HASH_VALUE_SIGNED(type)                  \
   V8_INLINE size_t hash_value(signed type v) {           \
