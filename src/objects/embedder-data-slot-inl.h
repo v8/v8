a9dcb35e7d0a3124ab79915b2aa1dc7c3474fff6
Samuel Groß <saelo@chromium.org>
2022-08-24 15:19:44 +0000

    Revert "Reland "[sandbox] Sandboxify EmbedderDataSlots""
    This reverts commit eca383c947da0c4faa890f72e4f89c6c8f592062.
    
    Reason for revert: Still seeing DCHECK failures in SweepAndCompact
    
    Original change's description:
    > Reland "[sandbox] Sandboxify EmbedderDataSlots"
    >
    > This is a reland of commit e1f585ed94d799555793a4150281ce2baf22a666
    >
    > ExternalPointerTable issues have been fixed in
    > https://crrev.com/c/3849650 and https://crrev.com/c/3849376
    >
    > Original change's description:
    > > [sandbox] Sandboxify EmbedderDataSlots
    > >
    > > Bug: v8:10391
    > > Change-Id: If85a308a6f6ed1b17d86f87b4911c82d2327ea72
    > > Cq-Include-Trybots: luci.v8.try:v8_linux64_heap_sandbox_dbg_ng,v8_linux_arm64_sim_heap_sandbox_dbg_ng
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3757341
    > > Reviewed-by: Igor Sheludko <ishell@chromium.org>
    > > Commit-Queue: Samuel Groß <saelo@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#82623}
    >
    > Bug: v8:10391
    > Change-Id: If77f6c10e81c30c2dfa6b33c788bc4a36e4da135
    > Cq-Include-Trybots: luci.v8.try:v8_linux64_heap_sandbox_dbg_ng,v8_linux_arm64_sim_heap_sandbox_dbg_ng
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3852602
    > Commit-Queue: Samuel Groß <saelo@chromium.org>
    > Reviewed-by: Igor Sheludko <ishell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#82686}
    
    Bug: v8:10391
    Change-Id: Icaa1ff64cabd1bb2f19d9b019eac0ca98e528eb6
    Cq-Include-Trybots: luci.v8.try:v8_linux64_heap_sandbox_dbg_ng,v8_linux_arm64_sim_heap_sandbox_dbg_ng
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3854677
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#82698}

--- src/objects/embedder-data-slot-inl.h
+++ src/objects/embedder-data-slot-inl.h
@@ -88,7 +88,7 @@ bool EmbedderDataSlot::ToAlignedPointer(Isolate* isolate,
   // are accessed this way only from the main thread via API during "mutator"
   // phase which is propely synched with GC (concurrent marker may still look
   // at the tagged part of the embedder slot but read-only access is ok).
-#ifdef V8_ENABLE_SANDBOX
+#ifdef V8_SANDBOXED_EXTERNAL_POINTERS
   // The raw part must always contain a valid external pointer table index.
   *out_pointer = reinterpret_cast<void*>(
       ReadExternalPointerField<kEmbedderDataSlotPayloadTag>(
@@ -107,13 +107,13 @@ bool EmbedderDataSlot::ToAlignedPointer(Isolate* isolate,
   }
   *out_pointer = reinterpret_cast<void*>(raw_value);
   return HAS_SMI_TAG(raw_value);
-#endif  // V8_ENABLE_SANDBOX
+#endif  // V8_SANDBOXED_EXTERNAL_POINTERS
 }
 
 bool EmbedderDataSlot::store_aligned_pointer(Isolate* isolate, void* ptr) {
   Address value = reinterpret_cast<Address>(ptr);
   if (!HAS_SMI_TAG(value)) return false;
-#ifdef V8_ENABLE_SANDBOX
+#ifdef V8_SANDBOXED_EXTERNAL_POINTERS
   DCHECK_EQ(0, value & kExternalPointerTagMask);
   // This also mark the entry as alive until the next GC.
   InitExternalPointerField<kEmbedderDataSlotPayloadTag>(
@@ -123,7 +123,7 @@ bool EmbedderDataSlot::store_aligned_pointer(Isolate* isolate, void* ptr) {
 #else
   gc_safe_store(isolate, value);
   return true;
-#endif  // V8_ENABLE_SANDBOX
+#endif  // V8_SANDBOXED_EXTERNAL_POINTERS
 }
 
 EmbedderDataSlot::RawData EmbedderDataSlot::load_raw(
