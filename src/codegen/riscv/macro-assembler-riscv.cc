f8aebf800230ab7db9e0653953ec161bc6451646
Lu Yahan <yahan@iscas.ac.cn>
2022-08-25 10:52:28 +0800

    Reland "[riscv] Port [heap] Add shared barrier to RecordWrite builtin"
    This is a reland of commit 59d7cf5259df9ad380574b0f3630f69d747df454
    
    Original change's description:
    > [riscv] Port [heap] Add shared barrier to RecordWrite builtin
    >
    > Bug: v8:11708
    >
    > Change-Id: I803b5499f1bbc3f7b4e626628a73f98239df8454
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3854435
    > Auto-Submit: Yahan Lu <yahan@iscas.ac.cn>
    > Commit-Queue: ji qiu <qiuji@iscas.ac.cn>
    > Reviewed-by: ji qiu <qiuji@iscas.ac.cn>
    > Cr-Commit-Position: refs/heads/main@{#82710}
    
    Bug: v8:11708
    Change-Id: I56fcfc8a92c71463bce22a8090e161173cc2c64c
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3857980
    Commit-Queue: ji qiu <qiuji@iscas.ac.cn>
    Auto-Submit: Yahan Lu <yahan@iscas.ac.cn>
    Reviewed-by: ji qiu <qiuji@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#82737}

--- src/codegen/riscv/macro-assembler-riscv.cc
+++ src/codegen/riscv/macro-assembler-riscv.cc
@@ -420,7 +420,7 @@ void MacroAssembler::RecordWrite(Register object, Operand offset,
     Register temp = temps.Acquire();
     CheckPageFlag(value,
                   temp,  // Used as scratch.
-                  MemoryChunk::kPointersToHereAreInterestingMask,
+                  MemoryChunk::kPointersToHereAreInterestingOrInSharedHeapMask,
                   eq,  // In RISC-V, it uses cc for a comparison with 0, so if
                        // no bits are set, and cc is eq, it will branch to done
                   &done);
