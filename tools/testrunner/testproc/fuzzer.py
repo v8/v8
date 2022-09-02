416d6308e07f31f8f00bf26aa6dceabd99a7c6e6
Omer Katz <omerkatz@chromium.org>
2022-09-02 20:33:15 +0200

    [heap] Turn on fuzzing for MinorMC
    Based on bots and local testing, MinorMC has reached a stable state in
    terms of correctness.
    Enable fuzzing with MinorMC to flush out additional issues.
    
    Bug: v8:12612
    Change-Id: I9cf8c5791d7256ff63c777b295863506436ee165
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3872265
    Reviewed-by: Almothana Athamneh <almuthanna@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#83190}

--- tools/testrunner/testproc/fuzzer.py
+++ tools/testrunner/testproc/fuzzer.py
@@ -21,6 +21,7 @@ EXTRA_FLAGS = [
     # (0.1, '--interrupt-budget-for-maglev=100'),
     (0.1, '--liftoff'),
     (0.1, '--maglev'),
+    (0.1, '--minor-mc'),
     (0.2, '--no-analyze-environment-liveness'),
     # TODO(machenbach): Enable when it doesn't collide with crashing on missing
     # simd features.
