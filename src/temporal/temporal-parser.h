4b0e8569cc7047ee1964ea1dd9acada1260bdfce
Frank Tang <ftang@chromium.org>
2022-09-16 12:56:08 -0700

    [Temporal] Add ParseTimeZoneIdentifier ParseCalendarName to parser
    Needed for https://chromium-review.googlesource.com/c/v8/v8/+/3893552
    and also https://chromium-review.googlesource.com/c/v8/v8/+/3901196
    
    Bug: v8:11544
    Change-Id: Ieb946abde1c623e9edcca608d737e8bf65a34637
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3901195
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Frank Tang <ftang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#83281}

--- src/temporal/temporal-parser.h
+++ src/temporal/temporal-parser.h
@@ -144,8 +144,10 @@ class V8_EXPORT_PRIVATE TemporalParser {
   DEFINE_PARSE_METHOD(ParsedISO8601Result, TemporalInstantString);
   DEFINE_PARSE_METHOD(ParsedISO8601Result, TemporalZonedDateTimeString);
   DEFINE_PARSE_METHOD(ParsedISO8601Result, TemporalTimeZoneString);
+  DEFINE_PARSE_METHOD(ParsedISO8601Result, TimeZoneIdentifier);
   DEFINE_PARSE_METHOD(ParsedISO8601Result, TemporalRelativeToString);
   DEFINE_PARSE_METHOD(ParsedISO8601Result, TemporalCalendarString);
+  DEFINE_PARSE_METHOD(ParsedISO8601Result, CalendarName);
   DEFINE_PARSE_METHOD(ParsedISO8601Duration, TemporalDurationString);
   DEFINE_PARSE_METHOD(ParsedISO8601Result, TimeZoneNumericUTCOffset);
 };
