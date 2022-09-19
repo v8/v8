3285ad9ea4a2a8794849f4dcdaef3f904bbfbfc1
Samuel Groß <saelo@chromium.org>
2022-09-19 18:28:30 +0000

    Reland "[sandbox] Increase ExternalPointerTable maximum capacity to 512MB"
    This is a reland of commit d7fcbba80e35e845a85e900cf4e3c479e3b69790
    
    The LSan support logic of the ExternalPointerTable has been optimized to
    avoid timeouts on sanitizer bots
    
    Original change's description:
    > [sandbox] Increase ExternalPointerTable maximum capacity to 512MB
    >
    > Bug: v8:10391
    > Change-Id: I383e11bdccf6fcaf13f29d25e1404545067d313e
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3891249
    > Reviewed-by: Igor Sheludko <ishell@chromium.org>
    > Commit-Queue: Samuel Groß <saelo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#83203}
    
    Bug: v8:10391
    Change-Id: If50156d6fecff7ca8ece5c350e7b08936f50daa6
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3905141
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#83322}

--- include/v8-internal.h
+++ include/v8-internal.h
@@ -232,7 +232,7 @@ static_assert(kSandboxMinimumReservationSize > kPtrComprCageReservationSize,
 // size allows omitting bounds checks on table accesses if the indices are
 // guaranteed (e.g. through shifting) to be below the maximum index. This
 // value must be a power of two.
-static const size_t kExternalPointerTableReservationSize = 128 * MB;
+static const size_t kExternalPointerTableReservationSize = 512 * MB;
 
 // The maximum number of entries in an external pointer table.
 static const size_t kMaxExternalPointers =
@@ -241,7 +241,7 @@ static const size_t kMaxExternalPointers =
 // The external pointer table indices stored in HeapObjects as external
 // pointers are shifted to the left by this amount to guarantee that they are
 // smaller than the maximum table size.
-static const uint32_t kExternalPointerIndexShift = 8;
+static const uint32_t kExternalPointerIndexShift = 6;
 static_assert((1 << (32 - kExternalPointerIndexShift)) == kMaxExternalPointers,
               "kExternalPointerTableReservationSize and "
               "kExternalPointerIndexShift don't match");
