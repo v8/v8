67106ff4943830130f89566af5967c1180791e3e
Matthias Liedtke <mliedtke@chromium.org>
2022-09-20 15:41:23 +0200

    [wasm-gc] JS interop: Do not wrap structs/arrays by default
    Bug: v8:7748
    Change-Id: I441fd294bc0f31c0396217bc55c27159abacdbd1
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3905725
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Auto-Submit: Matthias Liedtke <mliedtke@chromium.org>
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#83338}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -1095,7 +1095,7 @@ FOREACH_WASM_FEATURE_FLAG(DECL_WASM_FLAG)
 
 DEFINE_IMPLICATION(experimental_wasm_gc, experimental_wasm_typed_funcref)
 
-DEFINE_BOOL(wasm_gc_js_interop, false, "experimental WasmGC-JS interop")
+DEFINE_BOOL(wasm_gc_js_interop, true, "experimental WasmGC-JS interop")
 
 DEFINE_BOOL(wasm_staging, false, "enable staged wasm features")
 
