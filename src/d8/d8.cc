dd1db42d49df48a6a29f7444355d05db782e1c5d
Maya Lekova <mslekova@chromium.org>
2022-09-27 15:44:30 +0200

    [test] Remove an exception from a test callback
    ... as such is already thrown at an earlier point of the call chain.
    
    Change-Id: Iad28438c3b6b0d0fdc178d95701908338500eaa9
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3921520
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Maya Lekova <mslekova@chromium.org>
    Auto-Submit: Maya Lekova <mslekova@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#83476}

--- src/d8/d8.cc
+++ src/d8/d8.cc
@@ -3046,15 +3046,6 @@ Local<String> Shell::Stringify(Isolate* isolate, Local<Value> value) {
 void Shell::NodeTypeCallback(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
 
-  // HasInstance does a slow prototype chain lookup, and this function is used
-  // for micro benchmarks too.
-#ifdef DEBUG
-  PerIsolateData* data = PerIsolateData::Get(isolate);
-  if (!data->GetDomNodeCtor()->HasInstance(args.This())) {
-    isolate->ThrowError("Calling .nodeType on wrong instance type.");
-  }
-#endif
-
   args.GetReturnValue().Set(v8::Number::New(isolate, 1));
 }
 
