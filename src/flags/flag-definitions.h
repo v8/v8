60f29614e337fd5600715a4d5b3fe8148e587ace
Dominik Inführ <dinfuehr@chromium.org>
2022-10-27 12:22:29 +0000

    Revert "Reland^3 "[heap] Enable --shared-space flag by default""
    This reverts commit 0b251add9b5ede96d08c936fa8cf31eb349b320b.
    
    Reason for revert: Causes crashes in https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Linux%20-%20debug/41507/overview
    
    Original change's description:
    > Reland^3 "[heap] Enable --shared-space flag by default"
    >
    > This is a reland of commit 1352f2969acf687a0a4959e4f7b2d53365d82faa
    >
    > Original change's description:
    > > Reland^2 "[heap] Enable --shared-space flag by default"
    > >
    > > This is a reland of commit 3df866d67dc9513f74c40694ac21701cf4efcc2b
    > >
    > > Original change's description:
    > > > Reland "[heap] Enable --shared-space flag by default"
    > > >
    > > > This is a reland of commit 934afdf0d1f883f8a35c17f9e750ebc65f86446a
    > > >
    > > > After landing https://crrev.com/c/3959774 try to re-enable
    > > > --shared-space again.
    > > >
    > > > Original change's description:
    > > > > [heap] Enable --shared-space flag by default
    > > > >
    > > > > This switches V8 to the shared space isolate approach for the shared
    > > > > heap by default.
    > > > >
    > > > > Bug: v8:13267
    > > > > Change-Id: I06d36468a3a6b098475ec2bbfa998cb1934afe81
    > > > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3925754
    > > > > Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    > > > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > > > Cr-Commit-Position: refs/heads/main@{#83701}
    > > >
    > > > Bug: v8:13267
    > > > Change-Id: Icb3cfd7c1da2d2420741057c26d32893a6edc80c
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3959695
    > > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > > Auto-Submit: Dominik Inführ <dinfuehr@chromium.org>
    > > > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#83742}
    > >
    > > Bug: v8:13267
    > > Change-Id: I8d44788d05fd7aaf04ac528db5bb7ae98ee980ec
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3961511
    > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#83816}
    >
    > Bug: v8:13267
    > Change-Id: I212b47c9c8aa5d818c2875f10cc18b7b16233a0b
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3971118
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#83947}
    
    Bug: v8:13267
    Change-Id: I51cc3071c2e0ff6c9e75d92ab606b348da317502
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3986724
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#83954}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -1241,7 +1241,7 @@ DEFINE_BOOL(separate_gc_phases, false,
 DEFINE_BOOL(global_gc_scheduling, true,
             "enable GC scheduling based on global memory")
 DEFINE_BOOL(gc_global, false, "always perform global GCs")
-DEFINE_BOOL(shared_space, true,
+DEFINE_BOOL(shared_space, false,
             "Implement shared heap as shared space on a main isolate.")
 
 // TODO(12950): The next two flags only have an effect if
