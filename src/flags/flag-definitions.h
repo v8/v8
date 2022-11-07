f7454be685df8ed70786d1dba522683cd2104e6f
Shu-yu Guo <syg@chromium.org>
2022-11-07 13:50:09 -0800

    Unship JavaScript Array grouping
    Two web incompatibilities have been discovered:
    
    https://github.com/webcompat/web-bugs/issues/112552
    https://github.com/webcompat/web-bugs/issues/112923
    
    Unship the proposal until TC39 decides next steps.
    
    Bug: v8:12499, v8:13460
    Change-Id: Ib028b2e5b2e0b1db61473fc4f023a35c06e1f237
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4010598
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Adam Klein <adamk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#84103}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -234,6 +234,7 @@ DEFINE_BOOL(harmony_shipping, true, "enable all shipped harmony features")
 #define HARMONY_STAGED_BASE(V)                                    \
   V(harmony_rab_gsab,                                             \
     "harmony ResizableArrayBuffer / GrowableSharedArrayBuffer")   \
+  V(harmony_array_grouping, "harmony array grouping")             \
   V(harmony_change_array_by_copy, "harmony change-Array-by-copy") \
   V(harmony_symbol_as_weakmap_key, "harmony symbols as weakmap keys")
 
@@ -249,8 +250,7 @@ DEFINE_BOOL(harmony_shipping, true, "enable all shipped harmony features")
   V(harmony_atomics, "harmony atomics")                               \
   V(harmony_class_static_blocks, "harmony static initializer blocks") \
   V(harmony_array_find_last, "harmony array find last helpers")       \
-  V(harmony_import_assertions, "harmony import assertions")           \
-  V(harmony_array_grouping, "harmony array grouping")
+  V(harmony_import_assertions, "harmony import assertions")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_SHIPPING(V) \
