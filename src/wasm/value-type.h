8c9426b3816918b730a762a698ee4b976aa1cf8b
Jakob Kummerow <jkummerow@chromium.org>
2022-11-08 19:08:01 +0000

    Revert "[wasm-gc] Allow (ref none) to occur internally"
    This reverts commit bc777d5fd610b56a59078ab059bc8685a4b9714e.
    
    Reason for revert: WasmTyper needs adaptations.
    
    Original change's description:
    > [wasm-gc] Allow (ref none) to occur internally
    >
    > This type is uninhabited by any actual values, but it can show up
    > as a "theoretical" value in the type system, e.g. for conditional
    > branches that are statically never-taken.
    >
    > Bug: v8:7748
    > Change-Id: Ieb8a5354dbd04fe76e1047766c5bb60f02f6cdd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4013859
    > Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    > Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
    > Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#84125}
    
    Bug: v8:7748
    Change-Id: Ic6fdd5deaabb2bac99dee25ccc65971b8de31e7c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4013688
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#84132}

--- src/wasm/value-type.h
+++ src/wasm/value-type.h
@@ -437,6 +437,12 @@ class ValueType {
 
   // If {this} is (ref null $t), returns (ref $t). Otherwise, returns {this}.
   constexpr ValueType AsNonNull() const {
+    if (is_reference_to(HeapType::kNone) ||
+        is_reference_to(HeapType::kNoExtern) ||
+        is_reference_to(HeapType::kNoFunc)) {
+      // Non-null none type is not a valid type.
+      return ValueType::Primitive(kBottom);
+    }
     return is_nullable() ? Ref(heap_type()) : *this;
   }
 
