606c5c4c3ad173e11b6820d555264cba8120d669
Toon Verwaest <verwaest@chromium.org>
2022-11-09 18:24:22 +0100

    [maglev] Optimize BranchIfRootConstant with known RootConstant
    This kills the unreachable branch and makes the code fall through the
    reachable part.
    
    Bug: v8:7700
    Change-Id: I68330b01149f5272afdb6e8d033c25f7bc2f70a7
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4017193
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#84159}

--- src/maglev/maglev-graph-builder.cc
+++ src/maglev/maglev-graph-builder.cc
@@ -3851,12 +3851,26 @@ void MaglevGraphBuilder::BuildBranchIfRootConstant(ValueNode* node,
                                                    RootIndex root_index) {
   int fallthrough_offset = next_offset();
   int jump_offset = iterator_.GetJumpTargetOffset();
+  if (RootConstant* c = node->TryCast<RootConstant>()) {
+    bool constant_is_match = c->index() == root_index;
+    bool is_jump_taken = constant_is_match == (jump_type == kJumpIfTrue);
+    if (is_jump_taken) {
+      BasicBlock* block = FinishBlock<Jump>({}, &jump_targets_[jump_offset]);
+      MergeDeadIntoFrameState(fallthrough_offset);
+      MergeIntoFrameState(block, jump_offset);
+    } else {
+      MergeDeadIntoFrameState(jump_offset);
+    }
+    return;
+  }
+
   BasicBlockRef* true_target = jump_type == kJumpIfTrue
                                    ? &jump_targets_[jump_offset]
                                    : &jump_targets_[fallthrough_offset];
   BasicBlockRef* false_target = jump_type == kJumpIfFalse
                                     ? &jump_targets_[jump_offset]
                                     : &jump_targets_[fallthrough_offset];
+
   BasicBlock* block = FinishBlock<BranchIfRootConstant>(
       {node}, true_target, false_target, root_index);
   if (jump_type == kJumpIfTrue) {
