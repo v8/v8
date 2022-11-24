4994cd8aea8ce7f0373a87a8018908f0f713ca46
Leszek Swirski <leszeks@chromium.org>
2022-11-24 12:36:27 +0100

    [maglev] Fix OOB in StringFromCharCode
    StringFromCharCode expects an int32 value, but maglev isn't careful
    about keeping the top 32 bits of the register valid (to avoid needing to
    sign extend after every 32-bit operation). This means the top bits of
    an int32 register might be invalid when it is used -- in particular,
    complex addressing uses its inputs as 64-bit values, including the
    index.
    
    Long story short, we need to zero the top bits of the int32 char_code
    used as the index into the single character table.
    
    Bug: v8:7700
    Change-Id: I3540230c865a1d07c105f35d024d598cc8e15180
    Fixed: chromium:1392585
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4055502
    Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    Reviewed-by: Victor Gomes <victorgomes@chromium.org>
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#84456}

--- src/maglev/maglev-assembler.cc
+++ src/maglev/maglev-assembler.cc
@@ -109,8 +109,10 @@ void MaglevAssembler::LoadSingleCharacterString(Register result,
 void MaglevAssembler::LoadSingleCharacterString(Register result,
                                                 Register char_code,
                                                 Register scratch) {
+  // Make sure char_code is zero extended.
+  movl(char_code, char_code);
   if (v8_flags.debug_code) {
-    cmpl(char_code, Immediate(String::kMaxOneByteCharCode));
+    cmpq(char_code, Immediate(String::kMaxOneByteCharCode));
     Assert(below_equal, AbortReason::kUnexpectedValue);
   }
   DCHECK_NE(char_code, scratch);
