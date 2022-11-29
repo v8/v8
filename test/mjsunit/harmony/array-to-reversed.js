af84cc0b7b275426db92165c519fd2b62f262eac
Choongwoo Han <choongwoo.han@microsoft.com>
2022-11-28 16:35:23 -0800

    Optimize Array.reverse for HOLEY arrays
    Array.reverse was optimized only for PACKED_* arrays. This CL adds fast paths for HOLEY_* arrays as well.
    
    Bug: chromium:1305342
    Change-Id: I83c5ffa6e823478992c2caabd9a88d405b35e464
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4062673
    Commit-Queue: Choongwoo Han <choongwoo.han@microsoft.com>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#84580}

--- test/mjsunit/harmony/array-to-reversed.js
+++ test/mjsunit/harmony/array-to-reversed.js
@@ -31,6 +31,30 @@ assertEquals("toReversed", Array.prototype.toReversed.name);
   assertFalse(a === r);
 })();
 
+(function TestSmiHoley() {
+  let a = [1,,3,4];
+  let r = a.toReversed();
+  assertEquals([1,,3,4], a);
+  assertEquals([4,3,,1], r);
+  assertFalse(a === r);
+})();
+
+(function TestDoubleHoley() {
+  let a = [1.1,,3.3,4.4];
+  let r = a.toReversed();
+  assertEquals([1.1,,3.3,4.4], a);
+  assertEquals([4.4,3.3,,1.1], r);
+  assertFalse(a === r);
+})();
+
+(function TestHoley() {
+  let a = [true,false,,1,42.42];
+  let r = a.toReversed();
+  assertEquals([true,false,,1,42.42], a);
+  assertEquals([42.42,1,,false,true], r);
+  assertFalse(a === r);
+})();
+
 (function TestGeneric() {
   let a = { length: 4,
             get "0"() { return "hello"; },
