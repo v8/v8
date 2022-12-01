167ac0c079f344a297fd74fd1f672e79495cfc99
Clemens Backes <clemensb@chromium.org>
2022-12-01 16:51:56 +0100

    Reland "[wasm] Remove StartDecoding method"
    This is a reland of commit 48a6193e674ee1e96cc903c7885f5b16b9b91044.
    PS2 fixes the use-after-free by using the WasmEngine's allocator instead
    of the Isolate's. The module can live longer than the isolate, so on
    module destruction we would otherwise potentially access an already
    deleted allocator.
    
    Original change's description:
    > [wasm] Remove StartDecoding method
    >
    > Move it into the constructor instead, to simplify the API.
    >
    > R=ahaas@chromium.org
    >
    > Bug: v8:13447
    > Change-Id: I563a409c57c3cfdd91998c7c459f4e099211ccf7
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4062042
    > Reviewed-by: Andreas Haas <ahaas@chromium.org>
    > Commit-Queue: Clemens Backes <clemensb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#84589}
    
    Bug: v8:13447
    Change-Id: I33c3c641ab6de6b6e30365fe967a579469039740
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4070264
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#84604}

--- src/wasm/module-decoder.cc
+++ src/wasm/module-decoder.cc
@@ -75,9 +75,10 @@ const char* SectionName(SectionCode code) {
 class ModuleDecoderImpl : public ModuleDecoderTemplate<NoTracer> {
  public:
   ModuleDecoderImpl(WasmFeatures enabled_features,
-                    base::Vector<const uint8_t> wire_bytes, ModuleOrigin origin)
+                    base::Vector<const uint8_t> wire_bytes, ModuleOrigin origin,
+                    AccountingAllocator* allocator)
       : ModuleDecoderTemplate<NoTracer>(enabled_features, wire_bytes, origin,
-                                        no_tracer_) {}
+                                        allocator, no_tracer_) {}
 
  private:
   NoTracer no_tracer_;
@@ -141,32 +142,28 @@ ModuleResult DecodeWasmModule(WasmFeatures enabled_features,
                               base::Vector<const uint8_t> wire_bytes,
                               bool validate_functions, ModuleOrigin origin,
                               AccountingAllocator* allocator) {
-  ModuleDecoderImpl decoder{enabled_features, wire_bytes, origin};
-  return decoder.DecodeModule(allocator, validate_functions);
+  ModuleDecoderImpl decoder{enabled_features, wire_bytes, origin, allocator};
+  return decoder.DecodeModule(validate_functions);
 }
 
 ModuleResult DecodeWasmModuleForDisassembler(
     base::Vector<const uint8_t> wire_bytes, AccountingAllocator* allocator) {
   constexpr bool kNoValidateFunctions = false;
-  ModuleDecoderImpl decoder(WasmFeatures::All(), wire_bytes, kWasmOrigin);
-  return decoder.DecodeModule(allocator, kNoValidateFunctions);
+  ModuleDecoderImpl decoder{WasmFeatures::All(), wire_bytes, kWasmOrigin,
+                            allocator};
+  return decoder.DecodeModule(kNoValidateFunctions);
 }
 
-ModuleDecoder::ModuleDecoder(WasmFeatures enabled_features)
-    : enabled_features_(enabled_features) {}
+ModuleDecoder::ModuleDecoder(WasmFeatures enabled_features,
+                             AccountingAllocator* allocator)
+    : impl_(std::make_unique<ModuleDecoderImpl>(enabled_features,
+                                                base::Vector<const uint8_t>{},
+                                                kWasmOrigin, allocator)) {}
 
 ModuleDecoder::~ModuleDecoder() = default;
 
 const std::shared_ptr<WasmModule>& ModuleDecoder::shared_module() const {
   return impl_->shared_module();
-}
-
-void ModuleDecoder::StartDecoding(AccountingAllocator* allocator,
-                                  ModuleOrigin origin) {
-  DCHECK_NULL(impl_);
-  static constexpr base::Vector<const uint8_t> kNoWireBytes{nullptr, 0};
-  impl_.reset(new ModuleDecoderImpl(enabled_features_, kNoWireBytes, origin));
-  impl_->StartDecoding(allocator);
 }
 
 void ModuleDecoder::DecodeModuleHeader(base::Vector<const uint8_t> bytes,
@@ -212,16 +209,16 @@ bool ModuleDecoder::ok() { return impl_->ok(); }
 Result<const FunctionSig*> DecodeWasmSignatureForTesting(
     WasmFeatures enabled_features, Zone* zone,
     base::Vector<const uint8_t> bytes) {
-  ModuleDecoderImpl decoder(enabled_features, bytes, kWasmOrigin);
+  ModuleDecoderImpl decoder{enabled_features, bytes, kWasmOrigin,
+                            zone->allocator()};
   return decoder.toResult(decoder.DecodeFunctionSignature(zone, bytes.begin()));
 }
 
 ConstantExpression DecodeWasmInitExprForTesting(
     WasmFeatures enabled_features, base::Vector<const uint8_t> bytes,
     ValueType expected) {
-  ModuleDecoderImpl decoder(enabled_features, bytes, kWasmOrigin);
   AccountingAllocator allocator;
-  decoder.StartDecoding(&allocator);
+  ModuleDecoderImpl decoder{enabled_features, bytes, kWasmOrigin, &allocator};
   return decoder.DecodeInitExprForTesting(expected);
 }
 
@@ -233,7 +230,8 @@ FunctionResult DecodeWasmFunctionForTesting(
         WasmError{0, "size > maximum function size (%zu): %zu",
                   kV8MaxWasmFunctionSize, function_bytes.size()}};
   }
-  ModuleDecoderImpl decoder(enabled_features, function_bytes, kWasmOrigin);
+  ModuleDecoderImpl decoder{enabled_features, function_bytes, kWasmOrigin,
+                            zone->allocator()};
   return decoder.DecodeSingleFunctionForTesting(zone, wire_bytes, module);
 }
 
--- tools/wasm/module-inspector.cc
+++ tools/wasm/module-inspector.cc
@@ -360,10 +360,12 @@ class ExtendedFunctionDis : public FunctionBodyDisassembler {
 class HexDumpModuleDis;
 class DumpingModuleDecoder : public ModuleDecoderTemplate<HexDumpModuleDis> {
  public:
-  DumpingModuleDecoder(ModuleWireBytes wire_bytes, HexDumpModuleDis* module_dis)
-      : ModuleDecoderTemplate<HexDumpModuleDis>(WasmFeatures::All(),
-                                                wire_bytes.module_bytes(),
-                                                kWasmOrigin, *module_dis) {}
+  DumpingModuleDecoder(ModuleWireBytes wire_bytes,
+                       AccountingAllocator* allocator,
+                       HexDumpModuleDis* module_dis)
+      : ModuleDecoderTemplate<HexDumpModuleDis>(
+            WasmFeatures::All(), wire_bytes.module_bytes(), kWasmOrigin,
+            allocator, *module_dis) {}
 
   void onFirstError() override {
     // Pretend we've reached the end of the section, but contrary to the
@@ -386,7 +388,7 @@ class HexDumpModuleDis {
 
   // Public entrypoint.
   void PrintModule() {
-    DumpingModuleDecoder decoder(wire_bytes_, this);
+    DumpingModuleDecoder decoder{wire_bytes_, allocator_, this};
     decoder_ = &decoder;
 
     // If the module failed validation, create fakes to allow us to print
@@ -404,7 +406,7 @@ class HexDumpModuleDis {
     out_ << "[";
     out_.NextLine(0);
     constexpr bool kNoVerifyFunctions = false;
-    decoder.DecodeModule(allocator_, kNoVerifyFunctions);
+    decoder.DecodeModule(kNoVerifyFunctions);
     out_ << "]";
 
     if (total_bytes_ != wire_bytes_.length()) {
