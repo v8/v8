91872b9330dc3cae91753cf395899ad33c4bc173
Clemens Backes <clemensb@chromium.org>
2023-01-12 18:21:33 +0100

    Reland "[wasm][test] Remove useless macro"
    This is a reland of commit c74c9e63e18ed0e55ffa79d5ab97ae0e198aa6ba.
    Types have been fixed to make MSVC happy.
    
    Original change's description:
    > [wasm][test] Remove useless macro
    >
    > Remove the BUILD macro, call a function with an initializer list
    > instead. This makes the code slightly shorter, but most importantly it
    > avoids an unnecessary macro, which improves maintainability and
    > debuggability.
    >
    > R=jkummerow@chromium.org
    >
    > Bug: v8:13312
    > Change-Id: I904ccf8e5b98c4d2f487c0cedc865db1386321c6
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4152482
    > Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    > Commit-Queue: Clemens Backes <clemensb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#85259}
    
    Bug: v8:13312
    Cq-Include-Trybots: luci.v8.try:v8_win64_msvc_rel
    Change-Id: Ie3ca8dcd295ec22385b5d40262c2cb03bfcfeda4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4162932
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#85278}

--- test/cctest/wasm/wasm-run-utils.cc
+++ test/cctest/wasm/wasm-run-utils.cc
@@ -577,28 +577,26 @@ Handle<Code> WasmFunctionWrapper::GetWrapperCode(Isolate* isolate) {
 // This struct is just a type tag for Zone::NewArray<T>(size_t) call.
 struct WasmFunctionCompilerBuffer {};
 
-void WasmFunctionCompiler::Build(const byte* start, const byte* end) {
+void WasmFunctionCompiler::Build(base::Vector<const uint8_t> bytes) {
   size_t locals_size = local_decls.Size();
-  size_t total_size = end - start + locals_size + 1;
+  size_t total_size = bytes.size() + locals_size + 1;
   byte* buffer = zone()->NewArray<byte, WasmFunctionCompilerBuffer>(total_size);
   // Prepend the local decls to the code.
   local_decls.Emit(buffer);
   // Emit the code.
-  memcpy(buffer + locals_size, start, end - start);
+  memcpy(buffer + locals_size, bytes.begin(), bytes.size());
   // Append an extra end opcode.
   buffer[total_size - 1] = kExprEnd;
 
-  start = buffer;
-  end = buffer + total_size;
+  bytes = base::VectorOf(buffer, total_size);
 
-  CHECK_GE(kMaxInt, end - start);
-  int len = static_cast<int>(end - start);
-  function_->code = {builder_->AddBytes(base::Vector<const byte>(start, len)),
-                     static_cast<uint32_t>(len)};
+  function_->code = {builder_->AddBytes(bytes),
+                     static_cast<uint32_t>(bytes.size())};
 
   if (interpreter_) {
     // Add the code to the interpreter; do not generate compiled code.
-    interpreter_->SetFunctionCodeForTesting(function_, start, end);
+    interpreter_->SetFunctionCodeForTesting(function_, bytes.begin(),
+                                            bytes.end());
     return;
   }
 
