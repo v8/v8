c74c9e63e18ed0e55ffa79d5ab97ae0e198aa6ba
Clemens Backes <clemensb@chromium.org>
2023-01-12 11:55:52 +0100

    [wasm][test] Remove useless macro
    Remove the BUILD macro, call a function with an initializer list
    instead. This makes the code slightly shorter, but most importantly it
    avoids an unnecessary macro, which improves maintainability and
    debuggability.
    
    R=jkummerow@chromium.org
    
    Bug: v8:13312
    Change-Id: I904ccf8e5b98c4d2f487c0cedc865db1386321c6
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4152482
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#85259}

--- test/cctest/wasm/test-wasm-trap-position.cc
+++ test/cctest/wasm/test-wasm-trap-position.cc
@@ -72,7 +72,7 @@ WASM_COMPILED_EXEC_TEST(Unreachable) {
                      kRuntimeExceptionSupport);
   TestSignatures sigs;
 
-  BUILD(r, WASM_UNREACHABLE);
+  r.Build({WASM_UNREACHABLE});
   uint32_t wasm_index = r.function()->func_index;
 
   Handle<JSFunction> js_wasm_wrapper = r.builder().WrapCode(wasm_index);
@@ -108,14 +108,14 @@ WASM_COMPILED_EXEC_TEST(IllegalLoad) {
 
   r.builder().AddMemory(0L);
 
-  BUILD(r, WASM_IF(WASM_ONE, WASM_SEQ(WASM_LOAD_MEM(MachineType::Int32(),
-                                                    WASM_I32V_1(-3)),
-                                      WASM_DROP)));
+  r.Build({WASM_IF(
+      WASM_ONE, WASM_SEQ(WASM_LOAD_MEM(MachineType::Int32(), WASM_I32V_1(-3)),
+                         WASM_DROP))});
   uint32_t wasm_index_1 = r.function()->func_index;
 
   WasmFunctionCompiler& f2 = r.NewFunction<void>("call_main");
   // Insert a NOP such that the position of the call is not one.
-  BUILD(f2, WASM_NOP, WASM_CALL_FUNCTION0(wasm_index_1));
+  f2.Build({WASM_NOP, WASM_CALL_FUNCTION0(wasm_index_1)});
   uint32_t wasm_index_2 = f2.function_index();
 
   Handle<JSFunction> js_wasm_wrapper = r.builder().WrapCode(wasm_index_2);
