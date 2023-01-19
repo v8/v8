48e79783eed6548a2670ca59f5fb801a5a447812
Michael Lippautz <mlippautz@chromium.org>
2023-01-19 17:06:36 +0100

    cppgc: Optimize MakeGarbageCollected
    Annotate slow path call for creating a new GCInfo accordingly. This
    path will only hit for the first object allocation for a given type.
    All subsequent allocations will use the fast path.
    
    Bug: chromium:1408821
    Change-Id: Ifc1d3491a94b30dfeee1a2c9679c64939025fefe
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4161752
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#85408}

--- src/heap/cppgc/object-allocator.cc
+++ src/heap/cppgc/object-allocator.cc
@@ -113,22 +113,23 @@ ObjectAllocator::ObjectAllocator(RawHeap& heap, PageBackend& page_backend,
       oom_handler_(oom_handler),
       garbage_collector_(garbage_collector) {}
 
-void* ObjectAllocator::OutOfLineAllocate(NormalPageSpace& space, size_t size,
-                                         AlignVal alignment,
-                                         GCInfoIndex gcinfo) {
-  void* memory = OutOfLineAllocateImpl(space, size, alignment, gcinfo);
+void ObjectAllocator::OutOfLineAllocateGCSafePoint(NormalPageSpace& space,
+                                                   size_t size,
+                                                   AlignVal alignment,
+                                                   GCInfoIndex gcinfo,
+                                                   void** object) {
+  *object = OutOfLineAllocateImpl(space, size, alignment, gcinfo);
   stats_collector_.NotifySafePointForConservativeCollection();
   if (prefinalizer_handler_.IsInvokingPreFinalizers()) {
     // Objects allocated during pre finalizers should be allocated as black
     // since marking is already done. Atomics are not needed because there is
     // no concurrent marking in the background.
-    HeapObjectHeader::FromObject(memory).MarkNonAtomic();
+    HeapObjectHeader::FromObject(*object).MarkNonAtomic();
     // Resetting the allocation buffer forces all further allocations in pre
     // finalizers to go through this slow path.
     ReplaceLinearAllocationBuffer(space, stats_collector_, nullptr, 0);
     prefinalizer_handler_.NotifyAllocationInPrefinalizer(size);
   }
-  return memory;
 }
 
 void* ObjectAllocator::OutOfLineAllocateImpl(NormalPageSpace& space,
