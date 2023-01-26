02df9f3ef1a1b6f905568a33899f1addd1b034be
Nikolaos Papaspyrou <nikolaos@chromium.org>
2023-01-26 19:38:32 +0100

    [heap][test] Reland: Fix weakrefs tests for conservative stack scanning
    31 out of the 36 JS tests in test/mjsunit/harmony/weakrefs/ rely on
    precise GC with the following general pattern: they allocate some
    objects, clear all references to them, invoke a GC, then perform
    some test that assumes that the GC has reclaimed the objects.
    When conservative stack scanning is used, this may fail.
    
    This CL fixes the tests, ensuring that a precise GC will be invoked
    when necessary, without scanning the stack. To achieve this, the GC
    has to be invoked in asynchronous execution mode, which ensures that
    it will be invoked from the event loop without a stack. In some
    cases, this change requires a non-trivial change in the tests.
    
    In 5 tests, part of the test's objective was to verify that a weak
    reference is not cleared before the end of the turn. In those, it
    was not possible to invoke GC asynchronously, as this would
    immediately start a new turn. These tests still use synchronous GC
    and they have been modified, if necessary, to allow for CSS (i.e.,
    to not test that all possible garbage is reclaimed after a
    sequential GC). Because of CSS, these tests may not always test
    everything that they were intended to.
    
    Some tests are unsuitable for testing in "GC stress" mode, because
    this interferes with the execution of FinalizationRegistry cleanup
    tasks or with the clearing of WeakRefs, when asynchronous GC is used.
    
    Tests with trivial fix:
    
    - cleanup-from-different-realm
    - cleanup
    - cleanup-proxy-from-different-realm
    - cleanupsome-2
    - cleanupsome-after-unregister
    - cleanupsome
    - finalizationregistry-keeps-holdings-alive
    - multiple-dirty-finalization-groups
    - stress-finalizationregistry-dirty-enqueue
    - undefined-holdings
    - unregister-after-cleanup
    - unregister-before-cleanup
    - unregister-called-twice
    - unregister-inside-cleanup2
    - unregister-inside-cleanup3
    - unregister-inside-cleanup
    - unregister-many
    - unregister-when-cleanup-already-scheduled
    - weak-cell-basics
    
    Tests with non-trivial fixes; same logic but very restructured:
    
    - cleanup-is-not-a-microtask:
    - cleanup-on-detached-realm
    - finalizationregistry-scheduled-for-cleanup-multiple-times
    - finalizationregistry-independent-lifetime
    - finalizationregistry-independent-lifetime-multiple
    - reentrant-gc-from-cleanup
    - symbol-in-finalizationregistry
      (was 2nd part of former symbol-as-weakref-target-gc)
    - weak-unregistertoken
    
    Tests with non-trivial fixes; same logic, restructured, using
    synchronous GC:
    
    - finalizationregistry-and-weakref
    - symbol-as-weakref-target-gc
      (was 1st part of former symbol-as-weakref-target-gc)
    - two-weakrefs
    - weakref-creation-keeps-alive
    - weakref-deref-keeps-alive
    
    This is a reland of commit 20a954f4bc8fec786baee06e2a1a1517cfc336ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4191774
    > Reviewed-by: Marja Hölttä <marja@chromium.org>
    > Reviewed-by: Shu-yu Guo <syg@chromium.org>
    > Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#85477}
    
    Bug: v8:13257
    Bug: v8:13662
    Change-Id: I298ccbc932afc44d5c8c858620a180388a25f5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4197675
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#85512}

--- test/mjsunit/harmony/weakrefs/symbol-in-finalizationregistry.js
+++ test/mjsunit/harmony/weakrefs/symbol-in-finalizationregistry.js
@@ -1,4 +1,4 @@
-// Copyright 2022 the V8 project authors. All rights reserved.
+// Copyright 2023 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,27 +6,25 @@
 
 (async function () {
 
-  let weakRef;
+  let cleanUpCalled = false;
+  const fg = new FinalizationRegistry((target) => {
+    assertEquals('123', target);
+    cleanUpCalled = true;
+  });
+
   (function () {
     const innerKey = Symbol('123');
-    weakRef = new WeakRef(innerKey);
+    fg.register(innerKey, '123');
   })();
 
-  // Since the WeakRef was created during this turn, it is not cleared by GC.
-  // Here we invoke GC synchronously and, with conservative stack scanning, there is
-  // a chance that the object is not reclaimed now. In any case, the WeakRef should
-  // not be cleared.
-  gc();
-
-  assertNotEquals(undefined, weakRef.deref());
-
-  // Trigger GC again in next task. Now the WeakRef is cleared.
   // We need to invoke GC asynchronously and wait for it to finish, so that
   // it doesn't need to scan the stack. Otherwise, the objects may not be
   // reclaimed because of conservative stack scanning and the test may not
   // work as intended.
   await gc({ type: 'major', execution: 'async' });
+  assertFalse(cleanUpCalled);
 
-  assertEquals(undefined, weakRef.deref());
+  // Check that cleanup callback was called in a follow up task.
+  setTimeout(() => { assertTrue(cleanUpCalled); }, 0);
 
 })();
