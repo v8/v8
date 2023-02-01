0d4200055b7fa77b14d57f40362a36952769d9d1
Mikhail Khokhlov <khokhlov@google.com>
2023-02-01 11:04:00 +0000

    Reduce build size when building with Perfetto SDK
    Building Chromium with full Perfetto SDK included increases build time
    significantly. We can reduce this overhead by including only those
    parts that are required. See b/266913150 for context.
    
    Change-Id: I0cde5cb7df7b6151ec686e993488d8467c416fac
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4212390
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#85603}

--- test/unittests/libplatform/tracing-unittest.cc
+++ test/unittests/libplatform/tracing-unittest.cc
@@ -11,7 +11,8 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 #ifdef V8_USE_PERFETTO
-#include "perfetto/tracing.h"
+#include "perfetto/tracing/track_event.h"
+#include "perfetto/tracing/track_event_legacy.h"
 #include "protos/perfetto/trace/trace.pb.h"  // nogncheck
 #include "src/libplatform/tracing/trace-event-listener.h"
 #include "src/tracing/traced-value.h"
