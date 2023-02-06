dfeaaffa735f7a7eb880467120a2ef00862485de
Hao Xu <hao.a.xu@intel.com>
2023-02-06 20:32:55 +0800

    [x64] Merge ChangeInt32ToInt64 into previous Load
    Current instruction selector can fuse ChangeInt32ToInt64(Load) into
    one instruction. It emits the instruction at the position of
    ChangeInt32ToInt64 so the position of Load is changed. This requires
    the two nodes to have same effect level.
    
    This CL makes it to emit the instruction at the position of Load
    instead. Given that ChangeInt32ToInt64 do not have effect input, the
    two nodes do not need to have same effect level.
    
    
    Change-Id: Ic61cc9633c0ada7bb1b095b1c2b96043bca5f5fa
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4159856
    Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    Commit-Queue: Hao A Xu <hao.a.xu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#85949}

--- src/compiler/pipeline.cc
+++ src/compiler/pipeline.cc
@@ -2505,7 +2505,7 @@ struct InstructionSelectionPhase {
         data->info()->switch_jump_table()
             ? InstructionSelector::kEnableSwitchJumpTable
             : InstructionSelector::kDisableSwitchJumpTable,
-        &data->info()->tick_counter(), data->broker(),
+        &data->info()->tick_counter(), data->broker(), data->machine(),
         data->address_of_max_unoptimized_frame_height(),
         data->address_of_max_pushed_argument_count(),
         data->info()->source_positions()
