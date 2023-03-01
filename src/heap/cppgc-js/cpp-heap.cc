d41a93b0b821796afbffe5c604afee082036db58
Michael Lippautz <mlippautz@chromium.org>
2023-03-01 09:19:04 +0100

    Reland "[heap] Bail out of write barrier when not having enough embedder fields"
    This is a reland of commit cb3529bcc94f81593784bd940ab5eef43d35d63a
    
    Original change's description:
    > [heap] Bail out of write barrier when not having enough embedder fields
    >
    > The wrapper write barrier currently reads type information as well as
    > a an instance pointer. Bail out for objects that do not have enough
    > embedder fields and thus do not follow the expected memory layout.
    >
    > Bug: v8:13777
    > Change-Id: I8df17807bc885ac84f0efdf1f20b5592f02e9a3a
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4295417
    > Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#86133}
    
    Bug: v8:13777
    Change-Id: I121c872de556da81ca31bd056245ee894569c600
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4295291
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#86173}

--- src/heap/cppgc-js/cpp-heap.cc
+++ src/heap/cppgc-js/cpp-heap.cc
@@ -781,7 +781,13 @@ bool CppHeap::FinishConcurrentMarkingIfNeeded() {
 void CppHeap::WriteBarrier(JSObject js_object) {
   DCHECK(js_object.MayHaveEmbedderFields());
   DCHECK_NOT_NULL(isolate()->heap()->mark_compact_collector());
-  auto descriptor = wrapper_descriptor();
+
+  const auto descriptor = wrapper_descriptor();
+  const auto min_field_count =
+      1 + std::max(descriptor.wrappable_type_index,
+                   descriptor.wrappable_instance_index);
+  if (js_object.GetEmbedderFieldCount() < min_field_count) return;
+
   const EmbedderDataSlot type_slot(js_object, descriptor.wrappable_type_index);
   const EmbedderDataSlot instance_slot(js_object,
                                        descriptor.wrappable_instance_index);
