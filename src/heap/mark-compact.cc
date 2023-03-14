293b3fe8889bdd88aff788272abbcac0208b0aef
Omer Katz <omerkatz@chromium.org>
2023-03-14 14:57:16 +0000

    Revert "Reland "[heap] Allow PagedNewSpace to grow dynamically""
    This reverts commit 278f8a03602eb4077997ef1bc6c0c28e31467127.
    
    Reason for revert: DCHECK failure on MinorMC bot
    
    Original change's description:
    > Reland "[heap] Allow PagedNewSpace to grow dynamically"
    >
    > This is a reland of commit 9a927342acf78235e7452dc11181dc6363abf0f2
    >
    > Original change's description:
    > > [heap] Allow PagedNewSpace to grow dynamically
    > >
    > > Add pages dynamically when allocation fails as long as the usable
    > > capacity doesn't exceed the target capacity. Usable capacity is defined
    > > as target capacity - wasted bytes (due to blocks unusable by the
    > > freelist).
    > >
    > > After this CL, TotalCapacity() returns the target capactiy and Capacity()
    > > returns the current actual capacity. This CL also updates some callsites
    > > of Capacity() to TotalCapacity().
    > > TotalCapacity() will be renamed to TargetCapacity() later in a followup CL.
    > >
    > > Bug: v8:12612
    > > Change-Id: I84c520437feaa3323a6b91ad2803ca645ba1c88e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4321246
    > > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#86330}
    >
    > Bug: v8:12612
    > Change-Id: I12101c6b676c073c377bcd3d4257cc024f0a66dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4324603
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#86402}
    
    Bug: v8:12612
    Change-Id: I45091aeb51e90ac4b027b933a4f2c0cbde1da1c4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4338013
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#86414}

--- src/heap/mark-compact.cc
+++ src/heap/mark-compact.cc
@@ -4598,7 +4598,8 @@ void MarkCompactCollector::Evacuate() {
         DCHECK_EQ(0, non_atomic_marking_state()->live_bytes(p));
         DCHECK(p->SweepingDone());
         PagedNewSpace* space = heap()->paged_new_space();
-        if (space->ShouldReleaseEmptyPage()) {
+        if ((resize_new_space_ == ResizeNewSpaceMode::kShrink) &&
+            space->ShouldReleaseEmptyPage()) {
           space->ReleasePage(p);
         } else {
           sweeper()->SweepEmptyNewSpacePage(p);
@@ -5339,7 +5340,8 @@ void MarkCompactCollector::StartSweepNewSpace() {
       continue;
     }
 
-    if (paged_space->ShouldReleaseEmptyPage()) {
+    if ((resize_new_space_ == ResizeNewSpaceMode::kShrink) &&
+        paged_space->ShouldReleaseEmptyPage()) {
       paged_space->ReleasePage(p);
     } else {
       empty_new_space_pages_to_be_swept_.push_back(p);
@@ -6184,7 +6186,8 @@ bool MinorMarkCompactCollector::StartSweepNewSpace() {
 
     intptr_t live_bytes_on_page = non_atomic_marking_state()->live_bytes(p);
     if (live_bytes_on_page == 0) {
-      if (paged_space->ShouldReleaseEmptyPage()) {
+      if ((resize_new_space_ == ResizeNewSpaceMode::kShrink) &&
+          paged_space->ShouldReleaseEmptyPage()) {
         paged_space->ReleasePage(p);
       } else {
         sweeper()->SweepEmptyNewSpacePage(p);
--- src/heap/new-spaces-inl.h
+++ src/heap/new-spaces-inl.h
@@ -108,11 +108,8 @@ V8_INLINE bool PagedSpaceForNewSpace::EnsureAllocation(
     int* out_max_aligned_size) {
   if (!PagedSpaceBase::EnsureAllocation(size_in_bytes, alignment, origin,
                                         out_max_aligned_size)) {
-    if (!AddPageBeyondCapacity(size_in_bytes, origin)) {
-      return false;
-    }
+    return false;
   }
-
   allocated_linear_areas_ += limit() - top();
   return true;
 }
