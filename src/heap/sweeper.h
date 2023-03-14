cc8ef7ad5bdce5bcfc0fd1b47e3e5326b7e65a75
Omer Katz <omerkatz@chromium.org>
2023-03-14 09:58:30 +0100

    [heap] Fix uninitialized value in Sweeper
    Bug: chromium:1424274
    Change-Id: I7c4e2b0d94d161905cae41cf9c3c8b69770e674a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4334767
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#86395}

--- src/heap/sweeper.h
+++ src/heap/sweeper.h
@@ -293,7 +293,7 @@ class Sweeper {
   MemoryAllocator::NormalPagesSet snapshot_shared_normal_pages_set_;
   MemoryAllocator::LargePagesSet snapshot_shared_large_pages_set_;
   std::atomic<bool> promoted_page_iteration_in_progress_{false};
-  bool should_iterate_promoted_pages_;
+  bool should_iterate_promoted_pages_ = false;
 };
 
 }  // namespace internal
