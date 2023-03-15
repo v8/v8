3804733594a321befda2e652236217d98031bfcd
Toon Verwaest <verwaest@chromium.org>
2023-03-15 16:09:49 +0100

    [runtime] Faster check whether we're constructing
    If new.target is undefined we're in a call. It's faster to check for
    undefined than whether an object is a JSReceiver.
    
    Change-Id: I3675b4ed659834c3512139fe6184437f7673f10d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4340812
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#86479}

--- src/builtins/builtins-api.cc
+++ src/builtins/builtins-api.cc
@@ -140,14 +140,14 @@ BUILTIN(HandleApiCall) {
       args.target()->shared().get_api_func_data(), isolate);
   int argc = args.length() - 1;
   Address* argv = args.address_of_first_argument();
-  if (new_target->IsJSReceiver()) {
-    RETURN_RESULT_OR_FAILURE(
-        isolate, HandleApiCallHelper<true>(isolate, new_target, fun_data,
-                                           receiver, argv, argc));
-  } else {
+  if (new_target->IsUndefined()) {
     RETURN_RESULT_OR_FAILURE(
         isolate, HandleApiCallHelper<false>(isolate, new_target, fun_data,
                                             receiver, argv, argc));
+  } else {
+    RETURN_RESULT_OR_FAILURE(
+        isolate, HandleApiCallHelper<true>(isolate, new_target, fun_data,
+                                           receiver, argv, argc));
   }
 }
 
