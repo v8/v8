ada1bf6d09406c268485864873c3671615c49883
Clemens Backes <clemensb@chromium.org>
2023-03-31 12:38:52 +0200

    [debug] Avoid allocating too large TypedArrays
    When inspecting an ArrayBuffer we provide different TypedArray views on
    the data, to ease debugging.
    The TypedArray byte size limits is smaller than the ArrayBuffer limit
    though, so allocating those TypedArrays can fail.
    In that case, instead of crashing we just do not provide the TypedArrays
    for now.
    This will have to be fixed as part of issue 4153.
    
    Drive-by: Replace a macro by explicit iteration.
    
    R=ahaas@chromium.org
    
    Bug: v8:4153, chromium:1414221
    Change-Id: Iab858d8e54b87337a10a4dde223daf09cd5cf533
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4388575
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#86832}

--- src/runtime/runtime-debug.cc
+++ src/runtime/runtime-debug.cc
@@ -326,30 +326,24 @@ MaybeHandle<JSArray> Runtime::GetInternalProperties(Isolate* isolate,
           isolate->factory()->true_value());
     } else {
       const size_t byte_length = js_array_buffer->byte_length();
-      static const ExternalArrayType kTypes[] = {
-          kExternalInt8Array,
-          kExternalUint8Array,
-          kExternalInt16Array,
-          kExternalInt32Array,
-      };
-      for (auto type : kTypes) {
-        switch (type) {
-#define TYPED_ARRAY_CASE(Type, type, TYPE, ctype)                           \
-  case kExternal##Type##Array: {                                            \
-    if ((byte_length % sizeof(ctype)) != 0) continue;                       \
-    result = ArrayList::Add(                                                \
-        isolate, result,                                                    \
-        isolate->factory()->NewStringFromStaticChars("[[" #Type "Array]]"), \
-        isolate->factory()->NewJSTypedArray(kExternal##Type##Array,         \
-                                            js_array_buffer, 0,             \
-                                            byte_length / sizeof(ctype)));  \
-    break;                                                                  \
-  }
-          TYPED_ARRAYS(TYPED_ARRAY_CASE)
-#undef TYPED_ARRAY_CASE
-        default:
-          UNREACHABLE();
-        }
+      // TODO(v8:4153): Remove this code once the maximum lengths are equal (and
+      // add a static assertion that it stays that way).
+      static_assert(JSTypedArray::kMaxLength < JSArrayBuffer::kMaxByteLength);
+      CHECK_LE(byte_length, JSArrayBuffer::kMaxByteLength);
+      using DataView = std::tuple<const char*, ExternalArrayType, size_t>;
+      for (auto [name, type, elem_size] :
+           {DataView{"[[Int8Array]]", kExternalInt8Array, 1},
+            DataView{"[[Uint8Array]]", kExternalUint8Array, 1},
+            DataView{"[[Int16Array]]", kExternalInt16Array, 2},
+            DataView{"[[Int32Array]]", kExternalInt32Array, 4}}) {
+        if ((byte_length % elem_size) != 0) continue;
+        size_t length = byte_length / elem_size;
+        if (length > JSTypedArray::kMaxLength) continue;
+        result =
+            ArrayList::Add(isolate, result,
+                           isolate->factory()->NewStringFromAsciiChecked(name),
+                           isolate->factory()->NewJSTypedArray(
+                               type, js_array_buffer, 0, length));
       }
       result =
           ArrayList::Add(isolate, result,
