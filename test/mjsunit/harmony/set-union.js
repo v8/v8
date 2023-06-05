ebfe732fd4a8b02cce53c0d64ad31a4bdc4eefb0
Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
2023-06-05 22:21:11 +0000

    [set-methods] Add fast paths in union method
    This CL adds fast paths for having JSSet and JSMap as input argument
    in union method.
    
    Bug: v8:13556
    Change-Id: Ie8c9ce7693d1049a521ee10b6a76c9c5aaa8dc0b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4581731
    Commit-Queue: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#88070}

--- test/mjsunit/harmony/set-union.js
+++ test/mjsunit/harmony/set-union.js
@@ -1,10 +1,8 @@
 // Copyright 2023 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// Flags: --harmony-set-methods
 
-// --------- Test union method
+// Flags: --harmony-set-methods
 
 (function TestUnionSet() {
   const firstSet = new Set();
