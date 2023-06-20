43539b6648f475a7c87eb15fbf5b344706e0485d
Leszek Swirski <leszeks@chromium.org>
2023-06-20 16:37:55 +0200

    Reland^2 "[maglev] Avoid emitting phis for exception context"
    This is a reland of commit a0c59fe9245b854a6f62c6af21a221dc8ad862cc
    
    This reland fixes the exception phi generated for contexts in the case
    where there is a generator suspend in a try block, to have the correct
    register (i.e. the handler register, not the context register). This
    makes sure that the exception trampoline writes the correct value into
    the exception phi.
    
    It also moves the tests to top-level regression tests, relying on
    stress_maglev's --optimize-on-next-call-optimizes-to-maglev to execute
    the tests.
    
    Original change's description:
    > Reland "[maglev] Avoid emitting phis for exception context"
    >
    > This is a reland of commit 466ac7969efbccb08cb8e186e8d092c61b86d577
    >
    > The reland fixes a missing case where a context phi could be created,
    > returns of inlined functions, by clearing the context just before an
    > inlined return merge. The context is dead on return anyway. It also
    > fixes the DCHECK to correctly check the top level function's osr
    > and function kind, rather than the current compilation unit's one.
    >
    > Original change's description:
    > > [maglev] Avoid emitting phis for exception context
    > >
    > > When the handler context register has a known value, we can avoid
    > > emitting a Phi for the value, by using the same merge logic on the
    > > handler-specific context register as we do for other registers. In
    > > practice, this means that the current context is always statically
    > > known, except when there is a generator.
    > >
    > > Bug: v8:7700
    > > Change-Id: Ib00466f95ef582ef97bdff3ca44db791d77d024b
    > > Fixed: chromium:1450594
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4608004
    > > Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    > > Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    > > Reviewed-by: Victor Gomes <victorgomes@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#88204}
    >
    > Bug: v8:7700
    > Change-Id: I4060ddeb5d3e2fe098214d360b17ae71548c771f
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4614487
    > Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    > Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    > Reviewed-by: Victor Gomes <victorgomes@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#88289}
    
    Bug: v8:7700
    Bug: chromium:1455389
    Change-Id: Id56c2133696f77df674bbfb29bc327a9f42365a5
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4626497
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#88412}

--- src/maglev/maglev-graph-builder.cc
+++ src/maglev/maglev-graph-builder.cc
@@ -8420,11 +8420,7 @@ void MaglevGraphBuilder::PeelLoop() {
       if (merge_state->is_exception_handler()) {
         merge_state = MergePointInterpreterFrameState::NewForCatchBlock(
             *compilation_unit_, merge_state->frame_state().liveness(), offset,
-            merge_state->frame_state()
-                .context(*compilation_unit_)
-                ->Cast<Phi>()
-                ->owner(),
-            graph_);
+            merge_state->catch_block_context_register(), graph_);
       } else {
         // We only peel innermost loops.
         DCHECK(!merge_state->is_loop());
@@ -9093,6 +9089,9 @@ void MaglevGraphBuilder::VisitReturn() {
       NumPredecessors(inline_exit_offset()) > 1) {
     BasicBlock* block =
         FinishBlock<Jump>({}, &jump_targets_[inline_exit_offset()]);
+    // The context is dead by now, set it to optimized out to avoid creating
+    // unnecessary phis.
+    SetContext(GetRootConstant(RootIndex::kOptimizedOut));
     MergeIntoInlinedReturnFrameState(block);
   }
 }
