46be3ab26344c6283b8648aaf938144f443a471e
Leszek Swirski <leszeks@chromium.org>
2023-06-30 12:08:09 +0200

    [ic] Don't reset profiler ticks on megamorphic miss
    This is effectively a revert of crrev.com/c/779435, six years later --
    the compilation landscape has changed substantially since then, and
    faster compilers like Sparkplug and Maglev shouldn't be delayed by
    megamorphic load missses.
    
    Bug: v8:7700
    Change-Id: I3bdfb492b8b3f9f44748e6824585b2e4ab4ccd20
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4660404
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#88588}

--- src/ic/ic.cc
+++ src/ic/ic.cc
@@ -350,12 +350,11 @@ bool MigrateDeprecated(Isolate* isolate, Handle<Object> object) {
 bool IC::ConfigureVectorState(IC::State new_state, Handle<Object> key) {
   DCHECK_EQ(MEGAMORPHIC, new_state);
   DCHECK_IMPLIES(!is_keyed(), key->IsName());
-  // Even though we don't change the feedback data, we still want to reset the
-  // profiler ticks. Real-world observations suggest that optimizing these
-  // functions doesn't improve performance.
   bool changed = nexus()->ConfigureMegamorphic(
       key->IsName() ? IcCheckType::kProperty : IcCheckType::kElement);
-  OnFeedbackChanged("Megamorphic");
+  if (changed) {
+    OnFeedbackChanged("Megamorphic");
+  }
   return changed;
 }
 
