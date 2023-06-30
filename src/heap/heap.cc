a4eae9a44124bc8f47ded66b3cf9e8e1311b05f1
Dominik Inführ <dinfuehr@chromium.org>
2023-06-30 10:51:17 +0200

    Reland "Reland "[heap] DCHECK global memory limit is always higher than V8's limit""
    This is a reland of commit 42adef36559fd132649a90ee512aecf80f3d132d
    
    This CL removes the second DCHECK (which I believe caused the
    reverts) and turned the first one into a CHECK.
    
    Original change's description:
    > Reland "[heap] DCHECK global memory limit is always higher than V8's limit"
    >
    > This is a reland of commit 4ed5d32f40aaf674d02ea5ee9f1712cabddda630
    >
    > This CL replaces the set_max_old_generation_size method with
    > SetOldGenAndGlobalHeapLimit which always sets both fields
    > together.
    >
    > SetOldGenAndGlobalHeapLimit
    > Original change's description:
    > > [heap] DCHECK global memory limit is always higher than V8's limit
    > >
    > > Add a DCHECK in Heap::RecomputeLimits that enforces that the global
    > > memory limit is always greater (or equal) than V8's memory limit.
    > >
    > > At the moment we can also DCHECK that the maximum global memory
    > > size is computed from the maximum old generation size using
    > > GlobalMemorySizeFromV8Size. This was only violated in corner cases
    > > but can be enforced as well.
    > >
    > > Bug: v8:13842
    > > Change-Id: I499c60cc834b293426efa1e729b61a7685642096
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4626639
    > > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > > Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#88368}
    >
    > Bug: v8:13842
    > Change-Id: I5c01f8dcefd47a94b3aa1f53405b4ed81ea30509
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4628214
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#88436}
    
    Bug: v8:13842
    Change-Id: I39cf7bdeb9655c8f6b50af79050b2948ff64adee
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4645090
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#88585}

--- src/heap/heap.cc
+++ src/heap/heap.cc
@@ -1723,7 +1723,19 @@ void InvokeExternalCallbacks(Isolate* isolate, Callback callback) {
   callback();
 }
 
-}  // namespace
+size_t GlobalMemorySizeFromV8Size(size_t v8_size) {
+  const size_t kGlobalMemoryToV8Ratio = 2;
+  return std::min(static_cast<uint64_t>(std::numeric_limits<size_t>::max()),
+                  static_cast<uint64_t>(v8_size) * kGlobalMemoryToV8Ratio);
+}
+
+}  // anonymous namespace
+
+void Heap::SetOldGenAndGlobalHeapLimit(size_t max_old_generation_size) {
+  max_old_generation_size_.store(max_old_generation_size,
+                                 std::memory_order_relaxed);
+  max_global_memory_size_ = GlobalMemorySizeFromV8Size(max_old_generation_size);
+}
 
 void Heap::CollectGarbage(AllocationSpace space,
                           GarbageCollectionReason gc_reason,
@@ -1847,7 +1859,7 @@ void Heap::CollectGarbage(AllocationSpace space,
       if (initial_max_old_generation_size_ < max_old_generation_size() &&
           OldGenerationSizeOfObjects() <
               initial_max_old_generation_size_threshold_) {
-        set_max_old_generation_size(initial_max_old_generation_size_);
+        SetOldGenAndGlobalHeapLimit(initial_max_old_generation_size_);
       }
     }
 
@@ -2538,8 +2550,11 @@ void Heap::RecomputeLimits(GarbageCollector collector) {
       global_allocation_limit_ = new_global_limit;
     }
   }
+
+  CHECK_EQ(max_global_memory_size_,
+           GlobalMemorySizeFromV8Size(max_old_generation_size_));
+
   if (v8_flags.memory_balancer) {
-    DCHECK(global_allocation_limit_ > old_generation_allocation_limit_);
     mb_->UpdateExternalAllocationLimit(global_allocation_limit_ -
                                        old_generation_allocation_limit_);
     mb_->NotifyGC();
@@ -4171,7 +4186,7 @@ bool Heap::InvokeNearHeapLimitCallback() {
     size_t heap_limit = callback(data, max_old_generation_size(),
                                  initial_max_old_generation_size_);
     if (heap_limit > max_old_generation_size()) {
-      set_max_old_generation_size(
+      SetOldGenAndGlobalHeapLimit(
           std::min(heap_limit, AllocatorLimitOnMaxOldGenerationSize()));
       return true;
     }
@@ -4797,14 +4812,6 @@ void Heap::IterateConservativeStackRoots(RootVisitor* v,
 #endif  // V8_ENABLE_CONSERVATIVE_STACK_SCANNING
 }
 
-namespace {
-size_t GlobalMemorySizeFromV8Size(size_t v8_size) {
-  const size_t kGlobalMemoryToV8Ratio = 2;
-  return std::min(static_cast<uint64_t>(std::numeric_limits<size_t>::max()),
-                  static_cast<uint64_t>(v8_size) * kGlobalMemoryToV8Ratio);
-}
-}  // anonymous namespace
-
 void Heap::ConfigureHeap(const v8::ResourceConstraints& constraints) {
   // Initialize max_semi_space_size_.
   {
@@ -4872,9 +4879,7 @@ void Heap::ConfigureHeap(const v8::ResourceConstraints& constraints) {
     max_old_generation_size =
         RoundDown<Page::kPageSize>(max_old_generation_size);
 
-    max_global_memory_size_ =
-        GlobalMemorySizeFromV8Size(max_old_generation_size);
-    set_max_old_generation_size(max_old_generation_size);
+    SetOldGenAndGlobalHeapLimit(max_old_generation_size);
   }
 
   CHECK_IMPLIES(
