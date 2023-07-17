cc27fded5beff8b6c0b344851c3ccb3a597fddc7
Jakob Kummerow <jkummerow@chromium.org>
2023-07-17 18:52:57 +0200

    [wasm-gc] Merge a few fixes
    This commit cherry-picks small parts of:
    crrev.com/c/4669597
    crrev.com/c/4675296
    crrev.com/c/4677170
    that are suitable for backmerging.
    
    Bug: chromium:1462951
    Change-Id: I220498dc89a34b83c74e8f802b1089f700fb035d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4684072
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Cr-Commit-Position: refs/branch-heads/11.4@{#55}
    Cr-Branched-From: 8a8a1e7086dacc426965d3875914efa66663c431-refs/heads/11.4.183@{#1}
    Cr-Branched-From: 5483d8e816e0bbce865cbbc3fa0ab357e6330bab-refs/heads/main@{#87241}

--- src/wasm/function-body-decoder-impl.h
+++ src/wasm/function-body-decoder-impl.h
@@ -3113,7 +3113,7 @@ class WasmFullDecoder : public WasmDecoder<ValidationTag, decoding_mode> {
   }
 
   DECODE(BrOnNonNull) {
-    CHECK_PROTOTYPE_OPCODE(gc);
+    CHECK_PROTOTYPE_OPCODE(typed_funcref);
     BranchDepthImmediate imm(this, this->pc_ + 1, validate);
     if (!this->Validate(this->pc_ + 1, imm, control_.size())) return 0;
     Value ref_object = Peek(0);
@@ -3693,6 +3693,11 @@ class WasmFullDecoder : public WasmDecoder<ValidationTag, decoding_mode> {
     CHECK_PROTOTYPE_OPCODE(return_call);
     SigIndexImmediate imm(this, this->pc_ + 1, validate);
     if (!this->Validate(this->pc_ + 1, imm)) return 0;
+    if (!VALIDATE(this->CanReturnCall(imm.sig))) {
+      this->DecodeError("%s: %s", WasmOpcodes::OpcodeName(kExprReturnCallRef),
+                        "tail call return types mismatch");
+      return 0;
+    }
     Value func_ref = Peek(0, 0, ValueType::RefNull(imm.index));
     ArgVector args = PeekArgs(imm.sig, 1);
     CALL_INTERFACE_IF_OK_AND_REACHABLE(ReturnCallRef, func_ref, imm.sig,
@@ -4691,6 +4696,15 @@ class WasmFullDecoder : public WasmDecoder<ValidationTag, decoding_mode> {
         IndexImmediate elem_segment(this, elem_index_pc, "element segment",
                                     validate);
         if (!this->ValidateElementSegment(elem_index_pc, elem_segment)) {
+          return 0;
+        }
+        ValueType segment_type =
+            this->module_->elem_segments[elem_segment.index].type;
+        if (!VALIDATE(IsSubtypeOf(segment_type, element_type, this->module_))) {
+          this->DecodeError(
+              "array.init_elem: segment type %s is not a subtype of array "
+              "element type %s",
+              segment_type.name().c_str(), element_type.name().c_str());
           return 0;
         }
 
