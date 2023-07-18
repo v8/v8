ed93bef7ab786d5367c2ae7882922c23aa0eda64
Toon Verwaest <verwaest@chromium.org>
2023-07-18 15:48:29 +0200

    [maglev] Fix default constructor instantiation
    The new.target may not be in the correct state for fast instantiation.
    
    Bug: v8:7700, chromium:1465326
    Change-Id: I09f92576c0b5573e902ae3b2210a7b5fdbd1e415
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4694007
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#89006}

--- src/maglev/maglev-graph-builder.h
+++ src/maglev/maglev-graph-builder.h
@@ -1884,6 +1884,9 @@ class MaglevGraphBuilder {
   void MergeDeadLoopIntoFrameState(int target);
   void MergeIntoInlinedReturnFrameState(BasicBlock* block);
 
+  bool HasValidInitialMap(compiler::JSFunctionRef new_target,
+                          compiler::JSFunctionRef constructor);
+
   enum JumpType { kJumpIfTrue, kJumpIfFalse };
   enum class BranchSpecializationMode { kDefault, kAlwaysBoolean };
   JumpType NegateJumpType(JumpType jump_type);
