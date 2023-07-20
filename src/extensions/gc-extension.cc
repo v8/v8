0d7bc5860c772a24f657e5f50d83f33dbad22aea
Nikolaos Papaspyrou <nikolaos@chromium.org>
2023-07-20 12:41:28 +0200

    [heap] Fix GC extension with object parameter
    The GC extension does not handle correctly the following cases:
    
      gc({type: 'major'})
      gc({execution: 'sync']})
      gc({type: 'major', execution: 'sync'})
      gc({type: 'invalid'})
      gc({execution: 'invalid'})
      gc({type: 'invalid', execution: 'invalid'})
    
    In all six, it performs a minor synchronous GC. In fact, if any of
    the options 'type' or 'execution' appear with values different from
    'minor' and 'async', respectively, then a minor synchronous GC is
    performed, as if a truthy parameter was passed (legacy behaviour).
    
    This CL fixes this behaviour. If the parameter is a dictionary-like
    object for which any of the properties 'type' and 'execution' does
    not return undefined, then the legacy behaviour does not apply and
    the options are set by the object. If any is undefined or invalid,
    its default value (type: 'major' / execution: 'sync') is used.
    
    Change-Id: I4a98be6f46d4e298c6e2a0e5973fb42dfaeb36b2
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4702444
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#89057}

--- src/extensions/gc-extension.cc
+++ src/extensions/gc-extension.cc
@@ -31,46 +31,55 @@ struct GCOptions {
 
 Maybe<bool> IsProperty(v8::Isolate* isolate, v8::Local<v8::Context> ctx,
                        v8::Local<v8::Object> object, const char* key,
-                       const char* value) {
+                       const char* value, bool* found_options_object) {
   auto k = v8::String::NewFromUtf8(isolate, key).ToLocalChecked();
-  // Get will return undefined for non-existing keys which will make
-  // StrictEquals fail.
   auto maybe_property = object->Get(ctx, k);
+  // Handle pending or scheduled exception.
   if (maybe_property.IsEmpty()) return Nothing<bool>();
-  return Just<bool>(maybe_property.ToLocalChecked()->StrictEquals(
+  // If the property does not exist or is explicitly set to undefined,
+  // return false.
+  auto property = maybe_property.ToLocalChecked();
+  if (property->IsUndefined()) return Just<bool>(false);
+  // If it exists, the object defines the option.
+  *found_options_object = true;
+  return Just<bool>(property->StrictEquals(
       v8::String::NewFromUtf8(isolate, value).ToLocalChecked()));
 }
 
 Maybe<GCOptions> Parse(v8::Isolate* isolate,
                        const v8::FunctionCallbackInfo<v8::Value>& info) {
   DCHECK(ValidateCallbackInfo(info));
+  DCHECK_LT(0, info.Length());
+
   // Default values.
   auto options =
       GCOptions{v8::Isolate::GarbageCollectionType::kFullGarbageCollection,
                 ExecutionType::kSync};
   bool found_options_object = false;
 
-  if (info.Length() > 0 && info[0]->IsObject()) {
+  if (info[0]->IsObject()) {
     v8::HandleScope scope(isolate);
     auto ctx = isolate->GetCurrentContext();
     auto param = v8::Local<v8::Object>::Cast(info[0]);
-    auto maybe_type = IsProperty(isolate, ctx, param, "type", "minor");
+    auto maybe_type =
+        IsProperty(isolate, ctx, param, "type", "minor", &found_options_object);
     if (maybe_type.IsNothing()) return Nothing<GCOptions>();
     if (maybe_type.ToChecked()) {
-      found_options_object = true;
+      DCHECK(found_options_object);
       options.type =
           v8::Isolate::GarbageCollectionType::kMinorGarbageCollection;
     }
-    auto maybe_execution =
-        IsProperty(isolate, ctx, param, "execution", "async");
+    auto maybe_execution = IsProperty(isolate, ctx, param, "execution", "async",
+                                      &found_options_object);
     if (maybe_execution.IsNothing()) return Nothing<GCOptions>();
     if (maybe_execution.ToChecked()) {
-      found_options_object = true;
+      DCHECK(found_options_object);
       options.execution = ExecutionType::kAsync;
     }
   }
 
-  // If no options object is present default to legacy behavior.
+  // If the parameter is not an object or if it does not define any options,
+  // default to legacy behavior.
   if (!found_options_object) {
     options.type =
         info[0]->BooleanValue(isolate)
