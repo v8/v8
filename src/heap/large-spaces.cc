1bbe5596a2b60adffc6c63d8deb8c5f494cb8661
Dominik Inführ <dinfuehr@chromium.org>
2023-07-24 09:11:11 +0200

    [heap] Move LargePage class into separate files
    Change-Id: Iaf5977d38ea917a3958b0f490f3b0e7f7e002b62
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4705920
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#89129}

--- src/heap/large-spaces.cc
+++ src/heap/large-spaces.cc
@@ -12,6 +12,7 @@
 #include "src/heap/concurrent-marking.h"
 #include "src/heap/heap-verifier.h"
 #include "src/heap/incremental-marking.h"
+#include "src/heap/large-page.h"
 #include "src/heap/list.h"
 #include "src/heap/marking-state-inl.h"
 #include "src/heap/marking.h"
@@ -27,68 +28,6 @@
 
 namespace v8 {
 namespace internal {
-
-// This check is here to ensure that the lower 32 bits of any real heap object
-// can't overlap with the lower 32 bits of cleared weak reference value and
-// therefore it's enough to compare only the lower 32 bits of a MaybeObject in
-// order to figure out if it's a cleared weak reference or not.
-static_assert(kClearedWeakHeapObjectLower32 < LargePage::kHeaderSize);
-
-LargePage::LargePage(Heap* heap, BaseSpace* space, size_t chunk_size,
-                     Address area_start, Address area_end,
-                     VirtualMemory reservation, Executability executable)
-    : MemoryChunk(heap, space, chunk_size, area_start, area_end,
-                  std::move(reservation), executable, PageSize::kLarge) {
-  static_assert(LargePage::kMaxCodePageSize <= TypedSlotSet::kMaxOffset);
-
-  if (executable && chunk_size > LargePage::kMaxCodePageSize) {
-    FATAL("Code page is too large.");
-  }
-
-  SetFlag(MemoryChunk::LARGE_PAGE);
-  list_node().Initialize();
-}
-
-LargePage* LargePage::Initialize(Heap* heap, MemoryChunk* chunk,
-                                 Executability executable) {
-  if (executable && chunk->size() > LargePage::kMaxCodePageSize) {
-    static_assert(LargePage::kMaxCodePageSize <= TypedSlotSet::kMaxOffset);
-    FATAL("Code page is too large.");
-  }
-
-  MSAN_ALLOCATED_UNINITIALIZED_MEMORY(chunk->area_start(), chunk->area_size());
-
-  chunk->SetFlag(MemoryChunk::LARGE_PAGE);
-  chunk->list_node().Initialize();
-  return LargePage::cast(chunk);
-}
-
-size_t LargeObjectSpace::Available() const {
-  // We return zero here since we cannot take advantage of already allocated
-  // large object memory.
-  return 0;
-}
-
-void LargePage::ClearOutOfLiveRangeSlots(Address free_start) {
-  DCHECK_NULL(slot_set<OLD_TO_NEW>());
-  DCHECK_NULL(typed_slot_set<OLD_TO_NEW>());
-
-  DCHECK_NULL(slot_set<OLD_TO_NEW_BACKGROUND>());
-  DCHECK_NULL(typed_slot_set<OLD_TO_NEW_BACKGROUND>());
-
-  DCHECK_NULL(slot_set<OLD_TO_OLD>());
-  DCHECK_NULL(typed_slot_set<OLD_TO_OLD>());
-
-  // area_end() might not be aligned to a full bucket size with large objects.
-  // Align it to bucket size such that the following RemoveRange invocation just
-  // drops the whole bucket and the bucket is reset to nullptr.
-  Address aligned_area_end = address() + SlotSet::OffsetForBucket(buckets());
-  DCHECK_LE(area_end(), aligned_area_end);
-  RememberedSet<OLD_TO_SHARED>::RemoveRange(this, free_start, aligned_area_end,
-                                            SlotSet::FREE_EMPTY_BUCKETS);
-
-  RememberedSet<OLD_TO_SHARED>::RemoveRangeTyped(this, free_start, area_end());
-}
 
 // -----------------------------------------------------------------------------
 // LargeObjectSpaceObjectIterator
@@ -115,6 +54,12 @@ LargeObjectSpace::LargeObjectSpace(Heap* heap, AllocationSpace id)
       page_count_(0),
       objects_size_(0),
       pending_object_(0) {}
+
+size_t LargeObjectSpace::Available() const {
+  // We return zero here since we cannot take advantage of already allocated
+  // large object memory.
+  return 0;
+}
 
 void LargeObjectSpace::TearDown() {
   while (!memory_chunk_list_.Empty()) {
--- src/heap/large-spaces.h
+++ src/heap/large-spaces.h
@@ -15,6 +15,7 @@
 #include "src/common/globals.h"
 #include "src/heap/heap-verifier.h"
 #include "src/heap/heap.h"
+#include "src/heap/large-page.h"
 #include "src/heap/memory-chunk.h"
 #include "src/heap/spaces.h"
 #include "src/objects/heap-object.h"
@@ -24,51 +25,6 @@ namespace internal {
 
 class Isolate;
 class LocalHeap;
-
-class LargePage : public MemoryChunk {
- public:
-  // A limit to guarantee that we do not overflow typed slot offset in the old
-  // to old remembered set. Note that this limit is higher than what assembler
-  // already imposes on x64 and ia32 architectures.
-  static constexpr int kMaxCodePageSize = 512 * MB;
-
-  static LargePage* cast(MemoryChunk* chunk) {
-    DCHECK_IMPLIES(chunk, chunk->IsLargePage());
-    return static_cast<LargePage*>(chunk);
-  }
-
-  static LargePage* cast(BasicMemoryChunk* chunk) {
-    return cast(MemoryChunk::cast(chunk));
-  }
-
-  static LargePage* FromHeapObject(HeapObject o) {
-    DCHECK(!V8_ENABLE_THIRD_PARTY_HEAP_BOOL);
-    return cast(MemoryChunk::FromHeapObject(o));
-  }
-
-  LargePage(Heap* heap, BaseSpace* space, size_t chunk_size, Address area_start,
-            Address area_end, VirtualMemory reservation,
-            Executability executable);
-
-  Tagged<HeapObject> GetObject() const {
-    return HeapObject::FromAddress(area_start());
-  }
-
-  LargePage* next_page() { return LargePage::cast(list_node_.next()); }
-  const LargePage* next_page() const {
-    return static_cast<const LargePage*>(list_node_.next());
-  }
-
-  void ClearOutOfLiveRangeSlots(Address free_start);
-
- private:
-  static LargePage* Initialize(Heap* heap, MemoryChunk* chunk,
-                               Executability executable);
-
-  friend class MemoryAllocator;
-};
-
-static_assert(sizeof(LargePage) <= MemoryChunk::kHeaderSize);
 
 // -----------------------------------------------------------------------------
 // Large objects ( > kMaxRegularHeapObjectSize ) are allocated and managed by
