9776f53d8303f474632046ab373b0085aaa913d6
Leszek Swirski <leszeks@chromium.org>
2023-07-26 09:46:59 +0000

    Revert "Reland "[wasm] Do not inline export wrappers for JSPI""
    This reverts commit 9ee1ba176a52b9ba6f8a773f7b0a5f1d9c77e4af.
    
    Reason for revert: GC Stress: https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Mac64%20GC%20Stress/29112/overview
    
    
    Original change's description:
    > Reland "[wasm] Do not inline export wrappers for JSPI"
    >
    > This is a reland of commit f43a566ce60a5e22be10ed77ebea807dcdd82a18
    >
    > The issue was a data race between a background compilation thread
    > reading the wrapper_code field to check if we can inline it, and the
    > generic wrapper tier-up trying to update the wrapper_code.
    >
    > It does not matter whether we read the value before or after the
    > tier-up, so just get the field with a relaxed load.
    >
    > Original change's description:
    > > [wasm] Do not inline export wrappers for JSPI
    > >
    > > To preserve the "JSPI behavior" of the exported function, do not inline
    > > the optimized js-to-wasm wrapper at the call site. Keep using the
    > > special WasmReturnPromiseOnSuspend builtin.
    > >
    > > R=ahaas@chromium.org
    > >
    > > Bug: v8:12191,v8:14200
    > > Change-Id: I93a8b0293c0c96541c336a90317cee03410dcfd6
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4711685
    > > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    > > Reviewed-by: Andreas Haas <ahaas@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#89151}
    >
    > Bug: v8:12191,v8:14200
    > Change-Id: I30459d98dd9f1942780cba344dee118011fa98bd
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4714608
    > Reviewed-by: Andreas Haas <ahaas@chromium.org>
    > Reviewed-by: Tobias Tebbi <tebbi@chromium.org>
    > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#89195}
    
    Bug: v8:12191,v8:14200
    Change-Id: I30c3323375e9c990615ea680e165ee5857dd876f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4717492
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#89197}

--- src/compiler/js-call-reducer.cc
+++ src/compiler/js-call-reducer.cc
@@ -5111,8 +5111,7 @@ Reduction JSCallReducer::ReduceJSCall(Node* node,
   }
 
 #if V8_ENABLE_WEBASSEMBLY
-  if ((flags() & kInlineJSToWasmCalls) && shared.wasm_function_signature() &&
-      !shared.is_promising_wasm_export()) {
+  if ((flags() & kInlineJSToWasmCalls) && shared.wasm_function_signature()) {
     return ReduceCallWasmFunction(node, shared);
   }
 #endif  // V8_ENABLE_WEBASSEMBLY
