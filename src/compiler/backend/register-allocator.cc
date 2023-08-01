9ba84e4987194e308f6fa2f4359a44b6ddccfbe8
Daniel Lehmann <dlehmann@chromium.org>
2023-08-01 19:02:45 +0200

    [regalloc] Optimize insert of UsePositions
    See comment. This CL reduces profiler samples in `AddUsePosition` when
    running JetStream2 from ~280k to ~180k and locally reduces total
    samples spent in register allocation by ~0.9%.
    
    Bug: v8:14082
    Change-Id: Iaf8bb0fc121949fa23090dadbb53502875d47ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4738679
    Reviewed-by: Thibaud Michaud <thibaudm@chromium.org>
    Commit-Queue: Daniel Lehmann <dlehmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#89313}

--- src/compiler/backend/register-allocator.cc
+++ src/compiler/backend/register-allocator.cc
@@ -887,10 +887,13 @@ void TopLevelLiveRange::AddUsePosition(UsePosition* use_pos, Zone* zone,
                                        bool trace_alloc) {
   TRACE_COND(trace_alloc, "Add to live range %d use position %d\n", vreg(),
              use_pos->pos().value());
-  UsePositionVector::const_iterator insert_it = std::upper_bound(
-      positions_.begin(), positions_.end(), use_pos, UsePosition::Ordering());
-  // Since we `ProcessInstructions` in reverse, `positions_` are mostly
-  // inserted at the front, hence grow towards that direction exclusively.
+  // Since we `ProcessInstructions` in reverse, the `use_pos` is almost always
+  // inserted at the front of `positions_`, hence (i) use linear instead of
+  // binary search and (ii) grow towards the `kFront` exclusively on `insert`.
+  UsePositionVector::iterator insert_it = std::find_if(
+      positions_.begin(), positions_.end(), [=](const UsePosition* pos) {
+        return UsePosition::Ordering()(use_pos, pos);
+      });
   positions_.insert<kFront>(zone, insert_it, use_pos);
 
   positions_span_ = base::VectorOf(positions_);
