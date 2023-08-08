e33693d57f647407cd74b7ca4057614916d1e50f
Liu Yu <liuyu@loongson.cn>
2023-08-08 09:55:11 +0800

    [loong64][mips64][compiler] Generalize InstructionSelectorT for Turboshaft (part 10)
    Port commit 0dbea94dbdd950cbe5ff018a3e43a4bac18c6045
    
    Bug: v8:12783
    Change-Id: I91cf11fbfb500dc59a0b9a33ac912ff264f3f827
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4753620
    Auto-Submit: Liu Yu <liuyu@loongson.cn>
    Reviewed-by: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Commit-Queue: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Cr-Commit-Position: refs/heads/main@{#89436}

--- src/compiler/backend/mips64/instruction-selector-mips64.cc
+++ src/compiler/backend/mips64/instruction-selector-mips64.cc
@@ -374,14 +374,18 @@ static void VisitBinop(InstructionSelectorT<Adapter>* selector, Node* node,
 }
 
 template <typename Adapter>
-void InstructionSelectorT<Adapter>::VisitStackSlot(Node* node) {
-  StackSlotRepresentation rep = StackSlotRepresentationOf(node->op());
-  int alignment = rep.alignment();
-  int slot = frame_->AllocateSpillSlot(rep.size(), alignment);
-  OperandGenerator g(this);
+void InstructionSelectorT<Adapter>::VisitStackSlot(node_t node) {
+  if constexpr (Adapter::IsTurboshaft) {
+    UNIMPLEMENTED();
+  } else {
+    StackSlotRepresentation rep = StackSlotRepresentationOf(node->op());
+    int alignment = rep.alignment();
+    int slot = frame_->AllocateSpillSlot(rep.size(), alignment);
+    OperandGenerator g(this);
 
-  Emit(kArchStackSlot, g.DefineAsRegister(node),
-       sequence()->AddImmediate(Constant(slot)), 0, nullptr);
+    Emit(kArchStackSlot, g.DefineAsRegister(node),
+         sequence()->AddImmediate(Constant(slot)), 0, nullptr);
+  }
 }
 
 template <typename Adapter>
