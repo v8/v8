bb2bbec2f7b64af27c6a6ab7ba687fb91e3e20c1
Milad Fa <mfarazma@redhat.com>
2023-08-09 14:06:20 +0000

    Merged: PPC/s390: [compiler] Generalize InstructionSelectorT for Turboshaft (part 10)
    Revision: 16c6c585b750c7a8e13c1bebb57f05fd90f60d19
    
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=junyan@redhat.com
    
    Bug: v8:14251
    Change-Id: I5268cfb0a9099d4047e7029f0e3423c60bbef4b8
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4762697
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Commit-Queue: Milad Farazmand <mfarazma@redhat.com>
    Cr-Commit-Position: refs/branch-heads/11.7@{#2}
    Cr-Branched-From: fe608691065e23ae83720392bf61a59e8166bee6-refs/heads/11.7.439@{#1}
    Cr-Branched-From: aeb45525398afa173aa91fcf83eb6341c83850ea-refs/heads/main@{#89415}

--- src/compiler/backend/ppc/instruction-selector-ppc.cc
+++ src/compiler/backend/ppc/instruction-selector-ppc.cc
@@ -165,13 +165,17 @@ void VisitBinop(InstructionSelectorT<Adapter>* selector, Node* node,
 }  // namespace
 
 template <typename Adapter>
-void InstructionSelectorT<Adapter>::VisitStackSlot(Node* node) {
-  StackSlotRepresentation rep = StackSlotRepresentationOf(node->op());
-  int slot = frame_->AllocateSpillSlot(rep.size(), rep.alignment());
-  OperandGenerator g(this);
+void InstructionSelectorT<Adapter>::VisitStackSlot(node_t node) {
+  if constexpr (Adapter::IsTurboshaft) {
+    UNIMPLEMENTED();
+  } else {
+    StackSlotRepresentation rep = StackSlotRepresentationOf(node->op());
+    int slot = frame_->AllocateSpillSlot(rep.size(), rep.alignment());
+    OperandGenerator g(this);
 
-  Emit(kArchStackSlot, g.DefineAsRegister(node),
-       sequence()->AddImmediate(Constant(slot)), 0, nullptr);
+    Emit(kArchStackSlot, g.DefineAsRegister(node),
+         sequence()->AddImmediate(Constant(slot)), 0, nullptr);
+  }
 }
 
 template <typename Adapter>
