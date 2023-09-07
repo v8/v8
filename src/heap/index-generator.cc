5e24c3a271cae202faa93a66d78f07e8e57362a6
Nikolaos Papaspyrou <nikolaos@chromium.org>
2023-09-07 13:46:53 +0200

    Merged: [heap] Refactor IndexGenerator
    Since https://crrev.com/c/4594399, the IndexGenerator does not support
    the GiveBack method, so its implementation can be further simplified by
    entirely removing the stack.
    
    Bug: chromium:1479473
    (cherry picked from commit 04a5fbd7fcb7ea7bd83c64c15e7346735a4f9de4)
    
    Change-Id: I90ff14452e4d2d4937afee8b0383e9747d17404b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4850738
    Auto-Submit: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/11.8@{#2}
    Cr-Branched-From: 935bdbf34c05332eb418be80b48225350b10ec72-refs/heads/11.8.172@{#1}
    Cr-Branched-From: b82a91151f0d8af62ecead91800df26fe5de1479-refs/heads/main@{#89779}

--- src/heap/index-generator.cc
+++ src/heap/index-generator.cc
@@ -7,20 +7,17 @@
 namespace v8 {
 namespace internal {
 
-IndexGenerator::IndexGenerator(size_t size) : size_(size) {
+IndexGenerator::IndexGenerator(size_t size) : first_use_(size > 0) {
   if (size == 0) return;
   base::MutexGuard guard(&lock_);
-  pending_indices_.push(0);
-  ranges_to_split_.push({0, size_});
+  ranges_to_split_.emplace(0, size);
 }
 
 base::Optional<size_t> IndexGenerator::GetNext() {
   base::MutexGuard guard(&lock_);
-  if (!pending_indices_.empty()) {
-    // Return any pending index first.
-    auto index = pending_indices_.top();
-    pending_indices_.pop();
-    return index;
+  if (first_use_) {
+    first_use_ = false;
+    return 0;
   }
   if (ranges_to_split_.empty()) return base::nullopt;
 
@@ -32,8 +29,8 @@ base::Optional<size_t> IndexGenerator::GetNext() {
   size_t mid = range.first + size / 2;
   // Both sides of the range are added to |ranges_to_split_| so they may be
   // further split if possible.
-  if (mid - range.first > 1) ranges_to_split_.push({range.first, mid});
-  if (range.second - mid > 1) ranges_to_split_.push({mid, range.second});
+  if (mid - range.first > 1) ranges_to_split_.emplace(range.first, mid);
+  if (range.second - mid > 1) ranges_to_split_.emplace(mid, range.second);
   return mid;
 }
 
