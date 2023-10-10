82aa1fd7592c4ddeff7aae2323f5118963d9bd6b
Milad Fa <mfarazma@redhat.com>
2023-10-10 16:12:04 +0000

    PPC/s390: Reland^2 "[liftoff] Add support for direct C calls"
    Port 846b5bfe32b98bfc7750f502d3b00dc0cb98ce25
    
    Original Commit Message:
    
        This is a reland of commit 5fb5de9cfc6c54cf8710db01f395299d5acb435b.
    
        Release builds of ia32 are fixed by adding a `USE` statement.
        UB is fixed by restricting the shift width in the C function.
    
        Original change's description:
        > [liftoff] Add support for direct C calls
        >
        > This adds support for calling C functions and passing parameters
        > according to the C calling conventions. So far we could only call C
        > functions that expect their input in a pointer-referenced
        > stack-allocated buffer.
        >
        > For now, the four bit-rotating C functions are moved over. The same
        > infrastructure will be used for atomic notify in a follow-up, and we can
        > decide to move more functions over to generate smaller code and increase
        > performance.
        >
        > R=dlehmann@chromium.org
        >
        > Bug: v8:14101
        > Change-Id: Ifb85f57afa41f6a77d612fbd20076e37ce10bf99
        > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4905288
        > Commit-Queue: Clemens Backes <clemensb@chromium.org>
        > Reviewed-by: Daniel Lehmann <dlehmann@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#90308}
    
    R=clemensb@chromium.org, joransiu@ca.ibm.com, junyan@redhat.com, midawson@redhat.com
    BUG=
    LOG=N
    
    Change-Id: Icf03f966e8ee9e9a3abeab210a8dceeeded04fba
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4926611
    Commit-Queue: Milad Farazmand <mfarazma@redhat.com>
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Cr-Commit-Position: refs/heads/main@{#90343}

--- src/wasm/baseline/ppc/liftoff-assembler-ppc-inl.h
+++ src/wasm/baseline/ppc/liftoff-assembler-ppc-inl.h
@@ -9,6 +9,7 @@
 #include "src/codegen/assembler.h"
 #include "src/heap/memory-chunk.h"
 #include "src/wasm/baseline/liftoff-assembler.h"
+#include "src/wasm/baseline/parallel-move-inl.h"
 #include "src/wasm/object-access.h"
 #include "src/wasm/simd-shuffle.h"
 #include "src/wasm/wasm-objects.h"
@@ -2659,6 +2660,24 @@ void LiftoffAssembler::CallCWithStackBuffer(
     }
   }
   AddS64(sp, sp, Operand(total_size), r0);
+}
+
+void LiftoffAssembler::CallC(const std::initializer_list<VarState> args,
+                             ExternalReference ext_ref) {
+  constexpr Register kArgRegs[] = {arg_reg_1, arg_reg_2, arg_reg_3, arg_reg_4};
+  DCHECK_LE(args.size(), arraysize(kArgRegs));
+  const Register* next_arg_reg = kArgRegs;
+  ParallelMove parallel_move{this};
+  for (const VarState& arg : args) {
+    parallel_move.LoadIntoRegister(LiftoffRegister{*next_arg_reg}, arg);
+    ++next_arg_reg;
+  }
+  parallel_move.Execute();
+
+  // Now call the C function.
+  int num_args = static_cast<int>(args.size());
+  PrepareCallCFunction(num_args, r0);
+  CallCFunction(ext_ref, num_args);
 }
 
 void LiftoffAssembler::CallNativeWasmCode(Address addr) {
