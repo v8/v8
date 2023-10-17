c7a55f9b0b6e65509f62b400f24111b3db0fdbe4
Jeroen Bobbeldijk <jerbob92@gmail.com>
2023-10-17 16:26:55 +0200

    Fix ReadELFHWCaps method for ARM(64) builds on old/non glibc versions
    Fixed: v8:14381
    Change-Id: I7299e47a62e699a7be073006018c080008e283bc
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4946852
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#90458}

--- src/base/cpu.cc
+++ src/base/cpu.cc
@@ -175,7 +175,7 @@ static V8_INLINE void __cpuid(int cpu_info[4], int info_type) {
 static std::tuple<uint32_t, uint32_t> ReadELFHWCaps() {
   uint32_t hwcap = 0;
   uint32_t hwcap2 = 0;
-#if defined(AT_HWCAP)
+#if (V8_GLIBC_PREREQ(2, 16) || V8_OS_ANDROID) && defined(AT_HWCAP)
   hwcap = static_cast<uint32_t>(getauxval(AT_HWCAP));
 #if defined(AT_HWCAP2)
   hwcap2 = static_cast<uint32_t>(getauxval(AT_HWCAP2));
@@ -196,7 +196,7 @@ static std::tuple<uint32_t, uint32_t> ReadELFHWCaps() {
         break;
       }
       if (entry.tag == AT_HWCAP) {
-        result = entry.value;
+        hwcap = entry.value;
         break;
       }
     }
