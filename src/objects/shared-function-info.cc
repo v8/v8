eaa486909959e530ff157b258654fa0932225aa5
Seth Brenith <seth.brenith@microsoft.com>
2023-10-27 10:11:29 -0700

    Add fast path in SharedFunctionInfo::PassesFilter
    In a recent profiler recording, I saw that V8 was wasting about 0.4% of
    main-thread time running SharedFunctionInfo::DebugNameCStr. This change
    avoids calling that function unless we actually need the result.
    
    Change-Id: I8060f4978d054d1689b3d6e608df601f844a3076
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4984765
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Seth Brenith <seth.brenith@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#90671}

--- src/objects/shared-function-info.cc
+++ src/objects/shared-function-info.cc
@@ -318,6 +318,10 @@ Handle<String> SharedFunctionInfo::DebugName(
 }
 
 bool SharedFunctionInfo::PassesFilter(const char* raw_filter) {
+  // Filters are almost always "*", so check for that and exit quickly.
+  if (V8_LIKELY(raw_filter[0] == '*' && raw_filter[1] == '\0')) {
+    return true;
+  }
   base::Vector<const char> filter = base::CStrVector(raw_filter);
   return v8::internal::PassesFilter(base::CStrVector(DebugNameCStr().get()),
                                     filter);
