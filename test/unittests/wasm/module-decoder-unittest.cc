817f17ec6bc781e7ade6c7b854f16134e03a33a1
Clemens Backes <clemensb@chromium.org>
2023-10-30 12:21:48 +0100

    [wasm] Fix canonicalization of empty recursion group
    Empty recursion groups are allowed according to the spec. This fixes a
    DCHECK that failed when we tried to canonicalize one.
    
    R=manoskouk@chromium.org
    
    Fixed: chromium:1495855
    Change-Id: I69d8a89bca207457ba30ba79863b211d7b8b8c91
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4987308
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#90665}

--- test/unittests/wasm/module-decoder-unittest.cc
+++ test/unittests/wasm/module-decoder-unittest.cc
@@ -1052,6 +1052,13 @@ TEST_F(WasmModuleVerifyTest, TypeCanonicalization) {
   EXPECT_FAILURE_WITH_MSG(
       non_identical_group,
       "type error in constant expression[0] (expected (ref 0), got (ref 1))");
+
+  static const uint8_t empty_group[] = {
+      SECTION(Type,            // --
+              ENTRY_COUNT(1),  // one rec. group
+              kWasmRecursiveTypeGroupCode, ENTRY_COUNT(0))};
+
+  EXPECT_VERIFIES(empty_group);
 }
 
 // Tests that all types in a rec. group are checked for supertype validity.
