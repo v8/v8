358dfc64f90ba62b7b65942468419d5c3f177313
Clemens Backes <clemensb@chromium.org>
2023-11-03 14:49:53 +0100

    [wasm] Add a fast path for type checking
    TypeCheckStackAgainstMerge is called a lot, and often in trivial
    situations (like TypeCheckFallThru with no result at the merge).
    Since the general TypeCheckStackAgainstMerge implementation is pretty
    involved, clang does not inline it and we spend ~6% of validation time
    in that method.
    This CL adds a fast path implementation for the case that we have no
    merge values, or exactly one merge value with matching type. For MVP
    modules this hits in nearly 100% of cases, speeding up validation by
    roughly 5% overall.
    
    R=jkummerow@chromium.org
    
    Bug: v8:14402
    Change-Id: Ia09a6a635207701d10258bdca489bf29476bb254
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5001640
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#90787}

--- src/wasm/function-body-decoder-impl.h
+++ src/wasm/function-body-decoder-impl.h
@@ -6156,7 +6156,25 @@ class WasmFullDecoder : public WasmDecoder<ValidationTag, decoding_mode> {
   //   introduction of subtyping.
   template <StackElementsCountMode strict_count, bool push_branch_values,
             MergeType merge_type>
-  bool TypeCheckStackAgainstMerge(Merge<Value>* merge) {
+  V8_INLINE bool TypeCheckStackAgainstMerge(Merge<Value>* merge) {
+    uint32_t arity = merge->arity;
+    uint32_t actual = stack_.size() - control_.back().stack_depth;
+    // Handle trivial cases first. Arity 0 is the most common case.
+    if (arity == 0 && (!strict_count || actual == 0)) return true;
+    // Arity 1 is still common enough that we handle it separately (only doing
+    // the most basic subtype check).
+    if (arity == 1 && (strict_count ? actual == arity : actual >= arity)) {
+      if (stack_.back().type == merge->vals.first.type) return true;
+    }
+    return TypeCheckStackAgainstMerge_Slow<strict_count, push_branch_values,
+                                           merge_type>(merge);
+  }
+
+  // Slow path for {TypeCheckStackAgainstMerge}.
+  template <StackElementsCountMode strict_count, bool push_branch_values,
+            MergeType merge_type>
+  V8_PRESERVE_MOST V8_NOINLINE bool TypeCheckStackAgainstMerge_Slow(
+      Merge<Value>* merge) {
     constexpr const char* merge_description =
         merge_type == kBranchMerge     ? "branch"
         : merge_type == kReturnMerge   ? "return"
