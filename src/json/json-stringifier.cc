7941062aa9f264a72617be772de2b8dffdffc54a
Camillo Bruni <cbruni@chromium.org>
2023-11-07 15:10:37 +0100

    [json] Avoid FlatStringReader and use GetCharVector directly
    We know the character width, so there is not need to use the
    FlatStringReader helper,=.
    
    Drive-by-fix:
    - Dehandlify more code
    
    Change-Id: I238d7c67d7c39f82a1a512efaa74437973c88abb
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4897276
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#90817}

--- src/json/json-stringifier.cc
+++ src/json/json-stringifier.cc
@@ -299,12 +299,14 @@ class JsonStringifier {
 
   // Returns whether any escape sequences were used.
   template <typename SrcChar, typename DestChar, bool raw_json>
-  V8_INLINE bool SerializeString_(Handle<String> string);
+  V8_INLINE bool SerializeString_(Tagged<String> string,
+                                  const DisallowGarbageCollection& no_gc);
 
   // Tries to do fast-path serialization for a property key, and returns whether
   // it was successful.
   template <typename DestChar>
-  bool TrySerializeSimplePropertyKey(Tagged<String> string);
+  bool TrySerializeSimplePropertyKey(Tagged<String> string,
+                                     const DisallowGarbageCollection& no_gc);
 
   template <typename Char>
   V8_INLINE static bool DoNotEscape(Char c);
@@ -1313,24 +1315,23 @@ bool JsonStringifier::SerializeStringUnchecked_(
 }
 
 template <typename SrcChar, typename DestChar, bool raw_json>
-bool JsonStringifier::SerializeString_(Handle<String> string) {
+bool JsonStringifier::SerializeString_(Tagged<String> string,
+                                       const DisallowGarbageCollection& no_gc) {
   int length = string->length();
   bool required_escaping = false;
   if (!raw_json) Append<uint8_t, DestChar>('"');
   // We might be able to fit the whole escaped string in the current string
   // part, or we might need to allocate.
+  base::Vector<const SrcChar> vector = string->GetCharVector<SrcChar>(no_gc);
   if V8_LIKELY (EscapedLengthIfCurrentPartFits(length)) {
-    DisallowGarbageCollection no_gc;
-    base::Vector<const SrcChar> vector = string->GetCharVector<SrcChar>(no_gc);
     NoExtendBuilder<DestChar> no_extend(
         reinterpret_cast<DestChar*>(part_ptr_) + current_index_,
         &current_index_);
     required_escaping = SerializeStringUnchecked_<SrcChar, DestChar, raw_json>(
         vector, &no_extend);
   } else {
-    FlatStringReader reader(isolate_, string);
-    for (int i = 0; i < reader.length(); i++) {
-      SrcChar c = reader.Get<SrcChar>(i);
+    for (int i = 0; i < vector.length(); i++) {
+      SrcChar c = vector.at(i);
       if (raw_json || DoNotEscape(c)) {
         Append<SrcChar, DestChar>(c);
       } else if (sizeof(SrcChar) != 1 &&
@@ -1340,9 +1341,9 @@ bool JsonStringifier::SerializeString_(Handle<String> string) {
         required_escaping = true;
         if (c <= 0xDBFF) {
           // The current character is a leading surrogate.
-          if (i + 1 < reader.length()) {
+          if (i + 1 < vector.length()) {
             // There is a next character.
-            SrcChar next = reader.Get<SrcChar>(i + 1);
+            SrcChar next = vector.at(i + 1);
             if (base::IsInRange(next, static_cast<SrcChar>(0xDC00),
                                 static_cast<SrcChar>(0xDFFF))) {
               // The next character is a trailing surrogate, meaning this is a
@@ -1388,8 +1389,8 @@ bool JsonStringifier::SerializeString_(Handle<String> string) {
 }
 
 template <typename DestChar>
-bool JsonStringifier::TrySerializeSimplePropertyKey(Tagged<String> key) {
-  DisallowGarbageCollection no_gc;
+bool JsonStringifier::TrySerializeSimplePropertyKey(
+    Tagged<String> key, const DisallowGarbageCollection& no_gc) {
   ReadOnlyRoots roots(isolate_);
   if (key->map(isolate_) != roots.internalized_one_byte_string_map()) {
     return false;
@@ -1466,10 +1467,14 @@ void JsonStringifier::SerializeDeferredKey(bool deferred_comma,
                                            Handle<Object> deferred_key) {
   Separator(!deferred_comma);
   Handle<String> string_key = Handle<String>::cast(deferred_key);
-  bool wrote_simple =
-      encoding_ == String::ONE_BYTE_ENCODING
-          ? TrySerializeSimplePropertyKey<uint8_t>(*string_key)
-          : TrySerializeSimplePropertyKey<base::uc16>(*string_key);
+  bool wrote_simple = false;
+  {
+    DisallowGarbageCollection no_gc;
+    wrote_simple =
+        encoding_ == String::ONE_BYTE_ENCODING
+            ? TrySerializeSimplePropertyKey<uint8_t>(*string_key, no_gc)
+            : TrySerializeSimplePropertyKey<base::uc16>(*string_key, no_gc);
+  }
 
   if (!wrote_simple) {
     bool required_escaping = SerializeString<false>(string_key);
@@ -1485,19 +1490,20 @@ void JsonStringifier::SerializeDeferredKey(bool deferred_comma,
 template <bool raw_json>
 bool JsonStringifier::SerializeString(Handle<String> object) {
   object = String::Flatten(isolate_, object);
+  DisallowGarbageCollection no_gc;
+  auto string = *object;
   if (encoding_ == String::ONE_BYTE_ENCODING) {
-    if (String::IsOneByteRepresentationUnderneath(*object)) {
-      return SerializeString_<uint8_t, uint8_t, raw_json>(object);
+    if (String::IsOneByteRepresentationUnderneath(string)) {
+      return SerializeString_<uint8_t, uint8_t, raw_json>(string, no_gc);
     } else {
       ChangeEncoding();
-      return SerializeString<raw_json>(object);
     }
+  }
+  DCHECK_EQ(encoding_, String::TWO_BYTE_ENCODING);
+  if (String::IsOneByteRepresentationUnderneath(string)) {
+    return SerializeString_<uint8_t, base::uc16, raw_json>(string, no_gc);
   } else {
-    if (String::IsOneByteRepresentationUnderneath(*object)) {
-      return SerializeString_<uint8_t, base::uc16, raw_json>(object);
-    } else {
-      return SerializeString_<base::uc16, base::uc16, raw_json>(object);
-    }
+    return SerializeString_<base::uc16, base::uc16, raw_json>(string, no_gc);
   }
 }
 
