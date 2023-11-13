bec0fd95370043d3c1830356965d800be356056e
Victor Gomes <victorgomes@chromium.org>
2023-11-13 10:42:34 +0100

    [lazy-compilation] Clang tidy: bugprone inaccurate erase
    Spotted by Google3 Clang Tidy.
    
    See https://clang.llvm.org/extra/clang-tidy/checks/bugprone/inaccurate-erase.html
    
    Change-Id: I8d4041f8425cd30dc481690b07157318a5bd3dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5022953
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#90954}

--- src/compiler-dispatcher/lazy-compile-dispatcher.cc
+++ src/compiler-dispatcher/lazy-compile-dispatcher.cc
@@ -210,7 +210,8 @@ void LazyCompileDispatcher::WaitForJobIfRunningOnBackground(
       // vector.
       pending_background_jobs_.erase(
           std::remove(pending_background_jobs_.begin(),
-                      pending_background_jobs_.end(), job));
+                      pending_background_jobs_.end(), job),
+          pending_background_jobs_.end());
       job->state = Job::State::kPendingToRunOnForeground;
       NotifyRemovedBackgroundJob(lock);
     } else {
@@ -222,7 +223,8 @@ void LazyCompileDispatcher::WaitForJobIfRunningOnBackground(
       // TODO(leszeks): Remove from finalizable jobs without walking the whole
       // vector.
       finalizable_jobs_.erase(
-          std::remove(finalizable_jobs_.begin(), finalizable_jobs_.end(), job));
+          std::remove(finalizable_jobs_.begin(), finalizable_jobs_.end(), job),
+          finalizable_jobs_.end());
       job->state = Job::State::kFinalizingNow;
     }
     return;
@@ -240,7 +242,8 @@ void LazyCompileDispatcher::WaitForJobIfRunningOnBackground(
   // TODO(leszeks): Remove from finalizable jobs without walking the whole
   // vector.
   finalizable_jobs_.erase(
-      std::remove(finalizable_jobs_.begin(), finalizable_jobs_.end(), job));
+      std::remove(finalizable_jobs_.begin(), finalizable_jobs_.end(), job),
+      finalizable_jobs_.end());
   job->state = Job::State::kFinalizingNow;
 }
 
@@ -316,7 +319,8 @@ void LazyCompileDispatcher::AbortJob(Handle<SharedFunctionInfo> shared_info) {
 
       pending_background_jobs_.erase(
           std::remove(pending_background_jobs_.begin(),
-                      pending_background_jobs_.end(), job));
+                      pending_background_jobs_.end(), job),
+          pending_background_jobs_.end());
       job->state = Job::State::kAbortingNow;
       NotifyRemovedBackgroundJob(lock);
     } else if (job->state == Job::State::kReadyToFinalize) {
@@ -325,7 +329,8 @@ void LazyCompileDispatcher::AbortJob(Handle<SharedFunctionInfo> shared_info) {
           1);
 
       finalizable_jobs_.erase(
-          std::remove(finalizable_jobs_.begin(), finalizable_jobs_.end(), job));
+          std::remove(finalizable_jobs_.begin(), finalizable_jobs_.end(), job),
+          finalizable_jobs_.end());
       job->state = Job::State::kAbortingNow;
     } else {
       UNREACHABLE();
