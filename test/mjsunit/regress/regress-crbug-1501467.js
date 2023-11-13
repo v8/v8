449af38f0c9c942a5e052c3f5e4f592717728ab0
Olivier Flückiger <olivf@chromium.org>
2023-11-13 13:55:01 +0100

    [ml] Cleanup regression test
    Use %OptimizeOsr instead of long loop.
    
    Bug: chromium:1501467
    Change-Id: I114669f8bc5cd37df2a7559a66679c3691cb5828
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5024197
    Auto-Submit: Olivier Flückiger <olivf@chromium.org>
    Commit-Queue: Olivier Flückiger <olivf@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#90970}

--- test/mjsunit/regress/regress-crbug-1501467.js
+++ test/mjsunit/regress/regress-crbug-1501467.js
@@ -2,16 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function __f_1(__v_19) {
+// Flags: --allow-natives-syntax
+
+function __f_1(__v_191) {
     var __v_17 = 0;
-    var __v_18 = __v_19 ? 1 : 2147483648;
-    for (var __v_19 = 0; __v_19 < 100000; __v_19++) {
+    var __v_18 = __v_191 ? 1 : 2147483648;
+    for (var __v_19 = 0; __v_19 < 20; __v_19++) {
+        if (__v_191) %OptimizeOsr();
         __v_17 += __v_18;
     }
   return __v_17 + __v_18;
 }
+%PrepareFunctionForOptimization(__f_1);
+
 function __f_2(__v_20, __v_22) {
     __v_20(true);
-    assertEquals(214750512283648, __v_20());
+    assertEquals(45097156608, __v_20());
 }
   __f_2(__f_1);
