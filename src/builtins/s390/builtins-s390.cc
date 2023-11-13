d62459f10401390ede081ed9701a8300ec26c48f
Milad Fa <mfarazma@redhat.com>
2023-11-13 12:15:41 -0500

    PPC/s390: Reland^3 "[wasm][x64] Run JS imports on the central stack"
    Port be830f359c50f21db867f7e6f430124af314e0db
    
    Original Commit Message:
    
        This is a reland of commit 2d6e2bd5293624379214d831cdf82a336b820828
        The stack-switching test uses %SimulateNewspaceFull, which is
        incompatible with the single-generation flag, so just skip the test
        in this case.
    
        Original change's description:
        > Reland "Reland "[wasm][x64] Run JS imports on the central stack""
        >
        > This is a reland of commit 4be9ff946b727ea05e6049c72ad62f5c83f6a13c
        >
        > Builtins should have the same IR shape on all platforms for PGO. The
        > wasm-to-js torque wrapper was different on x64 because of a compile-time
        > guard, which is fixed by turning it into an IR check. The check will
        > eventually be removed when this is ported to other archs.
        >
        > Original change's description:
        > > Reland "[wasm][x64] Run JS imports on the central stack"
        > >
        > > This is a reland of commit 9d9f49d2018c6e268fb4c5a484f6de26ba251b12
        > >
        > > The reland includes:
        > >
        > > - Ignore secondary stacks for conservative scanning. Now that we always
        > >   switch to the central stack, all "interesting" frames are on the
        > >   central stack.  Without this change, the marker is not in sync with
        > >   the stack start because of the switch to the central stack.
        > > - Merging 8bbc90166d0dd1bb2037f89207aff00373a42584 into this change,
        > >   because landing them separately also breaks the CSS bot. I think this
        > >   was already broken before but reproduces more easily with this change.
        > >   The problem is that because of the switch to the central stack, the
        > >   marker is not in sync with the registered secondary stack start.
        > > - Also switch in the generic (torque) wrapper.
        > >
        > > Original change's description:
        > > > [wasm][x64] Run JS imports on the central stack
        > > >
        > > > Switch to the central stack before running a JS import, and switch
        > > > back after the call. Only on x64 for now, will be ported to other archs
        > > > in a separate CL.
        > > >
        > > > Design doc: https://docs.google.com/document/d/1DBVwKthBdejx9Qy1W8indme5pJ6mq_fSdhQ3N-K2XP4/edit?usp=sharing
        > > >
        > > > R=ahaas@chromium.org
        > > > CC=irezvov@chromium.org
        > > >
        > > > Bug: v8:13983
        > > > Change-Id: I217ef988f46bb9a747cc65a37688817a4abf9618
        > > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4846976
        > > > Reviewed-by: Jakob Linke <jgruber@chromium.org>
        > > > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
        > > > Reviewed-by: Andreas Haas <ahaas@chromium.org>
        > > > Cr-Commit-Position: refs/heads/main@{#90041}
        > >
        > > Bug: v8:13983
        > > Change-Id: I75492e0b26955c66ba59d4f19ef19764c516fcac
        > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4875537
        > > Reviewed-by: Igor Sheludko <ishell@chromium.org>
        > > Reviewed-by: Andreas Haas <ahaas@chromium.org>
        > > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
        > > Cr-Commit-Position: refs/heads/main@{#90864}
        >
        > Bug: v8:13983
        > Change-Id: I4808e17bcaafc309bd0c40c0705bee3fa52528c0
        > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5022950
        > Reviewed-by: Igor Sheludko <ishell@chromium.org>
        > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
        > Cr-Commit-Position: refs/heads/main@{#90879}
    
    R=thibaudm@chromium.org, joransiu@ca.ibm.com, junyan@redhat.com, midawson@redhat.com
    BUG=
    LOG=N
    
    Change-Id: I7c9d61e5da2e5dfd46e1b5385894dbaae7e37d92
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5024956
    Commit-Queue: Milad Farazmand <mfarazma@redhat.com>
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Cr-Commit-Position: refs/heads/main@{#90901}

--- src/builtins/s390/builtins-s390.cc
+++ src/builtins/s390/builtins-s390.cc
@@ -3150,8 +3150,14 @@ void Builtins::Generate_WasmToJsWrapperAsm(MacroAssembler* masm) {
     gp_regs.set(wasm::kGpParamRegisters[i]);
   }
   __ MultiPush(gp_regs);
-  // Push an arbitrary register to reserve stack space for the signature which
-  // will be spilled on the stack in Torque.
+  // Reserve fixed slots for the CSA wrapper.
+  // Two slots for stack-switching (central stack pointer and secondary stack
+  // limit):
+  Register scratch = r3;
+  __ mov(scratch, Operand::Zero());
+  __ Push(scratch);
+  __ Push(scratch);
+  // One slot for the signature:
   __ Push(r0);
   __ TailCallBuiltin(Builtin::kWasmToJsWrapperCSA);
 }
