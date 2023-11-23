acc1917b9da42e29e033aa4b7c4b3bdcfd8ccf23
Igor Sheludko <ishell@chromium.org>
2023-11-23 11:10:00 +0100

    [maglev] Enable inlining of Api call builtin behind --future
    Bug: chromium:1445925
    Change-Id: Icf9774b829835477be55567cea299830eea89571
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5055809
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91137}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -549,6 +549,7 @@ DEFINE_BOOL(maglev_inline_api_calls, false,
 DEFINE_WEAK_IMPLICATION(maglev_future, maglev_inlining)
 DEFINE_WEAK_NEG_IMPLICATION(maglev_future, maglev_loop_peeling_only_trivial)
 DEFINE_WEAK_IMPLICATION(maglev_future, maglev_speculative_hoist_phi_untagging)
+DEFINE_WEAK_IMPLICATION(future, maglev_inline_api_calls)
 // This might be too big of a hammer but we must prohibit moving the C++
 // trampolines while we are executing a C++ code.
 DEFINE_NEG_IMPLICATION(maglev_inline_api_calls, compact_code_space_with_stack)
