455faa2809d10c4cd5f644afd6e7938a01ada5f3
Thibaud Michaud <thibaudm@chromium.org>
2023-11-29 15:48:40 +0100

    [wasm][jspi][arm64] Add comment for reserved frame slots
    The comment is copied from x64 codegen. Also add a reference to the
    corresponding frame constants in the form of a static assert.
    
    R=nicohartmann@chromium.org
    CC=clemensb@chromium.org
    
    Bug: chromium:1505048
    Change-Id: I7215c04c69af6ff08110e0b15fc56b159f29c09d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5071553
    Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91252}

--- src/compiler/backend/arm64/code-generator-arm64.cc
+++ src/compiler/backend/arm64/code-generator-arm64.cc
@@ -3299,6 +3299,13 @@ void CodeGenerator::AssembleConstructFrame() {
         // The C-API function has one extra slot for the PC.
         required_slots++;
       } else if (call_descriptor->IsWasmImportWrapper()) {
+        // If the wrapper is running on a secondary stack, it will switch to the
+        // central stack and fill these slots with the central stack pointer and
+        // secondary stack limit. Otherwise the slots remain empty.
+        static_assert(WasmImportWrapperFrameConstants::kCentralStackSPOffset ==
+                      -24);
+        static_assert(
+            WasmImportWrapperFrameConstants::kSecondaryStackLimitOffset == -32);
         __ Push(xzr, xzr);
       }
 #endif  // V8_ENABLE_WEBASSEMBLY
