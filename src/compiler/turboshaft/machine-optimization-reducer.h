33dab337eaafb4c795adf82f4efd62e86bc164ab
Darius M <dmercadier@chromium.org>
2023-11-29 12:43:21 +0100

    [turboshaft] Normalize loads whose base is addition and index is invalid
    This make the instruction selector generate better code (because the
    addition is computed with a complex memory operand rather than before
    the load).
    
    Additionally, this makes the TS graphs more normalized, which could be
    good in particular for LoadElimination and StoreStoreElimination, and
    any alias analysis we may want to do.
    
    Bug: v8:12783
    Change-Id: I08f75d83c972f2fd60c064fce2ab944f0541f7ec
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5068362
    Auto-Submit: Darius Mercadier <dmercadier@chromium.org>
    Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    Commit-Queue: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91249}

--- src/compiler/turboshaft/machine-optimization-reducer.h
+++ src/compiler/turboshaft/machine-optimization-reducer.h
@@ -28,6 +28,7 @@
 #include "src/compiler/machine-operator-reducer.h"
 #include "src/compiler/turboshaft/assembler.h"
 #include "src/compiler/turboshaft/operations.h"
+#include "src/compiler/turboshaft/opmasks.h"
 #include "src/compiler/turboshaft/representations.h"
 #include "src/handles/handles.h"
 #include "src/numbers/conversions.h"
@@ -1657,42 +1658,57 @@ class MachineOptimizationReducer : public Next {
     goto no_change;
   }
 
-  OpIndex REDUCE(Store)(OpIndex base, OptionalOpIndex index, OpIndex value,
+  OpIndex REDUCE(Store)(OpIndex base_idx, OptionalOpIndex index, OpIndex value,
                         StoreOp::Kind kind, MemoryRepresentation stored_rep,
                         WriteBarrierKind write_barrier, int32_t offset,
                         uint8_t element_scale,
                         bool maybe_initializing_or_transitioning,
                         IndirectPointerTag maybe_indirect_pointer_tag) {
-    if (!ShouldSkipOptimizationStep()) {
-      if (stored_rep.SizeInBytes() <= 4) {
-        value = TryRemoveWord32ToWord64Conversion(value);
-      }
-      index =
-          ReduceMemoryIndex(index.value_or_invalid(), &offset, &element_scale);
-      switch (stored_rep) {
-        case MemoryRepresentation::Uint8():
-        case MemoryRepresentation::Int8():
-          value =
-              ReduceWithTruncation(value, std::numeric_limits<uint8_t>::max(),
-                                   WordRepresentation::Word32());
-          break;
-        case MemoryRepresentation::Uint16():
-        case MemoryRepresentation::Int16():
-          value =
-              ReduceWithTruncation(value, std::numeric_limits<uint16_t>::max(),
-                                   WordRepresentation::Word32());
-          break;
-        case MemoryRepresentation::Uint32():
-        case MemoryRepresentation::Int32():
-          value =
-              ReduceWithTruncation(value, std::numeric_limits<uint32_t>::max(),
-                                   WordRepresentation::Word32());
-          break;
-        default:
-          break;
-      }
+    LABEL_BLOCK(no_change) {
+      return Next::ReduceStore(base_idx, index, value, kind, stored_rep,
+                               write_barrier, offset, element_scale,
+                               maybe_initializing_or_transitioning,
+                               maybe_indirect_pointer_tag);
     }
-    return Next::ReduceStore(base, index, value, kind, stored_rep,
+    if (ShouldSkipOptimizationStep()) goto no_change;
+
+    if (stored_rep.SizeInBytes() <= 4) {
+      value = TryRemoveWord32ToWord64Conversion(value);
+    }
+    index =
+        ReduceMemoryIndex(index.value_or_invalid(), &offset, &element_scale);
+    switch (stored_rep) {
+      case MemoryRepresentation::Uint8():
+      case MemoryRepresentation::Int8():
+        value = ReduceWithTruncation(value, std::numeric_limits<uint8_t>::max(),
+                                     WordRepresentation::Word32());
+        break;
+      case MemoryRepresentation::Uint16():
+      case MemoryRepresentation::Int16():
+        value =
+            ReduceWithTruncation(value, std::numeric_limits<uint16_t>::max(),
+                                 WordRepresentation::Word32());
+        break;
+      case MemoryRepresentation::Uint32():
+      case MemoryRepresentation::Int32():
+        value =
+            ReduceWithTruncation(value, std::numeric_limits<uint32_t>::max(),
+                                 WordRepresentation::Word32());
+        break;
+      default:
+        break;
+    }
+
+    // If index is invalid and base is `left+right`, we use `left` as base and
+    // `right` as index.
+    if (!index.valid() && matcher.Is<Opmask::kWord64Add>(base_idx)) {
+      DCHECK_EQ(element_scale, 0);
+      const WordBinopOp& base = matcher.Cast<WordBinopOp>(base_idx);
+      base_idx = base.left();
+      index = base.right();
+    }
+
+    return Next::ReduceStore(base_idx, index, value, kind, stored_rep,
                              write_barrier, offset, element_scale,
                              maybe_initializing_or_transitioning,
                              maybe_indirect_pointer_tag);
@@ -1702,8 +1718,13 @@ class MachineOptimizationReducer : public Next {
                        LoadOp::Kind kind, MemoryRepresentation loaded_rep,
                        RegisterRepresentation result_rep, int32_t offset,
                        uint8_t element_scale) {
+    LABEL_BLOCK(no_change) {
+      return Next::ReduceLoad(base_idx, index, kind, loaded_rep, result_rep,
+                              offset, element_scale);
+    }
+    if (ShouldSkipOptimizationStep()) goto no_change;
+
     while (true) {
-      if (ShouldSkipOptimizationStep()) break;
       index =
           ReduceMemoryIndex(index.value_or_invalid(), &offset, &element_scale);
       if (!kind.tagged_base && !index.valid()) {
@@ -1717,8 +1738,8 @@ class MachineOptimizationReducer : public Next {
       }
       break;
     }
-    if (!index.valid() && matcher.Is<ConstantOp>(base_idx) &&
-        !ShouldSkipOptimizationStep()) {
+
+    if (!index.valid() && matcher.Is<ConstantOp>(base_idx)) {
       const ConstantOp& base = matcher.Cast<ConstantOp>(base_idx);
       if (base.kind == any_of(ConstantOp::Kind::kHeapObject,
                               ConstantOp::Kind::kCompressedHeapObject)) {
@@ -1754,6 +1775,16 @@ class MachineOptimizationReducer : public Next {
         // unreachable code or not)
       }
     }
+
+    // If index is invalid and base is `left+right`, we use `left` as base and
+    // `right` as index.
+    if (!index.valid() && matcher.Is<Opmask::kWord64Add>(base_idx)) {
+      DCHECK_EQ(element_scale, 0);
+      const WordBinopOp& base = matcher.Cast<WordBinopOp>(base_idx);
+      base_idx = base.left();
+      index = base.right();
+    }
+
     return Next::ReduceLoad(base_idx, index, kind, loaded_rep, result_rep,
                             offset, element_scale);
   }
