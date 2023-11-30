0a1519f2ab24f8837b98135201b5c787cbd5b7d6
Daniel Lehmann <dlehmann@chromium.org>
2023-11-30 14:45:38 +0100

    [turboshaft] Optimize allocation in NewBlock
    A couple minor changes that improve performance when creating new blocks
    in Turboshaft during graph building or optimizations:
    - Allocate backing storage for the full remaining capacity of
    `all_blocks_` when growing, not just for 64 blocks at a time.
    - Only allocate the space when growing, initilize later once when
    that block is added to `all_blocks_`.
    - Save (now superfluous) capacity field of `all_blocks_` by switching
    from `ZoneVector` to a Zone-allocated `base::Vector`.
    - This also avoids the capacity check when appending to `all_blocks_`.
    - Explicitly outline the slow growing case.
    
    Locally, I measured (eagerly compiling all functions in the PSWeb Wasm
    binary with Turboshaft, N=20 repetitions, x64 workstation):
    - About 35% reduction in samples attributed to `NewBlock`.
    - About 2% reduction of the time reported by --turbo-stats-wasm for
    `V8.WasmInitializing`.
    - About 0.55% reduction in total time reported by --turbo-stats-wasm.
    - About 0.6% reduction in wall clock compile time.
    
    Change-Id: Ie67762fe8fad3e81c3c542d99fa4def26262f2e7
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5076897
    Commit-Queue: Daniel Lehmann <dlehmann@chromium.org>
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91293}

--- src/compiler/turboshaft/graph.h
+++ src/compiler/turboshaft/graph.h
@@ -305,6 +305,8 @@ class Block : public RandomAccessStackDominatorNode<Block> {
  public:
   enum class Kind : uint8_t { kMerge, kLoopHeader, kBranchTarget };
 
+  explicit Block(Kind kind) : kind_(kind) {}
+
   bool IsLoopOrMerge() const { return IsLoop() || IsMerge(); }
   bool IsLoop() const { return kind_ == Kind::kLoopHeader; }
   bool IsMerge() const { return kind_ == Kind::kMerge; }
@@ -460,8 +462,6 @@ class Block : public RandomAccessStackDominatorNode<Block> {
       std::vector<const char*> tree_symbols = std::vector<const char*>(),
       bool has_next = false) const;
 
-  explicit Block(Kind kind) : kind_(kind) {}
-
   enum class CustomDataKind {
     kUnset,  // No custom data has been set for this block.
     kPhiInputIndex,
@@ -541,7 +541,7 @@ class Graph {
   explicit Graph(Zone* graph_zone, size_t initial_capacity = 2048)
       : operations_(graph_zone, initial_capacity),
         bound_blocks_(graph_zone),
-        all_blocks_(graph_zone),
+        all_blocks_(),
         block_permutation_(graph_zone),
         graph_zone_(graph_zone),
         source_positions_(graph_zone, this),
@@ -558,11 +558,13 @@ class Graph {
   void Reset() {
     operations_.Reset();
     bound_blocks_.clear();
+    // No need to explicitly reset `all_blocks_`, since we will placement-new
+    // new blocks into it, reusing the already allocated backing storage.
+    next_block_ = 0;
     block_permutation_.clear();
     source_positions_.Reset();
     operation_origins_.Reset();
     operation_types_.Reset();
-    next_block_ = 0;
     dominator_tree_depth_ = 0;
 #ifdef DEBUG
     block_type_refinement_.Reset();
@@ -709,15 +711,10 @@ class Graph {
 
   V8_INLINE Block* NewBlock(Block::Kind kind, const Block* origin = nullptr) {
     if (V8_UNLIKELY(next_block_ == all_blocks_.size())) {
-      constexpr size_t new_block_count = 64;
-      base::Vector<Block> blocks =
-          graph_zone_->NewVector<Block>(new_block_count, Block(kind));
-      for (size_t i = 0; i < new_block_count; ++i) {
-        all_blocks_.push_back(&blocks[i]);
-      }
+      AllocateNewBlocks();
     }
     Block* result = all_blocks_[next_block_++];
-    *result = Block(kind);
+    new (result) Block(kind);
 #ifdef DEBUG
     result->graph_generation_ = generation_;
 #endif
@@ -987,8 +984,8 @@ class Graph {
     std::swap(operations_, companion.operations_);
     std::swap(bound_blocks_, companion.bound_blocks_);
     std::swap(all_blocks_, companion.all_blocks_);
-    std::swap(block_permutation_, companion.block_permutation_);
     std::swap(next_block_, companion.next_block_);
+    std::swap(block_permutation_, companion.block_permutation_);
     std::swap(graph_zone_, companion.graph_zone_);
     source_positions_.SwapData(companion.source_positions_);
     operation_origins_.SwapData(companion.operation_origins_);
@@ -1032,14 +1029,49 @@ class Graph {
     }
   }
 
+  // Allocates pointer-stable storage for new blocks, and pushes the pointers
+  // to that storage to `bound_blocks_`. Initialization of the blocks is defered
+  // to when they are actually constructed in `NewBlocks`.
+  V8_NOINLINE V8_PRESERVE_MOST void AllocateNewBlocks() {
+    constexpr size_t kMinCapacity = 32;
+    size_t next_capacity = std::max(kMinCapacity, all_blocks_.size() * 2);
+    size_t new_block_count = next_capacity - all_blocks_.size();
+    DCHECK_GT(new_block_count, 0);
+    base::Vector<Block> block_storage =
+        graph_zone_->AllocateVector<Block>(new_block_count);
+    base::Vector<Block*> new_all_blocks =
+        graph_zone_->AllocateVector<Block*>(next_capacity);
+    DCHECK_EQ(new_all_blocks.size(), all_blocks_.size() + new_block_count);
+    std::copy(all_blocks_.begin(), all_blocks_.end(), new_all_blocks.begin());
+    Block** insert_begin = new_all_blocks.begin() + all_blocks_.size();
+    DCHECK_EQ(insert_begin + new_block_count, new_all_blocks.end());
+    for (size_t i = 0; i < new_block_count; ++i) {
+      insert_begin[i] = &block_storage[i];
+    }
+    base::Vector<Block*> old_all_blocks = all_blocks_;
+    all_blocks_ = new_all_blocks;
+    if (!old_all_blocks.empty()) {
+      graph_zone_->DeleteArray(old_all_blocks.data(), old_all_blocks.length());
+    }
+
+    // Eventually most new blocks will be bound anyway, so pre-allocate as well.
+    DCHECK_LE(bound_blocks_.size(), all_blocks_.size());
+    bound_blocks_.reserve(all_blocks_.size());
+  }
+
   OperationBuffer operations_;
   ZoneVector<Block*> bound_blocks_;
-  ZoneVector<Block*> all_blocks_;
+  // The next two fields essentially form a `ZoneVector` but with pointer
+  // stability for the `Block` elements. That is, `all_blocks_` contains
+  // pointers to (potentially non-contiguous) Zone-allocated `Block`s.
+  // Each pointer in `all_blocks_` points to already allocated space, but they
+  // are only properly value-initialized up to index `next_block_`.
+  base::Vector<Block*> all_blocks_;
+  size_t next_block_ = 0;
   // When `ReorderBlocks` is called, `block_permutation_` contains the original
   // order of blocks in order to provide a proper OpIndex->Block mapping for
   // `BlockOf`. In non-reordered graphs, this vector is empty.
   ZoneVector<Block*> block_permutation_;
-  size_t next_block_ = 0;
   Zone* graph_zone_;
   GrowingOpIndexSidetable<SourcePosition> source_positions_;
   GrowingOpIndexSidetable<OpIndex> operation_origins_;
