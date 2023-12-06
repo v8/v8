59950f40a648d1d1d682101c8b330808ee347e94
Daniel Lehmann <dlehmann@chromium.org>
2023-12-06 21:24:34 +0100

    [utils] Lower default size for ZoneAbslFlatHashMap/Set
    ...down to the value that is used in upstream Abseil (namely 0), see
    https://github.com/abseil/abseil-cpp/blob/d84f20a9f7b2ac0c46d569eeac7ba9306ff62b1b/absl/container/internal/raw_hash_set.h#L1882
    
    On JetStream2, this lowers the total used zone memory as reported by
    --turbo-stats from 5347MB to 5162MB (-3.5%) and also speeds up
    compilation a little (although I would already be happy if it doesn't
    regress).
    
    Change-Id: Ia5d868d26ca4f8d3b9162ce9689e7469b31546b5
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5096865
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Auto-Submit: Daniel Lehmann <dlehmann@chromium.org>
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91394}

--- src/zone/zone-containers.h
+++ src/zone/zone-containers.h
@@ -798,7 +798,7 @@ class ZoneAbslFlatHashMap
                                  ZoneAllocator<std::pair<const K, V>>> {
  public:
   // Constructs an empty map.
-  explicit ZoneAbslFlatHashMap(Zone* zone, size_t bucket_count = 100)
+  explicit ZoneAbslFlatHashMap(Zone* zone, size_t bucket_count = 0)
       : absl::flat_hash_map<K, V, Hash, KeyEqual,
                             ZoneAllocator<std::pair<const K, V>>>(
             bucket_count, Hash(), KeyEqual(),
@@ -815,7 +815,7 @@ class ZoneAbslFlatHashSet
     : public absl::flat_hash_set<K, Hash, KeyEqual, ZoneAllocator<K>> {
  public:
   // Constructs an empty map.
-  explicit ZoneAbslFlatHashSet(Zone* zone, size_t bucket_count = 100)
+  explicit ZoneAbslFlatHashSet(Zone* zone, size_t bucket_count = 0)
       : absl::flat_hash_set<K, Hash, KeyEqual, ZoneAllocator<K>>(
             bucket_count, Hash(), KeyEqual(), ZoneAllocator<K>(zone)) {}
 };
