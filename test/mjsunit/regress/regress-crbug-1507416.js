cf3e066e73eb0ca1dea0694aba33aa62777abef6
Igor Sheludko <ishell@chromium.org>
2023-12-07 11:39:28 +0100

    [builtin][tq] Fix bad DCHECK in Array.prototype.flat
    Bug: v8:14306, chromium:1507416, chromium:1509252
    Change-Id: I48ac9a43c8c9d8469f541b6ab7b715a09e37d3ad
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5097307
    Commit-Queue: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91407}

--- test/mjsunit/regress/regress-crbug-1507416.js
+++ test/mjsunit/regress/regress-crbug-1507416.js
@@ -11,6 +11,19 @@
   assertEquals(array.flatMap(f), [0,1,2,3]);
 })();
 
+(function TestGrow2() {
+  let array = [0,1];
+  let depth = {
+    valueOf() {
+      array.push(2);
+      array.push(3);
+      return 10000;
+    }
+  };
+  assertEquals(array.flat(depth), [0,1]);
+  assertEquals(array, [0,1,2,3]);
+})();
+
 (function TestShrink() {
   let array = [0,1,2,3];
   function f(e) {
