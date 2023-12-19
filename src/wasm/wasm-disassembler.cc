2f23c5a5d09dc50429d10ab72e5744f8e4037691
Thibaud Michaud <thibaudm@chromium.org>
2023-12-19 11:26:26 +0100

    [wasm][wami] Fix try-table indentation and label
    Increment indentation and push a new label for try-table blocks.
    The branch depth immediates of the catch entries in the try-table don't
    include the new label yet, so print the immediates before pushing the
    new label.
    
    R=manoskouk@chromium.org
    
    Change-Id: Ie9d42363f79055d3415fba54b5cc15946b78fd23
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5131014
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91599}

--- src/wasm/wasm-disassembler.cc
+++ src/wasm/wasm-disassembler.cc
@@ -237,7 +237,7 @@ void FunctionBodyDisassembler::DecodeAsWat(MultiLineStringBuilder& out,
     out << indentation;
     if (opcode == kExprElse || opcode == kExprCatch ||
         opcode == kExprCatchAll || opcode == kExprBlock || opcode == kExprIf ||
-        opcode == kExprLoop || opcode == kExprTry) {
+        opcode == kExprLoop || opcode == kExprTry || opcode == kExprTryTable) {
       indentation.increase();
     }
 
@@ -260,13 +260,18 @@ void FunctionBodyDisassembler::DecodeAsWat(MultiLineStringBuilder& out,
       out << WasmOpcodes::OpcodeName(opcode);
     }
     if (opcode == kExprBlock || opcode == kExprIf || opcode == kExprLoop ||
-        opcode == kExprTry) {
-      label_stack_.emplace_back(out.line_number(), out.length(),
-                                label_occurrence_index_++);
+        opcode == kExprTry || opcode == kExprTryTable) {
+      // Create the LabelInfo now to get the correct offset, but only push it
+      // after printing the immediates because the immediates don't see the new
+      // label yet.
+      LabelInfo label(out.line_number(), out.length(),
+                      label_occurrence_index_++);
+      pc_ += PrintImmediatesAndGetLength(out);
+      label_stack_.push_back(label);
+    } else {
+      pc_ += PrintImmediatesAndGetLength(out);
     }
-    uint32_t length = PrintImmediatesAndGetLength(out);
 
-    pc_ += length;
     out.NextLine(pc_offset());
   }
 
