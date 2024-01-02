e8f2feb2266832fb613b02822dd1e1e484a6c2e4
pthier <pthier@chromium.org>
2024-01-02 12:49:39 +0100

    [maglev] Disable hoisting of OSR value phi untagging
    Bug: chromium:1513626
    Change-Id: I24fb77cf436101972fa35a899273512907bd8a26
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5156709
    Commit-Queue: Patrick Thier <pthier@chromium.org>
    Reviewed-by: Victor Gomes <victorgomes@chromium.org>
    Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    Auto-Submit: Patrick Thier <pthier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#91653}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -588,7 +588,7 @@ DEFINE_BOOL(maglev_reuse_stack_slots, true,
             "reuse stack slots in the maglev optimizing compiler")
 DEFINE_BOOL(maglev_untagged_phis, true,
             "enable phi untagging in the maglev optimizing compiler")
-DEFINE_BOOL(maglev_hoist_osr_value_phi_untagging, true,
+DEFINE_BOOL(maglev_hoist_osr_value_phi_untagging, false,
             "enable phi untagging to hoist untagging of osr values")
 DEFINE_EXPERIMENTAL_FEATURE(
     maglev_speculative_hoist_phi_untagging,
