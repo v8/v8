d659cfc0655e99bdcd9827553e9a62c79ddf84c1
Darius Mercadier <dmercadier@chromium.org>
2024-02-01 14:20:07 +0100

    [turboshaft] ISEL: avoid more gap moves for Smi bitcasts
    In CL https://crrev.com/c/5246025, I only updated one of the callsite
    of VisitBitcastSmiToWord but not the other one.
    
    This CL now moves the logic to VisitBitcastSmiToWord so that we don't
    forget more callsites.
    
    Bug: v8:12783
    Change-Id: I4a09a66bf532345935551e2423847939911ee51a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5257340
    Commit-Queue: Darius Mercadier <dmercadier@chromium.org>
    Commit-Queue: Nico Hartmann <nicohartmann@chromium.org>
    Auto-Submit: Darius Mercadier <dmercadier@chromium.org>
    Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#92147}

--- src/compiler/backend/instruction-selector.cc
+++ src/compiler/backend/instruction-selector.cc
@@ -2043,9 +2043,22 @@ void InstructionSelectorT<TurboshaftAdapter>::VisitBitcastWordToTagged(
 template <>
 void InstructionSelectorT<TurboshaftAdapter>::VisitBitcastSmiToWord(
     node_t node) {
+  // TODO(dmercadier): using EmitIdentity here is not ideal, because users of
+  // {node} will then use its input, which may not have the Word32
+  // representation. This might in turn lead to the register allocator wrongly
+  // tracking Tagged values that are in fact just Smis. However, using
+  // Emit(kArchNop) hurts performance because it inserts a gap move which cannot
+  // always be eliminated because the operands may have different sizes (and the
+  // move is then truncating or extending). As a temporary work-around until the
+  // register allocator is fixed, we use Emit(kArchNop) in DEBUG mode to silence
+  // the register allocator verifier.
+#ifdef DEBUG
   OperandGenerator g(this);
   Emit(kArchNop, g.DefineSameAsFirst(node),
        g.Use(this->Get(node).Cast<turboshaft::TaggedBitcastOp>().input()));
+#else
+  EmitIdentity(node);
+#endif
 }
 
 // 32 bit targets do not implement the following instructions.
@@ -5038,21 +5051,7 @@ void InstructionSelectorT<TurboshaftAdapter>::VisitNode(
           if constexpr (Is64()) {
             DCHECK_EQ(cast.kind, TaggedBitcastOp::Kind::kSmi);
             DCHECK(SmiValuesAre31Bits());
-            // TODO(dmercadier): using EmitIdentity here is not ideal, because
-            // users of {node} will then use its input, which may not have the
-            // Word32 representation. This might in turn lead to the register
-            // allocator wrongly tracking Tagged values that are in fact just
-            // Smis. However, using VisitBitcastSmiToWord hurts performance
-            // because it inserts a gap move which cannot always be eliminated
-            // because the operands may have different sizes (and the move is
-            // then truncating or extending). As a temporary work-around until
-            // the register allocator is fixed, we use VisitBitcastSmiToWord in
-            // DEBUG mode to quiet the register allocator verifier.
-#ifdef DEBUG
             return VisitBitcastSmiToWord(node);
-#else
-            return EmitIdentity(node);
-#endif
           } else {
             return VisitBitcastTaggedToWord(node);
           }
