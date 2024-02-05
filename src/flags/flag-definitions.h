13b4b6d80f2c8eac43b85e972071e113a8369b86
Andreas Haas <ahaas@chromium.org>
2024-02-05 09:45:10 +0100

    [wasm] Enable generic wasm-to-js wrapper by default
    Bug: v8:14035, chromium:1489280
    Change-Id: I6b45d6c1b88d76591be913a8798722ec0eadb4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5259577
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#92183}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -1360,13 +1360,12 @@ DEFINE_VALUE_IMPLICATION(optimize_for_size, max_semi_space_size, size_t{1})
 DEFINE_BOOL(wasm_generic_wrapper, true,
             "allow use of the generic js-to-wasm wrapper instead of "
             "per-signature wrappers")
-DEFINE_BOOL(wasm_to_js_generic_wrapper, false,
+DEFINE_BOOL(wasm_to_js_generic_wrapper, true,
             "allow use of the generic wasm-to-js wrapper instead of "
             "per-signature wrappers")
 DEFINE_BOOL(wasm_js_js_generic_wrapper, true,
             "allow use of the generic js-to-js wrapper instead of "
             "per-signature wrappers")
-DEFINE_WEAK_IMPLICATION(future, wasm_to_js_generic_wrapper)
 DEFINE_BOOL(expose_wasm, true, "expose wasm interface to JavaScript")
 DEFINE_INT(wasm_num_compilation_tasks, 128,
            "maximum number of parallel compilation tasks for wasm")
