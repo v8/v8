dfb87674678f523c033b1a8ce624c025f3d9482f
Daniel Lehmann <dlehmann@chromium.org>
2024-02-09 11:18:30 +0100

    Enable JCC mitigation by default temporarily
    ...to get some more data on non-`--future` bots.
    
    Bug: v8:14225
    Change-Id: I5fe61002114aa92e306f9769cca798db83cd0175
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5272838
    Commit-Queue: Daniel Lehmann <dlehmann@chromium.org>
    Commit-Queue: Darius Mercadier <dmercadier@chromium.org>
    Auto-Submit: Daniel Lehmann <dlehmann@chromium.org>
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#92248}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -1972,9 +1972,8 @@ DEFINE_BOOL(partial_constant_pool, true,
 DEFINE_STRING(sim_arm64_optional_features, "none",
               "enable optional features on the simulator for testing: none or "
               "all")
-DEFINE_BOOL(intel_jcc_erratum_mitigation, false,
+DEFINE_BOOL(intel_jcc_erratum_mitigation, true,
             "enable mitigation for Intel JCC erratum on affected CPUs")
-DEFINE_WEAK_IMPLICATION(future, intel_jcc_erratum_mitigation)
 
 #if defined(V8_TARGET_ARCH_RISCV32) || defined(V8_TARGET_ARCH_RISCV64)
 DEFINE_BOOL(riscv_trap_to_simulator_debugger, false,
