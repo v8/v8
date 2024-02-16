5bed1f2574e292c2def45c334bd4e168ebfd2ac3
François Doray <fdoray@chromium.org>
2024-02-16 21:50:24 +0000

    [v8-tasks] Add source location to v8::TaskRunner, step 1/4.
    Steps:
    1. Add Post*TaskImpl variants of v8::TaskRunner::Post*Task methods,
       which take a v8::SourceLocation argument. Embedders should override
       these methods.
    2. Override Post*TaskImpl instead of Post*Task in Chromium.
    3. Override Post*TaskImpl instead of Post*Task in Node.js.
    4. Make Post*Task methods non-virtual and add a v8::SourceLocation
       argument which defaults to SourceLocation::Current().
    
    Bug: chromium:1424158
    Change-Id: I0606308d31d1f060e3f7ecc1a123759fb91ba8fa
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5300502
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#92411}

--- test/cctest/wasm/test-streaming-compilation.cc
+++ test/cctest/wasm/test-streaming-compilation.cc
@@ -63,29 +63,34 @@ class MockPlatform final : public TestPlatform {
  private:
   class MockTaskRunner final : public TaskRunner {
    public:
-    void PostTask(std::unique_ptr<v8::Task> task) override {
+    void PostTaskImpl(std::unique_ptr<v8::Task> task,
+                      const SourceLocation& location) override {
       base::MutexGuard lock_scope(&tasks_lock_);
       tasks_.push(std::move(task));
     }
 
-    void PostNonNestableTask(std::unique_ptr<Task> task) override {
+    void PostNonNestableTaskImpl(std::unique_ptr<Task> task,
+                                 const SourceLocation& location) override {
       PostTask(std::move(task));
     }
 
-    void PostDelayedTask(std::unique_ptr<Task> task,
-                         double delay_in_seconds) override {
+    void PostDelayedTaskImpl(std::unique_ptr<Task> task,
+                             double delay_in_seconds,
+                             const SourceLocation& location) override {
       base::MutexGuard lock_scope(&tasks_lock_);
       delayed_tasks_.emplace_back(
           std::move(task), base::TimeTicks::Now() +
                                base::TimeDelta::FromSecondsD(delay_in_seconds));
     }
 
-    void PostNonNestableDelayedTask(std::unique_ptr<Task> task,
-                                    double delay_in_seconds) override {
+    void PostNonNestableDelayedTaskImpl(
+        std::unique_ptr<Task> task, double delay_in_seconds,
+        const SourceLocation& location) override {
       PostDelayedTask(std::move(task), delay_in_seconds);
     }
 
-    void PostIdleTask(std::unique_ptr<IdleTask> task) override {
+    void PostIdleTaskImpl(std::unique_ptr<IdleTask> task,
+                          const SourceLocation& location) override {
       UNREACHABLE();
     }
 
