2bfdfc882d737a5e94e021e88a95232707fd241e
Andreas Haas <ahaas@chromium.org>
2024-04-02 13:53:10 +0200

    Merged: [wasm] Check for all new fields before calling InstallTypeReflection
    The origin trial of JSPI can enable the type reflection proposal after
    some JavaScript code has already been executed. This JavaScript code can
    already add the fields to objects that would be added by the type
    reflection proposal. Therefore, if any of these fields already exists,
    the type reflection proposal cannot be activated.
    
    With this CL we check for all fields that get added with the type
    reflection proposal, and don't add any fields if one of them exists
    already.
    
    R=fgm@chromium.org
    
    (cherry picked from commit 292c5a8536ae0a70ac59d8ea8b3bdcceac44e8a5)
    Bug: 331383939
    
    Change-Id: I10040761a0479b8c7d5253eeaf9179a8fa08d3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5438115
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/branch-heads/12.4@{#18}
    Cr-Branched-From: 309640da62fae0485c7e4f64829627c92d53b35d-refs/heads/12.4.254@{#1}
    Cr-Branched-From: 5dc24701432278556a9829d27c532f974643e6df-refs/heads/main@{#92862}

--- src/wasm/wasm-js.cc
+++ src/wasm/wasm-js.cc
@@ -3340,6 +3340,46 @@ void WasmJs::Install(Isolate* isolate, bool exposed_on_global_object) {
   }
 }
 
+namespace {
+bool CanInstallTypeReflection(Isolate* isolate, Handle<NativeContext> context) {
+  Handle<JSObject> webassembly(context->wasm_webassembly_object(), isolate);
+
+  Handle<String> function_string = v8_str(isolate, "Function");
+  if (JSObject::HasRealNamedProperty(isolate, webassembly, function_string)
+          .FromMaybe(true)) {
+    return false;
+  }
+
+  Handle<String> type_string = v8_str(isolate, "type");
+#define INSTANCE_PROTO_HANDLE(Name) \
+  handle(JSObject::cast(context->Name()->instance_prototype()), isolate)
+
+  if (JSObject::HasRealNamedProperty(
+          isolate, INSTANCE_PROTO_HANDLE(wasm_table_constructor), type_string)
+          .FromMaybe(true)) {
+    return false;
+  }
+  if (JSObject::HasRealNamedProperty(
+          isolate, INSTANCE_PROTO_HANDLE(wasm_global_constructor), type_string)
+          .FromMaybe(true)) {
+    return false;
+  }
+  if (JSObject::HasRealNamedProperty(
+          isolate, INSTANCE_PROTO_HANDLE(wasm_memory_constructor), type_string)
+          .FromMaybe(true)) {
+    return false;
+  }
+  if (JSObject::HasRealNamedProperty(
+          isolate, INSTANCE_PROTO_HANDLE(wasm_tag_constructor), type_string)
+          .FromMaybe(true)) {
+    return false;
+  }
+#undef INSTANCE_PROTO_HANDLE
+
+  return true;
+}
+}  // namespace
+
 // static
 void WasmJs::InstallConditionalFeatures(Isolate* isolate,
                                         Handle<NativeContext> context) {
@@ -3378,9 +3418,7 @@ void WasmJs::InstallConditionalFeatures(Isolate* isolate,
     }
 
     // Install Wasm type reflection features (if not already done).
-    Handle<String> function_string = v8_str(isolate, "Function");
-    if (!JSObject::HasRealNamedProperty(isolate, webassembly, function_string)
-             .FromMaybe(true)) {
+    if (CanInstallTypeReflection(isolate, context)) {
       InstallTypeReflection(isolate, context);
     }
   }
@@ -3400,6 +3438,7 @@ void WasmJs::InstallSuspenderConstructor(Isolate* isolate,
 // static
 void WasmJs::InstallTypeReflection(Isolate* isolate,
                                    Handle<NativeContext> context) {
+  DCHECK(CanInstallTypeReflection(isolate, context));
   Handle<JSObject> webassembly(context->wasm_webassembly_object(), isolate);
 
 #define INSTANCE_PROTO_HANDLE(Name) \
