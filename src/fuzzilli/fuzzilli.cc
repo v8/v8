eb20072d4b84666d1027bdbaef4681ac3e9925a1
Samuel Groß <saelo@chromium.org>
2024-04-09 13:34:41 +0000

    [fuzzilli] Fix 32-bit builds
    The crash simulation code used by Fuzzilli would previously not compile
    on 32 bit systems for various reasons. This CL fixes that.
    
    Change-Id: Icc564f0fa79f5a45f75303b430a23a78c19cdd5a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5439415
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Reviewed-by: Carl Smith <cffsmith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#93264}

--- src/fuzzilli/fuzzilli.cc
+++ src/fuzzilli/fuzzilli.cc
@@ -60,17 +60,20 @@ void FuzzilliExtension::Fuzzilli(const FunctionCallbackInfo<Value>& info) {
         // We want to use an "interesting" address for the access (instead of
         // e.g. nullptr). In the (unlikely) case that the address is actually
         // mapped, simply increment the pointer until it crashes.
-        Address addr = 0x414141414141ull;
+        // The cast ensures that this works correctly on both 32-bit and 64-bit.
+        Address addr = static_cast<Address>(0x414141414141ull);
         // If we're in sandbox testing mode, use the target page as address.
         // This is the page that must be written to to demonstrate a sandbox
         // bypass, in which case there should be a detectable crash.
+#ifdef V8_ENABLE_SANDBOX
         if (SandboxTesting::mode() == SandboxTesting::Mode::kForTesting) {
           addr = SandboxTesting::target_page_base();
         }
+#endif  // V8_ENABLE_SANDBOX
         char* ptr = reinterpret_cast<char*>(addr);
         for (int i = 0; i < 1024; i++) {
           *ptr = 'A';
-          ptr += 1 * i::GB;
+          ptr += 1 * i::MB;
         }
         break;
       }
