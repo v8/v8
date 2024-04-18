201eb6cd4556065dd33a5e3940e969284a846280
Igor Sheludko <ishell@chromium.org>
2024-04-18 16:36:06 +0200

    [api] Deprecate old named interceptors Api
    The old named interceptors Api will be removed in V8 12.7.
    
    Bug: chromium:328490288
    Change-Id: I7f52e196eba5d92215ec5ea8c60530a3bd6ea714
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5465509
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#93521}

--- include/v8-template.h
+++ include/v8-template.h
@@ -768,7 +768,7 @@ struct NamedPropertyHandlerConfiguration {
         data(data),
         flags(WithNewSignatureFlag(flags)) {}
 
-  V8_DEPRECATE_SOON(
+  V8_DEPRECATED(
       "Provide interceptor callbacks with new signatures instead "
       "(NamedPropertyXxxCallback)")
   NamedPropertyHandlerConfiguration(
@@ -809,7 +809,7 @@ struct NamedPropertyHandlerConfiguration {
         data(data),
         flags(WithNewSignatureFlag(flags)) {}
 
-  V8_DEPRECATE_SOON(
+  V8_DEPRECATED(
       "Provide interceptor callbacks with new signatures instead "
       "(NamedPropertyXxxCallback)")
   explicit NamedPropertyHandlerConfiguration(
@@ -849,7 +849,7 @@ struct NamedPropertyHandlerConfiguration {
         data(data),
         flags(WithNewSignatureFlag(flags)) {}
 
-  V8_DEPRECATE_SOON(
+  V8_DEPRECATED(
       "Provide interceptor callbacks with new signatures instead "
       "(NamedPropertyXxxCallback)")
   NamedPropertyHandlerConfiguration(
