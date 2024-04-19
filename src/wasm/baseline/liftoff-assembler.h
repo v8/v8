2d778f373d2491a5a8cf896bf8ba2e7966019a1a
Matthias Liedtke <mliedtke@chromium.org>
2024-04-19 13:48:00 +0200

    [liftoff] Carve out VarState into own header
    This CL moves the VarState out of the liftoff assembler.
    VarState describes the location of a wasm variable (like a value on the
    wasm value stack, a local or a parameter) and defines its "physical"
    location (i.e. a register, a stack slot or a constant).
    
    This information is needed by the deoptimizer to be able to construct a
    Liftoff frame as, differently to unoptimized JS frames, the wasm frame
    layout depends on internal liftoff details like register allocation.
    
    Bug: 42204618
    Change-Id: I5b55459541fd54796bcb4bbab0ef854fefbda27f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5463596
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#93466}

--- src/wasm/baseline/liftoff-assembler.h
+++ src/wasm/baseline/liftoff-assembler.h
@@ -13,6 +13,7 @@
 #include "src/wasm/baseline/liftoff-assembler-defs.h"
 #include "src/wasm/baseline/liftoff-compiler.h"
 #include "src/wasm/baseline/liftoff-register.h"
+#include "src/wasm/baseline/liftoff-varstate.h"
 #include "src/wasm/function-body-decoder.h"
 #include "src/wasm/wasm-module.h"
 #include "src/wasm/wasm-opcodes.h"
@@ -105,106 +106,7 @@ class LiftoffAssembler : public MacroAssembler {
 
   using ValueKindSig = Signature<ValueKind>;
 
-  class VarState {
-   public:
-    enum Location : uint8_t { kStack, kRegister, kIntConst };
-
-    VarState(ValueKind kind, int offset)
-        : loc_(kStack), kind_(kind), spill_offset_(offset) {
-      DCHECK_LE(0, offset);
-    }
-    VarState(ValueKind kind, LiftoffRegister r, int offset)
-        : loc_(kRegister), kind_(kind), reg_(r), spill_offset_(offset) {
-      DCHECK_EQ(r.reg_class(), reg_class_for(kind));
-      DCHECK_LE(0, offset);
-    }
-    VarState(ValueKind kind, int32_t i32_const, int offset)
-        : loc_(kIntConst),
-          kind_(kind),
-          i32_const_(i32_const),
-          spill_offset_(offset) {
-      DCHECK(kind_ == kI32 || kind_ == kI64);
-      DCHECK_LE(0, offset);
-    }
-
-    bool is_stack() const { return loc_ == kStack; }
-    bool is_gp_reg() const { return loc_ == kRegister && reg_.is_gp(); }
-    bool is_fp_reg() const { return loc_ == kRegister && reg_.is_fp(); }
-    bool is_reg() const { return loc_ == kRegister; }
-    bool is_const() const { return loc_ == kIntConst; }
-
-    ValueKind kind() const { return kind_; }
-
-    Location loc() const { return loc_; }
-
-    // The constant as 32-bit value, to be sign-extended if {kind() == kI64}.
-    int32_t i32_const() const {
-      DCHECK_EQ(loc_, kIntConst);
-      return i32_const_;
-    }
-    WasmValue constant() const {
-      DCHECK(kind_ == kI32 || kind_ == kI64);
-      DCHECK_EQ(loc_, kIntConst);
-      return kind_ == kI32 ? WasmValue(i32_const_)
-                           : WasmValue(int64_t{i32_const_});
-    }
-
-    int offset() const {
-      V8_ASSUME(spill_offset_ >= 0);
-      return spill_offset_;
-    }
-    void set_offset(int offset) {
-      DCHECK_LE(0, spill_offset_);
-      spill_offset_ = offset;
-    }
-
-    Register gp_reg() const { return reg().gp(); }
-    DoubleRegister fp_reg() const { return reg().fp(); }
-    LiftoffRegister reg() const {
-      DCHECK_EQ(loc_, kRegister);
-      return reg_;
-    }
-    RegClass reg_class() const { return reg().reg_class(); }
-
-    void MakeStack() { loc_ = kStack; }
-
-    void MakeRegister(LiftoffRegister r) {
-      loc_ = kRegister;
-      reg_ = r;
-    }
-
-    void MakeConstant(int32_t i32_const) {
-      DCHECK(kind_ == kI32 || kind_ == kI64);
-      loc_ = kIntConst;
-      i32_const_ = i32_const;
-    }
-
-    // Copy src to this, except for offset, since src and this could have been
-    // from different stack states.
-    void Copy(VarState src) {
-      loc_ = src.loc();
-      kind_ = src.kind();
-      if (loc_ == kRegister) {
-        reg_ = src.reg();
-      } else if (loc_ == kIntConst) {
-        i32_const_ = src.i32_const();
-      }
-    }
-
-   private:
-    Location loc_;
-    // TODO(wasm): This is redundant, the decoder already knows the type of each
-    // stack value. Try to collapse.
-    ValueKind kind_;
-
-    union {
-      LiftoffRegister reg_;  // used if loc_ == kRegister
-      int32_t i32_const_;    // used if loc_ == kIntConst
-    };
-    int spill_offset_;
-  };
-
-  ASSERT_TRIVIALLY_COPYABLE(VarState);
+  using VarState = LiftoffVarState;
 
   struct CacheState {
     explicit CacheState(Zone* zone) : stack_state(zone) {}
