8c4a4822cbfc231f99847407456d7e64142a8213
Manos Koukoutos <manoskouk@chromium.org>
2024-04-22 10:47:38 +0200

    Merged: [wasm][turboshaft] Emit tail call for invalid functions
    When we try to inline a tail call and we encounter an invalid inlinee,
    we should emit a tail call (over a regular call) as a replacement.
    This bug can manifest with lazy validation, when a tail call is never
    called and thus the callee is not validated by liftoff before tier-up.
    
    (cherry picked from commit 01219086d3c409979fb59fbae59608d719548884)
    
    Bug: chromium:334687959
    Change-Id: Idf299d81d1da6e590558129690c94c20f3e08666
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5468105
    Auto-Submit: Manos Koukoutos <manoskouk@chromium.org>
    Reviewed-by: Thibaud Michaud <thibaudm@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#93492}
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5492421
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Cr-Commit-Position: refs/branch-heads/12.5@{#10}
    Cr-Branched-From: 15b9756484d5bda98ba273ae13f8db58200db4db-refs/heads/12.5.227@{#1}
    Cr-Branched-From: 497d8573dc80b1b69052a834bec894cf5d4238e7-refs/heads/main@{#93350}

--- src/wasm/turboshaft-graph-interface.cc
+++ src/wasm/turboshaft-graph-interface.cc
@@ -7208,8 +7208,13 @@ class TurboshaftGraphBuildingInterface : public WasmGraphBuilderBase {
         // will be triggered again when actually compiling the invalid function.
         V<WordPtr> callee =
             __ RelocatableConstant(func_index, RelocInfo::WASM_CALL);
-        BuildWasmCall(decoder, sig, callee, trusted_instance_data(), args,
-                      returns);
+        if (is_tail_call) {
+          BuildWasmMaybeReturnCall(decoder, sig, callee,
+                                   trusted_instance_data(), args);
+        } else {
+          BuildWasmCall(decoder, sig, callee, trusted_instance_data(), args,
+                        returns);
+        }
         return;
       }
       decoder->module_->set_function_validated(func_index);
