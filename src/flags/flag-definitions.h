72d7518913007fd4205cb970fdffd8921596e07c
Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
2024-05-03 18:57:56 +0000

    Revert "[import-attributes] Disable flag for import assertions"
    This reverts commit fc0cd5fd4ecb855b374770ed437b0aea040d2cb4.
    
    Reason for revert: Caused failures on https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Blink%20Linux/30618/overview
    
    Original change's description:
    > [import-attributes] Disable flag for import assertions
    >
    > The 'assert' syntax has been deprecated for 3 milestones with a warning
    > message saying it will removed in 12.6.
    >
    > This CL disables the flag by default without removing any code, pending
    > evidence of web compatibility.
    >
    > See https://groups.google.com/a/chromium.org/g/blink-dev/c/ZHvzLaJZRvo/m/FgNDBjrtBQAJ
    >
    > Bug: 40643756
    > Change-Id: Ieed38216f62bc2c0791a22bee181913338e451bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5508153
    > Commit-Queue: Shu-yu Guo <syg@chromium.org>
    > Reviewed-by: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#93706}
    
    Bug: 40643756
    Change-Id: I43e3d0b35cb7239146900b9ebe3844a5b778faef
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5516098
    Owners-Override: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
    Auto-Submit: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#93710}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -242,8 +242,7 @@ DEFINE_BOOL(js_shipping, true, "enable all shipped JavaScript features")
     "harmony weak references with FinalizationRegistry.prototype.cleanupSome") \
   V(harmony_temporal, "Temporal")                                              \
   V(harmony_shadow_realm, "harmony ShadowRealm")                               \
-  V(harmony_struct, "harmony structs, shared structs, and shared arrays")      \
-  V(harmony_import_assertions, "harmony import assertions (deprecated)")
+  V(harmony_struct, "harmony structs, shared structs, and shared arrays")
 
 #define JAVASCRIPT_INPROGRESS_FEATURES_BASE(V)                       \
   V(js_explicit_resource_management, "explicit resource management") \
@@ -284,6 +283,7 @@ DEFINE_WEAK_IMPLICATION(harmony_rab_gsab_transfer, harmony_rab_gsab)
 
 // Features that are shipping (turned on by default, but internal flag remains).
 #define HARMONY_SHIPPING_BASE(V)                                      \
+  V(harmony_import_assertions, "harmony import assertions")           \
   V(harmony_rab_gsab,                                                 \
     "harmony ResizableArrayBuffer / GrowableSharedArrayBuffer")       \
   V(harmony_rab_gsab_transfer, "harmony ArrayBuffer.transfer")        \
