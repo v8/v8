e2db93807ca98a85fc1b466427dd1e8122e1c1c3
Darius Mercadier <dmercadier@chromium.org>
2024-05-10 08:01:41 +0200

    [turboshaft] Enable loop unrolling by default
    Bug: 42202729
    Change-Id: I3ce58c0aac7c3d85e7a5d18815cb44873767ecd0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5526308
    Auto-Submit: Darius Mercadier <dmercadier@chromium.org>
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#93818}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -1326,7 +1326,7 @@ DEFINE_BOOL(turboshaft_instruction_selection, true,
 DEFINE_BOOL(turboshaft_load_elimination, true,
             "enable Turboshaft's low-level load elimination for JS")
 DEFINE_BOOL(turboshaft_loop_peeling, false, "enable Turboshaft's loop peeling")
-DEFINE_BOOL(turboshaft_loop_unrolling, false,
+DEFINE_BOOL(turboshaft_loop_unrolling, true,
             "enable Turboshaft's loop unrolling")
 
 DEFINE_EXPERIMENTAL_FEATURE(turboshaft_typed_optimizations,
@@ -1350,7 +1350,6 @@ DEFINE_EXPERIMENTAL_FEATURE(
     turboshaft_future,
     "enable Turboshaft features that we want to ship in the not-too-far future")
 DEFINE_IMPLICATION(turboshaft_future, turboshaft)
-DEFINE_WEAK_IMPLICATION(turboshaft_future, turboshaft_loop_unrolling)
 DEFINE_WEAK_IMPLICATION(turboshaft_future, turboshaft_loop_peeling)
 DEFINE_WEAK_IMPLICATION(turboshaft_future, turboshaft_wasm)
 #if V8_TARGET_ARCH_X64 or V8_TARGET_ARCH_ARM64 or V8_TARGET_ARCH_ARM or \
