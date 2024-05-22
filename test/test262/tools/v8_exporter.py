3a6059be843de456c05b7b3ec9eaa9e62bc9c451
Liviu Rau <liviurau@chromium.com>
2024-05-22 14:02:51 +0200

    [test262] Export only landed CLs
    This CL is a no-op until dependant https://crrev.com/c/5554257 lands. After that our exporter and approver will no longer query for in flight CLs and try to export them.
    
    Bug: 330319384
    Change-Id: I65acfdd1f812a8ac901d0e4b2d87d9a58aacc897
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5555388
    Reviewed-by: Alexander Schulze <alexschulze@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@google.com>
    Cr-Commit-Position: refs/heads/main@{#94031}

--- test/test262/tools/v8_exporter.py
+++ test/test262/tools/v8_exporter.py
@@ -12,19 +12,22 @@ import logging
 _log = logging.getLogger(__name__)
 
 class V8TestExporter(TestExporter):
-    def merge_pull_request(self, pull_request):
-        """Exporter mode does not merge any PRs. We only create them."""
-        pass
+  def merge_pull_request(self, pull_request):
+    """Exporter mode does not merge any PRs. We only create them."""
+    pass
+
+  def export_in_flight_changes(self, pr_events) -> bool:
+    pass
 
 
 class V8TestApprover(TestExporter):
-    def merge_pull_request(self, pull_request):
-        """Merges a pull request only if in approver mode."""
-        self.approve(pull_request.number)
-        super().merge_pull_request(pull_request)
+  def merge_pull_request(self, pull_request):
+    """Merges a pull request only if in approver mode."""
+    self.approve(pull_request.number)
+    super().merge_pull_request(pull_request)
 
-    def approve(self, pr_number):
-        """Approves a PR.
+  def approve(self, pr_number):
+    """Approves a PR.
         Chromium/WPT exports are self approved via a web hook  with self
         approve functionality from an external github project
         (https://github.com/web-platform-tests/wpt-pr-bot).
@@ -32,24 +35,27 @@ class V8TestApprover(TestExporter):
         We will mimic the same behavior by sending an approve request just
         before we attempt a second pass merge.
         """
-        path = '/repos/%s/%s/pulls/%d/reviews'  % (
-            self.github.gh_org,
-            self.github.gh_repo_name,
-            pr_number)
+    path = '/repos/%s/%s/pulls/%d/reviews'  % (
+        self.github.gh_org,
+        self.github.gh_repo_name,
+        pr_number)
 
-        body = {
-            'body': ('The review process for this patch is being conducted in '
-                'the V8 project.'),
-            'event': 'APPROVE',
-            'comments': [],
-        }
+    body = {
+        'body': ('The review process for this patch is being conducted in '
+            'the V8 project.'),
+        'event': 'APPROVE',
+        'comments': [],
+    }
 
-        try:
-            response = self.github.request(path, method='POST', body=body)
-        except HTTPError as e:
-            response = e.response
-            _log.error('Failed to approve PR %d: %s', pr_number, response.text)
+    try:
+      response = self.github.request(path, method='POST', body=body)
+    except HTTPError as e:
+      response = e.response
+      _log.error('Failed to approve PR %d: %s', pr_number, response.text)
 
-    def create_or_update_pr_from_landed_commit(self, commit):
-        """Approver mode does not create any PRs"""
-        pass
+  def create_or_update_pr_from_landed_commit(self, commit):
+    """Approver mode does not create any PRs"""
+    pass
+
+  def export_in_flight_changes(self, pr_events) -> bool:
+    pass
