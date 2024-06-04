2106d9c81a1fb9d758ebda1560e2251b0f05150c
Leszek Swirski <leszeks@chromium.org>
2024-06-04 12:29:02 +0200

    [include] Disable std::assume_aligned on MSVC
    The 4GB alignment values are causing MSVC build issues.
    
    Change-Id: I19659fa2ce61ff0530879edb89680974d31cbf6a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5595013
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#94229}

--- include/v8config.h
+++ include/v8config.h
@@ -500,8 +500,9 @@ path. Add it with -I<path> to the command line
 # define V8_ASSUME USE
 #endif
 
-// Prefer c++20 std::assume_aligned
-#if __cplusplus >= 202002L && defined(__cpp_lib_assume_aligned)
+// Prefer c++20 std::assume_aligned. Don't use it on MSVC though, because it's
+// not happy with our large 4GB alignment values.
+#if __cplusplus >= 202002L && defined(__cpp_lib_assume_aligned) && !V8_CC_MSVC
 # define V8_ASSUME_ALIGNED(ptr, alignment) \
   std::assume_aligned<(alignment)>(ptr)
 #elif V8_HAS_BUILTIN_ASSUME_ALIGNED
