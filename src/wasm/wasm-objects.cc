fa25b9069658882f6843366cbdff70628afa1e72
Thibaud Michaud <thibaudm@chromium.org>
2024-06-05 16:04:05 +0200

    Merge to 125: [wasm] Disable js-to-wasm generic wrapper for imports
    There are some unresolved issues with tiering-up the wasm-to-js wrapper
    when it is called from the generic js-to-wasm wrapper.
    Disable the generic js-to-wasm wrapper for imports again until these
    issues are resolved.
    
    R=ahaas@chromium.org
    
    (cherry picked from commit 5ab0723917535db1836753f0a8dca0335ca29812)
    
    Bug: 343772336,343917751,342522151
    Change-Id: Ibf6d11ab759fbbb71da93d163121a28aaa0700e0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5600348
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#94270}
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5622012
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/branch-heads/12.5@{#30}
    Cr-Branched-From: 15b9756484d5bda98ba273ae13f8db58200db4db-refs/heads/12.5.227@{#1}
    Cr-Branched-From: 497d8573dc80b1b69052a834bec894cf5d4238e7-refs/heads/main@{#93350}

--- src/wasm/wasm-objects.cc
+++ src/wasm/wasm-objects.cc
@@ -1489,7 +1489,8 @@ Handle<JSFunction> WasmInternalFunction::GetOrCreateExternal(
   if (entry.IsStrongOrWeak() && IsCodeWrapper(entry.GetHeapObject())) {
     wrapper_code = handle(
         CodeWrapper::cast(entry.GetHeapObject())->code(isolate), isolate);
-  } else if (CanUseGenericJsToWasmWrapper(module, function.sig)) {
+  } else if (!function.imported &&
+             CanUseGenericJsToWasmWrapper(module, function.sig)) {
     wrapper_code = isolate->builtins()->code_handle(Builtin::kJSToWasmWrapper);
   } else {
     // The wrapper may not exist yet if no function in the exports section has
