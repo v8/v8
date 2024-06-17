3d617e890adf61c1db35a5a2617273835cfd94cd
Lu Yahan <yahan@iscas.ac.cn>
2024-06-17 09:24:46 +0800

    [riscv][liftoff] Use ptrsized comparison to detect OOB on table64
    Port commit b4c5a86a2799cb0ab4d82572920c8a9f5d5df8bc
    
    Bug: 338024338
    
    Change-Id: Ida3402d40ec069e07b41196bc215933d73d20516
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5635225
    Reviewed-by: Ji Qiu <qiuji@iscas.ac.cn>
    Auto-Submit: Yahan Lu <yahan@iscas.ac.cn>
    Commit-Queue: Ji Qiu <qiuji@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#94471}

--- src/wasm/baseline/riscv/liftoff-assembler-riscv64-inl.h
+++ src/wasm/baseline/riscv/liftoff-assembler-riscv64-inl.h
@@ -1512,6 +1512,12 @@ void LiftoffAssembler::emit_i32_cond_jumpi(Condition cond, Label* label,
   MacroAssembler::CompareTaggedAndBranch(label, cond, lhs, Operand(imm));
 }
 
+void LiftoffAssembler::emit_ptrsize_cond_jumpi(Condition cond, Label* label,
+                                               Register lhs, int32_t imm,
+                                               const FreezeCacheState& frozen) {
+  MacroAssembler::Branch(label, cond, lhs, Operand(imm));
+}
+
 void LiftoffAssembler::emit_i32_eqz(Register dst, Register src) {
   MacroAssembler::slliw(dst, src, 0);
   MacroAssembler::Sltu(dst, src, 1);
