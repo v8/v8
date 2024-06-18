c97a52b8c70068a287cd852c817918e299f6f135
pthier <pthier@chromium.org>
2024-06-18 10:14:03 +0200

    [test] Fix flaky regression test
    The test could fail flakily if allocation of the typed array fails.
    In a previous attempt to fix flakiness of this test the most important
    line (the allocation) was mistakenly left outside the try/catch.
    
    Fixed: 347813910
    Change-Id: I6cc752d24894f00c3a8a1b9201effe8c42138e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5633659
    Commit-Queue: Patrick Thier <pthier@chromium.org>
    Auto-Submit: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#94498}

--- test/mjsunit/regress/regress-340921322.js
+++ test/mjsunit/regress/regress-340921322.js
@@ -4,15 +4,15 @@
 
 // Flags: --allow-natives-syntax
 
-const obj = { 'i': 1096432812 };
-const arr = new Int8Array(obj.i+1);
-
-function foo() {
-    arr[obj.i];
-}
-
-%PrepareFunctionForOptimization(foo);
 try {
+  const obj = { 'i': 1096432812 };
+  const arr = new Int8Array(obj.i+1);
+
+  function foo() {
+      arr[obj.i];
+  }
+
+  %PrepareFunctionForOptimization(foo);
   foo();
   foo();
   %OptimizeFunctionOnNextCall(foo);
