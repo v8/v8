349a697aa6f594e57d5bc731d083ece95f89e71e
Michael Lippautz <mlippautz@chromium.org>
2024-06-27 19:35:12 +0200

    [heap] Enable idle gc on context disposal
    Bug: 333423696
    Change-Id: I8d61c9e234020fd73a93bf0b2af395cadc8d2d49
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5663317
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#94701}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -2075,7 +2075,7 @@ DEFINE_BOOL(manual_evacuation_candidates_selection, false,
 
 DEFINE_BOOL(clear_free_memory, false, "initialize free memory with 0")
 
-DEFINE_BOOL(idle_gc_on_context_disposal, false, "idle gc on context disposal")
+DEFINE_BOOL(idle_gc_on_context_disposal, true, "idle gc on context disposal")
 
 DEFINE_BOOL(trace_context_disposal, false, "trace context disposal")
 
