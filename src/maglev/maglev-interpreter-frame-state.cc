3cdb5c29399a6d802d3ddded4f5ee9c06a39a085
Olivier Flückiger <olivf@chromium.org>
2024-07-24 15:08:57 +0200

    [maglev] Improvements to loop speeling state merging
    * Ignore node infos which contain no actual information.
    * Don't check maps if the loop header only contains stable maps.
    
    Bug: chromium:5729416
    Change-Id: I8a0ba98b335b2a2dac5c7bfc3ee04c8090fbeee1
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5736095
    Commit-Queue: Olivier Flückiger <olivf@chromium.org>
    Auto-Submit: Olivier Flückiger <olivf@chromium.org>
    Reviewed-by: Victor Gomes <victorgomes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#95238}

--- src/maglev/maglev-interpreter-frame-state.cc
+++ src/maglev/maglev-interpreter-frame-state.cc
@@ -183,13 +183,15 @@ namespace {
 // Takes two ordered maps and ensures that every element in `as` is
 //  * also present in `bs` and
 //  * `Compare(a, b)` holds for each value.
-template <typename As, typename Bs, typename Function, bool kSkipEmpty = false>
-bool AspectIncludes(const As& as, const Bs& bs, const Function& Compare) {
+template <typename As, typename Bs, typename CompareFunction,
+          typename IsEmptyFunction = std::nullptr_t>
+bool AspectIncludes(const As& as, const Bs& bs, const CompareFunction& Compare,
+                    const IsEmptyFunction IsEmpty = nullptr) {
   typename As::const_iterator a = as.begin();
   typename Bs::const_iterator b = bs.begin();
   while (a != as.end()) {
-    if constexpr (kSkipEmpty) {
-      if (a->second.empty()) {
+    if constexpr (!std::is_same<IsEmptyFunction, std::nullptr_t>::value) {
+      if (IsEmpty(a->second)) {
         ++a;
         continue;
       }
@@ -214,17 +216,16 @@ bool AspectIncludes(const As& as, const Bs& bs, const Function& Compare) {
 template <typename As, typename Bs, typename Function>
 bool MaybeEmptyAspectIncludes(const As& as, const Bs& bs,
                               const Function& Compare) {
-  return AspectIncludes<As, Bs, Function, true>(as, bs, Compare);
+  return AspectIncludes<As, Bs, Function>(as, bs, Compare,
+                                          [](auto x) { return x.empty(); });
 }
 
 bool NodeInfoIncludes(const NodeInfo& before, const NodeInfo& after) {
   if (!NodeTypeIs(after.type(), before.type())) {
     return false;
   }
-  if (before.possible_maps_are_known()) {
-    if (!after.possible_maps_are_known() ||
-        (!before.possible_maps_are_unstable() &&
-         after.possible_maps_are_unstable())) {
+  if (before.possible_maps_are_known() && before.any_map_is_unstable()) {
+    if (!after.possible_maps_are_known()) {
       return false;
     }
     if (!before.possible_maps().contains(after.possible_maps())) {
@@ -232,6 +233,10 @@ bool NodeInfoIncludes(const NodeInfo& before, const NodeInfo& after) {
     }
   }
   return true;
+}
+
+bool NodeInfoIsEmpty(const NodeInfo& info) {
+  return info.type() == NodeType::kUnknown && !info.possible_maps_are_known();
 }
 
 bool NodeInfoTypeIs(const NodeInfo& before, const NodeInfo& after) {
@@ -249,7 +254,11 @@ bool KnownNodeAspects::IsCompatibleWithLoopHeader(
   bool had_effects = effect_epoch() != loop_header.effect_epoch();
 
   if (!had_effects) {
-    if (!AspectIncludes(loop_header.node_infos, node_infos, NodeInfoTypeIs)) {
+    if (!AspectIncludes(loop_header.node_infos, node_infos, NodeInfoTypeIs,
+                        NodeInfoIsEmpty)) {
+      if (V8_UNLIKELY(v8_flags.trace_maglev_loop_speeling)) {
+        std::cout << "KNA after effectless loop has incompatible node_infos\n";
+      }
       return false;
     }
     // In debug builds we do a full comparison to ensure that without an effect
@@ -259,7 +268,8 @@ bool KnownNodeAspects::IsCompatibleWithLoopHeader(
 #endif
   }
 
-  if (!AspectIncludes(loop_header.node_infos, node_infos, NodeInfoIncludes)) {
+  if (!AspectIncludes(loop_header.node_infos, node_infos, NodeInfoIncludes,
+                      NodeInfoIsEmpty)) {
     if (V8_UNLIKELY(v8_flags.trace_maglev_loop_speeling)) {
       std::cout << "KNA after loop has incompatible node_infos\n";
     }
