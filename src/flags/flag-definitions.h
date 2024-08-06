51b2df6127106ebf6b56f8ec5775cb4472baea6a
Ilya Rezvov <irezvov@chromium.org>
2024-08-05 20:13:25 -0600

    [x64] Disable F16C feature if AVX is not enabled
    F16C doesn't make sense if AVX is disabled.
    The CL disables F16C if AVX is not present or disabled by flags.
    
    Bug: 337998764
    Change-Id: I388e1caa1480cf45d0352eb2dd0a3ed03c05b5b3
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5764406
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#95491}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -2225,6 +2225,7 @@ DEFINE_BOOL(enable_avx2, true, "enable use of AVX2 instructions if available")
 DEFINE_BOOL(enable_avx_vnni, true,
             "enable use of AVX-VNNI instructions if available")
 DEFINE_BOOL(enable_fma3, true, "enable use of FMA3 instructions if available")
+DEFINE_BOOL(enable_f16c, true, "enable use of F16C instructions if available")
 DEFINE_BOOL(enable_bmi1, true, "enable use of BMI1 instructions if available")
 DEFINE_BOOL(enable_bmi2, true, "enable use of BMI2 instructions if available")
 DEFINE_BOOL(enable_lzcnt, true, "enable use of LZCNT instruction if available")
