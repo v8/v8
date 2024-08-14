2b41845eac2a67858ddc1dbfadfa81008dd71b7d
Frank Tang <ftang@chromium.org>
2024-08-13 22:54:32 -0700

    [intl] Ship Intl.DurationFormat
    Spec text: https://tc39.es/proposal-intl-duration-format/
    
      https://github.com/tc39/test262/tree/main/test/intl402/DurationFormat
      https://source.chromium.org/chromium/chromium/src/+/main:v8/test/intl/duration-format/
    
    Tests: 
    Status: https://chromestatus.com/feature/5193021205774336
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/lnv7whm5mXY/m/IHBucGUaBAAJ
    LGTMs: domenic@, bratell.d@gmail.com, vmpstr@
    Bug: 42201655
    Change-Id: I7c498aad6bc5035b751254c08873cc754db4485b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5787914
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Frank Tang <ftang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#95646}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -303,7 +303,6 @@ DEFINE_BOOL(js_shipping, true, "enable all shipped JavaScript features")
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_STAGED(V)                                    \
   HARMONY_STAGED_BASE(V)                                     \
-  V(harmony_intl_duration_format, "Intl DurationFormat API") \
   V(harmony_intl_locale_info_func, "Intl Locale Info API as functions")
 #define JAVASCRIPT_STAGED_FEATURES(V) JAVASCRIPT_STAGED_FEATURES_BASE(V)
 #else
@@ -324,7 +323,9 @@ DEFINE_BOOL(js_shipping, true, "enable all shipped JavaScript features")
   V(js_promise_try, "Promise.try")
 
 #ifdef V8_INTL_SUPPORT
-#define HARMONY_SHIPPING(V) HARMONY_SHIPPING_BASE(V)
+#define HARMONY_SHIPPING(V) \
+  HARMONY_SHIPPING_BASE(V)  \
+  V(harmony_intl_duration_format, "Intl DurationFormat API")
 #define JAVASCRIPT_SHIPPING_FEATURES(V) JAVASCRIPT_SHIPPING_FEATURES_BASE(V)
 #else
 #define HARMONY_SHIPPING(V) HARMONY_SHIPPING_BASE(V)
