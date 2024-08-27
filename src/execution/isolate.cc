9495e79f82f60da191211669e9de1b210d2af1c9
Thibaud Michaud <thibaudm@chromium.org>
2024-08-27 14:16:40 +0200

    [wasm] Don't catch uncatchable exceptions in the JSPI wrapper
    ... And forward the exception to the parent stack instead.
    
    R=jkummerow@chromium.org
    
    Fixed: 361717714
    Change-Id: I7c6a75b53bc7732546ec6a7a1425ac50b9b1756b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5817264
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#95847}

--- src/execution/isolate.cc
+++ src/execution/isolate.cc
@@ -2192,6 +2192,12 @@ Tagged<Object> Isolate::UnwindAndFindHandler() {
       }
     }
   };
+
+  Tagged<Object> maybe_continuation = root(RootIndex::kActiveContinuation);
+  Tagged<WasmContinuationObject> continuation;
+  if (!IsUndefined(maybe_continuation)) {
+    continuation = Cast<WasmContinuationObject>(maybe_continuation);
+  }
 #endif
 
   // Special handling of termination exceptions, uncatchable by JavaScript and
@@ -2212,15 +2218,25 @@ Tagged<Object> Isolate::UnwindAndFindHandler() {
   for (StackFrameIterator iter(this);; iter.Advance(), visited_frames++) {
 #if V8_ENABLE_WEBASSEMBLY
     if (iter.frame()->type() == StackFrame::STACK_SWITCH) {
-      Tagged<Code> code =
-          builtins()->code(Builtin::kWasmReturnPromiseOnSuspendAsm);
-      HandlerTable table(code);
-      Address instruction_start =
-          code->InstructionStart(this, iter.frame()->pc());
-      int handler_offset = table.LookupReturn(0);
-      return FoundHandler(Context(), instruction_start, handler_offset,
-                          kNullAddress, iter.frame()->sp(), iter.frame()->fp(),
-                          visited_frames);
+      if (catchable_by_js) {
+        Tagged<Code> code =
+            builtins()->code(Builtin::kWasmReturnPromiseOnSuspendAsm);
+        HandlerTable table(code);
+        Address instruction_start =
+            code->InstructionStart(this, iter.frame()->pc());
+        int handler_offset = table.LookupReturn(0);
+        return FoundHandler(Context(), instruction_start, handler_offset,
+                            kNullAddress, iter.frame()->sp(),
+                            iter.frame()->fp(), visited_frames);
+      } else {
+        // We reached the base of the wasm stack. Follow the chain of
+        // continuations to find the parent stack and reset the iterator.
+        DCHECK(!continuation.is_null());
+        continuation = Cast<WasmContinuationObject>(continuation->parent());
+        wasm::StackMemory* stack =
+            reinterpret_cast<wasm::StackMemory*>(continuation->stack());
+        iter.Reset(thread_local_top(), stack);
+      }
     }
 #endif
     // Handler must exist.
