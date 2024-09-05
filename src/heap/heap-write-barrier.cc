30a0052b74fb8392b41dc83954445ba8d50cf285
Samuel Groß <saelo@chromium.org>
2024-09-05 12:39:09 +0000

    [sandbox][leaptiering] Adjust dispatch handle write barrier
    As dispatch handles are only processed during major GC, we can always
    skip the write barrier during minor GC. This helps avoid scenarios where
    a minor-GC marking barrier marks a table entry as alive but not the
    contained Code object (because that's not a young-gen object). This in
    turn could lead to a somewhat inconsistent state, which can then confuse
    the code flushing logic (it would flush an unexpected code kind).
    
    Bug: 40931165, 42204201
    Change-Id: I844648fede59ad2208d61f58f52eaf66fb6c517d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5839867
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#95988}

--- src/heap/heap-write-barrier.cc
+++ src/heap/heap-write-barrier.cc
@@ -129,6 +129,15 @@ void WriteBarrier::MarkingSlow(Tagged<TrustedObject> host,
 void WriteBarrier::MarkingSlow(Tagged<HeapObject> host,
                                JSDispatchHandle handle) {
 #ifdef V8_ENABLE_LEAPTIERING
+  MarkingBarrier* marking_barrier = CurrentMarkingBarrier(host);
+
+  // The JSDispatchTable is only marked during major GC so we can skip the
+  // barrier if we're only doing a minor GC.
+  // This is mostly an optimization, but it does help avoid scenarios where a
+  // minor GC marking barrier marks a table entry as alive but not the Code
+  // object contained in it (because it's not a young-gen object).
+  if (marking_barrier->is_minor()) return;
+
   GetProcessWideJSDispatchTable()->Mark(handle);
 
   // We don't need to record a slot here because the entries in the
@@ -136,7 +145,6 @@ void WriteBarrier::MarkingSlow(Tagged<HeapObject> host,
   // table entries are updated after compacting GC.
   static_assert(!JSDispatchTable::kSupportsCompaction);
 
-  MarkingBarrier* marking_barrier = CurrentMarkingBarrier(host);
   if (GetProcessWideJSDispatchTable()->HasCode(handle)) {
     Tagged<Code> value = GetProcessWideJSDispatchTable()->GetCode(handle);
     marking_barrier->MarkValue(host, value);
