3eee872739ac3523af126d7f25a623c18f5bee39
Matthias Liedtke <mliedtke@chromium.org>
2024-09-17 11:58:26 +0200

    [wasm] Do not inline wrappers with 'ref extern' parameter type
    This was introduced in https://crrev.com/c/4212394.
    
    The wrapper would need to test for null and throw a type error but
    doesn't do that correctly.
    (The test case added only tested that a null check happens either in
    the wrapper or in the cast instruction because the test case was trying
    to test both cases without duplicating too much which was a bad design
    choice.)
    
    For simplicity, just disallow inlining of wrappers with parameters
    typed 'ref extern'. (Users should use `externref` aka 'ref null extern'
    instead anyways as the non-nullability doesn't add any benefits.)
    
    Fixed: 366635354
    Change-Id: Ibabeed0b305e3fee783ef4444543b07f55f49bb2
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5867409
    Auto-Submit: Matthias Liedtke <mliedtke@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Reviewed-by: Daniel Lehmann <dlehmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#96129}

--- src/compiler/js-call-reducer.cc
+++ src/compiler/js-call-reducer.cc
@@ -3792,14 +3792,13 @@ bool CanInlineJSToWasmCall(const wasm::FunctionSig* wasm_signature) {
     return false;
   }
 
-  wasm::ValueType externRefNonNull = wasm::kWasmExternRef.AsNonNull();
   for (auto type : wasm_signature->all()) {
 #if defined(V8_TARGET_ARCH_32_BIT)
     if (type == wasm::kWasmI64) return false;
 #endif
     if (type != wasm::kWasmI32 && type != wasm::kWasmI64 &&
         type != wasm::kWasmF32 && type != wasm::kWasmF64 &&
-        type != wasm::kWasmExternRef && type != externRefNonNull) {
+        type != wasm::kWasmExternRef) {
       return false;
     }
   }
