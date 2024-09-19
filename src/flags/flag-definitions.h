cc91a55d5fe826a2a10252d3616e19e2580c94c7
Igor Sheludko <ishell@chromium.org>
2024-09-19 13:46:51 +0200

    [maps] Make --move-prototype-transitions-first flag read-only
    ... in order to avoid inconsistency between state of transition trees
    included into context snapshot while the flag was disabled and
    expectations of heap verifier with enabled flag.
    
    This CL temporarily disables regression tests requiring the flag to be
    enabled.
    
    Bug: 332914927
    Fixed: 366863067
    Change-Id: Ic9fa2e1cd2982b651853182e6cd348d106604507
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5875802
    Commit-Queue: Patrick Thier <pthier@chromium.org>
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#96183}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -2535,9 +2535,9 @@ DEFINE_BOOL(trace_for_in_enumerate, false, "Trace for-in enumerate slow-paths")
 DEFINE_BOOL(log_maps, false, "Log map creation")
 DEFINE_BOOL(log_maps_details, true, "Also log map details")
 DEFINE_IMPLICATION(log_maps, log_code)
-DEFINE_BOOL(move_prototype_transitions_first, false,
-            "Always move prototype transitions to the front of the tree")
-DEFINE_WEAK_IMPLICATION(future, move_prototype_transitions_first)
+DEFINE_BOOL_READONLY(
+    move_prototype_transitions_first, false,
+    "Always move prototype transitions to the front of the tree")
 
 // parser.cc
 DEFINE_BOOL(allow_natives_syntax, false, "allow natives syntax")
