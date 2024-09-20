aa95fff0a8968aa8b882006761ca0b24e4cffc5a
Matthias Liedtke <mliedtke@chromium.org>
2024-09-20 15:26:17 +0200

    [wasm] Do not apply wasm_debug_mask_for_testing during deopt
    When running in debug mode, we do not tier up. If we are still running
    optimized code and this code triggers a deoptimization, we specifically
    compile the unoptimized frame with kNotForDebugging (as otherwise the
    deoptimizer would have to handle all special cases of debug side tables
    etc.).
    
    The --wasm-debug-mask-for-testing flag overwrote this kNotForDebugging,
    forcing debug mode for the deopted function.
    
    Bug: 42204618, 366350772
    Change-Id: I1dfaaed72745c97e45223b6ec9f39592a40c5537
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5873251
    Auto-Submit: Matthias Liedtke <mliedtke@chromium.org>
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#96248}

--- src/wasm/function-compiler.cc
+++ src/wasm/function-compiler.cc
@@ -140,7 +140,11 @@ WasmCompilationResult WasmCompilationUnit::ExecuteFunctionCompilation(
         std::unique_ptr<DebugSideTable> unused_debug_sidetable;
         if (V8_UNLIKELY(declared_index < 32 &&
                         (v8_flags.wasm_debug_mask_for_testing &
-                         (1 << declared_index)) != 0)) {
+                         (1 << declared_index)) != 0) &&
+            // Do not overwrite the debugging setting when performing a
+            // deoptimization.
+            (!v8_flags.wasm_deopt ||
+             env->deopt_location_kind == LocationKindForDeopt::kNone)) {
           options.set_debug_sidetable(&unused_debug_sidetable);
           if (!for_debugging_) options.set_for_debugging(kForDebugging);
         }
