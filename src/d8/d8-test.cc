ab80b86bf7b15f8a292d443989c40e84a232fdb0
Dominik Inführ <dinfuehr@chromium.org>
2024-10-01 13:24:07 +0200

    Reland "[api] Avoid GetIsolateFromWritableObject() in GetCreationContext()"
    This is a reland of commit 150ce0e3e96588559853405fe811be2dc86d70a0
    
    This CL was reverted because of a missing v8::Isolate::Scope in
    Chromium. After landing https://crrev.com/c/5909505 we should be able
    to reland this CL unchanged.
    
    Original change's description:
    > [api] Avoid GetIsolateFromWritableObject() in GetCreationContext()
    >
    > We can replace this call in GetCreationContext() with
    > Isolate::Current() similar to v8::String::MakeExternal. This CL also
    > starts the deprecation process for the Isolate-less version of this
    > method.
    >
    > Bug: 336738728
    > Change-Id: I69b0424f7967d3c82a4ff283cc27cf1087075ac0
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5898893
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#96361}
    
    Bug: 336738728
    Change-Id: I0935f8b9820ffa1bbe231d07559ff1901740249f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5906106
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#96440}

--- src/d8/d8-test.cc
+++ src/d8/d8-test.cc
@@ -1427,7 +1427,8 @@ void CreateFastCAPIObject(const FunctionCallbackInfo<Value>& info) {
   api_object->SetAccessorProperty(
       String::NewFromUtf8Literal(info.GetIsolate(), "supports_fp_params"),
       FunctionTemplate::New(info.GetIsolate(), FastCApiObject::SupportsFPParams)
-          ->GetFunction(api_object->GetCreationContext().ToLocalChecked())
+          ->GetFunction(api_object->GetCreationContext(info.GetIsolate())
+                            .ToLocalChecked())
           .ToLocalChecked());
 }
 
