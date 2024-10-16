61efc41c6dc3928a616bcacf653575c8862226f4
Samuel Groß <saelo@chromium.org>
2024-10-16 14:11:34 +0000

    Add a unittest for --stress-compaction with the EPT
    We rely on --stress-compaction to make EPT compaction testable and
    fuzzable. Previously, the logic for --stress-compaction was not
    aggressive enough. This new test now make sure that, when
    --stress-compaction is active, we compact the table whenever possible.
    
    Bug: 372524119
    Change-Id: I3cd2a7fb7c56fddff8eaa9823ca0403096ad239b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5938540
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#96631}

--- src/heap/factory.h
+++ src/heap/factory.h
@@ -863,7 +863,8 @@ class V8_EXPORT_PRIVATE Factory : public FactoryBase<Factory> {
   Handle<JSFunction> NewFunctionForTesting(DirectHandle<String> name);
 
   // Create an External object for V8's external API.
-  Handle<JSObject> NewExternal(void* value);
+  Handle<JSObject> NewExternal(
+      void* value, AllocationType allocation = AllocationType::kYoung);
 
   // Allocates a new code object and initializes it to point to the given
   // off-heap entry point.
