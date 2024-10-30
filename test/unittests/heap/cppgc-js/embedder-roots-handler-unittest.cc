aab08d20ff8547a674705fd71e616821eb12ee6a
Dominik Inführ <dinfuehr@chromium.org>
2024-10-30 10:21:00 +0100

    [test] Fix Android crash by moving TracedReference to stack
    This test was failing on the Pixel 7. On Android we run unittests
    with the system memory allocator. It seems that the allocator on
    Android uses the top-byte for storing additional bytes. However,
    in our test we store the pointer in the CppHeapPointerTable, where we
    DCHECK that the highest 16-bits are all 0.
    
    This CL avoids this by storing v8::TracedReference on the stack
    instead of the heap. That way we ensure that even on Android the
    highest bits of the address of v8::TracedReference are clear.
    
    Bug: 375121012
    Change-Id: Iba9a969cca87858c3d47da45db12caf95df14aac
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5973204
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Dominik Inführ <dinfuehr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#96922}

--- test/unittests/heap/cppgc-js/embedder-roots-handler-unittest.cc
+++ test/unittests/heap/cppgc-js/embedder-roots-handler-unittest.cc
@@ -102,12 +102,16 @@ void TracedReferenceTest(v8::Isolate* isolate,
   v8::HandleScope scope(isolate);
   auto* traced_handles = i_isolate->traced_handles();
   const size_t initial_count = traced_handles->used_node_count();
-  auto gc_invisible_handle =
-      std::make_unique<v8::TracedReference<v8::Object>>();
-  construct_function(isolate, isolate->GetCurrentContext(),
-                     gc_invisible_handle.get());
-  ASSERT_TRUE(IsNewObjectInCorrectGeneration(isolate, *gc_invisible_handle));
-  modifier_function(*gc_invisible_handle);
+  // Store v8::TracedReference on the stack here on purpose. On Android storing
+  // it on the heap is problematic. This is because the native memory allocator
+  // on Android sets the top-byte of allocations for verification. However, in
+  // same tests we store the address of the v8::TracedReference in the
+  // CppHeapPointerTable to simulate a cppgc wrapper object. The table expectes
+  // the hightest 16-bit to be 0 for all entries.
+  v8::TracedReference<v8::Object> handle;
+  construct_function(isolate, isolate->GetCurrentContext(), &handle);
+  ASSERT_TRUE(IsNewObjectInCorrectGeneration(isolate, handle));
+  modifier_function(handle);
   const size_t after_modification_count = traced_handles->used_node_count();
   gc_function();
   // Cannot check the handle as it is not explicitly cleared by the GC. Instead
