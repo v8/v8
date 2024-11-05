3a3aa00bc0d71c916afa6329cc8e5b91c6fdc30d
Thibaud Michaud <thibaudm@chromium.org>
2024-11-05 18:51:44 +0100

    Merged: [wasm][jspi] Fix stack-switching in unwinder
    Handle stack switching for a missing frame type, WASM_TO_JS_FUNCTION.
    
    R=jkummerow@chromium.org
    
    Fixed: 377384894
    (cherry picked from commit ef6ed156f1b78006de28ef262b55468d511d6984)
    
    Change-Id: Ib5e038ba26827002390fdff3b50ddea6e25a527f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6020069
    Reviewed-by: Jakob Linke <jgruber@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Cr-Commit-Position: refs/branch-heads/13.0@{#37}
    Cr-Branched-From: 4be854bd71ea878a25b236a27afcecffa2e29360-refs/heads/13.0.245@{#1}
    Cr-Branched-From: 1f5183f7ad6cca21029fd60653d075730c644432-refs/heads/main@{#96103}

--- src/execution/isolate.cc
+++ src/execution/isolate.cc
@@ -2410,7 +2410,8 @@ Tagged<Object> Isolate::UnwindAndFindHandler() {
         // out to user code that could throw.
         UNREACHABLE();
       }
-      case StackFrame::WASM_TO_JS: {
+      case StackFrame::WASM_TO_JS:
+      case StackFrame::WASM_TO_JS_FUNCTION: {
         HandleStackSwitch(iter);
         break;
       }
