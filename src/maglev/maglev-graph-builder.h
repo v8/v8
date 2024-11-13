75bf75651657bc88dd75d1574d56d989c023f8fe
Toon Verwaest <verwaest@chromium.org>
2024-11-13 19:30:34 +0000

    Revert "[maglev] Turn some DCHECKs into CHECKs"
    This reverts commit 0b8f93c3d2cb6ffe4095e365788b736890bc1995.
    
    Reason for revert: crashes
    
    Original change's description:
    > [maglev] Turn some DCHECKs into CHECKs
    >
    > Change-Id: Id9fd2349f596139a10c9b59b0e7fa08e3e4143bd
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6011708
    > Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    > Auto-Submit: Toon Verwaest <verwaest@chromium.org>
    > Commit-Queue: Toon Verwaest <verwaest@chromium.org>
    > Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#97123}
    
    Change-Id: I5cec897497849ad7604ca3d0414ab30887b7724d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6018315
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Toon Verwaest <verwaest@chromium.org>

--- src/maglev/maglev-graph-builder.h
+++ src/maglev/maglev-graph-builder.h
@@ -779,7 +779,7 @@ class MaglevGraphBuilder {
       }
 
       if (V8_UNLIKELY(merge_state->is_exception_handler())) {
-        CHECK_EQ(predecessors_[offset], 0);
+        DCHECK_EQ(predecessors_[offset], 0);
         // If we have no reference to this block, then the exception handler is
         // dead.
         if (!jump_targets_[offset].has_ref() ||
@@ -812,12 +812,14 @@ class MaglevGraphBuilder {
       // some earlier deopt). Mark this bytecode dead too and return.
       // TODO(leszeks): Merge these two conditions by marking dead states with
       // a sentinel value.
+#ifdef DEBUG
       if (predecessors_[offset] == 1) {
-        CHECK(bytecode_analysis().IsLoopHeader(offset));
-        CHECK_NULL(merge_state);
+        DCHECK(bytecode_analysis().IsLoopHeader(offset));
+        DCHECK_NULL(merge_state);
       } else {
-        CHECK_EQ(predecessors_[offset], 0);
+        DCHECK_EQ(predecessors_[offset], 0);
       }
+#endif
       MarkBytecodeDead();
       return;
     }
