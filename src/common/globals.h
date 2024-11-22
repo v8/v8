cfc37df6a8324d5236d6834790615d1e28de97e1
Olivier Flückiger <olivf@chromium.org>
2024-11-22 15:21:35 +0100

    [leaptiering] Introduce explicit build flag and tie it to the sandbox
    Let the sandbox require leaptiering and add an explicit flag, such that
    we can start work on supporting it without the sandbox.
    
    Bug: 42204201
    Change-Id: I520fcf6c1d30f487e4352acba5aeb8963d2da029
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6039758
    Auto-Submit: Olivier Flückiger <olivf@chromium.org>
    Reviewed-by: Samuel Groß <saelo@chromium.org>
    Commit-Queue: Olivier Flückiger <olivf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#97353}

--- src/common/globals.h
+++ src/common/globals.h
@@ -134,13 +134,7 @@ namespace internal {
 #define V8_ENABLE_SANDBOX_BOOL false
 #endif
 
-#if defined(V8_ENABLE_SANDBOX) && !defined(V8_DISABLE_LEAPTIERING)
-// Initially, Leaptiering is only available on sandbox-enabled builds, and so
-// V8_ENABLE_SANDBOX and V8_ENABLE_LEAPTIERING are effectively equivalent. Once
-// completed there, it will be ported to non-sandbox builds, at which point the
-// two defines will be separated from each other. Finally, once Leaptiering is
-// used on all configurations, the define will be removed completely.
-#define V8_ENABLE_LEAPTIERING 1
+#ifdef V8_ENABLE_LEAPTIERING
 #define V8_ENABLE_LEAPTIERING_BOOL true
 #else
 #define V8_ENABLE_LEAPTIERING_BOOL false
