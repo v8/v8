660e642ea71ffbec52d873ea1bab963a412f0c99
Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
2024-11-26 11:29:56 +0800

    [loong64][mips64][wasm][jspi] Use the stack-switching CEntry
    Port commit 62ed7f80104fa1e0305b5516882d5ee579874fa9
    
    Change-Id: I236811b5b00146fd3038ffc9989f5c725997d306
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6051437
    Reviewed-by: Thibaud Michaud <thibaudm@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Auto-Submit: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Cr-Commit-Position: refs/heads/main@{#97403}

--- src/codegen/loong64/macro-assembler-loong64.cc
+++ src/codegen/loong64/macro-assembler-loong64.cc
@@ -3850,7 +3850,8 @@ void MacroAssembler::CallRuntime(const Runtime::Function* f,
   // smarter.
   PrepareCEntryArgs(num_arguments);
   PrepareCEntryFunction(ExternalReference::Create(f));
-  CallBuiltin(Builtins::RuntimeCEntry(f->result_size));
+  bool switch_to_central_stack = options().is_wasm;
+  CallBuiltin(Builtins::RuntimeCEntry(f->result_size, switch_to_central_stack));
 }
 
 void MacroAssembler::TailCallRuntime(Runtime::FunctionId fid) {
