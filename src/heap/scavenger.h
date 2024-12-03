8df4b0d8e22de35e32a68dd8ea2eb7da66d142d4
Omer Katz <omerkatz@chromium.org>
2024-12-03 17:25:46 +0100

    Reland "[heap] Parallelize visitation of pinned objects"
    This is a reland of commit 0dd29c2e7a07be9cd85c5e4c04cf155cc1765b3d
    
    Original change's description:
    > [heap] Parallelize visitation of pinned objects
    >
    > On ToT all pinned objects are visited on the main thread, before the
    > parallel phase. To reduce the cost of CSS in terms of atomic pause
    > duration, we move the visitation to the parallel phase via a dedicated
    > worklist.
    >
    > Bug: 379788114
    > Change-Id: I5030b32040564a1e0080bbf4861b186400a9593a
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6055124
    > Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#97632}
    
    Bug: 379788114
    Change-Id: I6c09ff51a3e1eb84271a223d11022defd7275d34
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6081651
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#97670}

--- src/heap/scavenger.h
+++ src/heap/scavenger.h
@@ -30,6 +30,7 @@ enum class CopyAndForwardResult {
 };
 
 using ObjectAndSize = std::pair<Tagged<HeapObject>, int>;
+using ObjectAndMap = std::pair<Tagged<HeapObject>, Tagged<Map>>;
 using SurvivingNewLargeObjectsMap =
     std::unordered_map<Tagged<HeapObject>, Tagged<Map>, Object::Hasher>;
 using SurvivingNewLargeObjectMapEntry =
@@ -84,14 +85,17 @@ class Scavenger {
   };
 
   static const int kCopiedListSegmentSize = 256;
+  static const int kPinnedListSegmentSize = 64;
 
   using CopiedList =
       ::heap::base::Worklist<ObjectAndSize, kCopiedListSegmentSize>;
   using EmptyChunksList = ::heap::base::Worklist<MutablePageMetadata*, 64>;
+  using PinnedList =
+      ::heap::base::Worklist<ObjectAndMap, kPinnedListSegmentSize>;
 
   Scavenger(ScavengerCollector* collector, Heap* heap, bool is_logging,
             EmptyChunksList* empty_chunks, CopiedList* copied_list,
-            PromotionList* promotion_list,
+            PinnedList* pinned_list, PromotionList* promotion_list,
             EphemeronRememberedSet::TableList* ephemeron_table_list);
 
   // Entry point for scavenging an old generation page. For scavenging single
@@ -111,8 +115,8 @@ class Scavenger {
   // Returns true if the object is a large young object, and false otherwise.
   bool PromoteIfLargeObject(Tagged<HeapObject> object);
 
-  void VisitPinnedObject(ScavengeVisitor& visitor, Tagged<HeapObject> object,
-                         MapWord original_map_word);
+  void PushPinnedObject(Tagged<HeapObject> object, Tagged<Map> map);
+  void VisitPinnedObjects();
 
   size_t bytes_copied() const { return copied_size_; }
   size_t bytes_promoted() const { return promoted_size_; }
@@ -211,8 +215,9 @@ class Scavenger {
   ScavengerCollector* const collector_;
   Heap* const heap_;
   EmptyChunksList::Local local_empty_chunks_;
-  PromotionList::Local local_promotion_list_;
   CopiedList::Local local_copied_list_;
+  PinnedList::Local local_pinned_list_;
+  PromotionList::Local local_promotion_list_;
   EphemeronRememberedSet::TableList::Local local_ephemeron_table_list_;
   PretenuringHandler::PretenuringFeedbackMap local_pretenuring_feedback_;
   EphemeronRememberedSet::TableMap local_ephemeron_remembered_set_;
@@ -268,6 +273,7 @@ class ScavengerCollector {
             std::vector<std::pair<ParallelWorkItem, MutablePageMetadata*>>
                 old_to_new_chunks,
             const Scavenger::CopiedList& copied_list,
+            const Scavenger::PinnedList& pinned_list,
             const Scavenger::PromotionList& promotion_list);
 
     void Run(JobDelegate* delegate) override;
@@ -278,6 +284,7 @@ class ScavengerCollector {
    private:
     void ProcessItems(JobDelegate* delegate, Scavenger* scavenger);
     void ConcurrentScavengePages(Scavenger* scavenger);
+    void VisitPinnedObjects(Scavenger* scavenger);
 
     ScavengerCollector* collector_;
 
@@ -288,6 +295,7 @@ class ScavengerCollector {
     IndexGenerator generator_;
 
     const Scavenger::CopiedList& copied_list_;
+    const Scavenger::PinnedList& pinned_list_;
     const Scavenger::PromotionList& promotion_list_;
 
     const uint64_t trace_id_;
