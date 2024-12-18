346f42525378d7ee8aa86bbd216b1a0e76d22c7c
Milad Fa <mfarazma@redhat.com>
2024-12-18 12:34:32 -0500

    PPC: port JSPI simulator changes
    Port 553a1865dc69a9e9c3ecf1f3481eef0ed4fd093a
    
    Change-Id: Ia11a9d90abbf3b2bf5caa78f5ec4f8c92677c262
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6105018
    Reviewed-by: Junliang Yan <junyan@redhat.com>
    Commit-Queue: Milad Farazmand <mfarazma@redhat.com>
    Cr-Commit-Position: refs/heads/main@{#97864}

--- src/execution/ppc/simulator-ppc.h
+++ src/execution/ppc/simulator-ppc.h
@@ -203,6 +203,7 @@ class Simulator : public SimulatorBase {
   // Accessor to the internal simulator stack area. Adds a safety
   // margin to prevent overflows.
   uintptr_t StackLimit(uintptr_t c_limit) const;
+
   // Return central stack view, without additional safety margins.
   // Users, for example wasm::StackMemory, can add their own.
   base::Vector<uint8_t> GetCentralStackView() const;
@@ -242,6 +243,11 @@ class Simulator : public SimulatorBase {
   // below (bad_lr, end_sim_pc).
   bool has_bad_pc() const;
 
+  // Manage instruction tracing.
+  bool InstructionTracingEnabled();
+
+  void ToggleInstructionTracing();
+
   enum special_values {
     // Known bad pc value to ensure that the simulator does not execute
     // without being properly setup.
@@ -276,6 +282,10 @@ class Simulator : public SimulatorBase {
   void HandleVList(Instruction* inst);
   void SoftwareInterrupt(Instruction* instr);
   void DebugAtNextPC();
+
+  // Take a copy of v8 simulator tracing flag because flags are frozen after
+  // start.
+  bool instruction_tracing_ = v8_flags.trace_sim;
 
   // Stop helper functions.
   inline bool isStopInstruction(Instruction* instr);
@@ -470,7 +480,7 @@ class Simulator : public SimulatorBase {
     simd_registers_[reg] = value;
   }
 
-  // Simulator support.
+  // Simulator support for the stack.
   uint8_t* stack_;
   static const size_t kStackProtectionSize = 256 * kSystemPointerSize;
   // This includes a protection margin at each end of the stack area.
