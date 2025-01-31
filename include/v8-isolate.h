3da6b3dca55800d0e312f02995f2d1bde7a76b76
Andreas Haas <ahaas@chromium.org>
2025-01-31 10:51:03 +0100

    [api] Move AttachCppHeap and DetachCppHeap to V8_DEPRECATED
    Bug: 42203693
    Change-Id: I7ed7025a78288707997bb668fbfa93e37ca96818
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6218208
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#98445}

--- include/v8-isolate.h
+++ include/v8-isolate.h
@@ -1178,8 +1178,7 @@ class V8_EXPORT Isolate {
    *
    * If a CppHeap is set via CreateParams, then this call is a noop.
    */
-  V8_DEPRECATE_SOON(
-      "Set the heap on Isolate creation using CreateParams instead.")
+  V8_DEPRECATED("Set the heap on Isolate creation using CreateParams instead.")
   void AttachCppHeap(CppHeap*);
 
   /**
@@ -1187,8 +1186,8 @@ class V8_EXPORT Isolate {
    *
    * If a CppHeap is set via CreateParams, then this call is a noop.
    */
-  V8_DEPRECATE_SOON(
-      "Set the heap on Isolate creation using CreateParams instead.")
+  V8_DEPRECATED(
+      "The CppHeap gets detached automatically during Isolate tear down.")
   void DetachCppHeap();
 
   using ReleaseCppHeapCallback = void (*)(std::unique_ptr<CppHeap>);
