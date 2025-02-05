fd9d1daf420a26763daa013f7ba8bd5aa0c47bed
Frank Tang <ftang@chromium.org>
2025-02-04 17:16:24 -0800

    Merged: Fix out of bound string access
    Bug: 386857213
    (cherry picked from commit 0242cac4b20305b03b74c2e9588003378eebeb77)
    
    Change-Id: I0aee184b2ea8caab67eb11c5a249e910ac340418
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6243715
    Auto-Submit: Shu-yu Guo <syg@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Reviewed-by: Deepti Gandluri <gdeepti@chromium.org>
    Commit-Queue: Deepti Gandluri <gdeepti@chromium.org>
    Cr-Commit-Position: refs/branch-heads/13.4@{#8}
    Cr-Branched-From: 0f87a54dade4353b6ece1d7591ca8c66f90c1c93-refs/heads/13.4.114@{#1}
    Cr-Branched-From: 27af2e9363b2701abc5f3feb701b1dad7d1a9fe8-refs/heads/main@{#98459}

--- src/objects/js-date-time-format.cc
+++ src/objects/js-date-time-format.cc
@@ -1690,6 +1690,10 @@ std::optional<std::string> GetOffsetTimeZone(Isolate* isolate,
   if (m0 == ':') {
     // Ignore ':'
     p++;
+    if (len == p) {
+      // Error
+      return std::nullopt;
+    }
     m0 = flat.Get(p);
   }
   if (len - p != 2) {
