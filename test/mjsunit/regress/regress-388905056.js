66f52356a52811d25b485c875615d6883d6218b2
Thibaud Michaud <thibaudm@chromium.org>
2025-02-10 14:31:16 +0100

    Merged: "Reland "Lower the maximum JS parameter count""
    This is a reland of commit 1827ed8345369ca50a55a10ab3e45bcc581c6339
    
    Before the change, one of the nodes had more than 2^16 inputs
    so optimization bailed out.
    After the change, the function has fewer parameters and gets
    optimized, and the register allocator struggles with that many
    parameters and times out.
    Just mark the test as slow for now.
    
    Original change's description:
    > Lower the maximum JS parameter count
    >
    > To allow extra implicit arguments on the call node without overflowing
    > the uint16_t input count, in particular in the wasm-to-js wrapper where
    > we don't have a bailout mechanism.
    >
    > R=verwaest@chromium.org
    >
    > Fixed: 394350433
    > Change-Id: I61d2e2387539cafd6a0909c3ee035c93d0217be3
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6239302
    > Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    > Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#98556}
    
    (cherry picked from commit 84a0e230dabc2c874a129c2280d6be4f45636225)
    
    Change-Id: Ib0fe3fbbd5dc544261312b6674ca76baa2ea4d56
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6268482
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Cr-Commit-Position: refs/branch-heads/13.4@{#18}
    Cr-Branched-From: 0f87a54dade4353b6ece1d7591ca8c66f90c1c93-refs/heads/13.4.114@{#1}
    Cr-Branched-From: 27af2e9363b2701abc5f3feb701b1dad7d1a9fe8-refs/heads/main@{#98459}

--- test/mjsunit/regress/regress-388905056.js
+++ test/mjsunit/regress/regress-388905056.js
@@ -7,7 +7,7 @@
 
 function f() {}
 let array = [];
-array.length = 65534;
+array.length = 65525;
 let bound = f.bind(null, ...array);
 function g() { bound(1); }
 %PrepareFunctionForOptimization(g);
