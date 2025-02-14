e7735263b7aab83b7aed8199d3966342aa64a430
Shu-yu Guo <syg@chromium.org>
2025-02-14 11:40:58 -0800

    [float16array] Fix TypedArray.p.sort fast case
    Note that this CL doesn't have tests because it is already tested by
    test262 with the --js-float16array flag, or once it becomes on by
    default.
    
    Fixed: 396091323
    Bug: 42203953
    Change-Id: Ibcf32023f24fd0fadbff93f7075d5ca0afd2842a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6265787
    Reviewed-by: Ilya Rezvov <irezvov@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#98708}

--- src/runtime/runtime-typedarray.cc
+++ src/runtime/runtime-typedarray.cc
@@ -11,6 +11,7 @@
 #include "src/objects/objects-inl.h"
 #include "src/runtime/runtime-utils.h"
 #include "src/runtime/runtime.h"
+#include "third_party/fp16/src/include/fp16.h"
 
 namespace v8 {
 namespace internal {
@@ -105,6 +106,10 @@ bool CompareNum(T x, T y) {
   return false;
 }
 
+bool LessThanFloat16RawBits(uint16_t x, uint16_t y) {
+  return CompareNum(fp16_ieee_to_fp32_value(x), fp16_ieee_to_fp32_value(y));
+}
+
 }  // namespace
 
 RUNTIME_FUNCTION(Runtime_TypedArraySortFast) {
@@ -156,32 +161,33 @@ RUNTIME_FUNCTION(Runtime_TypedArraySortFast) {
   DisallowGarbageCollection no_gc;
 
   switch (array->type()) {
-#define TYPED_ARRAY_SORT(Type, type, TYPE, ctype)                          \
-  case kExternal##Type##Array: {                                           \
-    ctype* data = copy_data ? reinterpret_cast<ctype*>(data_copy_ptr)      \
-                            : static_cast<ctype*>(array->DataPtr());       \
-    if (kExternal##Type##Array == kExternalFloat64Array ||                 \
-        kExternal##Type##Array == kExternalFloat32Array ||                 \
-        kExternal##Type##Array == kExternalFloat16Array) {                 \
-      if (COMPRESS_POINTERS_BOOL && alignof(ctype) > kTaggedSize) {        \
-        /* TODO(ishell, v8:8875): See UnalignedSlot<T> for details. */     \
-        std::sort(UnalignedSlot<ctype>(data),                              \
-                  UnalignedSlot<ctype>(data + length), CompareNum<ctype>); \
-      } else {                                                             \
-        std::sort(data, data + length, CompareNum<ctype>);                 \
-      }                                                                    \
-    } else {                                                               \
-      if (COMPRESS_POINTERS_BOOL && alignof(ctype) > kTaggedSize) {        \
-        /* TODO(ishell, v8:8875): See UnalignedSlot<T> for details. */     \
-        std::sort(UnalignedSlot<ctype>(data),                              \
-                  UnalignedSlot<ctype>(data + length));                    \
-      } else {                                                             \
-        std::sort(data, data + length);                                    \
-      }                                                                    \
-    }                                                                      \
-    break;                                                                 \
+#define TYPED_ARRAY_SORT(Type, type, TYPE, ctype)                            \
+  case kExternal##Type##Array: {                                             \
+    ctype* data = copy_data ? reinterpret_cast<ctype*>(data_copy_ptr)        \
+                            : static_cast<ctype*>(array->DataPtr());         \
+    if (kExternal##Type##Array == kExternalFloat64Array ||                   \
+        kExternal##Type##Array == kExternalFloat32Array) {                   \
+      if (COMPRESS_POINTERS_BOOL && alignof(ctype) > kTaggedSize) {          \
+        /* TODO(ishell, v8:8875): See UnalignedSlot<T> for details. */       \
+        std::sort(UnalignedSlot<ctype>(data),                                \
+                  UnalignedSlot<ctype>(data + length), CompareNum<ctype>);   \
+      } else {                                                               \
+        std::sort(data, data + length, CompareNum<ctype>);                   \
+      }                                                                      \
+    } else if (kExternal##Type##Array == kExternalFloat16Array) {            \
+      DCHECK_IMPLIES(COMPRESS_POINTERS_BOOL, alignof(ctype) <= kTaggedSize); \
+      std::sort(data, data + length, LessThanFloat16RawBits);                \
+    } else {                                                                 \
+      if (COMPRESS_POINTERS_BOOL && alignof(ctype) > kTaggedSize) {          \
+        /* TODO(ishell, v8:8875): See UnalignedSlot<T> for details. */       \
+        std::sort(UnalignedSlot<ctype>(data),                                \
+                  UnalignedSlot<ctype>(data + length));                      \
+      } else {                                                               \
+        std::sort(data, data + length);                                      \
+      }                                                                      \
+    }                                                                        \
+    break;                                                                   \
   }
-
     TYPED_ARRAYS(TYPED_ARRAY_SORT)
 #undef TYPED_ARRAY_SORT
   }
