a3a57b77c96cd03c0a3691637ee9b780cfd6411a
Victor Gomes <victorgomes@chromium.org>
2025-02-24 14:59:20 +0100

    [maglev-inlining] Tag inlinee results and update predecessor blocks
    This change addresses two issues in Maglev inlining:
    
    -  Ensures that results from inlinee functions are correctly tagged.
    -  Updates the predecessor of the call's successor block to point to
       the callee's final block, maintaining correct control flow.
    
    Bug: 393989628
    Change-Id: Ie0432d5d361bd93eab78b9ce91f3ffc11c09c716
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6297949
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#98886}

--- src/maglev/maglev-inlining.h
+++ src/maglev/maglev-inlining.h
@@ -5,12 +5,15 @@
 #ifndef V8_MAGLEV_MAGLEV_INLINING_H_
 #define V8_MAGLEV_MAGLEV_INLINING_H_
 
+#include "src/base/logging.h"
 #include "src/compiler/js-heap-broker.h"
 #include "src/execution/local-isolate.h"
 #include "src/maglev/maglev-compilation-info.h"
+#include "src/maglev/maglev-compilation-unit.h"
 #include "src/maglev/maglev-deopt-frame-visitor.h"
 #include "src/maglev/maglev-graph-builder.h"
 #include "src/maglev/maglev-graph-processor.h"
+#include "src/maglev/maglev-ir.h"
 
 namespace v8::internal::maglev {
 
@@ -125,9 +128,12 @@ class MaglevInliner {
     }
 
     DCHECK(result.IsDoneWithValue());
+    ValueNode* returned_value =
+        EnsureTagged(inner_graph_builder, result.value());
 
     // Resume execution using the final block of the inner builder.
     BasicBlock* final_block = inner_graph_builder.current_block();
+    DCHECK_NOT_NULL(final_block);
 
     // Add remaining nodes to the final block and use the control flow of the
     // old call block.
@@ -147,25 +153,19 @@ class MaglevInliner {
       details->callee->graph_labeller()->RegisterBasicBlock(final_block);
     }
 
-    // TODO(victorgomes): Investigate whether we should update the predecessor_
-    // of the blocks that final_block jumps to when control_node != JumpLoop.
-    // Update the control node in case of loop.
-    if (control_node->Is<JumpLoop>()) {
-      // We need to update the predecessor of the LoopHeader.
-      BasicBlock* loop_header = control_node->Cast<JumpLoop>()->target();
-      CHECK(loop_header->is_loop());
-      auto state = loop_header->state();
-      CHECK_EQ(state->predecessor_count(), 2);
-      state->set_predecessor_at(1, final_block);
-    }
+    // Update the predecessor of the successors of the {final_block}, that were
+    // previously pointing to {call_block}.
+    final_block->ForEachSuccessor(
+        [call_block, final_block](BasicBlock* successor) {
+          UpdatePredecessorsOf(successor, call_block, final_block);
+        });
 
     // Restore the rest of the graph.
     for (auto bb : saved_bb) {
       graph_->Add(bb);
     }
 
-    // TODO(victorgomes): This (currently) need to be tagged.
-    return result.value();
+    return returned_value;
   }
 
   // Truncates the graph at the given basic block `block`.  All blocks
@@ -182,6 +182,62 @@ class MaglevInliner {
                                       graph_->blocks().end());
     graph_->blocks().resize(index);
     return saved_bb;
+  }
+
+  template <class Node, typename... Args>
+  ValueNode* AddNodeAtBlockEnd(MaglevGraphBuilder& builder,
+                               std::initializer_list<ValueNode*> inputs,
+                               Args&&... args) {
+    ValueNode* node = NodeBase::New<Node>(builder.zone(), inputs,
+                                          std::forward<Args>(args)...);
+    DCHECK(!node->properties().can_eager_deopt());
+    DCHECK(!node->properties().can_lazy_deopt());
+    DCHECK_NOT_NULL(builder.current_block());
+    builder.current_block()->nodes().Add(node);
+    RegisterNode(builder, node);
+    return node;
+  }
+
+  void RegisterNode(MaglevGraphBuilder& builder, Node* node) {
+    if (builder.has_graph_labeller()) {
+      builder.graph_labeller()->RegisterNode(node);
+    }
+  }
+
+  ValueNode* EnsureTagged(MaglevGraphBuilder& builder, ValueNode* node) {
+    // TODO(victorgomes): Use KNA to create better conversion nodes?
+    switch (node->value_representation()) {
+      case ValueRepresentation::kInt32:
+        return AddNodeAtBlockEnd<Int32ToNumber>(builder, {node});
+      case ValueRepresentation::kUint32:
+        return AddNodeAtBlockEnd<Uint32ToNumber>(builder, {node});
+      case ValueRepresentation::kFloat64:
+        return AddNodeAtBlockEnd<Float64ToTagged>(
+            builder, {node}, Float64ToTagged::ConversionMode::kForceHeapNumber);
+      case ValueRepresentation::kHoleyFloat64:
+        return AddNodeAtBlockEnd<HoleyFloat64ToTagged>(
+            builder, {node},
+            HoleyFloat64ToTagged::ConversionMode::kForceHeapNumber);
+      case ValueRepresentation::kIntPtr:
+        return AddNodeAtBlockEnd<IntPtrToNumber>(builder, {node});
+      case ValueRepresentation::kTagged:
+        return node;
+    }
+  }
+
+  static void UpdatePredecessorsOf(BasicBlock* block, BasicBlock* prev_pred,
+                                   BasicBlock* new_pred) {
+    if (!block->has_state()) {
+      DCHECK_EQ(block->predecessor(), prev_pred);
+      block->set_predecessor(new_pred);
+      return;
+    }
+    for (int i = 0; i < block->predecessor_count(); i++) {
+      if (block->predecessor_at(i) == prev_pred) {
+        block->state()->set_predecessor_at(i, new_pred);
+        break;
+      }
+    }
   }
 };
 
