672c812b72b3ff654ed190290ca7a0ecd4fc3cf9
Shu-yu Guo <syg@chromium.org>
2025-02-25 15:23:16 -0800

    [regexp-escape] Ship it
    I2S with 3 LGTMs:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/9J0IIfquFZY/m/LpKf5Ap_AgAJ
    
    Cq-Include-Trybots: luci.chromium.try:linux-rel
    Fixed: 353856236
    Change-Id: I007d949ea205b19bef982d795d7fa0c525177463
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6300239
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#99270}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -294,11 +294,10 @@ DEFINE_BOOL(js_shipping, true, "enable all shipped JavaScript features")
 
 // Features that are complete (but still behind the --harmony flag).
 #define HARMONY_STAGED_BASE(V)
-#define JAVASCRIPT_STAGED_FEATURES_BASE(V)                                   \
-  V(js_explicit_resource_management, "explicit resource management")         \
-  V(js_float16array,                                                         \
-    "Float16Array, Math.f16round, DataView.getFloat16, DataView.setFloat16") \
-  V(js_regexp_escape, "RegExp.escape")
+#define JAVASCRIPT_STAGED_FEATURES_BASE(V)                           \
+  V(js_explicit_resource_management, "explicit resource management") \
+  V(js_float16array,                                                 \
+    "Float16Array, Math.f16round, DataView.getFloat16, DataView.setFloat16")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_STAGED(V)                    \
@@ -322,7 +321,8 @@ DEFINE_BOOL(js_shipping, true, "enable all shipped JavaScript features")
   V(js_regexp_modifiers, "RegExp modifiers")                           \
   V(js_promise_try, "Promise.try")                                     \
   V(js_atomics_pause, "Atomics.pause")                                 \
-  V(js_error_iserror, "Error.isError")
+  V(js_error_iserror, "Error.isError")                                 \
+  V(js_regexp_escape, "RegExp.escape")
 
 #ifdef V8_INTL_SUPPORT
 #define HARMONY_SHIPPING(V) HARMONY_SHIPPING_BASE(V)
