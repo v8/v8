6c6ef6c3dd3e30bbc9e097605422ca08d543e748
Clemens Backes <clemensb@chromium.org>
2025-03-13 04:32:43 -0700

    Version 13.6.54.1 (cherry-pick)
    Merged 32fcf9e5238cbd6b11a531bd0e120b309c6f0926
    
    Revert "Reland "[sandbox] Check that GetIsolateForSandbox returns the current sandbox""
    
    Change-Id: I87c7ef0404fba7b9011c33f8aee5bd31fffdad3c
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6350003
    Owners-Override: Matthias Liedtke <mliedtke@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/13.6.54@{#2}
    Cr-Branched-From: 23b91ca7891b20d770168eeb798400fe723add2e-refs/heads/main@{#99227}

--- src/execution/isolate-utils-inl.h
+++ src/execution/isolate-utils-inl.h
@@ -50,6 +50,30 @@ V8_INLINE bool GetIsolateFromHeapObject(Tagged<HeapObject> object,
   return true;
 }
 
+// Use this function instead of `Internals::GetIsolateForSandbox` for internal
+// code, as this function is fully inlinable.
+// Note that this method might return an isolate which is not the "current" one
+// as returned by `Isolate::Current()`. Use `GetCurrentIsolateForSandbox`
+// instead where possible.
+// TODO(396607238): Replace all callers with `GetCurrentIsolateForSandbox()`.
+V8_INLINE IsolateForSandbox GetIsolateForSandbox(Tagged<HeapObject> object) {
+#ifdef V8_ENABLE_SANDBOX
+  // This method can be used on shared objects as opposed to
+  // GetHeapFromWritableObject because it only returns IsolateForSandbox instead
+  // of the Isolate. This is because shared objects will go to shared external
+  // pointer table which is the same for main and all worker isolates.
+  MemoryChunk* chunk = MemoryChunk::FromHeapObject(object);
+  Isolate* isolate = Isolate::FromHeap(chunk->GetHeap());
+  // TODO(396607238): Enable the following line, which requires resolving some
+  // inl-header cycles.
+  // SBXCHECK_EQ(isolate, Isolate::Current());
+  return isolate;
+#else
+  // Not used in non-sandbox mode.
+  return {};
+#endif
+}
+
 }  // namespace v8::internal
 
 #endif  // V8_EXECUTION_ISOLATE_UTILS_INL_H_
