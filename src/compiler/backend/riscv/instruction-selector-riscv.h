53343a5fa7971053aaa5b2e77cbab488d9ec4f31
Lu Yahan <yahan@iscas.ac.cn>
2025-03-13 10:30:56 +0800

    [riscv][turbofan] Inline Adapter's DeoptimizeView into instruction selectors
    Port commit 3cda165896d30d7a471f754962af7b8016966531
    Bug: 391750831
    
    Change-Id: I08db3e5583768d3e99bf32cbc98d0c55d80434b2
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6350634
    Reviewed-by: Ji Qiu <qiuji@iscas.ac.cn>
    Commit-Queue: Ji Qiu <qiuji@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#99274}

--- src/compiler/backend/riscv/instruction-selector-riscv.h
+++ src/compiler/backend/riscv/instruction-selector-riscv.h
@@ -276,9 +276,9 @@ static void VisitBinop(InstructionSelectorT* selector, OpIndex node,
 }
 
 void InstructionSelectorT::VisitStackSlot(OpIndex node) {
-  StackSlotRepresentation rep = this->stack_slot_representation_of(node);
-  int slot =
-      frame_->AllocateSpillSlot(rep.size(), rep.alignment(), rep.is_tagged());
+  const StackSlotOp& stack_slot = Cast<StackSlotOp>(node);
+  int slot = frame_->AllocateSpillSlot(stack_slot.size, stack_slot.alignment,
+                                       stack_slot.is_tagged);
   OperandGenerator g(this);
 
   Emit(kArchStackSlot, g.DefineAsRegister(node),
