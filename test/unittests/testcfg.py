9b4dcf9dc0831b6f6b6ef678c341ecaf36ee4dc4
Omer Katz <omerkatz@chromium.org>
2025-03-14 13:42:34 +0100

    Revert "[heap] Add test and fuzzer coverage for precise pinning"
    This reverts commit 0445bd9198532dc930fc57fe386bb4b64b3c8b69.
    
    Reason for revert: https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Linux64%20-%20gc%20stress/14005/overview
    
    Original change's description:
    > [heap] Add test and fuzzer coverage for precise pinning
    >
    > Bug: 402564646
    > Change-Id: I2de89cb627a11706e90d88f6a58a4b97f0a7bba3
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6343458
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#99233}
    
    Bug: 402564646
    Change-Id: I3c2fb76b24550d438c2917c6ff6a44f68d725479
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6356829
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#99260}

--- test/unittests/testcfg.py
+++ test/unittests/testcfg.py
@@ -16,7 +16,6 @@ ADDITIONAL_VARIANTS = set([
     "minor_ms",
     "stress_maglev",
     "conservative_pinning",
-    "precise_pinning",
 ])
 SHELL = "v8_unittests"
 
--- tools/testrunner/local/variants.py
+++ tools/testrunner/local/variants.py
@@ -52,9 +52,6 @@ ALL_VARIANT_FLAGS = {
         "--scavenger-conservative-object-pinning",
         "--stress-scavenger-conservative-object-pinning"
     ]],
-    "precise_pinning": [[
-        "--precise-object-pinning", "--scavenger-precise-object-pinning"
-    ]],
     # We test both the JS and Wasm Turboshaft pipelines under the same variant.
     # For extended Wasm Turboshaft coverage, we add --no-liftoff to the options.
     "turboshaft": [[
