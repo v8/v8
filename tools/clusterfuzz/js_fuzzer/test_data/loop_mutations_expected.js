6e5e005553ef0d96900bcdd1536c70b94e120c03
Michael Achenbach <machenbach@chromium.org>
2025-03-21 16:54:09 +0100

    [js_fuzzer] Update Babel traverse
    The upgrade is required for issue 406389895. In Babel 7.6.0 however,
    a bug was fixed that changes the behavior to fetch available
    identifiers. Though, it's correct now, the previous version might
    have been more beneficial for fuzzing.
    
    We'll look into a workaround to extend the available identifiers
    in a follow up.
    
    The expectation changes are all due to the fixed bug. In some
    examples, there are expectation changes due to the pseudo-random
    test flow.
    
    Bug: 389069288, 406389895
    Change-Id: I0a95be796f232e9172db973342efb744b1704f66
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6383037
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Stephen Röttger <sroettger@google.com>
    Cr-Commit-Position: refs/heads/main@{#99512}

--- tools/clusterfuzz/js_fuzzer/test_data/loop_mutations_expected.js
+++ tools/clusterfuzz/js_fuzzer/test_data/loop_mutations_expected.js
@@ -13,42 +13,22 @@ print("I'm a resource.");
 let __v_0 = 0;
 let __v_1 = 0;
 for (const __v_7 = 0; __v_7 < 10000; __v_7++) {
-  /* VariableOrObjectMutator: Random mutation */
-  __v_1 = __v_7;
   console.log(/* VariableMutator: Replaced __v_7 with __v_7 */__v_7);
-  console.log(/* VariableMutator: Replaced __v_1 with __v_1 */__v_1);
-  /* VariableOrObjectMutator: Random mutation */
-  __v_0 = __v_7;
-  __v_1 = /* VariableMutator: Replaced __v_7 with __v_0 */__v_0;
-  /* VariableOrObjectMutator: Random mutation */
-  __v_0 = __v_0;
-  /* VariableMutator: Replaced __v_1 with __v_1 */__v_1 = /* VariableMutator: Replaced __v_7 with __v_0 */__v_0;
-  /* VariableMutator: Replaced __v_1 with __v_1 */__v_1 = /* VariableMutator: Replaced __v_7 with __v_0 */__v_0;
-  /* VariableOrObjectMutator: Random mutation */
-  __v_0 = __v_0;
-  /* VariableOrObjectMutator: Random mutation */
-  __v_1 = __v_1, __callGC(false);
-  __v_7 = __v_1;
-  /* VariableOrObjectMutator: Random mutation */
-  __v_0 = __v_7, __callGC(true);
+  console.log(/* VariableMutator: Replaced __v_1 with __v_7 */__v_7);
+  __v_1 = /* VariableMutator: Replaced __v_7 with __v_7 */__v_7;
+  __v_1 = /* VariableMutator: Replaced __v_7 with __v_7 */__v_7;
+  __v_1 = /* VariableMutator: Replaced __v_7 with __v_7 */__v_7;
   __v_7 = __v_1;
   __v_7 = __v_1;
-  /* VariableMutator: Replaced __v_5 with __v_1 */__v_1(/* VariableMutator: Replaced __v_7 with __v_7 */__v_7);
+  __v_7 = __v_1;
+  /* VariableMutator: Replaced __v_5 with __v_7 */__v_7(/* VariableMutator: Replaced __v_7 with __v_7 */__v_7);
 }
 let __v_2 = 0;
 while (true) {
   if (__v_2++ < 10) break;
   console.log(__v_2);
-  /* VariableOrObjectMutator: Random mutation */
-  __v_1 = __v_0;
-  /* VariableOrObjectMutator: Random mutation */
-  __v_1 = __v_0;
   __v_0 = __v_0 + __v_2;
   __v_0 = __v_0 + __v_2;
-  /* VariableOrObjectMutator: Random mutation */
-  __v_0 = __v_1;
-  /* VariableOrObjectMutator: Random mutation */
-  __v_1 = __v_1;
   __v_0 = __v_0 + __v_2;
 }
 let __v_3 = 0;
@@ -58,29 +38,21 @@ while (__v_3 < 10) {
 let __v_4 = 0;
 let __v_5 = true;
 do {
-  /* VariableOrObjectMutator: Random mutation */
-  __v_2 = __v_2, __callGC(false);
-  console.log(/* VariableMutator: Replaced __v_4 with __v_2 */__v_2);
-} while (/* VariableMutator: Replaced __v_5 with __v_4 */__v_4);
+  console.log(__v_4);
+} while (/* VariableMutator: Replaced __v_5 with __v_3 */__v_3);
 const __v_6 = 10;
 for (const __v_8 = __v_6; __v_8 > 0; __v_8--) {
-  console.log(/* VariableMutator: Replaced __v_8 with __v_6 */__v_6);
-  console.log(/* VariableMutator: Replaced __v_6 with __v_3 */__v_3);
-  /* VariableOrObjectMutator: Random mutation */
-  __v_0 = __v_3, __callGC(true);
+  console.log(/* VariableMutator: Replaced __v_8 with __v_8 */__v_8);
+  console.log(/* VariableMutator: Replaced __v_6 with __v_8 */__v_8);
   if (strange) {
     __v_8 = __v_6;
     __v_8 = __v_6;
-    /* VariableOrObjectMutator: Random mutation */
-    __v_0 = __v_2;
-    /* VariableOrObjectMutator: Random mutation */
-    __v_1 = __v_2;
     __v_8 = __v_6;
   }
 }
+console.log(/* VariableMutator: Replaced __v_0 with __v_6 */__v_6);
 /* VariableOrObjectMutator: Random mutation */
-__v_0 = __v_2, __callGC(false);
-console.log(/* VariableMutator: Replaced __v_0 with __v_2 */__v_2);
+__v_1 = __v_2;
 /* VariableOrObjectMutator: Random mutation */
-__v_0 = __v_2, __callGC(true);
-console.log(/* VariableMutator: Replaced __v_6 with __v_2 */__v_2);
+__v_4 = __v_4;
+console.log(/* VariableMutator: Replaced __v_6 with __v_6 */__v_6);
--- tools/clusterfuzz/js_fuzzer/test_data/regress/large_loops/expected.js
+++ tools/clusterfuzz/js_fuzzer/test_data/regress/large_loops/expected.js
@@ -13,22 +13,19 @@ var __v_0 = 0;
 var __v_1 = 0;
 for (const __v_2 = 0; __v_2 < 10000; __v_2++) {
   console.log();
-  /* VariableOrObjectMutator: Random mutation */
-  __v_2[__getRandomProperty(__v_2, 512187)] = __v_1;
 }
 for (const __v_3 = 0; 1e5 >= __v_3; __v_3--) {
   console.log();
 }
 for (const __v_4 = -10000; __v_4 < 0; __v_4++) {
-  /* CrossOverMutator: Crossover from foo */
-  async (...__v_0) => {
-    const __v_4 = 0;
-  };
   console.log();
 }
 for (const __v_5 = 0n; __v_5 < 10000n; __v_5++) {
   console.log();
 }
 for (const __v_6 = -0.3; __v_6 < 1000.1; __v_6 += 0.5) {
+  /* VariableOrObjectMutator: Random mutation */
+  __v_6[__getRandomProperty(__v_6, 512187)] = __v_6;
+  __v_6[__getRandomProperty(__v_6, 563500)] = __getRandomObject(118972);
   console.log();
 }
