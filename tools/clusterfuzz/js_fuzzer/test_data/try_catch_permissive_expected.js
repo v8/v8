14da4e88ff742ae4751cb7d466c83496b74d344e
Michael Achenbach <machenbach@chromium.org>
2025-04-10 12:36:11 +0200

    [js_fuzzer] Let try-catch wrapper return a permissive dummy
    Try-catch-wrapped expressions often run into an exception and return
    undefined, which typically lets subsequent code bail out quickly.
    
    This returns a permissive object instead, which is designed to allow
    all sorts of property accesses to keep execution going as long as
    possible.
    
    Bug: 389069288
    Change-Id: I9639f0760364094e79189c0d8f082b1d6597790f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6440171
    Reviewed-by: Carl Smith <cffsmith@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#99740}

--- tools/clusterfuzz/js_fuzzer/test_data/try_catch_permissive_expected.js
+++ tools/clusterfuzz/js_fuzzer/test_data/try_catch_permissive_expected.js
@@ -1,14 +1,14 @@
-// Copyright 2020 the V8 project authors. All rights reserved.
+// Copyright 2025 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // Original: try_catch.js
 function blah() {
   try {
-    try {
-      var a = 10;
-      console.log(a);
-    } catch (e) {}
+    var a = 10;
+    console.log(a);
+  } catch (e) {}
+  try {
     label: for (var i = 0; i < 100; i++) {
       var b = 0;
       while (b < 10) {
@@ -17,8 +17,8 @@ function blah() {
         continue label;
       }
     }
-    return 1;
   } catch (e) {}
+  return 1;
 }
 try {
   blah();
@@ -28,24 +28,38 @@ try {
 } catch (e) {}
 try {
   (function () {
-    1;
-    1;
+    try {
+      1;
+    } catch (e) {}
+    try {
+      1;
+    } catch (e) {}
   })();
 } catch (e) {}
 try {
   if (true) {
-    2;
-    2;
+    try {
+      2;
+    } catch (e) {}
+    try {
+      2;
+    } catch (e) {}
   } else {
-    3;
-    3;
+    try {
+      3;
+    } catch (e) {}
+    try {
+      3;
+    } catch (e) {}
   }
 } catch (e) {}
 let a = 0;
 try {
   switch (a) {
     case 1:
-      1;
+      try {
+        1;
+      } catch (e) {}
   }
 } catch (e) {}
 try {
@@ -53,22 +67,20 @@ try {
     cos(PI);
   }
 } catch (e) {}
-let module = __wrapTC(() => new WebAssembly.Module(builder.toBuffer()), false);
-const complex1 = __wrapTC(() => [1, 2, 3], false);
-const complex2 = __wrapTC(() => boom(), false);
+let module = __wrapTC(() => new WebAssembly.Module(builder.toBuffer()));
+const complex1 = __wrapTC(() => [1, 2, 3]);
+const complex2 = __wrapTC(() => boom());
 let complex3 = __wrapTC(() => function () {
   let complex4 = [1, 2, 3];
   return 2;
-}(), false);
+}());
 try {
   if (true) {
-    let complex5 = new Map();
+    let complex5 = __wrapTC(() => new Map());
   }
 } catch (e) {}
 async function foo(a) {
-  try {
-    let val = await a;
-  } catch (e) {}
+  let val = await __wrapTC(() => a);
 }
 try {
   1;
@@ -84,19 +96,17 @@ try {
   });
 } catch (e) {}
 function foo() {
+  let a = 0;
+  let b = 1;
   try {
-    let a = 0;
-    let b = 1;
     boom();
-    return {
-      a: a,
-      b: b
-    };
-  } catch (e) {
-    return {};
-  }
+  } catch (e) {}
+  return {
+    a: a,
+    b: b
+  };
 }
 let {
   a: x,
   b: y
-} = __wrapTC(() => foo(), false);
+} = __wrapTC(() => foo());
