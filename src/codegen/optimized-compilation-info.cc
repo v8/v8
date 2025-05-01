36ababe3ce6f060a14ee9556c5c09c9d57abf4c6
Pierre Langlois <pierre.langlois@arm.com>
2025-05-01 16:24:21 +0100

    [codegen][arm64] Enable table switches in builtins and bytecode handlers
    Jump-table switches on Arm64 are position-independent, we can use them
    in builtins and bytecode handlers.
    
    Change-Id: Id28bfd7c79e09b281693663f568bac0aca74ab0f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6505641
    Commit-Queue: Pierre Langlois <pierre.langlois@arm.com>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#100283}

--- src/codegen/optimized-compilation-info.cc
+++ src/codegen/optimized-compilation-info.cc
@@ -94,13 +94,16 @@ void OptimizedCompilationInfo::ConfigureFlags() {
       break;
     case CodeKind::BYTECODE_HANDLER:
       set_called_with_code_start_register();
+#ifdef V8_ENABLE_BUILTIN_JUMP_TABLE_SWITCH
+      set_switch_jump_table();
+#endif  // V8_ENABLE_BUILTIN_JUMP_TABLE_SWITCH
       if (v8_flags.turbo_splitting) set_splitting();
       if (v8_flags.enable_allocation_folding) set_allocation_folding();
       break;
     case CodeKind::BUILTIN:
 #ifdef V8_ENABLE_BUILTIN_JUMP_TABLE_SWITCH
       set_switch_jump_table();
-#endif  // V8_TARGET_ARCH_X64
+#endif  // V8_ENABLE_BUILTIN_JUMP_TABLE_SWITCH
       [[fallthrough]];
     case CodeKind::FOR_TESTING:
       if (v8_flags.turbo_splitting) set_splitting();
