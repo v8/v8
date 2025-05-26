1e741fca4615d4e3d9f91595cb458c0c0ffd2212
Michael Achenbach <machenbach@chromium.org>
2025-05-26 14:23:28 +0200

    [foozzie] Extend deep printing for differential fuzzing to more types
    This ensures that we print object properties for all true object
    types, including functions. The object properties are concatenated
    to the originally deep printed value. This includes arrays, typed
    arrays, functions, Set, Map and Proxy.
    
    Deep printing is now also implemented for Set and Map contents.
    
    All deep printing can nest all other types and cuts off after a depth
    of 4 as before.
    
    The tests are extended with the new versions.
    
    Bug: 419144823
    Change-Id: I5d2c1d5d04971658fe398d2a3fa9c84738c735e3
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6579375
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#100505}

--- test/mjsunit/tools/foozzie_printing.js
+++ test/mjsunit/tools/foozzie_printing.js
@@ -30,6 +30,12 @@ arr[3] = 1;
 arr[10] = 1
 test("[, , , 1, , , , , , , 1]", arr);
 
+// Arrays with properties
+const arr2 = [, , 3, 4, 5];
+arr2["0"] = 0;
+arr2["a"] = 1;
+test("[0, , 3, 4, 5]{a: 1}", arr2);
+
 // Nested arrays
 test("[[], []]", [[],[]]);
 test("[[[1, 2, 3]], [4, 5], 6]", [[[1, 2, 3]],[4, 5], 6]);
@@ -39,7 +45,42 @@ test("Object{}", {});
 test("Object{}", new Object());
 test('Object{a: "b"}', {a: "b"});
 test("Object{1: 2, 3: 4}", {1: 2, 3: 4});
-test("A{}", new class A {}());
+
+const cls = new class A {}();
+cls["a"] = "b";
+test('A{a: "b"}', cls);
+
+const num = Object(1);
+num[0] = 1;
+test("Number(1){0: 1}", num);
+
+const big = Object(1n);
+big[0] = 1n;
+test("BigInt(1n){0: 1n}", big);
+
+const str = Object("1");
+str[0] = "1";
+test('String("1"){0: "1"}', str);
+
+const bool = Object(true);
+bool[0] = false;
+test("Boolean(true){0: false}", bool);
+
+const date = new Date(1748006413000);
+date[0] = 1;
+test("Date(1748006413000){0: 1}", date);
+
+const reg = Object(/ab/);
+reg[0] = 1;
+test("/ab/{0: 1}", reg);
+
+const i8a = new Int8Array([0, 1]);
+i8a["a"] = 2;
+test("Int8Array([0,1]){a: 2}", i8a);
+
+const f64a = new Float64Array([0.1, 0.2]);
+f64a["a"] = 0.3;
+test("Float64Array([0.1,0.2]){a: 0.3}", f64a);
 
 // Nested objects
 test("Object{0: Object{}}", {0: {}});
@@ -53,3 +94,61 @@ test("Object{0: Object{0: Object{0: Object{0: ...}}}}",
    {0: {0: {0: {0: {}}}}});
 test("Object{0: Object{0: Object{0: Object{0: ...}}, 1: [0, 1, [..., ...]]}}",
    {0: {0: {0: {0: {}}}, 1: [0, 1, [0, 1]]}});
+
+// Functions
+const fun = (a) => a + 1;
+test("Fun{(a) => a + 1}", fun);
+
+fun["a"] = [0, 1];
+test("Fun{(a) => a + 1}{a: [0, 1]}", fun);
+
+fun["a"] = function(a) {};
+test("Fun{(a) => a + 1}{a: Fun{function(a) {}}}", fun);
+
+// Sets
+const set = new Set([0, 1, 2]);
+set["a"] = 42;
+test("Set[0, 1, 2]{a: 42}", set);
+
+// Maps
+const map = new Map([[0, 1], [2, 3]]);
+map["a"] = 42;
+test("Map{[[0, 1], [2, 3]]}{a: 42}", map);
+
+// Proxies
+const proxy = new Proxy({}, {});
+proxy["a"] = 42;
+test("Object{a: 42}", proxy);
+
+// Some combinations
+test("Set[Map{[[Object{}, []]]}]", new Set([new Map([[{}, []]])]));
+test("Map{[[42, [Set[]]]]}", new Map([[42, [new Set([])]]]));
+
+// Infinite nestings
+const fun2 = (a) => a + 1;
+fun2["a"] = new Set([fun2]);
+test("Fun{(a) => a + 1}{a: Set[Fun{(a) => a + 1}{a: Set[...]}]}", fun2);
+
+const map2 = new Map();
+map2.set(0, map2);
+test("Map{[[0, Map{[[0, Map{[[0, Map{[[..., ...]]}]]}]]}]]}", map2);
+
+const map3 = new Map();
+map3[0] = map3;
+test("Map{[]}{0: Map{[]}{0: Map{[]}{0: Map{[]}{0: ...}}}}", map3);
+
+const map4 = new Map();
+map4.set(map4, 0);
+test("Map{[[Map{[[Map{[[Map{[[..., ...]]}, 0]]}, 0]]}, 0]]}", map4);
+
+const arr3 = [0];
+arr3[1] = arr3;
+test("[0, [0, [0, [..., ...]]]]", arr3);
+
+const arr4 = [0];
+arr4["a"] = arr4;
+test("[0]{a: [0]{a: [0]{a: [...]{a: ...}}}}", arr4);
+
+const arr5 = [];
+arr5[0] = {0: arr5};
+test("[Object{0: [Object{0: ...}]}]", arr5);
--- tools/clusterfuzz/js_fuzzer/resources/differential_fuzz_library.js
+++ tools/clusterfuzz/js_fuzzer/resources/differential_fuzz_library.js
@@ -23,55 +23,101 @@ var __hash = 0;
   const substring = String.prototype.substring;
   const toString = Object.prototype.toString;
 
-  // Same as in mjsunit.js.
-  const classOf = function(object) {
-    // Argument must not be null or undefined.
-    const string = toString.call(object);
-    // String has format [object <ClassName>].
-    return substring.call(string, 8, string.length - 1);
-  };
+  const wrapped = (inner) => inner ? `{${inner}}` : "";
 
   // For standard cases use original prettyPrinted from mjsunit.
   const origPrettyPrinted = prettyPrinted;
 
-  // Override prettyPrinted with a version that also recusively prints objects
-  // and arrays with a depth of 4. We don't track circles, but we'd cut off
-  // after a depth of 4 if there are any.
+  // Override prettyPrinted with a version that also recursively prints
+  // objects, arrays and object properties with a depth of 4. We don't track
+  // circles, but we'd cut off after a depth of 4 if there are any.
   prettyPrinted = function prettyPrinted(value, depth=4) {
     if (depth <= 0) {
       return "...";
     }
     switch (typeof value) {
+      case "function":
+        return prettyPrintedFunction(value, depth);
       case "object":
         if (value === null) return "null";
-        switch (classOf(value)) {
-          case "Array":
-            return prettyPrintedArray(value, depth);
-          case "Object":
-            return prettyPrintedObject(value, depth);
+        let typedArray = false;
+        if (value instanceof Array) return prettyPrintedArray(value, depth);
+        if (value instanceof Set) return prettyPrintedSet(value, depth);
+        if (value instanceof Map) return prettyPrintedMap(value, depth);
+        if (value instanceof Number ||
+            value instanceof BigInt ||
+            value instanceof String ||
+            value instanceof Boolean ||
+            value instanceof Date ||
+            value instanceof RegExp) {
+          return prettyPrintedObjectFallback(value, depth);
         }
+
+        if (value instanceof Int8Array ||
+            value instanceof Uint8Array ||
+            value instanceof Uint8ClampedArray ||
+            value instanceof Int16Array ||
+            value instanceof Uint16Array ||
+            value instanceof Int32Array ||
+            value instanceof Uint32Array ||
+            value instanceof Float32Array ||
+            value instanceof Float64Array ||
+            value instanceof BigInt64Array ||
+            value instanceof BigUint64Array) {
+          return prettyPrintedObjectFallback(value, depth, true);
+        }
+
+        return prettyPrintedObject(value, depth);
     }
     // Fall through to original version for all other types.
     return origPrettyPrinted(value);
   }
 
-  // Helper for pretty array with depth.
+  function prettyPrintedObjectProperties(object, depth, forArray) {
+    let keys = Object.keys(object);
+    if (forArray) keys = keys.filter((n) => !Number.isInteger(Number(n)));
+    const prettyValues = map.call(keys, (key) => {
+      return `${key}: ${prettyPrinted(object[key], depth - 1)}`;
+    });
+    return join.call(prettyValues, ", ");
+  }
+
   function prettyPrintedArray(array, depth) {
     const result = map.call(array, (value, index, array) => {
       if (value === undefined && !(index in array)) return "";
       return prettyPrinted(value, depth - 1);
     });
-    return `[${join.call(result, ", ")}]`;
+    const props = prettyPrintedObjectProperties(array, depth, true);
+    return `[${join.call(result, ", ")}]${wrapped(props)}`;
   }
 
-  // Helper for pretty objects with depth.
+  function prettyPrintedSet(set, depth) {
+    const result = prettyPrintedArray(Array.from(set), depth);
+    const props = prettyPrintedObjectProperties(set, depth, false);
+    return `Set${result}${wrapped(props)}`;
+  }
+
+  function prettyPrintedMap(map, depth) {
+    // Array.from creates an array of arrays (i.e. of tuples). Add depth +1
+    // here since we print through 2 levels of array.
+    const result = prettyPrintedArray(Array.from(map), depth + 1, false);
+    const props = prettyPrintedObjectProperties(map, depth, false);
+    return `Map{${result}}${wrapped(props)}`;
+  }
+
   function prettyPrintedObject(object, depth) {
-    const keys = Object.keys(object);
-    const prettyValues = map.call(keys, (key) => {
-      return `${key}: ${prettyPrinted(object[key], depth - 1)}`;
-    });
-    const content = join.call(prettyValues, ", ");
+    const content = prettyPrintedObjectProperties(object, depth, false);
     return `${object.constructor.name || "Object"}{${content}}`;
+  }
+
+  function prettyPrintedObjectFallback(object, depth, array=false) {
+    const props = prettyPrintedObjectProperties(object, depth, array);
+    return `${origPrettyPrinted(object)}${wrapped(props)}`;
+  }
+
+  function prettyPrintedFunction(fun, depth) {
+    const props = prettyPrintedObjectProperties(fun, depth, false);
+    return `Fun{${fun.toString()}}${wrapped(props)}`;
   }
 
   // Helper for calculating a hash code of a string.
