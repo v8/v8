5deb701979fa87d226b9c6b2a9f82d1b778565d5
Camillo Bruni <cbruni@chromium.org>
2025-05-28 13:25:28 +0200

    [d8] Change default counters logging
    - Simplify default output for easier parsing
    - --dump-counters now no longer enables slow histograms so we can
      more easily use it for benchmarks
    
    Change-Id: I4860f27a4a4c3cfdbe89ecdc138958baf7ebeba4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6597136
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#100554}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -3161,7 +3161,6 @@ DEFINE_BOOL(print_feature_flags_json, false,
             "Print JS and Wasm feature flags grouped by in-progress, staged, "
             "and shipped")
 
-// Slow histograms are also enabled via --dump-counters in d8.
 DEFINE_BOOL(slow_histograms, false,
             "Enable slow histograms with more overhead.")
 
