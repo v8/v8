ad5697b44ff41fc16a7fd47997d1dc7c0a289a1e
Emmanuel Romero Ruiz (from Dev Box) <emromero@microsoft.com>
2025-05-30 07:27:09 -0700

    [Drumbrake] Update drumbrake code with latest changes
    We push the changes made to drumbrake at 05/29/2025
    
    Change-Id: I1c36bf686e4a8bb3453bd27692819e63d13f5686
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6604637
    Reviewed-by: Daniel Lehmann <dlehmann@chromium.org>
    Reviewed-by: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Emmanuel Romero Ruiz <emromero@microsoft.com>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#100616}

--- src/execution/frames.cc
+++ src/execution/frames.cc
@@ -2884,7 +2884,7 @@ FrameSummary::WasmInterpretedFrameSummary::WasmInterpretedFrameSummary(
       byte_offset_(byte_offset) {}
 
 Handle<Object> FrameSummary::WasmInterpretedFrameSummary::receiver() const {
-  return wasm_instance_->GetIsolate()->global_proxy();
+  return Isolate::Current()->global_proxy();
 }
 
 int FrameSummary::WasmInterpretedFrameSummary::SourcePosition() const {
@@ -2899,8 +2899,7 @@ FrameSummary::WasmInterpretedFrameSummary::instance_data() const {
 }
 
 Handle<Script> FrameSummary::WasmInterpretedFrameSummary::script() const {
-  return handle(wasm_instance()->module_object()->script(),
-                wasm_instance()->GetIsolate());
+  return handle(wasm_instance()->module_object()->script(), Isolate::Current());
 }
 
 DirectHandle<Context>
--- src/wasm/interpreter/wasm-interpreter-runtime.cc
+++ src/wasm/interpreter/wasm-interpreter-runtime.cc
@@ -290,17 +290,16 @@ class IndirectFunctionTableEntry {
 
 IndirectFunctionTableEntry::IndirectFunctionTableEntry(
     DirectHandle<WasmInstanceObject> instance, int table_index, int entry_index)
-    : table_(
-          table_index != 0
-              ? direct_handle(Cast<WasmDispatchTable>(
-                                  instance->trusted_data(instance->GetIsolate())
-                                      ->dispatch_tables()
-                                      ->get(table_index)),
-                              instance->GetIsolate())
-              : direct_handle(Cast<WasmDispatchTable>(
-                                  instance->trusted_data(instance->GetIsolate())
-                                      ->dispatch_table0()),
-                              instance->GetIsolate())),
+    : table_(table_index != 0
+                 ? direct_handle(Cast<WasmDispatchTable>(
+                                     instance->trusted_data(Isolate::Current())
+                                         ->dispatch_tables()
+                                         ->get(table_index)),
+                                 Isolate::Current())
+                 : direct_handle(Cast<WasmDispatchTable>(
+                                     instance->trusted_data(Isolate::Current())
+                                         ->dispatch_table0()),
+                                 Isolate::Current())),
       index_(entry_index) {
   DCHECK_GE(entry_index, 0);
   DCHECK_LT(entry_index, table_->length());
@@ -444,8 +443,8 @@ void WasmInterpreterRuntime::TableInit(const uint8_t*& current_code,
 
   std::optional<MessageTemplate> msg_template =
       WasmTrustedInstanceData::InitTableEntries(
-          instance_object_->GetIsolate(), trusted_data, trusted_data,
-          table_index, element_segment_index, dst, src, size);
+          Isolate::Current(), trusted_data, trusted_data, table_index,
+          element_segment_index, dst, src, size);
   // See WasmInstanceObject::InitTableEntries.
   if (msg_template == MessageTemplate::kWasmTrapTableOutOfBounds) {
     SetTrap(TrapReason::kTrapTableOutOfBounds, current_code);
