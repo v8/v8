e36172944467c7e2901383f22cef09ba0340095d
Lu Yahan <yahan@iscas.ac.cn>
2025-06-16 10:24:38 +0800

    [riscv][maglev] Simple truncation pass
    Port commit e9151c316af885be25d77ae8ef98bf8a8935c8b2
    Bug: 422685865
    
    Change-Id: Ia11c73ed35696389d5be6309769e9c824266aad9
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6638821
    Reviewed-by: Ji Qiu <qiuji@iscas.ac.cn>
    Commit-Queue: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#100877}

--- src/maglev/riscv/maglev-ir-riscv.cc
+++ src/maglev/riscv/maglev-ir-riscv.cc
@@ -21,6 +21,15 @@ namespace maglev {
 
 #define __ masm->
 
+std::optional<int32_t> TryGetInt12ConstantInput(Node* node, int index) {
+  if (auto res = node->TryGetInt32ConstantInput(index)) {
+    if (is_int12(*res)) {
+      return res;
+    }
+  }
+  return {};
+}
+
 void Int32NegateWithOverflow::SetValueLocationConstraints() {
   UseRegister(value_input());
   DefineAsRegister(this);
@@ -256,6 +265,83 @@ void CheckFloat64SameValue::GenerateCode(MaglevAssembler* masm,
       }
     }
   }
+}
+
+void Int32Add::SetValueLocationConstraints() {
+  UseRegister(left_input());
+  if (TryGetInt12ConstantInput(this, kRightIndex)) {
+    UseAny(right_input());
+  } else {
+    UseRegister(right_input());
+  }
+  DefineAsRegister(this);
+}
+void Int32Add::GenerateCode(MaglevAssembler* masm,
+                            const ProcessingState& state) {
+  Register left = ToRegister(left_input());
+  Register out = ToRegister(result());
+  if (!right_input().operand().IsRegister()) {
+    auto right_const = TryGetInt32ConstantInput(kRightIndex);
+    DCHECK(right_const);
+    __ Add32(out, left, Operand(*right_const));
+  } else {
+    Register right = ToRegister(right_input());
+    __ Add32(out, left, right);
+  }
+}
+void Int32Subtract::SetValueLocationConstraints() {
+  UseRegister(left_input());
+  if (TryGetInt12ConstantInput(this, kRightIndex)) {
+    UseAny(right_input());
+  } else {
+    UseRegister(right_input());
+  }
+  DefineAsRegister(this);
+}
+void Int32Subtract::GenerateCode(MaglevAssembler* masm,
+                                 const ProcessingState& state) {
+  Register left = ToRegister(left_input());
+  Register out = ToRegister(result());
+  if (!right_input().operand().IsRegister()) {
+    auto right_const = TryGetInt32ConstantInput(kRightIndex);
+    DCHECK(right_const);
+    __ Sub32(out, left, Operand(*right_const));
+  } else {
+    Register right = ToRegister(right_input());
+    __ Sub32(out, left, right);
+  }
+}
+void Int32Multiply::SetValueLocationConstraints() {
+  UseRegister(left_input());
+  UseRegister(right_input());
+  DefineAsRegister(this);
+}
+void Int32Multiply::GenerateCode(MaglevAssembler* masm,
+                                 const ProcessingState& state) {
+  Register left = ToRegister(left_input());
+  Register right = ToRegister(right_input());
+  Register out = ToRegister(result());
+  // TODO(leszeks): peephole optimise multiplication by a constant.
+  __ Mul32(out, left, Operand(right));
+}
+void Int32Divide::SetValueLocationConstraints() {
+  UseRegister(left_input());
+  UseRegister(right_input());
+  DefineAsRegister(this);
+}
+void Int32Divide::GenerateCode(MaglevAssembler* masm,
+                               const ProcessingState& state) {
+  Register left = ToRegister(left_input());
+  Register right = ToRegister(right_input());
+  Register out = ToRegister(result());
+  // TODO(leszeks): peephole optimise division by a constant.
+  Label done, is_zero;
+  __ MacroAssembler::Branch(&is_zero, eq, right, Operand(0));
+  __ Div32(out, left, right);
+  __ Jump(&done);
+  __ bind(&is_zero);
+  __ mv(out, zero_reg);
+  __ bind(&done);
 }
 
 void Int32AddWithOverflow::SetValueLocationConstraints() {
