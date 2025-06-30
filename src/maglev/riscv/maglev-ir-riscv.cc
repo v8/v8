c04c9dded21ad36251d197c744fc32e418a67c41
Florian Loitsch <floitsch@rivosinc.com>
2025-06-30 14:42:59 +0000

    [riscv] Add support for Float64Ieee754Binary
    Adds support for the Float64Ieee754Binary Maglev operation to RISCV.
    
    Port commit 2d6447c148c3e918533355d0383faef351467114
    Bug: 42204525
    
    
    Change-Id: I7a578185a8717607dd1dfef21ac418723f455d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6687170
    Reviewed-by: Ji Qiu <qiuji@iscas.ac.cn>
    Commit-Queue: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#101149}

--- src/maglev/riscv/maglev-ir-riscv.cc
+++ src/maglev/riscv/maglev-ir-riscv.cc
@@ -854,6 +854,19 @@ void Float64Ieee754Unary::GenerateCode(MaglevAssembler* masm,
   __ CallCFunction(ieee_function_ref(), 1);
 }
 
+int Float64Ieee754Binary::MaxCallStackArgs() const { return 0; }
+void Float64Ieee754Binary::SetValueLocationConstraints() {
+  UseFixed(input_lhs(), fa0);
+  UseFixed(input_rhs(), fa1);
+  DefineSameAsFirst(this);
+}
+void Float64Ieee754Binary::GenerateCode(MaglevAssembler* masm,
+                                        const ProcessingState& state) {
+  AllowExternalCallThatCantCauseGC scope(masm);
+  __ PrepareCallCFunction(0, 2);
+  __ CallCFunction(ieee_function_ref(), 2);
+}
+
 void LoadTypedArrayLength::SetValueLocationConstraints() {
   UseRegister(receiver_input());
   DefineAsRegister(this);
