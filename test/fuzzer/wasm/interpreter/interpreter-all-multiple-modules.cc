6770ad991ca1d0a08f1041fc7a9b77c4c413b0a9
Emmanuel Romero Ruiz (from Dev Box) <emromero@microsoft.com>
2025-06-30 01:34:25 -0700

    [wasm interpreter][fuzzer] Add interpreter fuzzer for all features with multiple modules
    This patch adds a new fuzzer target that tests the Wasm interpreter with
    all features enabled (WasmGC + SIMD) using multiple modules.
    
    The fuzzer generates random Wasm modules with external functions and
    tests module interactions through the interpreter, helping identify
    potential issues in cross-module communication.
    
    I also fix missing dependency for Wasm interpreter fuzzer common.
    Now it passes gn gen check with v8_enable_drumbrake = true.
    
    Change-Id: Ibf772fc9c0b760e9132676e6680dddeaeba1d03f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6687756
    Reviewed-by: Paolo Severini <paolosev@microsoft.com>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Emmanuel Romero Ruiz <emromero@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#101142}

---
+++ test/fuzzer/wasm/interpreter/interpreter-all-multiple-modules.cc
@@ -0,0 +1,31 @@
+// Copyright 2024 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/wasm/fuzzing/random-module-generation.h"
+#include "src/wasm/wasm-module-builder.h"
+#include "src/zone/zone.h"
+#include "test/fuzzer/wasm/interpreter/interpreter-fuzzer-common.h"
+
+namespace v8::internal::wasm::fuzzing {
+
+// Fuzzer that may generate WasmGC+SIMD expressions.
+bool GenerateModuleWithExternalFuncs(
+    i::Isolate* isolate, i::Zone* zone, v8::base::Vector<const uint8_t> data,
+    i::wasm::ZoneBuffer* buffer, std::vector<fuzzing::ExportData>* exports,
+    std::vector<fuzzing::ExportData>* imports) {
+  v8::base::Vector<const uint8_t> wire_bytes =
+      fuzzing::GenerateRandomWasmModule(
+          zone, fuzzing::WasmModuleGenerationOptions::All(), data, exports,
+          imports);
+  if (wire_bytes.empty()) return false;
+  buffer->write(wire_bytes.data(), wire_bytes.size());
+  return true;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  return LLVMFuzzerTestTwoModulesCommon(data, size,
+                                        GenerateModuleWithExternalFuncs);
+}
+
+}  // namespace v8::internal::wasm::fuzzing
