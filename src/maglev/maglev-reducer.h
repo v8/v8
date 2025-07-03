949e8b26f75be482ddc9d2d99bdd4258c8dc0512
Manos Koukoutos <manoskouk@chromium.org>
2025-07-03 06:33:15 -0700

    Revert "[maglev] Add boilerplate for the MaglevGraphOptimizer pass"
    This reverts commit e0b7cfc0454738393406e0978af1057b65b45aac.
    
    Reason for revert: https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Linux64%20-%20undefined%20double%20-%20debug%20builder/964/overview
    
    Bug: 424157317
    Original change's description:
    > [maglev] Add boilerplate for the MaglevGraphOptimizer pass
    >
    > As an example, we are able to constant fold addition
    > through non-eager inlined functions.
    >
    > This process current only 3 nodes:
    > CheckedSmiUntag, Int32ToNumber and Int32AddWithOverflow
    >
    > Bug: 424157317
    > Change-Id: I4eeb15bdf2b711de84a0816fc621ee67f624339a
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6694957
    > Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    > Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#101231}
    
    Bug: 424157317
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I735453c74f1bab569f96a5e50e53ea8d32247aae
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6701457
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Manos Koukoutos <manoskouk@chromium.org>
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#101233}

--- src/maglev/maglev-reducer.h
+++ src/maglev/maglev-reducer.h
@@ -306,17 +306,8 @@ class MaglevReducer {
         inlining_id);
   }
 
-  void SetCurrentProvenance(MaglevGraphLabeller::Provenance provenance) {
-    current_provenance_ = provenance;
-  }
-
   void RegisterNode(NodeBase* node) {
     graph_labeller()->RegisterNode(node, &current_provenance_);
-  }
-
-  bool has_graph_labeller() const { return graph()->has_graph_labeller(); }
-  MaglevGraphLabeller* graph_labeller() const {
-    return graph()->graph_labeller();
   }
 
   // TODO(victorgomes): Delete these access (or move to private) when the
@@ -334,9 +325,6 @@ class MaglevReducer {
   void SetNewNodePosition(BasicBlockPosition position) {
     current_block_position_ = position;
   }
-
-  template <UseReprHintRecording hint = UseReprHintRecording::kRecord>
-  ValueNode* ConvertInputTo(ValueNode* input, ValueRepresentation expected);
 
 #ifdef DEBUG
   // TODO(victorgomes): Investigate if we can create a better API for this!
@@ -434,6 +422,9 @@ class MaglevReducer {
   NodeT* AddNewNodeOrGetEquivalent(std::initializer_list<ValueNode*> raw_inputs,
                                    Args&&... args);
 
+  template <UseReprHintRecording hint = UseReprHintRecording::kRecord>
+  ValueNode* ConvertInputTo(ValueNode* input, ValueRepresentation expected);
+
   template <typename NodeT>
   static constexpr UseReprHintRecording ShouldRecordUseReprHint() {
     // We do not record a Tagged use on Return, since they are never on the hot
@@ -497,6 +488,11 @@ class MaglevReducer {
   Graph* graph() const { return graph_; }
   compiler::JSHeapBroker* broker() const { return broker_; }
 
+  bool has_graph_labeller() const { return graph()->has_graph_labeller(); }
+  MaglevGraphLabeller* graph_labeller() const {
+    return graph()->graph_labeller();
+  }
+
  private:
   BaseT* base_;
 
@@ -512,8 +508,7 @@ class MaglevReducer {
 #ifdef DEBUG
   // This is used for dcheck purposes, it is the set of all nodes created in
   // the current "period". Where period is defined by Base. In the case of the
-  // GraphBuilder is all nodes created while visiting a bytecode. In the case
-  // of GraphOptimizer is all nodes created while visiting a maglev node.
+  // GraphBuilder is all nodes created while visiting a bytecode.
   ZoneSet<Node*> new_nodes_current_period_;
 #endif  // DEBUG
 
