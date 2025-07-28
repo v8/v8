85f91b353e551c30c0405c9d006385346117ea50
Victor Gomes <victorgomes@chromium.org>
2025-07-28 15:47:20 +0200

    Fix mjsunit/regress/regress-crbug-644111 failing in lite mode
    The test conflicts with lite mode, we pass --turbofan explicitly
    to indicate that.
    
    Drive-by: use %Prepare/%Optimize pattern.
    
    Fixed: 434599899
    Change-Id: Iabf1c2e260e3b72a502fa66ac0a5e64c79748107
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6787942
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#101665}

--- test/mjsunit/regress/regress-crbug-644111.js
+++ test/mjsunit/regress/regress-crbug-644111.js
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --validate-asm --no-lazy-feedback-allocation
-// Flags: --invocation-count-for-turbofan=1
+// Flags: --validate-asm --allow-natives-syntax --turbofan
 
 function Module() {
   "use asm";
   return {};
 }
+
+%PrepareFunctionForOptimization(Module);
 var m = Module();
+%OptimizeFunctionOnNextCall(Module);
 m = Module();
