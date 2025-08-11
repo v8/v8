29f4cda44e913389804b7930dd42ee5433c8d809
Olivier Flückiger <olivf@chromium.org>
2025-08-11 13:31:09 +0200

    Reland "Reland "[turboshaft] Direct call for known functions""
    This is a reland of commit f3187f46f5a61238895da998d4ae662e1778f208
    
    Fix: This time, let's actually remove the broken dcheck.
    
    Original change's description:
    > Reland "[turboshaft] Direct call for known functions"
    >
    > This is a reland of commit d0c359e7bf60d54fda042a4bb348c2f740b4f426
    >
    > Fix: RelocInfo::IsConstant did not expect 32bit dispatch handles in
    >      on non-compressed builds.
    >
    > Original change's description:
    > > [turboshaft] Direct call for known functions
    > >
    > > Call the dispatch entry directly in turboshaft instead of loading the
    > > code from the JSFunction if the function is constant.
    > >
    > > Change-Id: Ib2e7d95bacd4c549f801a809c5866d197a8e8d43
    > > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6805455
    > > Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    > > Auto-Submit: Olivier Flückiger <olivf@chromium.org>
    > > Commit-Queue: Olivier Flückiger <olivf@chromium.org>
    > > Commit-Queue: Nico Hartmann <nicohartmann@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#101763}
    >
    > Change-Id: I8fd2cf0898f9794d14c7951dc8ebc496cd148aa9
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6827803
    > Commit-Queue: Igor Sheludko <ishell@chromium.org>
    > Auto-Submit: Olivier Flückiger <olivf@chromium.org>
    > Reviewed-by: Igor Sheludko <ishell@chromium.org>
    > Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#101817}
    
    Change-Id: I316fdc6e51e88e6f675f723b75e928ca9f756f4b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6835869
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Olivier Flückiger <olivf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#101828}

--- src/compiler/backend/arm/code-generator-arm.cc
+++ src/compiler/backend/arm/code-generator-arm.cc
@@ -515,6 +515,10 @@ void CodeGenerator::AssemblePrepareTailCall() {
 
 namespace {
 
+bool HasImmediateInput(Instruction* instr, size_t index) {
+  return instr->InputAt(index)->IsImmediate();
+}
+
 void FlushPendingPushRegisters(MacroAssembler* masm,
                                FrameAccessState* frame_access_state,
                                ZoneVector<Register>* pending_pushes) {
@@ -751,18 +755,37 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
       break;
     }
     case kArchCallJSFunction: {
-      Register func = i.InputRegister(0);
-      if (v8_flags.debug_code) {
-        UseScratchRegisterScope temps(masm());
-        Register scratch = temps.Acquire();
-        // Check the function's context matches the context argument.
-        __ ldr(scratch, FieldMemOperand(func, JSFunction::kContextOffset));
-        __ cmp(cp, scratch);
-        __ Assert(eq, AbortReason::kWrongFunctionContext);
-      }
       uint32_t num_arguments =
           i.InputUint32(instr->JSCallArgumentCountInputIndex());
-      __ CallJSFunction(func, num_arguments);
+      if (HasImmediateInput(instr, 0)) {
+        Constant constant = i.ToConstant(instr->InputAt(0));
+        Handle<JSFunction> function = Cast<JSFunction>(constant.ToHeapObject());
+        __ Move(kJavaScriptCallTargetRegister, function);
+        if (function->shared()->HasBuiltinId()) {
+          Builtin builtin = function->shared()->builtin_id();
+          SBXCHECK_EQ(num_arguments,
+                      Builtins::GetFormalParameterCount(builtin));
+          __ CallBuiltin(builtin);
+        } else {
+          JSDispatchHandle dispatch_handle = function->dispatch_handle();
+          size_t expected =
+              IsolateGroup::current()->js_dispatch_table()->GetParameterCount(
+                  dispatch_handle);
+          SBXCHECK_GE(num_arguments, expected);
+          __ CallJSDispatchEntry(dispatch_handle, expected);
+        }
+      } else {
+        Register func = i.InputRegister(0);
+        if (v8_flags.debug_code) {
+          UseScratchRegisterScope temps(masm());
+          Register scratch = temps.Acquire();
+          // Check the function's context matches the context argument.
+          __ ldr(scratch, FieldMemOperand(func, JSFunction::kContextOffset));
+          __ cmp(cp, scratch);
+          __ Assert(eq, AbortReason::kWrongFunctionContext);
+        }
+        __ CallJSFunction(func, num_arguments);
+      }
       RecordCallPosition(instr);
       DCHECK_EQ(LeaveCC, i.OutputSBit());
       frame_access_state()->ClearSPDelta();
