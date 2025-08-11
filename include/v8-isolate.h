1e9a032b0c0011a6ecd9ebd22cb0a69854d6bd42
Olivier Flückiger <olivf@chromium.org>
2025-08-11 14:42:35 +0200

    [string] Add WebDX counter for String.prototype HTML wrapper methods
    This commit introduces a UseCounter for the String.prototype HTML
    wrapper methods (e.g., `anchor`, `bold`, `link`) to track their usage as
    a WebDX feature.
    
    Bug: 427938674
    Change-Id: I6ec55e0f6b60b4358231e6561e4948e9ac35fdb5
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6836368
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Olivier Flückiger <olivf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#101845}

--- include/v8-isolate.h
+++ include/v8-isolate.h
@@ -666,6 +666,7 @@ class V8_EXPORT Isolate {
     kRegExpStaticProperties = 178,
     kRegExpStaticPropertiesWithLastMatch = 179,
     kWithStatement = 180,
+    kHtmlWrapperMethods = 181,
 
     // If you add new values here, you'll also need to update Chromium's:
     // web_feature.mojom, use_counter_callback.cc, and enums.xml. V8 changes to
