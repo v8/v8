f515bcf1f42fa5daf4dc0420456f79ca08be6eb1
etiennep-chromium <etiennep@chromium.org>
2025-08-12 15:46:54 -0400

    [GC] Add default value for incremental_marking_reason
    To fix uninitialized value in
    https://chromium-review.googlesource.com/c/chromium/src/+/6840617
    
    Bug: 373759996
    Change-Id: I1c758a93aace1790d79d253c66df2b103c3ccee5
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6841605
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#101862}

--- src/heap/gc-tracer.h
+++ src/heap/gc-tracer.h
@@ -187,7 +187,8 @@ class V8_EXPORT_PRIVATE GCTracer {
     State state;
 
     GarbageCollectionReason gc_reason;
-    GarbageCollectionReason incremental_marking_reason;
+    GarbageCollectionReason incremental_marking_reason{
+        GarbageCollectionReason::kUnknown};
     const char* collector_reason;
 
     // The Isolate's priority during the current GC cycle. The priority is set
