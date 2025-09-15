99fc9b3713f3a667f647c742661fc5a1d2accbd0
Raphael Herouart <rherouart@chromium.org>
2025-09-15 15:22:45 +0000

    [maglev][turbofan] Making sure functions do get optimized
    Change-Id: Ia2c92855002895df26d200b26f243b4b6bcd460b
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6947933
    Commit-Queue: Raphael Herouart <rherouart@chromium.org>
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#102511}

--- test/mjsunit/ignition/optimize-proto-assign-seq.js
+++ test/mjsunit/ignition/optimize-proto-assign-seq.js
@@ -6,12 +6,14 @@
 // Flags: --proto-assign-seq-opt
 // Flags: --allow-natives-syntax
 // Flags: --no-jitless
+// Flags: --no-lazy-feedback-allocation
+// Flags: --maglev --turbofan
 
 function test_class_fast_path() {
-  console.log("\n\ntest_class_is_slow_path");
   class test_class {
     constructor() { }
   }
+
   test_class.prototype.func = function () { return "test_class.prototype.func" };
   test_class.prototype.arrow_func = () => { return "test_class.prototype.arrow_func" };
   test_class.prototype.smi = 1
@@ -19,8 +21,10 @@ function test_class_fast_path() {
   // TODO(rherouart): handle object and array literals
   // test_class.prototype.obj = {o:{smi:1, str:"str"}, smi:0};
   // test_class.prototype.arr = [0,1,2];
+  return new test_class();
+}
 
-  test_instance = new test_class();
+function assert_test_class_fast_path(test_instance){
   assertEquals(test_instance.func(), "test_class.prototype.func");
   assertEquals(test_instance.arrow_func(), "test_class.prototype.arrow_func");
   assertEquals(test_instance.smi, 1);
@@ -28,7 +32,6 @@ function test_class_fast_path() {
 }
 
 function test_function_fast_paths() {
-  console.log("\n\ntest_function_fast_paths");
   function test_function() { }
 
 
@@ -37,7 +40,10 @@ function test_function_fast_paths() {
   test_function.prototype.smi = 1
   test_function.prototype.str = "test_function.prototype.str"
 
-  test_instance = new test_function();
+  return new test_function();
+}
+
+function assert_test_function_fast_paths(test_instance){
   assertEquals(test_instance.func(), "test_function.prototype.func");
   assertEquals(test_instance.arrow_func(), "test_function.prototype.arrow_func");
   assertEquals(test_instance.smi, 1);
@@ -45,19 +51,20 @@ function test_function_fast_paths() {
 }
 
 function test_has_prototype_keys() {
-  console.log("\n\ntest_has_prototype_keys");
   const test_object = {}
   Object.defineProperty(test_object, "prototype", {
     value: {}
   });
-
-  console.log(JSON.stringify(test_object, null, 2));
 
   test_object.prototype.func = function () { return "test_object.prototype.func" };
   test_object.prototype.arrow_func = () => { return "test_object.prototype.arrow_func" };
   test_object.prototype.smi = 1
   test_object.prototype.str = "test_object.prototype.str"
 
+  return test_object;
+}
+
+function assert_test_has_prototype_keys(test_object){
   assertEquals(test_object.prototype.func(), "test_object.prototype.func");
   assertEquals(test_object.prototype.arrow_func(), "test_object.prototype.arrow_func");
   assertEquals(test_object.prototype.smi, 1);
@@ -65,7 +72,6 @@ function test_has_prototype_keys() {
 }
 
 function test_arrow_function() {
-  console.log("\n\ntest_arrow_function");
   var test_arrow_func = () => { };
 
   Object.defineProperty(test_arrow_func, "prototype", {
@@ -76,7 +82,10 @@ function test_arrow_function() {
   test_arrow_func.prototype.arrow_func = () => { return "test_function.prototype.arrow_func" };
   test_arrow_func.prototype.smi = 1
   test_arrow_func.prototype.str = "test_function.prototype.str"
+  return test_arrow_func;
+}
 
+function assert_test_arrow_function(test_arrow_func){
   assertEquals(test_arrow_func.prototype.func(), "test_function.prototype.func");
   assertEquals(test_arrow_func.prototype.arrow_func(), "test_function.prototype.arrow_func");
   assertEquals(test_arrow_func.prototype.smi, 1);
@@ -84,8 +93,6 @@ function test_arrow_function() {
 }
 
 function test_has_setters() {
-  console.log("\n\ntest_has_setters");
-
   function test_function() { };
 
   Object.defineProperty(test_function.prototype, "key", {
@@ -96,36 +103,22 @@ function test_has_setters() {
 
   test_function.prototype.func = function () { return "test_function.prototype.func" };
   test_function.prototype.arrow_func = () => { return "test_function.prototype.arrow_func" };
+  test_function.prototype.key = "key"
   test_function.prototype.smi = 1
   test_function.prototype.str = "test_function.prototype.str"
 
-  assertEquals(test_function.prototype.func(), "test_function.prototype.func");
-  assertEquals(test_function.prototype.arrow_func(), "test_function.prototype.arrow_func");
-  assertEquals(test_function.prototype.smi, 1);
-  assertEquals(test_function.prototype.str, "test_function.prototype.str");
 
-  var test_instance = new test_function();
+  return new test_function();
+}
 
-  test_function.prototype.func = function () { return "test_function.prototype.func" };
-  test_function.prototype.arrow_func = () => { return "test_function.prototype.arrow_func" };
-  test_function.prototype.smi = 1
-  test_function.prototype.str = "test_function.prototype.str"
-  test_function.prototype.key = 1
-  assertEquals(Object.keys(test_function.prototype).length, 0);
-
-  var test_instance2 = new test_function();
-  assertEquals(Object.keys(test_instance2.__proto__).length, 0);
-
-  assertEquals(test_instance.func(), "test_function.prototype.func");
-  assertEquals(test_instance.arrow_func(), "test_function.prototype.arrow_func");
+function assert_test_has_setters(test_instance) {
+  assertEquals(Object.keys(test_instance.__proto__).length, 2);
   assertEquals(test_instance.smi, 1);
   assertEquals(test_instance.str, "test_function.prototype.str");
 }
 
 
 function test_prototype_proto_keys() {
-  console.log("\n\ntest_prototype_proto_keys");
-
   function test_function() { };
   var das_proto = {}
   Object.defineProperty(das_proto, "smi", {
@@ -143,12 +136,10 @@ function test_prototype_proto_keys() {
   test_function.prototype.str = "test_function.prototype.str"
   test_function.prototype.smi = 1
 
-  assertEquals(test_function.prototype.func(), "test_function.prototype.func");
-  assertEquals(test_function.prototype.arrow_func(), "test_function.prototype.arrow_func");
-  assertEquals(test_function.prototype.smi, 0);
-  assertEquals(test_function.prototype.str, "foo");
+  return new test_function();
+}
 
-  var test_instance = new test_function();
+function assert_test_prototype_proto_keys(test_instance){
   assertEquals(test_instance.func(), "test_function.prototype.func");
   assertEquals(test_instance.arrow_func(), "test_function.prototype.arrow_func");
   assertEquals(test_instance.smi, 0);
@@ -156,8 +147,6 @@ function test_prototype_proto_keys() {
 }
 
 function test_feedback_vector_side_effect() {
-  console.log("\n\ntest_feedback_vector_side_effect");
-
   function outer() {
     function Class() { }
     Class.prototype.key_1 = function inner() {
@@ -174,27 +163,28 @@ function test_feedback_vector_side_effect() {
 
   let inner_1 = outer();
   let inner_2 = outer();
+  return [inner_1, inner_2]
+}
 
-  assertEquals(inner_1.key_1(), inner_2.key_1());
+function assert_test_feedback_vector_side_effect(inners){
+  assertEquals(inners[0].key_1(), inners[1].key_1());
 }
 
 function test_assign_key_multiple_times() {
-  console.log("\n\ntest_assign_key_multiple_times");
-
   function test_function() { };
 
   test_function.prototype.smi = function () { return "test_function.prototype.func" };
   test_function.prototype.smi = () => { return "test_function.prototype.arrow_func" };
   test_function.prototype.smi = 1
 
-  assertEquals(test_function.prototype.smi, 1);
+  return new test_function();
+}
 
-  var x = new test_function();
-  assertEquals(x.smi, 1);
+function assert_test_assign_key_multiple_times(x) {
+    assertEquals(x.smi, 1);
 }
 
 function test_not_proto_assign_seq() {
-  console.log("\n\ntest_not_proto_assign_seq")
   function test_function() { };
   test_function.prototype = []
 
@@ -202,15 +192,16 @@ function test_not_proto_assign_seq() {
   test_function.prototype["1"] = () => { return "test_function.prototype.arrow_func" };
   test_function.prototype["2"] = 1
 
+  return test_function;
+}
+
+function assert_test_not_proto_assign_seq(test_function){
   assertEquals(test_function.prototype["0"](), "test_function.prototype.func");
   assertEquals(test_function.prototype["1"](), "test_function.prototype.arrow_func");
   assertEquals(test_function.prototype["2"], 1);
 }
 
-
-
 function test_prototype_read_only() {
-  console.log("\n\ntest_prototype_read_only");
   function test_function() { }
 
   Object.defineProperty(test_function.prototype, "key", { value: 0, writable: false })
@@ -221,7 +212,10 @@ function test_prototype_read_only() {
   test_function.prototype.str = "test_function.prototype.str"
   test_function.prototype.key = 1
 
-  test_instance = new test_function();
+  return new test_function();
+}
+
+function assert_test_prototype_read_only(test_instance) {
   assertEquals(test_instance.func(), "test_function.prototype.func");
   assertEquals(test_instance.arrow_func(), "test_function.prototype.arrow_func");
   assertEquals(test_instance.smi, 1);
@@ -230,20 +224,19 @@ function test_prototype_read_only() {
 }
 
 function test_eval_return_last_set_property() {
-  console.log("\n\ntest_eval_return_last_set_property");
-
-  let result = eval(`
+  return eval(`
     function Foo(){}
     Foo.prototype.k1 = 1;
     Foo.prototype.k2 = 2;
     Foo.prototype.k3 = 3;
   `)
+}
+
+function assert_test_eval_return_last_set_property(result){
   assertEquals(3, result);
 }
 
-
 function test_null_prototype() {
-  console.log("\n\ntest_null_prototype");
   function test_function() { }
 
   test_function.prototype = null;
@@ -263,8 +256,10 @@ function test_null_prototype() {
   }, TypeError);
 }
 
+function assert_test_null_prototype(test_instance){
+}
+
 function test_variable_proxy() {
-  console.log("\n\ntest_variable_proxy");
   var calls = 0;
   let foo = {
     get prototype() {
@@ -278,14 +273,15 @@ function test_variable_proxy() {
     foo.prototype.k2 = 2;
   }, TypeError);
 
-  assertEquals(calls, 1);
-  assertEquals(Object.keys(foo).length, 0);
+  return [calls,foo]
 }
 
+function assert_test_variable_proxy(arr){
+  assertEquals(arr[0], 1);
+  assertEquals(Object.keys(arr[1]).length, 0);
+}
 
 function test_variable_proxy_eval() {
-  console.log("\n\ntest_variable_proxy_eval");
-
   var foo = function () { };
   (function inner_test() {
 
@@ -300,8 +296,11 @@ function test_variable_proxy_eval() {
   })();
 }
 
+function assert_test_variable_proxy_eval(foo){
+
+}
+
 function test_different_left_most_var() {
-  console.log("\n\ntest_different_left_most_var");
   // Note: This code should not generate a SetPrototypeProperties Instruction
   function foo() { }
   function bar() { }
@@ -311,41 +310,49 @@ function test_different_left_most_var() {
   foo.prototype.k2 = 3;
   bar.prototype.k2 = 4;
 
-  assertEquals(foo.prototype.k1, 1);
-  assertEquals(bar.prototype.k1, 2);
-  assertEquals(foo.prototype.k2, 3);
-  assertEquals(bar.prototype.k2, 4);
+  return [foo,bar];
+}
+
+function assert_test_different_left_most_var(arr){
+  assertEquals(arr[0].prototype.k1, 1);
+  assertEquals(arr[1].prototype.k1, 2);
+  assertEquals(arr[0].prototype.k2, 3);
+  assertEquals(arr[1].prototype.k2, 4);
 }
 
 functions = [
-  test_null_prototype,
-  test_class_fast_path,
-  test_function_fast_paths,
-  test_has_prototype_keys,
-  test_arrow_function,
-  test_has_setters,
-  test_prototype_proto_keys,
-  test_feedback_vector_side_effect,
-  test_assign_key_multiple_times,
-  test_not_proto_assign_seq,
-  test_prototype_read_only,
-  test_eval_return_last_set_property,
-  test_variable_proxy_eval,
-  test_variable_proxy,
-  test_different_left_most_var
+  "test_null_prototype",
+  "test_class_fast_path",
+  "test_function_fast_paths",
+  "test_has_prototype_keys",
+  "test_arrow_function",
+  "test_has_setters",
+  "test_prototype_proto_keys",
+  "test_feedback_vector_side_effect",
+  "test_assign_key_multiple_times",
+  "test_not_proto_assign_seq",
+  "test_prototype_read_only",
+  "test_eval_return_last_set_property",
+  "test_variable_proxy_eval",
+  "test_variable_proxy",
+  "test_different_left_most_var"
 ];
 
 functions.forEach(function (func, index) {
-  func();
-  %CompileBaseline(func);
-  func();
-  %PrepareFunctionForOptimization(func);
-  func();
-  func();
-  %OptimizeMaglevOnNextCall(func)
-  func();
-  func();
-  %OptimizeFunctionOnNextCall(func);
-  func();
-  func();
+  console.log(func);
+  globalThis["assert_"+func](globalThis[func]());
+  %CompileBaseline(globalThis[func]);
+  globalThis["assert_"+func](globalThis[func]());
+  %PrepareFunctionForOptimization(globalThis[func]);
+  globalThis["assert_"+func](globalThis[func]());
+  globalThis["assert_"+func](globalThis[func]());
+  %OptimizeMaglevOnNextCall(globalThis[func]);
+  globalThis["assert_"+func](globalThis[func]());
+  assertOptimized(globalThis[func]);
+  assertTrue(isMaglevved(globalThis[func]));
+  globalThis["assert_"+func](globalThis[func]());
+  %OptimizeFunctionOnNextCall(globalThis[func]);
+  globalThis["assert_"+func](globalThis[func]());
+  assertOptimized(globalThis[func]);
+  globalThis["assert_"+func](globalThis[func]());
 });
