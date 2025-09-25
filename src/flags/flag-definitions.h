78867e361f3495857778f7e063181f9e3c6cc1e0
pthier <pthier@chromium.org>
2025-09-25 10:50:01 +0200

    [regexp] Mark --regexp-assemble-from-bytecode non-experimental
    Assembling RegExp from bytecode is now feature complete, so it is not
    considered experimental anymore.
    
    Bug: 437003349
    Change-Id: I7374cdd007d54d1a7204fa1cadbed2c699054663
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6983043
    Reviewed-by: Jakob Linke <jgruber@chromium.org>
    Commit-Queue: Patrick Thier <pthier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#102754}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -3081,8 +3081,8 @@ DEFINE_INT(regexp_tier_up_ticks, 1,
 DEFINE_BOOL(regexp_peephole_optimization, REGEXP_PEEPHOLE_OPTIMIZATION_BOOL,
             "enable peephole optimization for regexp bytecode")
 DEFINE_BOOL(regexp_results_cache, true, "enable the regexp results cache")
-DEFINE_EXPERIMENTAL_FEATURE(regexp_assemble_from_bytecode,
-                            "assemble regexp JIT-code from bytecode")
+DEFINE_BOOL(regexp_assemble_from_bytecode, false,
+            "assemble regexp JIT-code from bytecode")
 DEFINE_NEG_NEG_IMPLICATION(regexp_tier_up, regexp_assemble_from_bytecode)
 DEFINE_BOOL(trace_regexp_peephole_optimization, false,
             "trace regexp bytecode peephole optimization")
