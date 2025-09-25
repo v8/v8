77c3daed6d2b89a45531bf4b0f81d9b697768f87
Florian Loitsch <floitsch@rivosinc.com>
2025-09-25 10:14:24 +0000

    [riscv] Enable highway for RISC-V
    This reverts commit 3b8bd6777fbe6959a0419467295043aeec5c0dfc.
    
    Change-Id: Idcfe5e8dc1d22a549b04a89e284795881cb8d0d9
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6979448
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Florian Loitsch <floitsch@rivosinc.com>
    Cr-Commit-Position: refs/heads/main@{#102820}

--- src/runtime/runtime-regexp.cc
+++ src/runtime/runtime-regexp.cc
@@ -2203,7 +2203,9 @@ inline void RegExpMatchGlobalAtom_OneCharPattern(
   while (block + stride * max_count <= end) {
     for (int i = 0; i < max_count; i++, block += stride) {
       const auto input = hw::LoadU(tag, block);
-      const auto match = input == mask;
+      // TODO(floitsch): use an operator for the comparison when it is available
+      // on RISC-V.
+      const auto match = hw::Eq(input, mask);
       // Lanes with matches have all bits set, so we subtract to increase the
       // count by 1.
       submatches = hw::Sub(submatches, hw::VecFromMask(tag, match));
@@ -2224,7 +2226,9 @@ inline void RegExpMatchGlobalAtom_OneCharPattern(
   DCHECK_LT(end - block, stride * max_count);
   for (; block + stride <= end; block += stride) {
     const auto input = hw::LoadU(tag, block);
-    const auto match = input == mask;
+    // TODO(floitsch): use an operator for the comparison when it is available
+    // on RISC-V.
+    const auto match = hw::Eq(input, mask);
     submatches = hw::Sub(submatches, hw::VecFromMask(tag, match));
     if (!hw::AllFalse(tag, match)) {
       last_match_block = block;
