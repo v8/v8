cc30a95ac63fd693f8d8b8aa7ea13b1b3a685e61
Clemens Backes <clemensb@chromium.org>
2025-10-09 12:49:46 +0200

    [wasm] Reuse `UseNativeModuleInIsolate` logic more often
    `UpdateNativeModuleCache` implements the same logic; reuse
    `UseNativeModuleInIsolate` there.
    
    R=jkummerow@chromium.org
    
    Bug: 446885278
    Change-Id: I7d3d940a3b5a7aed01e27212690603f1f41f5422
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7021161
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#103043}

--- src/wasm/wasm-engine.cc
+++ src/wasm/wasm-engine.cc
@@ -1652,27 +1652,8 @@ std::shared_ptr<NativeModule> WasmEngine::UpdateNativeModuleCache(
   void* prev = native_module.get();
   native_module =
       native_module_cache_.Update(std::move(native_module), has_error);
-  if (prev == native_module.get()) return native_module;
-  bool remove_all_code = false;
-  {
-    base::MutexGuard guard(&mutex_);
-    DCHECK(native_modules_.contains(native_module.get()));
-    native_modules_[native_module.get()]->isolates.insert(isolate);
-    DCHECK(isolates_.contains(isolate));
-    auto* isolate_data = isolates_[isolate].get();
-    isolate_data->native_modules.insert(native_module.get());
-    if (isolate_data->keep_in_debug_state && !native_module->IsInDebugState()) {
-      remove_all_code = true;
-      native_module->SetDebugState(kDebugging);
-    }
-    if (isolate_data->log_codes && !native_module->log_code()) {
-      EnableCodeLogging(native_module.get());
-    }
-  }
-  if (remove_all_code) {
-    WasmCodeRefScope ref_scope;
-    native_module->RemoveCompiledCode(
-        NativeModule::RemoveFilter::kRemoveNonDebugCode);
+  if (prev != native_module.get()) {
+    UseNativeModuleInIsolate(native_module.get(), isolate);
   }
   return native_module;
 }
