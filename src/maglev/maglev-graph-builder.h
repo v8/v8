39c9925e68f250813b65d624372c141b4ab1fc50
Dan Carney <dcarney@chromium.org>
2025-10-21 15:47:11 +0000

    [maglev] truncate passed arguments length to known js functions when they are unreachable
    Change-Id: I151825fbda0596d12e397fadf80a0fe5a244fa26
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7053222
    Reviewed-by: Toon Verwaest <verwaest@chromium.org>
    Commit-Queue: Dan Carney <dcarney@chromium.org>
    Reviewed-by: Erik Corry <erikcorry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#103343}

--- src/maglev/maglev-graph-builder.h
+++ src/maglev/maglev-graph-builder.h
@@ -96,12 +96,11 @@ class MaglevGraphBuilder {
     int result_size_;
   };
 
-  explicit MaglevGraphBuilder(LocalIsolate* local_isolate,
-                              MaglevCompilationUnit* compilation_unit,
-                              Graph* graph,
-                              MaglevCallerDetails* caller_details = nullptr);
+  V8_EXPORT_PRIVATE explicit MaglevGraphBuilder(
+      LocalIsolate* local_isolate, MaglevCompilationUnit* compilation_unit,
+      Graph* graph, MaglevCallerDetails* caller_details = nullptr);
 
-  bool Build();
+  V8_EXPORT_PRIVATE bool Build();
 
   ReduceResult BuildInlineFunction(SourcePosition call_site_position,
                                    ValueNode* context, ValueNode* function,
