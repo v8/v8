4c8dc70722c78a27c5ce15380de0868a2e271e51
Marja Hölttä <marja@chromium.org>
2025-10-27 12:45:00 +0100

    [maglev] Fix a bug in (a+b)|0 omitting overflow checks
    If a function returns the result of (a+b)|0, we cannot omit the overflow
    check.
    
    This gotcha here is that UnsetCanTruncateToInt32Inputs only worked for
    FixedInputNodes, and ReturnedValue is not a FixedInputNode.
    
    Fixed: 454314508
    Change-Id: Id3a5ae99332031bc864bd9928f96102f44f10802
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7079034
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#103360}

--- src/maglev/maglev-truncation.h
+++ src/maglev/maglev-truncation.h
@@ -96,7 +96,13 @@ class PropagateTruncationProcessor {
     return ProcessResult::kContinue;
   }
 
-  ProcessResult Process(Identity* node) { return ProcessResult::kContinue; }
+  ProcessResult Process(Identity* node) {
+    return ProcessSingleInputSpecialNode(node);
+  }
+  ProcessResult Process(ReturnedValue* node) {
+    return ProcessSingleInputSpecialNode(node);
+  }
+
   ProcessResult Process(Dead* node) { return ProcessResult::kContinue; }
 
   // TODO(victorgomes): We can only truncate CheckedHoleyFloat64ToInt32
@@ -130,16 +136,20 @@ class PropagateTruncationProcessor {
       return UnsetCanTruncateToInt32ForFixedInputNodes<NodeT, 0>(node);
     }
 #ifdef DEBUG
-    // Non-fixed input nodes don't expect float64 as inputs, except
-    // ReturnedValue.
-    if constexpr (std::is_same_v<NodeT, ReturnedValue>) {
-      return;
-    }
     for (Input input : node->inputs()) {
-      DCHECK_NE(input.node()->value_representation(),
-                ValueRepresentation::kFloat64);
+      DCHECK(!input.node()->can_truncate_to_int32());
     }
 #endif  // DEBUG
+  }
+
+  ProcessResult ProcessSingleInputSpecialNode(ValueNode* node) {
+    if (!node->can_truncate_to_int32()) {
+      ValueNode* input_node = node->input_node(0);
+      if (input_node->value_representation() != ValueRepresentation::kTagged) {
+        input_node->set_can_truncate_to_int32(false);
+      }
+    }
+    return ProcessResult::kContinue;
   }
 
   void UnsetCanTruncateToInt32ForDeoptFrameInput(ValueNode* node) {
