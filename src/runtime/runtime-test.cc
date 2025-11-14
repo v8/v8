65a7d708a014e06f9689c1848d23ca5f277e3e3e
Matthias Liedtke <mliedtke@chromium.org>
2025-11-14 11:17:59 +0100

    [runtime] Make %ConstructConsString more robust
    This is a follow-up to https://crrev.com/c/7137292 which exposed
    %ConstructConsString to fuzzers. This runtime function did not handle
    cases where one of the two inputs is a thin string.
    
    Fixed: 460300660
    Change-Id: Ic10eb0089d942b98f5703fac88839b73ff1a18d7
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7156856
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Matthias Liedtke <mliedtke@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#103718}

--- src/runtime/runtime-test.cc
+++ src/runtime/runtime-test.cc
@@ -173,11 +173,8 @@ RUNTIME_FUNCTION(Runtime_ConstructConsString) {
   DirectHandle<String> right = args.at<String>(1);
   CHECK_UNLESS_FUZZING(left->length() + right->length() >=
                        ConsString::kMinLength);
-
-  const bool is_one_byte =
-      left->IsOneByteRepresentation() && right->IsOneByteRepresentation();
-  const int length = left->length() + right->length();
-  return *isolate->factory()->NewConsString(left, right, length, is_one_byte);
+  CHECK_UNLESS_FUZZING(left->length() + right->length() <= String::kMaxLength);
+  return *isolate->factory()->NewConsString(left, right).ToHandleChecked();
 }
 
 RUNTIME_FUNCTION(Runtime_ConstructSlicedString) {
