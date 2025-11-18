5bd8e91bbe764b741aa557348466cfdaa6f26ff3
Yolanda Chen <yolanda.chen@intel.com>
2025-11-18 15:46:40 +0800

    [turboshaft][csa] Use pgo profile after empty copying phase
    The block id before and after BlockInstrumentationPhase may change.
    Read the profile after the empty copying phase will ensure we have the same control flow as when taking the profile.
    
    Bug: 459848855
    Change-Id: I9903e1356328c35662e062bc6066e9c08e641f14
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7164248
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Commit-Queue: Chen, Yolanda <yolanda.chen@intel.com>
    Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#103779}

--- src/compiler/turboshaft/pipelines.h
+++ src/compiler/turboshaft/pipelines.h
@@ -235,10 +235,6 @@ class V8_EXPORT_PRIVATE Pipeline {
     if (V8_UNLIKELY(data()->pipeline_kind() == TurboshaftPipelineKind::kCSA ||
                     data()->pipeline_kind() ==
                         TurboshaftPipelineKind::kTSABuiltin)) {
-      if (profile) {
-        RUN_MAYBE_ABORT(ProfileApplicationPhase, profile);
-      }
-
       if (v8_flags.reorder_builtins &&
           Builtins::IsBuiltinId(info()->builtin())) {
         UnparkedScopeIfNeeded unparked_scope(data()->broker());
@@ -273,6 +269,10 @@ class V8_EXPORT_PRIVATE Pipeline {
         ZoneWithName<kTempZoneName> temp_zone(data()->zone_stats(),
                                               kTempZoneName);
         CopyingPhase<>::Run(data(), temp_zone);
+
+        if (profile) {
+          RUN_MAYBE_ABORT(ProfileApplicationPhase, profile);
+        }
       }
     }
 
