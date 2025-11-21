c0eab624f8493e2edf4ebf4b4ef221adff8b03f4
Victor Gomes <victorgomes@chromium.org>
2025-11-21 07:44:50 -0800

    Revert "[maglev type system] Make GetTaggedValue bail out if it detects an empty type"
    This reverts commit 1c58335812ebe4952686a16c6b95554701587f9e.
    
    Reason for revert: https://crbug.com/462441046
    
    Original change's description:
    > [maglev type system] Make GetTaggedValue bail out if it detects an empty type
    >
    > The rest of the pipeline (ConvertInputTo <- AddNewNode <- ...) can already
    > handle the bailout.
    >
    > This CL makes the bailout actually happen.
    >
    > Bug: 428667907
    > Change-Id: I4c08c0561fbaff95cda0125d389c9e753fc27506
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7167027
    > Reviewed-by: Victor Gomes <victorgomes@chromium.org>
    > Commit-Queue: Marja Hölttä <marja@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#103770}
    
    Bug: 428667907
    Change-Id: I03b2445f04d0dda6480ee99ed64d4216ed4b2026
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7186762
    Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#103881}

--- src/maglev/maglev-reducer-inl.h
+++ src/maglev/maglev-reducer-inl.h
@@ -535,15 +535,15 @@ ReduceResult MaglevReducer<BaseT>::GetTaggedValue(
     return alt;
   }
 
-  // Check for the empty type first, so that we don't emit unsafe conversion
-  // nodes below.
-  if (IsEmptyNodeType(node_info->type())) {
-    return EmitUnconditionalDeopt(DeoptimizeReason::kWrongValue);
-  }
-
+  // This is called when converting inputs in AddNewNode. We might already have
+  // an empty type for `value` here. Make sure we don't add unsafe conversion
+  // nodes in that case by checking for the empty node type explicitly.
+  // TODO(marja): The checks can be removed after we're able to bail out
+  // earlier.
   switch (representation) {
     case ValueRepresentation::kInt32: {
-      if (NodeTypeIsSmi(node_info->type())) {
+      if (!IsEmptyNodeType(node_info->type()) &&
+          NodeTypeIsSmi(node_info->type())) {
         return alternative.set_tagged(
             AddNewNodeNoInputConversion<UnsafeSmiTagInt32>({value}));
       }
@@ -551,7 +551,8 @@ ReduceResult MaglevReducer<BaseT>::GetTaggedValue(
           AddNewNodeNoInputConversion<Int32ToNumber>({value}));
     }
     case ValueRepresentation::kUint32: {
-      if (NodeTypeIsSmi(node_info->type())) {
+      if (!IsEmptyNodeType(node_info->type()) &&
+          NodeTypeIsSmi(node_info->type())) {
         return alternative.set_tagged(
             AddNewNodeNoInputConversion<UnsafeSmiTagUint32>({value}));
       }
@@ -559,7 +560,7 @@ ReduceResult MaglevReducer<BaseT>::GetTaggedValue(
           AddNewNodeNoInputConversion<Uint32ToNumber>({value}));
     }
     case ValueRepresentation::kFloat64: {
-      if (node_info->is_smi()) {
+      if (!IsEmptyNodeType(node_info->type()) && node_info->is_smi()) {
         return alternative.set_tagged(
             AddNewNodeNoInputConversion<CheckedSmiTagFloat64>({value}));
       }
@@ -568,7 +569,7 @@ ReduceResult MaglevReducer<BaseT>::GetTaggedValue(
               {value}, Float64ToTagged::ConversionMode::kCanonicalizeSmi));
     }
     case ValueRepresentation::kHoleyFloat64: {
-      if (node_info->is_smi()) {
+      if (!IsEmptyNodeType(node_info->type()) && node_info->is_smi()) {
         return alternative.set_tagged(
             AddNewNodeNoInputConversion<CheckedSmiTagFloat64>({value}));
       }
@@ -578,7 +579,8 @@ ReduceResult MaglevReducer<BaseT>::GetTaggedValue(
     }
 
     case ValueRepresentation::kShiftedInt53:
-      if (NodeTypeIsSmi(node_info->type())) {
+      if (!IsEmptyNodeType(node_info->type()) &&
+          NodeTypeIsSmi(node_info->type())) {
         return alternative.set_tagged(
             AddNewNodeNoInputConversion<UnsafeSmiTagShiftedInt53>({value}));
       }
@@ -586,7 +588,8 @@ ReduceResult MaglevReducer<BaseT>::GetTaggedValue(
           AddNewNodeNoInputConversion<ShiftedInt53ToNumber>({value}));
 
     case ValueRepresentation::kIntPtr:
-      if (NodeTypeIsSmi(node_info->type())) {
+      if (!IsEmptyNodeType(node_info->type()) &&
+          NodeTypeIsSmi(node_info->type())) {
         return alternative.set_tagged(
             AddNewNodeNoInputConversion<UnsafeSmiTagIntPtr>({value}));
       }
