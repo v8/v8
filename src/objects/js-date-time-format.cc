cf9b44b56d2ded62263241e4aee635e0be1d0950
Frank Tang <ftang@chromium.org>
2025-11-24 23:19:38 -0800

    [intl] Fix intl402/*/prototype/resolvedOptions/resolved-*-unicode-extensions-and-options Bug: 423663304
    Change-Id: Ie60ce521a6a9282a7757c2d82933398a6e53ce2a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7202716
    Reviewed-by: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
    Commit-Queue: Frank Tang <ftang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#103994}

--- src/objects/js-date-time-format.cc
+++ src/objects/js-date-time-format.cc
@@ -2663,7 +2663,8 @@ MaybeDirectHandle<JSDateTimeFormat> JSDateTimeFormat::CreateDateTimeFormat(
   if (maybe_calendar.FromJust()) {
     auto ca_extension_it = r.extensions.find("ca");
     if (ca_extension_it != r.extensions.end() &&
-        ca_extension_it->second != calendar_stdstr) {
+        ca_extension_it->second != calendar_stdstr &&
+        Intl::IsValidCalendar(icu_locale, calendar_stdstr)) {
       icu_locale.setUnicodeKeywordValue("ca", nullptr, status);
       DCHECK(U_SUCCESS(status));
     }
@@ -2671,7 +2672,8 @@ MaybeDirectHandle<JSDateTimeFormat> JSDateTimeFormat::CreateDateTimeFormat(
   if (maybe_numberingSystem.FromJust()) {
     auto nu_extension_it = r.extensions.find("nu");
     if (nu_extension_it != r.extensions.end() &&
-        nu_extension_it->second != numbering_system_str) {
+        nu_extension_it->second != numbering_system_str &&
+        Intl::IsValidNumberingSystem(numbering_system_str)) {
       icu_locale.setUnicodeKeywordValue("nu", nullptr, status);
       DCHECK(U_SUCCESS(status));
     }
