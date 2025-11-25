4362f55955a732bded1bf829980e0bf45a112b39
Nico Hartmann <nicohartmann@chromium.org>
2025-11-25 08:12:08 -0800

    Revert "Move MemCopy and friends to base"
    This reverts commit 2b39289f92db84794cbfd7f34b891bda77c9ad6f.
    
    Reason for revert: https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Win64%20-%20debug%20builder/24362/overview
    
    Original change's description:
    > Move MemCopy and friends to base
    >
    > The methods don't depend on any VM-internal types. This also allows
    > reusing them in base itself.
    >
    > Change-Id: I5ce45f0f05ada202ef94eb567e5b6d94316cc46f
    > Bug: 448409803
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7201213
    > Reviewed-by: Patrick Thier <pthier@chromium.org>
    > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#103951}
    
    Bug: 448409803
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I25b629e1bd608e2ca24a837c46a04e3ec2e3db3d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7203291
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nico Hartmann <nicohartmann@chromium.org>
    Commit-Queue: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#103952}

--- src/utils/memcopy.cc
+++ src/utils/memcopy.cc
@@ -4,26 +4,56 @@
 
 #include "src/utils/memcopy.h"
 
-#include "src/base/memcopy.h"
 #include "src/snapshot/embedded/embedded-data-inl.h"
 
-namespace v8::internal {
+namespace v8 {
+namespace internal {
 
-void InitMemCopyFunctions() {
-#if defined(V8_TARGET_ARCH_IA32)
-  if (Isolate::CurrentEmbeddedBlobIsBinaryEmbedded()) {
-    EmbeddedData d = EmbeddedData::FromBlob();
-    v8::base::g_memmove_function = reinterpret_cast<v8::base::MemMoveFunction>(
-        d.InstructionStartOf(Builtin::kMemMove));
-  }
-#elif (V8_OS_POSIX || V8_OS_STARBOARD) && V8_HOST_ARCH_ARM
-  if (Isolate::CurrentEmbeddedBlobIsBinaryEmbedded()) {
-    EmbeddedData d = EmbeddedData::FromBlob();
-    v8::base::g_memcopy_uint8_function =
-        reinterpret_cast<v8::base::MemCopyUint8Function>(
-            d.InstructionStartOf(Builtin::kMemCopyUint8Uint8));
-  }
-#endif  // (V8_OS_POSIX || V8_OS_STARBOARD) && V8_HOST_ARCH_ARM
+#if V8_TARGET_ARCH_IA32
+static void MemMoveWrapper(void* dest, const void* src, size_t size) {
+  memmove(dest, src, size);
 }
 
-}  // namespace v8::internal
+// Initialize to library version so we can call this at any time during startup.
+static MemMoveFunction memmove_function = &MemMoveWrapper;
+
+// Copy memory area to disjoint memory area.
+DISABLE_CFI_ICALL
+V8_EXPORT_PRIVATE void MemMove(void* dest, const void* src, size_t size) {
+  if (size == 0) return;
+  // Note: here we rely on dependent reads being ordered. This is true
+  // on all architectures we currently support.
+  (*memmove_function)(dest, src, size);
+}
+#elif(V8_OS_POSIX || V8_OS_STARBOARD) && V8_HOST_ARCH_ARM
+V8_EXPORT_PRIVATE MemCopyUint8Function memcopy_uint8_function =
+    &MemCopyUint8Wrapper;
+#elif V8_OS_POSIX && V8_HOST_ARCH_MIPS
+V8_EXPORT_PRIVATE MemCopyUint8Function memcopy_uint8_function =
+    &MemCopyUint8Wrapper;
+#endif
+
+void init_memcopy_functions() {
+#if V8_TARGET_ARCH_IA32
+  if (Isolate::CurrentEmbeddedBlobIsBinaryEmbedded()) {
+    EmbeddedData d = EmbeddedData::FromBlob();
+    memmove_function = reinterpret_cast<MemMoveFunction>(
+        d.InstructionStartOf(Builtin::kMemMove));
+  }
+#elif(V8_OS_POSIX || V8_OS_STARBOARD) && V8_HOST_ARCH_ARM
+  if (Isolate::CurrentEmbeddedBlobIsBinaryEmbedded()) {
+    EmbeddedData d = EmbeddedData::FromBlob();
+    memcopy_uint8_function = reinterpret_cast<MemCopyUint8Function>(
+        d.InstructionStartOf(Builtin::kMemCopyUint8Uint8));
+  }
+#elif V8_OS_POSIX && V8_HOST_ARCH_MIPS
+  if (Isolate::CurrentEmbeddedBlobIsBinaryEmbedded()) {
+    EmbeddedData d = EmbeddedData::FromBlob();
+    memcopy_uint8_function = reinterpret_cast<MemCopyUint8Function>(
+        d.InstructionStartOf(Builtin::kMemCopyUint8Uint8));
+  }
+#endif
+}
+
+}  // namespace internal
+}  // namespace v8
