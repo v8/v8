80f8408607a6509ae8755803d985684f8226a936
Clemens Backes <clemensb@chromium.org>
2025-12-01 17:54:07 +0100

    [api] Advance deprecation of FixedArray::Get (with context)
    The context is not used, so the argument was removed in
    https://crrev.com/c/7141498 (v14.4).
    Now we officially deprecate the old API.
    
    R=mlippautz@chromium.org
    
    Change-Id: I48385bb455962189d0653f06718caa38791cfcfe
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7213426
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#104068}

--- include/v8-data.h
+++ include/v8-data.h
@@ -78,7 +78,7 @@ class V8_EXPORT FixedArray : public Data {
  public:
   int Length() const;
 
-  V8_DEPRECATE_SOON("Use the version without context")
+  V8_DEPRECATED("Use the version without context")
   Local<Data> Get(Local<Context> context, int i) const { return Get(i); }
 
   Local<Data> Get(int i) const;
