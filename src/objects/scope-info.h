6e0661d6b0f95270c198d690bb2c4f54ba2acf4d
Manos Koukoutos <manoskouk@chromium.org>
2025-12-08 09:06:46 +0000

    Merge: Add missing V8_EXPORT_PRIVATE annotation
    Version 14.5.42.1
    
    (Cherry-pick from commit 8d5aed8748634cb224aa0ba454587af5e404a31f)
    
    Change-Id: Id66a225a3345a0d81286b870a5acf30c539aaded
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7234154
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Manos Koukoutos <manoskouk@chromium.org>
    Cr-Commit-Position: refs/heads/14.5.42@{#2}
    Cr-Branched-From: 8aca2b7d20e4813076e95afb33568746426b6b4f-refs/heads/main@{#104159}

--- src/objects/scope-info.h
+++ src/objects/scope-info.h
@@ -77,7 +77,7 @@ class ScopeInfo : public TorqueGeneratedScopeInfo<ScopeInfo, HeapObject> {
   int ContextHeaderLength() const;
 
   // Returns true if the respective contexts have a context extension slot.
-  bool HasContextExtensionSlot() const;
+  V8_EXPORT_PRIVATE bool HasContextExtensionSlot() const;
 
   // Returns true if there is a context with created context extension
   // (meaningful only for contexts that call sloppy eval, see
