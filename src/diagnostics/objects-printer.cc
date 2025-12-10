1b7f2ea75df9bf6dd4b479cfe03bd59b79bbde2f
Manos Koukoutos <manoskouk@chromium.org>
2025-12-10 04:18:55 -0800

    Revert "[iterator-helpers] Fix spec deviations in return method"
    This reverts commit af35329bb456c923b43bf591a82b42f62080e49e.
    
    Reason for revert: https://ci.chromium.org/ui/p/v8/builders/ci/V8%20Linux64%20css%20-%20debug/15515/overview
    
    Original change's description:
    > [iterator-helpers] Fix spec deviations in return method
    >
    > This CL fixes the implementation of the `return` method for simple
    > iterator helpers (like those corresponding to .map, .filter, etc.)
    > and the specialized `flatMap` helper, so as to correctly mark the
    > helper's state as "executing" before calling an underlying iterator's
    > `.return()` method. It also makes sure that this is done correctly
    > if the underlying iterator is inactive (i.e. in SUSPENDED-START).
    > To do so, it adds what is necessary for distinguishing between
    > SUSPENDED-START and SUSPENDED-YIELD.
    >
    > Moreover, it fixes two cleanup bugs in the `flatMap` helper, ensuring
    > that if closing an inner iterator throws an exception, the outer
    > iterator is properly closed and marked as exhausted and, if multiple
    > exceptions occur in this process, the correct one is thrown.
    >
    > Bug: 463528051
    > Change-Id: I3d68c60b12eac336853823af94ccbf51a11efe4e
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7223551
    > Reviewed-by: Olivier Flückiger <olivf@chromium.org>
    > Reviewed-by: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
    > Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#104228}
    
    Bug: 463528051
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I61b0506790bdd0be171738a28dcf1e6dd921d70f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7246734
    Commit-Queue: Manos Koukoutos <manoskouk@chromium.org>
    Reviewed-by: Manos Koukoutos <manoskouk@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Manos Koukoutos <manoskouk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#104232}

--- src/diagnostics/objects-printer.cc
+++ src/diagnostics/objects-printer.cc
@@ -2299,14 +2299,12 @@ void JSIteratorFilterHelper::JSIteratorFilterHelperPrint(std::ostream& os) {
 void JSIteratorTakeHelper::JSIteratorTakeHelperPrint(std::ostream& os) {
   JSIteratorHelperPrintHeader(os, "JSIteratorTakeHelper");
   os << "\n - remaining: " << remaining();
-  os << "\n - innerAlive" << innerAlive();
   JSObjectPrintBody(os, *this);
 }
 
 void JSIteratorDropHelper::JSIteratorDropHelperPrint(std::ostream& os) {
   JSIteratorHelperPrintHeader(os, "JSIteratorDropHelper");
   os << "\n - remaining: " << remaining();
-  os << "\n - innerAlive" << innerAlive();
   JSObjectPrintBody(os, *this);
 }
 
