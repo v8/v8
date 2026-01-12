362b0a15d75e935cc5ecac18f02540c980244e5b
Leszek Swirski <leszeks@chromium.org>
2026-01-12 07:12:06 -0800

    [test] Disable stress-concurrent-marking for inspector test
    No-tree-checks: true
    No-Try: true
    Bug: 40789559
    Change-Id: Ica94ebfe0310e0a56ebcf4d773e2a6e8ba33b23a
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7454274
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#104642}

--- test/inspector/debugger/set-script-source-repl-mode.js
+++ test/inspector/debugger/set-script-source-repl-mode.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --inspector-live-edit
+// Flags: --inspector-live-edit --no-stress-concurrent-marking
 
 const {session, contextGroup, Protocol} =
   InspectorTest.start('Check that setScriptSource works with REPL mode');
