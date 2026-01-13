85967547ced280c3d038b5f73093c772f480bf1b
Leszek Swirski <leszeks@chromium.org>
2026-01-13 00:15:33 -0800

    [test] Disable stress-incremental-marking for inspector test
    https://crrev.com/c/7454274 set the wrong flag
    
    Change-Id: I97131d967117d073cb66e656b22c71426588e7eb
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7452568
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#104656}

--- test/inspector/debugger/set-script-source-repl-mode.js
+++ test/inspector/debugger/set-script-source-repl-mode.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --inspector-live-edit --no-stress-concurrent-marking
+// Flags: --inspector-live-edit --no-stress-incremental-marking
 
 const {session, contextGroup, Protocol} =
   InspectorTest.start('Check that setScriptSource works with REPL mode');
