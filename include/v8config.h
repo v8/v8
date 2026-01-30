e630a7a2926ee6ef198202515d3a32172c4be53c
Nico Weber <thakis@chromium.org>
2026-01-30 07:16:50 -0800

    Revert "[include] Disable std::assume_aligned on MSVC"
    This reverts commit 2106d9c81a1fb9d758ebda1560e2251b0f05150c.
    
    Reason for revert: MSVC is no longer supported.
    
    Original change's description:
    > [include] Disable std::assume_aligned on MSVC
    >
    > The 4GB alignment values are causing MSVC build issues.
    >
    > Change-Id: I19659fa2ce61ff0530879edb89680974d31cbf6a
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5595013
    > Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    > Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    > Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    > Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#94229}
    
    Change-Id: I2fa3e411863b2f9d8678c9b7fd8d1b1b09bc796d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7532145
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#105012}

--- include/v8config.h
+++ include/v8config.h
@@ -541,9 +541,8 @@ path. Add it with -I<path> to the command line
 # define V8_ASSUME USE
 #endif
 
-// Prefer c++20 std::assume_aligned. Don't use it on MSVC though, because it's
-// not happy with our large 4GB alignment values.
-#if __cplusplus >= 202002L && defined(__cpp_lib_assume_aligned) && !V8_CC_MSVC
+// Prefer c++20 std::assume_aligned.
+#if __cplusplus >= 202002L && defined(__cpp_lib_assume_aligned)
 # define V8_ASSUME_ALIGNED(ptr, alignment) \
   std::assume_aligned<(alignment)>(ptr)
 #elif V8_HAS_BUILTIN_ASSUME_ALIGNED
