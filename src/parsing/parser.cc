7c6fbddaea8b34279bc297bb03db2c1fd22a3fce
Igor Sheludko <ishell@chromium.org>
2026-02-19 17:40:20 +0100

    Merged: [parser] Make sure AST visitors handle stack overflow
    ... by enforcing them to explicitly clear the stack overflow state.
    
    Fixed: 485267831
    (cherry picked from commit b07860d0159521ebeaa4361700fe1cdae4818149)
    
    Change-Id: I21df97d19aa7f45fe30fbc3976e559f937f76605
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7613208
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/branch-heads/14.5@{#28}
    Cr-Branched-From: f09d67c66114951c0ea3dc9d4b025461670a9557-refs/heads/14.5.201@{#2}
    Cr-Branched-From: 3f006438f768659ed9776359a421dc432edce53f-refs/heads/main@{#104623}

--- src/parsing/parser.cc
+++ src/parsing/parser.cc
@@ -2737,6 +2737,7 @@ void Parser::ReindexArrowFunctionFormalParameters(
       reindexer.Reindex(p->initializer());
     }
     if (reindexer.HasStackOverflow()) {
+      reindexer.ClearStackOverflow();
       set_stack_overflow();
       return;
     }
@@ -2748,6 +2749,11 @@ void Parser::ReindexComputedMemberName(Expression* computed_name) {
   // name.
   AstFunctionLiteralIdReindexer reindexer(stack_limit_, 1);
   reindexer.Reindex(computed_name);
+  if (reindexer.HasStackOverflow()) {
+    reindexer.ClearStackOverflow();
+    set_stack_overflow();
+    return;
+  }
 }
 
 void Parser::PrepareGeneratorVariables() {
