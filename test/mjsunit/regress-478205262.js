4848752f8625ba778c0b4b15975e2069cf5f8d2b
Erik Corry <erikcorry@chromium.org>
2026-02-19 09:54:29 +0100

    [regexp] Fix flaky stack overflow test
    Bug: 485593067,478205262
    Change-Id: Ib3aecbb0d1c668baff32354fa97af27cafcb5793
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7593053
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Commit-Queue: Erik Corry <erikcorry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#105327}

--- test/mjsunit/regress-478205262.js
+++ test/mjsunit/regress-478205262.js
@@ -2,11 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --stack-size=300
-
 var re_src = "(?=^)";
-for (var i = 0; i < 300; i++) {
+for (var i = 0; i < 1000; i++) {
   re_src = "(?=(?:" + re_src + "|foo))";
 }
-var re = new RegExp(re_src);
-re.test("sdfdsfsdfdfs");
+try {
+  // Should not crash with a stack overflow.
+  var re = new RegExp(re_src);
+  re.test("sdfdsfsdfdfs");
+} catch (e) {
+  // Depending on the C++ compile options the stack frames
+  // may be so big that we get this error.
+  assertTrue(String(e).indexOf("Regular expression too large") > 0);
+}
