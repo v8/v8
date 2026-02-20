361a5e1bee08e5cb8d37b676ff2cfb1c22b9af00
Jakob Kummerow <jkummerow@chromium.org>
2026-02-20 15:12:18 +0100

    Merged: [asm.js] Fix reset of heap_access_shift_position_
    It could get confused by nested shift expressions, leading to
    invalid Wasm modules being generated.
    
    Fixed: 485152421
    (cherry picked from commit c0a41078e69f23668c8d34c61f286a1b5b211f19)
    
    Change-Id: Id2b682afd0e672dcd6909e275af851d1832cf0e4
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7623214
    Commit-Queue: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/branch-heads/14.4@{#62}
    Cr-Branched-From: 80acc26727d5a34e77dabeebe7c9213ec1bd4768-refs/heads/14.4.258@{#1}
    Cr-Branched-From: ce7e597e90f6df3fa4b6df224bc613b80c635450-refs/heads/main@{#104020}

--- src/asmjs/asm-parser.cc
+++ src/asmjs/asm-parser.cc
@@ -1891,7 +1891,6 @@ AsmType* AsmJsParser::ShiftExpression() {
 #define HANDLE_CASE(op, opcode, name, result)                        \
   case TOK(op): {                                                    \
     EXPECT_TOKENn(TOK(op));                                          \
-    heap_access_shift_position_ = kNoHeapAccessShift;                \
     AsmType* b = nullptr;                                            \
     RECURSEn(b = AdditiveExpression());                              \
     if (!(a->IsA(AsmType::Intish()) && b->IsA(AsmType::Intish()))) { \
@@ -1899,6 +1898,8 @@ AsmType* AsmJsParser::ShiftExpression() {
     }                                                                \
     current_function_builder_->Emit(kExpr##opcode);                  \
     a = AsmType::result();                                           \
+    /* Must happen after the RECURSE call to unset its state! */     \
+    heap_access_shift_position_ = kNoHeapAccessShift;                \
     continue;                                                        \
   }
         HANDLE_CASE(SHL, I32Shl, "<<", Signed);
