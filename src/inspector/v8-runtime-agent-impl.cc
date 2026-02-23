f0f1ec193cafd4d01b8a08778796a490336f18d2
Danil Somsikov <dsv@chromium.org>
2026-02-23 05:13:08 -0800

    [M146] Fix Use-After-Free in console message reporting.
    When enabling the Runtime or Console agent, cached console messages are reported. The process of reporting a message can execute JavaScript, which might modify the console message storage (e.g., by calling `console.clear()`). To prevent Use-After-Free issues caused by iterating over a modified container, the current message us now copied and the index is re-checked against the container bounds. This ensures that the reporting loop operates on a stable set of messages, even if JavaScript re-enters and clears the console.
    
    Bug: 485672657
    
    (cherry picked from commit 47a3dfaccda94a78dab9cd770999c60a68c33507)[
    
    Change-Id: Ic5a1887997901ca93df1e1f63c9dc148c9755d89
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7600292
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#105402}
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7633544
    Reviewed-by: Lutz Vahl <vahl@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/branch-heads/14.6@{#21}
    Cr-Branched-From: e04c3a1a2543bdbee7beac8846c9cbe8f657636f-refs/heads/14.6.202@{#1}
    Cr-Branched-From: 3b0b01e6594ec362369dc16f069012a81748c8ba-refs/heads/main@{#105132}

--- src/inspector/v8-runtime-agent-impl.cc
+++ src/inspector/v8-runtime-agent-impl.cc
@@ -1112,18 +1112,14 @@ Response V8RuntimeAgentImpl::enable() {
   m_session->reportAllContexts(this);
   V8ConsoleMessageStorage* storage =
       m_inspector->ensureConsoleMessageStorage(m_session->contextGroupId());
-  // reportMessage() may call back into JavaScript for some of the ValueMirrors
-  // and that JavaScript could add more log messages, invalidating iterators
-  // used here, hence we need to guard against that.
-  // See http://crbug.com/446941355 for more details.
+  // The message queue can be cleared by a getter during message formatting.
+  // Make a copy of the message to avoid a UAF.
   const auto& messages = storage->messages();
   const size_t size = messages.size();
   for (size_t i = 0; i < size; ++i) {
-    if (size < messages.size()) {
-      // Also guard against the case where the message queue was cleared.
-      break;
-    }
-    if (!reportMessage(messages[i].get(), false)) {
+    if (i >= messages.size()) break;
+    V8ConsoleMessage message = *messages[i];
+    if (!reportMessage(&message, false)) {
       break;
     }
   }
