2a189d386689cb4db6ae4d44ef71d72ed2fdc196
Liu Yu <liuyu@loongson.cn>
2026-02-27 11:23:53 +0800

    Merged:[loong64][wasm][SIMD] Avoid using output reg as a temp register
    An error may occur if the output register is the same as one of
    the input registers.
    
    (cherry picked from commit ff93af1d8e02f3482242b9050e57f9e12edb9469)
    
    Bug: 489897728
    Change-Id: I81afae3c46b818669e51d1fe3af131239e3365ce
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7632261
    Commit-Queue: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Auto-Submit: Liu Yu <liuyu@loongson.cn>
    Reviewed-by: Lutz Vahl <vahl@chromium.org>
    Reviewed-by: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Cr-Commit-Position: refs/branch-heads/14.6@{#31}
    Cr-Branched-From: e04c3a1a2543bdbee7beac8846c9cbe8f657636f-refs/heads/14.6.202@{#1}
    Cr-Branched-From: 3b0b01e6594ec362369dc16f069012a81748c8ba-refs/heads/main@{#105132}

--- src/wasm/baseline/loong64/liftoff-assembler-loong64-inl.h
+++ src/wasm/baseline/loong64/liftoff-assembler-loong64-inl.h
@@ -2608,10 +2608,11 @@ void LiftoffAssembler::emit_i32x4_dot_i8x16_i7x16_add_s(LiftoffRegister dst,
                                                         LiftoffRegister lhs,
                                                         LiftoffRegister rhs,
                                                         LiftoffRegister acc) {
-  DCHECK_NE(dst, acc);
-  Dotp_h(dst.fp().toV(), lhs.fp().toV(), rhs.fp().toV());
-  vhaddw_w_h(dst.fp().toV(), dst.fp().toV(), dst.fp().toV());
-  vadd_w(dst.fp().toV(), dst.fp().toV(), acc.fp().toV());
+  UseScratchRegisterScope temps(this);
+  VRegister scratch = temps.AcquireFp().toV();
+  Dotp_h(scratch, lhs.fp().toV(), rhs.fp().toV());
+  vhaddw_w_h(scratch, scratch, scratch);
+  vadd_w(dst.fp().toV(), scratch, acc.fp().toV());
 }
 
 void LiftoffAssembler::emit_i8x16_eq(LiftoffRegister dst, LiftoffRegister lhs,
@@ -3398,8 +3399,11 @@ void LiftoffAssembler::emit_f32x4_pmin(LiftoffRegister dst, LiftoffRegister lhs,
   VRegister dstReg = dst.fp().toV();
   VRegister lhsReg = lhs.fp().toV();
   VRegister rhsReg = rhs.fp().toV();
-  vfcmp_cond_s(CLT, dstReg, rhsReg, lhsReg);
-  vbitsel_v(dstReg, lhsReg, rhsReg, dstReg);
+  UseScratchRegisterScope temps(this);
+  VRegister scratchReg = temps.AcquireFp().toV();
+
+  vfcmp_cond_s(CLT, scratchReg, rhsReg, lhsReg);
+  vbitsel_v(dstReg, lhsReg, rhsReg, scratchReg);
 }
 
 void LiftoffAssembler::emit_f32x4_pmax(LiftoffRegister dst, LiftoffRegister lhs,
@@ -3407,8 +3411,11 @@ void LiftoffAssembler::emit_f32x4_pmax(LiftoffRegister dst, LiftoffRegister lhs,
   VRegister dstReg = dst.fp().toV();
   VRegister lhsReg = lhs.fp().toV();
   VRegister rhsReg = rhs.fp().toV();
-  vfcmp_cond_s(CLT, dstReg, lhsReg, rhsReg);
-  vbitsel_v(dstReg, lhsReg, rhsReg, dstReg);
+  UseScratchRegisterScope temps(this);
+  VRegister scratchReg = temps.AcquireFp().toV();
+
+  vfcmp_cond_s(CLT, scratchReg, lhsReg, rhsReg);
+  vbitsel_v(dstReg, lhsReg, rhsReg, scratchReg);
 }
 
 void LiftoffAssembler::emit_f64x2_abs(LiftoffRegister dst,
@@ -3519,8 +3526,11 @@ void LiftoffAssembler::emit_f64x2_pmin(LiftoffRegister dst, LiftoffRegister lhs,
   VRegister dstReg = dst.fp().toV();
   VRegister lhsReg = lhs.fp().toV();
   VRegister rhsReg = rhs.fp().toV();
-  vfcmp_cond_d(CLT, kSimd128ScratchReg, rhsReg, lhsReg);
-  vbitsel_v(dstReg, lhsReg, rhsReg, kSimd128ScratchReg);
+  UseScratchRegisterScope temps(this);
+  VRegister scratchReg = temps.AcquireFp().toV();
+
+  vfcmp_cond_d(CLT, scratchReg, rhsReg, lhsReg);
+  vbitsel_v(dstReg, lhsReg, rhsReg, scratchReg);
 }
 
 void LiftoffAssembler::emit_f64x2_pmax(LiftoffRegister dst, LiftoffRegister lhs,
@@ -3528,8 +3538,11 @@ void LiftoffAssembler::emit_f64x2_pmax(LiftoffRegister dst, LiftoffRegister lhs,
   VRegister dstReg = dst.fp().toV();
   VRegister lhsReg = lhs.fp().toV();
   VRegister rhsReg = rhs.fp().toV();
-  vfcmp_cond_d(CLT, kSimd128ScratchReg, lhsReg, rhsReg);
-  vbitsel_v(dstReg, lhsReg, rhsReg, kSimd128ScratchReg);
+  UseScratchRegisterScope temps(this);
+  VRegister scratchReg = temps.AcquireFp().toV();
+
+  vfcmp_cond_d(CLT, scratchReg, lhsReg, rhsReg);
+  vbitsel_v(dstReg, lhsReg, rhsReg, scratchReg);
 }
 
 void LiftoffAssembler::emit_f64x2_relaxed_min(LiftoffRegister dst,
