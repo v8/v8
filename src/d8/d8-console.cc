dc66874f095100f7867fb8bb7b5da0c8da4b3192
Andreas Haas <ahaas@chromium.org>
2026-03-06 02:35:50 -0800

    [test] Run log-wasm-to-js-wrapper-callref.js repeatedly
    The test may flake in isolate tests otherwise because for wasm lazy
    compilation, code publishing and code logging is not happening
    atomically.
    
    Bug: 489618921
    Change-Id: Id225d739b534cc81809ad1a43b278779d88887a0
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7634200
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#105639}

--- src/d8/d8-console.cc
+++ src/d8/d8-console.cc
@@ -130,6 +130,13 @@ void D8Console::ProfileEnd(const debug::ConsoleCallArguments& args,
     profile->Serialize(&out);
   }
   profile->Delete();
+  // Currently the profiler does not work correctly if it is started and stopped
+  // multiple times. One problem is that logged code gets cleared in
+  // `StopProfiling`, but builtins only get logged in the constructor and are
+  // therefore only available in the first profiling session.
+  // TODO(ahaas): Either fix the profiler to support multiple sessions, or
+  // introduce a CHECK that the profiler is only used once.
+  DisposeProfiler();
 }
 
 void D8Console::Time(const debug::ConsoleCallArguments& args,
