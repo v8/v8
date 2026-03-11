90110e5a4f09e7fd792f02319cc544449e1e02b2
Raphael Herouart <rherouart@chromium.org>
2026-03-11 14:40:04 +0100

    Add `--proto_assign_seq_lazy_func_opt` and `--private_field_bytecodes` to V8 fuzzing flags.
    Include `--private_field_bytecodes` with a 0.5 probability in both
    foozzie and clusterfuzz trials configurations.
    
    Include `--proto_assign_seq_lazy_func_opt` with a 0.5 probability in
    both foozzie and clusterfuzz trials configurations. This flag does imply
    the flag it replaces. Seq count is fixed to 1 to stress the pattern.
    
    Bug: 449885256
    Bug: 474269456
    Change-Id: I35d228d196aefd6974fce71c7820253ef25ba3b1
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7653846
    Commit-Queue: Raphael Herouart <rherouart@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#105735}

--- tools/testrunner/testproc/fuzzer.py
+++ tools/testrunner/testproc/fuzzer.py
@@ -82,8 +82,8 @@ EXTRA_FLAGS = [
     (0.25, '--experimental-wasm-revectorize'),
     (0.1, '--no-memory-pool'),
     (0.1, '--wasm-assert-types'),
-    (0.5, '--proto-assign-seq-opt'),
-    (0.1, '--proto-assign-seq-opt --proto-assign-seq-opt-count=1'),
+    (0.5, '--private-field-bytecodes'),
+    (0.5, '--proto-assign-seq-lazy-func-opt --proto-assign-seq-opt-count=1'),
 ]
 
 MIN_DEOPT = 1
