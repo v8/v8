eecad683edabdac9a3040b2ec35f070ccaafbba6
Victor Gomes <victorgomes@chromium.org>
2026-03-18 09:23:46 +0100

    [turbolev] Enable phi untagging for turbolev future
    Change-Id: I7e4c6edae3bb8f5486a3ac6fc7e8877e698f7a63
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7678250
    Commit-Queue: Victor Gomes <victorgomes@chromium.org>
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Commit-Queue: Darius Mercadier <dmercadier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#105862}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -750,6 +750,8 @@ DEFINE_BOOL(maglev_untagged_phis, false,
             "enable phi untagging in the maglev optimizing compiler")
 DEFINE_BOOL(turbolev_untagged_phis, false,
             "enable phi untagging in the Turbolev optimizing compiler")
+DEFINE_WEAK_IMPLICATION(turbolev_future, turbolev_untagged_phis)
+
 DEFINE_BOOL(maglev_hoist_osr_value_phi_untagging, true,
             "enable phi untagging to hoist untagging of osr values")
 DEFINE_EXPERIMENTAL_FEATURE(
