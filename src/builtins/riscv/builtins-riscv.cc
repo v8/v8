6a9a6f6201040da880085c828f31d1a857a63258
LuYahan <yahan@iscas.ac.cn>
2026-03-20 15:26:00 +0800

    [riscv][wasmfx] Trap on null for resume_throw_ref
    Port commit 3a3a1240a97135d528d96d0017ac9f91a272e2b9
    Fixed: 493430818
    
    Change-Id: I58b95f3e7e0aca1f81ce4209480d7b57cfc48398
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7685620
    Auto-Submit: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
    Reviewed-by: Ji Qiu <qiuji@iscas.ac.cn>
    Commit-Queue: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#105951}

--- src/builtins/riscv/builtins-riscv.cc
+++ src/builtins/riscv/builtins-riscv.cc
@@ -4161,8 +4161,7 @@ void Builtins::Generate_WasmFXResumeThrowRef(MacroAssembler* masm) {
   // If the stack has not been started yet, switching to it is invalid as it
   // does not have a stack entry frame. Instead, retire it and throw the
   // exception from the current stack.
-  // Both blocks exit with the arguments of the runtime call pushed on the
-  // stack.
+  // Both blocks exit with the exnref pushed on the stack.
   Register scratch = t0;
   DCHECK(!AreAliased(scratch, exnref, target_stack));
   __ LoadWord(scratch, MemOperand(target_stack, wasm::kStackFpOffset));
@@ -4187,9 +4186,11 @@ void Builtins::Generate_WasmFXResumeThrowRef(MacroAssembler* masm) {
     __ CallCFunction(ExternalReference::wasm_retire_stack(), 2);
   }
   __ bind(&throw_);
-  // Throw the exception.
-  __ Move(kContextRegister, Smi::zero());
-  __ CallRuntime(Runtime::kWasmReThrow);
+  // Throw the exnref. The builtin expects to be called from a wasm frame, so
+  // leave this frame first and tail call WasmThrowRef.
+  __ Pop(WasmThrowRefDescriptor::GetRegisterParameter(0));
+  __ LeaveFrame(StackFrame::WASM_STACK_EXIT);
+  __ TailCallBuiltin(Builtin::kWasmThrowRef);
   __ Trap();
   __ bind(&return_);
   // Return the arg buffer.
