616f56791d516b45585cc5d47b4a0c8754ea52be
Milad Fa <mfarazma@ibm.com>
2026-03-20 13:02:36 +0000

    Merged: PPC/s390: [sandbox] Check dispatch entry in code finalization
    Port b5db50f99bc02b1713a28c1718c4bd666264c9e7
    Port 0194241de6c097c8f77a7421ecfa54efd98d4ad8
    
    R=junyan1@ibm.com
    BUG=
    LOG=N
    
    (cherry picked from commit 141546b3d1725b72be68b0188f7cf0caa9fb24c6)
    
    Change-Id: I3ff44a5537e5b875baaf137e07e5c8106564c04f
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7697333
    Reviewed-by: John <junyan1@ibm.com>
    Auto-Submit: Milad Farazmand <mfarazma@ibm.com>
    Commit-Queue: John <junyan1@ibm.com>
    Cr-Commit-Position: refs/branch-heads/14.7@{#16}
    Cr-Branched-From: 723547b98d2e75cb85556ab85479688c9fbe2f1e-refs/heads/14.7.173@{#1}
    Cr-Branched-From: 3fc49d4c4cd9e6202fe21f5925899292ffadb20a-refs/heads/main@{#105661}

--- src/compiler/backend/ppc/code-generator-ppc.cc
+++ src/compiler/backend/ppc/code-generator-ppc.cc
@@ -982,6 +982,7 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
             size_t expected = isolate()->js_dispatch_table().GetParameterCount(
                 dispatch_handle);
             if (num_arguments >= expected) {
+              __ RecordJSDispatchHandle(dispatch_handle, expected);
               __ CallJSDispatchEntry(dispatch_handle, expected);
             } else {
               __ AssertUnreachable(AbortReason::kJSSignatureMismatch);
