7cf6b1da3c19e03806d9af7b96ae49a5119b8910
Matthias Liedtke <mliedtke@chromium.org>
2026-04-08 03:16:24 -0700

    Merge: Revert "[heap] Enable up new_old_generation_heap_size and update Android 64 bit" (Version 14.9.20.1)
    (Cherry-pick from commit a37c8f57cce6960bfccded391b3e0f2c03dea9df)
    
    Change-Id: I277df5cfce8dc5372c0f44ada6786e1caa150b56
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7735731
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Matthias Liedtke <mliedtke@chromium.org>
    Cr-Commit-Position: refs/heads/14.9.20@{#2}
    Cr-Branched-From: 6899a7f2d7a46b919151df31c7f1ee8048d0b5c4-refs/heads/main@{#106298}

--- test/unittests/heap/heap-controller-unittest.cc
+++ test/unittests/heap/heap-controller-unittest.cc
@@ -59,9 +59,8 @@ TEST_F(MemoryControllerTest, MaxHeapGrowingFactor) {
 
   CheckEqualRounded(
       1.3, V8Controller::MaxGrowingFactor(physical_memory, min_heap_size));
-  CheckEqualRounded(
-      i::Heap::HeapLimitMultiplier(physical_memory) > 1 ? 1.6 : 1.533,
-      V8Controller::MaxGrowingFactor(physical_memory, max_heap_size / 2));
+  CheckEqualRounded(1.600, V8Controller::MaxGrowingFactor(physical_memory,
+                                                          max_heap_size / 2));
   CheckEqualRounded(
       2.0, V8Controller::MaxGrowingFactor(physical_memory, max_heap_size - 1));
   CheckEqualRounded(
