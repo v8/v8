541e27ebb34d4f597896df30a23d702bd5799fd8
Victor Gomes <victorgomes@chromium.org>
2026-04-08 10:02:28 +0200

    [context] Add missing DCHECK for context cell flag
    Fixed: 498904291
    Change-Id: Iadedf56292f16f10e5745bcdf51b8256acaf0f87
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7736187
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#106308}

--- src/compiler/compilation-dependencies.cc
+++ src/compiler/compilation-dependencies.cc
@@ -1311,7 +1311,7 @@ bool CompilationDependencies::DependOnContextCell(ContextRef script_context,
 
 bool CompilationDependencies::DependOnContextCell(ContextCellRef slot,
                                                   ContextCell::State state) {
-  DCHECK(v8_flags.script_context_cells);
+  DCHECK(v8_flags.script_context_cells || v8_flags.function_context_cells);
   RecordDependency(zone_->New<ContextCellDependency>(slot, state));
   return true;
 }
