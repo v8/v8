f1917d3b041b114c9a613e98208e9d33e69e7bf3
Arash Kazemi <arashk@chromium.org>
2026-04-14 11:14:22 +0000

    [sandbox] Mask TypedArray stores to be within bounded size on Android
    This CL adds masking to all TypedArray stores to prevent out-of-sandbox
    writes caused by double fetching of the elements kind, aka ElementsKind
    switcheroo. This bypass is currently prevented by having additional
    guard regions, except for Android where address space is limited.
    Masking all stores mitigates this issue on Android without requiring
    large address reservations.
    
    Fixed: 475479180, 499717570, 500413224
    Change-Id: I55e6faea2351c854707fb1c01454723ea323d419
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7705535
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Arash Kazemi <arashk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#106485}

--- src/codegen/code-stub-assembler.cc
+++ src/codegen/code-stub-assembler.cc
@@ -13711,6 +13711,26 @@ MachineRepresentation ElementsKindToMachineRepresentation(ElementsKind kind) {
   }
 }
 
+TNode<IntPtrT> ComputeTypedArrayStoreOffset(CodeStubAssembler* csa,
+                                            TNode<IntPtrT> offset,
+                                            ElementsKind kind) {
+#if V8_ENABLE_SANDBOX
+  if constexpr (kRequiresTypedArrayAccessMasks) {
+    // If the ElementsKind is concurrently modified between the bound check and
+    // the store, we might write outside of the sandbox. To prevent this
+    // ElementsKind switcheroo, mask the offset such that it is at most
+    // kMaxSafeBufferSizeForSandbox.
+    // For 1-byte elements, the offset is identical to the index and cannot
+    // exceed the checked bounds to cause an out-of-bounds write relative to the
+    // backing store, so we can skip masking.
+    if (ElementsKindToByteSize(kind) != 1) {
+      return csa->WordAnd(offset, csa->IntPtrConstant(kBoundedSizeMask));
+    }
+  }
+#endif  // V8_ENABLE_SANDBOX
+  return offset;
+}
+
 }  // namespace
 
 // TODO(solanes): Since we can't use `if constexpr` until we enable C++17 we
@@ -13725,7 +13745,8 @@ void CodeStubAssembler::StoreElementTypedArrayBigInt(TNode<RawPtrT> elements,
       std::is_same_v<TIndex, UintPtrT> || std::is_same_v<TIndex, IntPtrT>,
       "Only UintPtrT or IntPtrT indices is allowed");
   DCHECK(kind == BIGINT64_ELEMENTS || kind == BIGUINT64_ELEMENTS);
-  TNode<IntPtrT> offset = ElementOffsetFromIndex(index, kind, 0);
+  TNode<IntPtrT> offset = ComputeTypedArrayStoreOffset(
+      this, ElementOffsetFromIndex(index, kind, 0), kind);
   TVARIABLE(UintPtrT, var_low);
   // Only used on 32-bit platforms.
   TVARIABLE(UintPtrT, var_high);
@@ -13779,7 +13800,8 @@ void CodeStubAssembler::StoreElementTypedArrayWord32(TNode<RawPtrT> elements,
   if (kind == UINT8_CLAMPED_ELEMENTS) {
     CSA_DCHECK(this, Word32Equal(value, Word32And(Int32Constant(0xFF), value)));
   }
-  TNode<IntPtrT> offset = ElementOffsetFromIndex(index, kind, 0);
+  TNode<IntPtrT> offset = ComputeTypedArrayStoreOffset(
+      this, ElementOffsetFromIndex(index, kind, 0), kind);
   // TODO(cbruni): Add OOB check once typed.
   MachineRepresentation rep = ElementsKindToMachineRepresentation(kind);
   StoreNoWriteBarrier(rep, elements, offset, value);
@@ -13821,7 +13843,8 @@ void CodeStubAssembler::StoreElementTypedArray(TNode<TArray> elements,
       "Only Int32T, Float32T, Float64T or object value "
       "types are allowed");
   DCHECK(IsTypedArrayElementsKind(kind));
-  TNode<IntPtrT> offset = ElementOffsetFromIndex(index, kind, 0);
+  TNode<IntPtrT> offset = ComputeTypedArrayStoreOffset(
+      this, ElementOffsetFromIndex(index, kind, 0), kind);
   // TODO(cbruni): Add OOB check once typed.
   MachineRepresentation rep = ElementsKindToMachineRepresentation(kind);
   StoreNoWriteBarrier(rep, elements, offset, value);
