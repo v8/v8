fcbf9087ca7fa6d867924067d07cb5615bbb31a7
Thibaud Michaud <thibaudm@chromium.org>
2026-04-15 18:26:05 +0200

    [wasm][jspi][wasmfx] Switch to the central stack for AbortCSADcheck
    We currently don't switch to the central stack for AbortCSADcheck:
    - Switches to the central stack for wasm->non-wasm builtin calls
      are added during graph building, in `CodeAssembler::CallBuiltin`. But
      the AbortCSADcheck operator is lowered much later, during codegen.
    - The AbortCSADcheck builtin is not a wasm builtin, so it selects the
      non-switching variant of the CEntry for the runtime call.
    
    The runtime function does not throw and does not allocate so it does not
    need to be a runtime call in principle. But we do need to switch to the
    central stack regardless because `PushStackTraceAndDie` requires a large
    amount of stack space, which might not be available in a side stack.
    
    R=mliedtke@chromium.org
    
    Bug: 498968665
    Change-Id: Id06ba35f1d30cd3b19a9ae2d3d7813d75806c579
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7760041
    Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#106518}

--- src/compiler/code-assembler.cc
+++ src/compiler/code-assembler.cc
@@ -655,6 +655,13 @@ void CodeAssembler::ReturnIf(TNode<BoolT> condition, TNode<Object> value) {
 }
 
 void CodeAssembler::AbortCSADcheck(Node* message) {
+#if V8_ENABLE_WEBASSEMBLY
+  if (wasm::BuiltinLookup::IsWasmBuiltinId(builtin())) {
+    // We switch to the central stack for AbortCSADcheck because it requires a
+    // large amount of stack space to push the stack trace.
+    SwitchToTheCentralStackIfNeeded();
+  }
+#endif
   raw_assembler()->AbortCSADcheck(message);
 }
 
