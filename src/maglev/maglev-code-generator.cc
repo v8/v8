2603f8fbf6cd97a45807bc29b007b8f0a2d0df51
Leszek Swirski <leszeks@chromium.org>
2026-05-05 12:38:04 +0200

    [M148] [maglev] Fix deopt literals IdentityMap race during GC
    Original change's description:
    > [maglev] Fix deopt literals IdentityMap race during GC
    >
    > Maglev used an IdentityMap to store deoptimization literals, assigning
    > IDs based on the map's size. During concurrent GC, ThinString
    > shortcutting could merge previously distinct string pointers, causing
    > the map to rehash and shrink. This led to corrupted IDs and subsequent
    > out-of-bounds writes in DeoptimizationLiteralArray.
    >
    > This CL implements an optimized hybrid approach:
    > 1. Pair the IdentityMap cache with ZoneVectors to preserve original
    >    insertion order.
    > 2. Populate the DeoptimizationLiteralArray from the vectors.
    > 3. Safely canonicalize vector handles using Maglev's persistent
    >    CanonicalHandlesMap.
    > 4. Optimize compiler::Ref overloads to bypass redundant lookups.
    >
    > We also improve V8's runtime testing infrastructure by fixing
    > OptimizeMaglevOnNextCall's registration in runtime.h to support
    > variable arguments, enabling native concurrent Maglev testing in JS.
    >
    > A robust randomized concurrent regression test is added to verify.
    >
    > TAG=agy
    > CONV=8a5b0da0-ce00-46ee-83f5-9a341372b6f7
    >
    > Fixed: 508811474
    > Change-Id: I69bffdbe6736552e273480c8cf23de25af112739
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7816278
    > Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    > Reviewed-by: Patrick Thier <pthier@chromium.org>
    > Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    > Commit-Queue: Patrick Thier <pthier@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#107059}
    
    (cherry picked from commit 00f6ecd8a7cca6911789a11b7a7b01aaf41f925b)
    
    Bug: 514925265,508811474
    Change-Id: I69bffdbe6736552e273480c8cf23de25af112739
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7864504
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/branch-heads/14.8@{#50}
    Cr-Branched-From: f9659283a5f8d42b3c09228cf5df606fcaf47a3d-refs/heads/14.8.178@{#1}
    Cr-Branched-From: 141232520dc4910401240c531db3af36910a0fd1-refs/heads/main@{#106240}

--- src/maglev/maglev-code-generator.cc
+++ src/maglev/maglev-code-generator.cc
@@ -1186,12 +1186,17 @@ class MaglevFrameTranslationBuilder {
       LocalIsolate* local_isolate, MaglevAssembler* masm,
       FrameTranslationBuilder* translation_array_builder,
       IdentityMap<int, base::DefaultAllocationPolicy>* protected_deopt_literals,
-      IdentityMap<int, base::DefaultAllocationPolicy>* deopt_literals)
+      IdentityMap<int, base::DefaultAllocationPolicy>* deopt_literals,
+      ZoneVector<IndirectHandle<TrustedObject>>*
+          protected_deopt_literals_vector,
+      ZoneVector<IndirectHandle<Object>>* deopt_literals_vector)
       : local_isolate_(local_isolate),
         masm_(masm),
         translation_array_builder_(translation_array_builder),
         protected_deopt_literals_(protected_deopt_literals),
         deopt_literals_(deopt_literals),
+        protected_deopt_literals_vector_(protected_deopt_literals_vector),
+        deopt_literals_vector_(deopt_literals_vector),
         object_ids_(10) {}
 
   void BuildEagerDeopt(EagerDeoptInfo* deopt_info) {
@@ -1684,7 +1689,14 @@ class MaglevFrameTranslationBuilder {
         protected_deopt_literals_->FindOrInsert(obj);
     if (!res.already_exists) {
       DCHECK_EQ(0, *res.entry);
-      *res.entry = protected_deopt_literals_->size() - 1;
+      IndirectHandle<TrustedObject> canonical =
+          masm_->code_gen_state()
+              ->compilation_info()
+              ->broker()
+              ->CanonicalPersistentHandle(obj);
+      protected_deopt_literals_vector_->push_back(canonical);
+      *res.entry =
+          static_cast<int>(protected_deopt_literals_vector_->size() - 1);
     }
     return *res.entry;
   }
@@ -1693,13 +1705,25 @@ class MaglevFrameTranslationBuilder {
     IdentityMapFindResult<int> res = deopt_literals_->FindOrInsert(obj);
     if (!res.already_exists) {
       DCHECK_EQ(0, *res.entry);
-      *res.entry = deopt_literals_->size() - 1;
+      IndirectHandle<Object> canonical = masm_->code_gen_state()
+                                             ->compilation_info()
+                                             ->broker()
+                                             ->CanonicalPersistentHandle(obj);
+      deopt_literals_vector_->push_back(canonical);
+      *res.entry = static_cast<int>(deopt_literals_vector_->size() - 1);
     }
     return *res.entry;
   }
 
   int GetDeoptLiteral(compiler::HeapObjectRef ref) {
-    return GetDeoptLiteral(*ref.object());
+    IdentityMapFindResult<int> res =
+        deopt_literals_->FindOrInsert(*ref.object());
+    if (!res.already_exists) {
+      DCHECK_EQ(0, *res.entry);
+      deopt_literals_vector_->push_back(ref.object());
+      *res.entry = static_cast<int>(deopt_literals_vector_->size() - 1);
+    }
+    return *res.entry;
   }
 
   LocalIsolate* local_isolate_;
@@ -1707,6 +1731,8 @@ class MaglevFrameTranslationBuilder {
   FrameTranslationBuilder* translation_array_builder_;
   IdentityMap<int, base::DefaultAllocationPolicy>* protected_deopt_literals_;
   IdentityMap<int, base::DefaultAllocationPolicy>* deopt_literals_;
+  ZoneVector<IndirectHandle<TrustedObject>>* protected_deopt_literals_vector_;
+  ZoneVector<IndirectHandle<Object>>* deopt_literals_vector_;
 
   static const int kNotDuplicated = -1;
   std::vector<intptr_t> object_ids_;
@@ -1729,6 +1755,8 @@ MaglevCodeGenerator::MaglevCodeGenerator(
       graph_(graph),
       protected_deopt_literals_(isolate->heap()->heap()),
       deopt_literals_(isolate->heap()->heap()),
+      protected_deopt_literals_vector_(compilation_info->zone()),
+      deopt_literals_vector_(compilation_info->zone()),
       retained_maps_(isolate->heap()),
       is_context_specialized_(
           compilation_info->specialize_to_function_context()),
@@ -1808,17 +1836,19 @@ bool MaglevCodeGenerator::EmitCode() {
 void MaglevCodeGenerator::RecordInlinedFunctions() {
   // The inlined functions should be the first literals.
   DCHECK_EQ(0u, deopt_literals_.size());
+  DCHECK_EQ(0u, deopt_literals_vector_.size());
   for (OptimizedCompilationInfo::InlinedFunctionHolder& inlined :
        graph_->inlined_functions()) {
     IdentityMapFindResult<int> res =
         deopt_literals_.FindOrInsert(inlined.shared_info);
     if (!res.already_exists) {
       DCHECK_EQ(0, *res.entry);
-      *res.entry = deopt_literals_.size() - 1;
+      deopt_literals_vector_.push_back(inlined.shared_info);
+      *res.entry = static_cast<int>(deopt_literals_vector_.size() - 1);
     }
     inlined.RegisterInlinedFunctionId(*res.entry);
   }
-  inlined_function_count_ = static_cast<int>(deopt_literals_.size());
+  inlined_function_count_ = static_cast<int>(deopt_literals_vector_.size());
 }
 
 void MaglevCodeGenerator::EmitDeferredCode() {
@@ -1843,7 +1873,8 @@ bool MaglevCodeGenerator::EmitDeopts() {
 
   MaglevFrameTranslationBuilder translation_builder(
       local_isolate_, &masm_, &frame_translation_builder_,
-      &protected_deopt_literals_, &deopt_literals_);
+      &protected_deopt_literals_, &deopt_literals_,
+      &protected_deopt_literals_vector_, &deopt_literals_vector_);
 
   // Deoptimization exits must be as small as possible, since their count grows
   // with function size. These labels are an optimization which extracts the
@@ -2062,10 +2093,10 @@ Handle<DeoptimizationData> MaglevCodeGenerator::GenerateDeoptimizationData(
       static_cast<uint32_t>(graph_->inlined_functions().size());
   DirectHandle<ProtectedDeoptimizationLiteralArray> protected_literals =
       local_isolate->factory()->NewProtectedFixedArray(
-          static_cast<uint32_t>(protected_deopt_literals_.size()));
+          static_cast<uint32_t>(protected_deopt_literals_vector_.size()));
   DirectHandle<DeoptimizationLiteralArray> literals =
       local_isolate->factory()->NewDeoptimizationLiteralArray(
-          static_cast<uint32_t>(deopt_literals_.size()));
+          static_cast<uint32_t>(deopt_literals_vector_.size()));
   DirectHandle<TrustedPodArray<InliningPosition>> inlining_positions =
       TrustedPodArray<InliningPosition>::New(local_isolate,
                                              inlined_functions_size);
@@ -2074,22 +2105,14 @@ Handle<DeoptimizationData> MaglevCodeGenerator::GenerateDeoptimizationData(
 
   Tagged<ProtectedDeoptimizationLiteralArray> raw_protected_literals =
       *protected_literals;
-  {
-    IdentityMap<int, base::DefaultAllocationPolicy>::IteratableScope iterate(
-        &protected_deopt_literals_);
-    for (auto it = iterate.begin(); it != iterate.end(); ++it) {
-      raw_protected_literals->set(*it.entry(),
-                                  TrustedCast<TrustedObject>(it.key()));
-    }
+  for (size_t i = 0; i < protected_deopt_literals_vector_.size(); ++i) {
+    raw_protected_literals->set(static_cast<int>(i),
+                                *protected_deopt_literals_vector_[i]);
   }
 
   Tagged<DeoptimizationLiteralArray> raw_literals = *literals;
-  {
-    IdentityMap<int, base::DefaultAllocationPolicy>::IteratableScope iterate(
-        &deopt_literals_);
-    for (auto it = iterate.begin(); it != iterate.end(); ++it) {
-      raw_literals->set(*it.entry(), it.key());
-    }
+  for (size_t i = 0; i < deopt_literals_vector_.size(); ++i) {
+    raw_literals->set(static_cast<int>(i), *deopt_literals_vector_[i]);
   }
 
   for (uint32_t i = 0; i < inlined_functions_size; i++) {
