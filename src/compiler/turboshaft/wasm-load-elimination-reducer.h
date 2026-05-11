55cc1df03832e742cba83a2491fc5a13031be0f3
Jakob Kummerow <jkummerow@chromium.org>
2026-05-11 16:46:45 +0200

    [wasm][turboshaft] Fix Phi handling in Load Elimination some more
    Multiple Phis can depend on each other, so we have to clear all their
    replacements up front.
    
    Fixed: 509268941
    Change-Id: I7899b28e89ed7b470bd869fb52f7443589305171
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7822799
    Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#107278}

--- src/compiler/turboshaft/wasm-load-elimination-reducer.h
+++ src/compiler/turboshaft/wasm-load-elimination-reducer.h
@@ -415,7 +415,8 @@ class WasmLoadEliminationAnalyzer {
         memory_(data, phase_zone, non_aliasing_objects_, replacements_, graph_),
         block_to_snapshot_mapping_(graph.block_count(), phase_zone),
         predecessor_alias_snapshots_(phase_zone),
-        predecessor_memory_snapshots_(phase_zone) {}
+        predecessor_memory_snapshots_(phase_zone),
+        phi_replacements_backups_(phase_zone, &graph_) {}
 
   void Run() {
     LoopFinder loop_finder(phase_zone_, &graph_, LoopFinder::Config{});
@@ -483,6 +484,7 @@ class WasmLoadEliminationAnalyzer {
   void ProcessAllocate(OpIndex op_idx, const AllocateOp& op);
   void ProcessCall(OpIndex op_idx, const CallOp& op);
   void ProcessPhi(OpIndex op_idx, const PhiOp& op);
+  OpIndex MaybeReplacePhi(const PhiOp& phi);
 
 #if V8_ENABLE_WEBASSEMBLY
   void ProcessAtomicRMW(OpIndex op_idx, const StructAtomicRMWOp& op);
@@ -529,6 +531,10 @@ class WasmLoadEliminationAnalyzer {
   // to process a block. We store them as members to avoid reallocation.
   ZoneVector<AliasSnapshot> predecessor_alias_snapshots_;
   ZoneVector<MemorySnapshot> predecessor_memory_snapshots_;
+
+  // When figuring out whether a Phi change should cause a loop revisit, we
+  // need to temporarily store all loop Phis' previous replacements.
+  SparseOpIndexSideTable<OpIndex> phi_replacements_backups_;
 };
 
 template <class Next>
@@ -974,12 +980,7 @@ void WasmLoadEliminationAnalyzer::ProcessAllocate(OpIndex op_idx,
   non_aliasing_objects_.Set(op_idx, true);
 }
 
-void WasmLoadEliminationAnalyzer::ProcessPhi(OpIndex op_idx, const PhiOp& phi) {
-  InvalidateAllNonAliasingInputs(phi);
-
-  // For robustness, unset the replacement by default.
-  replacements_[op_idx] = OpIndex::Invalid();
-
+OpIndex WasmLoadEliminationAnalyzer::MaybeReplacePhi(const PhiOp& phi) {
   base::Vector<const OpIndex> inputs = phi.inputs();
   // This copies some of the functionality of {RequiredOptimizationReducer}:
   // Phis whose inputs are all the same value can be replaced by that value.
@@ -998,8 +999,14 @@ void WasmLoadEliminationAnalyzer::ProcessPhi(OpIndex op_idx, const PhiOp& phi) {
     }
   }
   if (same_inputs) {
-    replacements_[op_idx] = first;
+    return first;
   }
+  return OpIndex::Invalid();
+}
+
+void WasmLoadEliminationAnalyzer::ProcessPhi(OpIndex op_idx, const PhiOp& phi) {
+  InvalidateAllNonAliasingInputs(phi);
+  replacements_[op_idx] = MaybeReplacePhi(phi);
 }
 
 void WasmLoadEliminationAnalyzer::FinishBlock(const Block* block) {
@@ -1105,6 +1112,29 @@ bool WasmLoadEliminationAnalyzer::BeginBlock(const Block* block) {
   };
   memory_.StartNewSnapshot(base::VectorOf(predecessor_memory_snapshots_),
                            merge_memory);
+
+  if constexpr (for_loop_revisit) {
+    phi_replacements_backups_.clear();
+    // Back up and clear all existing loop Phi replacements. Clearing is
+    // necessary because loop Phis can depend on each other, and are
+    // conceptually processed in parallel.
+    for (OpIndex op_idx : graph_.OperationIndices(*block)) {
+      if (graph_.Get(op_idx).Is<PhiOp>() && replacements_[op_idx].valid()) {
+        phi_replacements_backups_[op_idx] = replacements_[op_idx];
+        replacements_[op_idx] = OpIndex::Invalid();
+      }
+    }
+    // Check if any loop Phi replacement would change.
+    for (OpIndex op_idx : graph_.OperationIndices(*block)) {
+      if (const PhiOp* phi = graph_.Get(op_idx).TryCast<PhiOp>()) {
+        OpIndex new_replacement = MaybeReplacePhi(*phi);
+        if (new_replacement != phi_replacements_backups_[op_idx]) {
+          loop_needs_revisit = true;
+        }
+        replacements_[op_idx] = new_replacement;
+      }
+    }
+  }
 
   if (block->IsLoop()) return loop_needs_revisit;
   return false;
