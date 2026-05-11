84f4af52c655fd5648a4eb5a108eadc642614966
Andreas Haas <ahaas@chromium.org>
2026-05-11 03:58:51 -0700

    [fastapi] Store v8::CFunction pointer directly in FunctionTemplateInfo
    FunctionTemplateInfo objects store a list of C-function pointer and
    signature pairs used to generate fast API calls. Previously, a separate
    struct was allocated and wrapped in a Managed<> object to store these
    pairs.
    
    With this CL, the v8::CFunction object provided by the API is instead
    wrapped in a Foreign object and stored directly in the
    FunctionTemplateInfo. Replacing Managed<> objects with Foreign objects
    restores the ability to serialize FunctionTemplateInfo objects.
    
    This change relies on the assumption that the CFunction object passed to
    FunctionTemplate::New outlives the FunctionTemplate itself. In practice,
    all embedders—including Chrome—already ensure this by holding the
    CFunction object in a static variable. In Chrome, this is how it has
    always been done; see, for example:
    https://source.chromium.org/chromium/chromium/src/+/main:out/android-Debug/gen/third_party/blink/renderer/bindings/modules/v8/v8_webgl2_rendering_context.cc;l=19485;drc=398e74869153a12e825bc789c2134762cbe81c36
    
    Change-Id: Ib5eadb62b50edf7bd289a501f737750ed6e9e4fb
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7828135
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Arash Kazemi <arashk@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#107265}

--- include/v8-internal.h
+++ include/v8-internal.h
@@ -578,7 +578,6 @@ struct TagRange {
   V(WasmManagedDataTag)              \
   V(WasmNativeModuleTag)             \
   V(BackingStoreTag)                 \
-  V(CFunctionWithSignatureTag)       \
   V(IcuBreakIteratorTag)             \
   V(IcuListFormatterTag)             \
   V(IcuLocaleTag)                    \
@@ -605,6 +604,7 @@ struct TagRange {
   V(GenericForeignTag)                                    \
   V(ApiAccessCheckCallbackTag)                            \
   V(ApiAbortScriptExecutionCallbackTag)                   \
+  V(CFunctionTag)                                         \
   V(SyntheticModuleTag)                                   \
   V(MicrotaskCallbackTag)                                 \
   V(MicrotaskCallbackDataTag)                             \
