d84a06bc88a6dab19a886ed4407ae3a00e11eda5
Maksim Ivanov <emaxx@google.com>
2026-06-30 12:45:35 +0000

    [testing] Assert flags state in sync point natives
    Crash immediately if %BlockAt/%Resume/%WaitUntilBlocked is called in an
    unexpected environment (neither fuzzing nor allow_natives_syntax).
    
    This acts as an extra reassurance that these runtime functions aren't
    misused in production in any way.
    
    Bug: 514998642
    Change-Id: Ib76f51b4167015bc09e5d834e4a4988392f05482
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8022905
    Reviewed-by: Arash Kazemi <arashk@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@google.com>
    Cr-Commit-Position: refs/heads/main@{#108339}

--- src/runtime/runtime-test.cc
+++ src/runtime/runtime-test.cc
@@ -2874,6 +2874,8 @@ RUNTIME_FUNCTION(Runtime_AllocateHeapNumberWithValue) {
 // This is primarily used for deterministically testing race conditions.
 RUNTIME_FUNCTION(Runtime_BlockAt) {
   HandleScope scope(isolate);
+  CHECK_UNLESS_FUZZING(v8_flags.allow_natives_syntax ||
+                       v8_flags.allow_natives_for_differential_fuzzing);
   DCHECK_EQ(2, args.length());
   CHECK_UNLESS_FUZZING(IsString(args[0]));
   CHECK_UNLESS_FUZZING(IsSmi(args[1]));
@@ -2889,6 +2891,8 @@ RUNTIME_FUNCTION(Runtime_BlockAt) {
 // blocked by %BlockAt at the specified synchronization point.
 RUNTIME_FUNCTION(Runtime_Resume) {
   HandleScope scope(isolate);
+  CHECK_UNLESS_FUZZING(v8_flags.allow_natives_syntax ||
+                       v8_flags.allow_natives_for_differential_fuzzing);
   DCHECK_EQ(1, args.length());
   CHECK_UNLESS_FUZZING(IsString(args[0]));
   DirectHandle<String> phase_name = args.at<String>(0);
@@ -2906,6 +2910,8 @@ RUNTIME_FUNCTION(Runtime_Resume) {
 // provided in milliseconds.
 RUNTIME_FUNCTION(Runtime_WaitUntilBlocked) {
   HandleScope scope(isolate);
+  CHECK_UNLESS_FUZZING(v8_flags.allow_natives_syntax ||
+                       v8_flags.allow_natives_for_differential_fuzzing);
   DCHECK_EQ(2, args.length());
   CHECK_UNLESS_FUZZING(IsString(args[0]));
   CHECK_UNLESS_FUZZING(IsSmi(args[1]));
