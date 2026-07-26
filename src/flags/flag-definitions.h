9fba6fc25c0651b1efced42c601b92a85f919a26
Omer Katz <omerkatz@chromium.org>
2026-07-26 19:57:33 +0200

    [Flags] Prohibit --gc-fake-mmap with --disallow-unsafe-flags
    Bug: 536572129
    Change-Id: Iddbbca916b1cec01087c6f41352bf9339dfd92fd
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8147160
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#108883}

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -4425,6 +4425,7 @@ DEFINE_NEG_IMPLICATION(disallow_unsafe_flags, maglev_break_on_entry)
 #ifdef USE_SIMULATOR
 DEFINE_NOT_EXPLICITLY_SET_IMPLICATION(disallow_unsafe_flags, stop_sim_at)
 #endif
+DEFINE_NOT_EXPLICITLY_SET_IMPLICATION(disallow_unsafe_flags, gc_fake_mmap)
 
 // Runs a program as security POC. This mode is used to determine whether a bug
 // in a program is a security problem. V8 supports many different configurations
