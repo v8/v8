66fd5165afc59dee6cd4164c6068c437c7a90360
Zhijin Zeng <zhijin.zeng@spacemit.com>
2026-08-19 11:04:11 +0800

    [riscv] Fix integer masking and pointer branch comparisons
    1. Fix Maglev RISC-V integer masking to emit an AND operation instead of an ADD
    operation.
    
    2. Compare the original input register directly in `CompareIntPtrAndBranch`
    instead of using an uninitialized scratch register.
    
    Change-Id: If8f08eca4d3a5fca2d9fad739d47ae38530005fd
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8265241
    Reviewed-by: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
    Reviewed-by: Ji Qiu <qiuji@iscas.ac.cn>
    Commit-Queue: Ji Qiu <qiuji@iscas.ac.cn>
    Cr-Commit-Position: refs/heads/main@{#109445}

--- src/maglev/riscv/maglev-assembler-riscv-inl.h
+++ src/maglev/riscv/maglev-assembler-riscv-inl.h
@@ -779,7 +779,7 @@ inline void MaglevAssembler::AddInt32(Register dst, Register src, int amount) {
 }
 
 inline void MaglevAssembler::AndInt32(Register dst, Register src, int mask) {
-  Add32(dst, src, Operand(mask));
+  And(dst, src, Operand(mask));
 }
 
 inline void MaglevAssembler::AndInt32(Register reg, int mask) {
@@ -1692,8 +1692,6 @@ inline void MaglevAssembler::CompareIntPtrAndBranch(
       UNREACHABLE();  // not expected
   }
 
-  MaglevAssembler::TemporaryRegisterScope temps(this);
-  Register lhs = temps.AcquireScratch();
   if (fallthrough_when_false) {
     if (fallthrough_when_true) {
       // If both paths are a fallthrough, do nothing.
@@ -1701,10 +1699,10 @@ inline void MaglevAssembler::CompareIntPtrAndBranch(
       return;
     }
     // Jump over the false block if true, otherwise fall through into it.
-    MacroAssembler::Branch(if_true, cond, lhs, Operand(value), true_distance);
+    MacroAssembler::Branch(if_true, cond, r1, Operand(value), true_distance);
   } else {
     // Jump to the false block if true.
-    MacroAssembler::Branch(if_false, NegateCondition(cond), lhs, Operand(value),
+    MacroAssembler::Branch(if_false, NegateCondition(cond), r1, Operand(value),
                            false_distance);
     // Jump to the true block if it's not the next block.
     if (!fallthrough_when_true) {
