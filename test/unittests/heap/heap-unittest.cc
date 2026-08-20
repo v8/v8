6aed6351085ceedd29d1605be814889d003b65ff
Hannes Payer <hpayer@chromium.org>
2026-08-20 10:00:02 +0000

    [heap] Move LeakNativeContextViaMapProto test from cctest to unittests
    Moves TEST(LeakNativeContextViaMapProto) from
    test/cctest/heap/test-heap.cc to test/unittests/heap/heap-unittest.cc.
    
    This test verifies that optimizing prototype property access on an
    object with a prototype originating from a foreign context does not
    retain the foreign context's JSGlobalObject when that context is
    disposed.
    
    TAG=agy
    
    Bug: 548296797
    Change-Id: I6df53bcfbd846e76acd11620465c0b454cf4077d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8265809
    Commit-Queue: Hannes Payer <hpayer@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#109392}

--- test/unittests/heap/heap-unittest.cc
+++ test/unittests/heap/heap-unittest.cc
@@ -2308,6 +2308,79 @@ TEST_F(HeapTest, LeakNativeContextViaMap) {
   }
   EXPECT_EQ(1, NumberOfGlobalObjects());
 }
+
+TEST_F(HeapTest, LeakNativeContextViaMapProto) {
+  v8_flags.allow_natives_syntax = true;
+  v8::Isolate* isolate = v8_isolate();
+  Heap* heap_instance = heap();
+  v8::HandleScope outer_scope(isolate);
+  v8::Persistent<v8::Context> ctx1p;
+  v8::Persistent<v8::Context> ctx2p;
+  {
+    v8::HandleScope scope(isolate);
+    ctx1p.Reset(isolate, v8::Context::New(isolate));
+    ctx2p.Reset(isolate, v8::Context::New(isolate));
+    v8::Local<v8::Context>::New(isolate, ctx1p)->Enter();
+  }
+
+  {
+    // In this test, we need to invoke GC without stack, otherwise some objects
+    // may not be reclaimed because of conservative stack scanning.
+    DisableConservativeStackScanningScopeForTesting no_stack_scanning(
+        heap_instance);
+    InvokeMemoryReducingMajorGCs();
+  }
+  EXPECT_EQ(3, NumberOfGlobalObjects());
+
+  {
+    v8::HandleScope inner_scope(isolate);
+    v8::Local<v8::Context> ctx1 = v8::Local<v8::Context>::New(isolate, ctx1p);
+    v8::Local<v8::Context> ctx2 = v8::Local<v8::Context>::New(isolate, ctx2p);
+    RunJS(ctx1, "var v = { y: 42};");
+    v8::Local<v8::Value> v =
+        ctx1->Global()
+            ->Get(ctx1, v8::String::NewFromUtf8Literal(isolate, "v"))
+            .ToLocalChecked();
+    ctx2->Enter();
+    EXPECT_TRUE(ctx2->Global()
+                    ->Set(ctx2, v8::String::NewFromUtf8Literal(isolate, "o"), v)
+                    .FromJust());
+    Handle<Object> res = RunJS(ctx2,
+                               "function f() {"
+                               "  var p = {x: 42};"
+                               "  p.__proto__ = o;"
+                               "  return p.x;"
+                               "}"
+                               "%PrepareFunctionForOptimization(f);"
+                               "for (var i = 0; i < 10; ++i) f();"
+                               "%OptimizeFunctionOnNextCall(f);"
+                               "f();");
+    EXPECT_EQ(42, Object::NumberValue(*res));
+    EXPECT_TRUE(ctx2->Global()
+                    ->Set(ctx2, v8::String::NewFromUtf8Literal(isolate, "o"),
+                          v8::Int32::New(isolate, 0))
+                    .FromJust());
+    ctx2->Exit();
+    v8::Local<v8::Context>::New(isolate, ctx1)->Exit();
+    ctx1p.Reset();
+    isolate->ContextDisposedNotification(
+        v8::ContextDependants::kSomeDependants);
+  }
+  {
+    DisableConservativeStackScanningScopeForTesting no_stack_scanning(
+        heap_instance);
+    InvokeMemoryReducingMajorGCs();
+  }
+  EXPECT_EQ(2, NumberOfGlobalObjects());
+  ctx2p.Reset();
+  {
+    DisableConservativeStackScanningScopeForTesting no_stack_scanning(
+        heap_instance);
+    InvokeMemoryReducingMajorGCs();
+  }
+  EXPECT_EQ(1, NumberOfGlobalObjects());
+}
+
 TEST_F(HeapTest, OptimizedPretenuringNestedDoubleLiterals) {
   v8_flags.allow_natives_syntax = true;
   v8_flags.expose_gc = true;
