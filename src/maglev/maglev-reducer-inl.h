10f98d3082b2652d7ceebbe787231f69ccc249be
Olivier Flückiger <olivf@chromium.org>
2026-08-20 13:04:36 +0000

    [M153] [maglev] Use GetSmiValue for from_index in TryWithArrayIterationArgs
    Original change's description:
    > [maglev] Use GetSmiValue for from_index in TryWithArrayIterationArgs
    >
    > In TryWithArrayIterationArgs, from_index was previously checked using
    > BuildCheckSmi(from_index). However, from_index is an Int32 node, so
    > BuildCheckSmi only annotates its known type and emits CheckInt32IsSmi
    > without converting it to a tagged Smi representation. When subsequently
    > passed to Reducer, GetTaggedValue or type assertions (such as
    > CheckMaglevType) could encounter type representation inconsistencies.
    >
    > Using GetSmiValue(from_index) properly performs the conversion to a
    > tagged Smi with the appropriate check (CheckedSmiTagInt32) and returns
    > a tagged Smi value expected by the array builtins.
    >
    > Fixed: 547982135
    > TAG=agy
    > CONV=1f7d7708-36cf-4bd7-82b2-81eff89e6c4d
    >
    > Change-Id: I1e3a2beafa4c034c48ccde4f31fdc1f6b184b719
    > Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8262810
    > Auto-Submit: Olivier Flückiger <olivf@chromium.org>
    > Reviewed-by: Patrick Thier <pthier@chromium.org>
    > Commit-Queue: Olivier Flückiger <olivf@chromium.org>
    > Commit-Queue: Patrick Thier <pthier@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#109399}
    
    (cherry picked from commit 27b8e0e26a712c8fa24754e07d523695aea5929a)
    
    Bug: 550174027,547982135
    Change-Id: I1e3a2beafa4c034c48ccde4f31fdc1f6b184b719
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8303732
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/15.3@{#9}
    Cr-Branched-From: bea59ed367fb0c56e18a8716e5824338592b02ed-refs/heads/15.3.76@{#1}
    Cr-Branched-From: 445f45796516823984c123282865478b0c4fc2f4-refs/heads/main@{#109278}

--- src/maglev/maglev-reducer-inl.h
+++ src/maglev/maglev-reducer-inl.h
@@ -1113,7 +1113,7 @@ MaybeReduceResult MaglevReducer<BaseT>::TryWithArrayIterationArgs(
                   [&]() -> ReduceResult { return from_index; }));
         }
 
-        GET_VALUE_OR_ABORT(from_index, BuildCheckSmi(from_index));
+        GET_VALUE_OR_ABORT(from_index, GetSmiValue(from_index));
 
         return Reducer(elements_kind, elements, search_element, length,
                        from_index);
