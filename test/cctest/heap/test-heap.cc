8202e2b5e62a08fbfd8dbd7e8060a1c105a99965
Hannes Payer <hpayer@chromium.org>
2026-08-20 06:41:46 +0000

    [heap] Move OptimizedPretenuringDoubleArrayLiterals test to unittests
    Moves TEST(OptimizedPretenuringDoubleArrayLiterals) from
    test/cctest/heap/test-heap.cc to test/unittests/heap/heap-unittest.cc.
    
    This test verifies that double array literals in optimized functions
    are correctly pretenured into OldSpace when pretenured allocation sites
    are triggered.
    
    TAG=agy
    
    Bug: 548296797
    Change-Id: I581840595cca362468cf4a73c901b160c970ecf3
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8263637
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Hannes Payer <hpayer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#109383}

--- test/cctest/heap/test-heap.cc
+++ test/cctest/heap/test-heap.cc
@@ -868,45 +868,6 @@ TEST(LeakNativeContextViaMapProto) {
 
 
 
-TEST(OptimizedPretenuringDoubleArrayLiterals) {
-  v8_flags.allow_natives_syntax = true;
-  v8_flags.expose_gc = true;
-  CcTest::InitializeVM();
-  if (!CcTest::i_isolate()->use_optimizer()) return;
-  if (v8_flags.gc_global || v8_flags.stress_compaction ||
-      v8_flags.stress_incremental_marking || v8_flags.single_generation ||
-      v8_flags.stress_concurrent_allocation || v8_flags.scavenger_chaos_mode) {
-    return;
-  }
-  v8::HandleScope scope(CcTest::isolate());
-  ManualGCScope manual_gc_scope;
-  GrowNewSpaceToMaximumCapacity(CcTest::heap());
-
-  auto source = base::OwnedVector<char>::NewForOverwrite(1024);
-  base::SNPrintF(source.as_vector(),
-                 "var number_elements = %d;"
-                 "var elements = new Array(number_elements);"
-                 "function f() {"
-                 "  for (var i = 0; i < number_elements; i++) {"
-                 "    elements[i] = [1.1, 2.2, 3.3];"
-                 "  }"
-                 "  return elements[number_elements - 1];"
-                 "};"
-                 "%%PrepareFunctionForOptimization(f);"
-                 "f(); gc({type: 'minor'});"
-                 "f(); f();"
-                 "%%OptimizeFunctionOnNextCall(f);"
-                 "f();",
-                 kPretenureCreationCount);
-
-  v8::Local<v8::Value> res = CompileRun(source.begin());
-
-  i::DirectHandle<JSObject> o = Cast<JSObject>(
-      v8::Utils::OpenDirectHandle(*v8::Local<v8::Object>::Cast(res)));
-
-  CHECK(CcTest::heap()->InOldSpace(o->elements()));
-  CHECK(CcTest::heap()->InOldSpace(*o));
-}
 
 TEST(OptimizedPretenuringNestedMixedArrayLiterals) {
   v8_flags.allow_natives_syntax = true;
