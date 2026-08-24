a2141aa940c63a7f53554cce222edbd39ea3aa3f
Fanchen Kong <fanchen.kong@intel.com>
2026-08-24 15:21:41 +0800

    [disasm][x64] Add missing mod==0 disasm for opcode F6F7
    Use PrintRightOperand for all F6/F7 memory operands and add imulq
    disassembly tests for mod=0 addressing modes.
    
    Bug: 550484947
    Change-Id: I6807779d453795ce8b571065eaf647541f4bb019
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8284123
    Reviewed-by: Patrick Thier <pthier@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Kong, Fanchen <fanchen.kong@intel.com>
    Cr-Commit-Position: refs/heads/main@{#109710}

--- test/unittests/assembler/disasm-x64-unittest.cc
+++ test/unittests/assembler/disasm-x64-unittest.cc
@@ -646,6 +646,16 @@ TEST_F(DisasmX64Test, DisasmX64CheckOutput) {
 
   COMPARE("48f7ac8b10270000     REX.W imulq [rbx+rcx*4+0x2710]",
           imulq(Operand(rbx, rcx, times_4, 10000)));
+  Label imul_rip;
+  t.assm_.bind(&imul_rip);
+  COMPARE("48f72df9ffffff       REX.W imulq [rip+0xfffffff9]",
+          imulq(Operand(&imul_rip)));
+  COMPARE("48f729               REX.W imulq [rcx]", imulq(Operand(rcx, 0)));
+  COMPARE("48f72c24             REX.W imulq [rsp]", imulq(Operand(rsp, 0)));
+  COMPARE("48f72c39             REX.W imulq [rcx+rdi*1]",
+          imulq(Operand(rcx, rdi, times_1, 0)));
+  COMPARE("48f72c7d00000000     REX.W imulq [rdi*2+0x0]",
+          imulq(Operand(rdi, times_2, 0)));
   COMPARE("486bd10c             REX.W imulq rdx,rcx,0xc",
           imulq(rdx, rcx, Immediate(12)));
   COMPARE("4869d1e8030000       REX.W imulq rdx,rcx,0x3e8",
