97327f050e9483ee97627244f37f971cdce414a9
V8 Autoroll <v8-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com>
2026-08-24 04:42:02 -0700

    Version 15.1.1690206
    Version incremented at https://cr-buildbucket.appspot.com/build/8672589076859011441
    
    Change-Id: I64ce2f3f766a53ebcaf721a28b64022a4929dd48
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8280437
    Bot-Commit: v8-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com <v8-ci-autoroll-builder@chops-service-accounts.iam.gserviceaccount.com>

--- include/v8-version.h
+++ include/v8-version.h
@@ -10,8 +10,8 @@
 // system so their names cannot be changed without changing the scripts.
 #define V8_MAJOR_VERSION 15
 #define V8_MINOR_VERSION 1
-#define V8_BUILD_NUMBER 206
-#define V8_PATCH_LEVEL 21
+#define V8_BUILD_NUMBER 1690206
+#define V8_PATCH_LEVEL 0
 
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
