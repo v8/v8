bcefebe8498628369427a91063982fc0160da99f
Marja Hölttä <marja@chromium.org>
2026-09-08 08:31:47 +0200

    [maglev] Fix bailouts in VisitIntrinsicGeneratorYieldResult
    We have bailouts in the middle of manual subgraph building - this won't
    work.
    
    Bug 558240761 tracks fixing this code pattern in other places +
    hardening the code to make making this mistake more difficult.
    
    Bug: 558240761
    Change-Id: Ie8dc1d2141b43a76dc62edfa0117ca98df125bc7
    Fixed: 556962428
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8364189
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#109714}

--- src/maglev/maglev-graph-builder.cc
+++ src/maglev/maglev-graph-builder.cc
@@ -12307,38 +12307,41 @@ ReduceResult MaglevGraphBuilder::VisitIntrinsicGeneratorYieldResult(
   compiler::MapRef map =
       broker()->target_native_context().iterator_result_map(broker());
 
-  MaglevSubGraphBuilder sub_graph(this, 1);
-  MaglevSubGraphBuilder::Variable ret_val(0);
-  MaglevSubGraphBuilder::Label done_label(&sub_graph, 2, {&ret_val});
-  MaglevSubGraphBuilder::Label allocate_label(&sub_graph, 1);
-
   // Check if yielded_value_ is TheHole (signaling to skip allocating the result
   // object).
   ValueNode* current_yielded_value;
   GET_VALUE_OR_ABORT(current_yielded_value,
                      BuildLoadTaggedField(generator, offsetof(JSGeneratorObject,
                                                               yielded_value_)));
-  RETURN_IF_ABORT(sub_graph.GotoIfFalse<BranchIfRootConstant>(
-      &allocate_label, {current_yielded_value}, RootIndex::kTheHoleValue));
 
-  RETURN_IF_ABORT(BuildStoreTaggedField(
-      generator, value, offsetof(JSGeneratorObject, yielded_value_),
-      StoreTaggedMode::kDefault));
+  Subgraph<MaglevGraphBuilder> subgraph(&reducer_, 1);
+  Subgraph<MaglevGraphBuilder>::Variable ret_val(0);
 
-  sub_graph.set(ret_val, GetRootConstant(RootIndex::kTheHoleValue));
-  sub_graph.Goto(&done_label);
+  RETURN_IF_ABORT(subgraph.Branch(
+      {&ret_val},
+      [&](BranchBuilder& builder) {
+        return BuildBranchIfRootConstant(builder, current_yielded_value,
+                                         RootIndex::kTheHoleValue);
+      },
+      [&] {
+        RETURN_IF_ABORT(BuildStoreTaggedField(
+            generator, value, offsetof(JSGeneratorObject, yielded_value_),
+            StoreTaggedMode::kDefault));
+        subgraph.set(ret_val, GetRootConstant(RootIndex::kTheHoleValue));
+        return ReduceResult::Done();
+      },
+      [&] {
+        VirtualObject* iter_result = reducer_.CreateJSIteratorResult(
+            map, value, GetBooleanConstant(false));
+        ValueNode* alloc_result;
+        GET_VALUE_OR_ABORT(alloc_result,
+                           reducer_.BuildInlinedAllocation(
+                               iter_result, AllocationType::kYoung));
+        subgraph.set(ret_val, alloc_result);
+        return ReduceResult::Done();
+      }));
 
-  sub_graph.Bind(&allocate_label);
-  VirtualObject* iter_result =
-      reducer_.CreateJSIteratorResult(map, value, GetBooleanConstant(false));
-  ValueNode* alloc_result;
-  GET_VALUE_OR_ABORT(alloc_result, reducer_.BuildInlinedAllocation(
-                                       iter_result, AllocationType::kYoung));
-  sub_graph.set(ret_val, alloc_result);
-  sub_graph.Goto(&done_label);
-
-  sub_graph.Bind(&done_label);
-  SetAccumulator(sub_graph.get(ret_val));
+  SetAccumulator(subgraph.get(ret_val));
   return ReduceResult::Done();
 }
 
