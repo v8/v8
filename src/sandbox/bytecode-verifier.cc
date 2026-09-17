0373571aee788b2fdfdf1f2ca1aa7e3b5c982c5b
Arash Kazemi <arashk@chromium.org>
2026-09-17 06:46:09 +0000

    [sandbox] Add DebugPrintPtr to bytecode verifiery blocklist
    Fixed: 562551227
    Change-Id: I3f609c029f4791134bed9292116f440ba8884857
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8424180
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Arash Kazemi <arashk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#109892}

--- src/sandbox/bytecode-verifier.cc
+++ src/sandbox/bytecode-verifier.cc
@@ -294,6 +294,7 @@ bool BytecodeVerifier::IsAllowedRuntimeFunction(Runtime::FunctionId id) {
     case Runtime::kSwissTableUpdate:
     case Runtime::kSwissTableValueAt:
     case Runtime::kSwissTableKeyAt:
+    case Runtime::kDebugPrintPtr:
 #if V8_ENABLE_SPARKPLUG_PLUS
     case Runtime::kPatchCompareOpBaselineCode:
     case Runtime::kPatchCompareOpBaselineCodeAndThrow:
