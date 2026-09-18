v8/v8

commit ea39022c92f23a175c5a299c81f0ec54aff03c7e

[riscv] Avoid clobbering expect value in atomic compare-exchange
Atomic compare-exchange code generation modified the expected value register
when extracting a narrow value or sign-extending a Word32 value.

Reuse `TempRegister(2)` to hold expected narrow value in
`ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER_EXT`. `TempRegister(2)` is free after
the first `ExtractBits` execution completes.

Add a new parameter for `ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER` for passing
the normalized `expect_value`.

Add Wasm regression tests for compare-exchange. The tests discard the CAS result
and return the original expected local, verifying that code generation does not
clobber it.

Change-Id: I43c7cc5b2f286c0c20be5e980022a02fb89e854d
Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8319928
Reviewed-by: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
Commit-Queue: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
Cr-Commit-Position: refs/heads/main@{#109846}

src/compiler/backend/riscv/code-generator-riscv.cc
@@ -684,8 +684,8 @@ void RecordTrapInfoIfNeeded(Zone* zone, CodeGenerator* codegen,
     __ sync();                                                                 \
   } while (0)
 
-#define ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(load_linked,                  \
-                                                 store_conditional)            \
+#define ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(                              \
+    load_linked, store_conditional, expect_value)                              \
   do {                                                                         \
     Label compareExchange;                                                     \
     Label exit;                                                                \
@@ -694,9 +694,8 @@ void RecordTrapInfoIfNeeded(Zone* zone, CodeGenerator* codegen,
     __ bind(&compareExchange);                                                 \
     __ load_linked(i.OutputRegister(0), MemOperand(i.TempRegister(0), 0),      \
                    trapper);                                                   \
-    DCHECK_NE(i.InputRegister(2), i.OutputRegister(0));                        \
-    __ BranchShort(&exit, ne, i.InputRegister(2),                              \
-                   Operand(i.OutputRegister(0)));                              \
+    DCHECK_NE(expect_value, i.OutputRegister(0));                              \
+    __ BranchShort(&exit, ne, expect_value, Operand(i.OutputRegister(0)));     \
     __ Move(i.TempRegister(2), i.InputRegister(3));                            \
     __ store_conditional(i.TempRegister(2), MemOperand(i.TempRegister(0), 0)); \
     __ BranchShort(&compareExchange, ne, i.TempRegister(2),                    \
@@ -726,10 +725,10 @@ void RecordTrapInfoIfNeeded(Zone* zone, CodeGenerator* codegen,
                    trapper);                                                   \
     __ ExtractBits(i.OutputRegister(0), i.TempRegister(2), i.TempRegister(1),  \
                    size, sign_extend);                                         \
-    __ ExtractBits(i.InputRegister(2), i.InputRegister(2), 0, size,            \
+    __ ExtractBits(i.TempRegister(2), i.InputRegister(2), 0, size,             \
                    sign_extend);                                               \
-    DCHECK_NE(i.InputRegister(2), i.OutputRegister(0));                        \
-    __ BranchShort(&exit, ne, i.InputRegister(2),                              \
+    DCHECK_NE(i.TempRegister(2), i.OutputRegister(0));                         \
+    __ BranchShort(&exit, ne, i.TempRegister(2),                               \
                    Operand(i.OutputRegister(0)));                              \
     __ InsertBits(i.TempRegister(2), i.InputRegister(3), i.TempRegister(1),    \
                   size);                                                       \
@@ -2603,9 +2602,11 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
       switch (AtomicWidthField::decode(opcode)) {
         case AtomicWidth::kWord32:
 #if V8_TARGET_ARCH_RISCV64
-          __ SignExtendWord(i.InputRegister(2), i.InputRegister(2));
+          __ SignExtendWord(i.TempRegister(1), i.InputRegister(2));
+          ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(Ll, Sc, i.TempRegister(1));
+#else
+          ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(Ll, Sc, i.InputRegister(2));
 #endif
-          ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(Ll, Sc);
           break;
 #if V8_TARGET_ARCH_RISCV64
         case AtomicWidth::kWord64:
@@ -2675,7 +2676,7 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
       break;
     }
     case kRiscvWord64AtomicCompareExchangeUint64:
-      ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(Lld, Scd);
+      ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(Lld, Scd, i.InputRegister(2));
       break;
 #define ATOMIC_BINOP_CASE(op, inst32, inst64, amoinst32, amoinst64)           \
   case kAtomic##op##Int8:                                                     \
@@ -2733,7 +2734,7 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
 #undef ATOMIC_BINOP_CASE
 #elif V8_TARGET_ARCH_RISCV32
     case kAtomicCompareExchangeWithWriteBarrier: {
-      ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(Ll, Sc);
+      ASSEMBLE_ATOMIC_COMPARE_EXCHANGE_INTEGER(Ll, Sc, i.InputRegister(2));
       if (v8_flags.disable_write_barriers) break;
 
       // Emit the write barrier.

test/cctest/wasm/test-run-wasm-atomics64.cc
@@ -181,7 +181,7 @@ WASM_EXEC_TEST(I64AtomicCompareExchange16U) {
   }
 }
 
-WASM_EXEC_TEST(I32AtomicCompareExchange8U) {
+WASM_EXEC_TEST(I64AtomicCompareExchange8U) {
   WasmRunner<uint64_t, uint64_t, uint64_t> r(execution_tier);
   uint8_t* memory = r.builder().AddMemoryElems<uint8_t>(kWasmPageSize);
   r.builder().SetMemoryShared();
@@ -198,6 +198,34 @@ WASM_EXEC_TEST(I32AtomicCompareExchange8U) {
     }
   }
 }
+
+#define WASM_TEST_CAS_PRESERVES_EXPECTED(Name, CType, Representation)     \
+  WASM_EXEC_TEST(I64AtomicCompareExchange##Name##PreservesExpected) {     \
+    WasmRunner<uint64_t, uint64_t, uint64_t> r(execution_tier);           \
+    CType* memory =                                                       \
+        r.builder().AddMemoryElems<CType>(kWasmPageSize / sizeof(CType)); \
+    r.builder().SetMemoryShared();                                        \
+    r.Build({WASM_ATOMICS_TERNARY_OP(kExprI64AtomicCompareExchange##Name, \
+                                     WASM_I32V_1(0), WASM_LOCAL_GET(0),   \
+                                     WASM_LOCAL_GET(1), Representation),  \
+             WASM_DROP, WASM_LOCAL_GET(0)});                              \
+    FOR_UINT64_INPUTS(i) {                                                \
+      CType initial = static_cast<CType>(i);                              \
+      FOR_UINT64_INPUTS(j) {                                              \
+        CType replacement = static_cast<CType>(j);                        \
+        r.builder().WriteMemory(&memory[0], initial);                     \
+        CHECK_EQ(i, r.Call(i, j));                                        \
+        CType expected =                                                  \
+            CompareExchange(initial, static_cast<CType>(i), replacement); \
+        CHECK_EQ(expected, r.builder().ReadMemory(&memory[0]));           \
+      }                                                                   \
+    }                                                                     \
+  }
+
+WASM_TEST_CAS_PRESERVES_EXPECTED(8U, uint8_t, MachineRepresentation::kWord8)
+WASM_TEST_CAS_PRESERVES_EXPECTED(16U, uint16_t, MachineRepresentation::kWord16)
+WASM_TEST_CAS_PRESERVES_EXPECTED(32U, uint32_t, MachineRepresentation::kWord32)
+#undef WASM_TEST_CAS_PRESERVES_EXPECTED
 
 WASM_EXEC_TEST(I64AtomicLoad) {
   WasmRunner<uint64_t> r(execution_tier);

commit de0cae989ac0e09108b0675df09e16e34e078817

[riscv] Fix register clobbering for atomic compare-exchange
Fix the issue caused by commit ea39022c.
See it in https://chromium-review.googlesource.com/c/v8/v8/+/8319928.

`InsertBits` will use `i.TempRegister(2)` as source register operand, so it
can't be reused by `ExtractBits`.

Add an extra temporary register for atomic compare-exchange

Change-Id: I4e71c42a423209a78687cc5fe7b1549b3d1e52aa
Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8411976
Reviewed-by: Yahan Lu (LuYahan) <yahan@iscas.ac.cn>
Reviewed-by: Ji Qiu <qiuji@iscas.ac.cn>
Commit-Queue: Ji Qiu <qiuji@iscas.ac.cn>
Cr-Commit-Position: refs/heads/main@{#109935}

src/compiler/backend/riscv/code-generator-riscv.cc
@@ -708,10 +708,10 @@ void RecordTrapInfoIfNeeded(Zone* zone, CodeGenerator* codegen,
                    trapper);                                                   \
     __ ExtractBits(i.OutputRegister(0), i.TempRegister(2), i.TempRegister(1),  \
                    size, sign_extend);                                         \
-    __ ExtractBits(i.TempRegister(2), i.InputRegister(2), 0, size,             \
+    __ ExtractBits(i.TempRegister(3), i.InputRegister(2), 0, size,             \
                    sign_extend);                                               \
-    DCHECK_NE(i.TempRegister(2), i.OutputRegister(0));                         \
-    __ BranchShort(&exit, ne, i.TempRegister(2),                               \
+    DCHECK_NE(i.TempRegister(3), i.OutputRegister(0));                         \
+    __ BranchShort(&exit, ne, i.TempRegister(3),                               \
                    Operand(i.OutputRegister(0)));                              \
     __ InsertBits(i.TempRegister(2), i.InputRegister(3), i.TempRegister(1),    \
                   size);                                                       \

src/compiler/backend/riscv/instruction-selector-riscv32.cc
@@ -1039,13 +1039,14 @@ void VisitAtomicCompareExchange(InstructionSelector* selector, OpIndex node,
   inputs[input_count++] = g.UseUniqueRegister(new_value);
   InstructionOperand outputs[1];
   outputs[0] = g.UseUniqueRegister(node);
-  InstructionOperand temp[3];
+  InstructionOperand temp[4];
   temp[0] = g.TempRegister();
   temp[1] = g.TempRegister();
   temp[2] = g.TempRegister();
+  temp[3] = g.TempRegister();
   InstructionCode code = opcode | AddressingModeField::encode(addressing_mode) |
                          AtomicWidthField::encode(width);
-  selector->Emit(code, 1, outputs, input_count, inputs, 3, temp);
+  selector->Emit(code, 1, outputs, input_count, inputs, 4, temp);
 }
 
 void InstructionSelector::VisitWord32AtomicExchange(OpIndex node) {

src/compiler/backend/riscv/instruction-selector-riscv64.cc
@@ -2479,7 +2479,7 @@ void VisitAtomicCompareExchange(InstructionSelector* selector, OpIndex node,
 
   InstructionOperand outputs[] = {g.UseUniqueRegister(node)};
   InstructionOperand temps[] = {g.TempRegister(), g.TempRegister(),
-                                g.TempRegister()};
+                                g.TempRegister(), g.TempRegister()};
 
   InstructionCode code = opcode | AddressingModeField::encode(addressing_mode) |
                          AtomicWidthField::encode(width);