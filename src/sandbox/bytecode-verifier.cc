bc36a18736c48c238d41dac6c7243148d6559146
Arash Kazemi <arashk@chromium.org>
2026-09-21 09:22:16 +0000

    [bytecode-verifier] Disallow CopyDataPropertiesWithExcludedProperties
    Add kCopyDataPropertiesWithExcludedProperties to the bytecode verifier
    blocklist such that fuzzers don't report violations when calling it
    directly from bytecode. This method is never emitted from the bytecode
    compiler so it's safe to disallow.
    
    Fixed: 563441119
    Change-Id: Iba8d7f38b731b6b1e832b81b370823c64d90b791
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8435122
    Auto-Submit: Arash Kazemi <arashk@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#109971}

--- src/sandbox/bytecode-verifier.cc
+++ src/sandbox/bytecode-verifier.cc
@@ -295,6 +295,7 @@ bool BytecodeVerifier::IsAllowedRuntimeFunction(Runtime::FunctionId id) {
     case Runtime::kSwissTableValueAt:
     case Runtime::kSwissTableKeyAt:
     case Runtime::kDebugPrintPtr:
+    case Runtime::kCopyDataPropertiesWithExcludedPropertiesOnStack:
 #if V8_ENABLE_SPARKPLUG_PLUS
     case Runtime::kPatchCompareOpBaselineCode:
     case Runtime::kPatchCompareOpBaselineCodeAndThrow:
