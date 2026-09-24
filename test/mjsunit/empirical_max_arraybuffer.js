677277f42485942fc065d57ea61e25c0de488970
Omer Katz <omerkatz@chromium.org>
2026-09-24 15:04:00 +0200

    Fix empirical_max_arraybuffer.js mjsunit test
    The test started failing after crrev.com/c/8440878 which migrated d8 to
    using the same array buffer allocation strategy as chromium. After that
    CL, allocations of fixed-lenngth ArrayBuffers go through PA which is
    limiting sizes up to 2GB (assuming a large enough block is available).
    The test is trying to allocate close to 2GB, and PA fails to allocate it
    and crashes with OOM.
    
    The test was originally added to support allocating large wasm memory.
    Therefore, we fix the test by updating it to allocate a wasm memory,
    which doesn't go through PA and doesn't have the 2GB limitation.
    
    This both fixes the test and aligns it with its original intention.
    
    Bug: 565659766
    Change-Id: Ica9dc5e4efdb54883c0c92852626f53e0f9db973
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8458064
    Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#110068}

--- test/mjsunit/empirical_max_arraybuffer.js
+++ test/mjsunit/empirical_max_arraybuffer.js
@@ -2,25 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+d8.file.execute('test/mjsunit/wasm/wasm-module-builder.js');
+
 let kMax31BitSmi = (1 << 30) - 1;
 let k1MiB = 1 * 1024 * 1024;
 let k1GiB = 1 * 1024 * 1024 * 1024;
 let k4GiB = 4 * k1GiB;
-let kPageSize = 65536;
-let kMaxArrayBufferSize = 2 * k1GiB - kPageSize; // TODO(titzer): raise this to 4GiB
+let kMaxBufferSize = 2 * k1GiB - kPageSize; // TODO(titzer): raise this to 4GiB
 let kStrideLength = 65536;
 
 (function Test() {
+  if (typeof WebAssembly === 'undefined') return;
   var buffer;
   try {
-    buffer = new ArrayBuffer(kMaxArrayBufferSize);
+    let kPages = kMaxBufferSize / kPageSize;
+    let memory = new WebAssembly.Memory({initial: kPages, maximum: kPages});
+    buffer = memory.buffer;
   } catch (e) {
     print("OOM: sorry, best effort max array buffer size test!");
     return;
   }
 
   print("Allocated " + buffer.byteLength + " bytes");
-  assertEquals(kMaxArrayBufferSize, buffer.byteLength);
+  assertEquals(kMaxBufferSize, buffer.byteLength);
 
   function probe(view, stride, f) {
     print("--------------------");
@@ -37,11 +41,11 @@ let kStrideLength = 65536;
   {
     // Make an uint32 view and probe it.
     let elemSize = 4;
-    let viewSize = kMaxArrayBufferSize / elemSize;
+    let viewSize = kMaxBufferSize / elemSize;
     // TODO(titzer): view sizes are limited to 31 bit SMIs. fix.
     if (viewSize <= kMax31BitSmi) {
       let uint32 = new Uint32Array(buffer);
-      assertEquals(kMaxArrayBufferSize / elemSize, uint32.length);
+      assertEquals(kMaxBufferSize / elemSize, uint32.length);
       probe(uint32, kStrideLength / elemSize,
             i => (0xaabbccee ^ ((i >> 11) * 0x110005)) >>> 0);
     }
@@ -50,11 +54,11 @@ let kStrideLength = 65536;
   {
     // Make an uint16 view and probe it.
     let elemSize = 2;
-    let viewSize = kMaxArrayBufferSize / elemSize;
+    let viewSize = kMaxBufferSize / elemSize;
     // TODO(titzer): view sizes are limited to 31 bit SMIs. fix.
     if (viewSize <= kMax31BitSmi) {
       let uint16 = new Uint16Array(buffer);
-      assertEquals(kMaxArrayBufferSize / elemSize, uint16.length);
+      assertEquals(kMaxBufferSize / elemSize, uint16.length);
       probe(uint16, kStrideLength / elemSize,
             i => (0xccee ^ ((i >> 11) * 0x110005)) & 0xFFFF);
     }
@@ -63,11 +67,11 @@ let kStrideLength = 65536;
   {
     // Make an uint8 view and probe it.
     let elemSize = 1;
-    let viewSize = kMaxArrayBufferSize / elemSize;
+    let viewSize = kMaxBufferSize / elemSize;
     // TODO(titzer): view sizes are limited to 31 bit SMIs. fix.
     if (viewSize <= kMax31BitSmi) {
       let uint8 = new Uint8Array(buffer);
-      assertEquals(kMaxArrayBufferSize / elemSize, uint8.length);
+      assertEquals(kMaxBufferSize / elemSize, uint8.length);
       probe(uint8, kStrideLength / elemSize,
             i => (0xee ^ ((i >> 11) * 0x05)) & 0xFF);
     }
@@ -76,11 +80,11 @@ let kStrideLength = 65536;
   {
     // Make a float64 view and probe it.
     let elemSize = 8;
-    let viewSize = kMaxArrayBufferSize / elemSize;
+    let viewSize = kMaxBufferSize / elemSize;
     // TODO(titzer): view sizes are limited to 31 bit SMIs. fix.
     if (viewSize <= kMax31BitSmi) {
       let float64 = new Float64Array(buffer);
-      assertEquals(kMaxArrayBufferSize / elemSize, float64.length);
+      assertEquals(kMaxBufferSize / elemSize, float64.length);
       probe(float64, kStrideLength / elemSize,
             i => 0xaabbccee ^ ((i >> 11) * 0x110005));
     }
@@ -89,11 +93,11 @@ let kStrideLength = 65536;
   {
     // Make a float32 view and probe it.
     let elemSize = 4;
-    let viewSize = kMaxArrayBufferSize / elemSize;
+    let viewSize = kMaxBufferSize / elemSize;
     // TODO(titzer): view sizes are limited to 31 bit SMIs. fix.
     if (viewSize <= kMax31BitSmi) {
       let float32 = new Float32Array(buffer);
-      assertEquals(kMaxArrayBufferSize / elemSize, float32.length);
+      assertEquals(kMaxBufferSize / elemSize, float32.length);
       probe(float32, kStrideLength / elemSize,
             i => Math.fround(0xaabbccee ^ ((i >> 11) * 0x110005)));
     }
